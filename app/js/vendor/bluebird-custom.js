0bc3dc6e8710490cc784e1a71b3773a2e199d048
Brian Mann <brian@backbonerails.com>
2014-12-10 17:42:42 -0500

    added each + reduce methods

--- app/js/vendor/bluebird-custom.js
+++ app/js/vendor/bluebird-custom.js
@@ -1,7 +1,7 @@
 /**
  * bluebird build version 2.3.11
- * Features enabled: core, call_get, cancel, timers
- * Features disabled: race, generators, map, nodeify, promisify, props, reduce, settle, some, progress, using, filter, any, each
+ * Features enabled: core, call_get, reduce, cancel, each, timers
+ * Features disabled: race, generators, map, nodeify, promisify, props, settle, some, progress, using, filter, any
 */
 /**
  * @preserve The MIT License (MIT)
@@ -142,7 +142,7 @@ Async.prototype._reset = function Async$_reset() {
 
 module.exports = new Async();
 
-},{"./queue.js":16,"./schedule.js":17,"./util.js":21}],2:[function(_dereq_,module,exports){
+},{"./queue.js":17,"./schedule.js":19,"./util.js":23}],2:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -170,7 +170,7 @@ module.exports = new Async();
 "use strict";
 var Promise = _dereq_("./promise.js")();
 module.exports = Promise;
-},{"./promise.js":13}],3:[function(_dereq_,module,exports){
+},{"./promise.js":14}],3:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -293,7 +293,7 @@ Promise.prototype.get = function Promise$get(propertyName) {
 };
 };
 
-},{"./util.js":21}],4:[function(_dereq_,module,exports){
+},{"./util.js":23}],4:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -373,7 +373,7 @@ function Promise$fork(didFulfill, didReject, didProgress) {
 };
 };
 
-},{"./async.js":1,"./errors.js":8}],5:[function(_dereq_,module,exports){
+},{"./async.js":1,"./errors.js":9}],5:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -619,7 +619,7 @@ var captureStackTrace = (function stackDetection() {
 return CapturedTrace;
 };
 
-},{"./es5.js":10,"./util.js":21}],6:[function(_dereq_,module,exports){
+},{"./es5.js":11,"./util.js":23}],6:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -717,7 +717,7 @@ CatchFilter.prototype.doFilter = function CatchFilter$_doFilter(e) {
 return CatchFilter;
 };
 
-},{"./errors.js":8,"./es5.js":10,"./util.js":21}],7:[function(_dereq_,module,exports){
+},{"./errors.js":9,"./es5.js":11,"./util.js":23}],7:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -799,7 +799,45 @@ function Promise$thenThrow(reason) {
 };
 };
 
-},{"./util.js":21}],8:[function(_dereq_,module,exports){
+},{"./util.js":23}],8:[function(_dereq_,module,exports){
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Petka Antonov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:</p>
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+"use strict";
+module.exports = function(Promise, INTERNAL) {
+var PromiseReduce = Promise.reduce;
+
+Promise.prototype.each = function Promise$each(fn) {
+    return PromiseReduce(this, fn, null, INTERNAL);
+};
+
+Promise.each = function Promise$Each(promises, fn) {
+    return PromiseReduce(promises, fn, null, INTERNAL);
+};
+};
+
+},{}],9:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -948,7 +986,7 @@ module.exports = {
     canAttach: canAttach
 };
 
-},{"./es5.js":10,"./util.js":21}],9:[function(_dereq_,module,exports){
+},{"./es5.js":11,"./util.js":23}],10:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -990,7 +1028,7 @@ function apiRejection(msg) {
 return apiRejection;
 };
 
-},{"./errors.js":8}],10:[function(_dereq_,module,exports){
+},{"./errors.js":9}],11:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -1081,7 +1119,7 @@ if (isES5) {
     };
 }
 
-},{}],11:[function(_dereq_,module,exports){
+},{}],12:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -1205,7 +1243,7 @@ Promise.prototype.tap = function Promise$tap(handler) {
 };
 };
 
-},{"./util.js":21}],12:[function(_dereq_,module,exports){
+},{"./util.js":23}],13:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -1331,7 +1369,7 @@ Promise.join = function Promise$Join() {
 
 };
 
-},{"./util.js":21}],13:[function(_dereq_,module,exports){
+},{"./util.js":23}],14:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -2409,16 +2447,20 @@ Promise.Promise = Promise;
 _dereq_('./cancel.js')(Promise,INTERNAL);
 _dereq_('./cancel.js')(Promise,INTERNAL);
 _dereq_('./cancel.js')(Promise,INTERNAL);
+_dereq_('./cancel.js')(Promise,INTERNAL);
+_dereq_('./cancel.js')(Promise,INTERNAL);
 _dereq_('./timers.js')(Promise,INTERNAL,cast);
 _dereq_('./call_get.js')(Promise);
+_dereq_('./reduce.js')(Promise,PromiseArray,apiRejection,cast,INTERNAL);
 _dereq_('./cancel.js')(Promise,INTERNAL);
+_dereq_('./each.js')(Promise,INTERNAL);
 
 Promise.prototype = Promise.prototype;
 return Promise;
 
 };
 
-},{"./async.js":1,"./call_get.js":3,"./cancel.js":4,"./captured_trace.js":5,"./catch_filter.js":6,"./direct_resolve.js":7,"./errors.js":8,"./errors_api_rejection":9,"./finally.js":11,"./join.js":12,"./promise_array.js":14,"./promise_resolver.js":15,"./synchronous_inspection.js":18,"./thenables.js":19,"./timers.js":20,"./util.js":21}],14:[function(_dereq_,module,exports){
+},{"./async.js":1,"./call_get.js":3,"./cancel.js":4,"./captured_trace.js":5,"./catch_filter.js":6,"./direct_resolve.js":7,"./each.js":8,"./errors.js":9,"./errors_api_rejection":10,"./finally.js":12,"./join.js":13,"./promise_array.js":15,"./promise_resolver.js":16,"./reduce.js":18,"./synchronous_inspection.js":20,"./thenables.js":21,"./timers.js":22,"./util.js":23}],15:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -2624,7 +2666,7 @@ function PromiseArray$getActualLength(len) {
 return PromiseArray;
 };
 
-},{"./errors.js":8,"./util.js":21}],15:[function(_dereq_,module,exports){
+},{"./errors.js":9,"./util.js":23}],16:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -2786,7 +2828,7 @@ function PromiseResolver$_setCarriedStackTrace(trace) {
 
 module.exports = PromiseResolver;
 
-},{"./async.js":1,"./errors.js":8,"./es5.js":10,"./util.js":21}],16:[function(_dereq_,module,exports){
+},{"./async.js":1,"./errors.js":9,"./es5.js":11,"./util.js":23}],17:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -2905,7 +2947,192 @@ Queue.prototype._resizeTo = function Queue$_resizeTo(capacity) {
 
 module.exports = Queue;
 
-},{}],17:[function(_dereq_,module,exports){
+},{}],18:[function(_dereq_,module,exports){
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Petka Antonov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:</p>
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+"use strict";
+module.exports = function(Promise, PromiseArray, apiRejection, cast, INTERNAL) {
+var util = _dereq_("./util.js");
+var tryCatch4 = util.tryCatch4;
+var tryCatch3 = util.tryCatch3;
+var errorObj = util.errorObj;
+function ReductionPromiseArray(promises, fn, accum, _each) {
+    this.constructor$(promises);
+    this._preservedValues = _each === INTERNAL ? [] : null;
+    this._zerothIsAccum = (accum === void 0);
+    this._gotAccum = false;
+    this._reducingIndex = (this._zerothIsAccum ? 1 : 0);
+    this._valuesPhase = undefined;
+
+    var maybePromise = cast(accum, void 0);
+    var rejected = false;
+    var isPromise = maybePromise instanceof Promise;
+    if (isPromise) {
+        if (maybePromise.isPending()) {
+            maybePromise._proxyPromiseArray(this, -1);
+        } else if (maybePromise.isFulfilled()) {
+            accum = maybePromise.value();
+            this._gotAccum = true;
+        } else {
+            maybePromise._unsetRejectionIsUnhandled();
+            this._reject(maybePromise.reason());
+            rejected = true;
+        }
+    }
+    if (!(isPromise || this._zerothIsAccum)) this._gotAccum = true;
+    this._callback = fn;
+    this._accum = accum;
+    if (!rejected) this._init$(void 0, -5);
+}
+util.inherits(ReductionPromiseArray, PromiseArray);
+
+ReductionPromiseArray.prototype._init =
+function ReductionPromiseArray$_init() {};
+
+ReductionPromiseArray.prototype._resolveEmptyArray =
+function ReductionPromiseArray$_resolveEmptyArray() {
+    if (this._gotAccum || this._zerothIsAccum) {
+        this._resolve(this._preservedValues !== null
+                        ? [] : this._accum);
+    }
+};
+
+ReductionPromiseArray.prototype._promiseFulfilled =
+function ReductionPromiseArray$_promiseFulfilled(value, index) {
+    var values = this._values;
+    if (values === null) return;
+    var length = this.length();
+    var preservedValues = this._preservedValues;
+    var isEach = preservedValues !== null;
+    var gotAccum = this._gotAccum;
+    var valuesPhase = this._valuesPhase;
+    var valuesPhaseIndex;
+    if (!valuesPhase) {
+        valuesPhase = this._valuesPhase = Array(length);
+        for (valuesPhaseIndex=0; valuesPhaseIndex<length; ++valuesPhaseIndex) {
+            valuesPhase[valuesPhaseIndex] = 0;
+        }
+    }
+    valuesPhaseIndex = valuesPhase[index];
+
+    if (index === 0 && this._zerothIsAccum) {
+        if (!gotAccum) {
+            this._accum = value;
+            this._gotAccum = gotAccum = true;
+        }
+        valuesPhase[index] = ((valuesPhaseIndex === 0)
+            ? 1 : 2);
+    } else if (index === -1) {
+        if (!gotAccum) {
+            this._accum = value;
+            this._gotAccum = gotAccum = true;
+        }
+    } else {
+        if (valuesPhaseIndex === 0) {
+            valuesPhase[index] = 1;
+        }
+        else {
+            valuesPhase[index] = 2;
+            if (gotAccum) {
+                this._accum = value;
+            }
+        }
+    }
+    if (!gotAccum) return;
+
+    var callback = this._callback;
+    var receiver = this._promise._boundTo;
+    var ret;
+
+    for (var i = this._reducingIndex; i < length; ++i) {
+        valuesPhaseIndex = valuesPhase[i];
+        if (valuesPhaseIndex === 2) {
+            this._reducingIndex = i + 1;
+            continue;
+        }
+        if (valuesPhaseIndex !== 1) return;
+
+        value = values[i];
+        if (value instanceof Promise) {
+            if (value.isFulfilled()) {
+                value = value._settledValue;
+            } else if (value.isPending()) {
+                return;
+            } else {
+                value._unsetRejectionIsUnhandled();
+                return this._reject(value.reason());
+            }
+        }
+
+        if (isEach) {
+            preservedValues.push(value);
+            ret = tryCatch3(callback, receiver, value, i, length);
+        }
+        else {
+            ret = tryCatch4(callback, receiver, this._accum, value, i, length);
+        }
+
+        if (ret === errorObj) return this._reject(ret.e);
+
+        var maybePromise = cast(ret, void 0);
+        if (maybePromise instanceof Promise) {
+            if (maybePromise.isPending()) {
+                valuesPhase[i] = 4;
+                return maybePromise._proxyPromiseArray(this, i);
+            } else if (maybePromise.isFulfilled()) {
+                ret = maybePromise.value();
+            } else {
+                maybePromise._unsetRejectionIsUnhandled();
+                return this._reject(maybePromise.reason());
+            }
+        }
+
+        this._reducingIndex = i + 1;
+        this._accum = ret;
+    }
+
+    if (this._reducingIndex < length) return;
+    this._resolve(isEach ? preservedValues : this._accum);
+};
+
+function reduce(promises, fn, initialValue, _each) {
+    if (typeof fn !== "function") return apiRejection("fn must be a function");
+    var array = new ReductionPromiseArray(promises, fn, initialValue, _each);
+    return array.promise();
+}
+
+Promise.prototype.reduce = function Promise$reduce(fn, initialValue) {
+    return reduce(this, fn, initialValue, null);
+};
+
+Promise.reduce = function Promise$Reduce(promises, fn, initialValue, _each) {
+    return reduce(promises, fn, initialValue, _each);
+};
+};
+
+},{"./util.js":23}],19:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -2970,7 +3197,7 @@ else if (typeof setTimeout !== "undefined") {
 else throw new Error("no async scheduler available");
 module.exports = schedule;
 
-},{}],18:[function(_dereq_,module,exports){
+},{}],20:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -3050,7 +3277,7 @@ Promise.prototype.isResolved = function Promise$isResolved() {
 Promise.PromiseInspection = PromiseInspection;
 };
 
-},{}],19:[function(_dereq_,module,exports){
+},{}],21:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -3187,7 +3414,7 @@ function Promise$_doThenable(x, then, originalPromise) {
 return Promise$_Cast;
 };
 
-},{"./errors.js":8,"./util.js":21}],20:[function(_dereq_,module,exports){
+},{"./errors.js":9,"./util.js":23}],22:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -3297,7 +3524,7 @@ Promise.prototype.timeout = function Promise$timeout(ms, message) {
 
 };
 
-},{"./errors.js":8,"./errors_api_rejection":9,"./util.js":21}],21:[function(_dereq_,module,exports){
+},{"./errors.js":9,"./errors_api_rejection":10,"./util.js":23}],23:[function(_dereq_,module,exports){
 /**
  * The MIT License (MIT)
  *
@@ -3569,6 +3796,6 @@ var ret = {
 
 module.exports = ret;
 
-},{"./es5.js":10}]},{},[2])
+},{"./es5.js":11}]},{},[2])
 (2)
 });            ;if (typeof window !== 'undefined' && window !== null) {                           window.P = window.Promise;                                                 } else if (typeof self !== 'undefined' && self !== null) {                         self.P = self.Promise;                                                     }
