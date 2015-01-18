5c3513667000917196d3fcb01825d8b7c5a21aff
Sam Saccone <samccone@gmail.com>
2015-01-18 16:21:23 -0500

    Open ID generator from CLI invocation

--- bin/server_boot.js
+++ bin/server_boot.js
@@ -1,5 +1,16 @@
 require("coffee-script/register");
 
+open = require('open');
+settings = require("../lib/util/settings");
+
+projectRoot = process.argv[2];
+
 require('../lib/server')({
-  projectRoot: process.argv[2]
+  projectRoot: projectRoot
+}).then(function() {
+  return settings.read({projectRoot: projectRoot})
 })
+.then(function(settings) {
+  var port = settings.eclectus.port || 3000;
+  open("http://localhost:"+port+"/id_generator");
+});
