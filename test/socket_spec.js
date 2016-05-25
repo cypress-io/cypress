2080d7e5b77e2dd85c4860a3bf326dfc10448105
Brian Mann <brian@backbonerails.com>
2016-05-25 16:09:31 -0400

    added .getClientVersion + .getClientSource

--- test/socket_spec.js
+++ test/socket_spec.js
@@ -2,6 +2,7 @@ var fs     = require("fs")
 var server = require("socket.io")
 var client = require("socket.io-client")
 var expect = require("chai").expect
+var pkg    = require("../package.json")
 var lib    = require("../index")
 
 describe("Socket", function(){
@@ -21,6 +22,25 @@ describe("Socket", function(){
 
     it("makes sure socket.io.js actually exists", function(done){
       fs.stat(lib.getPathToClientSource(), done)
+    })
+  })
+
+  context(".getClientVersion", function(){
+    it("returns client version", function(){
+      expect(lib.getClientVersion()).to.eq(pkg.dependencies["socket.io-client"])
+    })
+  })
+
+  context(".getClientSource", function(){
+    it("returns client source as a string", function(done) {
+      var p = process.cwd() + "/node_modules/socket.io-client/socket.io.js"
+
+      fs.readFile(p, "utf8", function(err, str){
+        if (err) done(err)
+
+        expect(lib.getClientSource()).to.eq(str)
+        done()
+      })
     })
   })
 })
