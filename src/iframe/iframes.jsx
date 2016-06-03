7bf8e83f36dfbf68715e0cd632b3fdc05bd0e4c5
Chris Breiding <chrisbreiding@gmail.com>
2016-06-03 10:13:34 -0400

    refactor

--- src/iframe/iframes.jsx
+++ src/iframe/iframes.jsx
@@ -1,5 +1,6 @@
 /* global $ */
 
+import Promise from 'bluebird'
 import { observer } from 'mobx-react'
 import React, { Component } from 'react'
 
@@ -33,37 +34,44 @@ export default class Iframes extends Component {
   // jQuery is a better fit for managing these iframes, since they need to get
   // wiped out and reset on re-runs
   componentDidMount () {
-    const name = 'foobar' //App.config.get('projectName')
-    const specSrc = '/__cypress/runner/test.html' // should come from URL hash
-
-    const $container = $(this.refs.container)
-
-    const $autIframe = $('<iframe>', {
-      id: `Your App: '${name}'`,
-      class: 'aut-iframe',
-    })
-    .appendTo($container)
-
-    $autIframe.contents().find('body').append(blankContents)
-
-    const $specIframe = $('<iframe />', {
-      id: `Your Spec: '${specSrc}'`,
-      class: 'spec-iframe',
-    }).appendTo($container)
-
-    $specIframe.prop('src', specSrc).one('load', () => {
-      // make a reference between the iframes
-      // @contentWindow.remote = view.$remote[0].contentWindow
-
-      runner.run($specIframe[0].contentWindow, $autIframe)
-      // view.$el.show()
-      // view.calcWidth()
+    this._loadIframes().then(([specWindow, $autIframe]) => {
+      runner.run(specWindow, $autIframe)
     })
 
     const $window = $(window)
     $window.on('resize', () => {
       this.props.uiState.updateWindowDimensions($window.width(), $window.height())
     }).trigger('resize')
+  }
+
+  _loadIframes () {
+    return new Promise((resolve) => {
+      const name = '<Real Project Name Goes Here>' //App.config.get('projectName')
+      const specSrc = '/__cypress/runner/test.html' // should come from URL hash
+
+      const $container = $(this.refs.container)
+
+      const $autIframe = $('<iframe>', {
+        id: `Your App: '${name}'`,
+        class: 'aut-iframe',
+      }).appendTo($container)
+
+      $autIframe.contents().find('body').append(blankContents)
+
+      const $specIframe = $('<iframe />', {
+        id: `Your Spec: '${specSrc}'`,
+        class: 'spec-iframe',
+      }).appendTo($container)
+
+      $specIframe.prop('src', specSrc).one('load', () => {
+        // make a reference between the iframes
+        // @contentWindow.remote = view.$remote[0].contentWindow
+
+        resolve([$specIframe[0].contentWindow, $autIframe])
+        // view.$el.show()
+        // view.calcWidth()
+      })
+    })
   }
 
   componentWillUnmount () {
