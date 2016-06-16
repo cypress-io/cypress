05f1c9df5add4ee4cd36e0cc6c7c6bb98138d466
Chris Breiding <chrisbreiding@gmail.com>
2016-06-16 14:34:47 -0400

    utilize the latest zunder goodness

--- zunderfile.js
+++ zunderfile.js
@@ -1,11 +1,9 @@
 var vfs = require('vinyl-fs')
 var zunder = require('zunder')
 
-function copyFonts (dir) {
-  return function () {
-    vfs.src('bower_components/font-awesome/fonts/**').pipe(vfs.dest(dir + '/fonts'))
+zunder.setConfig({
+  staticGlobs: {
+    'static/**': '',
+    'bower_components/font-awesome/fonts/**': '/fonts',
   }
-}
-
-zunder.on('before:watch', copyFonts(zunder.config.devDir))
-zunder.on('before:build-prod', copyFonts(zunder.config.prodDir))
+})
