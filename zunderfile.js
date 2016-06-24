d37c30d13b040669827d51b7df2adcf3718d4487
Chris Breiding <chrisbreiding@gmail.com>
2016-06-24 10:18:14 -0400

    adjust build for being embedded

--- zunderfile.js
+++
@@ -1,9 +0,0 @@
-var vfs = require('vinyl-fs')
-var zunder = require('zunder')
-
-zunder.setConfig({
-  staticGlobs: {
-    'static/**': '',
-    'bower_components/font-awesome/fonts/**': '/fonts',
-  }
-})
