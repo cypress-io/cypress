a8b316fbc2e983d3bc1e5557f09e7f50545f336c
Chris Breiding <chrisbreiding@gmail.com>
2016-07-05 14:27:24 -0400

    alphabetize

--- src/test/command-model.js
+++ src/test/command-model.js
@@ -3,34 +3,34 @@ import { observable } from 'mobx'
 import Log from './log-model'
 
 export default class Command extends Log {
+  @observable displayMessage
   @observable error = null
   @observable event = false
   @observable number
   @observable numElements
   @observable indicator
-  @observable displayMessage
   @observable visible = true
 
   constructor (props) {
     super(props)
 
+    this.displayMessage = props.displayMessage
     this.error = props.error
     this.event = props.event
     this.number = props.number
     this.numElements = props.numElements
     this.indicator = props.indicator
-    this.displayMessage = props.displayMessage
     this.visible = props.visible
   }
 
   update (props) {
     super.update(props)
 
+    this.displayMessage = props.displayMessage
     this.error = props.error
     this.event = props.event
     this.numElements = props.numElements
     this.indicator = props.indicator
-    this.displayMessage = props.displayMessage
     this.visible = props.visible
   }
 
