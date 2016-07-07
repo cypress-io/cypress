1c5526192729fd765bd938e98396a4304a4b3648
Jennifer Mann <jennifermann17@gmail.com>
2016-07-07 12:05:15 -0400

    Commented out update banner logic

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 import Nav from '../nav/nav'
 import Footer from '../footer/footer'
-import UpdateBanner from '../update/update-banner'
+// import UpdateBanner from '../update/update-banner'
 
 export default ({ params, children }) => {
   return (
     <div>
       <Nav params={params}/>
       {children}
-      <UpdateBanner />
+      {
+        // <UpdateBanner />
+      }
       <Footer />
     </div>
   )
