ef7adbafbdf29a85943a51fecf62c3f74eec62c2
Chris Breiding <chrisbreiding@gmail.com>
2016-07-08 16:07:17 -0400

    update command renderProps and punt on errors for now

--- src/commands/command-model.js
+++ src/commands/command-model.js
@@ -1,43 +1,40 @@
 import _ from 'lodash'
-import { observable } from 'mobx'
+import { asStructure, observable } from 'mobx'
 import Log from '../instruments/log-model'
 
 export default class Command extends Log {
-  @observable displayMessage
-  @observable errorMessage = null
+  @observable renderProps = asStructure({})
+  // @observable errorMessage = null
   @observable event = false
   @observable isLongRunning = false
   @observable number
   @observable numElements
-  @observable indicator
   @observable visible = true
 
   constructor (props) {
     super(props)
 
-    this.displayMessage = props.displayMessage
-    this.errorMessage = props.error
+    // this.errorMessage = props.error
     this.event = props.event
     this.number = props.number
     this.numElements = props.numElements
-    this.indicator = props.indicator
+    this.renderProps = props.renderProps
     this.visible = props.visible
   }
 
   update (props) {
     super.update(props)
 
-    this.displayMessage = props.displayMessage
-    this.errorMessage = props.error
+    // this.errorMessage = props.error
     this.event = props.event
     this.numElements = props.numElements
-    this.indicator = props.indicator
+    this.renderProps = props.renderProps
     this.visible = props.visible
   }
 
   serialize () {
     return _.extend(super.serialize(), {
-      error: this.errorMessage,
+      // error: this.errorMessage,
       event: this.event,
       number: this.number,
       numElements: this.numElements,
