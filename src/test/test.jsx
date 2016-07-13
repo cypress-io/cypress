722f0ffb2ec7076755090ae08c3331343e722089
Chris Breiding <chrisbreiding@gmail.com>
2016-07-13 14:35:47 -0400

    handle test clicking differently than other collapsibles

--- src/test/test.jsx
+++ src/test/test.jsx
@@ -1,5 +1,7 @@
+import cs from 'classnames'
+import { action, observable } from 'mobx'
 import { observer } from 'mobx-react'
-import React from 'react'
+import React, { Component } from 'react'
 import Tooltip from '../tooltip/tooltip'
 
 import events from '../lib/events'
@@ -9,7 +11,6 @@ import runnablesStore from '../runnables/runnables-store'
 import Hooks from '../hooks/hooks'
 import Agents from '../agents/agents'
 import Routes from '../routes/routes'
-import Collapsible from '../collapsible/collapsible'
 import FlashOnClick from '../lib/flash-on-click'
 
 const NoCommands = observer(() => (
@@ -20,39 +21,62 @@ const NoCommands = observer(() => (
   </ul>
 ))
 
-const TestHeader = observer(({ model }) => (
-  <span>
-    <i className='runnable-state fa'></i>
-    <span className='runnable-title'>{model.title}</span>
-    <div className='runnable-controls'>
-      <Tooltip placement='left' align={{ offset: [0, 0] }} title='One or more commands failed'>
-        <i className='fa fa-warning'></i>
-      </Tooltip>
-    </div>
-  </span>
-))
+@observer
+class Test extends Component {
+  @observable isOpen = null
 
-const Test = observer(({ model }) => (
-  <div className='runnable-wrapper' style={{ paddingLeft: indent(model.level) }}>
-    <Collapsible
-      header={<TestHeader model={model} />}
-      headerClass='runnable-content-region'
-      contentClass='runnable-instruments'
-      isOpen={model.state === 'failed' || model.isLongRunning || runnablesStore.hasSingleTest}
-    >
-      <Agents model={model} />
-      <Routes model={model} />
-      <div className='runnable-commands-region'>
-        {model.commands.length ? <Hooks model={model} /> : <NoCommands />}
+  render () {
+    const { model } = this.props
+
+    return (
+      <div
+        className={cs('runnable-wrapper', { 'is-open': this._shouldBeOpen() })}
+        onClick={this._toggleOpen}
+        style={{ paddingLeft: indent(model.level) }}
+      >
+        <div className='runnable-content-region'>
+          <i className='runnable-state fa'></i>
+          <span className='runnable-title'>{model.title}</span>
+          <div className='runnable-controls'>
+            <Tooltip placement='left' align={{ offset: [0, 0] }} title='One or more commands failed'>
+              <i className='fa fa-warning'></i>
+            </Tooltip>
+          </div>
+        </div>
+        <div
+          className='runnable-instruments collapsible-content'
+          onClick={(e) => { e.stopPropagation() }}
+        >
+          <Agents model={model} />
+          <Routes model={model} />
+          <div className='runnable-commands-region'>
+            {model.commands.length ? <Hooks model={model} /> : <NoCommands />}
+          </div>
+        </div>
+        <FlashOnClick
+          message='Printed output to your console!'
+          onClick={() => events.emit('show:error', model.id)}
+        >
+          <pre className='test-error'>{model.err.displayMessage}</pre>
+        </FlashOnClick>
       </div>
-    </Collapsible>
-    <FlashOnClick
-      message='Printed output to your console!'
-      onClick={() => events.emit('show:error', model.id)}
-    >
-      <pre className='test-error'>{model.err.displayMessage}</pre>
-    </FlashOnClick>
-  </div>
-))
+    )
+  }
+
+  _shouldBeOpen () {
+    // means isOpen has been explicitly set by the user clicking the test
+    if (this.isOpen !== null) return this.isOpen
+
+    return this.props.model.state === 'failed' || this.props.model.isLongRunning || runnablesStore.hasSingleTest
+  }
+
+  @action _toggleOpen = () => {
+    if (this.isOpen === null) {
+      this.isOpen = !this._shouldBeOpen()
+    } else {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
 
 export default Test
