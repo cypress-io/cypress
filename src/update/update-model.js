76599438d525b977625d67e2d38892ff27a4f5a2
Jennifer Mann <jennifermann17@gmail.com>
2016-07-13 10:08:17 -0400

    pushed to get specs to pass in CI

--- src/update/update-model.js
+++ src/update/update-model.js
@@ -2,7 +2,6 @@ import { computed, observable, action } from 'mobx'
 
 class Updater {
   @observable version
-  @observable stateFormatted
   @observable finished = false
 
   @action setVersion (version) {
@@ -17,25 +16,25 @@ class Updater {
     this.state = state
   }
 
-  @computed get state () {
+  @computed get stateFormatted () {
     switch (this.state) {
       case 'checking':
-        this.stateFormatted = 'Checking for updates...'
+        'Checking for updates...'
         break
       case 'downloading':
-        this.stateFormatted = 'Downloading updates...'
+        'Downloading updates...'
         break
       case 'applying':
-        this.stateFormatted = 'Applying updates...'
+        'Applying updates...'
         break
       case 'done':
-        this.stateFormatted = 'Updates ready'
+        'Updates ready'
         break
       case 'none':
-        this.stateFormatted = 'No updates available'
+        'No updates available'
         break
       case 'error':
-        this.stateFormatted = 'An error occurred updating'
+        'An error occurred updating'
         break
       default:
         return
