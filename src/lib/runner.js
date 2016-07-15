454ee31c0c17eddccb3c9e6a9bc81b5a87956ebb
Brian Mann <brian@backbonerails.com>
2016-07-15 13:30:07 -0400

    rename events for clarity

--- src/lib/runner.js
+++ src/lib/runner.js
@@ -21,7 +21,7 @@ channel.on('connect', () => {
 
 const driverToReporterEvents = 'paused'.split(' ')
 const driverToLocalAndReporterEvents = 'run:start run:end'.split(' ')
-const driverToSocketEvents = 'fixture request history:entries exec domain:set domain:change'.split(' ')
+const driverToSocketEvents = 'fixture request history:entries exec set:domain preserve:run:state'.split(' ')
 const driverTestEvents = 'test:before:run test:after:run'.split(' ')
 const driverAutomationEvents = 'get:cookies get:cookie set:cookie clear:cookies clear:cookie take:screenshot'.split(' ')
 const driverToLocalEvents = 'viewport config stop url:changed page:loading'.split(' ')
