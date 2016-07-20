7484806c5cc8d7cb4e878fe52c09a5ce7c9ebe47
Chris Breiding <chrisbreiding@gmail.com>
2016-07-20 15:04:20 -0400

    remove status from route model, it's not used

--- src/routes/route-model.js
+++ src/routes/route-model.js
@@ -5,7 +5,6 @@ export default class Route extends Instrument {
   @observable isStubbed
   @observable method
   @observable numResponses = 0
-  @observable status
   @observable url
 
   constructor (props) {
@@ -14,7 +13,6 @@ export default class Route extends Instrument {
     this.isStubbed = props.isStubbed
     this.method = props.method
     this.numResponses = props.numResponses
-    this.status = props.status
     this.url = props.url
   }
 
@@ -24,7 +22,6 @@ export default class Route extends Instrument {
     this.isStubbed = props.isStubbed
     this.method = props.method
     this.numResponses = props.numResponses
-    this.status = props.status
     this.url = props.url
   }
 }
