7ec640dd9d6f3d4bef89cb28dcb94236b24f0e2b
Chris Breiding <chrisbreiding@gmail.com>
2016-08-01 11:23:11 -0400

    make header responsive

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -11,7 +11,8 @@ const Header = observer(({ events, statsStore }) => (
   <header>
     <Tooltip placement='bottom' title='View All Tests'>
       <button className='focus-tests' onClick={() => events.emit('focus:tests')}>
-        <i className='fa fa-chevron-left'></i> All Tests
+        <i className='fa fa-chevron-left'></i>
+        <span>Tests</span>
       </button>
     </Tooltip>
     <Stats stats={statsStore} />
