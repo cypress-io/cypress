e7493005fb0ea4260a750ba6e67110922fe92be1
Chris Breiding <chrisbreiding@gmail.com>
2016-08-04 09:52:51 -0400

    add unit tests for <Portal />

--- src/lib/portal.jsx
+++ src/lib/portal.jsx
@@ -2,23 +2,21 @@ import _ from 'lodash'
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-let idNum = 0
-
-export default class Portal extends Component {
+class Portal extends Component {
   componentDidMount () {
-    const id = `portal-${idNum++}`
+    const id = `portal-${Portal.idNum++}`
     let element = document.getElementById(id)
     if (!element) {
       element = document.createElement('div')
       element.id = id
       document.body.appendChild(element)
     }
-    this.element = element
+    this._element = element
     this.componentDidUpdate()
   }
 
   componentWillUnmount () {
-    document.body.removeChild(this.element)
+    document.body.removeChild(this._element)
   }
 
   componentDidUpdate () {
@@ -26,10 +24,14 @@ export default class Portal extends Component {
       <div ref={(node) => this.domNode = node} {..._.omit(this.props, 'children')}>
         {this.props.children}
       </div>
-    ), this.element)
+    ), this._element)
   }
 
   render () {
     return null
   }
 }
+
+Portal.idNum = 0
+
+export default Portal
