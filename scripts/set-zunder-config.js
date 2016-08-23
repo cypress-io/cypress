3678aece665c0aee98e4ec13abcdf6428485f37b
Chris Breiding <chrisbreiding@gmail.com>
2016-08-23 14:16:22 -0400

    change built file names to runner

--- scripts/set-zunder-config.js
+++ scripts/set-zunder-config.js
@@ -1,6 +1,8 @@
 module.exports = function setZunderConfig (zunder) {
   zunder.setConfig({
     resolutions: ['react', 'react-dom', 'mobx', 'mobx-react'],
+    scriptName: 'runner.js',
+    stylesheetName: 'runner.css',
     staticGlobs: {
       'static/**': '',
       'bower_components/font-awesome/fonts/**': '/fonts',
