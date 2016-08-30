74a28c4019fc40f03419a5ef05830db4250ffc00
Brian Mann <brian@backbonerails.com>
2016-08-30 14:51:00 -0400

    removed bower, just use npm, updated all paths

--- zunderfile.js
+++ zunderfile.js
@@ -8,17 +8,17 @@ function copyScripts (dir) {
   return function () {
     return Promise.all([
       gulp.src([
-        "bower_components/jquery/dist/jquery.js",
-        "bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js",
-        "bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert.js",
+        "node_modules/jquery/dist/jquery.js",
+        "node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js",
+        "node_modules/bootstrap-sass/assets/javascripts/bootstrap/alert.js",
       ])
       .pipe(concat('vendor.js'))
       .pipe(gulp.dest(dir)),
 
-      gulp.src("bower_components/fira/woff/**/*")
+      gulp.src("node_modules/fira/woff/**/*")
         .pipe(gulp.dest(dir + "/woff")),
 
-      gulp.src("bower_components/font-awesome/fonts/**")
+      gulp.src("node_modules/font-awesome/fonts/**")
         .pipe(gulp.dest(dir + "/fonts")),
 
       gulp.src(cyIcons.getPathToLogo('cypress-inverse.png'))
