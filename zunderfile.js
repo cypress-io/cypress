f288e0c9cebce6af2c74e6909a40d5d81a0b5676
Chris Breiding <chrisbreiding@gmail.com>
2016-09-06 10:19:50 -0400

    use zunder config instead of manually copying files

--- zunderfile.js
+++ zunderfile.js
@@ -1,29 +1,29 @@
 var gulp = require("gulp")
-var concat  = require('gulp-concat')
+var concat = require('gulp-concat')
 var zunder = require("zunder")
-var Promise = require('bluebird')
 var cyIcons = require('@cypress/core-icons')
+
+var staticGlobs = {
+  'node_modules/fira/woff/**/*': '/woff',
+  'node_modules/font-awesome/fonts/*.+(eot|svg|ttf|woff|woff2|otf)': '/fonts',
+}
+staticGlobs[cyIcons.getPathToLogo('cypress-inverse.png')] = '/img'
+
+zunder.setConfig({
+  prodDir: 'dist',
+  staticGlobs: staticGlobs
+})
+
 
 function copyScripts (dir) {
   return function () {
-    return Promise.all([
-      gulp.src([
-        "node_modules/jquery/dist/jquery.js",
-        "node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js",
-        "node_modules/bootstrap-sass/assets/javascripts/bootstrap/alert.js",
-      ])
-      .pipe(concat('vendor.js'))
-      .pipe(gulp.dest(dir)),
-
-      gulp.src("node_modules/fira/woff/**/*")
-        .pipe(gulp.dest(dir + "/woff")),
-
-      gulp.src("node_modules/font-awesome/fonts/**")
-        .pipe(gulp.dest(dir + "/fonts")),
-
-      gulp.src(cyIcons.getPathToLogo('cypress-inverse.png'))
-        .pipe(gulp.dest(dir + '/img')),
+    return gulp.src([
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js",
+      "node_modules/bootstrap-sass/assets/javascripts/bootstrap/alert.js",
     ])
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(dir))
   }
 }
 
