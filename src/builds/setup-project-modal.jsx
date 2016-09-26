202e299f9bcb927521e07e4d54cd39a1ab90b6a4
Jennifer Mann <jennifermann17@gmail.com>
2016-09-26 17:16:04 -0400

    Added more form styles to setup project dialog

--- src/builds/setup-project-modal.jsx
+++ src/builds/setup-project-modal.jsx
@@ -23,40 +23,47 @@ class SetupProject extends Component {
         >
         <div className='modal-body os-dialog'>
           <form
-            className="form-horizontal"
+            className='form-horizontal'
             onSubmit={this.props.onConfirm}>
-            <div className="form-group">
-              <label htmlFor="projectName" className="col-sm-4 control-label">
+            <div className='form-group'>
+              <label htmlFor='projectName' className='col-sm-4 control-label'>
                 Project Name:
               </label>
-              <div className="col-sm-8">
+              <div className='col-sm-6'>
                 <input
-                  type="text"
-                  className="form-control"
-                  id="projectName"
+                  type='text'
+                  className='form-control'
+                  id='projectName'
                   defaultValue={this._projectName()}/>
               </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="projectName" className="col-sm-4 control-label">
+            <div className='form-group'>
+              <label htmlFor='projectName' className='col-sm-4 control-label'>
                 Account:
               </label>
-              <div className="col-sm-8">
-                <input type="text" className="form-control" id="projectName" />
+              <div className='col-sm-6'>
+                <select className='form-control'>
+                  <option>Jane Lane</option>
+                  <option>ACME Developers</option>
+                </select>
+              </div>
+              <div className='col-sm-2'>
+                <a href='#' onClick=''>manage</a>
               </div>
             </div>
-            <div className="form-group">
-              <div className="col-sm-offset-4 col-sm-8">
-                <div className="checkbox">
+            <div className='form-group'>
+              <div className='col-sm-offset-4 col-sm-8'>
+                <div className='checkbox'>
                   <label>
-                    <input type="checkbox"/> Make Builds Private
+                    <input type='checkbox'/>Make Builds Private
                   </label>
+                  <small className='text-muted'>{' '}This requires a paid account with Cypress</small>
                 </div>
               </div>
             </div>
-            <div className="form-group">
-              <div className="col-sm-offset-4 col-sm-8">
-                <button type="submit" className="btn btn-primary btn-sm">
+            <div className='form-group'>
+              <div className='col-sm-offset-4 col-sm-8'>
+                <button type='submit' className='btn btn-primary'>
                   Setup Project
                 </button>
               </div>
