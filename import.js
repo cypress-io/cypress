2aacd4fc294ef7ba80103dd904f526d63e0ef398
Chris Breiding <chrisbreiding@gmail.com>
2016-10-06 13:09:09 -0400

    reference where git commands came from

--- import.js
+++ import.js
@@ -1,3 +1,5 @@
+// http://stackoverflow.com/questions/1683531/how-to-import-existing-git-repository-into-another#answer-8396318
+
 const { snakeCase } = require('lodash')
 const { execSync } = require('child_process')
 const argv = require('yargs').argv
