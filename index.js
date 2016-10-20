9fa4b98451667df983f2e315c724f752888709e6
Chris Breiding <chrisbreiding@gmail.com>
2016-10-20 09:41:53 -0400

    make path-adding more explicit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("app-module-path/register")
+require("app-module-path").addPath(__dirname)
 require("coffee-script/register")
 
 module.exports = require("./lib").start(process.argv.slice(2))
