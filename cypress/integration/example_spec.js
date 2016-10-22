2efcfa3a8ddd7588cba7165959bfc4b4e9a1cf6a
Brian Mann <brian@backbonerails.com>
2016-10-22 14:59:37 -0400

    bump default config values

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1423,14 +1423,14 @@ describe('Kitchen Sink', function(){
       expect(myConfig).to.have.property('baseUrl', null)
       expect(myConfig).to.have.property('defaultCommandTimeout', 4000)
       expect(myConfig).to.have.property('requestTimeout', 5000)
-      expect(myConfig).to.have.property('responseTimeout', 20000)
+      expect(myConfig).to.have.property('responseTimeout', 30000)
       expect(myConfig).to.have.property('viewportHeight', 660)
       expect(myConfig).to.have.property('viewportWidth', 1000)
-      expect(myConfig).to.have.property('pageLoadTimeout', 30000)
+      expect(myConfig).to.have.property('pageLoadTimeout', 60000)
       expect(myConfig).to.have.property('waitForAnimations', true)
 
       // *** get a single configuration option **
-      expect(Cypress.config('pageLoadTimeout')).to.eq(30000)
+      expect(Cypress.config('pageLoadTimeout')).to.eq(60000)
 
       // *** set a single configuration option **
       //
@@ -1440,7 +1440,7 @@ describe('Kitchen Sink', function(){
 
       expect(Cypress.config('pageLoadTimeout')).to.eq(20000)
 
-      Cypress.config('pageLoadTimeout', 30000)
+      Cypress.config('pageLoadTimeout', 60000)
     })
   })
 
