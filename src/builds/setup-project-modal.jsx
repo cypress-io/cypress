b49de4afddc0a144f17ac91373c686e605d8615d
Jennifer Mann <jennifermann17@gmail.com>
2016-11-03 15:45:00 -0400

    Worked on styling of adding a new project to CI modal.

--- src/builds/setup-project-modal.jsx
+++ src/builds/setup-project-modal.jsx
@@ -22,16 +22,17 @@ class SetupProject extends Component {
         backdrop='static'
         >
         <div className='modal-body os-dialog'>
+          <BootstrapModal.Dismiss className='btn btn-link close'>x</BootstrapModal.Dismiss>
           <h4>Setup Project for CI</h4>
           <p className='text-muted'>After configuring your project's settings, we will generate a secret key to be used during your CI run.</p>
           <form
             className='form-horizontal'
             onSubmit={this.props.onConfirm}>
             <div className='form-group'>
-              <label htmlFor='projectName' className='col-sm-4 control-label'>
+              <label htmlFor='projectName' className='col-sm-3 control-label'>
                 Project Name:
               </label>
-              <div className='col-sm-6'>
+              <div className='col-sm-7'>
                 <input
                   type='text'
                   className='form-control'
@@ -40,30 +41,35 @@ class SetupProject extends Component {
               </div>
             </div>
             <div className='form-group'>
-              <label htmlFor='projectName' className='col-sm-4 control-label'>
-                Account:
+              <label htmlFor='projectName' className='col-sm-3 control-label'>
+                Organization:
               </label>
-              <div className='col-sm-6'>
+              <div className='col-sm-7'>
                 <select className='form-control'>
                   <option>Jane Lane</option>
                   <option>ACME Developers</option>
                 </select>
               </div>
-              <div className='col-sm-2'>
-                <a href='#' onClick=''>manage</a>
+              <div className='col-sm-2 no-left-padding'>
+                <button href='#' className='btn btn-link manage-orgs-btn' onClick=''>manage</button>
               </div>
             </div>
             <div className='form-group'>
-              <div className='col-sm-offset-4 col-sm-8'>
+              <label htmlFor='projectName' className='col-sm-3 control-label'>
+                Access:
+              </label>
+              <div className='col-sm-9'>
                 <div className='checkbox'>
                   <label>
-                    <input type='checkbox'/>Make Builds Private
+                    <input type='checkbox'/>
+                    Make Builds Private{' '}
+                    <small>(Requires paid Cypress account)</small>
                   </label>
                 </div>
               </div>
             </div>
             <div className='form-group'>
-              <div className='col-sm-offset-4 col-sm-8'>
+              <div className='col-sm-offset-8 col-sm-4'>
                 <button type='submit' className='btn btn-primary'>
                   Setup Project
                 </button>
