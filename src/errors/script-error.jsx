1f00ed6b7f604452cc0d3b00a93eef5fe238ad0e
Chris Breiding <chrisbreiding@gmail.com>
2016-11-14 13:38:19 -0500

    add support folder error

--- src/errors/script-error.jsx
+++ src/errors/script-error.jsx
@@ -1,26 +1,52 @@
 import { observer } from 'mobx-react'
 import React from 'react'
 
+const BundleError = ({ specPath }) => (
+  <div>
+    <h4>
+      <i className='fa fa-warning'></i>
+      Oops... we found an error preparing your spec file:
+    </h4>
+    <pre>{specPath.replace(/\//g, ' / ')}</pre>
+    <p>Cypress automatically compiles and bundles your test code so you can use ES2015, JS modules, CoffeeScript, etc (<a href='https://on.cypress.io/guides/test-transpilation' target='_blank' rel='noopener noreferrer'>more details here</a>). This can get tripped up by the following:</p>
+    <ul>
+      <li>The file missing</li>
+      <li>A syntax error in the file or one of its dependencies</li>
+      <li>A missing dependency</li>
+    </ul>
+    <p>Fix the error in your code to clear this error and re-run your tests.</p>
+  </div>
+)
 
+const SupportFolderError = () => (
+  <div>
+    <h4>
+      <i className='fa fa-warning'></i>
+      The supportFolder option has been removed
+    </h4>
+    <p>We noticed that you have explicity set the supportFolder option in your configuration. This option is no supported and has been replaced by the supportScripts option.</p>
+    <p>Please update your configuration to use the supportScripts option. <a href='https://on.cypress.io/guides/support-scripts' target='_blank' rel='noopener noreferrer'>Read more about supportScripts here</a></p>
+  </div>
+)
 
 const ScriptError = observer(({ specPath, state }) => {
   if (!state.scriptError) return null
 
+  function error () {
+    switch (state.scriptError.type) {
+      case "BUNDLE_ERROR":
+        return <BundleError specPath={specPath} />
+      case "SUPPORT_FOLDER":
+        return <SupportFolderError />
+      default:
+        return null
+    }
+  }
+
   return (
     <div className='runner script-error'>
       <aside style={{ width: state.reporterWidth }}>
-        <h4>
-          <i className='fa fa-warning'></i>
-          Oops... we found an error preparing your spec file:
-        </h4>
-        <pre>{specPath.split('/').join(' / ')}</pre>
-        <p>Cypress automatically compiles and bundles your test code so you can use ES2015, JS modules, CoffeeScript, etc (<a href='https://on.cypress.io/guides/test-transpilation' target='_blank' rel='noopener noreferrer'>more details here</a>). This can get tripped up by the following:</p>
-        <ul>
-          <li>The file missing</li>
-          <li>A syntax error in the file or one of its dependencies</li>
-          <li>A missing dependency</li>
-        </ul>
-        <p>Fix the error in your code to clear this error and re-run your tests.</p>
+        {error()}
       </aside>
       <main style={{ marginTop: state.headerHeight }}>
         <pre>{state.scriptError.error.replace(/\{newline\}/g, '\n')}</pre>
--- src/errors/script-error.spec.jsx
+++ src/errors/script-error.spec.jsx
@@ -3,28 +3,10 @@ import { shallow } from 'enzyme'
 
 import ScriptError from './script-error'
 
-const stateWithError = {
-  scriptError: {
-    error: 'Error is{newline}this{newline}error',
-  },
-}
-
 describe('<ScriptError />', () => {
   it('renders nothing when there is no script error', () => {
     const state = { scriptError: null }
     const component = shallow(<ScriptError state={state} />)
     expect(component).to.be.empty
-  })
-
-  it('splits the spec path by / and displays it', () => {
-    const state = stateWithError
-    const component = shallow(<ScriptError state={state} specPath='/foo/bar' />)
-    expect(component.find('aside pre').text()).to.include('/ foo / bar')
-  })
-
-  it('replaces {newline} placeholders in error stack', () => {
-    const state = stateWithError
-    const component = shallow(<ScriptError state={state} specPath='/foo/bar' />)
-    expect(component.find('main pre').text()).to.include('Error is\nthis\nerror')
   })
 })
