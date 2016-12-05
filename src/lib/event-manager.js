4897e58f0523db7503382e7ed78cb464213617ad
Chris Breiding <chrisbreiding@gmail.com>
2016-12-05 10:44:17 -0500

    stop test run when there’s a script error

--- src/lib/event-manager.js
+++ src/lib/event-manager.js
@@ -22,7 +22,7 @@ const driverToLocalAndReporterEvents = 'run:start run:end'.split(' ')
 const driverToSocketEvents = 'fixture request history:entries exec resolve:url preserve:run:state read:file write:file'.split(' ')
 const driverTestEvents = 'test:before:run test:after:run'.split(' ')
 const driverAutomationEvents = 'get:cookies get:cookie set:cookie clear:cookies clear:cookie take:screenshot'.split(' ')
-const driverToLocalEvents = 'viewport config stop url:changed page:loading visit:failed script:error'.split(' ')
+const driverToLocalEvents = 'viewport config stop url:changed page:loading visit:failed'.split(' ')
 const socketRerunEvents = 'runner:restart watched:file:changed'.split(' ')
 
 const localBus = new EventEmitter()
@@ -188,6 +188,11 @@ export default {
 
     _.each(driverToLocalEvents, (event) => {
       driver.on(event, (...args) => localBus.emit(event, ...args))
+    })
+
+    driver.on('script:error', (err) => {
+      driver.abort()
+      localBus.emit('script:error', err)
     })
 
     _.each(socketRerunEvents, (event) => {
