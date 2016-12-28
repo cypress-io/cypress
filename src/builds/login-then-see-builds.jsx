216543c8c68642d7c1727fe7754d78228ae61c88
Jennifer Mann <jennifermann17@gmail.com>
2016-12-28 11:44:54 -0500

    commented out unused 'login' code

--- src/builds/login-then-see-builds.jsx
+++ src/builds/login-then-see-builds.jsx
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router'
-import { observer } from 'mobx-react'
+// THIS IS NOT CURRENTLY IN USE WHILE
+// LOGIN IS RESTRICTED ON THE APP
 
-@observer
-class LoginMessage extends Component {
-  render () {
-    return (
-      <div id='builds-list-page'>
-        <div className="empty">
-          <h4>Log in to see Builds</h4>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Nullam id dolor id nibh ut id elit.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl <a href="">ultricies vehicula</a>.</p>
-          <div>
-            <Link
-              className='btn btn-black'
-              to='/login'
-              >
-              <i className='fa fa-sign-in'></i>{' '}
-              Log In
-            </Link>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+// import React, { Component } from 'react'
+// import { Link } from 'react-router'
+// import { observer } from 'mobx-react'
 
-export default LoginMessage
+// @observer
+// class LoginMessage extends Component {
+//   render () {
+//     return (
+//       <div id='builds-list-page'>
+//         <div className="empty">
+//           <h4>Log in to see Builds</h4>
+//           <p>Aenean lacinia bibendum nulla sed consectetur. Nullam id dolor id nibh ut id elit.</p>
+//           <p>Praesent commodo cursus magna, vel scelerisque nisl <a href="">ultricies vehicula</a>.</p>
+//           <div>
+//             <Link
+//               className='btn btn-black'
+//               to='/login'
+//               >
+//               <i className='fa fa-sign-in'></i>{' '}
+//               Log In
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default LoginMessage
