e9b3ff74bcf56d90eae4d24a9a94a884bf8c9160
Jennifer Mann <jennifermann17@gmail.com>
2017-04-19 16:28:17 -0400

    moved convert script out of scripts folder (since hexo auto reads scripts) & converted to JS

---
+++ cy_scripts/convert.js
@@ -0,0 +1,104 @@
+var _ = require('lodash')
+var fs = require('fs-extra')
+var str = require('underscore.string')
+var path = require('path')
+var glob = require('glob')
+var Promise = require('bluebird')
+
+var fs = Promise.promisifyAll(fs)
+var glob = Promise.promisify(glob)
+
+var startsWithNumberAndDashRe = /(\d+-)/
+var excerptRe = /excerpt:.+/
+var newLinesRe = /\n{3,}/
+
+var LOOKUP = {
+  guides: 'v0.0',
+  api: 'v1.0',
+}
+
+getFolderFromFile = function(file) {
+  return path.basename(path.resolve(file, '..'))
+}
+
+getNameFromFile = function(file) {
+  return path.basename(file)
+}
+
+normalize = function(string) {
+  var string = string.replace(startsWithNumberAndDashRe, '').toLowerCase()
+
+  return str.dasherize(string)
+}
+
+find = function(type) {
+  var folder = LOOKUP[type]
+  var globstar = path.join('cypress', folder, 'documentation', '**', '*.md')
+
+  return glob(globstar, {
+    realpath: true
+  })
+}
+
+transfer = function(type) {
+  return find(type).then(function(files) {
+    if (files == null) {
+      files = []
+    }
+
+    return _.filter(files, fileStartsWithNumberAndDash)
+  }).map(function(file) {
+
+    var name = normalize(getNameFromFile(file))
+    var folder = normalize(getFolderFromFile(file))
+    var dest = path.join('source', type, folder, name)
+
+    return copy(file, dest)
+    .then(function() {
+      return fs.readFileAsync(dest, 'utf8')
+    })
+    .then(function(string) {
+      return string.replace('slug:', 'title:').replace(excerptRe, '---')
+    })
+    .then(function(string) {
+      var contentsIndex = string.indexOf('# Contents')
+      var dividerIndex = string.indexOf('***') + 3
+      var chunkToRemove = string.slice(contentsIndex, dividerIndex)
+
+      return string.split(chunkToRemove).join('').split(newLinesRe).join('\n\n')
+    })
+      .then(function(string) {
+      return fs.writeFileAsync(dest, string)
+    })
+  })
+}
+
+copy = function(src, dest) {
+  return fs.copyAsync(src, dest, {
+    overwrite: true
+  })
+}
+
+fileStartsWithNumberAndDash = function(file) {
+  var name = getNameFromFile(file)
+  return startsWithNumberAndDashRe.test(name)
+}
+
+transferIncomplete = function() {
+  return find('guides').then(function(files) {
+    if (files == null) {
+      files = []
+    }
+    return _.reject(files, fileStartsWithNumberAndDash)
+  }).map(function(file) {
+    var dest, folder, name
+    name = getNameFromFile(file)
+    folder = getFolderFromFile(file)
+    dest = path.join('source', 'incomplete', folder, name)
+    return copy(file, dest)
+  })
+}
+
+transferIncomplete().then(function() {
+  return Promise.all([transfer('api'), transfer('guides')])
+})
