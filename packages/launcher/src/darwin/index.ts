b9f1f92b938737cec6888b77180c205ec1ae4da0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-09 13:45:35 -0400

    convert chromium finder to typescript

--- packages/launcher/src/darwin/index.ts
+++ packages/launcher/src/darwin/index.ts
@@ -1,17 +1,9 @@
 import canary from './canary'
 import chrome from './chrome'
-// import chromium from './chromium'
-
-// import chromium from './chrome'
-
-// module.exports = {
-//   'chrome':   require(),
-//   'canary':   require('./canary'),
-//   'chromium': require('./chromium')
-// }
+import chromium from './chromium'
 
 export default {
   chrome,
-  canary
-  // chromium
+  canary,
+  chromium
 }
