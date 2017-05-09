c8df54bd44f9179d9c00fe17a8c173fd85dc0ce2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-09 10:34:45 -0400

    initial typescript for launcher file

---
+++ packages/launcher/src/launcher.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs'
+
+const Promise = require('bluebird')
+const detect = require('./detect')
+const browsers = require('./browsers')
+
+const missingConfig = () =>
+  Promise.reject(new Error('You must provide a path to a config file.'))
+
+const api = browser => (
+{
+  launch: (name, url, args = []) =>
+      browsers.launch(browser, name, url, args)
+}
+)
+
+const init = browsers =>
+  browsers ? api(browsers) : detect().then(api)
+
+const update = (pathToConfig) => {
+  if (!pathToConfig) {
+    return missingConfig()
+  }
+
+  // detect the browsers and set the config
+  return detect()
+    .then((browers) =>
+      fs.writeJsonAsync(pathToConfig, browers, {spaces: 2})
+    )
+}
+
+const launcher = {
+  init,
+  update,
+  detect
+}
+
+module.exports = launcher
