f31586d4b90a659a935c48bcc9e93d9ecbb5db83
Chris Breiding <chrisbreiding@gmail.com>
2017-05-15 13:59:41 -0400

    make import/merge more flexible

--- import.js
+++ import.js
@@ -4,10 +4,12 @@
 
 const { snakeCase } = require('lodash')
 const { execSync } = require('child_process')
+const path = require('path')
 const argv = require('minimist')(process.argv.slice(2))
 
 const from = argv.from
-const to = argv.to
+const to = path.join(argv.to, '/') // ensure trailing slash
+const branch = argv.banch || 'master'
 const remoteName = snakeCase(to)
 
 function exec (command) {
@@ -15,8 +17,12 @@ function exec (command) {
   execSync(command, { stdio: 'inherit' })
 }
 
+console.log(`Importing ${from} (${branch}) to ${to}`)
+console.log('---------')
 exec(`git remote add ${remoteName} ${from}`)
 exec(`git fetch ${remoteName}`)
-exec(`git merge -s ours --allow-unrelated-histories --no-commit ${remoteName}/master`)
-exec(`git read-tree --prefix=packages/${to}/ -u ${remoteName}/master`)
-exec(`git commit -m "import ${to}"`)
+exec(`git merge -s ours --allow-unrelated-histories --no-commit ${remoteName}/${branch}`)
+exec(`git read-tree --prefix=${to} -u ${remoteName}/${branch}`)
+exec(`git commit -m "import ${to} (${branch})"`)
+console.log('---------')
+console.log(`Finished mporting ${from} (${branch}) to ${to}`)
--- merge.js
+++ merge.js
@@ -4,12 +4,17 @@
 
 const { snakeCase } = require('lodash')
 const { execSync } = require('child_process')
+const path = require('path')
 const argv = require('minimist')(process.argv.slice(2))
 
-const to = argv._[0]
+const to = path.join(argv._[0], '/') // ensure trailing slash
 const branch = argv._[1] || 'master'
 const remote = snakeCase(to)
-const command = `git subtree pull --prefix=packages/${to}/ ${remote} ${branch}`
+const command = `git subtree pull --prefix=${to} ${remote} ${branch}`
 
+console.log(`Merging remote ${remote} (${branch}) to ${to}`)
+console.log('---------')
 console.log(command)
 execSync(command, { stdio: 'inherit' })
+console.log('---------')
+console.log(`Finished erging remote ${remote} (${branch}) to ${to}`)
