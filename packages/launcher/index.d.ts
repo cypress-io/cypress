ff45857eaa4cde418d568bbb61a80aa6f6fc1132
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-16 17:53:17 -0400

    On the fly TypeScript transpile using ts-node (#26)
    Using require hook

--- packages/launcher/index.d.ts
+++ packages/launcher/index.d.ts
@@ -1,29 +1,3 @@
-// all common type definition for this module
-
-type NotInstalledError = Error & {notInstalled: boolean}
-
-type BrowserNotFoundError = Error & {specificBrowserNotFound: boolean}
-
-interface ExtraLauncherMethods {
-  update: Function,
-  detect: Function
-}
-
-type LauncherLaunch = (browsers?: any[]) => Promise<any>
-
-type LauncherApi = LauncherLaunch & ExtraLauncherMethods
-
-type Browser = {
-  name: string,
-  re: RegExp,
-  profile: boolean,
-  binary: string,
-  executable: string,
-  version?: string,
-  majorVersion?: string,
-  page?: string
-}
-
 // missing type definitions for 3rd party libraries
 // https://glebbahmutov.com/blog/trying-typescript/#manual-types-for-3rd-party-libraries
 declare module 'execa' {
