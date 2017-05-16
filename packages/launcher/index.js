df3f3fcd4277bd84a5a492193bc35fdaf4ed4ee7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-16 17:53:17 -0400

    On the fly TypeScript transpile using ts-node (#26)
    Using require hook

--- packages/launcher/index.js
+++ packages/launcher/index.js
@@ -1,2 +1,6 @@
 // @ts-check
+
+// compile TypeScript files on the fly using
+// Node require hook project
+require('../ts')
 module.exports = require("./lib/launcher")
---
+++ packages/ts/index.js
@@ -0,0 +1,3 @@
+// gives anyone access to ts-node
+// https://github.com/TypeStrong/ts-node#programmatic-usage
+module.exports = require('ts-node')
