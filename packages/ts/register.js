e62d5d00bdc92adb56a67256ee7ddd96b614fa49
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-19 17:45:54 -0400

    ts: transpile TypeScript without checking types by default for speed

--- packages/ts/register.js
+++ packages/ts/register.js
@@ -1,8 +1,14 @@
 // register TypeScript Node require hook
 // https://github.com/TypeStrong/ts-node#programmatic-usage
 const project = require('path').join(__dirname, '../tsconfig.json')
+
+// transpile TypeScript without checking types by default
+// set environment variable when you want to actually verify types
+const fast = Boolean(process.env.TS_CHECK_TYPES) === false
+
 require('ts-node').register({
-  project
+  project,
+  fast
 })
 
 // do we need to prevent any other TypeScript hooks?
