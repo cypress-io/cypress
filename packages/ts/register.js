3a52caf1836f1902db6c086c0965533e9e0f71a7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-22 12:08:18 -0400

    ts: use debug cypress:ts to see TypeScript registration message

--- packages/ts/register.js
+++ packages/ts/register.js
@@ -1,10 +1,13 @@
 // register TypeScript Node require hook
 // https://github.com/TypeStrong/ts-node#programmatic-usage
 const project = require('path').join(__dirname, '../tsconfig.json')
+const log = require('debug')('cypress:ts')
 
 // transpile TypeScript without checking types by default
 // set environment variable when you want to actually verify types
 const fast = Boolean(process.env.TS_CHECK_TYPES) === false
+
+log('register TypeScript project %s fast? %s', project, fast)
 
 require('ts-node').register({
   project,
