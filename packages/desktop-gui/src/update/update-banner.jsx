b17aef41c5b1e309f0b43650b4f23fc7a72c5c82
Chris Breiding <chrisbreiding@gmail.com>
2017-06-20 16:50:07 -0400

    desktop: show modal when update available in project mode

--- packages/desktop-gui/src/update/update-banner.jsx
+++ packages/desktop-gui/src/update/update-banner.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { action, observable } from 'mobx'
 import { observer } from 'mobx-react'
+import BootstrapModal from 'react-bootstrap-modal'
 
 import appStore from '../lib/app-store'
 import updater from './update-model'
@@ -7,6 +9,8 @@ import ipc from '../lib/ipc'
 
 @observer
 class UpdateBanner extends Component {
+  @observable showingModal = false
+
   componentDidMount () {
     this.checkId = setInterval(this._checkForUpdate, (5 * 60 * 1000))
     this._checkForUpdate()
@@ -31,7 +35,39 @@ class UpdateBanner extends Component {
           <i className='fa fa-download'></i>{' '}
           Update
         </strong>
+        {this._modal()}
       </div>
+    )
+  }
+
+  _modal () {
+    return (
+      <BootstrapModal
+        show={this.showingModal}
+        onHide={() => this._toggleModal(false)}
+        backdrop='static'
+      >
+        <div className='update-modal modal-body os-dialog'>
+          <BootstrapModal.Dismiss className='btn btn-link close'>x</BootstrapModal.Dismiss>
+          <h4>Update to the latest version</h4>
+          <p>Version <strong>{appStore.newVersion}</strong> is now available (<a href='#' onClick={this._openChangelog}>Changelog</a>)</p>
+          <ol>
+            <li>
+              Set <code>"cypress"</code> in your app's <code>package.json</code> to <code>"{appStore.newVersion}"</code>
+
+              <pre>
+                {'{\n'}
+                {'  "devDependencies": {\n'}
+                {`    "cypress": "${appStore.newVersion}"\n`}
+                {'  }\n'}
+                {'}'}
+              </pre>
+            </li>
+            <li>Quit this app and run <code>npm install</code></li>
+            <li>Run <code>cypress open</code> to re-open the new version of the app</li>
+          </ol>
+        </div>
+      </BootstrapModal>
     )
   }
 
@@ -39,15 +75,28 @@ class UpdateBanner extends Component {
     ipc.offUpdaterCheck()
     ipc.updaterCheck()
     .then((version) => {
-      appStore.set({ updateAvailable: !!version })
+      appStore.setNewVersion(version)
     })
     .catch((error) => {
       console.warn('Error checking for updates:', error) // eslint-disable-line no-console
     })
   }
 
-  _downloadUpdate () {
-    updater.openUpdateWindow()
+  _downloadUpdate = () => {
+    if (appStore.isGlobalMode) {
+      updater.openUpdateWindow()
+    } else {
+      this._toggleModal(true)
+    }
+  }
+
+  @action _toggleModal = (showModal) => {
+    this.showingModal = showModal
+  }
+
+  _openChangelog (e) {
+    e.preventDefault()
+    ipc.externalOpen('https://on.cypress.io/changelog')
   }
 }
 
