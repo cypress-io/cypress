dea718e36a5cf3ed127ee124a4991694be03578f
Brian Mann <brian@backbonerails.com>
2017-06-24 16:20:29 -0400

    docs: refactor, cleanup

--- docs/lib/tags/icons.js
+++ docs/lib/tags/icons.js
@@ -1,16 +1,5 @@
 const rawRender = require('../raw_render')
 
-function helperIconUrl (hexo, url) {
-  const icon = '<i class="fa fa-question-circle"></i>'
-
-  url = `{% url -placeholder- ${url} %}`
-
-  return rawRender.call(this, hexo, url)
-  .then((markdown) => {
-    return markdown.replace('-placeholder-', icon)
-  })
-}
-
 function fa (hexo, args) {
   // {% fa fa-angle-right green fa-fw %}
   //
@@ -30,15 +19,26 @@ function helperIcon (hexo, args) {
 
   const type = args[0]
 
+  const urlTo = (u) => {
+    const icon = '<i class="fa fa-question-circle"></i>'
+
+    u = `{% url -placeholder- ${u} %}`
+
+    return rawRender.call(this, hexo, u)
+    .then((markdown) => {
+      return markdown.replace('-placeholder-', icon)
+    })
+  }
+
   switch (type) {
     case 'yields':
-      return helperIconUrl.call(this, hexo, 'introduction-to-cypress#Subject-Management')
+      return urlTo('introduction-to-cypress#Subject-Management')
 
     case 'timeout':
-      return helperIconUrl.call(this, hexo, 'introduction-to-cypress#Timeouts')
+      return urlTo('introduction-to-cypress#Timeouts')
 
     case 'defaultAssertion':
-      return helperIconUrl.call(this, hexo, 'introduction-to-cypress#Default-Assertions')
+      return urlTo('introduction-to-cypress#Default-Assertions')
 
     default:
       // error when an invalid usage option was provided
