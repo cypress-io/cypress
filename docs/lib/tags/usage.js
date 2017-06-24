52d9f7c6874a47698cb5f8212294adfbfc1b741e
Brian Mann <brian@backbonerails.com>
2017-06-24 15:43:58 -0400

    docs: add usageOptions templates for log, force, and timeout

--- docs/lib/tags/usage.js
+++ docs/lib/tags/usage.js
@@ -1,28 +1,34 @@
 const rawRender = require('../raw_render')
 
-module.exports = function (hexo, args) {
+module.exports = function usageOptions (hexo, args) {
   const opt = args[0]
   const type = args[1]
 
-  const logUsage = (hexo) => {
+  const log = () => {
     /* eslint-disable quotes */
     const url = `{% url 'Command Log' overview-of-the-gui#Command-Log %}`
 
     return rawRender.call(this, hexo, `Whether to display the command in the ${url}`)
   }
 
-  const forceUsage = (hexo, type) => {
+  const force = () => {
     /* eslint-disable quotes */
     const url = `{% url 'waiting for actionability' interacting-with-elements#Bypassing %}`
 
-    return rawRender.call(this, hexo, `Forces the ${type}, disables ${url}`)
+    return rawRender.call(this, hexo, `Forces the <code>${type}</code>, disables ${url}`)
+  }
+
+  const timeout = () => {
+    return `Total time to wait until the <code>${type}</code> times out and errors`
   }
 
   switch (opt) {
     case 'log':
-      return logUsage(hexo)
+      return log()
     case 'force':
-      return forceUsage(hexo, type)
+      return force()
+    case 'timeout':
+      return timeout()
     default:
       // error when an invalid usage option was provided
       throw new Error(`{% usage_options %} tag helper was provided an invalid option: ${opt}`)
