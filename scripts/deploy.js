01ae68f91ac43692c943c702c52ff70434c3f894
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-27 14:42:48 -0400

    refactored questions to allow asking only some information

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,22 @@
+/* eslint-disable no-console */
 require('@packages/coffee/register')
 
-require('./deploy/index').deploy()
+const command = process.argv[2]
+if (!command) {
+  console.error('Missing deploy command ⛔️')
+  process.exit(1)
+}
+
+const commands = require('./deploy/index')
+const fn = commands[command]
+if (!fn) {
+  console.error('Invalid deploy command %s 🚫', command)
+}
+
+fn()
+.then(() => console.log('✅ %s completed', command))
+.catch((err) => {
+  console.error('🔥 deploy error')
+  console.error(err)
+  process.exit(1)
+})
