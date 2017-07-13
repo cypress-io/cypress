c3fb4ba4eccaff4429e38826d3d1ac5f97f4ceef
Brian Mann <brian@backbonerails.com>
2017-07-13 14:55:40 -0400

    server: properly test timers, fix edge cases and bugs with clearing them

--- packages/server/timers/parent.js
+++ packages/server/timers/parent.js
@@ -8,93 +8,142 @@
 
 const cp = require('child_process')
 const path = require('path')
+const log = require('debug')('cypress:server:timers')
 
-const queue = {}
+const st = global.setTimeout
+const si = global.setInterval
+const ct = global.clearTimeout
+const ci = global.clearInterval
 
-let idCounter = 0
-
-function clear (id) {
-  delete queue[id]
-}
+let child = null
 
 function noop () {}
 
-function sendAndQueue (id, cb, ms, args) {
-  // const started = Date.now()
+function restore () {
+  // restore
+  global.setTimeout = st
+  global.setInterval = si
+  global.clearTimeout = ct
+  global.clearInterval = ci
 
-  queue[id] = {
-    // started,
-    args,
-    ms,
-    cb,
+  if (child) {
+    child.kill()
   }
 
-  child.send({
-    id,
-    ms,
+  child = null
+}
+
+function fix () {
+  const queue = {}
+  let idCounter = 0
+
+  function sendAndQueue (id, cb, ms, args) {
+    // const started = Date.now()
+    log('queuing timer id %d after %d ms', id, ms)
+
+    queue[id] = {
+      // started,
+      args,
+      ms,
+      cb,
+    }
+
+    child.send({
+      id,
+      ms,
+    })
+
+    // return the timer object
+    return {
+      id,
+      ref: noop,
+      unref: noop,
+    }
+  }
+
+  function clear (id) {
+    log('clearing timer id %d from queue %o', id, queue)
+
+    delete queue[id]
+  }
+
+  // fork the child process
+  let child = cp.fork(path.join(__dirname, 'child.js'), [], {
+    env: {
+      ELECTRON_RUN_AS_NODE: true,
+    },
+  })
+  .on('message', (obj = {}) => {
+    const { id } = obj
+
+    const msg = queue[id]
+
+    // if we didn't get a msg
+    // that means we must have
+    // cleared the timeout already
+    if (!msg) {
+      return
+    }
+
+    const { cb, args } = msg
+
+    clear(id)
+
+    cb(...args)
   })
 
-  // return the timer id
+  global.setTimeout = function (cb, ms, ...args) {
+    idCounter += 1
+
+    return sendAndQueue(idCounter, cb, ms, args)
+  }
+
+  global.clearTimeout = function (timer) {
+    if (!timer) {
+      return
+    }
+
+    // return undefined per the spec
+    clear(timer.id)
+  }
+
+  global.clearInterval = function (timer) {
+    if (!timer) {
+      return
+    }
+
+    // return undefined per the spec
+    clear(timer.id)
+  }
+
+  global.setInterval = function (fn, ms, ...args) {
+    const permId = idCounter += 1
+
+    function cb () {
+      // we want to immediately poll again
+      // because our permId was just cleared
+      // from the queue stack
+      poll()
+
+      fn()
+    }
+
+    function poll () {
+      return sendAndQueue(permId, cb, ms, args)
+    }
+
+    return poll()
+  }
+
   return {
-    id,
-    ref: noop,
-    unref: noop,
+    child,
+
+    queue,
   }
 }
 
-// fork the child process
-const child = cp.fork(path.join(__dirname, 'child.js'), [], {
-  env: {
-    ELECTRON_RUN_AS_NODE: true,
-  },
-})
-.on('message', (obj = {}) => {
-  const { id } = obj
+module.exports = {
+  restore,
 
-  const msg = queue[id]
-
-  // if we didn't get a msg
-  // that means we must have
-  // cleared the timeout already
-  if (!msg) {
-    return
-  }
-
-  const { cb, args } = msg
-
-  clear(id)
-
-  cb(...args)
-})
-
-global.setTimeout = function (cb, ms, ...args) {
-  idCounter += 1
-
-  return sendAndQueue(idCounter, cb, ms, args)
-}
-
-global.clearTimeout = function (id) {
-  // return undefined per the spec
-  clear(id)
-}
-
-global.clearInterval = function (id) {
-  // return undefined per the spec
-  clear(id)
-}
-
-global.setInterval = function (fn, ms, ...args) {
-  const permId = idCounter += 1
-
-  function cb () {
-    fn()
-
-    poll()
-  }
-
-  function poll () {
-    return sendAndQueue(permId, cb, ms, args)
-  }
-
-  return poll()
+  fix,
 }
