42807000ba435ad3d197560f02a21ea755ee6a0c
Chris Breiding <chrisbreiding@gmail.com>
2017-07-13 11:04:36 -0400

    runner: pass @package/coffee into zunder

--- packages/runner/scripts/set-zunder-config.js
+++ packages/runner/scripts/set-zunder-config.js
@@ -1,6 +1,7 @@
 module.exports = function setZunderConfig (zunder) {
   zunder.setConfig({
     cacheBust: false,
+    coffeeCompiler: require('@packages/coffee'),
     prodDir: 'dist',
     resolutions: ['react', 'react-dom', 'mobx', 'mobx-react', 'lodash'],
     scriptName: 'runner.js',
