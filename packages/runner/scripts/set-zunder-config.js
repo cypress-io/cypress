c5e71173c79825db3135c9ffa2df477b606f602e
Chris Breiding <chrisbreiding@gmail.com>
2017-07-13 14:38:34 -0400

    runner: prefix output files with cypress_

--- packages/runner/scripts/set-zunder-config.js
+++ packages/runner/scripts/set-zunder-config.js
@@ -4,8 +4,8 @@ module.exports = function setZunderConfig (zunder) {
     coffeeCompiler: require('@packages/coffee'),
     prodDir: 'dist',
     resolutions: ['react', 'react-dom', 'mobx', 'mobx-react', 'lodash'],
-    scriptName: 'runner.js',
-    stylesheetName: 'runner.css',
+    scriptName: 'cypress_runner.js',
+    stylesheetName: 'cypress_runner.css',
     staticGlobs: {
       'static/**': '',
       'node_modules/font-awesome/fonts/**': '/fonts',
