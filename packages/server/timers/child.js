ddfb71f89ae84a9adbcb009463a21cb2f669313e
Brian Mann <brian@backbonerails.com>
2017-07-16 21:04:44 -0400

    server: add try / catch for child timers sending to parent

--- packages/server/timers/child.js
+++ packages/server/timers/child.js
@@ -2,9 +2,15 @@ process.on('message', (obj = {}) => {
   const { id, ms } = obj
 
   setTimeout(() => {
-    process.send({
-      id,
-      ms,
-    })
+    try {
+      // process.send could throw if
+      // parent process has already exited
+      process.send({
+        id,
+        ms,
+      })
+    } catch (err) {
+      // eslint-disable no-empty
+    }
   }, ms)
 })
