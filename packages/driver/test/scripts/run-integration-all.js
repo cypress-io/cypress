6fe6f80d99ceadd2055f6bc50c68ba001a9b4145
Brian Mann <brian@backbonerails.com>
2017-08-13 16:28:33 -0400

    driver: enable running only actions or non action specs, promisify interface

--- packages/driver/test/scripts/run-integration-all.js
+++ packages/driver/test/scripts/run-integration-all.js
@@ -1,25 +1,61 @@
-const { execSync } = require('child_process')
-const glob = require('glob')
-const path = require('path')
+/* eslint-disable no-console */
 
-const specs = glob.sync('test/cypress/integration/**/*.coffee')
+const cp = require('child_process')
+const path = require('path')
+const Promise = require('bluebird')
+
+const glob = Promise.promisify(require('glob'))
+
+const actions = process.argv.includes('--actions')
+const noActions = process.argv.includes('--no-actions')
+
 let numFailed = 0
 
-specs.forEach((spec) => {
+function spawn (cmd, args, opts) {
+  return new Promise((resolve, reject) => {
+    cp.spawn(cmd, args, opts)
+    .on('exit', resolve)
+    .on('error', reject)
+  })
+}
+
+glob('test/cypress/integration/**/*.coffee')
+.filter((spec) => {
+  if (actions) {
+    return spec.includes('actions')
+  }
+
+  if (noActions) {
+    return !spec.includes('actions')
+  }
+
+  return spec
+})
+.tap(console.log)
+.each((spec = []) => {
+  console.log('Running spec', spec)
+
   const args = [
+    'test/scripts/run-integration',
     '--project',
     path.resolve('test'),
     '--browser=chrome',
     '--driver',
     '--spec',
     spec.replace('test/', ''),
-  ].join(' ')
+  ]
 
-  try {
-    execSync(`node test/scripts/run-integration ${args}`, { stdio: 'inherit' })
-  } catch (err) {
-    numFailed += err.status
-  }
+  return spawn('node', args, { stdio: 'inherit' })
+  .then((code) => {
+    console.log(`${spec} exited with code`, code)
+
+    numFailed += code
+  })
+  .catch((err) => {
+    console.log(err)
+    throw err
+  })
 })
-
-process.exit(numFailed)
+.then(() => {
+  process.exit(numFailed)
+})
