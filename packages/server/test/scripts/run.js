ac71bf61f50bb773e83641978f6ca2089a101a53
Brian Mann <brian@backbonerails.com>
2017-08-28 23:31:35 -0400

    server: WIP node script for running tests, verbose handling

---
+++ packages/server/test/scripts/run.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-console */
+
+const _ = require('lodash')
+const cp = require('child_process')
+const chalk = require('chalk')
+const minimist = require('minimist')
+
+const options = minimist(process.argv.slice(2))
+
+function exitErr (msg) {
+  console.error(chalk.red(msg))
+
+  return process.exit(1)
+}
+
+if (!options.run) {
+  return exitErr(`
+    Error: A path to a spec file must be specified!
+
+    It should look something like this:
+
+      $ npm test --run ./test/unit/api_spec.coffee
+
+    If you want to run all a specific group of tests:
+
+      $ npm run test-unit
+      $ npm run test-integration
+      $ npm run test-e2e
+  `)
+}
+
+const args = [
+  '--xvfb-run-args',
+  '"-s \"-screen 0 1280x1024x8\""',
+  'mocha',
+  options.run,
+]
+
+if (options.fgrep) {
+  args.push(
+    '--fgrep',
+    options.fgrep
+  )
+}
+
+args.push(
+  '--timeout',
+  '10000',
+  '--recursive',
+  '--compilers',     'ts:@packages/ts/register,coffee:@packages/coffee/register',
+  '--reporter',
+  'mocha-multi-reporters',
+  '--reporter-options',
+  'configFile=../../mocha-reporter-config.json'
+)
+
+const env = _.clone(process.env)
+
+env.NODE_ENV = 'test'
+
+if (env.VERBOSE === '1') {
+  _.extend(env, {
+    CYPRESS_ENV: 'test',
+    CYPRESS_DEBUG: true,
+    NODE_DEBUG: 'request',
+    BLUEBIRD_DEBUG: 1,
+    DEBUG: _.chain([
+      env.DEBUG,
+      'nock.*',
+      '-nock.common',
+      '-nock.scope',
+      'socket.io:*',
+      'xvfb-maybe',
+    ])
+    .compact()
+    .join(','),
+  })
+}
+
+if (options.browser) {
+  env.BROWSER = options.browser
+}
+
+cp.spawn('xvfb-maybe', args, {
+  env,
+  stdio: 'inherit',
+})
+.on('exit', process.exit)
