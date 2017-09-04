2be65b47b46ac40b43fae0bb8a18c222cb9911b7
Brian Mann <brian@backbonerails.com>
2017-09-04 13:26:55 -0400

    cli: prune existing unused snapshots

--- cli/__snapshots__/download_spec.js
+++ cli/__snapshots__/download_spec.js
@@ -1,9 +1,3 @@
-exports['download when finished logs out Finished Installing 1'] = `[37m  -[39m [34mFinished Installing[39m [32m../../../foo/bar[39m [90m(version: 0.11.1)[39m
-[33m  You can now open Cypress by running:[39m [36mcypress open[39m`
-
-exports['download when finished the reported path is relative 1'] = `[37m  -[39m [34mFinished Installing[39m [32mnode_modules/cypress/dist/Cypress.app[39m [90m(version: 0.11.1)[39m
-[33m  You can now open Cypress by running:[39m [36mcypress open[39m`
-
 exports['download catches download status errors and exits 1'] = `[31mError: The Cypress App could not be downloaded.[39m
 [31m[39m
 [31mPlease check network connectivity and try again:[39m
