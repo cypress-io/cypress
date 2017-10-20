d907f642d006034e353e67f68e8183570fa053fe
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-20 13:51:14 -0400

    moved appveyor commands into shell script

--- scripts/win-appveyor-build.js
+++ scripts/win-appveyor-build.js
@@ -6,6 +6,8 @@
 // but only on the right branch
 
 const shell = require('shelljs')
+const la = require('lazy-ass')
+const is = require('check-more-types')
 
 shell.set('-v') // verbose
 shell.set('-e') // any error is fatal
@@ -27,3 +29,20 @@ if (!shouldBuildBinary()) {
 }
 
 console.log('building binary')
+
+const hash = `${process.env.APPVEYOR_REPO_BRANCH}-${process.env.APPVEYOR_REPO_COMMIT}-${process.env.APPVEYOR_BUILD_ID}`
+const filename = `cypress-${process.env.NEXT_DEV_VERSION}.tgz`
+const version = process.env.NEXT_DEV_VERSION
+la(is.unemptyString(version), 'missing NEXT_DEV_VERSION')
+
+console.log('building version', version)
+console.log('upload hash', hash)
+
+shell.exec(`node scripts/binary.js upload-npm-package --file cli/build/${filename} --version ${version} --hash ${hash}`)
+shell.cat('npm-package-url.json')
+shell.exec(`npm run binary-build -- --platform windows --version ${version}`)
+shell.exec('npm run binary-zip')
+shell.ls('-l', '*.zip')
+shell.exec(`node scripts/binary.js upload-unique-binary --file cypress.zip --version ${version} --hash ${hash}`)
+shell.cat('binary-url.json')
+shell.exec('node scripts/test-other-projects.js --npm npm-package-url.json --binary binary-url.json --provider appVeyor')
