9c7996863e8a43d795c47c95da5f64a5025b892a
Patrick Mowrer <pmowrer@gmail.com>
2017-11-29 18:17:41 -0500

    feat: refactor plugin to use main repo's plugin structure
    BREAKING CHANGE:
    Use standard plugins instead of proposed `getCommits` plugin.
    
    The previous plugin implementation was based around a [proposed, but
    rejected `getCommits`
    plugin](https://github.com/semantic-release/semantic-release/pull/531).
    
    The new implementation is functionally equivalent, but is slower as it
    has to repeat the filtering of commits an additional time. Rather than
    doing it once, as in the proposed `getCommits`, we need to do it for
    both `analyzeCommits` and `generateNotes` separately. Could probably be
    optimized through memoization or global variable.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const getCommits = require('./src/get-commits');
+// Default plugins that we'll wrap to filter commits by package.
+const commitAnalyzer = require('@semantic-release/commit-analyzer');
+const releaseNotesGenerator = require('@semantic-release/release-notes-generator');
+
+const overrideOption = (key, wrapperFn) => pluginFn => async (pluginConfig, options) => {
+  const overriddenValue = await wrapperFn(options[key]);
+  return pluginFn(pluginConfig, { ...options, [key]: overriddenValue });
+};
+
+const withPackageCommits = overrideOption('commits', require('./src/with-package-commits'));
 const publish = require('./src/publish');
 
 module.exports = {
-  getCommits,
+  analyzeCommits: withPackageCommits(commitAnalyzer),
+  generateNotes: withPackageCommits(releaseNotesGenerator),
   publish,
 };
