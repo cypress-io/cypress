ccb8d831a128e3bd8c540d6fbada1f4b84be154e
Brian Mann <brian@backbonerails.com>
2017-11-29 17:50:24 -0500

    cli: use existing build script for copying typescript definitions

--- cli/scripts/start-build.js
+++ cli/scripts/start-build.js
@@ -10,6 +10,7 @@ shell.mkdir('-p', 'build/bin')
 shell.cp('bin/cypress', 'build/bin/cypress')
 shell.cp('NPM_README.md', 'build/README.md')
 shell.cp('.release.json', 'build/.release.json')
+shell.cp('index.d.ts', 'build/index.d.ts')
 
 shell.exec('babel lib -d build/lib')
 shell.exec('babel index.js -o build/index.js')
