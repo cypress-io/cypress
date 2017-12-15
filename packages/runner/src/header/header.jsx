1dd5c16780d30226c6d9022057721aa14ad5cb99
Chris Breiding <chrisbreiding@gmail.com>
2017-12-15 10:32:45 -0500

    runner: refactor header to use mobx state

--- packages/runner/src/header/header.jsx
+++ packages/runner/src/header/header.jsx
@@ -1,4 +1,5 @@
 import cs from 'classnames'
+import { action, observable } from 'mobx'
 import { observer } from 'mobx-react'
 import React, { Component } from 'react'
 import Tooltip from '@cypress/react-tooltip'
@@ -9,15 +10,9 @@ import selectorHelperModel from '../selector-helper/selector-helper-model'
 
 @observer
 export default class Header extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = { showingViewportMenu: false }
-  }
+  @observable showingViewportMenu = false
 
   render () {
-    const { width, height, displayScale, defaults, isLoadingUrl, url, highlightUrl } = this.props.state
-    const { showingViewportMenu } = this.state
     const { state } = this.props
 
     return (
@@ -42,29 +37,29 @@ export default class Header extends Component {
           </Tooltip>
           <div
             className={cs('url-container', {
-              'loading': isLoadingUrl,
-              'highlighted': highlightUrl,
+              'loading': state.isLoadingUrl,
+              'highlighted': state.highlightUrl,
             })}
           >
-            <input className='url' value={url} readOnly onClick={this._openUrl} />
+            <input className='url' value={state.url} readOnly onClick={this._openUrl} />
             <span className='loading-container'>
               ...loading <i className='fa fa-spinner fa-spin fa-pulse'></i>
             </span>
           </div>
         </div>
         <ul className='menu'>
-          <li className={cs('viewport-info', { open: showingViewportMenu })}>
+          <li className={cs('viewport-info', { open: this.showingViewportMenu })}>
             <button onClick={this._toggleViewportMenu}>
-              {width} x {height} <span className='viewport-scale'>({displayScale}%)</span>
+              {state.width} x {state.height} <span className='viewport-scale'>({state.displayScale}%)</span>
               <i className='fa fa-fw fa-info-circle'></i>
             </button>
             <div className='viewport-menu'>
-              <p>The <strong>viewport</strong> determines the width and height of your application. By default the viewport will be <strong>{defaults.width}px</strong> by <strong>{defaults.height}px</strong> unless specified by a <code>cy.viewport</code> command.</p>
+              <p>The <strong>viewport</strong> determines the width and height of your application. By default the viewport will be <strong>{state.defaults.width}px</strong> by <strong>{state.defaults.height}px</strong> unless specified by a <code>cy.viewport</code> command.</p>
               <p>Additionally you can override the default viewport dimensions by specifying these values in your <code>cypress.json</code>.</p>
               <pre>{/* eslint-disable indent */}
 {`{
-  viewportWidth: ${defaults.width},
-  viewportHeight: ${defaults.height}
+  viewportWidth: ${state.defaults.width},
+  viewportHeight: ${state.defaults.height}
 }`}
               </pre>{/* eslint-enable indent */}
               <p>
@@ -102,7 +97,7 @@ export default class Header extends Component {
     window.open(this.props.state.url)
   }
 
-  _toggleViewportMenu = () => {
-    this.setState({ showingViewportMenu: !this.state.showingViewportMenu })
+  @action _toggleViewportMenu = () => {
+    this.showingViewportMenu = !this.state.showingViewportMenu
   }
 }
