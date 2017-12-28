ca5ee72d560afee1cbfe5bfe046717bc6dcec517
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-27 22:37:46 -0500

    add MessageList component and test

---
+++ components/MessageList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for="message in messages">
+      {{ message }}
+    </li>
+  </ul>
+</template>
+
+<script>
+// from https://alexjoverm.github.io/2017/08/21/Write-the-first-Vue-js-Component-Unit-Test-in-Jest/
+export default {
+  name: 'list',
+  props: ['messages']
+}
+</script>
---
+++ cypress/integration/message-list-spec.js
@@ -0,0 +1,63 @@
+import MessageList from '../../components/MessageList.vue'
+const mountVue = require('../..')
+
+// common utils for MessageList
+const getItems = () => cy.get('ul li')
+
+/* eslint-env mocha */
+describe('MessageList', () => {
+  beforeEach(mountVue(MessageList))
+
+  it('shows no messages', () => {
+    getItems().should('not.exist')
+  })
+
+  it('shows messages', () => {
+    Cypress.vue.messages = ['one', 'two']
+    getItems().should('have.length', 2)
+    cy.then(() => {
+      Cypress.vue.messages.push('three')
+      getItems().should('have.length', 3)
+    })
+  })
+})
+
+describe('MessageList with props', () => {
+  const template = `
+    <div>
+      <MessageList :messages="messages"/>
+    </div>
+  `
+
+  const data = () => ({ messages: ['uno', 'dos'] })
+
+  const components = {
+    MessageList
+  }
+
+  beforeEach(mountVue({ template, data, components }))
+
+  it('shows two items at the start', () => {
+    getItems().should('have.length', 2)
+  })
+})
+
+describe('MessageList under message-list name', () => {
+  const template = `
+    <div>
+      <message-list :messages="messages"/>
+    </div>
+  `
+
+  const data = () => ({ messages: ['uno', 'dos'] })
+
+  const components = {
+    'message-list': MessageList
+  }
+
+  beforeEach(mountVue({ template, data, components }))
+
+  it('shows two items at the start', () => {
+    getItems().should('have.length', 2)
+  })
+})
