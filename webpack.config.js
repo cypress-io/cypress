4476e058ae93042857d4b150ae3a3894aedd9333
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-15 22:35:35 -0500

    chore: linting using standard

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
-var webpack = require('webpack')
-var path = require('path')
+const path = require('path')
 
-var BUILD_DIR = path.resolve(__dirname, 'public')
-var APP_DIR = path.resolve(__dirname, 'src')
+const BUILD_DIR = path.resolve(__dirname, 'public')
+const APP_DIR = path.resolve(__dirname, 'src')
 
-var config = {
+const config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
         include: APP_DIR,
-        loader: 'babel-loader',
-      },
-    ],
-  },
+        loader: 'babel-loader'
+      }
+    ]
+  }
 }
 
 module.exports = config
