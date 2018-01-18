cd78ecc434aa9b588df8c7e86d12fa3a06b35410
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-18 08:54:29 -0500

    feat: spy on window.alert close bahmutov/cypress-react-unit-test#6

--- cypress/integration/alert-spec.js
+++ cypress/integration/alert-spec.js
@@ -7,7 +7,7 @@ describe('Stateless alert', () => {
   beforeEach(() => {
     const spy = cy.spy().as('alert')
     cy.on('window:alert', spy)
-    mount(<HelloWorld name="Alert" />)
+    mount(<HelloWorld name='Alert' />)
   })
 
   it('shows link', () => {
@@ -18,6 +18,6 @@ describe('Stateless alert', () => {
     cy.contains('Say Hi').click()
     // spy on window:alert is never called
     // https://github.com/bahmutov/cypress-react-unit-test/issues/6
-    // cy.get('@alert').should('have.been.calledOnce')
+    cy.get('@alert').should('have.been.calledOnce')
   })
 })
