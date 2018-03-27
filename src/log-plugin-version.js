798b0cd64a24b417502f29d6540548bad65da8be
Ali Sheehan-Dare <alisd23@live.co.uk>
2018-03-27 08:58:18 +0100

    refactor: revert path import change

--- src/log-plugin-version.js
+++ src/log-plugin-version.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const { resolve } = require('path');
 const readPkg = require('read-pkg');
 const debug = require('debug')('semantic-release:monorepo');
 
 const logPluginVersion = type => plugin => async (pluginConfig, config) => {
   if (config.options.debug) {
-    const { version } = await readPkg(path.resolve(__dirname, '../'));
+    const { version } = await readPkg(resolve(__dirname, '../'));
     debug('Running %o version %o', type, version);
   }
 
