242f3c3819139e630a48aa2dd0050b259a40624b
Chris Breiding <chrisbreiding@gmail.com>
2018-05-14 12:11:56 -0400

    new Promise + setTimeout -> Promise.delay

--- packages/driver/test/cypress/plugins/index.js
+++ packages/driver/test/cypress/plugins/index.js
@@ -1,12 +1,12 @@
+const Promise = require('bluebird')
+
 module.exports = (on) => {
   on('task', {
     'return:arg' (arg) {
       return arg
     },
     'wait' () {
-      return new Promise((resolve) => {
-        setTimeout(resolve, 2000)
-      })
+      return Promise.delay(2000)
     },
   })
 }
