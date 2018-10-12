57558c93a322ca8e617c5596c2e6952fd1cc2418
duranje <joeduran8@gmail.com>
2018-10-12 16:34:59 -0500

    adds keydown listener to Controls

--- packages/reporter/src/header/controls.jsx
+++ packages/reporter/src/header/controls.jsx
@@ -12,62 +12,85 @@ const ifThen = (condition, component) => (
 @observer
 class Controls extends Component {
   emit = (event) => () => events.emit(event)
-  
+
   toggleAutoScrolling = () => {
-    appState.toggleAutoScrolling()
+    this.props.appState.toggleAutoScrolling()
     events.emit('save:state')
   }
 
-  render() {
+  _handleKeyDown = (e) => {
+    switch (e.key) {
+      case ('R'): {
+        this.emit('restart')
+        break
+      }
+      case ('S'): {
+        if (this.props.appState.isRunning) {
+          this.emit('stop')
+        }
+        break
+      }
+      default: {
+        break
+      }
+    }
+  }
+
+  componentDidMount () {
+    document.addEventListener('onkeydown', this._handleKeyDown)
+  }
+
+  render () {
+    const { appState, toggleAutoScrolling } = this.props
     return (
-    <div className='controls'>
-      {ifThen(appState.isPaused, (
-        <span className='paused-label'>
-          <label>Paused</label>
-        </span>
-      ))}
-      {ifThen(appState.isPaused, (
-        <Tooltip placement='bottom' title='Resume'>
-          <button className='play' onClick={emit('resume')}>
-            <i className='fa fa-play'></i>
-          </button>
-        </Tooltip>
-      ))}
-      {ifThen(!appState.isPaused, (
-        <Tooltip placement='bottom' title={`${appState.autoScrollingEnabled ? 'Disable' : 'Enable'} Auto-scrolling`}>
-          <button
-            className={cs('toggle-auto-scrolling', { 'auto-scrolling-enabled': appState.autoScrollingEnabled })}
-            onClick={action('toggle:auto:scrolling', toggleAutoScrolling)}
-          >
-            <i className='fa'></i>
-            <i className='fa fa-arrows-v'></i>
-          </button>
-        </Tooltip>
-      ))}
-      {ifThen(appState.isRunning && !appState.isPaused, (
-        <Tooltip placement='bottom' title='Stop Running'>
-          <button className='stop' onClick={emit('stop')}>
-            <i className='fa fa-stop'></i>
-          </button>
-        </Tooltip>
-      ))}
-      {ifThen(!appState.isRunning, (
-        <Tooltip placement='bottom' title='Run all tests'>
-          <button className='restart' onClick={emit('restart')}>
-            <i className='fa fa-repeat'></i>
-          </button>
-        </Tooltip>
-      ))}
-      {ifThen(!!appState.nextCommandName, (
-        <Tooltip placement='bottom' title={`Next: '${appState.nextCommandName}'`}>
-          <button className='next' onClick={emit('next')}>
-            <i className='fa fa-step-forward'></i>
-          </button>
-        </Tooltip>
-      ))}
-    </div>
-  )
-}
+      <div className='controls'>
+        {ifThen(appState.isPaused, (
+          <span className='paused-label'>
+            <label>Paused</label>
+          </span>
+        ))}
+        {ifThen(appState.isPaused, (
+          <Tooltip placement='bottom' title='Resume'>
+            <button className='play' onClick={this.emit('resume')}>
+              <i className='fa fa-play'></i>
+            </button>
+          </Tooltip>
+        ))}
+        {ifThen(!appState.isPaused, (
+          <Tooltip placement='bottom' title={`${appState.autoScrollingEnabled ? 'Disable' : 'Enable'} Auto-scrolling`}>
+            <button
+              className={cs('toggle-auto-scrolling', { 'auto-scrolling-enabled': appState.autoScrollingEnabled })}
+              onClick={action('toggle:auto:scrolling', toggleAutoScrolling)}
+            >
+              <i className='fa'></i>
+              <i className='fa fa-arrows-v'></i>
+            </button>
+          </Tooltip>
+        ))}
+        {ifThen(appState.isRunning && !appState.isPaused, (
+          <Tooltip placement='bottom' title='Stop Running'>
+            <button className='stop' onClick={this.emit('stop')}>
+              <i className='fa fa-stop'></i>
+            </button>
+          </Tooltip>
+        ))}
+        {ifThen(!appState.isRunning, (
+          <Tooltip placement='bottom' title='Run all tests'>
+            <button className='restart' onClick={this.emit('restart')}>
+              <i className='fa fa-repeat'></i>
+            </button>
+          </Tooltip>
+        ))}
+        {ifThen(!!appState.nextCommandName, (
+          <Tooltip placement='bottom' title={`Next: '${appState.nextCommandName}'`}>
+            <button className='next' onClick={this.emit('next')}>
+              <i className='fa fa-step-forward'></i>
+            </button>
+          </Tooltip>
+        ))}
+      </div>
+    )
+  }
 }
 
 Controls.defaultProps = {
