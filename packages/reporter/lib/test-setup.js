b6611e20b5aeecfe93637e6068c026d1f7d9c995
Chris Breiding <chrisbreiding@users.noreply.github.com>
2018-10-15 14:34:59 -0400

    Fix interactive mode reload throwing (#2603)
    * modify method of de-duping runner deps and upgrade deps
    
    * nevermind on path-based deps between driver/reporter/runner
    
    * fix tests broken by upgraded deps
    
    * remove unused var

--- packages/reporter/lib/test-setup.js
+++ packages/reporter/lib/test-setup.js
@@ -1,5 +1,4 @@
 let chai = require('chai')
-let chaiEnzyme = require('chai-enzyme')
 let jsdom = require('jsdom').jsdom
 let sinonChai = require('sinon-chai')
 
@@ -19,6 +18,16 @@ global.navigator = {
   appVersion: 'node',
   userAgent: 'node.js',
 }
+global.requestAnimationFrame = (fn) => fn()
+global.cancelAnimationFrame = () => {}
+
+// enzyme, and therefore chai-enzyme, needs to be required after
+// global.navigator is set up (https://github.com/airbnb/enzyme/issues/395)
+const enzyme = require('enzyme')
+const EnzymeAdapter = require('enzyme-adapter-react-16')
+const chaiEnzyme = require('chai-enzyme')
+
+enzyme.configure({ adapter: new EnzymeAdapter() })
 
 chai.use(chaiEnzyme())
 chai.use(sinonChai)
