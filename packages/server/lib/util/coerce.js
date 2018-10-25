47f1abf6b3a4613b088a539a804116773f587cfa
Brian Mann <brian.mann86@gmail.com>
2018-10-25 00:09:31 -0400

    fix busted coerce

--- packages/server/lib/util/coerce.js
+++ packages/server/lib/util/coerce.js
@@ -2,12 +2,18 @@ const _ = require('lodash')
 const toBoolean = require('underscore.string/toBoolean')
 
 const isValue = (value) => {
-  return _.invoke(value, 'toString') === value
+  return (comparison) => {
+    return _.invoke(comparison, 'toString') === value
+  }
 }
 
 module.exports = function (value) {
   const num = _.toNumber(value)
   const bool = toBoolean(value)
 
-  return _.find([num, bool], isValue) || value
+  return _
+  .chain([num, bool])
+  .find(isValue(value))
+  .defaultTo(value)
+  .value()
 }
