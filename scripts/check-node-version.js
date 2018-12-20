46f7393097a5eaa5a32f2c0ca02b13870abf2ca6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-20 18:04:47 -0500

    allow Node version mismatch on CircleCI Mac build

--- scripts/check-node-version.js
+++ scripts/check-node-version.js
@@ -10,8 +10,13 @@ const nodeVersionNeeded = nodeVersionNeededString.split('.')
 
 const nodeVersion = process.versions.node.split('.')
 
+const isMac = process.platform === 'darwin'
+const isCircleCI = process.env.CIRCLECI === 'true'
+
+const isAllowedNodeMismatch = isMac && isCircleCI
+
 // check just major version for now
-if (nodeVersionNeeded[0] !== nodeVersion[0]) {
+if (nodeVersionNeeded[0] !== nodeVersion[0] && !isAllowedNodeMismatch) {
   /* eslint-disable no-console */
   console.error('🛑 .node-version specified %s', nodeVersionNeededString)
   console.error('but current Node is %s', process.versions.node)
