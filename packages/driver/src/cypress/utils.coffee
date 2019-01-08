cypress-io/cypress

commit 6f9b47249e4704521ae7e5fbf4416e834b5ed47a

convert regular array of elements to jquery (#2870)
- fixes #2820 
- this fixes a situation where arrays of elements were not being
converted to their pre 3.1.2 behavior (which is to become jquery
instances)
- made notes of the anticipated behavior in 4.0

packages/driver/src/cypress/utils.coffee
@@ -3,6 +3,7 @@ _ = require("lodash")
 moment = require("moment")
 Promise = require("bluebird")
 
+$jquery = require("../dom/jquery")
 $Location = require("./location")
 $errorMessages = require("./error_messages")
 
@@ -30,6 +31,17 @@ module.exports = {
 
   logInfo: (msgs...) ->
     console.info(msgs...)
+
+  unwrapFirst: (val) ->
+    ## this method returns the first item in an array
+    ## and if its still a jquery object, then we return
+    ## the first() jquery element
+    item = [].concat(val)[0]
+
+    if $jquery.isJquery(item)
+      return item.first()
+
+    return item
 
   appendErrMsg: (err, message) ->
     ## preserve stack

packages/driver/src/cy/commands/actions/check.coffee
@@ -1,5 +1,4 @@
 _ = require("lodash")
-$ = require("jquery")
 Promise = require("bluebird")
 
 $dom = require("../../../dom")
@@ -53,7 +52,7 @@ checkOrUncheck = (type, subject, values = [], options = {}) ->
   ## blow up if any member of the subject
   ## isnt a checkbox or radio
   checkOrUncheckEl = (el, index) =>
-    $el = $(el)
+    $el = $dom.wrap(el)
 
     if not isAcceptableElement($el)
       node   = $dom.stringify($el)

packages/driver/src/cy/commands/actions/click.coffee
@@ -1,5 +1,4 @@
 _ = require("lodash")
-$ = require("jquery")
 Promise = require("bluebird")
 
 $Mouse = require("../../../cypress/mouse")
@@ -42,7 +41,7 @@ module.exports = (Commands, Cypress, cy, state, config) ->
       win = state("window")
 
       click = (el, index) =>
-        $el = $(el)
+        $el = $dom.wrap(el)
 
         domEvents = {}
         $previouslyFocusedEl = null
@@ -217,7 +216,7 @@ module.exports = (Commands, Cypress, cy, state, config) ->
       dblclicks = []
 
       dblclick = (el, index) =>
-        $el = $(el)
+        $el = $dom.wrap(el)
 
         ## we want to add this delay delta to our
         ## runnables timeout so we prevent it from

packages/driver/src/cy/commands/actions/select.coffee
@@ -1,5 +1,4 @@
 _ = require("lodash")
-$ = require("jquery")
 Promise = require("bluebird")
 
 $dom = require("../../../dom")
@@ -72,7 +71,7 @@ module.exports = (Commands, Cypress, cy, state, config) ->
           ## push the value in values array if its
           ## found within the valueOrText
           value = $elements.getNativeProp(el, "value")
-          optEl = $(el)
+          optEl = $dom.wrap(el)
 
           if value in valueOrText
             optionEls.push optEl

packages/driver/src/cy/commands/actions/type.coffee
@@ -1,5 +1,4 @@
 _ = require("lodash")
-$ = require("jquery")
 Promise = require("bluebird")
 moment = require("moment")
 
@@ -161,7 +160,7 @@ module.exports = (Commands, Cypress, cy, state, config) ->
 
       getDefaultButtons = (form) ->
         form.find("input, button").filter (__, el) ->
-          $el = $(el)
+          $el = $dom.wrap(el)
           ($dom.isSelector($el, "input") and $dom.isType($el, "submit")) or
           ($dom.isSelector($el, "button") and not $dom.isType($el, "button"))
 
@@ -387,7 +386,7 @@ module.exports = (Commands, Cypress, cy, state, config) ->
       ## blow up if any member of the subject
       ## isnt a textarea or text-like
       clear = (el, index) ->
-        $el = $(el)
+        $el = $dom.wrap(el)
 
         if options.log
           ## figure out the options which actually change the behavior of clicks

packages/driver/src/cy/commands/connectors.coffee
@@ -1,5 +1,4 @@
 _ = require("lodash")
-$ = require("jquery")
 Promise = require("bluebird")
 
 $dom = require("../../dom")
@@ -320,7 +319,7 @@ module.exports = (Commands, Cypress, cy, state, config) ->
         return if endEarly
 
         if $dom.isElement(el)
-          el = $(el)
+          el = $dom.wrap(el)
 
         callback = ->
           ret = fn.call(ctx, el, index, subject)

packages/driver/src/cy/ensures.coffee
@@ -4,6 +4,12 @@ $utils = require("../cypress/utils")
 
 VALID_POSITIONS = "topLeft top topRight left center right bottomLeft bottom bottomRight".split(" ")
 
+## TODO: in 4.0 we should accept a new validation type called 'elements'
+## which accepts an array of elements (and they all have to be elements!!)
+## this would fix the TODO below, and also ensure that commands understand
+## they may need to work with both element arrays, or specific items
+## such as a single element, a single document, or single window
+
 returnFalse = -> return false
 
 create = (state, expect) ->

packages/driver/src/cy/jquery.coffee
@@ -1,6 +1,7 @@
 $ = require("jquery")
 
 $dom = require("../dom")
+$utils = require("../cypress/utils")
 
 remoteJQueryisNotSameAsGlobal = (remoteJQuery) ->
   remoteJQuery and (remoteJQuery isnt $)
@@ -13,7 +14,12 @@ create = (state) ->
     getRemotejQueryInstance: (subject) ->
       remoteJQuery = jquery()
 
-      if $dom.isElement(subject) and remoteJQueryisNotSameAsGlobal(remoteJQuery)
+      ## we make assumptions that you cannot have
+      ## an array of mixed types, so we only look at
+      ## the first item (if there's an array)
+      firstSubject = $utils.unwrapFirst(subject)
+
+      if $dom.isElement(firstSubject) and remoteJQueryisNotSameAsGlobal(remoteJQuery)
         remoteSubject = remoteJQuery(subject)
 
         return remoteSubject

commit c905dec4e7d4088aa5190bd460ab51400d959d05

check that item.first function exists before calling
- should help with support of jQuery legacy
- fix issue introduced by
https://github.com/cypress-io/cypress/pull/2870

packages/driver/src/cypress/utils.coffee
@@ -39,7 +39,8 @@ module.exports = {
     item = [].concat(val)[0]
 
     if $jquery.isJquery(item)
-      return item.first()
+      if item.first
+        return item.first()
 
     return item
 