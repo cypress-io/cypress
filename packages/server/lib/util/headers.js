1a61b1c42de52f43d48160efd95d137eade43458
Zach Bloomquist <github@chary.us>
2019-01-17 20:24:09 -0500

    Show the content type in errors even if it is invalid (#3162)
    Fixes #3101

--- packages/server/lib/util/headers.js
+++ packages/server/lib/util/headers.js
@@ -5,13 +5,14 @@ module.exports = {
     try {
       return contentType.parse(res).type
     } catch (err) {
-      return null
+      // https://github.com/cypress-io/cypress/issues/3101
+      return res.headers['content-type']
     }
   },
 
   hasContentType (res, type) {
-    //# does the response object have a content-type
-    //# that matches what we expect
+    // does the response object have a content-type
+    // that matches what we expect
     try {
       return contentType.parse(res).type === type
     } catch (err) {
