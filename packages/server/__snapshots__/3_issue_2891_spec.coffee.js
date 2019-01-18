9b5349fd9d387511837e8e3b2978f8d44b3a89fe
Zach Bloomquist <zbloomq@live.com>
2019-01-18 12:11:31 -0500

    tests

---
+++ packages/server/__snapshots__/3_issue_2891_spec.coffee.js
@@ -0,0 +1,56 @@
+exports['e2e issue 2891 passes 1'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (default_layout_spec.coffee)                                               │
+  │ Searched:   cypress/integration/default_layout_spec.coffee                                     │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running: default_layout_spec.coffee...                                                   (1 of 1) 
+
+
+  0 passing
+
+
+  (Results)
+
+  ┌──────────────────────────────────────────┐
+  │ Tests:        0                          │
+  │ Passing:      0                          │
+  │ Failing:      0                          │
+  │ Pending:      0                          │
+  │ Skipped:      0                          │
+  │ Screenshots:  0                          │
+  │ Video:        true                       │
+  │ Duration:     X seconds                  │
+  │ Spec Ran:     default_layout_spec.coffee │
+  └──────────────────────────────────────────┘
+
+
+  (Video)
+
+  - Started processing:   Compressing to 32 CRF
+  - Finished processing:  /foo/bar/.projects/default-layout/cypress/videos/abc123.mp4 (X seconds)
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+      Spec                                                Tests  Passing  Failing  Pending  Skipped 
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✔ default_layout_spec.coffee                XX:XX        -        -        -        -        - │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    All specs passed!                           XX:XX        -        -        -        -        -  
+
+
+`
