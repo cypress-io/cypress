a27b31a87171ebac21fa83f177ff6f46ca368626
Zach Bloomquist <github@chary.us>
2019-01-28 15:03:38 -0500

    desktop-gui: use displayName for display

--- packages/desktop-gui/src/lib/browser-model.js
+++ packages/desktop-gui/src/lib/browser-model.js
@@ -1,7 +1,7 @@
-import _ from 'lodash'
 import { computed, observable } from 'mobx'
 
 export default class Browser {
+  @observable displayName
   @observable name
   @observable version
   @observable path
@@ -10,15 +10,12 @@ export default class Browser {
   @observable isChosen = false
 
   constructor (browser) {
+    this.displayName = browser.displayName
     this.name = browser.name
     this.version = browser.version
     this.path = browser.path
     this.majorVersion = browser.majorVersion
     this.info = browser.info
-  }
-
-  @computed get displayName () {
-    return _.capitalize(this.name)
   }
 
   @computed get icon () {
