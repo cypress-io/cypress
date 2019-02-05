9b78616d7a088e54f12ae695fe2fffa8a98db522
Zach Bloomquist <github@chary.us>
2019-02-05 10:18:30 -0500

    PR changes

--- packages/desktop-gui/src/lib/config-file-formatted.jsx
+++ packages/desktop-gui/src/lib/config-file-formatted.jsx
@@ -3,11 +3,11 @@ import { isUndefined } from 'lodash'
 
 const configFileFormatted = (configFile) => {
     if (configFile === false) {
-        return <><code>cypress.json</code> (currently disabled by <code>--config-file false</code>)</>
+        return <><code>cypress.json</code> file (currently disabled by <code>--config-file false</code>)</>
     }
 
     if (isUndefined(configFile) || configFile === 'cypress.json') {
-        return <><code>cypress.json</code></>
+        return <><code>cypress.json</code> file</>
     }
 
     return <>custom config file <code>{configFile}</code></>
