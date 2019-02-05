8bb89155f66a6e6010fd59516bdc11d7a8a323f7
Zach Bloomquist <github@chary.us>
2019-02-05 18:37:46 -0500

    launcher: test that browsers contains what we like it to

---
+++ packages/launcher/__snapshots__/browsers_spec.ts.js
@@ -0,0 +1,30 @@
+exports['browsers returns the expected list of browsers 1'] = [
+  {
+    "name": "chrome",
+    "displayName": "Chrome",
+    "versionRegex": {},
+    "profile": true,
+    "binary": [
+      "google-chrome",
+      "chrome",
+      "google-chrome-stable"
+    ]
+  },
+  {
+    "name": "chromium",
+    "displayName": "Chromium",
+    "versionRegex": {},
+    "profile": true,
+    "binary": [
+      "chromium-browser",
+      "chromium"
+    ]
+  },
+  {
+    "name": "canary",
+    "displayName": "Canary",
+    "versionRegex": {},
+    "profile": true,
+    "binary": "google-chrome-canary"
+  }
+]
