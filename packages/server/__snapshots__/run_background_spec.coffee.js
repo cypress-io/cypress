9c5ad5847ea449eebcb61bd40132833020c61b14
Chris Breiding <chrisbreiding@gmail.com>
2019-02-25 14:59:59 -0500

    update server unit test snapshots

--- packages/server/__snapshots__/run_background_spec.coffee.js
+++ packages/server/__snapshots__/run_background_spec.coffee.js
@@ -1,10 +1,10 @@
 exports['lib/background/child/run_background sends error message if backgroundFile is missing 1'] = `
 Error: Cannot find module '/does/not/exist.coffee'
-    at Function.Module._resolveFilename <path>module.js
-    at Module._load <path>module.js
+    at Function.Module._resolveFilename <path>loader.js
+    at Module._load <path>loader.js
     at Function.hookedLoader [as _load] <path>mockery.js
-    at Module.require <path>module.js
-    at require <path>module.js
+    at Module.require <path>loader.js
+    at require <path>helpers.js
     at module.exports <path>run_background.js
     at Context.<anonymous> <path>run_background_spec.coffee
     at callFn <path>runnable.js
@@ -19,10 +19,10 @@ Error: Cannot find module '/does/not/exist.coffee'
     at callFn <path>runnable.js
     at Hook.Runnable.run <path>runnable.js
     at next <path>runner.js
-    at Immediate.<anonymous> <path>runner.js
+    at Immediate._onImmediate <path>runner.js
     at runCallback <path>timers.js
     at tryOnImmediate <path>timers.js
-    at processImmediate [as _immediateCallback] <path>timers.js
+    at processImmediate <path>timers.js
 
 `
 
@@ -30,14 +30,14 @@ exports['lib/background/child/run_background sends error message if requiring ba
 Error: error thrown by backgroundFile
     at Object.<anonymous> <path>throws_error.coffee
     at Object.<anonymous> <path>throws_error.coffee
-    at Module._compile <path>module.js
+    at Module._compile <path>loader.js
     at Object.loadFile <path>register.js
     at Module.load <path>register.js
-    at tryModuleLoad <path>module.js
-    at Module._load <path>module.js
+    at tryModuleLoad <path>loader.js
+    at Module._load <path>loader.js
     at Function.hookedLoader [as _load] <path>mockery.js
-    at Module.require <path>module.js
-    at require <path>module.js
+    at Module.require <path>loader.js
+    at require <path>helpers.js
     at module.exports <path>run_background.js
     at Context.<anonymous> <path>run_background_spec.coffee
     at callFn <path>runnable.js
@@ -52,10 +52,10 @@ Error: error thrown by backgroundFile
     at callFn <path>runnable.js
     at Hook.Runnable.run <path>runnable.js
     at next <path>runner.js
-    at Immediate.<anonymous> <path>runner.js
+    at Immediate._onImmediate <path>runner.js
     at runCallback <path>timers.js
     at tryOnImmediate <path>timers.js
-    at processImmediate [as _immediateCallback] <path>timers.js
+    at processImmediate <path>timers.js
 
 `
 
