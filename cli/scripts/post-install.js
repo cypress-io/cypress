47755202a3d4b361cab7cb542a2dbf474ab0571e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-04 14:30:32 -0500

    replace types with relative paths

--- cli/scripts/post-install.js
+++ cli/scripts/post-install.js
@@ -9,7 +9,7 @@ shell.set('-e') // any error is fatal
 
 // we include the TypeScript definitions for the bundled 3rd party tools
 // thus we need to copy them from "dev" dependencies into our types folder
-includeTypes.forEach((folder) => {
+includeTypes.forEach(folder => {
   const source = join('node_modules', '@types', folder)
 
   shell.cp('-R', source, 'types')
@@ -28,3 +28,23 @@ try {
 } catch (e) {
   // do nothing on error
 }
+
+// fix paths to Chai, jQuery to be relative
+shell.sed(
+  '-i',
+  '<reference types="chai" />',
+  '<reference path="../chai/index.d.ts" />',
+  join('types', 'chai-jquery', 'index.d.ts')
+)
+shell.sed(
+  '-i',
+  '<reference types="jquery" />',
+  '<reference path="../jquery/index.d.ts" />',
+  join('types', 'chai-jquery', 'index.d.ts')
+)
+shell.sed(
+  '-i',
+  '<reference types="chai" />',
+  '<reference path="../chai/index.d.ts" />',
+  join('types', 'sinon-chai', 'index.d.ts')
+)
