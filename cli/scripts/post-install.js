d18b745ac6d4cc65cdc9db549af1db468e809aed
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-04 14:40:11 -0500

    do not delete d.ts files

--- cli/scripts/post-install.js
+++ cli/scripts/post-install.js
@@ -9,27 +9,13 @@ shell.set('-e') // any error is fatal
 
 // we include the TypeScript definitions for the bundled 3rd party tools
 // thus we need to copy them from "dev" dependencies into our types folder
-includeTypes.forEach((folder) => {
+includeTypes.forEach(folder => {
   const source = join('node_modules', '@types', folder)
 
   shell.cp('-R', source, 'types')
 })
 
-// for these folders, having duplicate folders presents a problem
-// so we need to remove `index.d.ts` files :(
-// Could not find a better way to avoid conflicts during dtslint check
-try {
-  shell.rm('node_modules/@types/jquery/index.d.ts')
-} catch (e) {
-  // do nothing on error
-}
-try {
-  shell.rm('node_modules/@types/chai/index.d.ts')
-} catch (e) {
-  // do nothing on error
-}
-
-// fix paths to Chai, jQuery to be relative
+// fix paths to Chai, jQuery and other types to be relative
 shell.sed(
   '-i',
   '<reference types="chai" />',
