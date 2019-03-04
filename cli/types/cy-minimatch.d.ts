8762518e1ccc0e067b08623ea6d8daa7060da7e3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-04 15:17:35 -0500

    add minimatch relative reference

--- cli/types/cy-minimatch.d.ts
+++ cli/types/cy-minimatch.d.ts
@@ -1,6 +1,8 @@
 // Shim definition to export a namespace. Cypress is actually a global module
 // so import/export isn't allowed there. We import here and define a global module
 // so that Cypress can get and use the Blob type
+/// <reference path="./minimatch/index.d.ts" />
+
 // tslint:disable-next-line:no-implicit-dependencies
 import * as mimimatch from 'minimatch'
 export = Mimimatch
