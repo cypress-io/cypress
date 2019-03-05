f81dc7a6b23b764903460737dcccbc62bb2e74a4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-05 12:49:42 -0500

    move 3rd party types to dev dependencies in CLI NPM package (#3425)
    * wip: move lodash types to dev dependencies
    
    * move blob-util types
    
    * move types for minimatch
    
    * do not lint types from minimatch
    
    * move types sinon to dev dependencies
    
    * move sinon-chai types to dev dependencies
    
    * update tslint
    
    * move types bluebird to dev dependencies
    
    * move mocha types
    
    * move jquery types to dev dependencies
    
    * rename moment local wrapper
    
    * move chai and chai-jquery
    
    * refactor code for building CLI and dealing with folders
    
    * linting
    
    * include types subfolders
    
    * replace types with relative paths
    
    * transform sinon path to relative
    
    * linting
    
    * do not delete d.ts files
    
    * linting
    
    * chore: build npm package from this branch
    
    * add minimatch relative reference
    
    * work around minimatch
    
    * set sinon to be relative load
    
    * add readme to CLI
    
    * linting readme

---
+++ cli/scripts/post-install.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const { includeTypes } = require('./utils')
+const shell = require('shelljs')
+const { join } = require('path')
+
+shell.set('-v') // verbose
+shell.set('-e') // any error is fatal
+
+// we include the TypeScript definitions for the bundled 3rd party tools
+// thus we need to copy them from "dev" dependencies into our types folder
+includeTypes.forEach((folder) => {
+  const source = join('node_modules', '@types', folder)
+
+  shell.cp('-R', source, 'types')
+})
+
+// fix paths to Chai, jQuery and other types to be relative
+shell.sed(
+  '-i',
+  '<reference types="chai" />',
+  '<reference path="../chai/index.d.ts" />',
+  join('types', 'chai-jquery', 'index.d.ts')
+)
+shell.sed(
+  '-i',
+  '<reference types="jquery" />',
+  '<reference path="../jquery/index.d.ts" />',
+  join('types', 'chai-jquery', 'index.d.ts')
+)
+shell.sed(
+  '-i',
+  '<reference types="chai" />',
+  '<reference path="../chai/index.d.ts" />',
+  join('types', 'sinon-chai', 'index.d.ts')
+)
+shell.sed(
+  '-i',
+  '<reference types="sinon" />',
+  '<reference types="../sinon" />',
+  join('types', 'sinon-chai', 'index.d.ts')
+)
+// also use relative import for sinon-chai
+shell.sed(
+  '-i',
+  'from \'sinon\';',
+  'from \'../sinon\';',
+  join('types', 'sinon-chai', 'index.d.ts')
+)
