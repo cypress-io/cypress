5a13bfec4a834bcf731de447754aeaa6e11b452f
Zach Bloomquist <github@chary.us>
2019-03-20 12:29:17 -0400

    ts: add missing Agent type declaration

--- packages/ts/index.d.ts
+++ packages/ts/index.d.ts
@@ -10,3 +10,10 @@ declare module 'plist' {
   const plist: Plist
   export = plist
 }
+
+declare module 'http' {
+  interface Agent {
+    addRequest(req: ClientRequest, options: any): void
+    createSocket(req: ClientRequest, options: any, cb: (err?: Error, sock: Socket) => void): void
+  }
+}
