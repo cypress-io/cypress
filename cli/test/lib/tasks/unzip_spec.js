36ab8b005326f1066c9148cfaa84922f14287875
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-20 16:00:03 -0400

    update snapshot use in verify_spec

--- cli/test/lib/tasks/unzip_spec.js
+++ cli/test/lib/tasks/unzip_spec.js
@@ -15,7 +15,6 @@ const normalize = require('../../support/normalize')
 const version = '1.2.3'
 const installDir = path.join(os.tmpdir(), 'Cypress', version)
 
-
 describe('lib/tasks/unzip', function () {
   require('mocha-banner').register()
   beforeEach(function () {
@@ -60,6 +59,7 @@ describe('lib/tasks/unzip', function () {
     })
     .then(() => {
       expect(onProgress).to.be.called
+
       return fs.statAsync(installDir)
     })
   })
