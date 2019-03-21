1e80583851bcbc9e404414e7c20c7fae2e0fc48f
Zach Bloomquist <github@chary.us>
2019-03-21 17:27:40 -0400

    server: add net_profiler

--- packages/server/index.js
+++ packages/server/index.js
@@ -7,6 +7,12 @@ if (process.versions.electron) {
   require('./timers/parent').fix()
 }
 
+if (process.env.CY_NET_PROFILE) {
+  const netProfiler = require('./lib/util/net_profiler')()
+
+  process.stdout.write(`Network profiler writing to ${netProfiler.logPath}\n`)
+}
+
 process.env.UV_THREADPOOL_SIZE = 128
 require('graceful-fs').gracefulify(require('fs'))
 // if running in production mode (CYPRESS_ENV)
