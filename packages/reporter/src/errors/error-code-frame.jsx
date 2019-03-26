a41c21552bff03ca222a61efff21cf00640eb9d2
Jennifer Shehane <jennifer@cypress.io>
2019-03-26 16:10:27 +0630

    some more syntax highlighting for codeframe - only highlight within pre

--- packages/reporter/src/errors/error-code-frame.jsx
+++ packages/reporter/src/errors/error-code-frame.jsx
@@ -5,18 +5,18 @@ import Prism from 'prismjs'
 @observer
 class ErrorCodeFrame extends Component {
   componentDidMount () {
-    Prism.highlightAll()
+    Prism.highlightAllUnder(this.refs.codeFrame)
   }
 
   render () {
     return (
       <div className='test-error-code-frame'>
         <div className='runnable-err-code-frame-file-path'>users-flow/users-login.spec.js:26:17</div>
-        <pre>
+        <pre ref='codeFrame' data-line='2' className='line-numbers'>
           <code className='language-javascript'>cy.get('.as-table')
-    .find('tbody>tr').first()
+    .find('tbody>tr').eq(12)
     .find('td').first()
-    .find('button').as('firstBtn')</code>
+    .find('button').as('firstBtn').then(() => {})</code>
         </pre>
       </div>
     )
