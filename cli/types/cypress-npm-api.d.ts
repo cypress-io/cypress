6373a8db508733bf9d2e05c3a293faeba792e25d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-02 17:02:23 -0400

    add types for test results

--- cli/types/cypress-npm-api.d.ts
+++ cli/types/cypress-npm-api.d.ts
@@ -333,7 +333,113 @@ declare module 'cypress' {
     project: string
   }
 
+  // small utility types to better express meaning of other types
   type dateTimeISO = string
+  type ms = number
+  type hookId = string
+  type testId = string
+  type pixels = number
+
+  /**
+   * Cypress single test result
+   */
+  interface TestResult {
+    testId: testId
+    title: string[]
+    state: string
+    body: string
+    /**
+     * Error stack string if there is an error
+     */
+    stack: string | null
+    /**
+     * Error message if there is an error
+     */
+    error: string | null
+    timings: any
+    failedFromHookId: hookId | null
+    wallClockStartedAt: dateTimeISO
+    wallClockDuration: ms
+    videoTimestamp: ms
+  }
+
+  /**
+   * Information about a single "before", "beforeEach", "afterEach" and "after" hook.
+  */
+  interface HookInformation {
+    hookId: hookId
+    hookName: 'before' | 'beforeEach' | 'afterEach' | 'after'
+    title: string[]
+    body: string
+  }
+
+  /**
+   * Information about a single screenshot.
+   */
+  interface ScreenshotInformation {
+    screenshotId: string
+    name: string
+    testId: testId
+    takenAt: dateTimeISO
+    /**
+     * Absolute path to the saved image
+     */
+    path: string
+    height: pixels
+    width: pixels
+  }
+
+  /**
+   * Cypress test run result for a single spec.
+  */
+  interface RunResult {
+    /**
+     * Accurate test results collected by Cypress.
+     */
+    stats: {
+      suites: number
+      tests: number
+      passes: number
+      pending: number
+      skipped: number
+      failures: number
+      wallClockStartedAt: dateTimeISO
+      wallClockEndedAt: dateTimeISO
+      wallClockDuration: ms
+    },
+    /**
+     * Reporter name like "spec"
+     */
+    reporter: string
+    /**
+     * This is controlled by the reporter, and Cypress cannot guarantee
+     * the properties. Usually this object has suites, tests, passes, etc
+     */
+    reporterStats: object
+    hooks: HookInformation[]
+    tests: TestResult[]
+    error: string | null
+    video: string | null
+    screenshots: ScreenshotInformation[]
+    /**
+     * information about the spec test file.
+    */
+    spec: {
+      /**
+       * filename like "spec.js"
+       */
+      name: string
+      /**
+       * name relative to the project root, like "cypress/integration/spec.js"
+      */
+      relative: string
+      /**
+       * resolved filename of the spec
+       */
+      absolute: string
+    },
+    shouldUploadVideo: boolean
+  }
 
   /**
    * Results returned by the test run.
@@ -341,6 +447,22 @@ declare module 'cypress' {
   interface CypressRunResult {
     startedTestsAt: dateTimeISO
     endedTestsAt: dateTimeISO
+    totalDuration: ms
+    totalSuites: number
+    totalTests: number
+    totalFailed: number
+    totalPassed: number
+    totalPending: number
+    totalSkipped: number
+    runs: RunResult[]
+    browserPath: string
+    browserName: string
+    browserVersion: string
+    osName: string
+    osVersion: string
+    cypressVersion: string
+    // TODO add resolved object to the configuration
+    config: CypressConfiguration
   }
 
   /**
