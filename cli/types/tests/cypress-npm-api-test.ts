03fc65409b76ef315ea97de71221188c4a09e0a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-02 15:43:45 -0400

    fix expected type

--- cli/types/tests/cypress-npm-api-test.ts
+++ cli/types/tests/cypress-npm-api-test.ts
@@ -1,7 +1,7 @@
 import cypress from 'cypress'
 
-cypress.run // $ExpectType (options: Partial<CypressRunOptions>) => Promise<CypressRunResult>
-cypress.open // $ExpectType (options: Partial<CypressOpenOptions>) => Promise<void>
+cypress.run // $ExpectType (options?: Partial<CypressRunOptions> | undefined) => Promise<CypressRunResult>
+cypress.open // $ExpectType (options?: Partial<CypressOpenOptions> | undefined) => Promise<void>
 cypress.run({}).then(results => {
   results // $ExpectType CypressRunResult
 })
