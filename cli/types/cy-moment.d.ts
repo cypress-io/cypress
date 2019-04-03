143eccd60723df5a1b195feb75cc6962aedea214
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-03 11:25:36 -0400

    add TypeScript definitions for the cypress npm module (#3826)
    * WIP: start TS definition for Cypress NPM module
    
    * describe run and open options
    
    * add jsdoc and make options optional
    
    * fix expected type
    
    * add more properties to Cypress config interface
    
    * explain all config properties
    
    * more definitions
    
    * add types for test results
    
    * export = cypress in NPM module
    
    * remove ts ignore that is no longer needed

--- cli/types/cy-moment.d.ts
+++ cli/types/cy-moment.d.ts
@@ -1,8 +1,4 @@
-// Shim definition to export a namespace. Cypress is actually a global module
-// so import/export isn't allowed there. We import here and define a global module
-// so that Cypress can get and use the Moment type
-// tslint:disable-next-line:no-implicit-dependencies
-import * as moment from 'moment'
+import moment = require('moment')
 export = Moment
 export as namespace Moment
 
