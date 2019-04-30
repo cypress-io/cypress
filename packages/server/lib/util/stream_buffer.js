e3959b215219ed55821e66fa90f3dec787afe68e
Zach Bloomquist <github@chary.us>
2019-04-30 16:22:43 -0400

    remove fs stuff from stream_buffer, add stream piping tests

--- packages/server/lib/util/stream_buffer.js
+++ packages/server/lib/util/stream_buffer.js
@@ -1,43 +1,36 @@
 const debug = require('debug')('cypress:server:stream_buffer')
-const fs = require('fs')
 const stream = require('stream')
 const through2 = require('through2')
 
-function streamBuffer (filename, highWaterMark = 4096) {
-  let buffer = Buffer.alloc(highWaterMark)
+function streamBuffer (initialSize = 2048) {
+  let buffer = Buffer.alloc(initialSize)
   let bytesWritten = 0
-  let writeStream
-  let writeStreamReady = false
   let finished = false
 
   const onChunk = (chunk, enc, cb) => {
-    if (!writeStream && bytesWritten + chunk.length <= highWaterMark) {
-      debug('appending chunk to buffer %o', { bytesWritten, chunkLength: chunk.length })
+    if (chunk.length + bytesWritten > buffer.length) {
+      let newBufferLength = buffer.length
 
-      bytesWritten += chunk.copy(buffer, bytesWritten)
-
-      return cb(null, chunk)
-    }
-
-    // chunk won't fit in buffer, begin writing to disk
-    if (!writeStream) {
-      debug('buffer too big, bufferin to disk %o', { filename })
-      writeStream = fs.createWriteStream(filename)
-      writeStream.write(buffer.slice(0, bytesWritten))
-    }
-
-    debug('writing chunk to disk %o', { chunkLength: chunk.length })
-    writeStream.write(chunk, (err) => {
-      debug('chunk written %o', { err })
-
-      if (err) {
-        return cb(err)
+      while (newBufferLength < chunk.length + bytesWritten) {
+        newBufferLength *= 2
       }
 
-      writeStreamReady = true
+      debug('extending buffer to accomodate new chunk', {
+        bufferLength: buffer.length,
+        newBufferLength,
+      })
 
-      cb(null, chunk)
-    })
+      const newBuffer = Buffer.alloc(newBufferLength)
+
+      buffer.copy(newBuffer)
+      buffer = newBuffer
+    }
+
+    debug('appending chunk to buffer %o', { bytesWritten, chunkLength: chunk.length })
+
+    bytesWritten += chunk.copy(buffer, bytesWritten)
+
+    return cb(null, chunk)
   }
 
   const onFlush = (cb) => {
@@ -47,26 +40,11 @@ function streamBuffer (filename, highWaterMark = 4096) {
 
   const bufferer = through2(onChunk, onFlush)
 
-  const reader = () => {
+  bufferer.reader = () => {
     let bytesRead = 0
-    let readStream
 
     const readable = new stream.Readable({
-      read (size = highWaterMark) {
-        // if there's a file on disk, we should be reading from that
-        if (writeStreamReady) {
-          if (!readStream) {
-            debug('reading stream from file %o', { filename, start: bytesRead })
-            readStream = fs.createReadStream(filename, { start: bytesRead })
-          }
-
-          const chunk = readStream.read(size)
-
-          debug('reading chunk from file %o', { size, chunk: chunk ? chunk.toString() : chunk })
-
-          return this.push(chunk || '')
-        }
-
+      read (size = initialSize) {
         // if there are unread bytes in the buffer, send up to bytesWritten back
         if (bytesRead < bytesWritten) {
           const chunkLength = Math.min(size, bytesWritten)
@@ -95,10 +73,14 @@ function streamBuffer (filename, highWaterMark = 4096) {
     return readable
   }
 
-  return {
-    bufferer,
-    reader,
+  bufferer._buffer = () => {
+    return buffer
   }
+  bufferer._finished = () => {
+    return finished
+  }
+
+  return bufferer
 }
 
 module.exports = {
--- packages/server/test/unit/stream_buffer_spec.js
+++ packages/server/test/unit/stream_buffer_spec.js
@@ -1,58 +1,116 @@
 const { describe, it } = require('mocha')
 const { expect } = require('chai')
+const fs = require('fs')
 const streamBuffer = require('../../lib/util/stream_buffer')
 const stream = require('stream')
 
-describe('lib/util/stream_buffer', function () {
-  it('reads out no matter when we write', function (done) {
-    const pt = stream.PassThrough()
-    const ls = streamBuffer.streamBuffer(`/tmp/test-${Number(new Date())}`, 22)
+function drain (stream) {
+  let buf = ''
+  let chunk
 
-    pt.pipe(ls.bufferer)
+  while ((chunk = stream.read())) {
+    buf += chunk
+  }
+
+  return buf
+}
+
+describe('lib/util/stream_buffer', function () {
+  it('reads out no matter when we write', function () {
+    const pt = stream.PassThrough()
+    const sb = streamBuffer.streamBuffer()
+
+    pt.pipe(sb)
     pt.write('test')
     pt.write('test 2')
 
-    let chunk
-    let buf = ''
-    const reader = ls.reader()
+    const reader = sb.reader()
 
-    while ((chunk = reader.read())) {
-      buf += chunk
-    }
+    let buf = drain(reader)
 
     expect(buf).to.eq('testtest 2')
 
-    // this won't trigger a disk write
     pt.write('test 3')
 
-    while ((chunk = reader.read())) {
-      buf += chunk
-    }
+    buf += drain(reader)
 
     expect(buf).to.eq('testtest 2test 3')
 
     pt.write('test 4')
-    // this will trigger a disk write
+
+    const reader2 = sb.reader()
+
+    let buf2 = drain(reader2)
+
+    expect(buf2).to.eq('testtest 2test 3test 4')
+
     pt.write('test 5')
 
-    while ((chunk = reader.read())) {
-      buf += chunk
-    }
-
+    buf += drain(reader)
     expect(buf).to.eq('testtest 2test 3test 4test 5')
 
-    // const r = () => {
-    //   setImmediate(() => {
-    //     if (buf.length < 28) {
-    //       buf += reader.read() || ''
-
-    //       return r()
-    //     }
-
-    //     done()
-    //   })
-    // }
-
-    // r()
+    buf2 += drain(reader2)
+    expect(buf2).to.eq('testtest 2test 3test 4test 5')
   })
+
+  it('on overflow, enlarges the internal buffer by the smallest power of 2 that can fit the chunk', function () {
+    const sb = streamBuffer.streamBuffer(64)
+
+    sb.write('A'.repeat(65))
+
+    expect(sb._buffer().length).to.eq(128)
+
+    sb.write('A'.repeat(1024))
+
+    expect(sb._buffer().length).to.eq(2048)
+
+    const reader = sb.reader()
+    const buf = drain(reader)
+
+    expect(buf).to.eq('A'.repeat(1089))
+  })
+
+  it('finishes when buffer stream closes while still allowing data to be drained', function () {
+    const sb = streamBuffer.streamBuffer()
+
+    sb.pipe(sb)
+
+    sb.write('foo')
+    sb.write('bar')
+
+    expect(sb._finished()).to.be.false
+
+    sb.end()
+    expect(sb._finished()).to.be.true
+
+    const reader = sb.reader()
+    const buf = drain(reader)
+
+    expect(buf).to.eq('foobar')
+
+    const reader2 = sb.reader()
+    const buf2 = drain(reader2)
+
+    expect(buf2).to.eq('foobar')
+  })
+
+  it('can be piped into and then read from', function (done) {
+    const expected = fs.readFileSync(__filename).toString()
+    const rs = fs.createReadStream(__filename)
+    const ws = fs.createWriteStream('/dev/null')
+    const sb = streamBuffer.streamBuffer()
+
+    rs.pipe(sb).pipe(ws)
+
+    const reader = sb.reader()
+
+    rs.on('end', () => {
+      const buf = drain(reader)
+
+      expect(buf).to.eq(expected)
+
+      done()
+    })
+  })
+
 })
