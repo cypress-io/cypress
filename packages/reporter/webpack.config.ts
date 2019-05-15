819f8b4a23e3d1b982442ec7a8baca8ee275bbb1
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-05-15 12:16:21 -0400

    node-sass-globbing force unix paths for windows 2

--- packages/reporter/webpack.config.ts
+++ packages/reporter/webpack.config.ts
@@ -93,6 +93,7 @@ const config: webpack.Configuration = {
               sourceMap: true,
               importer: function(...args) {
 								args[0] = args[0].replace(/\\/g, '/')
+								args[1] = args[1].replace(/\\/g, '/')
 								console.log(args)
 								return sassGlobImporter.apply(this, args)
 							},
