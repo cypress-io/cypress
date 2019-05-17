bed50de27843f0d0e4a2577d9f30bac47c39bf44
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-05-17 16:35:32 -0400

    add test for extending types 03-00000014

---
+++ cli/types/tests/plugins-test.ts
@@ -0,0 +1,27 @@
+/**
+ * Test that plugin authors can write .d.ts files to extend Cypress types
+ * Unfortunately we cannot test that vendor types located in node_modules are working
+ * since those are copied as park of the deploy process
+ *
+ */
+
+declare namespace Cypress {
+  interface cy {
+    cyProp: number
+	}
+	interface Chainable {
+    chainerProp: number
+  }
+}
+
+// $ExpectType number
+cy.cyProp
+
+// $ExpectError
+cy.get('.element').cyProp
+
+// $ExpectType number
+cy.chainerProp
+
+// $ExpectType number
+cy.get('.element').chainerProp
