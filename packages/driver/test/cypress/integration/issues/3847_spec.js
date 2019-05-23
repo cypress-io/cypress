9f144496322ac412c33d1e9fe3ae923cbbbe11ec
Chris Breiding <chrisbreiding@gmail.com>
2019-05-23 14:49:36 -0400

    fix more tests

--- packages/driver/test/cypress/integration/issues/3847_spec.js
+++ packages/driver/test/cypress/integration/issues/3847_spec.js
@@ -11,7 +11,7 @@ describe('issue 3847', () => {
   })
 
   it('options default { log: true } should be work without Unhandled rejection', (done) => {
-    cy.on('fail', (err) => {
+    cy.on('test:fail', (err) => {
       expect(err.message).to.eql(error.message)
       expect(err.name).to.eql(error.name)
       done()
@@ -27,7 +27,7 @@ describe('issue 3847', () => {
   it('options { log: false } will not throw Unhandled rejection', (done) => {
 
     // error should seem like { log: true }
-    cy.on('fail', (err) => {
+    cy.on('test:fail', (err) => {
       expect(err.message).to.eql(error.message)
       expect(err.name).to.eql(error.name)
       expect(err.message).not.to.match(/Unhandled\srejection\sTypeError/)
