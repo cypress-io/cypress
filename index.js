49b4f48c73a0a4d9fe0a520dde72a6c525698490
Chris Breiding <chrisbreiding@gmail.com>
2019-05-29 10:22:05 -0400

    patch: improve lazy-loading of babel deps and add tests

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-const cloneDeep = require('lodash.clonedeep')
 const path = require('path')
 const webpack = require('webpack')
 const debug = require('debug')('cypress:webpack')
 
 const createDeferred = require('./deferred')
+const stubbableRequire = require('./stubbable-require')
 
 const bundles = {}
 
-// by default, we transform JavaScript supported by @babel/preset-env
-const defaultBabelLoaderRules = () => {
-  return [
-    {
-      test: /\.js?$/,
-      exclude: [/node_modules/],
-      use: [
+// we don't automatically load the rules, so that the babel dependencies are
+// not required if a user passes in their own configuration
+const getDefaultWebpackOptions = () => {
+  debug('load default options')
+
+  return {
+    module: {
+      rules: [
         {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [require.resolve('@babel/preset-env')],
-          },
+          test: /\.jsx?$/,
+          exclude: [/node_modules/],
+          use: [
+            {
+              loader: stubbableRequire.resolve('babel-loader'),
+              options: {
+                presets: [stubbableRequire.resolve('@babel/preset-env')],
+              },
+            },
+          ],
         },
       ],
     },
-  ]
-}
-
-// we don't automatically load the rules, so that the babel dependencies are
-// not required if a user passes in their own configuration
-const defaultOptions = {
-  webpackOptions: {
-    module: {
-      rules: [],
-    },
-  },
-  watchOptions: {},
+  }
 }
 
 // export a function that returns another function, making it easy for users
@@ -57,6 +53,7 @@ const preprocessor = (options = {}) => {
   // the supported file and spec file to be requested again
   return (file) => {
     const filePath = file.filePath
+
     debug('get', filePath)
 
     // since this function can get called multiple times with the same
@@ -64,17 +61,16 @@ const preprocessor = (options = {}) => {
     // since we don't want or need to re-initiate webpack for it
     if (bundles[filePath]) {
       debug(`already have bundle for ${filePath}`)
+
       return bundles[filePath]
     }
 
     // user can override the default options
-    let webpackOptions = Object.assign({}, defaultOptions.webpackOptions, options.webpackOptions)
-    // here is where we load the default rules if the user has not passed
-    // in their own configuration
-    if (webpackOptions.module.rules === defaultOptions.webpackOptions) {
-      webpackOptions.module.rules = defaultBabelLoaderRules()
-    }
-    let watchOptions = Object.assign({}, defaultOptions.watchOptions, options.watchOptions)
+    let webpackOptions = options.webpackOptions || getDefaultWebpackOptions()
+    const watchOptions = options.watchOptions || {}
+
+    debug('webpackOptions: %o', webpackOptions)
+    debug('watchOptions: %o', watchOptions)
 
     // we're provided a default output path that lives alongside Cypress's
     // app data files so we don't have to worry about where to put the bundled
@@ -186,14 +182,16 @@ const preprocessor = (options = {}) => {
   }
 }
 
-// provide a clone of the default options, making sure to lazy-load
-// babel dependencies so that they aren't required unless the user
-// utilizes them
+// provide a clone of the default options, lazy-loading them
+// so they aren't required unless the user utilizes them
 Object.defineProperty(preprocessor, 'defaultOptions', {
   get () {
-    const clonedDefaults = cloneDeep(defaultOptions)
-    clonedDefaults.webpackOptions.module.rules = defaultBabelLoaderRules()
-    return clonedDefaults
+    debug('get default options')
+
+    return {
+      webpackOptions: getDefaultWebpackOptions(),
+      watchOptions: {},
+    }
   },
 })
 
