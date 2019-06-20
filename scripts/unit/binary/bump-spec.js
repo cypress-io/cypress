aa2b80524d9a13a03daf6ccda65210d2a857b338
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-20 12:56:59 -0400

    linting

--- scripts/unit/binary/bump-spec.js
+++ scripts/unit/binary/bump-spec.js
@@ -9,6 +9,7 @@ describe('bump', () => {
     it('returns flat list of projects', () => {
       la(bump._PROVIDERS, 'has _PROVIDERS', bump)
       const list = bump.remapProjects(bump._PROVIDERS)
+
       snapshot('list of all projects', list)
     })
   })
@@ -19,6 +20,7 @@ describe('bump', () => {
       const filter = bump.getFilterByProvider()
       // should return ALL projects
       const filtered = R.filter(filter, projects)
+
       la(
         R.equals(filtered, projects),
         'should have kept all projects',
@@ -28,6 +30,7 @@ describe('bump', () => {
 
     it('returns a filter function for circle and darwin', () => {
       const projects = bump.remapProjects(bump._PROVIDERS)
+
       la(
         projects.length,
         'there should be at least a few projects in the list of projects',
@@ -36,6 +39,7 @@ describe('bump', () => {
 
       const filter = bump.getFilterByProvider('circle', 'darwin')
       const filtered = R.filter(filter, projects)
+
       la(filtered.length, 'there should be at least a few projects', filtered)
       snapshot('should have just circle and darwin projects', filtered)
     })
