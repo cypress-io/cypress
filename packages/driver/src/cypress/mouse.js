d73bbff78ac6406acb5c357c3336e528dd1c3ae8
decaffeinate <14625260+Bkucera@users.noreply.github.com>
2019-07-02 16:46:06 -0400

    decaffeinate: Run post-processing cleanups on mouse.coffee

--- packages/driver/src/cypress/mouse.js
+++ packages/driver/src/cypress/mouse.js
@@ -1,22 +1,28 @@
+/* eslint-disable
+    no-unused-vars,
+    prefer-rest-params,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const Promise = require("bluebird");
+const Promise = require('bluebird')
 
-const $Cypress = require("../cypress");
-const $Keyboard = require("./keyboard");
-const $dom = require("../dom");
+const $Cypress = require('../cypress')
+const $Keyboard = require('./keyboard')
+const $dom = require('../dom')
 
-const { stopPropagation } = window.MouseEvent.prototype;
+const { stopPropagation } = window.MouseEvent.prototype
 
 module.exports = {
-  mouseDown($elToClick, fromViewport) {
-    const el = $elToClick.get(0);
+  mouseDown ($elToClick, fromViewport) {
+    const el = $elToClick.get(0)
 
-    const win = $dom.getWindowByElement(el);
+    const win = $dom.getWindowByElement(el)
 
     const mdownEvtProps = $Keyboard.mixinModifiers({
       bubbles: true,
@@ -25,35 +31,42 @@ module.exports = {
       clientX: fromViewport.x,
       clientY: fromViewport.y,
       buttons: 1,
-      detail: 1
-    });
+      detail: 1,
+    })
 
-    const mdownEvt = new window.MouseEvent("mousedown", mdownEvtProps);
+    const mdownEvt = new window.MouseEvent('mousedown', mdownEvtProps)
 
     //# ensure this property exists on older chromium versions
-    if (mdownEvt.buttons == null) { mdownEvt.buttons = 1; }
+    if (mdownEvt.buttons == null) {
+      mdownEvt.buttons = 1
+    }
 
-    mdownEvt.stopPropagation = function() {
-      this._hasStoppedPropagation = true;
-      return stopPropagation.apply(this, arguments);
-    };
+    mdownEvt.stopPropagation = function () {
+      this._hasStoppedPropagation = true
 
-    const cancelled = !el.dispatchEvent(mdownEvt);
+      return stopPropagation.apply(this, arguments)
+    }
+
+    const cancelled = !el.dispatchEvent(mdownEvt)
 
     const props = {
       preventedDefault: cancelled,
-      stoppedPropagation: !!mdownEvt._hasStoppedPropagation
-    };
+      stoppedPropagation: !!mdownEvt._hasStoppedPropagation,
+    }
 
-    const modifiers = $Keyboard.activeModifiers();
-    if (modifiers.length) { props.modifiers = modifiers.join(", "); }
-    return props;
+    const modifiers = $Keyboard.activeModifiers()
+
+    if (modifiers.length) {
+      props.modifiers = modifiers.join(', ')
+    }
+
+    return props
   },
 
-  mouseUp($elToClick, fromViewport) {
-    const el = $elToClick.get(0);
+  mouseUp ($elToClick, fromViewport) {
+    const el = $elToClick.get(0)
 
-    const win = $dom.getWindowByElement(el);
+    const win = $dom.getWindowByElement(el)
 
     const mupEvtProps = $Keyboard.mixinModifiers({
       bubbles: true,
@@ -62,35 +75,42 @@ module.exports = {
       clientX: fromViewport.x,
       clientY: fromViewport.y,
       buttons: 0,
-      detail: 1
-    });
+      detail: 1,
+    })
 
-    const mupEvt = new MouseEvent("mouseup", mupEvtProps);
+    const mupEvt = new MouseEvent('mouseup', mupEvtProps)
 
     //# ensure this property exists on older chromium versions
-    if (mupEvt.buttons == null) { mupEvt.buttons = 0; }
+    if (mupEvt.buttons == null) {
+      mupEvt.buttons = 0
+    }
 
-    mupEvt.stopPropagation = function() {
-      this._hasStoppedPropagation = true;
-      return stopPropagation.apply(this, arguments);
-    };
+    mupEvt.stopPropagation = function () {
+      this._hasStoppedPropagation = true
 
-    const cancelled = !el.dispatchEvent(mupEvt);
+      return stopPropagation.apply(this, arguments)
+    }
+
+    const cancelled = !el.dispatchEvent(mupEvt)
 
     const props = {
       preventedDefault: cancelled,
-      stoppedPropagation: !!mupEvt._hasStoppedPropagation
-    };
+      stoppedPropagation: !!mupEvt._hasStoppedPropagation,
+    }
 
-    const modifiers = $Keyboard.activeModifiers();
-    if (modifiers.length) { props.modifiers = modifiers.join(", "); }
-    return props;
+    const modifiers = $Keyboard.activeModifiers()
+
+    if (modifiers.length) {
+      props.modifiers = modifiers.join(', ')
+    }
+
+    return props
   },
 
-  click($elToClick, fromViewport) {
-    const el = $elToClick.get(0);
+  click ($elToClick, fromViewport) {
+    const el = $elToClick.get(0)
 
-    const win = $dom.getWindowByElement(el);
+    const win = $dom.getWindowByElement(el)
 
     const clickEvtProps = $Keyboard.mixinModifiers({
       bubbles: true,
@@ -99,28 +119,35 @@ module.exports = {
       clientX: fromViewport.x,
       clientY: fromViewport.y,
       buttons: 0,
-      detail: 1
-    });
+      detail: 1,
+    })
 
-    const clickEvt = new MouseEvent("click", clickEvtProps);
+    const clickEvt = new MouseEvent('click', clickEvtProps)
 
     //# ensure this property exists on older chromium versions
-    if (clickEvt.buttons == null) { clickEvt.buttons = 0; }
+    if (clickEvt.buttons == null) {
+      clickEvt.buttons = 0
+    }
 
-    clickEvt.stopPropagation = function() {
-      this._hasStoppedPropagation = true;
-      return stopPropagation.apply(this, arguments);
-    };
+    clickEvt.stopPropagation = function () {
+      this._hasStoppedPropagation = true
 
-    const cancelled = !el.dispatchEvent(clickEvt);
+      return stopPropagation.apply(this, arguments)
+    }
+
+    const cancelled = !el.dispatchEvent(clickEvt)
 
     const props = {
       preventedDefault: cancelled,
-      stoppedPropagation: !!clickEvt._hasStoppedPropagation
-    };
+      stoppedPropagation: !!clickEvt._hasStoppedPropagation,
+    }
 
-    const modifiers = $Keyboard.activeModifiers();
-    if (modifiers.length) { props.modifiers = modifiers.join(", "); }
-    return props;
-  }
-};
+    const modifiers = $Keyboard.activeModifiers()
+
+    if (modifiers.length) {
+      props.modifiers = modifiers.join(', ')
+    }
+
+    return props
+  },
+}
