1e16c2fd2d80f7b483f4cf777d394b93dd60fb71
Jennifer Shehane <jennifer@cypress.io>
2019-07-15 22:56:26 +0630

    remove skip from fixture tests (since 2.2.0 api was released)

--- packages/desktop-gui/cypress/integration/fixtures_spec.js
+++ packages/desktop-gui/cypress/integration/fixtures_spec.js
@@ -8,9 +8,7 @@ const omitErrObjects = {
 }
 
 describe('fixtures match api object schemas: ', () => {
-  // 2.2.0 isn't released yet, so this always fails
-  // TODO: remove .skip after API is released
-  it.skip('runs', () => {
+  it('runs', () => {
     cy.fixture('runs').each(assertSchema('getRunResponse', '2.2.0', {
       substitutions: ['orgId'], omit: omitErrObjects,
     }))
