38937619dab3e8d3b495ce7d9dc04003c94565c2
Jennifer Shehane <jennifer@cypress.io>
2019-07-19 22:53:27 +0630

    push updated node version panel design

--- packages/desktop-gui/src/settings/node-version.jsx
+++ packages/desktop-gui/src/settings/node-version.jsx
@@ -27,33 +27,37 @@ const NodeVersion = observer(({ project }) => {
       return <code>{pluginsFile}</code>
     }
 
-    return 'currently disabled'
+    return null
+  }
+
+  function formatSupportFile () {
+    const supportFile = _.get(resolvedConfig, 'supportFile.value')
+
+    if (supportFile) {
+      return <code>{supportFile}</code>
+    }
+
+    return null
+  }
+
+  function formatIntegrationFolder () {
+    const integrationFolder = _.get(resolvedConfig, 'integrationFolder.value')
+
+    return <code>{integrationFolder}</code>
   }
 
   return (
     <div className="node-version">
       {renderLearnMore()}
-      <p className="text-muted">
-        Used to build files found in the <code>cypress</code> folder and execute code in the plugins in {formatPluginsFile()}. To access features or modules only available in your system Node version, set <code>"nodeVersion": "system"</code> in your configuration. Otherwise, everything will use the bundled Node version that comes with Cypress.
+      <p>
+        <strong>Node Version v{resolvedNodeVersion}</strong> {!resolvedNodePath && <span className='text-muted'>(bundled with Cypress)</span>} {resolvedNodePath ? <span> at <code>{resolvedNodePath}</code></span> : ''} is used to:
+        <ul>
+          <li>Build files in the {formatIntegrationFolder()} folder.</li>
+          <li>Build files in the {formatSupportFile() ? formatSupportFile() : 'support'} folder.</li>
+          <li>Execute code in the {formatPluginsFile() ? formatPluginsFile() : 'plugins'} file.</li>
+        </ul>
+        To access features or modules only available in your system Node version, set <code>"nodeVersion": "system"</code> in your configuration. Otherwise, everything will use the bundled Node version that comes with Cypress.
       </p>
-      <table className="node-table">
-        <tbody>
-          <tr>
-            <th>
-              Node Version:
-            </th>
-            <td className="version">
-              v{resolvedNodeVersion} {!resolvedNodePath && <span className='text-muted'>(bundled with Cypress)</span>}
-            </td>
-          </tr>
-          {resolvedNodePath && <tr>
-            <th>Node Path:</th>
-            <td className="path">
-              <code>{resolvedNodePath}</code>
-            </td>
-          </tr>}
-        </tbody>
-      </table>
     </div>
   )
 })
