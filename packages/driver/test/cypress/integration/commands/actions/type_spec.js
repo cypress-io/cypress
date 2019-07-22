0ddd36a52efa8518ec63e70572ee1e9669c05fb7
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-07-22 18:16:00 -0400

    fix newlines

--- packages/driver/test/cypress/integration/commands/actions/type_spec.js
+++ packages/driver/test/cypress/integration/commands/actions/type_spec.js
@@ -12,10 +12,33 @@ const trimInnerText = ($el) => {
 }
 
 describe('src/cy/commands/actions/type', () => {
-
   beforeEach(function () {
     cy
     .visit('/fixtures/dom.html')
+    .then(function (win) {
+      const el = cy.$$('[contenteditable]:first').get(0)
+
+      // by default... the last new line by itself
+      // will only ever count as a single new line...
+      // but new lines above it will count as 2 new lines...
+      // so by adding "3" new lines, the last counts as 1
+      // and the first 2 count as 2...
+      el.innerHTML = '<div><br></div>'.repeat(3)
+
+      // browsers changed their implementation
+      // of the number of newlines that <div><br></div>
+      // create. newer versions of chrome set 2 new lines
+      // per set - whereas older ones create only 1 new line.
+      // so we grab the current sets for the assertion later
+      // so this test is browser version agnostic
+      const newLines = el.innerText
+
+      // disregard the last new line, and divide by 2...
+      // this tells us how many multiples of new lines
+      // the browser inserts for new lines other than
+      // the last new line
+      this.multiplierNumNewLines = (newLines.length - 1) / 2
+    })
   })
 
   context('#type', () => {
