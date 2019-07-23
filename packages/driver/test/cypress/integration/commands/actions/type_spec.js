75f4f46ec562f97418398b3645fb410847e15de8
Jennifer Shehane <jennifer@cypress.io>
2019-07-23 13:10:06 +0630

    update logic to check for readonly prop to retry / throw err on failure of retry

--- packages/driver/test/cypress/integration/commands/actions/type_spec.js
+++ packages/driver/test/cypress/integration/commands/actions/type_spec.js
@@ -271,7 +271,7 @@ describe('src/cy/commands/actions/type', () => {
         })
 
         cy.on('command:retry', _.after(3, () => {
-          $txt.removeProp('readonly')
+          $txt.prop('readonly', false)
           retried = true
         }))
 
@@ -4226,9 +4226,9 @@ describe('src/cy/commands/actions/type', () => {
           cy.get(`#${attrs.id}`).type('foo')
 
           cy.on('fail', (err) => {
-            expect(err.message).to.include('cy.type() cannot type into an element with a \'readonly\' attribute.')
-            expect(err.message).to.include('The element typed into was:')
+            expect(err.message).to.include('cy.type() failed because this element is readonly:')
             expect(err.message).to.include(`<input id="${attrs.id}" readonly="${attrs.val}">`)
+            expect(err.message).to.include('Fix this problem, or use {force: true} to disable error checking.')
 
             done()
           })
