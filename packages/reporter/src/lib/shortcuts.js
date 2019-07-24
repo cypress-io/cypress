ef21cbc1ee6120cfd715a1f5ad93dce054bbbf18
Jennifer Shehane <jennifer@cypress.io>
2019-07-24 14:35:25 +0630

    remove events that are not coded to work yet

--- packages/reporter/src/lib/shortcuts.js
+++ packages/reporter/src/lib/shortcuts.js
@@ -14,16 +14,7 @@ class Shortcuts {
         break
       case 's': events.emit('stop')
         break
-      case 'n': events.emit('next')
-        break
-      case 'c': events.emit('resume')
-        break
       case 'f': events.emit('focus:tests')
-        break
-      case 'a': {
-        this._appState.toggleAutoScrolling()
-        events.emit('save:state')
-      }
         break
       default: return
     }
