d261a389dcd0ba32e8073323a57e46284dc363f4
Jennifer Shehane <jennifer@cypress.io>
2019-07-26 12:29:52 +0630

    fix favicon task that I broke. :grimacing:

--- packages/static/gulpfile.js
+++ packages/static/gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp')
 const icons = require('@cypress/icons')
 
-gulp.task('build', gulp.parallel('favicon', () => {
+gulp.task('favicon', () => {
   return gulp.src(icons.getPathToFavicon('**/**'))
   .pipe(gulp.dest('./dist'))
-}))
+})
+
+gulp.task('build', gulp.parallel('favicon'))
