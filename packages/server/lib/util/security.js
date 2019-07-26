b3fe0232c177e28841481eb9c1062d3dfbc80648
Brian Mann <brian.mann86@gmail.com>
2019-07-26 13:40:55 -0400

    small readability refactor

--- packages/server/lib/util/security.js
+++ packages/server/lib/util/security.js
@@ -7,7 +7,7 @@ const stream = require('stream')
 const pumpify = require('pumpify')
 const replacestream = require('replacestream')
 
-const topOrParentEqualityBeforeRe = /((?:window|self)(?:\.|\[['"](?:top|self)['"]\])?\s*[!=]==?\s*(?:(?:window|self)(?:\.|\[['"]))?)(top(?![\w])|parent(?![\w]))/g
+const topOrParentEqualityBeforeRe = /((?:window|self)(?:\.|\[['"](?:top|self)['"]\])?\s*[!=]==?\s*(?:(?:window|self)(?:\.|\[['"]))?)(top|parent)(?![\w])/g
 const topOrParentEqualityAfterRe = /(top|parent)((?:["']\])?\s*[!=]==?\s*(?:window|self))/g
 const topOrParentLocationOrFramesRe = /([^\da-zA-Z\(\)])?(top|parent)([.])(location|frames)/g
 const jiraTopWindowGetterRe = /(!function\s*\((\w{1})\)\s*{\s*return\s*\w{1}\s*(?:={2,})\s*\w{1}\.parent)(\s*}\(\w{1}\))/g
