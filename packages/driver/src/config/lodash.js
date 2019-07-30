375ef9837e59a0fc7a5a2b8a778ed395a2217041
decaffeinate <14625260+Bkucera@users.noreply.github.com>
2019-07-29 21:17:39 -0400

    decaffeinate: Run post-processing cleanups on lodash.coffee and 1 other file

--- packages/driver/src/config/lodash.js
+++ packages/driver/src/config/lodash.js
@@ -1,13 +1,13 @@
-const _ = require("lodash");
+const _ = require('lodash')
 
-const inflection = require("@cypress/underscore.inflection")(_);
+const inflection = require('@cypress/underscore.inflection')(_)
 
-//# only export exactly what we need, nothing more!
+// only export exactly what we need, nothing more!
 _.mixin({
-  clean:      require("underscore.string/clean"),
-  count:      require("underscore.string/count"),
-  isBlank:    require("underscore.string/isBlank"),
-  toBoolean:  require("underscore.string/toBoolean"),
-  capitalize: require("underscore.string/capitalize"), //# its mo' better the lodash version
-  ordinalize: inflection.ordinalize
-});
+  clean: require('underscore.string/clean'),
+  count: require('underscore.string/count'),
+  isBlank: require('underscore.string/isBlank'),
+  toBoolean: require('underscore.string/toBoolean'),
+  capitalize: require('underscore.string/capitalize'), // its mo' better the lodash version
+  ordinalize: inflection.ordinalize,
+})
--- packages/driver/src/cy/jquery.js
+++ packages/driver/src/cy/jquery.js
@@ -1,36 +1,35 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const $ = require("jquery");
+const $ = require('jquery')
 
-const $dom = require("../dom");
-const $utils = require("../cypress/utils");
+const $dom = require('../dom')
+const $utils = require('../cypress/utils')
 
-const remoteJQueryisNotSameAsGlobal = remoteJQuery => remoteJQuery && (remoteJQuery !== $);
+const remoteJQueryisNotSameAsGlobal = (remoteJQuery) => {
+  return remoteJQuery && (remoteJQuery !== $)
+}
 
-const create = function(state) {
-  const jquery = () => state("jQuery") || state("window").$;
+const create = function (state) {
+  const jquery = () => {
+    return state('jQuery') || state('window').$
+  }
 
   return {
-    getRemotejQueryInstance(subject) {
-      const remoteJQuery = jquery();
+    getRemotejQueryInstance (subject) {
+      const remoteJQuery = jquery()
 
-      //# we make assumptions that you cannot have
-      //# an array of mixed types, so we only look at
-      //# the first item (if there's an array)
-      const firstSubject = $utils.unwrapFirst(subject);
+      // we make assumptions that you cannot have
+      // an array of mixed types, so we only look at
+      // the first item (if there's an array)
+      const firstSubject = $utils.unwrapFirst(subject)
 
       if ($dom.isElement(firstSubject) && remoteJQueryisNotSameAsGlobal(remoteJQuery)) {
-        const remoteSubject = remoteJQuery(subject);
+        const remoteSubject = remoteJQuery(subject)
 
-        return remoteSubject;
+        return remoteSubject
       }
-    }
-  };
-};
+    },
+  }
+}
 
 module.exports = {
-  create
-};
+  create,
+}
