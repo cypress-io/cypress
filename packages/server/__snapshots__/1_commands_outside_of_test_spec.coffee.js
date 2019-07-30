8a2e24ece27930e5b6555ccf249d654e83a1edee
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-30 16:28:40 -0400

    updated spec snapshots

--- packages/server/__snapshots__/1_commands_outside_of_test_spec.coffee.js
+++ packages/server/__snapshots__/1_commands_outside_of_test_spec.coffee.js
@@ -73,12 +73,6 @@ exports['e2e commands outside of test [chrome] fails on cy commands 1'] = `
                                                                                                     
   Running: commands_outside_of_test_spec.coffee...                                         (1 of 1) 
 
-Warning: Cypress can only record videos when using the built in 'electron' browser.
-
-You have set the browser to: 'chrome'
-
-A video will not be recorded when using this browser.
-
 
   1) An uncaught error was detected outside of a test
 
@@ -123,7 +117,7 @@ We dynamically generated a new test to display this failure.
   │ Pending:      0                                    │
   │ Skipped:      0                                    │
   │ Screenshots:  1                                    │
-  │ Video:        false                                │
+  │ Video:        true                                 │
   │ Duration:     X seconds                            │
   │ Spec Ran:     commands_outside_of_test_spec.coffee │
   └────────────────────────────────────────────────────┘
@@ -132,6 +126,12 @@ We dynamically generated a new test to display this failure.
   (Screenshots)
 
   - /foo/bar/.projects/e2e/cypress/screenshots/commands_outside_of_test_spec.coffee/An uncaught error was detected outside of a test (failed).png (YYYYxZZZZ)
+
+
+  (Video)
+
+  - Started processing:   Compressing to 32 CRF
+  - Finished processing:  /foo/bar/.projects/e2e/cypress/videos/abc123.mp4 (X seconds)
 
 
 ====================================================================================================
@@ -166,12 +166,6 @@ exports['e2e commands outside of test [chrome] fails on failing assertions 1'] =
                                                                                                     
   Running: assertions_failing_outside_of_test_spec.coffee...                               (1 of 1) 
 
-Warning: Cypress can only record videos when using the built in 'electron' browser.
-
-You have set the browser to: 'chrome'
-
-A video will not be recorded when using this browser.
-
 
   1) An uncaught error was detected outside of a test
 
@@ -202,7 +196,7 @@ We dynamically generated a new test to display this failure.
   │ Pending:      0                                              │
   │ Skipped:      0                                              │
   │ Screenshots:  1                                              │
-  │ Video:        false                                          │
+  │ Video:        true                                           │
   │ Duration:     X seconds                                      │
   │ Spec Ran:     assertions_failing_outside_of_test_spec.coffee │
   └──────────────────────────────────────────────────────────────┘
@@ -211,6 +205,12 @@ We dynamically generated a new test to display this failure.
   (Screenshots)
 
   - /foo/bar/.projects/e2e/cypress/screenshots/assertions_failing_outside_of_test_spec.coffee/An uncaught error was detected outside of a test (failed).png (YYYYxZZZZ)
+
+
+  (Video)
+
+  - Started processing:   Compressing to 32 CRF
+  - Finished processing:  /foo/bar/.projects/e2e/cypress/videos/abc123.mp4 (X seconds)
 
 
 ====================================================================================================
