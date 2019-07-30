efcd9a48c8a7fdfa0b304eb9d94a931bbd1593c0
decaffeinate <jennifer@cypress.io>
2019-07-30 11:49:14 +0630

    decaffeinate: Convert cookies_spec.coffee and 3 other files to JS

--- packages/server/test/e2e/2_cookies_spec.js
+++ packages/server/test/e2e/2_cookies_spec.js
@@ -1,88 +1,101 @@
-moment   = require("moment")
-parser   = require("cookie-parser")
-e2e      = require("../support/helpers/e2e")
-humanInterval = require("human-interval")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const moment   = require("moment");
+const parser   = require("cookie-parser");
+const e2e      = require("../support/helpers/e2e");
+const humanInterval = require("human-interval");
 
-onServer = (app) ->
-  app.use(parser())
+const onServer = function(app) {
+  app.use(parser());
 
-  app.get "/logout", (req, res) ->
-    res.send("<html>logged out</html>")
+  app.get("/logout", (req, res) => res.send("<html>logged out</html>"));
 
-  app.get "/requestCookies", (req, res) ->
-    res.send(req.cookies)
+  app.get("/requestCookies", (req, res) => res.send(req.cookies));
 
-  app.get "/set", (req, res) ->
-    res.cookie("shouldExpire", "endOfsession")
+  app.get("/set", function(req, res) {
+    res.cookie("shouldExpire", "endOfsession");
 
-    res.send("<html></html>")
+    return res.send("<html></html>");
+  });
 
-  app.get "/setOneHourFromNowAndSecure", (req, res) ->
+  app.get("/setOneHourFromNowAndSecure", function(req, res) {
     res.cookie("shouldExpire", "oneHour", {
-      secure: true
+      secure: true,
       maxAge: humanInterval("1 hour")
-    })
+    });
 
-    res.send("<html></html>")
+    return res.send("<html></html>");
+  });
 
-  app.get "/expirationRedirect", (req, res) ->
+  app.get("/expirationRedirect", function(req, res) {
     res.cookie("shouldExpire", "now", {
-      ## express maxAge is relative to current time
-      ## in milliseconds
+      //# express maxAge is relative to current time
+      //# in milliseconds
       maxAge: 0
-    })
+    });
 
-    res.redirect("/logout")
+    return res.redirect("/logout");
+  });
 
-  app.get "/expirationMaxAge", (req, res) ->
+  app.get("/expirationMaxAge", function(req, res) {
     res.header("Set-Cookie",
       "shouldExpire=; Max-Age=0; Path=/; Expires=Sun, 24 Jun 1997 20:36:13 GMT"
-    )
-    ## response to set
-    # auth=p1_2FruNr1entizk9QEGHFYQlWjIK5LULzdDj17lkYhZTz7XA5GOfnVgbbeBDAqnfImkwof2qz0M3yi3AUVusKPqh1BRK6253h0kiBENwdjWDsx3mYQQKpHn6o3XOXX7poSkzrHThnrDlH4w9zoLItwIVNhR2hQrCYhQhtHuw20YM_3D; Domain=.surveymonkey.com;Max-Age=3600; Path=/; expires=Fri, 26-Oct-2018 06:13:48 GMT; secure; HttpOnly'
+    );
+    //# response to set
+    // auth=p1_2FruNr1entizk9QEGHFYQlWjIK5LULzdDj17lkYhZTz7XA5GOfnVgbbeBDAqnfImkwof2qz0M3yi3AUVusKPqh1BRK6253h0kiBENwdjWDsx3mYQQKpHn6o3XOXX7poSkzrHThnrDlH4w9zoLItwIVNhR2hQrCYhQhtHuw20YM_3D; Domain=.surveymonkey.com;Max-Age=3600; Path=/; expires=Fri, 26-Oct-2018 06:13:48 GMT; secure; HttpOnly'
 
-    ## response to clear
-    # auth=; Domain=.surveymonkey.com; Max-Age=0; Path=/; expires=Wed, 31-Dec-97 23:59:59 GMT
+    //# response to clear
+    // auth=; Domain=.surveymonkey.com; Max-Age=0; Path=/; expires=Wed, 31-Dec-97 23:59:59 GMT
 
-    res.send("<html></html>")
+    return res.send("<html></html>");
+  });
 
-  app.get "/expirationExpires", (req, res) ->
+  app.get("/expirationExpires", function(req, res) {
     res.cookie("shouldExpire", "now", {
       expires: moment().subtract(1, "day").toDate()
-    })
+    });
 
-    res.send("<html></html>")
+    return res.send("<html></html>");
+  });
 
-  app.get "/cookieWithNoName", (req, res) ->
+  app.get("/cookieWithNoName", function(req, res) {
     res.header("Set-Cookie",
       "=deleted; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/"
-    )
+    );
 
-    res.send("<html></html>")
+    return res.send("<html></html>");
+  });
 
-  app.get "/invalidCookies", (req, res) ->
-    res.header("Set-Cookie", "foo=bar; domain=nope.not.this.one")
+  return app.get("/invalidCookies", function(req, res) {
+    res.header("Set-Cookie", "foo=bar; domain=nope.not.this.one");
 
-    res.send("<html></html>")
+    return res.send("<html></html>");
+  });
+};
 
-describe "e2e cookies", ->
+describe("e2e cookies", function() {
   e2e.setup({
     servers: [{
-      onServer
+      onServer,
       port: 2121
     }, {
-      onServer
-      port: 2323
+      onServer,
+      port: 2323,
       https: true
-    }]
+    }],
     settings: {
       baseUrl: "https://localhost:2323/",
     }
-  })
+  });
 
-  it "passes", ->
-    e2e.exec(@, {
-      spec: "cookies_spec.coffee"
-      snapshot: true
+  return it("passes", function() {
+    return e2e.exec(this, {
+      spec: "cookies_spec.coffee",
+      snapshot: true,
       expectedExitCode: 0
-    })
+    });
+  });
+});
--- packages/server/test/support/fixtures/projects/e2e/cypress/integration/cookies_spec.js
+++ packages/server/test/support/fixtures/projects/e2e/cypress/integration/cookies_spec.js
@@ -1,96 +1,101 @@
-describe "cookies", ->
-  beforeEach ->
-    cy.wrap({foo: "bar"})
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+describe("cookies", function() {
+  beforeEach(() => cy.wrap({foo: "bar"}));
 
-  context "with whitelist", ->
-    before ->
+  context("with whitelist", function() {
+    before(() =>
       Cypress.Cookies.defaults({
         whitelist: "foo1"
       })
+    );
 
-    it "can get all cookies", ->
+    it("can get all cookies", () =>
       cy
         .clearCookie("foo1")
-        .setCookie("foo", "bar").then (c) ->
-          expect(c.domain).to.eq("localhost")
-          expect(c.httpOnly).to.eq(false)
-          expect(c.name).to.eq("foo")
-          expect(c.value).to.eq("bar")
-          expect(c.path).to.eq("/")
-          expect(c.secure).to.eq(false)
-          expect(c.expiry).to.be.a("number")
+        .setCookie("foo", "bar").then(function(c) {
+          expect(c.domain).to.eq("localhost");
+          expect(c.httpOnly).to.eq(false);
+          expect(c.name).to.eq("foo");
+          expect(c.value).to.eq("bar");
+          expect(c.path).to.eq("/");
+          expect(c.secure).to.eq(false);
+          expect(c.expiry).to.be.a("number");
 
-          expect(c).to.have.keys(
+          return expect(c).to.have.keys(
             "domain", "name", "value", "path", "secure", "httpOnly", "expiry"
-          )
-        .getCookies()
+          );}).getCookies()
           .should("have.length", 1)
-          .then (cookies) ->
-            c = cookies[0]
+          .then(function(cookies) {
+            const c = cookies[0];
 
-            expect(c.domain).to.eq("localhost")
-            expect(c.httpOnly).to.eq(false)
-            expect(c.name).to.eq("foo")
-            expect(c.value).to.eq("bar")
-            expect(c.path).to.eq("/")
-            expect(c.secure).to.eq(false)
-            expect(c.expiry).to.be.a("number")
+            expect(c.domain).to.eq("localhost");
+            expect(c.httpOnly).to.eq(false);
+            expect(c.name).to.eq("foo");
+            expect(c.value).to.eq("bar");
+            expect(c.path).to.eq("/");
+            expect(c.secure).to.eq(false);
+            expect(c.expiry).to.be.a("number");
 
-            expect(c).to.have.keys(
+            return expect(c).to.have.keys(
               "domain", "name", "value", "path", "secure", "httpOnly", "expiry"
-            )
-        .clearCookies()
+            );}).clearCookies()
           .should("be.null")
         .setCookie("wtf", "bob", {httpOnly: true, path: "/foo", secure: true})
-        .getCookie("wtf").then (c) ->
-          expect(c.domain).to.eq("localhost")
-          expect(c.httpOnly).to.eq(true)
-          expect(c.name).to.eq("wtf")
-          expect(c.value).to.eq("bob")
-          expect(c.path).to.eq("/foo")
-          expect(c.secure).to.eq(true)
-          expect(c.expiry).to.be.a("number")
+        .getCookie("wtf").then(function(c) {
+          expect(c.domain).to.eq("localhost");
+          expect(c.httpOnly).to.eq(true);
+          expect(c.name).to.eq("wtf");
+          expect(c.value).to.eq("bob");
+          expect(c.path).to.eq("/foo");
+          expect(c.secure).to.eq(true);
+          expect(c.expiry).to.be.a("number");
 
-          expect(c).to.have.keys(
+          return expect(c).to.have.keys(
             "domain", "name", "value", "path", "secure", "httpOnly", "expiry"
-          )
-        .clearCookie("wtf")
+          );}).clearCookie("wtf")
           .should("be.null")
         .getCookie("doesNotExist")
           .should("be.null")
         .document()
           .its("cookie")
           .should("be.empty")
+    );
 
-    it "resets cookies between tests correctly", ->
-      Cypress.Cookies.preserveOnce("foo2")
+    it("resets cookies between tests correctly", function() {
+      Cypress.Cookies.preserveOnce("foo2");
 
-      for i in [1..100]
-        do (i) ->
-          cy.setCookie("foo" + i, "#{i}")
+      for (let i = 1; i <= 100; i++) {
+        (i => cy.setCookie(`foo${i}`, `${i}`))(i);
+      }
 
-      cy.getCookies().should("have.length", 100)
+      return cy.getCookies().should("have.length", 100);
+    });
 
-    it "should be only two left now", ->
-      cy.getCookies().should("have.length", 2)
+    it("should be only two left now", () => cy.getCookies().should("have.length", 2));
 
-    it "handles undefined cookies", ->
-      cy.visit("http://localhost:2121/cookieWithNoName")
+    return it("handles undefined cookies", () => cy.visit("http://localhost:2121/cookieWithNoName"));
+  });
 
-  context "without whitelist", ->
-    before ->
+  return context("without whitelist", function() {
+    before(() =>
       Cypress.Cookies.defaults({
         whitelist: []
       })
+    );
 
-    it "sends cookies to localhost:2121", ->
+    it("sends cookies to localhost:2121", () =>
       cy
         .clearCookies()
         .setCookie("asdf", "jkl")
         .request("http://localhost:2121/requestCookies")
           .its("body").should("deep.eq", { asdf: "jkl" })
+    );
 
-    it "handles expired cookies secure", ->
+    it("handles expired cookies secure", () =>
       cy
         .visit("http://localhost:2121/set")
         .getCookie("shouldExpire").should("exist")
@@ -100,8 +105,9 @@ describe "cookies", ->
         .getCookie("shouldExpire").should("exist")
         .visit("http://localhost:2121/expirationExpires")
         .getCookie("shouldExpire").should("not.exist")
+    );
 
-    it "issue: #224 sets expired cookies between redirects", ->
+    it("issue: #224 sets expired cookies between redirects", () =>
       cy
         .visit("http://localhost:2121/set")
         .getCookie("shouldExpire").should("exist")
@@ -113,25 +119,28 @@ describe "cookies", ->
         .getCookie("shouldExpire").should("exist")
         .request("http://localhost:2121/expirationRedirect")
         .getCookie("shouldExpire").should("not.exist")
+    );
 
-    it "issue: #1321 failing to set or parse cookie", ->
-      ## this is happening because the original cookie was set
-      ## with a secure flag, and then expired without the secure
-      ## flag.
+    it("issue: #1321 failing to set or parse cookie", () =>
+      //# this is happening because the original cookie was set
+      //# with a secure flag, and then expired without the secure
+      //# flag.
       cy
         .visit("https://localhost:2323/setOneHourFromNowAndSecure")
         .getCookies().should("have.length", 1)
 
-        ## secure cookies should have been attached
+        //# secure cookies should have been attached
         .request("https://localhost:2323/requestCookies")
           .its("body").should("deep.eq", { shouldExpire: "oneHour" })
 
-        ## secure cookies should not have been attached
+        //# secure cookies should not have been attached
         .request("http://localhost:2121/requestCookies")
           .its("body").should("deep.eq", {})
 
         .visit("https://localhost:2323/expirationMaxAge")
         .getCookies().should("be.empty")
+    );
 
-    it "issue: #2724 does not fail on invalid cookies", ->
-      cy.request('https://localhost:2323/invalidCookies')
+    return it("issue: #2724 does not fail on invalid cookies", () => cy.request('https://localhost:2323/invalidCookies'));
+  });
+});
