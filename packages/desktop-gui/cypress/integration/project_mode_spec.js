b405fe7ae00dd0bafe232a593a3ca854e7ed48aa
Chris Breiding <chrisbreiding@gmail.com>
2019-08-01 14:37:58 -0400

    dry up project mode spec

--- packages/desktop-gui/cypress/integration/project_mode_spec.js
+++ packages/desktop-gui/cypress/integration/project_mode_spec.js
@@ -2,80 +2,46 @@ describe('Project Mode', function () {
   beforeEach(function () {
     cy.fixture('user').as('user')
     cy.fixture('config').as('config')
-
     cy.fixture('specs').as('specs')
-  })
 
-  context('Mac', function () {
-    beforeEach(function () {
-      cy.visitIndex().then((win) => {
-        let start = win.App.start
+    cy.visitIndex().then((win) => {
+      this.start = win.App.start
+      this.win = win
+      this.ipc = win.App.ipc
 
-        this.win = win
-        this.ipc = win.App.ipc
+      this.config.projectName = 'my-kitchen-sink'
 
-        this.config.projectName = 'my-kitchen-sink'
+      cy.stub(this.ipc, 'onMenuClicked')
+      cy.stub(this.ipc, 'onFocusTests')
+      cy.stub(this.ipc, 'getOptions').resolves({ projectRoot: '/foo/bar' })
+      cy.stub(this.ipc, 'updaterCheck').resolves(false)
+      cy.stub(this.ipc, 'openProject').resolves(this.config)
+      cy.stub(this.ipc, 'getSpecs').yields(null, this.specs)
 
-        cy.stub(this.ipc, 'onMenuClicked')
-        cy.stub(this.ipc, 'onFocusTests')
-        cy.stub(this.ipc, 'getOptions').resolves({ projectRoot: '/foo/bar' })
-        cy.stub(this.ipc, 'updaterCheck').resolves(false)
-        cy.stub(this.ipc, 'openProject').resolves(this.config)
-        cy.stub(this.ipc, 'getSpecs').yields(null, this.specs)
-
-        this.getCurrentUser = this.util.deferred()
-
-        start()
-      })
-    })
-
-    it('goes straight to project specs list', () => {
-      cy.shouldBeOnProjectSpecs()
-    })
-
-    it('sets title as project path', () => {
-      cy.title().should('eq', '/foo/bar')
-    })
-
-    it('shows project name in nav', function () {
-      cy.get('.main-nav .nav:first-child div')
-      .should('contain', this.config.projectName)
-      .and('not.contain', 'foo')
+      this.getCurrentUser = this.util.deferred()
     })
   })
 
-  context('Windows', function () {
-    beforeEach(function () {
-      cy.visitIndex().then((win) => {
-        let start;
+  it('goes straight to project specs list', function () {
+    this.start()
+    cy.shouldBeOnProjectSpecs()
+  })
 
-        ({ start, ipc: this.ipc } = win.App)
+  it('sets title as project path on Mac/Linux', function () {
+    this.start()
+    cy.title().should('eq', '/foo/bar')
+  })
 
-        cy.stub(this.ipc, 'onMenuClicked')
-        cy.stub(this.ipc, 'onFocusTests')
-        cy.stub(this.ipc, 'getOptions').resolves({ projectRoot: 'C:\\foo\\bar' })
-        cy.stub(this.ipc, 'updaterCheck').resolves(false)
-        cy.stub(this.ipc, 'openProject').resolves(this.config)
-        cy.stub(this.ipc, 'getSpecs').yields(null, this.specs)
+  it('sets title as project path on Windows', function () {
+    this.ipc.getOptions.resolves({ projectRoot: 'C:\\foo\\bar' })
+    this.start()
+    cy.title().should('eq', 'C:\\foo\\bar')
+  })
 
-        this.getCurrentUser = this.util.deferred()
-
-        start()
-      })
-    })
-
-    it('goes straight to project specs list', () => {
-      cy.shouldBeOnProjectSpecs()
-    })
-
-    it('sets title as project path', () => {
-      cy.title().should('eq', 'C:\\foo\\bar')
-    })
-
-    it('shows project name in nav', function () {
-      cy.get('.main-nav .nav:first-child div')
-      .should('contain', this.config.projectName)
-      .and('not.contain', 'foo')
-    })
+  it('shows project name in nav', function () {
+    this.start()
+    cy.get('.main-nav .nav:first-child div')
+    .should('contain', this.config.projectName)
+    .and('not.contain', 'foo')
   })
 })
