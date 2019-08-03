71c76b1cb65a00c63733afbbc8cc78ddcb683b5c
Mayfield <3902405+mayfieldiv@users.noreply.github.com>
2019-08-02 21:03:14 -0400

    refactor invoke for clarity. use dtslint tests

--- cli/types/index.d.ts
+++ cli/types/index.d.ts
@@ -814,7 +814,10 @@ declare namespace Cypress {
      *
      * @see https://on.cypress.io/invoke
      */
-    invoke<K extends { [K in keyof Subject]: Subject[K] extends (...args: any[]) => any ? K : never }[keyof Subject], R extends Subject[K] extends (...args: any[]) => infer R ? R : never>(functionName: K, ...args: any[]): Chainable<R>
+    invoke<
+      TName extends { [K in keyof Subject]: Subject[K] extends (...args: any[]) => any ? K : never }[keyof Subject],
+      TReturn extends Subject[TName] extends (...args: any[]) => infer R ? R : never,
+    >(functionName: TName, ...args: any[]): Chainable<TReturn>
 
     /**
      * Get a property’s value on the previously yielded subject.
