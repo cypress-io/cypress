a55d16af3d83d84bef1652eddae7674709a6d285
Zach Bloomquist <github@chary.us>
2019-08-21 10:42:28 -0400

    use commit-info for timestamp, commit message

--- packages/server/test/support/helpers/performance.js
+++ packages/server/test/support/helpers/performance.js
@@ -1,6 +1,5 @@
-const _ = require('lodash')
 const ciProvider = require('../../../lib/util/ci_provider')
-const execa = require('execa')
+const { commitInfo } = require('@cypress/commit-info')
 const pkg = require('../../../../../package.json')
 const Promise = require('bluebird')
 const rp = require('request-promise')
@@ -14,40 +13,39 @@ function track (type, data) {
     return Promise.resolve()
   }
 
-  return Promise.join(
-    execa.stdout('git', ['log', 'HEAD', '-1', '|', 'tail', '-n1'], { shell: true }).then(_.trim),
-    execa.stdout('git', ['log', '-1', '--format=%cd']).then((d) => new Date(d).toISOString()),
-    (commitMessage, commitTimestamp) => {
-      const { sha, branch, author } = ciProvider.commitParams()
+  return commitInfo()
+  .then(({ message, timestamp }) => {
+    const { sha, branch, author } = ciProvider.commitParams()
 
-      const body = {
-        type,
-        data: {
-          'package.json Version': pkg.version,
-          'Next Version': process.env.NEXT_DEV_VERSION,
-          'Commit SHA': sha,
-          'Commit Branch': branch,
-          'Commit Author': author,
-          'Commit Message': commitMessage,
-          'Commit Timestamp': commitTimestamp,
-          'Build URL': process.env.CIRCLE_BUILD_URL,
-          'Build Platform': process.platform,
-          'Build Arch': process.arch,
-          ...data,
-        },
-      }
+    timestamp = new Date(timestamp * 1000).toISOString()
 
-      return rp.post({
-        url: API_URL,
-        json: true,
-        headers: {
-          authorization: `token ${API_KEY}`,
-        },
-        body,
-        timeout: 5000,
-      })
+    const body = {
+      type,
+      data: {
+        'package.json Version': pkg.version,
+        'Next Version': process.env.NEXT_DEV_VERSION,
+        'Commit SHA': sha,
+        'Commit Branch': branch,
+        'Commit Author': author,
+        'Commit Message': message,
+        'Commit Timestamp': timestamp,
+        'Build URL': process.env.CIRCLE_BUILD_URL,
+        'Build Platform': process.platform,
+        'Build Arch': process.arch,
+        ...data,
+      },
     }
-  )
+
+    return rp.post({
+      url: API_URL,
+      json: true,
+      headers: {
+        authorization: `token ${API_KEY}`,
+      },
+      body,
+      timeout: 5000,
+    })
+  })
   .catchReturn()
 }
 
