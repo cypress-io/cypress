ca0b74930c15229eb1ac6165031c5a0ae2eccb16
Zach Bloomquist <github@chary.us>
2019-08-21 14:19:48 -0400

    Move fast_visit_spec to performance-test stage, track perf (#4989)
    * Move fast_visit_spec to performance-test stage
    
    * upload results from fast_visit_spec and proxy_performance_spec
    
    * test
    
    * Track URL in proxy_perf_spec
    
    * also send commit timestamp
    
    * use more params, use ciProviders where possible
    
    * test
    
    * add platform + arch
    
    * use commit-info for timestamp, commit message
    
    * Revert "use commit-info for timestamp, commit message"
    
    This reverts commit a55d16af3d83d84bef1652eddae7674709a6d285.
    
    * use commit-info for timestamp, commit message
    
    * just always have nock
    
    * slice(1) the testCases
    
    * send current retry with fast_visit_spec
    
    * currentRetry

---
+++ packages/server/test/performance/cy_visit_performance_spec.js
@@ -0,0 +1,50 @@
+const e2e = require('../support/helpers/e2e')
+
+// https://github.com/cypress-io/cypress/issues/4313
+context('cy.visit performance tests', function () {
+  this.retries(3)
+
+  e2e.setup({
+    servers: {
+      port: 3434,
+      onServer (app) {
+        app.get('/keepalive', function (req, res) {
+          res.type('html').send('hi')
+        })
+
+        app.get('/close', function (req, res) {
+          res.set('connection', 'close').type('html').send('hi')
+        })
+      },
+    },
+    settings: {
+      baseUrl: 'http://localhost:3434',
+    },
+  })
+
+  const onStdout = (stdout) => {
+    return stdout.replace(/^\d+%\s+of visits to [^\s]+ finished in less than.*$/gm, 'histogram line')
+  }
+
+  context('pass', function () {
+    [
+      'chrome',
+      'electron',
+    ].forEach((browser) => {
+      it(`in ${browser}`, function () {
+        return e2e.exec(this, {
+          spec: 'fast_visit_spec.coffee',
+          snapshot: true,
+          expectedExitCode: 0,
+          config: {
+            env: {
+              currentRetry: this.test._currentRetry,
+            },
+          },
+          browser,
+          onStdout,
+        })
+      })
+    })
+  })
+})
