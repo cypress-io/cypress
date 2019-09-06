e119bb5595af88102686421d79031fdee41e2b1e
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-09-06 18:17:09 -0400

    fix e2e test e2e_config_fails

--- packages/server/test/support/fixtures/projects/e2e/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/e2e/cypress/plugins/index.js
@@ -3,7 +3,7 @@ const Jimp = require('jimp')
 const path = require('path')
 const Promise = require('bluebird')
 
-const performance = require('../../../../test/support/helpers/performance')
+const performance = require('../../../../../helpers/performance')
 
 module.exports = (on) => {
   // save some time by only reading the originals once
