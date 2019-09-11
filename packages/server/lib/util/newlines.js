66da3096fb1884250c434de7bb7e1fd9a76e3465
Jennifer Shehane <jennifer@cypress.io>
2019-09-11 17:52:37 -0400

    Add gaurd in newLine function in case there is no specfile name

--- packages/server/lib/util/newlines.js
+++ packages/server/lib/util/newlines.js
@@ -2,7 +2,7 @@ const addNewlineAtEveryNChar = (str, n) => {
   // Add a newline char after every 'n' char
   let result = ''
 
-  while (str.length > 0) {
+  while (str && str.length > 0) {
     result += `${str.substring(0, n)}\n`
     str = str.substring(n)
   }
