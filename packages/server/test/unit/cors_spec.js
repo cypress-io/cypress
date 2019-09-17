40b389ca9f7e33992fe0fdbe90ba253e7d0d2ec0
decaffeinate <github@chary.us>
2019-09-17 12:06:50 -0400

    decaffeinate: Convert blacklist_spec.coffee and 3 other files to JS

--- packages/server/test/unit/cors_spec.js
+++ packages/server/test/unit/cors_spec.js
@@ -1,214 +1,264 @@
-require("../spec_helper")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+require("../spec_helper");
 
-cors = require("#{root}lib/util/cors")
+const cors = require(`${root}lib/util/cors`);
 
-describe "lib/util/cors", ->
-  context ".parseUrlIntoDomainTldPort", ->
-    beforeEach ->
-      @isEq = (url, obj) ->
-        expect(cors.parseUrlIntoDomainTldPort(url)).to.deep.eq(obj)
+describe("lib/util/cors", function() {
+  context(".parseUrlIntoDomainTldPort", function() {
+    beforeEach(function() {
+      return this.isEq = (url, obj) => expect(cors.parseUrlIntoDomainTldPort(url)).to.deep.eq(obj);
+    });
 
-    it "parses https://www.google.com", ->
-      @isEq("https://www.google.com", {
-        port: "443"
-        domain: "google"
+    it("parses https://www.google.com", function() {
+      return this.isEq("https://www.google.com", {
+        port: "443",
+        domain: "google",
         tld: "com"
-      })
+      });
+    });
 
-    it "parses http://localhost:8080", ->
-      @isEq("http://localhost:8080", {
-        port: "8080"
-        domain: ""
+    it("parses http://localhost:8080", function() {
+      return this.isEq("http://localhost:8080", {
+        port: "8080",
+        domain: "",
         tld: "localhost"
-      })
+      });
+    });
 
-    it "parses http://app.localhost:8080", ->
-      @isEq("http://app.localhost:8080", {
-        port: "8080"
-        domain: "app"
+    it("parses http://app.localhost:8080", function() {
+      return this.isEq("http://app.localhost:8080", {
+        port: "8080",
+        domain: "app",
         tld: "localhost"
-      })
+      });
+    });
 
-    it "parses http://app.localhost.dev:8080", ->
-      @isEq("http://app.localhost.dev:8080", {
-        port: "8080"
-        domain: "localhost"
+    it("parses http://app.localhost.dev:8080", function() {
+      return this.isEq("http://app.localhost.dev:8080", {
+        port: "8080",
+        domain: "localhost",
         tld: "dev"
-      })
+      });
+    });
 
-    it "parses http://app.local:8080", ->
-      @isEq("http://app.local:8080", {
-        port: "8080"
-        domain: "app"
+    it("parses http://app.local:8080", function() {
+      return this.isEq("http://app.local:8080", {
+        port: "8080",
+        domain: "app",
         tld: "local"
-      })
+      });
+    });
 
-    ## public suffix example of a private tld
-    it "parses https://example.herokuapp.com", ->
-      @isEq("https://example.herokuapp.com", {
-        port: "443"
-        domain: "example"
+    //# public suffix example of a private tld
+    it("parses https://example.herokuapp.com", function() {
+      return this.isEq("https://example.herokuapp.com", {
+        port: "443",
+        domain: "example",
         tld: "herokuapp.com"
-      })
+      });
+    });
 
-    it "parses http://www.local.nl", ->
-      @isEq("http://www.local.nl", {
-        port: "80"
-        domain: "local"
+    it("parses http://www.local.nl", function() {
+      return this.isEq("http://www.local.nl", {
+        port: "80",
+        domain: "local",
         tld: "nl"
-      })
+      });
+    });
 
-    ## https://github.com/cypress-io/cypress/issues/3717
-    it "parses http://dev.classea12.beta.gouv.fr", ->
-      @isEq("http://dev.classea12.beta.gouv.fr", {
-        port: "80"
-        domain: "beta"
+    //# https://github.com/cypress-io/cypress/issues/3717
+    it("parses http://dev.classea12.beta.gouv.fr", function() {
+      return this.isEq("http://dev.classea12.beta.gouv.fr", {
+        port: "80",
+        domain: "beta",
         tld: "gouv.fr"
-      })
+      });
+    });
 
-    it "parses http://www.local.nl:8080", ->
-      @isEq("http://www.local.nl:8080", {
-        port: "8080"
-        domain: "local"
+    it("parses http://www.local.nl:8080", function() {
+      return this.isEq("http://www.local.nl:8080", {
+        port: "8080",
+        domain: "local",
         tld: "nl"
-      })
+      });
+    });
 
-    it "parses 192.168.1.1:8080", ->
-      @isEq("http://192.168.1.1:8080", {
-        port: "8080"
-        domain: ""
+    return it("parses 192.168.1.1:8080", function() {
+      return this.isEq("http://192.168.1.1:8080", {
+        port: "8080",
+        domain: "",
         tld: "192.168.1.1"
-      })
+      });
+    });
+  });
 
-  context ".urlMatchesOriginPolicyProps", ->
-    beforeEach ->
-      @isFalse = (url, props) =>
-        expect(cors.urlMatchesOriginPolicyProps(url, props)).to.be.false
+  context(".urlMatchesOriginPolicyProps", function() {
+    beforeEach(function() {
+      this.isFalse = (url, props) => {
+        return expect(cors.urlMatchesOriginPolicyProps(url, props)).to.be.false;
+      };
 
-      @isTrue = (url, props) =>
-        expect(cors.urlMatchesOriginPolicyProps(url, props)).to.be.true
+      return this.isTrue = (url, props) => {
+        return expect(cors.urlMatchesOriginPolicyProps(url, props)).to.be.true;
+      };
+    });
 
-    describe "domain + subdomain", ->
-      beforeEach ->
-        @props = cors.parseUrlIntoDomainTldPort("https://staging.google.com")
+    describe("domain + subdomain", function() {
+      beforeEach(function() {
+        return this.props = cors.parseUrlIntoDomainTldPort("https://staging.google.com");
+      });
 
-      it "does not match", ->
-        @isFalse("https://foo.bar:443", @props)
-        @isFalse("http://foo.bar:80", @props)
-        @isFalse("http://foo.bar", @props)
-        @isFalse("http://staging.google.com", @props)
-        @isFalse("http://staging.google.com:80", @props)
-        @isFalse("https://staging.google2.com:443", @props)
-        @isFalse("https://staging.google.net:443", @props)
-        @isFalse("https://google.net:443", @props)
-        @isFalse("http://google.com", @props)
+      it("does not match", function() {
+        this.isFalse("https://foo.bar:443", this.props);
+        this.isFalse("http://foo.bar:80", this.props);
+        this.isFalse("http://foo.bar", this.props);
+        this.isFalse("http://staging.google.com", this.props);
+        this.isFalse("http://staging.google.com:80", this.props);
+        this.isFalse("https://staging.google2.com:443", this.props);
+        this.isFalse("https://staging.google.net:443", this.props);
+        this.isFalse("https://google.net:443", this.props);
+        return this.isFalse("http://google.com", this.props);
+      });
 
-      it "matches", ->
-        @isTrue("https://staging.google.com:443", @props)
-        @isTrue("https://google.com:443", @props)
-        @isTrue("https://foo.google.com:443", @props)
-        @isTrue("https://foo.bar.google.com:443", @props)
+      return it("matches", function() {
+        this.isTrue("https://staging.google.com:443", this.props);
+        this.isTrue("https://google.com:443", this.props);
+        this.isTrue("https://foo.google.com:443", this.props);
+        return this.isTrue("https://foo.bar.google.com:443", this.props);
+      });
+    });
 
-    describe "public suffix", ->
-      beforeEach ->
-        @props = cors.parseUrlIntoDomainTldPort("https://example.gitlab.io")
+    describe("public suffix", function() {
+      beforeEach(function() {
+        return this.props = cors.parseUrlIntoDomainTldPort("https://example.gitlab.io");
+      });
 
-      it "does not match", ->
-        @isFalse("http://example.gitlab.io", @props)
-        @isFalse("https://foo.gitlab.io:443", @props)
+      it("does not match", function() {
+        this.isFalse("http://example.gitlab.io", this.props);
+        return this.isFalse("https://foo.gitlab.io:443", this.props);
+      });
 
-      it "matches", ->
-        @isTrue("https://example.gitlab.io:443", @props)
-        @isTrue("https://foo.example.gitlab.io:443", @props)
+      return it("matches", function() {
+        this.isTrue("https://example.gitlab.io:443", this.props);
+        return this.isTrue("https://foo.example.gitlab.io:443", this.props);
+      });
+    });
 
-    describe "localhost", ->
-      beforeEach ->
-        @props = cors.parseUrlIntoDomainTldPort("http://localhost:4200")
+    describe("localhost", function() {
+      beforeEach(function() {
+        return this.props = cors.parseUrlIntoDomainTldPort("http://localhost:4200");
+      });
 
-      it "does not match", ->
-        @isFalse("http://localhost:4201", @props)
-        @isFalse("http://localhoss:4200", @props)
+      it("does not match", function() {
+        this.isFalse("http://localhost:4201", this.props);
+        return this.isFalse("http://localhoss:4200", this.props);
+      });
 
-      it "matches", ->
-        @isTrue("http://localhost:4200", @props)
+      return it("matches", function() {
+        return this.isTrue("http://localhost:4200", this.props);
+      });
+    });
 
-    describe "app.localhost", ->
-      beforeEach ->
-        @props = cors.parseUrlIntoDomainTldPort("http://app.localhost:4200")
+    describe("app.localhost", function() {
+      beforeEach(function() {
+        return this.props = cors.parseUrlIntoDomainTldPort("http://app.localhost:4200");
+      });
 
-      it "does not match", ->
-        @isFalse("http://app.localhost:4201", @props)
-        @isFalse("http://app.localhoss:4200", @props)
+      it("does not match", function() {
+        this.isFalse("http://app.localhost:4201", this.props);
+        return this.isFalse("http://app.localhoss:4200", this.props);
+      });
 
-      it "matches", ->
-        @isTrue("http://app.localhost:4200", @props)
-        @isTrue("http://name.app.localhost:4200", @props)
+      return it("matches", function() {
+        this.isTrue("http://app.localhost:4200", this.props);
+        return this.isTrue("http://name.app.localhost:4200", this.props);
+      });
+    });
 
-    describe "local", ->
-      beforeEach ->
-        @props = cors.parseUrlIntoDomainTldPort("http://brian.dev.local")
+    describe("local", function() {
+      beforeEach(function() {
+        return this.props = cors.parseUrlIntoDomainTldPort("http://brian.dev.local");
+      });
 
-      it "does not match", ->
-        @isFalse("https://brian.dev.local:443", @props)
-        @isFalse("https://brian.dev.local", @props)
-        @isFalse("http://brian.dev2.local:81", @props)
+      it("does not match", function() {
+        this.isFalse("https://brian.dev.local:443", this.props);
+        this.isFalse("https://brian.dev.local", this.props);
+        return this.isFalse("http://brian.dev2.local:81", this.props);
+      });
 
-      it "matches", ->
-        @isTrue("http://jennifer.dev.local:80", @props)
-        @isTrue("http://jennifer.dev.local", @props)
+      return it("matches", function() {
+        this.isTrue("http://jennifer.dev.local:80", this.props);
+        return this.isTrue("http://jennifer.dev.local", this.props);
+      });
+    });
 
-    describe "ip address", ->
-      beforeEach ->
-        @props = cors.parseUrlIntoDomainTldPort("http://192.168.5.10")
+    return describe("ip address", function() {
+      beforeEach(function() {
+        return this.props = cors.parseUrlIntoDomainTldPort("http://192.168.5.10");
+      });
 
-      it "does not match", ->
-        @isFalse("http://192.168.5.10:443", @props)
-        @isFalse("https://192.168.5.10", @props)
-        @isFalse("http://193.168.5.10", @props)
-        @isFalse("http://193.168.5.10:80", @props)
+      it("does not match", function() {
+        this.isFalse("http://192.168.5.10:443", this.props);
+        this.isFalse("https://192.168.5.10", this.props);
+        this.isFalse("http://193.168.5.10", this.props);
+        return this.isFalse("http://193.168.5.10:80", this.props);
+      });
 
-      it "matches", ->
-        @isTrue("http://192.168.5.10", @props)
-        @isTrue("http://192.168.5.10:80", @props)
+      return it("matches", function() {
+        this.isTrue("http://192.168.5.10", this.props);
+        return this.isTrue("http://192.168.5.10:80", this.props);
+      });
+    });
+  });
 
-  context ".urlMatchesOriginProtectionSpace", ->
-    isMatch = (urlStr, origin) ->
-      expect(urlStr, "the url: '#{urlStr}' did not match origin protection space: '#{origin}'").to.satisfy ->
-        cors.urlMatchesOriginProtectionSpace(urlStr, origin)
+  return context(".urlMatchesOriginProtectionSpace", function() {
+    const isMatch = (urlStr, origin) =>
+      expect(urlStr, `the url: '${urlStr}' did not match origin protection space: '${origin}'`).to.satisfy(() => cors.urlMatchesOriginProtectionSpace(urlStr, origin))
+    ;
 
-    isNotMatch = (urlStr, origin) ->
-      expect(urlStr, "the url: '#{urlStr}' matched origin protection space: '#{origin}'")
-      .not.to.satisfy ->
-        cors.urlMatchesOriginProtectionSpace(urlStr, origin)
+    const isNotMatch = (urlStr, origin) =>
+      expect(urlStr, `the url: '${urlStr}' matched origin protection space: '${origin}'`)
+      .not.to.satisfy(() => cors.urlMatchesOriginProtectionSpace(urlStr, origin))
+    ;
 
-    it "ports", ->
-      isMatch("http://example.com/", "http://example.com:80")
-      isMatch("http://example.com:80/", "http://example.com")
-      isMatch("http://example.com:80/", "http://example.com:80")
-      isMatch("https://example.com:443/", "https://example.com:443")
-      isMatch("https://example.com:443/", "https://example.com")
-      isMatch("https://example.com/", "https://example.com:443")
+    it("ports", function() {
+      isMatch("http://example.com/", "http://example.com:80");
+      isMatch("http://example.com:80/", "http://example.com");
+      isMatch("http://example.com:80/", "http://example.com:80");
+      isMatch("https://example.com:443/", "https://example.com:443");
+      isMatch("https://example.com:443/", "https://example.com");
+      isMatch("https://example.com/", "https://example.com:443");
 
-      isNotMatch("https://example.com:1234/", "https://example.com")
-      isNotMatch("https://example.com:1234/", "https://example.com:443")
+      isNotMatch("https://example.com:1234/", "https://example.com");
+      return isNotMatch("https://example.com:1234/", "https://example.com:443");
+    });
 
-    it "schemes", ->
-      isNotMatch("http://example.com/", "https://example.com")
-      isNotMatch("https://example.com/", "http://example.com")
-      isNotMatch("http://example.com/", "ftp://example.com")
-      isNotMatch("http://example.com/", "file://example.com")
+    it("schemes", function() {
+      isNotMatch("http://example.com/", "https://example.com");
+      isNotMatch("https://example.com/", "http://example.com");
+      isNotMatch("http://example.com/", "ftp://example.com");
+      return isNotMatch("http://example.com/", "file://example.com");
+    });
 
-    it "does not factor in path or search", ->
-      isMatch("http://example.com/foo", "http://example.com")
-      isMatch("http://example.com/foo/bar", "http://example.com")
-      isMatch("http://example.com/?foo=bar", "http://example.com")
-      isMatch("http://example.com/foo?bar=baz", "http://example.com")
+    it("does not factor in path or search", function() {
+      isMatch("http://example.com/foo", "http://example.com");
+      isMatch("http://example.com/foo/bar", "http://example.com");
+      isMatch("http://example.com/?foo=bar", "http://example.com");
+      return isMatch("http://example.com/foo?bar=baz", "http://example.com");
+    });
 
-    it "subdomains", ->
-      isMatch("http://example.com/", "http://example.com")
-      isMatch("http://www.example.com/", "http://www.example.com")
-      isMatch("http://foo.bar.example.com/", "http://foo.bar.example.com")
+    return it("subdomains", function() {
+      isMatch("http://example.com/", "http://example.com");
+      isMatch("http://www.example.com/", "http://www.example.com");
+      isMatch("http://foo.bar.example.com/", "http://foo.bar.example.com");
 
-      isNotMatch("http://www.example.com/", "http://example.com")
-      isNotMatch("http://foo.example.com/", "http://bar.example.com")
-      isNotMatch("http://foo.example.com/", "http://foo.bar.example.com")
+      isNotMatch("http://www.example.com/", "http://example.com");
+      isNotMatch("http://foo.example.com/", "http://bar.example.com");
+      return isNotMatch("http://foo.example.com/", "http://foo.bar.example.com");
+    });
+  });
+});
--- packages/server/test/unit/security_spec.js
+++ packages/server/test/unit/security_spec.js
@@ -1,13 +1,18 @@
-require("../spec_helper")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+require("../spec_helper");
 
-_ = require("lodash")
-rp = require("request-promise")
-concat = require("concat-stream")
-fs = require("#{root}lib/util/fs")
-security = require("#{root}lib/util/security")
-Fixtures = require("#{root}test/support/helpers/fixtures")
+const _ = require("lodash");
+const rp = require("request-promise");
+const concat = require("concat-stream");
+const fs = require(`${root}lib/util/fs`);
+const security = require(`${root}lib/util/security`);
+const Fixtures = require(`${root}test/support/helpers/fixtures`);
 
-original = """
+const original = `\
 <html>
   <body>
     top1
@@ -69,10 +74,10 @@ original = """
       if (top==self) return
     </script>
   </body>
-</html>
-"""
+</html>\
+`;
 
-expected = """
+const expected = `\
 <html>
   <body>
     top1
@@ -134,144 +139,160 @@ expected = """
       if (self==self) return
     </script>
   </body>
-</html>
-"""
+</html>\
+`;
 
-describe "lib/util/security", ->
-  context ".strip", ->
-    it "replaces obstructive code", ->
-      expect(security.strip(original)).to.eq(expected)
+describe("lib/util/security", function() {
+  context(".strip", function() {
+    it("replaces obstructive code", () => expect(security.strip(original)).to.eq(expected));
 
-    it "replaces jira window getter", ->
-      jira = """
-        for (; !function (n) {
-          return n === n.parent
-        }(n)
-      """
+    it("replaces jira window getter", function() {
+      const jira = `\
+for (; !function (n) {
+  return n === n.parent
+}(n)\
+`;
 
-      jira2 = """
-        function(n){for(;!function(l){return l===l.parent}(l)&&function(l){try{if(void 0==l.location.href)return!1}catch(l){return!1}return!0}(l.parent);)l=l.parent;return l}
-      """
+      const jira2 = `\
+function(n){for(;!function(l){return l===l.parent}(l)&&function(l){try{if(void 0==l.location.href)return!1}catch(l){return!1}return!0}(l.parent);)l=l.parent;return l}\
+`;
 
-      expect(security.strip(jira)).to.eq("""
-        for (; !function (n) {
-          return n === n.parent || n.parent.__Cypress__
-        }(n)
-      """)
+      expect(security.strip(jira)).to.eq(`\
+for (; !function (n) {
+  return n === n.parent || n.parent.__Cypress__
+}(n)\
+`);
 
-      expect(security.strip(jira2)).to.eq("""
-        function(n){for(;!function(l){return l===l.parent || l.parent.__Cypress__}(l)&&function(l){try{if(void 0==l.location.href)return!1}catch(l){return!1}return!0}(l.parent);)l=l.parent;return l}
-      """)
+      return expect(security.strip(jira2)).to.eq(`\
+function(n){for(;!function(l){return l===l.parent || l.parent.__Cypress__}(l)&&function(l){try{if(void 0==l.location.href)return!1}catch(l){return!1}return!0}(l.parent);)l=l.parent;return l}\
+`);
+    });
 
-    describe "libs", ->
-      ## go out and download all of these libs and ensure
-      ## that we can run them through the security strip
-      ## and that they are not modified!
+    return describe("libs", function() {
+      //# go out and download all of these libs and ensure
+      //# that we can run them through the security strip
+      //# and that they are not modified!
 
-      cdnUrl = "https://cdnjs.cloudflare.com/ajax/libs"
+      const cdnUrl = "https://cdnjs.cloudflare.com/ajax/libs";
 
-      needsDash = ["backbone", "underscore"]
+      const needsDash = ["backbone", "underscore"];
 
-      libs = {
-        jquery: "#{cdnUrl}/jquery/3.3.1/jquery.js"
-        jqueryui: "#{cdnUrl}/jqueryui/1.12.1/jquery-ui.js"
-        angular: "#{cdnUrl}/angular.js/1.6.5/angular.js"
-        bootstrap: "#{cdnUrl}/twitter-bootstrap/4.0.0/js/bootstrap.js"
-        fontawesome: "#{cdnUrl}/font-awesome/4.7.0/css/font-awesome.css"
-        moment: "#{cdnUrl}/moment.js/2.20.1/moment.js"
-        lodash: "#{cdnUrl}/lodash.js/4.17.5/lodash.js"
-        vue: "#{cdnUrl}/vue/2.5.13/vue.js"
-        backbone: "#{cdnUrl}/backbone.js/1.3.3/backbone.js"
-        cycle: "#{cdnUrl}/cyclejs-core/7.0.0/cycle.js"
-        d3: "#{cdnUrl}/d3/4.13.0/d3.js"
-        normalize: "#{cdnUrl}/normalize/8.0.0/normalize.css"
-        underscore: "#{cdnUrl}/underscore.js/1.8.3/underscore.js"
-        foundation: "#{cdnUrl}/foundation/6.4.3/js/foundation.js"
-        require: "#{cdnUrl}/require.js/2.3.5/require.js"
-        rxjs: "#{cdnUrl}/rxjs/5.5.6/Rx.js"
-        bluebird: "#{cdnUrl}/bluebird/3.5.1/bluebird.js"
-      }
+      let libs = {
+        jquery: `${cdnUrl}/jquery/3.3.1/jquery.js`,
+        jqueryui: `${cdnUrl}/jqueryui/1.12.1/jquery-ui.js`,
+        angular: `${cdnUrl}/angular.js/1.6.5/angular.js`,
+        bootstrap: `${cdnUrl}/twitter-bootstrap/4.0.0/js/bootstrap.js`,
+        fontawesome: `${cdnUrl}/font-awesome/4.7.0/css/font-awesome.css`,
+        moment: `${cdnUrl}/moment.js/2.20.1/moment.js`,
+        lodash: `${cdnUrl}/lodash.js/4.17.5/lodash.js`,
+        vue: `${cdnUrl}/vue/2.5.13/vue.js`,
+        backbone: `${cdnUrl}/backbone.js/1.3.3/backbone.js`,
+        cycle: `${cdnUrl}/cyclejs-core/7.0.0/cycle.js`,
+        d3: `${cdnUrl}/d3/4.13.0/d3.js`,
+        normalize: `${cdnUrl}/normalize/8.0.0/normalize.css`,
+        underscore: `${cdnUrl}/underscore.js/1.8.3/underscore.js`,
+        foundation: `${cdnUrl}/foundation/6.4.3/js/foundation.js`,
+        require: `${cdnUrl}/require.js/2.3.5/require.js`,
+        rxjs: `${cdnUrl}/rxjs/5.5.6/Rx.js`,
+        bluebird: `${cdnUrl}/bluebird/3.5.1/bluebird.js`
+      };
 
       libs = _
       .chain(libs)
       .clone()
-      .reduce (memo, url, lib) ->
-        memo[lib] = url
+      .reduce(function(memo, url, lib) {
+        memo[lib] = url;
         memo[lib + "Min"] = url
           .replace(/js$/, "min.js")
-          .replace(/css$/, "min.css")
+          .replace(/css$/, "min.css");
 
-        if lib in needsDash
-          memo[lib + "Min"] = url.replace("min", "-min")
+        if (needsDash.includes(lib)) {
+          memo[lib + "Min"] = url.replace("min", "-min");
+        }
 
-        memo
-      , {}
+        return memo;
+      }
+      , {})
       .extend({
-        knockoutDebug: "#{cdnUrl}/knockout/3.4.2/knockout-debug.js"
-        knockoutMin: "#{cdnUrl}/knockout/3.4.2/knockout-min.js"
-        emberMin: "#{cdnUrl}/ember.js/2.18.2/ember.min.js"
-        emberProd: "#{cdnUrl}/ember.js/2.18.2/ember.prod.js"
-        reactDev: "#{cdnUrl}/react/16.2.0/umd/react.development.js"
-        reactProd: "#{cdnUrl}/react/16.2.0/umd/react.production.min.js"
-        vendorBundle: "https://s3.amazonaws.com/internal-test-runner-assets.cypress.io/vendor.bundle.js"
+        knockoutDebug: `${cdnUrl}/knockout/3.4.2/knockout-debug.js`,
+        knockoutMin: `${cdnUrl}/knockout/3.4.2/knockout-min.js`,
+        emberMin: `${cdnUrl}/ember.js/2.18.2/ember.min.js`,
+        emberProd: `${cdnUrl}/ember.js/2.18.2/ember.prod.js`,
+        reactDev: `${cdnUrl}/react/16.2.0/umd/react.development.js`,
+        reactProd: `${cdnUrl}/react/16.2.0/umd/react.production.min.js`,
+        vendorBundle: "https://s3.amazonaws.com/internal-test-runner-assets.cypress.io/vendor.bundle.js",
         hugeApp: "https://s3.amazonaws.com/internal-test-runner-assets.cypress.io/huge_app.js"
       })
-      .value()
+      .value();
 
-      _.each libs, (url, lib) ->
-        it "does not alter code from: '#{lib}'", ->
-          nock.enableNetConnect()
+      return _.each(libs, (url, lib) =>
+        it(`does not alter code from: '${lib}'`, function() {
+          nock.enableNetConnect();
 
-          @timeout(10000)
+          this.timeout(10000);
 
-          pathToLib = Fixtures.path("server/libs/#{lib}")
+          const pathToLib = Fixtures.path(`server/libs/${lib}`);
 
-          downloadFile = ->
+          const downloadFile = () =>
             rp(url)
-            .then (resp) ->
+            .then(resp =>
               fs
               .outputFileAsync(pathToLib, resp)
               .return(resp)
-          fs
+            )
+          ;
+          return fs
           .readFileAsync(pathToLib, "utf8")
           .catch(downloadFile)
-          .then (libCode) ->
-            stripped = security.strip(libCode)
-            ## nothing should have changed!
+          .then(function(libCode) {
+            let stripped = security.strip(libCode);
+            //# nothing should have changed!
 
-            ## TODO: this is currently failing but we're
-            ## going to accept this for now and make this
-            ## test pass, but need to refactor to using
-            ## inline expressions and change the strategy
-            ## for removing obstructive code
-            if lib is "hugeApp"
+            //# TODO: this is currently failing but we're
+            //# going to accept this for now and make this
+            //# test pass, but need to refactor to using
+            //# inline expressions and change the strategy
+            //# for removing obstructive code
+            if (lib === "hugeApp") {
               stripped = stripped.replace(
                 "window.self !== window.self",
                 "window.self !== window.top"
-              )
+              );
+            }
 
-            try
-              expect(stripped).to.eq(libCode)
-            catch err
-              fs.outputFileSync(pathToLib + "Diff", stripped)
-              throw new Error("code from '#{lib}' was different")
+            try {
+              return expect(stripped).to.eq(libCode);
+            } catch (err) {
+              fs.outputFileSync(pathToLib + "Diff", stripped);
+              throw new Error(`code from '${lib}' was different`);
+            }
+          });
+        })
+      );
+    });
+  });
 
-  context ".stripStream", ->
-    it "replaces obstructive code", (done) ->
-      haystacks = original.split("\n")
+  return context(".stripStream", () =>
+    it("replaces obstructive code", function(done) {
+      const haystacks = original.split("\n");
 
-      replacer = security.stripStream()
+      const replacer = security.stripStream();
 
-      replacer.pipe concat {encoding: "string"}, (str) ->
-        str = str.trim()
+      replacer.pipe(concat({encoding: "string"}, function(str) {
+        str = str.trim();
 
-        try
-          expect(str).to.eq(expected)
-          done()
-        catch err
-          done(err)
+        try {
+          expect(str).to.eq(expected);
+          return done();
+        } catch (err) {
+          return done(err);
+        }
+      })
+      );
 
-      haystacks.forEach (haystack) ->
-        replacer.write(haystack + "\n")
+      haystacks.forEach(haystack => replacer.write(haystack + "\n"));
 
-      replacer.end()
+      return replacer.end();
+    })
+  );
+});
