0a4aefe45e9e882a866c4721b45d837f856815d8
Chris Breiding <chrisbreiding@gmail.com>
2019-09-18 17:39:27 -0400

    wip

---
+++ packages/driver/test/cypress/integration/cypress/network_utils_spec.js
@@ -0,0 +1,20 @@
+const $networkUtils = require('../../../../src/cypress/network_utils')
+
+describe('src/cypress/network_utils', () => {
+  context('#runSpecs', () => {
+    let win
+
+    beforeEach(() => {
+      win = {
+      }
+    })
+
+    it('fetches the resource via XHR', () => {
+
+    })
+
+    it('resolves the promise when it loads')
+
+    it('rejects the promise when it errors')
+  })
+})
