8488906a6c8ab34915def011d28afd2913100378
ryan.snyder <ryan.snyder@screenscape.com>
2019-09-20 13:54:02 -0300

    Didn't realize it ran this test as well, whoops

--- packages/driver/test/cypress/integration/issues/2953_spec.js
+++ packages/driver/test/cypress/integration/issues/2953_spec.js
@@ -2,12 +2,20 @@
 describe('Issue 2953: Improper arguments for cy.get()', () => {
 
   it('calls cy get with 2 strings', () => {
+    cy.on('fail', (err) => {
+      expect(err.message).to.include('expected bar to be of type object, instead got string')
+    })
+
     // TypeError: Cannot read property 'set' of undefined
     cy.get('input', 'bar')
   })
 
   it('calls cy get with string and number', () => {
     // TypeError: Cannot read property 'set' of undefined
+    cy.on('fail', (err) => {
+      expect(err.message).to.include('expected 42 to be of type object, instead got number')
+    })
+
     cy.get('input', 42)
   })
 })
