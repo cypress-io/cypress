b92857917e992429cf2cd98ff0057819399fe9a9
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-09-30 12:02:46 -0400

    test refac

--- packages/driver/test/cypress/integration/e2e/domSnapshots.spec.js
+++ packages/driver/test/cypress/integration/e2e/domSnapshots.spec.js
@@ -1,6 +1,24 @@
 const { withMutableReporterState, findReactInstance, getCommandLogWithText } = require('../../support/utils')
 const { $ } = Cypress
 
+describe('rect highlight', () => {
+  beforeEach(() => {
+    cy.visit('/fixtures/dom.html')
+  })
+
+  it('highlight elements are properly positioned', () => {
+    getAndPin('button:first')
+
+    expectCorrectHighlightPositions()
+  })
+
+  it('highlight elements properly positioned for content-box', () => {
+    getAndPin('button.content-box:first')
+
+    expectCorrectHighlightPositions()
+  })
+})
+
 const getAndPin = (sel) => {
   cy.get(sel)
 
@@ -19,41 +37,18 @@ const getAndPin = (sel) => {
   })
 }
 
-describe('rect highlight', () => {
-  it('highlight elements are properly positioned', () => {
-    cy.visit('/fixtures/dom.html')
+const expectCorrectHighlightPositions = () => {
+  return cy.wrap(null, { timeout: 400 }).should(() => {
+    const dims = {
+      content: cy.$$('div[data-layer=Content]')[0].getBoundingClientRect(),
+      padding: cy.$$('div[data-layer=Padding]')[0].getBoundingClientRect(),
+      border: cy.$$('div[data-layer=Border]')[0].getBoundingClientRect(),
+    }
 
-    getAndPin('button:first')
-
-    cy.wrap(null, { timeout: 400 }).should(() => {
-      const dims = {
-        content: cy.$$('div[data-layer=Content]')[0].getBoundingClientRect(),
-        padding: cy.$$('div[data-layer=Padding]')[0].getBoundingClientRect(),
-        border: cy.$$('div[data-layer=Border]')[0].getBoundingClientRect(),
-      }
-
-      expectToBeInside(dims.content, dims.padding, 'content to be inside padding')
-      expectToBeInside(dims.padding, dims.border, 'padding to be inside border')
-    })
+    expectToBeInside(dims.content, dims.padding, 'content to be inside padding')
+    expectToBeInside(dims.padding, dims.border, 'padding to be inside border')
   })
-
-  it('highlight elements properly positioned for content-box', () => {
-    cy.visit('/fixtures/dom.html')
-
-    getAndPin('button.content-box:first')
-
-    cy.wrap(null, { timeout: 400 }).should(() => {
-      const dims = {
-        content: cy.$$('div[data-layer=Content]')[0].getBoundingClientRect(),
-        padding: cy.$$('div[data-layer=Padding]')[0].getBoundingClientRect(),
-        border: cy.$$('div[data-layer=Border]')[0].getBoundingClientRect(),
-      }
-
-      expectToBeInside(dims.content, dims.padding, 'content to be inside padding')
-      expectToBeInside(dims.padding, dims.border, 'padding to be inside border')
-    })
-  })
-})
+}
 
 const expectToBeInside = (rectInner, rectOuter, mes = 'rect to be inside rect') => {
   try {
