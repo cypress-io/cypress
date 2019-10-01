e00c87a91934ec064c921624f5967ef5ccf8377c
Zach Bloomquist <github@chary.us>
2019-10-01 14:00:02 -0400

    use spread operator for args

--- packages/server/lib/util/suppress_unauthorized_warning.ts
+++ packages/server/lib/util/suppress_unauthorized_warning.ts
@@ -16,7 +16,7 @@ export function suppress () {
 
   const originalEmitWarning = process.emitWarning
 
-  process.emitWarning = (warning, options) => {
+  process.emitWarning = (warning, ...args) => {
     if (_.isString(warning) && _.includes(warning, 'NODE_TLS_REJECT_UNAUTHORIZED')) {
       // node will only emit the warning once
       // https://github.com/nodejs/node/blob/82f89ec8c1554964f5029fab1cf0f4fad1fa55a8/lib/_tls_wrap.js#L1378-L1384
@@ -25,6 +25,6 @@ export function suppress () {
       return
     }
 
-    return originalEmitWarning.call(process, warning, options)
+    return originalEmitWarning.call(process, warning, ...args)
   }
 }
