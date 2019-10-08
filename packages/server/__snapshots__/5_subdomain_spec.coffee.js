7a66da41cac7453348cf0977c4aaf1d652d2c5c6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-10-08 14:33:26 -0400

    update snapshot for 5_subdomain_spec

--- packages/server/__snapshots__/5_subdomain_spec.coffee.js
+++ packages/server/__snapshots__/5_subdomain_spec.coffee.js
@@ -118,6 +118,12 @@ exports['e2e subdomain passes in chrome 1'] = `
   └─────────────────────────────────────┘
 
 
+  (Video)
+
+  - Started processing:   Compressing to 32 CRF
+  - Finished processing:  /foo/bar/.projects/e2e/cypress/videos/abc123.mp4 (X seconds)
+
+
 ====================================================================================================
 
   (Run Finished)
