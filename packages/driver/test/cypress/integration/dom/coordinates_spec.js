db1ab58b4ea09beafed57ea3c9c9503a440093f4
decaffeinate <github@chary.us>
2019-10-11 11:23:43 -0400

    decaffeinate: Convert coordinates_spec.coffee and 2 other files to JS

--- packages/driver/test/cypress/integration/dom/coordinates_spec.js
+++ packages/driver/test/cypress/integration/dom/coordinates_spec.js
@@ -1,189 +1,227 @@
-$ = Cypress.$.bind(Cypress)
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const $ = Cypress.$.bind(Cypress);
 
-describe "src/dom/coordinates", ->
-  before ->
+describe("src/dom/coordinates", function() {
+  before(() =>
     cy
       .visit("/fixtures/generic.html")
-      .then (win) ->
-        @body = win.document.body.outerHTML
+      .then(function(win) {
+        return this.body = win.document.body.outerHTML;
+    })
+  );
 
-  beforeEach ->
-    @doc = cy.state("document")
+  beforeEach(function() {
+    this.doc = cy.state("document");
 
-    $(@doc.body).empty().html(@body)
+    $(this.doc.body).empty().html(this.body);
 
-    @$button = $("<button style='position: absolute; top: 25px; left: 50px; width: 100px; line-height: 50px; padding: 10px; margin: 10px; border: 10px solid black'>foo</button>")
-    .appendTo(cy.$$("body"))
+    return this.$button = $("<button style='position: absolute; top: 25px; left: 50px; width: 100px; line-height: 50px; padding: 10px; margin: 10px; border: 10px solid black'>foo</button>")
+    .appendTo(cy.$$("body"));
+  });
 
-  context ".getElementPositioning", ->
-    ## this is necessary so that document.elementFromPoint
-    ## does not miss elements
-    it "returns the leftCenter and topCenter normalized", ->
-      win = Cypress.dom.getWindowByElement(@$button.get(0))
+  context(".getElementPositioning", () =>
+    //# this is necessary so that document.elementFromPoint
+    //# does not miss elements
+    it("returns the leftCenter and topCenter normalized", function() {
+      const win = Cypress.dom.getWindowByElement(this.$button.get(0));
 
-      pageYOffset = Object.getOwnPropertyDescriptor(win, "pageYOffset")
-      pageXOffset = Object.getOwnPropertyDescriptor(win, "pageXOffset")
+      const pageYOffset = Object.getOwnPropertyDescriptor(win, "pageYOffset");
+      const pageXOffset = Object.getOwnPropertyDescriptor(win, "pageXOffset");
 
       Object.defineProperty(win, "pageYOffset", {
         value: 10
-      })
+      });
 
       Object.defineProperty(win, "pageXOffset", {
         value: 20
-      })
+      });
 
-      cy.stub(@$button.get(0), "getClientRects").returns([{
-        top: 100.9
-        left: 60.9
-        width: 50
+      cy.stub(this.$button.get(0), "getClientRects").returns([{
+        top: 100.9,
+        left: 60.9,
+        width: 50,
         height: 40
-      }])
+      }]);
 
-      { fromViewport, fromWindow } = Cypress.dom.getElementPositioning(@$button)
+      const { fromViewport, fromWindow } = Cypress.dom.getElementPositioning(this.$button);
 
-      expect(fromViewport.topCenter).to.eq(120)
-      expect(fromViewport.leftCenter).to.eq(85)
+      expect(fromViewport.topCenter).to.eq(120);
+      expect(fromViewport.leftCenter).to.eq(85);
 
-      expect(fromWindow.topCenter).to.eq(130)
-      expect(fromWindow.leftCenter).to.eq(105)
+      expect(fromWindow.topCenter).to.eq(130);
+      expect(fromWindow.leftCenter).to.eq(105);
 
-      Object.defineProperty(win, "pageYOffset", pageYOffset)
-      Object.defineProperty(win, "pageXOffset", pageXOffset)
+      Object.defineProperty(win, "pageYOffset", pageYOffset);
+      return Object.defineProperty(win, "pageXOffset", pageXOffset);
+    })
+  );
 
-  context ".getCoordsByPosition", ->
-    it "rounds down x and y values to object", ->
-      expect(Cypress.dom.getCoordsByPosition(5.96, 7.68)).to.deep.eq({x: 5, y: 7})
+  context(".getCoordsByPosition", () =>
+    it("rounds down x and y values to object", () => expect(Cypress.dom.getCoordsByPosition(5.96, 7.68)).to.deep.eq({x: 5, y: 7}))
+  );
 
-  context ".getElementAtPointFromViewport", ->
-    it "returns same element based on x/y coords", ->
-      expect(Cypress.dom.getElementAtPointFromViewport(@doc, 100, 60)).to.eq @$button.get(0)
+  context(".getElementAtPointFromViewport", function() {
+    it("returns same element based on x/y coords", function() {
+      return expect(Cypress.dom.getElementAtPointFromViewport(this.doc, 100, 60)).to.eq(this.$button.get(0));
+    });
 
-    it "does not return if element is hidden", ->
-      @$button.hide()
-      expect(Cypress.dom.getElementAtPointFromViewport(@doc, 100, 60)).not.to.eq @$button.get(0)
+    it("does not return if element is hidden", function() {
+      this.$button.hide();
+      return expect(Cypress.dom.getElementAtPointFromViewport(this.doc, 100, 60)).not.to.eq(this.$button.get(0));
+    });
 
-    it "returns null if no element was found", ->
-      expect(Cypress.dom.getElementAtPointFromViewport(@doc, 1e9, 1e9)).to.be.null
+    return it("returns null if no element was found", function() {
+      return expect(Cypress.dom.getElementAtPointFromViewport(this.doc, 1e9, 1e9)).to.be.null;
+    });
+  });
 
-  context ".getElementCoordinatesByPosition", ->
-    beforeEach ->
-      @fromWindowPos = (pos) =>
-        Cypress.dom.getElementCoordinatesByPosition(@$button, pos)
-        .fromWindow
+  context(".getElementCoordinatesByPosition", function() {
+    beforeEach(function() {
+      return this.fromWindowPos = pos => {
+        return Cypress.dom.getElementCoordinatesByPosition(this.$button, pos)
+        .fromWindow;
+      };
+    });
 
-    describe "topLeft", ->
-      it "returns top left x/y including padding + border", ->
-        obj = @fromWindowPos("topLeft")
+    describe("topLeft", () =>
+      it("returns top left x/y including padding + border", function() {
+        const obj = this.fromWindowPos("topLeft");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(60)
-        expect(obj.y).to.eq(35)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(60);
+        return expect(obj.y).to.eq(35);
+      })
+    );
 
-    describe "top", ->
-      it "returns top center x/y including padding + border", ->
-        obj = @fromWindowPos("top")
+    describe("top", () =>
+      it("returns top center x/y including padding + border", function() {
+        const obj = this.fromWindowPos("top");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(110)
-        expect(obj.y).to.eq(35)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(110);
+        return expect(obj.y).to.eq(35);
+      })
+    );
 
-    describe "topRight", ->
-      it "returns top right x/y including padding + border", ->
-        obj = @fromWindowPos("topRight")
+    describe("topRight", () =>
+      it("returns top right x/y including padding + border", function() {
+        const obj = this.fromWindowPos("topRight");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(159)
-        expect(obj.y).to.eq(35)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(159);
+        return expect(obj.y).to.eq(35);
+      })
+    );
 
-    describe "left", ->
-      it "returns center left x/y including padding + border", ->
-        obj = @fromWindowPos("left")
+    describe("left", () =>
+      it("returns center left x/y including padding + border", function() {
+        const obj = this.fromWindowPos("left");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(60)
-        expect(obj.y).to.eq(80)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(60);
+        return expect(obj.y).to.eq(80);
+      })
+    );
 
-    describe "center", ->
-      it "returns center x/y including padding + border", ->
-        obj = @fromWindowPos()
+    describe("center", function() {
+      it("returns center x/y including padding + border", function() {
+        const obj = this.fromWindowPos();
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(110)
-        expect(obj.y).to.eq(80)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(110);
+        return expect(obj.y).to.eq(80);
+      });
 
-      it "returns width / height factoring in rotation transforms", ->
-        ## normally our outerWidth is 100 and outerHeight is 70
-        ## after we've been rotated these are reversed and our previous
-        ## calculation would be wrong. using getBoundingClientRect passes this test
-        @$button.css({transform: "rotate(90deg)"})
+      return it("returns width / height factoring in rotation transforms", function() {
+        //# normally our outerWidth is 100 and outerHeight is 70
+        //# after we've been rotated these are reversed and our previous
+        //# calculation would be wrong. using getBoundingClientRect passes this test
+        this.$button.css({transform: "rotate(90deg)"});
 
-        obj = @fromWindowPos()
+        const obj = this.fromWindowPos();
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(110)
-        expect(obj.y).to.eq(80)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(110);
+        return expect(obj.y).to.eq(80);
+      });
+    });
 
-    describe "right", ->
-      it "returns center right x/y including padding + border", ->
-        obj = @fromWindowPos("right")
+    describe("right", () =>
+      it("returns center right x/y including padding + border", function() {
+        const obj = this.fromWindowPos("right");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(159)
-        expect(obj.y).to.eq(80)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(159);
+        return expect(obj.y).to.eq(80);
+      })
+    );
 
-    describe "bottomLeft", ->
-      it "returns bottom left x/y including padding + border", ->
-        obj = @fromWindowPos("bottomLeft")
+    describe("bottomLeft", () =>
+      it("returns bottom left x/y including padding + border", function() {
+        const obj = this.fromWindowPos("bottomLeft");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(60)
-        expect(obj.y).to.eq(124)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(60);
+        return expect(obj.y).to.eq(124);
+      })
+    );
 
-    context "bottom", ->
-      it "returns bottom center x/y including padding + border", ->
-        obj = @fromWindowPos("bottom")
+    context("bottom", () =>
+      it("returns bottom center x/y including padding + border", function() {
+        const obj = this.fromWindowPos("bottom");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(110)
-        expect(obj.y).to.eq(124)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(110);
+        return expect(obj.y).to.eq(124);
+      })
+    );
 
-    context "bottomRight", ->
-      it "returns bottom right x/y including padding + border", ->
-        obj = @fromWindowPos("bottomRight")
+    return context("bottomRight", () =>
+      it("returns bottom right x/y including padding + border", function() {
+        const obj = this.fromWindowPos("bottomRight");
 
-        ## padding is added to the line-height but width includes the padding
-        expect(obj.x).to.eq(159)
-        expect(obj.y).to.eq(124)
+        //# padding is added to the line-height but width includes the padding
+        expect(obj.x).to.eq(159);
+        return expect(obj.y).to.eq(124);
+      })
+    );
+  });
 
-  context "span spanning multiple lines", ->
-    it 'gets first dom rect in multiline text', ->
-      $ '
-      <div style="width:150px; margin-top:100px">
-      this is some long text with a single <span id="multiple" style="color:darkblue"> span that spans lines</span> making it tricky to click
-      </div>
-      '
-      .appendTo $ 'body'
+  return context("span spanning multiple lines", () =>
+    it('gets first dom rect in multiline text', function() {
+      $(`\
+<div style="width:150px; margin-top:100px"> \
+this is some long text with a single <span id="multiple" style="color:darkblue"> span that spans lines</span> making it tricky to click \
+</div>\
+`).appendTo($('body'));
 
-      $el = cy.$$("#multiple")
-      el = $el[0]
+      const $el = cy.$$("#multiple");
+      const el = $el[0];
 
-      cy.stub(el, 'getClientRects', ->
+      cy.stub(el, 'getClientRects', () =>
         [
           {
-          top: 100
-          left: 100
-          width: 50
+          top: 100,
+          left: 100,
+          width: 50,
           height: 40
           },
           {
-          top: 200
-          left: 50
-          width: 10
+          top: 200,
+          left: 50,
+          width: 10,
           height: 10
           }
       ]
 
-      ).as 'getClientRects'
-      obj = Cypress.dom.getElementCoordinatesByPosition($el, 'center').fromViewport
+      ).as('getClientRects');
+      const obj = Cypress.dom.getElementCoordinatesByPosition($el, 'center').fromViewport;
 
-      expect({x: obj.x, y: obj.y}).to.deep.eq({x:125, y:120})
+      return expect({x: obj.x, y: obj.y}).to.deep.eq({x:125, y:120});
+    })
+  );
+});
