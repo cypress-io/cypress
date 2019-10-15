937fd9707b8f829730e5e45914c8018c47c44f26
Brendon Barreto <brendondominic@gmail.com>
2019-10-15 13:16:03 -0300

    Removing ipc parameter being passed to validateEvent

--- packages/server/lib/plugins/child/run_plugins.js
+++ packages/server/lib/plugins/child/run_plugins.js
@@ -29,7 +29,7 @@ const load = (ipc, config, pluginsFile) => {
   // we track the register calls and then send them all at once
   // to the parent process
   const register = (event, handler) => {
-    const { isValid, error } = validateEvent(ipc, event, handler)
+    const { isValid, error } = validateEvent(event, handler)
 
     if (!isValid) {
       sendError(ipc, error)
