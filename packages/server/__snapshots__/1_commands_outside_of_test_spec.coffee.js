f244f1e60fb9f9af2023ca700825e3bfcde52add
Jennifer Shehane <jennifer@cypress.io>
2019-10-17 11:07:51 -0400

    Fix merge conflict failure - didn't regenerate snapshot correctly last time.

--- packages/server/__snapshots__/1_commands_outside_of_test_spec.coffee.js
+++ packages/server/__snapshots__/1_commands_outside_of_test_spec.coffee.js
@@ -57,8 +57,89 @@ exports['e2e commands outside of test / passes on passing assertions'] = `
 
 `
 
-exports['e2e commands outside of test [chrome] fails on cy commands 1'] = `
+exports['e2e commands outside of test / fails on failing assertions'] = `
 
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (assertions_failing_outside_of_test_spec.coffee)                           │
+  │ Searched:   cypress/integration/assertions_failing_outside_of_test_spec.coffee                 │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  assertions_failing_outside_of_test_spec.coffee                                  (1 of 1)
+
+
+  1) An uncaught error was detected outside of a test
+
+  0 passing
+  1 failing
+
+  1)  An uncaught error was detected outside of a test:
+     expected true to be false
+
+This error originated from your test code, not from Cypress.
+
+When Cypress detects uncaught errors originating from your test code it will automatically fail the current test.
+
+Cypress could not associate this error to any specific test.
+
+We dynamically generated a new test to display this failure.
+  AssertionError: expected true to be false
+
+
+
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        1                                                                                │
+  │ Passing:      0                                                                                │
+  │ Failing:      1                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  1                                                                                │
+  │ Video:        true                                                                             │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     assertions_failing_outside_of_test_spec.coffee                                   │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+  (Screenshots)
+
+  -  /XXX/XXX/XXX/cypress/screenshots/assertions_failing_outside_of_test_spec.coffee/    (3360x2012)
+     An uncaught error was detected outside of a test (failed).png                                  
+
+
+  (Video)
+
+  -  Started processing:  Compressing to 32 CRF                                                     
+  -  Finished processing: /XXX/XXX/XXX/cypress/videos/assertions_failing_outside_of_t     (X second)
+                          est_spec.coffee.mp4                                                       
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+       Spec                                              Tests  Passing  Failing  Pending  Skipped  
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✖  assertions_failing_outside_of_test_      XX:XX        1        -        1        -        - │
+  │    spec.coffee                                                                                 │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    ✖  1 of 1 failed (100%)                     XX:XX        1        -        1        -        -  
+
+
+`
+
+exports['e2e commands outside of test / fails on cy commands'] = `
 
 ====================================================================================================
 
@@ -128,7 +209,7 @@ We dynamically generated a new test to display this failure.
 
   (Screenshots)
 
-  -  /XXX/XXX/XXX/cypress/screenshots/commands_outside_of_test_spec.coffee/An uncaugh     (1280x720)
+  -  /XXX/XXX/XXX/cypress/screenshots/commands_outside_of_test_spec.coffee/An uncaugh    (3360x2012)
      t error was detected outside of a test (failed).png                                            
 
 
@@ -151,13 +232,5 @@ We dynamically generated a new test to display this failure.
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
     ✖  1 of 1 failed (100%)                     XX:XX        1        -        1        -        -  
 
-
-`
-
-exports['e2e commands outside of test / fails on failing assertions'] = `
-
-`
-
-exports['e2e commands outside of test / fails on cy commands'] = `
 
 `
