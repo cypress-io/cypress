410673d5321b3d80eba114a44240fc6ed65990c8
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-10-17 11:45:07 -0400

    cleanup minor

--- packages/driver/src/dom/selection.ts
+++ packages/driver/src/dom/selection.ts
@@ -1,11 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import * as $document from './document'
 import * as $elements from './elements'
 
