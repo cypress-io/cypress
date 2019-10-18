033689f8bacaeb71df311be2034f76d1e776d7ea
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-10-18 12:46:46 -0400

    use typescript in driver (#5383)
    * upgrade eslint-plugin-dev to 5.0.0
    
    * more [lint fixes]
    
    * use typescript in driver
    
    * cleanup minor
    
    * remove unneeded file
    
    * mild refactor webpack configs
    
    * fix elements.ts
    
    * fix isTextLike signature change
    
    * fix isType check, fix type_spec
    
    * actually use webpack-preprocessor
    
    * fix isType domUtil
    
    * fix connectors_spec
    
    * cleanup connectors_spec
    
    * address feedback- cleanup, add comments, refac
    
    * answer: no, need index.js

--- packages/driver/src/cy/commands/actions/type.js
+++ packages/driver/src/cy/commands/actions/type.js
@@ -111,7 +111,7 @@ module.exports = function (Commands, Cypress, cy, state, config) {
       }
 
       const isBody = options.$el.is('body')
-      const isTextLike = $dom.isTextLike(options.$el)
+      const isTextLike = $dom.isTextLike(options.$el.get(0))
       const isDate = $dom.isType(options.$el, 'date')
       const isTime = $dom.isType(options.$el, 'time')
       const isMonth = $dom.isType(options.$el, 'month')
@@ -537,7 +537,7 @@ module.exports = function (Commands, Cypress, cy, state, config) {
 
         const node = $dom.stringify($el)
 
-        if (!$dom.isTextLike($el)) {
+        if (!$dom.isTextLike($el.get(0))) {
           const word = $utils.plural(subject, 'contains', 'is')
 
           $utils.throwErrByPath('clear.invalid_element', {
