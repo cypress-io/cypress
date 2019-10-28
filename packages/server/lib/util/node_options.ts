097000d69f821cbccc49f55d18bb17b6c3f8e8a2
Zach Bloomquist <github@chary.us>
2019-10-28 15:03:05 -0400

    Always reset NODE_OPTIONS, even if no ORIGINAL_
    Co-authored-by: Andrew Smith <andrew@andrew.codes>

--- packages/server/lib/util/node_options.ts
+++ packages/server/lib/util/node_options.ts
@@ -3,9 +3,22 @@ import debugModule from 'debug'
 
 const debug = debugModule('cypress:server:util:node_options')
 
-export function maybeFork () {
+/**
+ * If Cypress was not launched via CLI, it may be missing certain startup
+ * options. This checks that those startup options were applied; and if not,
+ * applies them, forks, and pipes stdio.
+ *
+ * @returns {boolean} was a fork created?
+ */
+export function maybeFork (): boolean {
   if ((process.env.NODE_OPTIONS || '').includes(`--max-http-header-size=${1024 ** 2}`)) {
     debug('NODE_OPTIONS check passed, not forking %o', { NODE_OPTIONS: process.env.NODE_OPTIONS })
+
+    return false
+  }
+
+  if (typeof require.main === 'undefined') {
+    debug('require.main is undefined, this should not happen normally, not forking')
 
     return false
   }
@@ -21,27 +34,41 @@ export function maybeFork () {
     ORIGINAL_NODE_OPTIONS: process.env.ORIGINAL_NODE_OPTIONS,
   })
 
-  cp.fork(require.main.filename, process.argv.slice(1), {
-    stdio: 'inherit',
-  })
+  cp.fork(
+    require.main.filename,
+    process.argv.slice(1),
+    // @ts-ignore
+    {
+      // This is actually the correct way to do it, despite TS - ['inherit', 'inherit', 'inherit'] won't work with cp.fork
+      // @see https://github.com/nodejs/node/blob/2f45ad8060e13d5ac912335096d21526f2f9602b/lib/child_process.js#L106-L115
+      stdio: 'inherit',
+    }
+  )
   .on('error', () => {})
 
   return true
 }
 
 /**
-  * Once the Electron process is launched, restore the user's original NODE_OPTIONS
-  * environment variables from before the CLI added extra NODE_OPTIONS.
-  *
-  * This way, any `node` processes launched by Cypress will retain the user's
-  * `NODE_OPTIONS` without unexpected modificiations that could cause issues with
-  * user code.
-  */
-export function reset () {
+ * Once the Electron process is launched, restore the user's original NODE_OPTIONS
+ * environment variables from before the CLI added extra NODE_OPTIONS.
+ *
+ * This way, any `node` processes launched by Cypress will retain the user's
+ * `NODE_OPTIONS` without unexpected modificiations that could cause issues with
+ * user code.
+ */
+export function reset (): void {
   // @ts-ignore
-  if (process.versions.electron && typeof process.env.ORIGINAL_NODE_OPTIONS === 'string') {
-    process.env.NODE_OPTIONS = process.env.ORIGINAL_NODE_OPTIONS
+  if (!process.versions.electron) {
+    debug('not restoring NODE_OPTIONS since not yet in Electron')
 
-    delete process.env.ORIGINAL_NODE_OPTIONS
+    return
   }
+
+  debug('restoring NODE_OPTIONS %o', {
+    NODE_OPTIONS: process.env.NODE_OPTIONS,
+    ORIGINAL_NODE_OPTIONS: process.env.ORIGINAL_NODE_OPTIONS,
+  })
+
+  process.env.NODE_OPTIONS = process.env.ORIGINAL_NODE_OPTIONS || ''
 }
