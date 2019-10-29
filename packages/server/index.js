bc22f794e898867ad7b356c4fb5f767d47329e76
Zach Bloomquist <github@chary.us>
2019-10-29 11:00:09 -0400

    Clean up based on Brian's feedback

--- packages/server/index.js
+++ packages/server/index.js
@@ -25,10 +25,17 @@ process.enablePromiseAPIs = process.env.CYPRESS_ENV !== 'production'
 
 require('./lib/util/suppress_unauthorized_warning').suppress()
 
-const nodeOptions = require('./lib/util/node_options')
+function launchOrFork () {
+  const nodeOptions = require('./lib/util/node_options')
 
-if (!nodeOptions.maybeFork()) {
-  nodeOptions.reset()
+  if (nodeOptions.needsOptions()) {
+    // https://github.com/cypress-io/cypress/pull/5492
+    return nodeOptions.forkWithCorrectOptions()
+  }
+
+  nodeOptions.restoreOriginalOptions()
 
   module.exports = require('./lib/cypress').start(process.argv)
 }
+
+launchOrFork()
--- packages/server/lib/util/node_options.ts
+++ packages/server/lib/util/node_options.ts
@@ -5,12 +5,11 @@ const debug = debugModule('cypress:server:util:node_options')
 
 /**
  * If Cypress was not launched via CLI, it may be missing certain startup
- * options. This checks that those startup options were applied; and if not,
- * applies them, forks, and pipes stdio.
+ * options. This checks that those startup options were applied.
  *
- * @returns {boolean} was a fork created?
+ * @returns {boolean} does Cypress have the expected NODE_OPTIONS?
  */
-export function maybeFork (): boolean {
+export function needsOptions (): boolean {
   if ((process.env.NODE_OPTIONS || '').includes(`--max-http-header-size=${1024 ** 2}`)) {
     debug('NODE_OPTIONS check passed, not forking %o', { NODE_OPTIONS: process.env.NODE_OPTIONS })
 
@@ -23,6 +22,14 @@ export function maybeFork (): boolean {
     return false
   }
 
+  return true
+}
+
+/**
+ * Fork the current process using the good NODE_OPTIONS and pipe stdio
+ * through the current process. On exit, copy the error code too.
+ */
+export function forkWithCorrectOptions (): void {
   // this should only happen when running from global mode, when the CLI couldn't set the NODE_OPTIONS
   const NODE_OPTIONS = `--max-http-header-size=${1024 * 1024}`
 
@@ -35,6 +42,7 @@ export function maybeFork (): boolean {
   })
 
   cp.fork(
+    // @ts-ignore
     require.main.filename,
     process.argv.slice(1),
     // @ts-ignore
@@ -48,8 +56,6 @@ export function maybeFork (): boolean {
   .on('exit', (code) => {
     process.exit(code)
   })
-
-  return true
 }
 
 /**
@@ -60,7 +66,7 @@ export function maybeFork (): boolean {
  * `NODE_OPTIONS` without unexpected modificiations that could cause issues with
  * user code.
  */
-export function reset (): void {
+export function restoreOriginalOptions (): void {
   // @ts-ignore
   if (!process.versions.electron) {
     debug('not restoring NODE_OPTIONS since not yet in Electron')
