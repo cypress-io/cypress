87ccd8e5e5cfcf571cb160976b1d8a430edc4085
Andrew Smith <andrew@andrew.codes>
2019-10-31 16:38:22 -0400

    properly resolve socket.io-client node_module
    We cannot reach directly into node_modules to grab the socket.io-client file. Furthermore, running it locally will require resolving from a different cwd, the repo root, as opposed to the package's dist directory.
    
    The existence of the file deterimes whether we are building the binary or running locally.

--- packages/socket/lib/socket.js
+++ packages/socket/lib/socket.js
@@ -5,8 +5,6 @@ const { version } = require('socket.io-client/package.json')
 const { client, circularParser } = require('./browser')
 const resolvePkg = require('resolve-pkg')
 
-const clientSource = resolvePkg('socket.io-client/dist/socket.io.js', { cwd: join(__dirname, '..', '..', '..') })
-
 module.exports = {
   server,
 
@@ -15,7 +13,13 @@ module.exports = {
   circularParser,
 
   getPathToClientSource () {
-    return clientSource
+    const clientSource = resolvePkg('socket.io-client/dist/socket.io.js', { cwd: join(__dirname, '..', '..', '..') })
+
+    if (fs.existsSync(clientSource)) {
+      return clientSource
+    }
+
+    return require.resolve('socket.io-client/dist/socket.io.js')
   },
 
   getClientVersion () {
