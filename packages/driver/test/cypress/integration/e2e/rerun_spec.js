a436137fc1c5ed3286a1261d674dfa08712ca992
KHeo <sainthkh@naver.com>
2019-10-31 11:22:50 +0900

    Fixed eslint errors and removed excessive returns.

--- packages/driver/test/cypress/integration/e2e/rerun_spec.js
+++ packages/driver/test/cypress/integration/e2e/rerun_spec.js
@@ -1,20 +1,9 @@
-/* eslint-disable
-    brace-style,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-//# NOTE: we could clean up this test a lot
-//# by probably using preserve:run:state event
-//# or by using localstorage
+// NOTE: we could clean up this test a lot
+// by probably using preserve:run:state event
+// or by using localstorage
 
-//# store these on our outer top window
-//# so they are globally preserved
+// store these on our outer top window
+// so they are globally preserved
 if (window.top.hasRunOnce == null) {
   window.top.hasRunOnce = false
 }
@@ -26,31 +15,28 @@ if (window.top.previousHash == null) {
 const isTextTerminal = Cypress.config('isTextTerminal')
 
 describe('rerun state bugs', () => {
-  it('stores viewport globally and does not hang on re-runs', () =>
-  //# NOTE: there's probably other ways to cause a re-run
-  //# event more programatically (like firing it through Cypress)
-  //# but we get the hashchange coverage for free on this.
-
-  {
-    return cy.viewport(500, 500).then(() => {
-      let hash
-
+  // NOTE: there's probably other ways to cause a re-run
+  // event more programatically (like firing it through Cypress)
+  // but we get the hashchange coverage for free on this.
+  it('stores viewport globally and does not hang on re-runs', () => {
+    cy.viewport(500, 500).then(() => {
       if (!window.top.hasRunOnce) {
-      //# turn off mocha events for a second
+      // turn off mocha events for a second
         Cypress.config('isTextTerminal', false)
 
-        //# 1st time around
-        window.top.hasRunOnce = true;
+        // 1st time around
+        window.top.hasRunOnce = true
 
-        //# cause a rerun event to occur
-        //# by changing the hash
-        ({ hash } = window.top.location)
+        // cause a rerun event to occur
+        // by changing the hash
+        let { hash } = window.top.location
+
         window.top.location.hash = `${hash}?rerun`
       } else {
         if (window.top.location.hash === window.top.previousHash) {
-        //# 3rd time around
-        //# let the mocha end events fire if they're supposed to
-          return Cypress.config('isTextTerminal', isTextTerminal)
+          // 3rd time around
+          // let the mocha end events fire if they're supposed to
+          Cypress.config('isTextTerminal', isTextTerminal)
         }
 
         window.top.location.hash = window.top.previousHash
