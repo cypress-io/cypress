058679f4ce998da58eac45957fc2994c8ee8e806
Zach Bloomquist <github@chary.us>
2019-11-12 14:52:56 -0500

    always disable xvfb

--- cli/lib/exec/xvfb.js
+++ cli/lib/exec/xvfb.js
@@ -1,11 +1,8 @@
-const os = require('os')
 const Promise = require('bluebird')
 const Xvfb = require('@cypress/xvfb')
-const { stripIndent } = require('common-tags')
 const debug = require('debug')('cypress:cli')
 const debugXvfb = require('debug')('cypress:xvfb')
 const { throwFormErrorText, errors } = require('../errors')
-const util = require('../util')
 
 const xvfb = Promise.promisifyAll(new Xvfb({
   timeout: 30000, // milliseconds
@@ -47,33 +44,9 @@ module.exports = {
   },
 
   isNeeded () {
-    if (os.platform() !== 'linux') {
-      return false
-    }
+    debug('stubbing isNeeded false %o', new Error().stack)
 
-    if (process.env.DISPLAY) {
-      const issueUrl = util.getGitHubIssueUrl(4034)
-
-      const message = stripIndent`
-        DISPLAY environment variable is set to ${process.env.DISPLAY} on Linux
-        Assuming this DISPLAY points at working X11 server,
-        Cypress will not spawn own Xvfb
-
-        NOTE: if the X11 server is NOT working, Cypress will exit without explanation,
-          see ${issueUrl}
-        Solution: Unset the DISPLAY variable and try again:
-          DISPLAY= npx cypress run ...
-      `
-
-      debug(message)
-
-      return false
-    }
-
-    debug('undefined DISPLAY environment variable')
-    debug('Cypress will spawn its own Xvfb')
-
-    return true
+    return false
   },
 
   // async method, resolved with Boolean
