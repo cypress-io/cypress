88af96ce510adef0390824938a25021767a1fcff
KHeo <sainthkh@naver.com>
2019-11-12 17:17:08 +0900

    Tsified lib/util

--- packages/reporter/src/lib/util.ts
+++ packages/reporter/src/lib/util.ts
@@ -1,13 +1,15 @@
+import { KeyboardEvent } from 'react'
+
 const INDENT_BASE = 5
 const INDENT_AMOUNT = 15
 
-function indent (level) {
+function indent (level: number) {
   return INDENT_BASE + level * INDENT_AMOUNT
 }
 
 // Returns a keyboard handler that invokes the provided function when either enter or space is pressed
-const onEnterOrSpace = (f) => {
-  return (e) => {
+const onEnterOrSpace = (f: (() => void)) => {
+  return (e: KeyboardEvent) => {
     if (e.key === ' ' || e.key === 'Enter') {
       f()
     }
