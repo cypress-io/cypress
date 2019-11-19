720a63077485bee2451dcb49eebdbc2b98ae95dd
decaffeinate <chrisbreiding@gmail.com>
2019-11-19 11:07:11 -0500

    decaffeinate: Convert saved_state.coffee to JS

--- packages/server/lib/saved_state.js
+++ packages/server/lib/saved_state.js
@@ -1,60 +1,73 @@
-_ = require("lodash")
-debug = require("debug")("cypress:server:saved_state")
-FileUtil = require("./util/file")
-appData = require("./util/app_data")
-savedStateUtil = require("./util/saved_state")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS205: Consider reworking code to avoid use of IIFEs
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const _ = require("lodash");
+const debug = require("debug")("cypress:server:saved_state");
+const FileUtil = require("./util/file");
+const appData = require("./util/app_data");
+const savedStateUtil = require("./util/saved_state");
 
-stateFiles = {}
+const stateFiles = {};
 
-whitelist = """
-  appWidth
-  appHeight
-  appX
-  appY
-  autoScrollingEnabled
-  browserWidth
-  browserHeight
-  browserX
-  browserY
-  isAppDevToolsOpen
-  isBrowserDevToolsOpen
-  reporterWidth
-  showedOnBoardingModal
-""".trim().split(/\s+/)
+const whitelist = `\
+appWidth
+appHeight
+appX
+appY
+autoScrollingEnabled
+browserWidth
+browserHeight
+browserX
+browserY
+isAppDevToolsOpen
+isBrowserDevToolsOpen
+reporterWidth
+showedOnBoardingModal\
+`.trim().split(/\s+/);
 
-normalizeAndWhitelistSet = (set, key, value) ->
-  valueObject = if _.isString(key)
-    tmp = {}
-    tmp[key] = value
-    tmp
-  else
-    key
+const normalizeAndWhitelistSet = function(set, key, value) {
+  const valueObject = (() => {
+    if (_.isString(key)) {
+    const tmp = {};
+    tmp[key] = value;
+    return tmp;
+  } else {
+    return key;
+  }
+  })();
 
-  invalidKeys = _.filter _.keys(valueObject), (key) ->
-    not _.includes(whitelist, key)
+  const invalidKeys = _.filter(_.keys(valueObject), key => !_.includes(whitelist, key));
 
-  if invalidKeys.length
-    console.error("WARNING: attempted to save state for non-whitelisted key(s): #{invalidKeys.join(', ')}. All keys must be whitelisted in server/lib/saved_state.coffee")
+  if (invalidKeys.length) {
+    console.error(`WARNING: attempted to save state for non-whitelisted key(s): ${invalidKeys.join(', ')}. All keys must be whitelisted in server/lib/saved_state.coffee`);
+  }
 
-  set(_.pick(valueObject, whitelist))
+  return set(_.pick(valueObject, whitelist));
+};
 
-module.exports = (projectRoot, isTextTerminal) ->
-  if isTextTerminal
-    debug("noop saved state")
-    return Promise.resolve(FileUtil.noopFile)
+module.exports = function(projectRoot, isTextTerminal) {
+  if (isTextTerminal) {
+    debug("noop saved state");
+    return Promise.resolve(FileUtil.noopFile);
+  }
 
-  savedStateUtil.formStatePath(projectRoot)
-  .then (statePath) ->
-    fullStatePath = appData.projectsPath(statePath)
-    debug('full state path %s', fullStatePath)
-    return stateFiles[fullStatePath] if stateFiles[fullStatePath]
+  return savedStateUtil.formStatePath(projectRoot)
+  .then(function(statePath) {
+    const fullStatePath = appData.projectsPath(statePath);
+    debug('full state path %s', fullStatePath);
+    if (stateFiles[fullStatePath]) { return stateFiles[fullStatePath]; }
 
-    debug('making new state file around %s', fullStatePath)
-    stateFile = new FileUtil({
+    debug('making new state file around %s', fullStatePath);
+    const stateFile = new FileUtil({
       path: fullStatePath
-    })
+    });
 
-    stateFile.set = _.wrap(stateFile.set.bind(stateFile), normalizeAndWhitelistSet)
+    stateFile.set = _.wrap(stateFile.set.bind(stateFile), normalizeAndWhitelistSet);
 
-    stateFiles[fullStatePath] = stateFile
-    stateFile
+    stateFiles[fullStatePath] = stateFile;
+    return stateFile;
+  });
+};
