687bc2bf1d18f2a5961463e23f45df40a0ebea9c
KHeo <sainthkh@naver.com>
2019-11-20 10:04:35 +0900

    Merged tests into one file.

--- scripts/decaff/__tests__/decaff.test.js
+++ scripts/decaff/__tests__/decaff.test.js
@@ -3,4 +3,7 @@
 jest.autoMockOff()
 const defineTest = require('jscodeshift/dist/testUtils').defineTest
 
+defineTest(__dirname, 'switch-false')
+defineTest(__dirname, 'empty-catch')
 defineTest(__dirname, 'remove-comment-sharp')
+defineTest(__dirname, 'arrow-comment')
--- scripts/decaff/__tests__/switch-false.test.js
+++
@@ -1,6 +0,0 @@
-/* global jest */
-
-jest.autoMockOff()
-const defineTest = require('jscodeshift/dist/testUtils').defineTest
-
-defineTest(__dirname, 'switch-false')
