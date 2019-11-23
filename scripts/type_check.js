7b22fd984ed811195fca2a3d742b52e426afd57a
KHeo <sainthkh@naver.com>
2019-11-23 17:18:28 +0900

    Added type_check.js

---
+++ scripts/type_check.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const path = require('path')
+const execa = require('execa')
+const Listr = require('listr')
+const commander = require('commander')
+
+const program = new commander.Command()
+
+program.usage('[options]')
+
+program
+.option('-p, --project <projects>', 'projects to check types (separated by commas)')
+.option('--skip-lib-check', 'skip type checking of all declaration files (*.d.ts)')
+.action((...args) => {
+  const projects = []
+  const packageRoot = path.join(__dirname, '../packages')
+  const getPackagePath = (name) => path.join(packageRoot, name)
+
+  if (program.project) {
+    program.project.split(',').forEach((p) => {
+      projects.push({
+        name: p,
+        path: getPackagePath(p),
+      })
+    })
+  } else {
+    fs.readdirSync(packageRoot).forEach((file) => {
+      const packagePath = getPackagePath(file)
+
+      if (fs.lstatSync(packagePath).isDirectory()) {
+        if (fs.existsSync(path.join(packagePath, 'tsconfig.json'))) {
+          projects.push({
+            name: file,
+            path: packagePath,
+          })
+        }
+      }
+    })
+  }
+
+  const tasks = new Listr(projects.map((proj) => {
+    return {
+      title: proj.name,
+      task: () => {
+        const cwd = proj.path
+        const tsc = require.resolve('typescript/bin/tsc')
+
+        return execa(tsc, ['--noEmit', '--pretty'], {
+          cwd,
+        }).catch((err) => {
+          throw {
+            name: proj.name,
+            err,
+          }
+        })
+      },
+    }
+  }), {
+    concurrent: 4,
+    exitOnError: false,
+  })
+
+  tasks.run().catch((err) => {
+    process.exitCode = 1
+
+    err.errors.forEach((e) => {
+      log('')
+      log(`${e.name} failed\n${e.err.stdout}`)
+    })
+  })
+})
+
+const log = (msg) => {
+  // eslint-disable-next-line no-console
+  console.log(msg)
+}
+
+program.parse(process.argv)
