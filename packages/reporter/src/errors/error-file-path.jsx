412378456e4b559c7da30c5faaac01f5451a07b1
Chris Breiding <chrisbreiding@gmail.com>
2019-12-17 10:23:39 -0500

    refactor editor picker modal

--- packages/reporter/src/errors/error-file-path.jsx
+++ packages/reporter/src/errors/error-file-path.jsx
@@ -21,31 +21,52 @@ const openFile = (where, { absoluteFile: file, line, column }, editor) => {
   })
 }
 
-const Modal = observer(({ close, state, onSetEditor, onSelectEditor, onUpdateOtherPath }) => {
+const EditorPickerModal = observer(({ editors, onClose, onSetEditor }) => {
+  const state = useLocalStore(() => ({
+    chosenEditor: {},
+    setChosenEditor: action((editor) => {
+      state.chosenEditor = editor
+    }),
+    setOtherPath: action((otherPath) => {
+      const otherOption = _.find(editors, { isOther: true })
+
+      otherOption.openerId = otherPath
+    }),
+  }))
+
+  const setEditor = () => {
+    const editor = state.chosenEditor
+    // TODO: validate editor is chosen
+
+    onSetEditor(editor)
+  }
+
   const cancelRef = useRef()
+
+  if (!editors.length) return null
 
   return (
     <Dialog
       aria-label="Explanation of choosing an editor"
       initialFocusRef={cancelRef}
       isOpen={state.isModalOpen}
-      onDismiss={close}
+      onDismiss={onClose}
     >
       <div className='content'>
         <p>It looks like this is your first time opening a file in your editor. Please select from one of the editors we found on your system.</p>
         <p>The editor you select will be used automatically from now on to open files. You can change your selection in the Settings tab of the Cypress app.</p>
         <EditorPicker
           chosen={state.chosenEditor}
-          editors={state.editors}
-          onSelect={onSelectEditor}
-          onUpdateOtherPath={onUpdateOtherPath}
+          editors={editors}
+          onSelect={state.setChosenEditor}
+          onUpdateOtherPath={state.setOtherPath}
         />
       </div>
       <div className='controls'>
-        <button className='submit' onClick={onSetEditor}>Set Editor</button>
-        <button className='cancel' onClick={close} ref={cancelRef}>Cancel</button>
+        <button className='submit' onClick={setEditor}>Set Editor</button>
+        <button className='cancel' onClick={onClose} ref={cancelRef}>Cancel</button>
       </div>
-      <button className='close-button' onClick={close}>
+      <button className='close-button' onClick={onClose}>
         <VisuallyHidden>Close</VisuallyHidden>
         <span aria-hidden>
           <i className='fa fa-remove' />
@@ -57,26 +78,17 @@ const Modal = observer(({ close, state, onSetEditor, onSelectEditor, onUpdateOth
 
 const ErrorFilePath = observer(({ fileDetails }) => {
   const state = useLocalStore(() => ({
+    editors: [],
     isLoadingEditor: false,
     isModalOpen: false,
-    editors: [],
-    chosenEditor: {},
+    setEditors: action((editors) => {
+      state.editors = editors
+    }),
     setIsLoadingEditor: action((isLoading) => {
       state.isLoadingEditor = isLoading
     }),
     setIsModalOpen: action((isOpen) => {
       state.isModalOpen = isOpen
-    }),
-    setEditors: action((editors) => {
-      state.editors = editors
-    }),
-    setChosenEditor: action((editor) => {
-      state.chosenEditor = editor
-    }),
-    setOtherPath: action((otherPath) => {
-      const otherOption = _.find(state.editors, { isOther: true })
-
-      otherOption.openerId = otherPath
     }),
   }))
 
@@ -95,11 +107,7 @@ const ErrorFilePath = observer(({ fileDetails }) => {
     })
   }
 
-  const setEditor = () => {
-    // TODO: validate editor is chosen
-    const editor = state.chosenEditor
-
-    // QUESTION: add error handling for this?
+  const setEditor = (editor) => {
     events.emit('set:user:editor', editor)
     state.setIsModalOpen(false)
     openFile('editor', fileDetails, editor)
@@ -132,12 +140,10 @@ const ErrorFilePath = observer(({ fileDetails }) => {
       <Tooltip title={tooltipContent} className='err-file-options tooltip'>
         <span>{relativeFile}:{line}:{column}</span>
       </Tooltip>
-      <Modal
-        state={state}
-        onSelectEditor={state.setChosenEditor}
+      <EditorPickerModal
+        editors={state.editors}
         onSetEditor={setEditor}
-        onUpdateOtherPath={state.setOtherPath}
-        close={_.partial(state.setIsModalOpen, false)}
+        onClose={_.partial(state.setIsModalOpen, false)}
       />
     </span>
   )
