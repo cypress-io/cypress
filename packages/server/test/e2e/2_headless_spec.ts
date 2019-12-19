8c1fefebe96126f04b58cbef5f608182fbe972bc
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-12-19 13:36:22 -0500

    fix headless e2e spec for firefox

--- packages/server/test/e2e/2_headless_spec.ts
+++ packages/server/test/e2e/2_headless_spec.ts
@@ -3,9 +3,10 @@ const e2e = require('../support/helpers/e2e')
 describe('e2e headless', function () {
   e2e.setup()
 
-  // cypress run
+  // cypress run --headless
   e2e.it('tests in headless mode pass', {
     spec: 'headless_spec.js',
+    browser: ['chrome', 'electron'],
     config: {
       env: {
         'CI': process.env.CI,
@@ -18,18 +19,20 @@ describe('e2e headless', function () {
   })
 
   // cypress run --headed
-  e2e.it('tests in headed mode pass', {
-    spec: 'headless_spec.js',
-    config: {
-      env: {
-        'CI': process.env.CI,
+  // currently, Electron differs because it displays a
+  // "can not record video in headed mode" error
+  ;['electron', '!electron'].map((b) => {
+    e2e.it(`tests in headed mode pass in ${b}`, {
+      spec: 'headless_spec.js',
+      config: {
+        env: {
+          'CI': process.env.CI,
+        },
       },
-    },
-    expectedExitCode: 0,
-    headed: true,
-    snapshot: true,
-    // currently, Electron differs because it displays a
-    // "can not record video in headed mode" error
-    useSeparateBrowserSnapshots: true,
+      expectedExitCode: 0,
+      headed: true,
+      snapshot: true,
+      browser: b,
+    })
   })
 })
