0bdeeeb7699757ce8da24df7976ff1cb51d2175d
KHeo <sainthkh@naver.com>
2020-01-06 17:58:07 +0900

    Wrote script that generates config option object from configuration.md

--- packages/server/lib/modes/init/prompt.ts
+++ packages/server/lib/modes/init/prompt.ts
@@ -7,6 +7,10 @@ export const prompt = async (options: any) => {
     message: 'Customize settings?',
   })
 
+  if (customize) {
+
+  }
+
   const { useTypeScript } = await prompts({
     type: 'confirm',
     name: 'useTypeScript',
