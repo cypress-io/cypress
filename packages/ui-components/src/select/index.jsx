c8e05261c93698885ab99719a2203d53f92088a1
Chris Breiding <chrisbreiding@gmail.com>
2020-01-08 14:55:51 -0500

    rename select to select-group
    changing the capitalization from Select to select wasn’t being picked up by git

--- packages/ui-components/src/select/index.jsx
+++ packages/ui-components/src/select/index.jsx
@@ -1,5 +1,5 @@
 export { default as SelectItem } from './select-item'
 
-export { default as Select } from './select'
+export { default as Select } from './select-group'
 
 export { default as useSelect } from './use-select'
