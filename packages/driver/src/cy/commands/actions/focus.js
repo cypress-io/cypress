35d38534f7deb2aeeec4c66b37817ded60962b95
decaffeinate <clare.m.so+github@gmail.com>
2020-01-13 00:40:22 -0500

    decaffeinate: Convert check.coffee and 6 other files to JS

--- packages/driver/src/cy/commands/actions/focus.js
+++ packages/driver/src/cy/commands/actions/focus.js
@@ -1,138 +1,157 @@
-_ = require("lodash")
-Promise = require("bluebird")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const _ = require("lodash");
+const Promise = require("bluebird");
 
-$dom = require("../../../dom")
-$utils = require("../../../cypress/utils")
-$elements = require("../../../dom/elements")
-$actionability = require("../../actionability")
+const $dom = require("../../../dom");
+const $utils = require("../../../cypress/utils");
+const $elements = require("../../../dom/elements");
+const $actionability = require("../../actionability");
 
-module.exports = (Commands, Cypress, cy, state, config) ->
-  Commands.addAll({ prevSubject: ["element", "window"] }, {
-    focus: (subject, options = {}) ->
-      ## we should throw errors by default!
-      ## but allow them to be silenced
-      _.defaults(options, {
-        $el: subject
-        error: true
-        log: true
-        verify: true
-      })
+module.exports = (Commands, Cypress, cy, state, config) => Commands.addAll({ prevSubject: ["element", "window"] }, {
+  focus(subject, options = {}) {
+    //# we should throw errors by default!
+    //# but allow them to be silenced
+    let isWin, num, verifyAssertions;
+    _.defaults(options, {
+      $el: subject,
+      error: true,
+      log: true,
+      verify: true
+    });
 
-      if isWin = $dom.isWindow(options.$el)
-        ## get this into a jquery object
-        options.$el = $dom.wrap(options.$el)
+    if (isWin = $dom.isWindow(options.$el)) {
+      //# get this into a jquery object
+      options.$el = $dom.wrap(options.$el);
+    }
 
-      if options.log
-        options._log = Cypress.log
-          $el: options.$el
-          consoleProps: ->
-            "Applied To": $dom.getElements(options.$el)
+    if (options.log) {
+      options._log = Cypress.log({
+        $el: options.$el,
+        consoleProps() {
+          return {"Applied To": $dom.getElements(options.$el)};
+        }
+      });
+    }
 
-      el = options.$el.get(0)
+    const el = options.$el.get(0);
 
-      ## the body is not really focusable, but it
-      ## can have focus on initial page load.
-      ## this is instead a noop.
-      ## TODO: throw on body instead (breaking change)
-      isBody = $dom.isJquery(options.$el) &&
-        $elements.isElement(options.$el.get(0)) &&
-        $elements.isBody(options.$el.get(0))
+    //# the body is not really focusable, but it
+    //# can have focus on initial page load.
+    //# this is instead a noop.
+    //# TODO: throw on body instead (breaking change)
+    const isBody = $dom.isJquery(options.$el) &&
+      $elements.isElement(options.$el.get(0)) &&
+      $elements.isBody(options.$el.get(0));
 
-      ## http://www.w3.org/$R/html5/editing.html#specially-focusable
-      ## ensure there is only 1 dom element in the subject
-      ## make sure its allowed to be focusable
-      if not (isWin or isBody or $dom.isFocusable(options.$el))
-        return if options.error is false
+    //# http://www.w3.org/$R/html5/editing.html#specially-focusable
+    //# ensure there is only 1 dom element in the subject
+    //# make sure its allowed to be focusable
+    if (!(isWin || isBody || $dom.isFocusable(options.$el))) {
+      if (options.error === false) { return; }
 
-        node = $dom.stringify(options.$el)
-        $utils.throwErrByPath("focus.invalid_element", {
-          onFail: options._log
-          args: { node }
-        })
+      const node = $dom.stringify(options.$el);
+      $utils.throwErrByPath("focus.invalid_element", {
+        onFail: options._log,
+        args: { node }
+      });
+    }
 
-      if (num = options.$el.length) and num > 1
-        return if options.error is false
+    if ((num = options.$el.length) && (num > 1)) {
+      if (options.error === false) { return; }
 
-        $utils.throwErrByPath("focus.multiple_elements", {
-          onFail: options._log
-          args: { num }
-        })
+      $utils.throwErrByPath("focus.multiple_elements", {
+        onFail: options._log,
+        args: { num }
+      });
+    }
 
 
-      cy.fireFocus(el)
+    cy.fireFocus(el);
 
-      # return options.$el if options.verify is false
+    // return options.$el if options.verify is false
 
-      do verifyAssertions = ->
-        cy.verifyUpcomingAssertions(options.$el, options, {
-          onRetry: verifyAssertions
-        })
+    return (verifyAssertions = () => cy.verifyUpcomingAssertions(options.$el, options, {
+      onRetry: verifyAssertions
+    }))();
+  },
 
-    blur: (subject, options = {}) ->
-      ## we should throw errors by default!
-      ## but allow them to be silenced
-      _.defaults(options, {
-        $el: subject
-        $focused: cy.getFocused()
-        error: true
-        log: true
-        verify: true
-        force: false
-      })
+  blur(subject, options = {}) {
+    //# we should throw errors by default!
+    //# but allow them to be silenced
+    let isWin, num, verifyAssertions;
+    _.defaults(options, {
+      $el: subject,
+      $focused: cy.getFocused(),
+      error: true,
+      log: true,
+      verify: true,
+      force: false
+    });
 
-      { $el, $focused } = options
+    const { $el, $focused } = options;
 
-      if isWin = $dom.isWindow(options.$el)
-        ## get this into a jquery object
-        options.$el = $dom.wrap(options.$el)
+    if (isWin = $dom.isWindow(options.$el)) {
+      //# get this into a jquery object
+      options.$el = $dom.wrap(options.$el);
+    }
 
-      isBody = options.$el.is("body")
+    const isBody = options.$el.is("body");
 
-      if options.log
-        ## figure out the options which actually change the behavior of clicks
-        deltaOptions = $utils.filterOutOptions(options)
+    if (options.log) {
+      //# figure out the options which actually change the behavior of clicks
+      const deltaOptions = $utils.filterOutOptions(options);
 
-        options._log = Cypress.log
-          $el: options.$el
-          message: deltaOptions
-          consoleProps: ->
-            "Applied To": $dom.getElements(options.$el)
+      options._log = Cypress.log({
+        $el: options.$el,
+        message: deltaOptions,
+        consoleProps() {
+          return {"Applied To": $dom.getElements(options.$el)};
+        }
+      });
+    }
 
-      if (num = options.$el.length) and num > 1
-        return if options.error is false
+    if ((num = options.$el.length) && (num > 1)) {
+      if (options.error === false) { return; }
 
-        $utils.throwErrByPath("blur.multiple_elements", {
-          onFail: options._log
-          args: { num }
-        })
+      $utils.throwErrByPath("blur.multiple_elements", {
+        onFail: options._log,
+        args: { num }
+      });
+    }
 
-      ## if we haven't forced the blur, and we don't currently
-      ## have a focused element OR we aren't the window or body then error
-      if (options.force isnt true) and (not $focused) and (not isWin) and (not isBody)
-        return if options.error is false
+    //# if we haven't forced the blur, and we don't currently
+    //# have a focused element OR we aren't the window or body then error
+    if ((options.force !== true) && (!$focused) && (!isWin) && (!isBody)) {
+      if (options.error === false) { return; }
 
-        $utils.throwErrByPath("blur.no_focused_element", { onFail: options._log })
+      $utils.throwErrByPath("blur.no_focused_element", { onFail: options._log });
+    }
 
-      ## if we're currently window dont check for the wrong
-      ## focused element because window will not show up
-      ## as $focused
-      if (options.force isnt true) and (not isWin) and (not isBody) and (
-        options.$el.get(0) isnt $focused.get(0)
-      )
-        return if options.error is false
+    //# if we're currently window dont check for the wrong
+    //# focused element because window will not show up
+    //# as $focused
+    if ((options.force !== true) && (!isWin) && (!isBody) && (
+      options.$el.get(0) !== $focused.get(0)
+    )) {
+      if (options.error === false) { return; }
 
-        node = $dom.stringify($focused)
-        $utils.throwErrByPath("blur.wrong_focused_element", {
-          onFail: options._log
-          args: { node }
-        })
+      const node = $dom.stringify($focused);
+      $utils.throwErrByPath("blur.wrong_focused_element", {
+        onFail: options._log,
+        args: { node }
+      });
+    }
 
-      el = options.$el.get(0)
+    const el = options.$el.get(0);
 
-      cy.fireBlur(el)
+    cy.fireBlur(el);
 
-      do verifyAssertions = ->
-        cy.verifyUpcomingAssertions(options.$el, options, {
-          onRetry: verifyAssertions
-        })
-  })
+    return (verifyAssertions = () => cy.verifyUpcomingAssertions(options.$el, options, {
+      onRetry: verifyAssertions
+    }))();
+  }
+});
--- packages/driver/src/cy/commands/actions/hover.js
+++ packages/driver/src/cy/commands/actions/hover.js
@@ -1,7 +1,12 @@
-$utils = require("../../../cypress/utils")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const $utils = require("../../../cypress/utils");
 
-module.exports = (Commands, Cypress, cy, state, config) ->
-  Commands.addAll({ prevSubject: "element" }, {
-    hover: (args) ->
-      $utils.throwErrByPath("hover.not_implemented")
-  })
+module.exports = (Commands, Cypress, cy, state, config) => Commands.addAll({ prevSubject: "element" }, {
+  hover(args) {
+    return $utils.throwErrByPath("hover.not_implemented");
+  }
+});
