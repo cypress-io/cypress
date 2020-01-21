e2904f9add342b6852b4bd511247bd1de3782849
Waldemar Penner <waldemar.penner@ti8m.ch>
2020-01-21 15:37:17 +0100

    test: add tests for special sequences {moveToStart} / {moveToEnd}
    add blank line
    
    remove trailing spaces

--- packages/driver/test/cypress/integration/commands/actions/type_spec.js
+++ packages/driver/test/cypress/integration/commands/actions/type_spec.js
@@ -3314,6 +3314,18 @@ describe('src/cy/commands/actions/type', () => {
             done()
           })
         })
+
+        it('moves the cursor to start position', () => {
+          cy
+          .get('input:first').type('123{moveToStart}456')
+          .should('have.value', '456123')
+        })
+
+        it('moves the cursor in-between start and end position', () => {
+          cy
+          .get('input:first').type('123{moveToStart}456{moveToEnd}789')
+          .should('have.value', '456123789')
+        })
       })
 
       describe('release: false', () => {
