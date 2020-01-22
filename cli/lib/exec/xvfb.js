2cc78caf5f8a531267ae14edd2118f376714525a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-22 12:27:07 -0500

    add comment back to the issue

--- cli/lib/exec/xvfb.js
+++ cli/lib/exec/xvfb.js
@@ -9,7 +9,9 @@ const util = require('../util')
 
 const xvfb = Promise.promisifyAll(new Xvfb({
   timeout: 30000, // milliseconds
-  xvfb_args: ['-screen', '0', '1280x1024x24'], // need to explicitly define screen otherwise electron will crash
+  // need to explicitly define screen otherwise electron will crash
+  // https://github.com/cypress-io/cypress/issues/6184
+  xvfb_args: ['-screen', '0', '1280x1024x24'],
   onStderrData (data) {
     if (debugXvfb.enabled) {
       debugXvfb(data.toString())
