13e7d9338a3bcd080f784b1ed821c1bc688388c3
Zach Bloomquist <github@chary.us>
2020-01-24 18:03:21 -0500

    fix e2e test

---
+++ packages/server/__snapshots__/5_browser_crashing_spec.ts.js
@@ -0,0 +1,61 @@
+exports['e2e browser crashing / is gracefully handled'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (browser_crashing_spec.js)                                                 │
+  │ Searched:   cypress/integration/browser_crashing_spec.js                                       │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  browser_crashing_spec.js                                                        (1 of 1)
+
+
+  e2e browser crashing spec
+
+We detected that the FooBrowser process running your tests just exited unexpectedly.
+
+This error can occur whenever Cypress detects that the browser that was launched for running tests has exited or crashed before the tests could finish running, or without the user manually stopping the tests in Cypress.
+
+Learn more at https://on.cypress.io/browser-process-crashed
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        0                                                                                │
+  │ Passing:      0                                                                                │
+  │ Failing:      1                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  0                                                                                │
+  │ Video:        true                                                                             │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     browser_crashing_spec.js                                                         │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+  (Video)
+
+  -  Started processing:  Compressing to 32 CRF                                                     
+  -  Finished processing: /XXX/XXX/XXX/cypress/videos/browser_crashing_spec.js.mp4        (X second)
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+       Spec                                              Tests  Passing  Failing  Pending  Skipped  
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✖  browser_crashing_spec.js                 XX:XX        -        -        1        -        - │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    ✖  1 of 1 failed (100%)                     XX:XX        -        -        1        -        -  
+
+
+`
--- packages/server/test/e2e/5_browser_crashing_spec.ts
+++ packages/server/test/e2e/5_browser_crashing_spec.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+import { expect } from 'chai'
 const e2e = require('../support/helpers/e2e')
 
 describe('e2e browser crashing', function () {
@@ -5,7 +7,18 @@ describe('e2e browser crashing', function () {
 
   e2e.it('is gracefully handled', {
     spec: 'browser_crashing_spec.js',
-    expectedExitCode: 0,
+    expectedExitCode: 1,
     snapshot: true,
+    onRun (exec, browser) {
+      return exec({
+        onStdout: (stdout) => {
+          const needle = `the ${_.upperFirst(browser)} process`
+
+          expect(stdout).to.contain(needle)
+
+          return stdout.replace(needle, 'the FooBrowser process')
+        },
+      })
+    },
   })
 })
