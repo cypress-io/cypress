095d3a533ed1a85a40f1fa9b40532fbf9455b55d
Zach Bloomquist <github@chary.us>
2020-01-29 17:19:50 -0500

    add tests for process_profiler

--- packages/server/lib/util/process_profiler.ts
+++ packages/server/lib/util/process_profiler.ts
@@ -4,9 +4,9 @@ import _ from 'lodash'
 import si from 'systeminformation'
 import { concatStream } from '@packages/network'
 
-const { getBrowserPid } = require('../browsers')
-const { getPluginPid } = require('../plugins')
-const { getFfmpegPid } = require('../video_capture')
+const browsers = require('../browsers')
+const plugins = require('../plugins')
+const videoCapture = require('../video_capture')
 
 type Group = 'browser' | 'cypress' | 'plugin' | 'desktop-gui' | 'ffmpeg' | 'electron-shared' | 'other'
 type Process = si.Systeminformation.ProcessesProcessData & {
@@ -34,178 +34,186 @@ const formatPidDisplay = (groupedProcesses) => {
   return display
 }
 
-function checkProcesses () {
+export const _groupCyProcesses = ({ list }: si.Systeminformation.ProcessesData) => {
+  const cyProcesses: Process[] = []
+  const thisProcess: Process = _.find(list, { pid: process.pid })!
+
+  la(thisProcess, 'expected to find current pid in process list', process.pid)
+
+  const isParentProcessInGroup = (proc: Process, group: Group) => {
+    return _.chain(cyProcesses).filter({ group }).map('pid').includes(proc.parentPid).value()
+  }
+
+  // is this a browser process launched to run Cypress tests?
+  const isBrowserProcess = (proc: Process): boolean => {
+    // electron will return a list of pids, since it's not a hierarchy
+    const pid: number | number[] = browsers.getBrowserPid()
+
+    debug({ pid })
+
+    return (Array.isArray(pid) ? (pid as number[]).includes(proc.pid) : proc.pid === pid)
+      || isParentProcessInGroup(proc, 'browser')
+  }
+
+  const isPluginProcess = (proc: Process): boolean => {
+    return proc.pid === plugins.getPluginPid()
+      || isParentProcessInGroup(proc, 'plugin')
+  }
+
+  // is this the renderer for the desktop-gui?
+  const isDesktopGuiProcess = (proc: Process): boolean => {
+    return proc.params.includes('--type=renderer')
+      && !isBrowserProcess(proc)
+  }
+
+  // these processes may be shared between the AUT and desktop-gui
+  // rather than treat them as part of the `browser` in `run` mode and have
+  // their usage in `open` mode be ambiguous, just put them in their own group
+  const isElectronSharedProcess = (proc: Process): boolean => {
+    const isType = (type) => {
+      return proc.params.includes(`--type=${type}`)
+    }
+
+    return isType('broker')
+    || isType('gpu-process')
+    || isType('utility')
+    || isType('zygote')
+  }
+
+  const getProcessGroup = (proc: Process): Group => {
+    if (proc === thisProcess) {
+      return 'cypress'
+    }
+
+    if (isBrowserProcess(proc)) {
+      return 'browser'
+    }
+
+    if (isPluginProcess(proc)) {
+      return 'plugin'
+    }
+
+    if (isDesktopGuiProcess(proc)) {
+      return 'desktop-gui'
+    }
+
+    if (proc.pid === videoCapture.getFfmpegPid()) {
+      return 'ffmpeg'
+    }
+
+    if (isElectronSharedProcess(proc)) {
+      return 'electron-shared'
+    }
+
+    return 'other'
+  }
+
+  const classifyProcess = (proc: Process) => {
+    const classify = (group: Group) => {
+      proc.group = group
+      cyProcesses.push(proc)
+
+      // queue all children
+      _.chain(list)
+      .filter({ parentPid: proc.pid })
+      .map(classifyProcess)
+      .value()
+    }
+
+    classify(getProcessGroup(proc))
+  }
+
+  classifyProcess(thisProcess)
+
+  return cyProcesses
+}
+
+export const _printGroupedProcesses = (processes) => {
+  debugVerbose('all Cypress-launched processes: %s', require('util').inspect(processes))
+
+  const consoleBuffer = concatStream((buf) => {
+    // get rid of trailing newline
+    debug(String(buf).trim())
+  })
+
+  // eslint-disable-next-line no-console
+  const buffedConsole = new console.Console(consoleBuffer)
+
+  const groupTotals = _.chain(processes)
+  .groupBy('group')
+  .mapValues((groupedProcesses, group) => {
+    return {
+      group,
+      processCount: groupedProcesses.length,
+      pids: formatPidDisplay(groupedProcesses),
+      cpuPercent: _.sumBy(groupedProcesses, 'pcpu'),
+      memRssMb: _.sumBy(groupedProcesses, 'mem_rss') / 1024,
+    }
+  })
+  .values()
+  .sortBy('memRssMb')
+  .reverse()
+  .value()
+
+  buffedConsole.log('current & mean memory and CPU usage by process group:')
+  groupTotals.push(_.reduce(groupTotals, (acc, val) => {
+    acc.processCount += val.processCount
+    acc.cpuPercent += val.cpuPercent
+    acc.memRssMb += val.memRssMb
+
+    return acc
+  }, { group: 'TOTAL', processCount: 0, pids: '-', cpuPercent: 0, memRssMb: 0 }))
+
+  groupTotals.forEach((total) => {
+    if (!groupsOverTime[total.group]) {
+      groupsOverTime[total.group] = []
+    }
+
+    const measurements = groupsOverTime[total.group]
+
+    measurements.push(total)
+
+    _.merge(total, {
+      meanCpuPercent: _.meanBy(measurements, 'cpuPercent'),
+      meanMemRssMb: _.meanBy(measurements, 'memRssMb'),
+      maxMemRssMb: _.max(_.map(measurements, _.property('memRssMb'))),
+    })
+
+    _.forEach(total, (v, k) => {
+      // round all numbers to 100ths precision
+      if (_.isNumber(v)) {
+        total[k] = _.round(v, 2)
+      }
+    })
+  })
+
+  buffedConsole.table(groupTotals, [
+    'group',
+    'processCount',
+    'pids',
+    'cpuPercent',
+    'meanCpuPercent',
+    'memRssMb',
+    'meanMemRssMb',
+    'maxMemRssMb',
+  ])
+
+  consoleBuffer.end()
+}
+
+function _checkProcesses () {
   return si.processes()
-  .then(({ list }) => {
-    const cyProcesses: Process[] = []
-    const thisProcess: Process = _.find(list, { pid: process.pid })!
-
-    la(thisProcess, 'expected to find current pid in process list')
-
-    const isParentProcessInGroup = (proc: Process, group: Group) => {
-      return _.chain(cyProcesses).filter({ group }).map('pid').includes(proc.parentPid).value()
-    }
-
-    // is this a browser process launched to run Cypress tests?
-    const isBrowserProcess = (proc: Process): boolean => {
-      // electron will return a list of pids, since it's not a hierarchy
-      const pid: number | number[] = getBrowserPid()
-
-      debug({ pid })
-
-      return (Array.isArray(pid) ? (pid as number[]).includes(proc.pid) : proc.pid === pid)
-        || isParentProcessInGroup(proc, 'browser')
-    }
-
-    const isPluginProcess = (proc: Process): boolean => {
-      return proc.pid === getPluginPid()
-        || isParentProcessInGroup(proc, 'plugin')
-    }
-
-    // is this the renderer for the desktop-gui?
-    const isDesktopGuiProcess = (proc: Process): boolean => {
-      return proc.params.includes('--type=renderer')
-        && !isBrowserProcess(proc)
-    }
-
-    // these processes may be shared between the AUT and desktop-gui
-    // rather than treat them as part of the `browser` in `run` mode and have
-    // their usage in `open` mode be ambiguous, just put them in their own group
-    const isElectronSharedProcess = (proc: Process): boolean => {
-      return proc.params.includes('--type=broker')
-      || proc.params.includes('--type=gpu-process')
-      || proc.params.includes('--type=utility')
-      || proc.params.includes('--type=zygote')
-    }
-
-    const getProcessGroup = (proc: Process): Group => {
-      if (proc === thisProcess) {
-        return 'cypress'
-      }
-
-      if (isBrowserProcess(proc)) {
-        return 'browser'
-      }
-
-      if (isPluginProcess(proc)) {
-        return 'plugin'
-      }
-
-      if (isDesktopGuiProcess(proc)) {
-        return 'desktop-gui'
-      }
-
-      if (proc.pid === getFfmpegPid()) {
-        return 'ffmpeg'
-      }
-
-      if (isElectronSharedProcess(proc)) {
-        return 'electron-shared'
-      }
-
-      return 'other'
-    }
-
-    const classifyProcess = (proc: Process) => {
-      const classify = (group: Group) => {
-        proc.group = group
-        cyProcesses.push(proc)
-
-        // queue all children
-        _.chain(list)
-        .filter({ parentPid: proc.pid })
-        .map(classifyProcess)
-        .value()
-      }
-
-      classify(getProcessGroup(proc))
-    }
-
-    classifyProcess(thisProcess)
-
-    return cyProcesses
-  })
-  .then((processes) => {
-    debugVerbose('all Cypress-launched processes: %s', require('util').inspect(processes))
-
-    const consoleBuffer = concatStream((buf) => {
-      // get rid of trailing newline
-      debug(String(buf).trim())
-    })
-
-    // eslint-disable-next-line no-console
-    const buffedConsole = new console.Console(consoleBuffer)
-
-    const groupTotals = _.chain(processes)
-    .groupBy('group')
-    .mapValues((groupedProcesses, group) => {
-      return {
-        group,
-        processCount: groupedProcesses.length,
-        pids: formatPidDisplay(groupedProcesses),
-        cpuPercent: _.sumBy(groupedProcesses, 'pcpu'),
-        memRssMb: _.sumBy(groupedProcesses, 'mem_rss') / 1024,
-      }
-    })
-    .values()
-    .sortBy('memRssMb')
-    .reverse()
-    .value()
-
-    buffedConsole.log('current & mean memory and CPU usage by process group:')
-    groupTotals.push(_.reduce(groupTotals, (acc, val) => {
-      acc.processCount += val.processCount
-      acc.cpuPercent += val.cpuPercent
-      acc.memRssMb += val.memRssMb
-
-      return acc
-    }, { group: 'TOTAL', processCount: 0, pids: '-', cpuPercent: 0, memRssMb: 0 }))
-
-    groupTotals.forEach((total) => {
-      if (!groupsOverTime[total.group]) {
-        groupsOverTime[total.group] = []
-      }
-
-      const measurements = groupsOverTime[total.group]
-
-      measurements.push(total)
-
-      _.merge(total, {
-        meanCpuPercent: _.meanBy(measurements, 'cpuPercent'),
-        meanMemRssMb: _.meanBy(measurements, 'memRssMb'),
-        maxMemRssMb: _.max(_.map(measurements, _.property('memRssMb'))),
-      })
-
-      _.forEach(total, (v, k) => {
-        // round all numbers to 100ths precision
-        if (_.isNumber(v)) {
-          total[k] = _.round(v, 2)
-        }
-      })
-    })
-
-    buffedConsole.table(groupTotals, [
-      'group',
-      'processCount',
-      'pids',
-      'cpuPercent',
-      'meanCpuPercent',
-      'memRssMb',
-      'meanMemRssMb',
-      'maxMemRssMb',
-    ])
-
-    consoleBuffer.end()
-  })
-  .then(scheduleProcessCheck)
+  .then(_groupCyProcesses)
+  .then(_printGroupedProcesses)
+  .then(_scheduleProcessCheck)
   .catch((err) => {
     debug('error running process profiler: %o', err)
   })
 }
 
-function scheduleProcessCheck () {
+function _scheduleProcessCheck () {
   // not setinterval, since checkProcesses is asynchronous
-  setTimeout(checkProcesses, interval)
+  setTimeout(_checkProcesses, interval)
 }
 
 export function start () {
@@ -219,7 +227,7 @@ export function start () {
     return
   }
 
-  checkProcesses()
+  _checkProcesses()
 
   started = true
 }
---
+++ packages/server/test/unit/util/process_profiler_spec.ts
@@ -0,0 +1,140 @@
+import '../../spec_helper'
+
+import _ from 'lodash'
+import si from 'systeminformation'
+import { expect } from 'chai'
+import { _groupCyProcesses } from '../../../lib/util/process_profiler'
+import sinon from 'sinon'
+
+const browsers = require('../../../lib/browsers')
+const plugins = require('../../../lib/plugins')
+const videoCapture = require('../../../lib/video_capture')
+
+const BROWSER_PID = 11111
+const SUB_BROWSER_PID = 11112
+const GUI_PID = 77777
+const PLUGIN_PID = 22222
+const SUB_PLUGIN_PID = 22223
+const FFMPEG_PID = 33333
+const MAIN_PID = process.pid
+const OTHER_PID = 66666
+const ANOTHER_PID = 88888
+const LAUNCHER_PID = 55555
+const SHARED_BROKER_PID = 99990
+const SHARED_GPU_PID = 99991
+const SHARED_UTILITY_PID = 99992
+const SHARED_ZYGOTE_PID = 99993
+
+const PROCESSES: Partial<si.Systeminformation.ProcessesProcessData>[] = [
+  {
+    pid: MAIN_PID,
+    parentPid: LAUNCHER_PID,
+    params: '',
+    name: 'Cypress',
+  },
+  {
+    pid: BROWSER_PID,
+    parentPid: MAIN_PID,
+    params: '',
+    name: 'firefox',
+  },
+  {
+    pid: SUB_BROWSER_PID,
+    parentPid: BROWSER_PID,
+    params: '',
+    name: 'firefox-bin',
+  },
+  {
+    pid: GUI_PID,
+    parentPid: MAIN_PID,
+    params: '--type=renderer',
+    name: 'Cypress',
+  },
+  {
+    pid: PLUGIN_PID,
+    parentPid: MAIN_PID,
+    params: 'plugin.js',
+    name: 'node',
+  },
+  {
+    pid: SUB_PLUGIN_PID,
+    parentPid: PLUGIN_PID,
+    params: '',
+    name: 'msword.exe',
+  },
+  {
+    pid: FFMPEG_PID,
+    parentPid: MAIN_PID,
+    params: '',
+    name: 'ffmpeg',
+  },
+  {
+    pid: OTHER_PID,
+    parentPid: MAIN_PID,
+    params: '',
+    name: 'foo',
+  },
+  {
+    pid: ANOTHER_PID,
+    parentPid: MAIN_PID,
+    params: '',
+    name: 'bar',
+  },
+  {
+    pid: SHARED_GPU_PID,
+    parentPid: MAIN_PID,
+    params: '--type=gpu-process',
+    name: 'Cypress',
+  },
+  {
+    pid: SHARED_BROKER_PID,
+    parentPid: MAIN_PID,
+    params: '--type=broker',
+    name: 'Cypress',
+  },
+  {
+    pid: SHARED_UTILITY_PID,
+    parentPid: MAIN_PID,
+    params: '--type=utility',
+    name: 'Cypress',
+  },
+  {
+    pid: SHARED_ZYGOTE_PID,
+    parentPid: MAIN_PID,
+    params: '--type=zygote',
+    name: 'Cypress',
+  },
+]
+
+describe('lib/util/process_profiler', function () {
+  context('._groupCyProcesses', () => {
+    it('groups correctly', () => {
+      sinon.stub(browsers, 'getBrowserPid').returns(BROWSER_PID)
+      sinon.stub(plugins, 'getPluginPid').returns(PLUGIN_PID)
+      sinon.stub(videoCapture, 'getFfmpegPid').returns(FFMPEG_PID)
+
+      // @ts-ignore
+      const groupedProcesses = _groupCyProcesses({ list: PROCESSES })
+
+      const checkGroup = (pid, group) => {
+        expect(_.find(groupedProcesses, { pid }))
+        .to.have.property('group')
+        .eq(group)
+      }
+
+      checkGroup(BROWSER_PID, 'browser')
+      checkGroup(SUB_BROWSER_PID, 'browser')
+      checkGroup(GUI_PID, 'desktop-gui')
+      checkGroup(PLUGIN_PID, 'plugin')
+      checkGroup(SUB_PLUGIN_PID, 'plugin')
+      checkGroup(FFMPEG_PID, 'ffmpeg')
+      checkGroup(MAIN_PID, 'cypress')
+      checkGroup(OTHER_PID, 'other')
+      checkGroup(ANOTHER_PID, 'other')
+      checkGroup(SHARED_GPU_PID, 'electron-shared')
+      checkGroup(SHARED_BROKER_PID, 'electron-shared')
+      checkGroup(SHARED_UTILITY_PID, 'electron-shared')
+      checkGroup(SHARED_ZYGOTE_PID, 'electron-shared')
+    })
+  })
+})
