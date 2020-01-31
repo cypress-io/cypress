d8167721336d9bb2284696b57913bbe3a6f971cc
Zach Bloomquist <github@chary.us>
2020-01-31 12:29:47 -0500

    Add process profiler for debugging (#6171)
    * add process profiler for debugging
    
    * remove existing CYPRESS_MEMORY check
    
    * rename some columns, format PIDs a little nicer, start when debug is enabled or verbosedebug is enabled
    
    * use shared method for determining if this is the electron process running
    
    * finish adding enhanced grouping
    
    * cleanup
    
    * add tests for changes to other parts of cypress
    
    * add tests for process_profiler
    
    * use browser displayname to group
    
    * fix ffmpeg and add tests
    
    Co-authored-by: Brian Mann <brian.mann86@gmail.com>

---
+++ packages/server/__snapshots__/process_profiler_spec.ts.js
@@ -0,0 +1,82 @@
+exports['lib/util/process_profiler ._aggregateGroups aggregates groups as expected 1'] = [
+  {
+    "group": "electron-shared",
+    "processCount": 4,
+    "pids": "99991, 99990, 99992, 99993",
+    "cpuPercent": 80,
+    "memRssMb": 40,
+    "meanCpuPercent": 80,
+    "meanMemRssMb": 40,
+    "maxMemRssMb": 40
+  },
+  {
+    "group": "other",
+    "processCount": 2,
+    "pids": "66666, 88888",
+    "cpuPercent": 40,
+    "memRssMb": 20,
+    "meanCpuPercent": 40,
+    "meanMemRssMb": 20,
+    "maxMemRssMb": 20
+  },
+  {
+    "group": "plugin",
+    "processCount": 2,
+    "pids": "22222, 22223",
+    "cpuPercent": 40,
+    "memRssMb": 20,
+    "meanCpuPercent": 40,
+    "meanMemRssMb": 20,
+    "maxMemRssMb": 20
+  },
+  {
+    "group": "browser",
+    "processCount": 2,
+    "pids": "11111, 11112",
+    "cpuPercent": 40,
+    "memRssMb": 20,
+    "meanCpuPercent": 40,
+    "meanMemRssMb": 20,
+    "maxMemRssMb": 20
+  },
+  {
+    "group": "ffmpeg",
+    "processCount": 1,
+    "pids": "33333",
+    "cpuPercent": 20,
+    "memRssMb": 10,
+    "meanCpuPercent": 20,
+    "meanMemRssMb": 10,
+    "maxMemRssMb": 10
+  },
+  {
+    "group": "desktop-gui",
+    "processCount": 1,
+    "pids": "77777",
+    "cpuPercent": 20,
+    "memRssMb": 10,
+    "meanCpuPercent": 20,
+    "meanMemRssMb": 10,
+    "maxMemRssMb": 10
+  },
+  {
+    "group": "cypress",
+    "processCount": 1,
+    "pids": "111111111",
+    "cpuPercent": 20,
+    "memRssMb": 10,
+    "meanCpuPercent": 20,
+    "meanMemRssMb": 10,
+    "maxMemRssMb": 10
+  },
+  {
+    "group": "TOTAL",
+    "processCount": 13,
+    "pids": "-",
+    "cpuPercent": 260,
+    "memRssMb": 130,
+    "meanCpuPercent": 260,
+    "meanMemRssMb": 130,
+    "maxMemRssMb": 130
+  }
+]
