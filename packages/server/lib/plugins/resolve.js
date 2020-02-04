1b16b45cc5c4f308beb18fdf914453c3d8a4e75c
KHeo <sainthkh@naver.com>
2020-02-04 17:31:37 +0900

    Added JSDoc comment.

--- packages/server/lib/plugins/resolve.js
+++ packages/server/lib/plugins/resolve.js
@@ -2,6 +2,12 @@ const resolve = require('resolve')
 const env = require('../util/env')
 
 module.exports = {
+  /**
+   * Resolves the path to 'typescript' module.
+   *
+   * @param {Config} cypress config object
+   * @returns {string|null} path if typescript exists, otherwise null
+   */
   typescript: (config) => {
     if (env.get('CYPRESS_INTERNAL_NO_TYPESCRIPT') === '1') {
       return null
