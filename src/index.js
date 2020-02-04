25236204f74a930d359be385c731539acafcb584
Patrick Mowrer <pmowrer@gmail.com>
2020-02-03 19:12:51 -0500

    fix: update plugin wrappers for 15.11+ compat
    BREAKING CHANGE: This plugin now only supports `semantic-release` 15.11 and above.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,27 @@
 const readPkg = require('read-pkg');
 const { compose } = require('ramda');
-const pluginDefinitions = require('semantic-release/lib/definitions/plugins');
 const withOnlyPackageCommits = require('./only-package-commits');
 const versionToGitTag = require('./version-to-git-tag');
 const logPluginVersion = require('./log-plugin-version');
-const {
-  wrapPlugin,
-  wrapMultiPlugin,
-} = require('semantic-release-plugin-decorators');
+const { wrapStep } = require('semantic-release-plugin-decorators');
 
 const {
   mapNextReleaseVersion,
   withOptionsTransforms,
 } = require('./options-transforms');
 
-const NAMESPACE = 'monorepo';
-
-const analyzeCommits = wrapPlugin(
-  NAMESPACE,
+const analyzeCommits = wrapStep(
   'analyzeCommits',
-  compose(logPluginVersion('analyzeCommits'), withOnlyPackageCommits),
-  pluginDefinitions.analyzeCommits.default
+  compose(logPluginVersion('analyzeCommits'), withOnlyPackageCommits)
 );
 
-const generateNotes = wrapMultiPlugin(
-  NAMESPACE,
+const generateNotes = wrapStep(
   'generateNotes',
   compose(
     logPluginVersion('generateNotes'),
     withOnlyPackageCommits,
     withOptionsTransforms([mapNextReleaseVersion(versionToGitTag)])
-  ),
-  pluginDefinitions.generateNotes.default
+  )
 );
 
 module.exports = {
