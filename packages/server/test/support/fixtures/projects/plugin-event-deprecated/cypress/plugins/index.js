e068c6dc0e092e31d2588a70ef9fb519bebd9833
Brian Mann <brian.mann86@gmail.com>
2020-02-04 04:00:02 -0500

    add more scenarios / return values

--- packages/server/test/support/fixtures/projects/plugin-event-deprecated/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/plugin-event-deprecated/cypress/plugins/index.js
@@ -12,8 +12,10 @@ module.exports = (on, config) => {
 
   if (config.env.CONCAT_RETURN) {
     on('before:browser:launch', (browser, options) => {
-      // this will emit a warning
+      // this will emit a warning but only once
       options = options.concat(['--foo'])
+      options.push('--foo=bar')
+      options.unshift('--load-extension=/foo/bar/baz.js')
 
       return options
     })
@@ -25,7 +27,7 @@ module.exports = (on, config) => {
     on('before:browser:launch', (browser, options) => {
       // this will NOT emit a warning
       options.args.push('--foo')
-      options.args.push('--bar')
+      options.args.unshift('--bar')
 
       return options
     })
@@ -38,6 +40,6 @@ module.exports = (on, config) => {
     options.push('--foo')
     options.push('--bar')
 
-    return options
+    return
   })
 }
