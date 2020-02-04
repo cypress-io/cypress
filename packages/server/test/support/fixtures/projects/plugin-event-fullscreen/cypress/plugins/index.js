b6e9c9ae8f0e1cd32f0f17ab5e94722916501039
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2020-02-04 16:34:09 -0500

    remove conditional args in fullscreen test

--- packages/server/test/support/fixtures/projects/plugin-event-fullscreen/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/plugin-event-fullscreen/cypress/plugins/index.js
@@ -5,9 +5,7 @@
  */
 module.exports = (on, config) => {
   on('before:browser:launch', (browser, options) => {
-    if (browser.family === 'chromium') {
-      options.args.push('--disable-gesture-requirement-for-presentation')
-    }
+    options.args.push('--disable-gesture-requirement-for-presentation')
 
     options.windowSize = 'fullscreen'
 
