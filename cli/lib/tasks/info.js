4b7781961e5d3627302c07d4e9027bb1b69261a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-07 15:44:49 -0500

    change to list

--- cli/lib/tasks/info.js
+++ cli/lib/tasks/info.js
@@ -1,13 +1,34 @@
 /* eslint-disable no-console */
 const launcher = require('@packages/launcher')
 const pluralize = require('pluralize')
+const { stripIndent } = require('common-tags')
+const { sortWith, ascend, prop } = require('ramda')
 
 const print = (browsers = []) => {
   console.log('Found %s', pluralize('local browser', browsers.length, true))
-  browsers.forEach((browser) => {
-    console.log('%s %s (as %s.%s) at %s', browser.displayName, browser.version,
-      browser.name, browser.channel, browser.path)
+  console.log('')
+
+  const sortByNameAndMajor = sortWith([
+    ascend(prop('name')),
+    ascend(prop('majorVersion')),
+  ])
+  const sortedByNameAndMajorVersion = sortByNameAndMajor(browsers)
+
+  sortedByNameAndMajorVersion.forEach((browser) => {
+    const text = stripIndent`
+      ${browser.displayName}
+        - Name: ${browser.name}
+        - Version: ${browser.version}
+        - Channel: ${browser.channel}
+        - Path: ${browser.path}
+    `
+
+    console.log(text)
+    console.log('')
   })
+
+  console.log('To use these browsers, pass their "Name" to the \'--browser\' field')
+  console.log('such as: `cypress run --browser firefox:dev` or `cypress run --browser chrome:canary`')
 }
 
 const start = () => {
