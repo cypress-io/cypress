bc02dab606fd7431a3948abb7d599deda012f26a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-07 15:22:17 -0500

    print found browsers

--- cli/lib/tasks/info.js
+++ cli/lib/tasks/info.js
@@ -1,8 +1,18 @@
+/* eslint-disable no-console */
 const launcher = require('@packages/launcher')
+const pluralize = require('pluralize')
+
+const print = (browsers = []) => {
+  console.log('Found %s', pluralize('local browser', browsers.length, true))
+  browsers.forEach((browser) => {
+    console.log('%s %s (as %s.%s) at %s', browser.displayName, browser.version,
+      browser.name, browser.channel, browser.path)
+  })
+}
+
 const start = () => {
   return launcher.detect()
-  /* eslint-disable-next-line no-console */
-  .then(console.log)
+  .then(print)
 }
 
 module.exports = {
