79b0a552b9c013d5797146eff3d90cb468bc36e4
Zach Bloomquist <github@chary.us>
2020-02-10 16:14:54 -0500

    move detectByPath specs to appropriate location

--- packages/launcher/test/unit/linux/spec.ts
+++ packages/launcher/test/unit/linux/spec.ts
@@ -1,26 +1,10 @@
 import * as linuxHelper from '../../../lib/linux'
 import { log } from '../../log'
-import { detect, detectByPath } from '../../../lib/detect'
+import { detect } from '../../../lib/detect'
+import { goalBrowsers } from '../../fixtures'
 import { expect } from 'chai'
 import execa from 'execa'
 import sinon, { SinonStub } from 'sinon'
-
-const goalBrowsers = [
-  {
-    displayName: 'Test Browser',
-    name: 'test-browser-name',
-    versionRegex: /test-browser v(\S+)$/,
-    profile: true,
-    binary: 'test-browser',
-  },
-  {
-    displayName: 'Foo Browser',
-    name: 'foo-browser',
-    versionRegex: /foo-browser v(\S+)$/,
-    profile: true,
-    binary: ['foo-browser', 'foo-bar-browser'],
-  },
-]
 
 describe('linux browser detection', () => {
   let stdout: SinonStub
@@ -37,17 +21,8 @@ describe('linux browser detection', () => {
     stdout.withArgs('foo-bar-browser', ['--version'])
     .resolves('foo-browser v100.1.2.3')
 
-    stdout.withArgs('/Applications/My Shiny New Browser.app', ['--version'])
-    .resolves('foo-browser v100.1.2.3')
-
     stdout.withArgs('/foo/bar/browser', ['--version'])
     .resolves('foo-browser v9001.1.2.3')
-
-    stdout.withArgs('/not/a/browser', ['--version'])
-    .resolves('not a browser version string')
-
-    stdout.withArgs('/not/a/real/path', ['--version'])
-    .rejects()
   })
 
   afterEach(() => {
@@ -121,68 +96,6 @@ describe('linux browser detection', () => {
       log('Browsers: %o', browsers)
       log('Expected browsers: %o', expected)
       expect(browsers).to.deep.equal(expected)
-    })
-  })
-
-  describe('detectByPath', () => {
-    it('detects by path', () => {
-      // @ts-ignore
-      return detectByPath('/foo/bar/browser', goalBrowsers)
-      .then((browser) => {
-        expect(browser).to.deep.equal(
-          Object.assign({}, goalBrowsers.find((gb) => {
-            return gb.name === 'foo-browser'
-          }), {
-            displayName: 'Custom Foo Browser',
-            info: 'Loaded from /foo/bar/browser',
-            custom: true,
-            version: '9001.1.2.3',
-            majorVersion: 9001,
-            path: '/foo/bar/browser',
-          })
-        )
-      })
-    })
-
-    it('rejects when there was no matching versionRegex', () => {
-      // @ts-ignore
-      return detectByPath('/not/a/browser', goalBrowsers)
-      .then(() => {
-        throw Error('Should not find a browser')
-      })
-      .catch((err) => {
-        expect(err.notDetectedAtPath).to.be.true
-      })
-    })
-
-    it('rejects when there was an error executing the command', () => {
-      // @ts-ignore
-      return detectByPath('/not/a/real/path', goalBrowsers)
-      .then(() => {
-        throw Error('Should not find a browser')
-      })
-      .catch((err) => {
-        expect(err.notDetectedAtPath).to.be.true
-      })
-    })
-
-    it('works with spaces in the path', () => {
-      // @ts-ignore
-      return detectByPath('/Applications/My Shiny New Browser.app', goalBrowsers)
-      .then((browser) => {
-        expect(browser).to.deep.equal(
-          Object.assign({}, goalBrowsers.find((gb) => {
-            return gb.name === 'foo-browser'
-          }), {
-            displayName: 'Custom Foo Browser',
-            info: 'Loaded from /Applications/My Shiny New Browser.app',
-            custom: true,
-            version: '100.1.2.3',
-            majorVersion: 100,
-            path: '/Applications/My Shiny New Browser.app',
-          })
-        )
-      })
     })
   })
 })
