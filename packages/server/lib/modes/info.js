48e3be3b6d0e20c409a6338e73a242fbc41e3b72
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-10 14:09:27 -0500

    pick real browsers as examples

--- packages/server/lib/modes/info.js
+++ packages/server/lib/modes/info.js
@@ -5,9 +5,32 @@ const { stripIndent } = require('common-tags')
 const { sortWith, ascend, prop } = require('ramda')
 const appData = require('../util/app_data')
 const browserUtils = require('../browsers/utils')
+const _ = require('lodash')
+
+/**
+ * If the list has at least 1 item, picks a random item
+ * and returns it AND the remaining items.
+ */
+const pickRandomItem = (list) => {
+  if (!list.length) {
+    return {
+      item: null,
+      remaining: list,
+    }
+  }
+
+  const item = list[_.random(0, list.length, false)]
+  const remaining = _.without(list, item)
+
+  return {
+    item, remaining,
+  }
+}
 
 const print = (browsers = []) => {
-  console.log('Found %s', pluralize('local browser', browsers.length, true))
+  console.log('Displaying Cypress info...')
+  console.log('')
+  console.log('Detected %s installed', pluralize('Browser', browsers.length, true))
   console.log('')
 
   const sortByNameAndMajor = sortWith([
@@ -16,10 +39,10 @@ const print = (browsers = []) => {
   ])
   const sortedByNameAndMajorVersion = sortByNameAndMajor(browsers)
 
-  sortedByNameAndMajorVersion.forEach((browser) => {
+  sortedByNameAndMajorVersion.forEach((browser, k) => {
     const profilePath = browserUtils.getBrowserPath(browser)
     const text = stripIndent`
-      ${browser.displayName}
+      ${k + 1}. ${browser.displayName}
         - Name: ${browser.name}
         - Version: ${browser.version}
         - Channel: ${browser.channel}
@@ -31,9 +54,26 @@ const print = (browsers = []) => {
     console.log('')
   })
 
-  console.log('To use these browsers, pass their "Name" to the \'--browser\' field')
-  console.log('such as: `cypress run --browser firefox:dev` or `cypress run --browser chrome:canary`')
-  console.log('')
+  // randomly a few detected browsers to use as examples
+  if (browsers.length) {
+    console.log('Note: To use these browsers, pass their name and channel to the \'--browser\' field')
+    console.log('')
+
+    const firstRandom = pickRandomItem(browsers)
+
+    if (firstRandom.item) {
+      console.log('Examples:')
+      console.log(`- cypress run --browser ${firstRandom.item.name}:${firstRandom.item.channel}`)
+
+      const secondRandom = pickRandomItem(firstRandom.remaining)
+
+      if (secondRandom.item) {
+        console.log(`- cypress run --browser ${secondRandom.item.name}:${secondRandom.item.channel}`)
+      }
+
+      console.log('')
+    }
+  }
 
   console.log('Application data stored in folder: %s', appData.path())
 }
