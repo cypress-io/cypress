784ff4f85cf6922c224d54931dac0c20053eb58d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-10 13:35:18 -0500

    add browser profile path

--- packages/server/lib/modes/info.js
+++ packages/server/lib/modes/info.js
@@ -3,6 +3,8 @@ const launcher = require('@packages/launcher')
 const pluralize = require('pluralize')
 const { stripIndent } = require('common-tags')
 const { sortWith, ascend, prop } = require('ramda')
+const appData = require('../util/app_data')
+const browserUtils = require('../browsers/utils')
 
 const print = (browsers = []) => {
   console.log('Found %s', pluralize('local browser', browsers.length, true))
@@ -15,12 +17,14 @@ const print = (browsers = []) => {
   const sortedByNameAndMajorVersion = sortByNameAndMajor(browsers)
 
   sortedByNameAndMajorVersion.forEach((browser) => {
+    const profilePath = browserUtils.getProfileDir(browser)
     const text = stripIndent`
       ${browser.displayName}
         - Name: ${browser.name}
         - Version: ${browser.version}
         - Channel: ${browser.channel}
         - Path: ${browser.path}
+        - Profile path: ${profilePath}
     `
 
     console.log(text)
@@ -29,6 +33,9 @@ const print = (browsers = []) => {
 
   console.log('To use these browsers, pass their "Name" to the \'--browser\' field')
   console.log('such as: `cypress run --browser firefox:dev` or `cypress run --browser chrome:canary`')
+  console.log('')
+
+  console.log('Application data stored in folder: %s', appData.path())
 }
 
 const info = () => {
