4aef63fa0cf7bcb430864c8a5ac55004c807a6e8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-10 13:13:48 -0500

    add binary cache path to info command

--- cli/lib/exec/info.js
+++ cli/lib/exec/info.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const spawn = require('./spawn')
 const util = require('../util')
+const state = require('../tasks/state')
 
 const start = (options = {}) => {
   const args = ['--mode=info']
@@ -11,6 +12,11 @@ const start = (options = {}) => {
     console.log()
 
     return util.getPlatformInfo().then(console.log)
+  })
+  .then(() => {
+    console.log()
+
+    console.log('Cypress binary cached in folder: %s', state.getCacheDir())
   })
 }
 
