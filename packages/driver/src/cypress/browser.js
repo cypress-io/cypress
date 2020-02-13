f2f06be60b9cf7ee7dae1687cd2a263efdbc8253
decaffeinate <14625260+Bkucera@users.noreply.github.com>
2020-02-13 13:13:50 -0500

    decaffeinate: Convert browser.coffee to JS

--- packages/driver/src/cypress/browser.js
+++ packages/driver/src/cypress/browser.js
@@ -1,22 +1,29 @@
-_ = require("lodash")
-$utils = require("./utils")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const _ = require("lodash");
+const $utils = require("./utils");
 
-isBrowser = (config, obj='') ->
-  if _.isString(obj)
-    name = obj.toLowerCase()
-    currentName = config.browser.name.toLowerCase()
+const isBrowser = function(config, obj='') {
+  if (_.isString(obj)) {
+    const name = obj.toLowerCase();
+    const currentName = config.browser.name.toLowerCase();
 
-    return name == currentName
-
-  if _.isObject(obj)
-    return _.isMatch(config.browser, obj)
-
-  $utils.throwErrByPath("browser.invalid_arg", {
-    args: { method: 'isBrowser', obj: $utils.stringify(obj) }
-  })
-
-module.exports = (config) ->
-  {
-    browser: config.browser
-    isBrowser: _.partial(isBrowser, config)
+    return name === currentName;
   }
+
+  if (_.isObject(obj)) {
+    return _.isMatch(config.browser, obj);
+  }
+
+  return $utils.throwErrByPath("browser.invalid_arg", {
+    args: { method: 'isBrowser', obj: $utils.stringify(obj) }
+  });
+};
+
+module.exports = config => ({
+  browser: config.browser,
+  isBrowser: _.partial(isBrowser, config)
+});
