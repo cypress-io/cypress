ffd3aa38df44e26b7c036d23a75003d3ac2c8afa
Chris Breiding <chrisbreiding@gmail.com>
2020-02-19 10:42:24 -0500

    update more snapshots

--- packages/server/__snapshots__/4_return_value_spec.coffee.js
+++ packages/server/__snapshots__/4_return_value_spec.coffee.js
@@ -33,11 +33,12 @@ The return value was:
 
 Because cy commands are asynchronous and are queued to be run later, it doesn't make sense to return anything else.
 
-For convenience, you can also simply omit any return value or return 'undefined' and Cypress will not error.
+For convenience, you can also simply omit any return value or return \`undefined\` and Cypress will not error.
 
 In previous versions of Cypress we automatically detected this and forced the cy commands to be returned. To make things less magical and clearer, we are now throwing an error.
 
 https://on.cypress.io/returning-value-and-commands-in-test
+
       [stack trace lines]
 
   2)  errors when invoking commands in custom command and returning different value:
@@ -45,7 +46,7 @@ https://on.cypress.io/returning-value-and-commands-in-test
 
 The custom command was:
 
-  > cy.foo()
+  > \`cy.foo()\`
 
 The return value was:
 
@@ -53,19 +54,16 @@ The return value was:
 
 Because cy commands are asynchronous and are queued to be run later, it doesn't make sense to return anything else.
 
-For convenience, you can also simply omit any return value or return 'undefined' and Cypress will not error.
+For convenience, you can also simply omit any return value or return \`undefined\` and Cypress will not error.
 
 In previous versions of Cypress we automatically detected this and forced the cy commands to be returned. To make things less magical and clearer, we are now throwing an error.
 
 https://on.cypress.io/returning-value-and-commands-in-custom-command
+
       [stack trace lines]
 
   3)  errors when not invoking commands, invoking done callback, and returning a promise:
      Error: Cypress detected that you returned a promise in a test, but also invoked a done callback. Return a promise -or- invoke a done callback, not both.
-
-Read  more here: https://on.cypress.io/returning-promise-and-invoking-done-callback
-
------------------------------------------------------------
 
 Original mocha error:
 
