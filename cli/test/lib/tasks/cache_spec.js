26e65337f1d1f925bca2fc8332ce1e8d6393f6b2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-20 18:15:54 -0500

    start trying to save HTML but only after snapshot text is confirmed

--- cli/test/lib/tasks/cache_spec.js
+++ cli/test/lib/tasks/cache_spec.js
@@ -9,6 +9,10 @@ const stdout = require('../../support/stdout')
 const snapshot = require('../../support/snapshot')
 const moment = require('moment')
 const stripAnsi = require('strip-ansi')
+const path = require('path')
+const termToHtml = require('term-to-html')
+
+const outputHtmlFolder = path.join(__dirname, '..', '..', 'html')
 
 describe('lib/tasks/cache', () => {
   beforeEach(() => {
@@ -28,19 +32,39 @@ describe('lib/tasks/cache', () => {
     this.stdout = stdout.capture()
   })
 
-  afterEach(() => {
-    mockfs.restore()
-    this.stdout = this.stdout.toString().split('\n').slice(0, -2).join('\n')
+  const getSnapshotText = () => {
+    this.stdout = this.stdout.toString().split('\n').slice(0, -1).join('\n')
     const stdoutAsString = this.stdout.toString() || '[no output]'
 
-    snapshot(stripAnsi(stdoutAsString))
+    // first restore the STDOUT, then confirm the value
+    // otherwise the error might not even appear or appear twice!
     stdout.restore()
+
+    return stdoutAsString
+  }
+
+  const saveHtml = async (filename, html) => {
+    await fs.ensureDirAsync(outputHtmlFolder)
+    const htmlFilename = path.join(outputHtmlFolder, filename)
+
+    await fs.writeFileAsync(htmlFilename, html, 'utf8')
+  }
+
+  afterEach(() => {
+    mockfs.restore()
   })
+
+  const defaultSnapshot = () => {
+    const stdoutAsString = getSnapshotText()
+
+    snapshot(stripAnsi(stdoutAsString))
+  }
 
   describe('.path', () => {
     it('lists path to cache', () => {
       cache.path()
       expect(this.stdout.toString()).to.eql('/.cache/Cypress\n')
+      defaultSnapshot()
     })
   })
 
@@ -50,21 +74,24 @@ describe('lib/tasks/cache', () => {
       .then(() => {
         return fs.pathExistsAsync('/.cache/Cypress')
         .then((exists) => {
-          return expect(exists).to.eql(false)
+          expect(exists).to.eql(false)
+          defaultSnapshot()
         })
       })
     })
   })
 
   describe('.list', () => {
-    it('lists all versions of cached binary', () => {
+    it('lists all versions of cached binary', async function () {
       // unknown access times
       sinon.stub(state, 'getPathToExecutable').returns('/.cache/Cypress/1.2.3/app/cypress')
 
-      return cache.list()
+      await cache.list()
+
+      defaultSnapshot()
     })
 
-    it('lists all versions of cached binary with last access', () => {
+    it('lists all versions of cached binary with last access', async function () {
       sinon.stub(state, 'getPathToExecutable').returns('/.cache/Cypress/1.2.3/app/cypress')
 
       const statAsync = sinon.stub(fs, 'statAsync')
@@ -77,7 +104,16 @@ describe('lib/tasks/cache', () => {
         atime: moment().subtract(5, 'day').valueOf(),
       })
 
-      return cache.list()
+      await cache.list()
+
+      const stdoutAsString = getSnapshotText()
+
+      snapshot(stripAnsi(stdoutAsString))
+
+      // if the sanitized snapshot matches, let's save the ANSI colors converted into HTML
+      const html = termToHtml.strings(stdoutAsString, termToHtml.themes.dark.name)
+
+      await saveHtml('list-of-versions.html', html)
     })
   })
 })
