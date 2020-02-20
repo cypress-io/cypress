900286b1fb76437d8274cff60b45baaac598db05
decaffeinate <github@chary.us>
2020-02-20 12:04:58 -0500

    decaffeinate: Run post-processing cleanups on ca.coffee and 11 other files

--- packages/https-proxy/test/helpers/http_server.js
+++ packages/https-proxy/test/helpers/http_server.js
@@ -1,31 +1,42 @@
+/* eslint-disable
+    no-console,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const http      = require("http");
-const Promise   = require("bluebird");
+const http = require('http')
+const Promise = require('bluebird')
 
-const srv = http.createServer(function(req, res) {
-  console.log("HTTP SERVER REQUEST URL:", req.url);
-  console.log("HTTP SERVER REQUEST HEADERS:", req.headers);
+const srv = http.createServer((req, res) => {
+  console.log('HTTP SERVER REQUEST URL:', req.url)
+  console.log('HTTP SERVER REQUEST HEADERS:', req.headers)
 
-  res.setHeader("Content-Type", "text/html");
-  res.writeHead(200);
-  return res.end("<html><body>http server</body></html>");
-});
+  res.setHeader('Content-Type', 'text/html')
+  res.writeHead(200)
+
+  return res.end('<html><body>http server</body></html>')
+})
 
 module.exports = {
   srv,
 
-  start() {
-    return new Promise(resolve => srv.listen(8080, function() {
-      console.log("server listening on port: 8080");
-      return resolve(srv);
-    }));
+  start () {
+    return new Promise((resolve) => {
+      return srv.listen(8080, () => {
+        console.log('server listening on port: 8080')
+
+        return resolve(srv)
+      })
+    })
   },
 
-  stop() {
-    return new Promise(resolve => srv.close(resolve));
-  }
-};
+  stop () {
+    return new Promise((resolve) => {
+      return srv.close(resolve)
+    })
+  },
+}
--- packages/https-proxy/test/unit/ca_spec.js
+++ packages/https-proxy/test/unit/ca_spec.js
@@ -1,104 +1,110 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-require("../spec_helper");
+require('../spec_helper')
 
-let fs      = require("fs-extra");
-const path    = require("path");
-const Promise = require("bluebird");
-const CA      = require("../../lib/ca");
+let fs = require('fs-extra')
+const path = require('path')
+const Promise = require('bluebird')
+const CA = require('../../lib/ca')
 
-fs = Promise.promisifyAll(fs);
+fs = Promise.promisifyAll(fs)
 
-describe("lib/ca", function() {
-  beforeEach(function() {
-    this.timeout(5000);
+describe('lib/ca', () => {
+  beforeEach(function () {
+    this.timeout(5000)
 
-    this.dir = path.join(process.cwd(), "tmp");
+    this.dir = path.join(process.cwd(), 'tmp')
 
     return fs.ensureDirAsync(this.dir)
     .then(() => {
-      return CA.create(this.dir);
-  }).then(ca => {
-      this.ca = ca;
-      
-    });
-  });
+      return CA.create(this.dir)
+    }).then((ca) => {
+      this.ca = ca
+    })
+  })
 
-  afterEach(function() {
-    return fs.removeAsync(this.dir);
-  });
+  afterEach(function () {
+    return fs.removeAsync(this.dir)
+  })
 
-  context("#generateServerCertificateKeys", () => it("generates certs for each host", function() {
-    return this.ca.generateServerCertificateKeys("www.cypress.io")
-    .spread(function(certPem, keyPrivatePem) {
-      expect(certPem).to.include("-----BEGIN CERTIFICATE-----");
-      return expect(keyPrivatePem).to.include("-----BEGIN RSA PRIVATE KEY-----");
-    });
-  }));
+  context('#generateServerCertificateKeys', () => {
+    it('generates certs for each host', function () {
+      return this.ca.generateServerCertificateKeys('www.cypress.io')
+      .spread((certPem, keyPrivatePem) => {
+        expect(certPem).to.include('-----BEGIN CERTIFICATE-----')
 
-  return context(".create", function() {
-    it("returns a new CA instance", function() {
-      return expect(this.ca).to.be.an.instanceof(CA);
-    });
+        expect(keyPrivatePem).to.include('-----BEGIN RSA PRIVATE KEY-----')
+      })
+    })
+  })
 
-    it("creates certs + keys dir", function() {
+  context('.create', () => {
+    it('returns a new CA instance', function () {
+      expect(this.ca).to.be.an.instanceof(CA)
+    })
+
+    it('creates certs + keys dir', function () {
       return Promise.join(
-        fs.statAsync(path.join(this.dir, "certs")),
-        fs.statAsync(path.join(this.dir, "keys"))
-      );
-    });
+        fs.statAsync(path.join(this.dir, 'certs')),
+        fs.statAsync(path.join(this.dir, 'keys'))
+      )
+    })
 
-    it("writes certs/ca.pem", function() {
-      return fs.statAsync(path.join(this.dir, "certs", "ca.pem"));
-    });
+    it('writes certs/ca.pem', function () {
+      return fs.statAsync(path.join(this.dir, 'certs', 'ca.pem'))
+    })
 
-    it("writes keys/ca.private.key", function() {
-      return fs.statAsync(path.join(this.dir, "keys", "ca.private.key"));
-    });
+    it('writes keys/ca.private.key', function () {
+      return fs.statAsync(path.join(this.dir, 'keys', 'ca.private.key'))
+    })
 
-    it("writes keys/ca.public.key", function() {
-      return fs.statAsync(path.join(this.dir, "keys", "ca.public.key"));
-    });
+    it('writes keys/ca.public.key', function () {
+      return fs.statAsync(path.join(this.dir, 'keys', 'ca.public.key'))
+    })
 
-    it("sets ca.CAcert", function() {
-      return expect(this.ca.CAcert).to.be.an("object");
-    });
+    it('sets ca.CAcert', function () {
+      expect(this.ca.CAcert).to.be.an('object')
+    })
 
-    it("sets ca.CAkeys", function() {
-      expect(this.ca.CAkeys).to.be.an("object");
-      expect(this.ca.CAkeys).to.have.a.property("privateKey");
-      return expect(this.ca.CAkeys).to.have.a.property("publicKey");
-    });
+    it('sets ca.CAkeys', function () {
+      expect(this.ca.CAkeys).to.be.an('object')
+      expect(this.ca.CAkeys).to.have.a.property('privateKey')
 
-    return describe("existing CA folder", function() {
-      beforeEach(function() {
-        this.sandbox.spy(CA.prototype, "loadCA");
-        this.sandbox.spy(CA.prototype, "generateCA");
+      expect(this.ca.CAkeys).to.have.a.property('publicKey')
+    })
+
+    describe('existing CA folder', () => {
+      beforeEach(function () {
+        this.sandbox.spy(CA.prototype, 'loadCA')
+        this.sandbox.spy(CA.prototype, 'generateCA')
 
         return CA.create(this.dir)
-        .then(ca2 => {
-          this.ca2 = ca2;
-          
-      });
-      });
+        .then((ca2) => {
+          this.ca2 = ca2
+        })
+      })
 
-      it("calls loadCA and not generateCA", function() {
-        expect(CA.prototype.loadCA).to.be.calledOnce;
-        return expect(CA.prototype.generateCA).not.to.be.called;
-      });
+      it('calls loadCA and not generateCA', () => {
+        expect(CA.prototype.loadCA).to.be.calledOnce
 
-      it("sets ca.CAcert", function() {
-        return expect(this.ca2.CAcert).to.be.an("object");
-      });
+        expect(CA.prototype.generateCA).not.to.be.called
+      })
 
-      return it("sets ca.CAkeys", function() {
-        expect(this.ca2.CAkeys).to.be.an("object");
-        expect(this.ca2.CAkeys).to.have.a.property("privateKey");
-        return expect(this.ca2.CAkeys).to.have.a.property("publicKey");
-      });
-    });
-  });
-});
+      it('sets ca.CAcert', function () {
+        expect(this.ca2.CAcert).to.be.an('object')
+      })
+
+      it('sets ca.CAkeys', function () {
+        expect(this.ca2.CAkeys).to.be.an('object')
+        expect(this.ca2.CAkeys).to.have.a.property('privateKey')
+
+        expect(this.ca2.CAkeys).to.have.a.property('publicKey')
+      })
+    })
+  })
+})
