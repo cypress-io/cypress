bc4c29f07c29661f5a4a409696ec09008a50d175
Chris Breiding <chrisbreiding@gmail.com>
2020-02-25 09:55:00 -0500

    restore mistakenly deleted snapshot

---
+++ packages/server/__snapshots__/3_page_loading_spec.coffee.js
@@ -0,0 +1,62 @@
+exports['e2e page_loading / passes'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (page_loading_spec.coffee)                                                 │
+  │ Searched:   cypress/integration/page_loading_spec.coffee                                       │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  page_loading_spec.coffee                                                        (1 of 1)
+
+
+  page_loading
+    ✓ sets __cypress.initial, properly injects, and avoids json injection
+    issue #258: opener is undefined during snapshot
+      ✓ causes the xhr to be aborted while in flight
+
+
+  2 passing
+
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        2                                                                                │
+  │ Passing:      2                                                                                │
+  │ Failing:      0                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  0                                                                                │
+  │ Video:        true                                                                             │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     page_loading_spec.coffee                                                         │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+  (Video)
+
+  -  Started processing:  Compressing to 32 CRF                                                     
+  -  Finished processing: /XXX/XXX/XXX/cypress/videos/page_loading_spec.coffee.mp4        (X second)
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+       Spec                                              Tests  Passing  Failing  Pending  Skipped  
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✔  page_loading_spec.coffee                 XX:XX        2        2        -        -        - │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    ✔  All specs passed!                        XX:XX        2        2        -        -        -  
+
+
+`
