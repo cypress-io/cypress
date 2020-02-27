9f3dc4482be93a378a3bfc3be326ee85bf9a6990
Zach Bloomquist <github@chary.us>
2020-02-27 16:48:46 -0500

    Revert "also run appveyor"
    This reverts commit bfe7b0499a4ff6b0367d71f8c70a61df3bf186af.

--- scripts/win-appveyor-build.js
+++ scripts/win-appveyor-build.js
@@ -26,7 +26,7 @@ const isRightBranch = () => {
     process.env.APPVEYOR_REPO_COMMIT_MESSAGE || ''
   ).includes('[build binary]')
 
-  return branch === 'issue-6582-fix-patch-package' || shouldForceBinaryBuild
+  return branch === 'develop' || shouldForceBinaryBuild
 }
 
 const isForkedPullRequest = () => {
