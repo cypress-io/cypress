7b1cf618857618183f2ebc10426f4e87f87e4375
decaffeinate <clare.m.so+github@gmail.com>
2020-03-05 23:41:01 -0500

    decaffeinate: Convert browser_spec.coffee and 7 other files to JS

--- packages/driver/test/cypress/integration/cypress/screenshot_spec.js
+++ packages/driver/test/cypress/integration/cypress/screenshot_spec.js
@@ -1,175 +1,186 @@
-{ Screenshot, $ } = Cypress
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS205: Consider reworking code to avoid use of IIFEs
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const { Screenshot, $ } = Cypress;
 
-DEFAULTS = {
-  capture: "fullPage"
-  scale: false
-  disableTimersAndAnimations: true
-  screenshotOnRunFailure: true
+const DEFAULTS = {
+  capture: "fullPage",
+  scale: false,
+  disableTimersAndAnimations: true,
+  screenshotOnRunFailure: true,
   blackout: []
-}
+};
 
-describe "src/cypress/screenshot", ->
-  beforeEach ->
-    ## reset state since this is a singleton
-    Screenshot.reset()
+describe("src/cypress/screenshot", function() {
+  beforeEach(() => //# reset state since this is a singleton
+  Screenshot.reset());
 
-  it "has defaults", ->
-    expect(Screenshot.getConfig()).to.deep.eq(DEFAULTS)
-    expect(-> Screenshot.onBeforeScreenshot()).not.to.throw()
-    expect(-> Screenshot.onAfterScreenshot()).not.to.throw()
+  it("has defaults", function() {
+    expect(Screenshot.getConfig()).to.deep.eq(DEFAULTS);
+    expect(() => Screenshot.onBeforeScreenshot()).not.to.throw();
+    return expect(() => Screenshot.onAfterScreenshot()).not.to.throw();
+  });
 
-  context ".getConfig", ->
-    it "returns copy of config", ->
-      config = Screenshot.getConfig()
-      config.blackout.push(".foo")
-      expect(Screenshot.getConfig().blackout).to.deep.eq(DEFAULTS.blackout)
+  context(".getConfig", () => it("returns copy of config", function() {
+    const config = Screenshot.getConfig();
+    config.blackout.push(".foo");
+    return expect(Screenshot.getConfig().blackout).to.deep.eq(DEFAULTS.blackout);
+  }));
 
-  context ".defaults", ->
-    it "is noop if not called with any valid properties", ->
-      Screenshot.defaults({})
-      expect(Screenshot.getConfig()).to.deep.eq(DEFAULTS)
-      expect(-> Screenshot.onBeforeScreenshot()).not.to.throw()
-      expect(-> Screenshot.onAfterScreenshot()).not.to.throw()
+  return context(".defaults", function() {
+    it("is noop if not called with any valid properties", function() {
+      Screenshot.defaults({});
+      expect(Screenshot.getConfig()).to.deep.eq(DEFAULTS);
+      expect(() => Screenshot.onBeforeScreenshot()).not.to.throw();
+      return expect(() => Screenshot.onAfterScreenshot()).not.to.throw();
+    });
 
-    it "sets capture if specified", ->
+    it("sets capture if specified", function() {
       Screenshot.defaults({
         capture: "runner"
-      })
-      expect(Screenshot.getConfig().capture).to.eql("runner")
+      });
+      return expect(Screenshot.getConfig().capture).to.eql("runner");
+    });
 
-    it "sets scale if specified", ->
+    it("sets scale if specified", function() {
       Screenshot.defaults({
         scale: true
-      })
-      expect(Screenshot.getConfig().scale).to.equal(true)
+      });
+      return expect(Screenshot.getConfig().scale).to.equal(true);
+    });
 
-    it "sets disableTimersAndAnimations if specified", ->
+    it("sets disableTimersAndAnimations if specified", function() {
       Screenshot.defaults({
         disableTimersAndAnimations: false
-      })
-      expect(Screenshot.getConfig().disableTimersAndAnimations).to.equal(false)
+      });
+      return expect(Screenshot.getConfig().disableTimersAndAnimations).to.equal(false);
+    });
 
-    it "sets screenshotOnRunFailure if specified", ->
+    it("sets screenshotOnRunFailure if specified", function() {
       Screenshot.defaults({
         screenshotOnRunFailure: false
-      })
-      expect(Screenshot.getConfig().screenshotOnRunFailure).to.equal(false)
+      });
+      return expect(Screenshot.getConfig().screenshotOnRunFailure).to.equal(false);
+    });
 
-    it "sets clip if specified", ->
+    it("sets clip if specified", function() {
       Screenshot.defaults({
         clip: { width: 200, height: 100, x: 0, y: 0 }
-      })
-      expect(
+      });
+      return expect(
         Screenshot.getConfig().clip
       ).to.eql(
         { width: 200, height: 100, x: 0, y:0 }
-      )
+      );
+    });
 
-    it "sets and normalizes padding if specified", ->
-      tests = [
-        [ 50, [50, 50, 50, 50] ]
-        [ [15], [15, 15, 15, 15] ]
-        [ [30, 20], [30, 20, 30, 20] ]
-        [ [10, 20, 30], [10, 20, 30, 20] ]
+    it("sets and normalizes padding if specified", function() {
+      const tests = [
+        [ 50, [50, 50, 50, 50] ],
+        [ [15], [15, 15, 15, 15] ],
+        [ [30, 20], [30, 20, 30, 20] ],
+        [ [10, 20, 30], [10, 20, 30, 20] ],
         [ [20, 10, 20, 10], [20, 10, 20, 10] ]
-      ]
+      ];
 
-      for test in tests
-        [ input, expected ] = test
-        Screenshot.defaults({
-          padding: input
-        })
-        expect(
-          Screenshot.getConfig().padding
-        ).to.eql(
-          expected
-        )
+      return (() => {
+        const result = [];
+        for (let test of tests) {
+          const [ input, expected ] = test;
+          Screenshot.defaults({
+            padding: input
+          });
+          result.push(expect(
+            Screenshot.getConfig().padding
+          ).to.eql(
+            expected
+          ));
+        }
+        return result;
+      })();
+    });
 
-    it "sets onBeforeScreenshot if specified", ->
-      onBeforeScreenshot = cy.stub()
-      Screenshot.defaults({ onBeforeScreenshot })
-      Screenshot.onBeforeScreenshot()
-      expect(onBeforeScreenshot).to.be.called
+    it("sets onBeforeScreenshot if specified", function() {
+      const onBeforeScreenshot = cy.stub();
+      Screenshot.defaults({ onBeforeScreenshot });
+      Screenshot.onBeforeScreenshot();
+      return expect(onBeforeScreenshot).to.be.called;
+    });
 
-    it "sets onAfterScreenshot if specified", ->
-      onAfterScreenshot = cy.stub()
-      Screenshot.defaults({ onAfterScreenshot })
-      Screenshot.onAfterScreenshot()
-      expect(onAfterScreenshot).to.be.called
+    it("sets onAfterScreenshot if specified", function() {
+      const onAfterScreenshot = cy.stub();
+      Screenshot.defaults({ onAfterScreenshot });
+      Screenshot.onAfterScreenshot();
+      return expect(onAfterScreenshot).to.be.called;
+    });
 
-    describe "errors", ->
-      it "throws if not passed an object", ->
-        expect =>
-          Screenshot.defaults()
-        .to.throw("Cypress.Screenshot.defaults() must be called with an object. You passed: ")
+    return describe("errors", function() {
+      it("throws if not passed an object", () => expect(() => {
+        return Screenshot.defaults();
+    }).to.throw("Cypress.Screenshot.defaults() must be called with an object. You passed: "));
 
-      it "throws if capture is not a string", ->
-        expect =>
-          Screenshot.defaults({ capture: true })
-        .to.throw("Cypress.Screenshot.defaults() 'capture' option must be one of the following: 'fullPage', 'viewport', or 'runner'. You passed: true")
+      it("throws if capture is not a string", () => expect(() => {
+        return Screenshot.defaults({ capture: true });
+    }).to.throw("Cypress.Screenshot.defaults() 'capture' option must be one of the following: 'fullPage', 'viewport', or 'runner'. You passed: true"));
 
-      it "throws if capture is not a valid option", ->
-        expect =>
-          Screenshot.defaults({ capture: "foo" })
-        .to.throw("Cypress.Screenshot.defaults() 'capture' option must be one of the following: 'fullPage', 'viewport', or 'runner'. You passed: foo")
+      it("throws if capture is not a valid option", () => expect(() => {
+        return Screenshot.defaults({ capture: "foo" });
+    }).to.throw("Cypress.Screenshot.defaults() 'capture' option must be one of the following: 'fullPage', 'viewport', or 'runner'. You passed: foo"));
 
-      it "throws if scale is not a boolean", ->
-        expect =>
-          Screenshot.defaults({ scale: "foo" })
-        .to.throw("Cypress.Screenshot.defaults() 'scale' option must be a boolean. You passed: foo")
+      it("throws if scale is not a boolean", () => expect(() => {
+        return Screenshot.defaults({ scale: "foo" });
+    }).to.throw("Cypress.Screenshot.defaults() 'scale' option must be a boolean. You passed: foo"));
 
-      it "throws if disableTimersAndAnimations is not a boolean", ->
-        expect =>
-          Screenshot.defaults({ disableTimersAndAnimations: "foo" })
-        .to.throw("Cypress.Screenshot.defaults() 'disableTimersAndAnimations' option must be a boolean. You passed: foo")
+      it("throws if disableTimersAndAnimations is not a boolean", () => expect(() => {
+        return Screenshot.defaults({ disableTimersAndAnimations: "foo" });
+    }).to.throw("Cypress.Screenshot.defaults() 'disableTimersAndAnimations' option must be a boolean. You passed: foo"));
 
-      it "throws if screenshotOnRunFailure is not a boolean", ->
-        expect =>
-          Screenshot.defaults({ screenshotOnRunFailure: "foo" })
-        .to.throw("Cypress.Screenshot.defaults() 'screenshotOnRunFailure' option must be a boolean. You passed: foo")
+      it("throws if screenshotOnRunFailure is not a boolean", () => expect(() => {
+        return Screenshot.defaults({ screenshotOnRunFailure: "foo" });
+    }).to.throw("Cypress.Screenshot.defaults() 'screenshotOnRunFailure' option must be a boolean. You passed: foo"));
 
-      it "throws if blackout is not an array", ->
-        expect =>
-          Screenshot.defaults({ blackout: "foo" })
-        .to.throw("Cypress.Screenshot.defaults() 'blackout' option must be an array of strings. You passed: foo")
+      it("throws if blackout is not an array", () => expect(() => {
+        return Screenshot.defaults({ blackout: "foo" });
+    }).to.throw("Cypress.Screenshot.defaults() 'blackout' option must be an array of strings. You passed: foo"));
 
-      it "throws if blackout is not an array of strings", ->
-        expect =>
-          Screenshot.defaults({ blackout: [true] })
-        .to.throw("Cypress.Screenshot.defaults() 'blackout' option must be an array of strings. You passed: true")
+      it("throws if blackout is not an array of strings", () => expect(() => {
+        return Screenshot.defaults({ blackout: [true] });
+    }).to.throw("Cypress.Screenshot.defaults() 'blackout' option must be an array of strings. You passed: true"));
 
-      it "throws if padding is not a number or an array of numbers with a length between 1 and 4", ->
-        expect =>
-          Screenshot.defaults({ padding: '50px' })
-        .to.throw("Cypress.Screenshot.defaults() 'padding' option must be either a number or an array of numbers with a maximum length of 4. You passed: 50px")
-        expect =>
-          Screenshot.defaults({ padding: ['bad', 'bad', 'bad', 'bad'] })
-        .to.throw("Cypress.Screenshot.defaults() 'padding' option must be either a number or an array of numbers with a maximum length of 4. You passed: bad, bad, bad, bad")
-        expect =>
-          Screenshot.defaults({ padding: [20, 10, 20, 10, 50] })
-        .to.throw("Cypress.Screenshot.defaults() 'padding' option must be either a number or an array of numbers with a maximum length of 4. You passed: 20, 10, 20, 10, 50")
+      it("throws if padding is not a number or an array of numbers with a length between 1 and 4", function() {
+        expect(() => {
+          return Screenshot.defaults({ padding: '50px' });
+      }).to.throw("Cypress.Screenshot.defaults() 'padding' option must be either a number or an array of numbers with a maximum length of 4. You passed: 50px");
+        expect(() => {
+          return Screenshot.defaults({ padding: ['bad', 'bad', 'bad', 'bad'] });
+      }).to.throw("Cypress.Screenshot.defaults() 'padding' option must be either a number or an array of numbers with a maximum length of 4. You passed: bad, bad, bad, bad");
+        return expect(() => {
+          return Screenshot.defaults({ padding: [20, 10, 20, 10, 50] });
+      }).to.throw("Cypress.Screenshot.defaults() 'padding' option must be either a number or an array of numbers with a maximum length of 4. You passed: 20, 10, 20, 10, 50");
+      });
 
-      it "throws if clip is lacking proper keys", ->
-        expect =>
-          Screenshot.defaults({ clip: { x: 5 } })
-        .to.throw("Cypress.Screenshot.defaults() 'clip' option must be an object with the keys { width, height, x, y } and number values. You passed: {x: 5}")
+      it("throws if clip is lacking proper keys", () => expect(() => {
+        return Screenshot.defaults({ clip: { x: 5 } });
+    }).to.throw("Cypress.Screenshot.defaults() 'clip' option must be an object with the keys { width, height, x, y } and number values. You passed: {x: 5}"));
 
-      it "throws if clip has extraneous keys", ->
-        expect =>
-          Screenshot.defaults({ clip: { width: 100, height: 100, x: 5, y: 5, foo: 10 } })
-        .to.throw("Cypress.Screenshot.defaults() 'clip' option must be an object with the keys { width, height, x, y } and number values. You passed: Object{5}")
+      it("throws if clip has extraneous keys", () => expect(() => {
+        return Screenshot.defaults({ clip: { width: 100, height: 100, x: 5, y: 5, foo: 10 } });
+    }).to.throw("Cypress.Screenshot.defaults() 'clip' option must be an object with the keys { width, height, x, y } and number values. You passed: Object{5}"));
 
-      it "throws if clip has non-number values", ->
-        expect =>
-          Screenshot.defaults({ clip: { width: 100, height: 100, x: 5, y: "5" } })
-        .to.throw("Cypress.Screenshot.defaults() 'clip' option must be an object with the keys { width, height, x, y } and number values. You passed: Object{4}")
+      it("throws if clip has non-number values", () => expect(() => {
+        return Screenshot.defaults({ clip: { width: 100, height: 100, x: 5, y: "5" } });
+    }).to.throw("Cypress.Screenshot.defaults() 'clip' option must be an object with the keys { width, height, x, y } and number values. You passed: Object{4}"));
 
-      it "throws if onBeforeScreenshot is not a function", ->
-        expect =>
-          Screenshot.defaults({ onBeforeScreenshot: "foo" })
-        .to.throw("Cypress.Screenshot.defaults() 'onBeforeScreenshot' option must be a function. You passed: foo")
+      it("throws if onBeforeScreenshot is not a function", () => expect(() => {
+        return Screenshot.defaults({ onBeforeScreenshot: "foo" });
+    }).to.throw("Cypress.Screenshot.defaults() 'onBeforeScreenshot' option must be a function. You passed: foo"));
 
-      it "throws if onAfterScreenshot is not a function", ->
-        expect =>
-          Screenshot.defaults({ onAfterScreenshot: "foo" })
-        .to.throw("Cypress.Screenshot.defaults() 'onAfterScreenshot' option must be a function. You passed: foo")
+      return it("throws if onAfterScreenshot is not a function", () => expect(() => {
+        return Screenshot.defaults({ onAfterScreenshot: "foo" });
+    }).to.throw("Cypress.Screenshot.defaults() 'onAfterScreenshot' option must be a function. You passed: foo"));
+    });
+  });
+});
--- packages/driver/test/cypress/integration/cypress/selector_playground_spec.js
+++ packages/driver/test/cypress/integration/cypress/selector_playground_spec.js
@@ -1,74 +1,83 @@
-{ SelectorPlayground, $ } = Cypress
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const { SelectorPlayground, $ } = Cypress;
 
-SELECTOR_DEFAULTS = [
+const SELECTOR_DEFAULTS = [
   "data-cy", "data-test", "data-testid", "id", "class", "tag", "attributes", "nth-child"
-]
+];
 
-describe "src/cypress/selector_playground", ->
-  beforeEach ->
-    ## reset state since this is a singleton
-    SelectorPlayground.reset()
+describe("src/cypress/selector_playground", function() {
+  beforeEach(() => //# reset state since this is a singleton
+  SelectorPlayground.reset());
 
-  it "has defaults", ->
-    expect(SelectorPlayground.getSelectorPriority()).to.deep.eq(SELECTOR_DEFAULTS)
-    expect(SelectorPlayground.getOnElement()).to.be.null
+  it("has defaults", function() {
+    expect(SelectorPlayground.getSelectorPriority()).to.deep.eq(SELECTOR_DEFAULTS);
+    return expect(SelectorPlayground.getOnElement()).to.be.null;
+  });
 
-  context ".defaults", ->
-    it "is noop if not called with selectorPriority or onElement", ->
-      SelectorPlayground.defaults({})
-      expect(SelectorPlayground.getSelectorPriority()).to.deep.eq(SELECTOR_DEFAULTS)
-      expect(SelectorPlayground.getOnElement()).to.be.null
+  context(".defaults", function() {
+    it("is noop if not called with selectorPriority or onElement", function() {
+      SelectorPlayground.defaults({});
+      expect(SelectorPlayground.getSelectorPriority()).to.deep.eq(SELECTOR_DEFAULTS);
+      return expect(SelectorPlayground.getOnElement()).to.be.null;
+    });
 
-    it "sets selector:playground:priority if selectorPriority specified", ->
+    it("sets selector:playground:priority if selectorPriority specified", function() {
       SelectorPlayground.defaults({
         selectorPriority: ["foo"]
-      })
-      expect(SelectorPlayground.getSelectorPriority()).to.eql(["foo"])
+      });
+      return expect(SelectorPlayground.getSelectorPriority()).to.eql(["foo"]);
+    });
 
-    it "sets selector:playground:on:element if onElement specified", ->
-      onElement = ->
-      SelectorPlayground.defaults({ onElement })
-      expect(SelectorPlayground.getOnElement()).to.equal(onElement)
+    it("sets selector:playground:on:element if onElement specified", function() {
+      const onElement = function() {};
+      SelectorPlayground.defaults({ onElement });
+      return expect(SelectorPlayground.getOnElement()).to.equal(onElement);
+    });
 
-    it "throws if not passed an object", ->
-      expect =>
-        SelectorPlayground.defaults()
-      .to.throw("Cypress.SelectorPlayground.defaults() must be called with an object. You passed: ")
+    it("throws if not passed an object", () => expect(() => {
+      return SelectorPlayground.defaults();
+  }).to.throw("Cypress.SelectorPlayground.defaults() must be called with an object. You passed: "));
 
-    it "throws if selectorPriority is not an array", ->
-      expect =>
-        SelectorPlayground.defaults({ selectorPriority: "foo" })
-      .to.throw("Cypress.SelectorPlayground.defaults() called with invalid 'selectorPriority' property. It must be an array. You passed: foo")
+    it("throws if selectorPriority is not an array", () => expect(() => {
+      return SelectorPlayground.defaults({ selectorPriority: "foo" });
+  }).to.throw("Cypress.SelectorPlayground.defaults() called with invalid 'selectorPriority' property. It must be an array. You passed: foo"));
 
-    it "throws if onElement is not a function", ->
-      expect =>
-        SelectorPlayground.defaults({ onElement: "foo" })
-      .to.throw("Cypress.SelectorPlayground.defaults() called with invalid 'onElement' property. It must be a function. You passed: foo")
+    return it("throws if onElement is not a function", () => expect(() => {
+      return SelectorPlayground.defaults({ onElement: "foo" });
+  }).to.throw("Cypress.SelectorPlayground.defaults() called with invalid 'onElement' property. It must be a function. You passed: foo"));
+  });
 
-  context ".getSelector", ->
-    it "uses defaults.selectorPriority", ->
-      $div = $("<div data-cy='main button 123' data-foo-bar-baz='quux' data-test='qwerty' data-foo='bar' />")
+  return context(".getSelector", () => it("uses defaults.selectorPriority", function() {
+    const $div = $("<div data-cy='main button 123' data-foo-bar-baz='quux' data-test='qwerty' data-foo='bar' />");
 
-      Cypress.$("body").append($div)
+    Cypress.$("body").append($div);
 
-      expect(SelectorPlayground.getSelector($div)).to.eq("[data-cy=\"main button 123\"]")
+    expect(SelectorPlayground.getSelector($div)).to.eq("[data-cy=\"main button 123\"]");
 
-      SelectorPlayground.defaults({
-        selectorPriority: ['data-foo']
-      })
+    SelectorPlayground.defaults({
+      selectorPriority: ['data-foo']
+    });
 
-      expect(SelectorPlayground.getSelector($div)).to.eq("[data-foo=bar]")
+    expect(SelectorPlayground.getSelector($div)).to.eq("[data-foo=bar]");
 
-      SelectorPlayground.defaults({
-        onElement: ($el) ->
-          return "quux"
-      })
+    SelectorPlayground.defaults({
+      onElement($el) {
+        return "quux";
+      }
+    });
 
-      expect(SelectorPlayground.getSelector($div)).to.eq("quux")
+    expect(SelectorPlayground.getSelector($div)).to.eq("quux");
 
-      SelectorPlayground.defaults({
-        onElement: ($el) ->
-          return null
-      })
+    SelectorPlayground.defaults({
+      onElement($el) {
+        return null;
+      }
+    });
 
-      expect(SelectorPlayground.getSelector($div)).to.eq("[data-foo=bar]")
+    return expect(SelectorPlayground.getSelector($div)).to.eq("[data-foo=bar]");
+  }));
+});
