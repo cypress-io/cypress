7a09e3630e01e2cada4ffd37f63d829abc080b37
Zach Bloomquist <github@chary.us>
2020-03-10 14:32:36 -0400

    debug e2e tests

--- packages/server/test/support/helpers/e2e.js
+++ packages/server/test/support/helpers/e2e.js
@@ -677,6 +677,7 @@ const e2e = {
           // because we are piping the child process
           COLUMNS: 100,
           LINES: 24,
+          DEBUG: 'cypress:*',
         })
         .defaults({
           FAKE_CWD_PATH: '/XXX/XXX/XXX',
