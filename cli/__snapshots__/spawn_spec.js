fd01a8d8f08f71426f70d4c6c0899e39177996b2
Zach Bloomquist <github@chary.us>
2020-03-16 14:22:39 -0400

    readd spawn_spec

--- cli/__snapshots__/spawn_spec.js
+++ cli/__snapshots__/spawn_spec.js
@@ -16,3 +16,22 @@ exports['lib/exec/spawn .start does not force colors and streams when not suppor
   "FORCE_STDERR_TTY": "0",
   "NODE_OPTIONS": "--max-http-header-size=1048576 --http-parser=legacy"
 }
+
+exports['lib/exec/spawn .start detects kill signal exits with error on SIGKILL 1'] = `
+The Test Runner unexpectedly exited via a [36mexit[39m event with signal [36mSIGKILL[39m
+
+Please search Cypress documentation for possible solutions:
+
+[34mhttps://on.cypress.io[39m
+
+Check if there is a GitHub issue describing this crash:
+
+[34mhttps://github.com/cypress-io/cypress/issues[39m
+
+Consider opening a new issue.
+
+----------
+
+Platform: darwin (Foo-OsVersion)
+Cypress Version: 0.0.0
+`
