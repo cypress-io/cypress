5322572bb45ce314481df1f6e0c09aae304cb6ea
decaffeinate <chrisbreiding@gmail.com>
2020-03-17 14:42:34 -0400

    decaffeinate: Run post-processing cleanups on saved_state_spec.coffee

--- packages/server/test/unit/saved_state_spec.js
+++ packages/server/test/unit/saved_state_spec.js
@@ -1,77 +1,118 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-require("../spec_helper");
+require('../spec_helper')
 
-const path = require("path");
-const Promise = require("bluebird");
-const savedState = require(`${root}lib/saved_state`);
-const fs = require(`${root}lib/util/fs`);
-const FileUtil = require(`${root}lib/util/file`);
-const appData = require(`${root}lib/util/app_data`);
-const savedStateUtil = require(`${root}lib/util/saved_state`);
+const path = require('path')
+const Promise = require('bluebird')
+const savedState = require(`${root}lib/saved_state`)
+const fs = require(`${root}lib/util/fs`)
+const FileUtil = require(`${root}lib/util/file`)
+const appData = require(`${root}lib/util/app_data`)
+const savedStateUtil = require(`${root}lib/util/saved_state`)
 
-describe("lib/util/saved_state", () => describe("project name hash", function() {
-  const projectRoot = "/foo/bar";
+describe('lib/util/saved_state', () => {
+  describe('project name hash', () => {
+    const projectRoot = '/foo/bar'
 
-  it("starts with folder name", function() {
-    const hash = savedStateUtil.toHashName(projectRoot);
-    return expect(hash).to.match(/^bar-/);
-  });
+    it('starts with folder name', () => {
+      const hash = savedStateUtil.toHashName(projectRoot)
 
-  it("computed for given path", function() {
-    const hash = savedStateUtil.toHashName(projectRoot);
-    const expected = "bar-1df481b1ec67d4d8bec721f521d4937d";
-    return expect(hash).to.equal(expected);
-  });
+      expect(hash).to.match(/^bar-/)
+    })
 
-  return it("does not handle empty project path", function() {
-    const tryWithoutPath = () => savedStateUtil.toHashName();
-    return expect(tryWithoutPath).to.throw("Missing project path");
-  });
-}));
+    it('computed for given path', () => {
+      const hash = savedStateUtil.toHashName(projectRoot)
+      const expected = 'bar-1df481b1ec67d4d8bec721f521d4937d'
 
-describe("lib/saved_state", function() {
-  beforeEach(() => savedState().then(state => fs.unlinkAsync(state.path)).catch(function() {}));
-      //# ignore error if file didn't exist in the first place
+      expect(hash).to.equal(expected)
+    })
 
-  it("is a function", () => expect(savedState).to.be.a("function"));
+    it('does not handle empty project path', () => {
+      const tryWithoutPath = () => {
+        return savedStateUtil.toHashName()
+      }
 
-  it("resolves with an instance of FileUtil", () => savedState()
-  .then(state => expect(state).to.be.instanceof(FileUtil)));
+      expect(tryWithoutPath).to.throw('Missing project path')
+    })
+  })
+})
 
-  it("resolves with a noop instance if isTextTerminal", () => savedState("/foo/bar", true)
-  .then(state => expect(state).to.equal(FileUtil.noopFile)));
+describe('lib/saved_state', () => {
+  beforeEach(() => {
+    return savedState().then((state) => {
+      return fs.unlinkAsync(state.path)
+    }).catch(() => {}) // ignore error if file didn't exist in the first place
+  })
 
-  it("caches state file instance per path", () => Promise.all([
-    savedState("/foo/bar"),
-    savedState("/foo/bar")
-  ]).spread((a, b) => expect(a).to.equal(b)));
+  it('is a function', () => {
+    expect(savedState).to.be.a('function')
+  })
 
-  it("returns different state file for different path", function() {
-    const a = savedState("/foo/bar");
-    const b = savedState("/foo/baz");
-    return expect(a).to.not.equal(b);
-  });
-
-  it("sets path to project name + hash if projectRoot", () => savedState("/foo/the-project-name")
-  .then(state => expect(state.path).to.include("the-project-name")));
-
-  it("sets path __global__ if no projectRoot", () => savedState()
-  .then(function(state) {
-    const expected = path.join(appData.path(), "projects", "__global__", "state.json");
-    return expect(state.path).to.equal(expected);
-  }));
-
-  it("only saves whitelisted keys", () => savedState()
-  .then(state => state.set({ foo: "bar", appWidth: 20 })
-  .then(() => state.get())).then(stateObject => expect(stateObject).to.eql({ appWidth: 20 })));
-
-  return it("logs error when attempting to set invalid key(s)", function() {
-    sinon.spy(console, "error");
+  it('resolves with an instance of FileUtil', () => {
     return savedState()
-    .then(state => state.set({ foo: "bar", baz: "qux" })).then(() => expect(console.error).to.be.calledWith("WARNING: attempted to save state for non-whitelisted key(s): foo, baz. All keys must be whitelisted in server/lib/saved_state.coffee"));
-  });
-});
+    .then((state) => {
+      expect(state).to.be.instanceof(FileUtil)
+    })
+  })
+
+  it('resolves with a noop instance if isTextTerminal', () => {
+    return savedState('/foo/bar', true)
+    .then((state) => {
+      expect(state).to.equal(FileUtil.noopFile)
+    })
+  })
+
+  it('caches state file instance per path', () => {
+    return Promise.all([
+      savedState('/foo/bar'),
+      savedState('/foo/bar'),
+    ]).spread((a, b) => {
+      expect(a).to.equal(b)
+    })
+  })
+
+  it('returns different state file for different path', () => {
+    const a = savedState('/foo/bar')
+    const b = savedState('/foo/baz')
+
+    expect(a).to.not.equal(b)
+  })
+
+  it('sets path to project name + hash if projectRoot', () => {
+    return savedState('/foo/the-project-name')
+    .then((state) => {
+      expect(state.path).to.include('the-project-name')
+    })
+  })
+
+  it('sets path __global__ if no projectRoot', () => {
+    return savedState()
+    .then((state) => {
+      const expected = path.join(appData.path(), 'projects', '__global__', 'state.json')
+
+      expect(state.path).to.equal(expected)
+    })
+  })
+
+  it('only saves whitelisted keys', () => {
+    return savedState()
+    .then((state) => {
+      return state.set({ foo: 'bar', appWidth: 20 })
+      .then(() => {
+        return state.get()
+      })
+    }).then((stateObject) => {
+      expect(stateObject).to.eql({ appWidth: 20 })
+    })
+  })
+
+  it('logs error when attempting to set invalid key(s)', () => {
+    sinon.spy(console, 'error')
+
+    return savedState()
+    .then((state) => {
+      return state.set({ foo: 'bar', baz: 'qux' })
+    }).then(() => {
+      // eslint-disable-next-line no-console
+      expect(console.error).to.be.calledWith('WARNING: attempted to save state for non-whitelisted key(s): foo, baz. All keys must be whitelisted in server/lib/saved_state.js')
+    })
+  })
+})
