b843256b320edfb757dbaf3d33775ee4a260f676
decaffeinate <chrisbreiding@gmail.com>
2020-03-27 09:39:30 -0400

    decaffeinate: Run post-processing cleanups on cypress_spec.coffee

--- packages/driver/test/cypress/integration/cypress/cypress_spec.js
+++ packages/driver/test/cypress/integration/cypress/cypress_spec.js
@@ -1,108 +1,124 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const {
-  $
-} = Cypress;
-const {
-  Promise
-} = Cypress;
+const { $, Promise } = Cypress
 
-describe("driver/src/cypress/index", function() {
-  beforeEach(function() {
-    cy.stub(Promise, "config");
+describe('driver/src/cypress/index', () => {
+  let CypressInstance
 
-    return this.Cypress = Cypress.$Cypress.create({});
-  });
+  beforeEach(function () {
+    cy.stub(Promise, 'config')
 
-  context("$Cypress", () => it("is attached but not global", function() {
-    expect(window.$Cypress).to.be.undefined;
-    return expect(window.top.$Cypress).to.be.undefined;
-  }));
+    CypressInstance = Cypress.$Cypress.create({})
+  })
 
-  context("$", function() {
-    afterEach(() => delete Cypress.$.expr[":"].foo);
+  context('$Cypress', () => {
+    it('is attached but not global', () => {
+      expect(window.$Cypress).to.be.undefined
+      expect(window.top.$Cypress).to.be.undefined
+    })
+  })
 
-    //# https://github.com/cypress-io/cypress/issues/2830
-    return it("exposes expr", function() {
-      expect(Cypress.$).to.have.property("expr");
+  context('$', () => {
+    afterEach(() => {
+      delete Cypress.$.expr[':'].foo
+    })
 
-      Cypress.$.expr[":"].foo = elem => Boolean(elem.getAttribute("foo"));
+    // https://github.com/cypress-io/cypress/issues/2830
+    it('exposes expr', () => {
+      expect(Cypress.$).to.have.property('expr')
 
-      const $foo = $("<div foo='bar'>foo element</div>").appendTo(cy.$$("body"));
+      Cypress.$.expr[':'].foo = (elem) => {
+        return Boolean(elem.getAttribute('foo'))
+      }
 
-      return cy.get(":foo").then($el => expect($el.get(0)).to.eq($foo.get(0)));
-    });
-  });
+      const $foo = $('<div foo=\'bar\'>foo element</div>').appendTo(cy.$$('body'))
 
-  context("#backend", function() {
-    it("sets __stackCleaned__ on errors", function() {
-      cy.stub(this.Cypress, "emit")
-      .withArgs("backend:request")
+      cy.get(':foo').then(($el) => {
+        expect($el.get(0)).to.eq($foo.get(0))
+      })
+    })
+  })
+
+  context('#backend', () => {
+    it('sets __stackCleaned__ on errors', function () {
+      cy.stub(CypressInstance, 'emit')
+      .withArgs('backend:request')
       .yieldsAsync({
         error: {
-          name: "Error",
-          message: "msg",
-          stack: "stack"
-        }
-      });
+          name: 'Error',
+          message: 'msg',
+          stack: 'stack',
+        },
+      })
 
-      return this.Cypress.backend("foo")
-      .catch(function(err) {
-        expect(err.backend).to.be.true;
-        return expect(err.stack).not.to.include("From previous event");
-      });
-    });
+      return CypressInstance.backend('foo')
+      .catch((err) => {
+        expect(err.backend).to.be.true
 
-    //# https://github.com/cypress-io/cypress/issues/4346
-    return it("can complete if a circular reference is sent", function() {
+        expect(err.stack).not.to.include('From previous event')
+      })
+    })
+
+    // https://github.com/cypress-io/cypress/issues/4346
+    it('can complete if a circular reference is sent', () => {
       const foo = {
-        bar: {}
-      };
+        bar: {},
+      }
 
-      foo.bar.baz = foo;
+      foo.bar.baz = foo
 
-      return Cypress.backend("foo", foo)
-      .then(function() {
-        throw new Error("should not reach");}).catch(e => expect(e.message).to.eq("You requested a backend event we cannot handle: foo"));
-    });
-  });
+      return Cypress.backend('foo', foo)
+      .then(() => {
+        throw new Error('should not reach')
+      }).catch((e) => {
+        expect(e.message).to.eq('You requested a backend event we cannot handle: foo')
+      })
+    })
+  })
 
-  context(".isCy", function() {
-    it("returns true on cy, cy chainable", function() {
-      let chainer;
-      expect(Cypress.isCy(cy)).to.be.true;
-      return chainer = cy.wrap().then(() => expect(Cypress.isCy(chainer)).to.be.true);
-    });
+  context('.isCy', () => {
+    it('returns true on cy, cy chainable', () => {
+      expect(Cypress.isCy(cy)).to.be.true
 
-    return it("returns false on non-cy objects", function() {
-      expect(Cypress.isCy(undefined)).to.be.false;
-      return expect(Cypress.isCy(() => ({}))).to.be.false;
-    });
-  });
+      const chainer = cy.wrap().then(() => {
+        expect(Cypress.isCy(chainer)).to.be.true
+      })
+    })
 
-  return context(".Log", function() {
-    it("throws when using Cypress.Log.command()", function() {
-      const fn = () => Cypress.Log.command({});
+    it('returns false on non-cy objects', () => {
+      expect(Cypress.isCy(undefined)).to.be.false
 
-      return expect(fn).to.throw('has been renamed to `Cypress.log()`');
-    });
+      expect(Cypress.isCy(() => {
+        return {}
+      })).to.be.false
+    })
+  })
 
-    it("throws when passing non-object to Cypress.log()", function() {
-      const fn = () => Cypress.log('My Log');
+  context('.Log', () => {
+    it('throws when using Cypress.Log.command()', () => {
+      const fn = () => {
+        Cypress.Log.command({})
+      }
 
-      expect(fn).to.throw().with.property("message")
-        .and.include("`Cypress.log()` can only be called with an options object. Your argument was: `My Log`");
-      return expect(fn).to.throw().with.property("docsUrl")
-        .and.eq("https://on.cypress.io/cypress-log");
-    });
+      expect(fn).to.throw('has been renamed to `Cypress.log()`')
+    })
 
-    return it("does not throw when Cypress.log() called outside of command", function() {
-      const fn = () => Cypress.log({ message: 'My Log' });
+    it('throws when passing non-object to Cypress.log()', () => {
+      const fn = () => {
+        Cypress.log('My Log')
+      }
 
-      return expect(fn).to.not.throw();
-    });
-  });
-});
+      expect(fn).to.throw().with.property('message')
+      .and.include('`Cypress.log()` can only be called with an options object. Your argument was: `My Log`')
+
+      expect(fn).to.throw().with.property('docsUrl')
+      .and.eq('https://on.cypress.io/cypress-log')
+    })
+
+    it('does not throw when Cypress.log() called outside of command', () => {
+      const fn = () => {
+        Cypress.log({ message: 'My Log' })
+      }
+
+      expect(fn).to.not.throw()
+    })
+  })
+})
