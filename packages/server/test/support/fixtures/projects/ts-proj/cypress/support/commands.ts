04d0ef980873b75f749831cadeb3b24695700caf
KHeo <sainthkh@naver.com>
2020-04-13 15:46:00 +0900

    Add working examples for support and plugins.

--- packages/server/test/support/fixtures/projects/ts-proj/cypress/support/commands.ts
+++ packages/server/test/support/fixtures/projects/ts-proj/cypress/support/commands.ts
@@ -1,25 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+/// <reference types="cypress" />
+
+// Copied an example command from https://on.cypress.io/custom-commands
+Cypress.Commands.add('clickLink', (label: string | number | RegExp) => {
+  cy.get('a').contains(label).click()
+})
