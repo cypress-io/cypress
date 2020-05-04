46c116db16b874e32676f8e3e778057a3dee0d81
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2020-05-04 14:42:55 -0400

    fix windowSize for browser e2e test

--- packages/server/test/support/fixtures/projects/hooks-after-rerun/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/hooks-after-rerun/cypress/plugins/index.js
@@ -16,4 +16,16 @@ module.exports = (on) => {
       return state
     },
   })
+
+  on('before:browser:launch', (browser, launchOptions) => {
+    // this is needed to ensure correct error screenshot / video recording
+    // resolution of exactly 1280x720 (height must account for url bar)
+    if (browser.family === 'chromium') {
+      launchOptions.args.push('--window-size=1288,805')
+    } else if (browser.family === 'firefox') {
+      launchOptions.args.push('-width', '1280', '-height', '794')
+    }
+
+    return launchOptions
+  })
 }
