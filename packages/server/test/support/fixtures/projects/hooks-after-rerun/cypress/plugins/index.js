590a573407e14cce663bb5a0a501d962525dc03e
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2020-05-04 15:08:47 -0400

    fix windowSize for xvfb chrome in e2e test

--- packages/server/test/support/fixtures/projects/hooks-after-rerun/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/hooks-after-rerun/cypress/plugins/index.js
@@ -21,7 +21,11 @@ module.exports = (on) => {
     // this is needed to ensure correct error screenshot / video recording
     // resolution of exactly 1280x720 (height must account for url bar)
     if (browser.family === 'chromium') {
-      launchOptions.args.push('--window-size=1288,805')
+      if (process.env.DISPLAY === ':99') {
+        launchOptions.args.push('--window-size=1280,792')
+      } else {
+        launchOptions.args.push('--window-size=1288,805')
+      }
     } else if (browser.family === 'firefox') {
       launchOptions.args.push('-width', '1280', '-height', '794')
     }
