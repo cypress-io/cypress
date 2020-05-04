becab3243ee8c28de70ee59e72cc86147d525922
Zach Bloomquist <github@chary.us>
2020-05-04 10:49:37 -0400

    add repro for #3975

--- packages/server/test/support/fixtures/projects/e2e/cypress/integration/source_rewriting_spec.js
+++ packages/server/test/support/fixtures/projects/e2e/cypress/integration/source_rewriting_spec.js
@@ -9,14 +9,31 @@ describe('source rewriting spec', function () {
     })
   })
 
-  // TODO: trying to reproduce https://github.com/cypress-io/cypress/issues/3975
-  context.skip('issue 3975', function () {
-    it('can redirect in a xhr onload', function () {
-      cy.visit('/xhr_onload_redirect.html')
-      cy.get('button').click()
+  // @see https://github.com/cypress-io/cypress/issues/3975
+  context('issue 3975', function () {
+    it('can relative redirect in a xhr onload', function () {
+      cy.visit('/static/xhr_onload_redirect.html')
+      cy.location('pathname').should('eq', '/static/index.html')
     })
 
-    it('Login demo', function () {
+    it('can relative redirect in a settimeout', function () {
+      cy.visit('/static/settimeout_redirect.html')
+      cy.location('pathname').should('eq', '/static/index.html')
+    })
+
+    it('can relative redirect in a onclick handler', function () {
+      cy.visit('/static/onclick_redirect.html')
+      cy.get('button').click()
+      cy.location('pathname').should('eq', '/static/index.html')
+    })
+
+    it('can relative redirect in a settimeout with a base tag', function () {
+      cy.visit('/static/settimeout_basetag_redirect.html')
+      cy.location('pathname').should('eq', '/static/foo/bar/index.html')
+    })
+
+    // NOTE: user's repro
+    it.skip('Login demo', function () {
       // cy.on('fail', console.error)
       cy.visit('https://apex.oracle.com/pls/apex/f?p=54707:LOGIN_DESKTOP', { timeout: 60000 })
       cy.get('#P9999_USERNAME').type('ApexUser')
