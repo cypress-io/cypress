14c470314cbedd9f0439d16f5c8102b6e6766d99
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-08 16:35:58 -0400

    add to circleci

--- examples/webpack-options/cypress/plugins/index.js
+++ examples/webpack-options/cypress/plugins/index.js
@@ -1,9 +1,14 @@
 // @ts-check
 const webpackPreprocessor = require('@cypress/webpack-preprocessor')
+
+// Cypress Webpack preprocessor includes Babel env preset,
+// but to transpile JSX code we need to add Babel React preset
 module.exports = (on, config) => {
-  on(
-    'file:preprocessor',
-    // @ts-ignore
-    webpackPreprocessor(webpackPreprocessor.defaultOptions),
+  // @ts-ignore
+  const opts = webpackPreprocessor.defaultOptions
+  // add React preset to be able to transpile JSX
+  opts.webpackOptions.module.rules[0].use[0].options.presets.push(
+    require.resolve('@babel/preset-react'),
   )
+  on('file:preprocessor', webpackPreprocessor(opts))
 }
