2cafe4e9a01ce847a439d13116e6397563fa2a9f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-08 10:31:21 -0400

    add a folder with react-scripts example (bahmutov/cypress-react-unit-test#183)
    This is a start, need to add another example where component spec is outside the `src` folder for bahmutov/cypress-react-unit-test#178

---
+++ examples/react-scripts/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+// this CSS will be inlined ✅
+import './App.css'
+import logo from './logo.svg' // => "/__root/src/logo.svg"
+import cypressLogo from './cypress-logo-dark.png' // => "/__root/src/cypress-logo-dark.png"
+
+// large image will be transformed into
+// a different url static/media/vans.25e5784d.jpg
+// import giantImage from './vans.jpg'
+
+// we cannot load the image from direct url
+// import giantImage from '/__root/src/vans.jpg'
+// <img src={giantImage} alt="cypress-logo" />
+
+function App() {
+  return (
+    <div className="App">
+      <img src={cypressLogo} className="Cypress-log" />
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
