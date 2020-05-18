5385af48a36cc64e2fc63b33860a9e02c9e9abad
Brian Mann <brian.mann86@gmail.com>
2020-05-18 08:42:18 -0400

    use latest version of webpack-preprocessor to validate typescript sourcemaps

--- packages/server/test/support/fixtures/projects/webpack-preprocessor/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/webpack-preprocessor/cypress/plugins/index.js
@@ -1,4 +1,9 @@
-const wp = require('@cypress/webpack-preprocessor')
+const proxyquire = require('proxyquire')
+
+// force typescript to always be non-requireable
+const wp = proxyquire('@cypress/webpack-preprocessor', {
+  typescript: null,
+})
 
 module.exports = (on) => {
   on('file:preprocessor', wp())
