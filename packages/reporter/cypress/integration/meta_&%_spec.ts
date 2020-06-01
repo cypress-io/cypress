1ef31e4cea040b3405641af07ae2043565e4701e
Chris Breiding <chrisbreiding@gmail.com>
2020-06-01 14:35:50 -0400

    arrow ftw

--- packages/reporter/cypress/integration/meta_&%_spec.ts
+++ packages/reporter/cypress/integration/meta_&%_spec.ts
@@ -1,5 +1,5 @@
 describe('special characters', () => {
-  it('displays file name with decoded special characters', function () {
+  it('displays file name with decoded special characters', () => {
     cy.wrap(Cypress.$(window.top.document.body))
     .find('.reporter .runnable-header a')
     .should('have.text', 'cypress/integration/meta_&%_spec.ts')
