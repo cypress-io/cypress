17f6373f8523b08c9d1ac18bbd4d686e80ecec4b
Zach Bloomquist <github@chary.us>
2020-06-05 15:19:12 -0400

    cleanup plugin decaf

--- packages/server/test/support/fixtures/projects/plugin-filter-browsers/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/plugin-filter-browsers/cypress/plugins/index.js
@@ -1,5 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
 const debug = require('debug')('cypress:e2e')
 
 module.exports = function (onFn, config) {
--- packages/server/test/support/fixtures/projects/plugin-returns-bad-config/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/plugin-returns-bad-config/cypress/plugins/index.js
@@ -1,10 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // returns object with invalid properties
 module.exports = (onFn, config) => {
   return {
