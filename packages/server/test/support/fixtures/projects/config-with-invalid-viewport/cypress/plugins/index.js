4cc301d3c406c55d06295aee539c0a3e5c755c96
Zach Bloomquist <github@chary.us>
2020-06-05 15:19:12 -0400

    cleanup plugin decaf

--- packages/server/test/support/fixtures/projects/config-with-invalid-viewport/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/config-with-invalid-viewport/cypress/plugins/index.js
@@ -1,3 +1,1 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
 module.exports = function (onFn, config) {}
--- packages/server/test/support/fixtures/projects/plugin-returns-invalid-browser/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/plugin-returns-invalid-browser/cypress/plugins/index.js
@@ -1,10 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // returns invalid config with a browser that is invalid
 // (missing multiple properties)
 module.exports = (onFn, config) => {
