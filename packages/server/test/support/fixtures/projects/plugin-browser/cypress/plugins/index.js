935ee52e4e7dbfe25e3185726b551272b1166cfa
decaffeinate <github@chary.us>
2020-06-05 15:16:09 -0400

    decaffeinate: Run post-processing cleanups on index.coffee and 7 other files

--- packages/server/test/support/fixtures/projects/plugin-browser/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/plugin-browser/cypress/plugins/index.js
@@ -1,22 +1,26 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-module.exports = (onFn, config) => onFn('before:browser:launch', function(browser, options) {
-  const { name } = browser;
+module.exports = (onFn, config) => {
+  return onFn('before:browser:launch', (browser, options) => {
+    const { name } = browser
 
-  switch (name) {
-    case "chrome":
-      return [name, "foo", "bar", "baz"];
-    case "electron":
-      return {
-        preferences: {
-          browser: "electron",
-          foo: "bar"
+    switch (name) {
+      case 'chrome':
+        return [name, 'foo', 'bar', 'baz']
+      case 'electron':
+        return {
+          preferences: {
+            browser: 'electron',
+            foo: 'bar',
+          },
         }
-      };
-    default:
-      throw new Error(`unrecognized browser name: '${name}' for before:browser:launch`);
-  }
-});
+      default:
+        throw new Error(`unrecognized browser name: '${name}' for before:browser:launch`)
+    }
+  })
+}
--- packages/server/test/support/fixtures/projects/plugin-extension/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/plugin-extension/cypress/plugins/index.js
@@ -1,13 +1,18 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const path = require("path");
+const path = require('path')
 
-module.exports = (onFn, config) => onFn("before:browser:launch", function(browser = {}, options) {
-  const pathToExt = path.resolve("ext");
+module.exports = (onFn, config) => {
+  return onFn('before:browser:launch', (browser = {}, options) => {
+    const pathToExt = path.resolve('ext')
 
-  options.args.push(`--load-extension=${pathToExt}`);
-  return options;
-});
+    options.args.push(`--load-extension=${pathToExt}`)
+
+    return options
+  })
+}
