d0043ff4fd3b660b802c7752b44a523c660e4fe1
Kukhyeon Heo <sainthkh@naver.com>
2020-06-09 22:42:27 +0900

    fix: Cypress.Commands.add Type Signature for @typescript-eslint/no-misused-promises rule (#7547)

--- packages/server/test/support/fixtures/projects/ts-proj-with-own-tsconfig/cypress/support/commands.ts
+++ packages/server/test/support/fixtures/projects/ts-proj-with-own-tsconfig/cypress/support/commands.ts
@@ -4,3 +4,10 @@
 Cypress.Commands.add('clickLink', (label: string | number | RegExp) => {
   cy.get('a').contains(label).click()
 })
+
+// https://github.com/cypress-io/cypress/issues/7510
+// The code below fails when @typescript-eslint/no-misused-promises is error
+// and the return type of function in Cypress.Commands.add doesn't support Chainable.
+Cypress.Commands.add('dataCy', (value) => {
+  return cy.get(`[data-cy=${value}]`)
+})
