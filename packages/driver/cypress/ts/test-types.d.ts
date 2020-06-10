b696fbbd45f7e0e6cdba6dff1796d75c4d3dfd29
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2020-06-10 19:57:34 -0400

    move internal-types to "types" folder, fix driver/index.d.ts

--- packages/driver/cypress/ts/test-types.d.ts
+++
@@ -1,4 +0,0 @@
-// NOTE: this loads in the Cypress type defitions from cli/types
-//   to give us Cypress global types during our tests.
-//   Only needed for internal development
-/// <reference path="../../../../cli/types/index.d.ts" />
