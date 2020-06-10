f1f9ddeb8902ff8db77f483dd7865cd6837084e8
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2020-06-10 19:36:23 -0400

    move type definition loading for driver/cypress into dedicated file

---
+++ packages/driver/cypress/ts/test-types.d.ts
@@ -0,0 +1,4 @@
+// NOTE: this loads in the Cypress type defitions from cli/types
+//   to give us Cypress global types during our tests.
+//   Only needed for internal development
+/// <reference path="../../../../cli/types/index.d.ts" />
