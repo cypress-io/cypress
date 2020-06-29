a5a02f7a5e049728030f8261d2dc364b83343507
decaffeinate <github@chary.us>
2020-06-29 16:09:09 -0400

    decaffeinate: Convert request_http_network_error_failing_spec.coffee and 3 other files to JS

--- packages/server/test/support/fixtures/projects/e2e/cypress/integration/request_long_http_props_failing_spec.js
+++ packages/server/test/support/fixtures/projects/e2e/cypress/integration/request_long_http_props_failing_spec.js
@@ -1,4 +1,7 @@
-describe "when status code isnt 2xx or 3xx", ->
-  it "fails", ->
-    cy.request("http://localhost:2294/myreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallylong
-")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+describe("when status code isnt 2xx or 3xx", () => it("fails", () => cy.request(`http://localhost:2294/myreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallylong\
+`)));
--- packages/server/test/support/fixtures/projects/e2e/cypress/integration/request_status_code_failing_spec.js
+++ packages/server/test/support/fixtures/projects/e2e/cypress/integration/request_status_code_failing_spec.js
@@ -1,3 +1,6 @@
-describe "when status code isnt 2xx or 3xx", ->
-  it "fails", ->
-    cy.request("http://localhost:2294/statusCode?code=503")
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+describe("when status code isnt 2xx or 3xx", () => it("fails", () => cy.request("http://localhost:2294/statusCode?code=503")));
