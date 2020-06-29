dae4b7996528fabf529e9333d53083acf448c99d
decaffeinate <github@chary.us>
2020-06-29 16:09:28 -0400

    decaffeinate: Run post-processing cleanups on request_http_network_error_failing_spec.coffee and 3 other files

--- packages/server/test/support/fixtures/projects/e2e/cypress/integration/request_http_network_error_failing_spec.js
+++ packages/server/test/support/fixtures/projects/e2e/cypress/integration/request_http_network_error_failing_spec.js
@@ -1,6 +1,12 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-describe("when network connection cannot be established", () => it("fails", () => cy.request("http://localhost:16795")));
+describe('when network connection cannot be established', () => {
+  it('fails', () => {
+    return cy.request('http://localhost:16795')
+  })
+})
