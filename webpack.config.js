d46a31d714d8af008c704e2801624c351825356c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-01 12:20:05 -0400

    format root level files

--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,35 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path')
 module.exports = {
-    resolve: {
-      extensions: ['.js', '.json', '.vue'],
-      alias: {
-        'cypress-vue-unit-test': path.join(__dirname, 'src')
-      }
+  resolve: {
+    extensions: ['.js', '.json', '.vue'],
+    alias: {
+      'cypress-vue-unit-test': path.join(__dirname, 'src'),
     },
-    module: {
-      rules: [
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader',
-          options: {
-            loaders: {
-              // you need to specify `i18n` loaders key with
-              // `vue-i18n-loader` (https://github.com/kazupon/vue-i18n-loader)
-              i18n: '@kazupon/vue-i18n-loader'
-            }
-          }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // you need to specify `i18n` loaders key with
+            // `vue-i18n-loader` (https://github.com/kazupon/vue-i18n-loader)
+            i18n: '@kazupon/vue-i18n-loader',
+          },
         },
-        // this will apply to both plain `.css` files
-        // AND `<style>` blocks in `.vue` files
-        {
-          test: /\.css$/,
-          use: [
-            'vue-style-loader',
-            'css-loader'
-          ]
-        }
-      ]
-    },
-    plugins: [
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
     // make sure to include the plugin for the magic
-      new VueLoaderPlugin()
-    ]
+    new VueLoaderPlugin(),
+  ],
 }
