c72538d1895b73304e0265cceb54073ac659887c
Brian Mann <brian.mann86@gmail.com>
2020-07-11 16:34:23 -0400

    Fix lodash dupe dep (#7954)
    * blow away yarn.lock
    
    * bump lodash to 4.17.19
    
    * reduce binary size again
    
    * bump @types/node to correct version, fix types
    
    * fix react + react-dom types
    
    * add clean command for .ts conversion to .js files
    
    * fix flaky test

--- packages/reporter/src/test/test.spec.tsx
+++ packages/reporter/src/test/test.spec.tsx
@@ -139,8 +139,10 @@ describe('<Test />', () => {
     beforeEach(() => {
       scroller = scrollerStub()
 
-      global.window.requestAnimationFrame = function (callback: () => void) {
-        callback()
+      // @ts-ignore
+      global.window.requestAnimationFrame = function (callback: FrameRequestCallback) {
+        // @ts-ignore
+        return callback()
       }
     })
 
