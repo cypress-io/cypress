0a8196ec5ada4394d5d32ea9e5699f0072405591
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-13 10:25:50 -0400

    feat: add mountHook function for testing React hooks (#335)

--- lib/index.ts
+++ lib/index.ts
@@ -166,4 +166,77 @@ export const unmount = () => {
   })
 }
 
+// mounting hooks inside a test component mostly copied from
+// https://github.com/testing-library/react-hooks-testing-library/blob/master/src/pure.js
+function resultContainer() {
+  let value: any = null
+  let error: Error | null = null
+  const resolvers: any[] = []
+
+  const result = {
+    get current() {
+      if (error) {
+        throw error
+      }
+      return value
+    },
+    get error() {
+      return error
+    },
+  }
+
+  const updateResult = (val: any, err: Error | null = null) => {
+    value = val
+    error = err
+    resolvers.splice(0, resolvers.length).forEach(resolve => resolve())
+  }
+
+  return {
+    result,
+    addResolver: (resolver: any) => {
+      resolvers.push(resolver)
+    },
+    setValue: (val: any) => updateResult(val),
+    setError: (err: Error) => updateResult(undefined, err),
+  }
+}
+
+// @ts-ignore
+function TestHook({ callback, onError, children }) {
+  try {
+    children(callback())
+  } catch (err) {
+    if (err.then) {
+      throw err
+    } else {
+      onError(err)
+    }
+  }
+
+  // TODO decide what the test hook component should show
+  // maybe nothing, or maybe useful information about the hook?
+  // maybe its current properties?
+  // return <div>TestHook</div>
+  return null
+}
+
+/**
+ * Mounts a React hook function in a test component for testing.
+ *
+ * @see https://github.com/bahmutov/cypress-react-unit-test#advanced-examples
+ */
+export const mountHook = (hookFn: Function) => {
+  const { result, setValue, setError } = resultContainer()
+
+  return mount(
+    React.createElement(TestHook, {
+      callback: hookFn,
+      onError: setError,
+      children: setValue,
+    }),
+  ).then(() => {
+    cy.wrap(result)
+  })
+}
+
 export default mount
