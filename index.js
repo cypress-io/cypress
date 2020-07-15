48d2c0e54f8a56274bb0da8c8d941d36feca32eb
Chris Breiding <chrisbreiding@gmail.com>
2020-07-15 10:46:40 -0400

    fix: ensure dependencies resolve correctly

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ const getDefaultWebpackOptions = (typescriptPath) => {
         test: /\.jsx?$/,
         exclude: [/node_modules/],
         use: [{
-          loader: 'babel-loader',
+          loader: require.resolve('babel-loader'),
           options: {
             plugins: [
               '@babel/plugin-transform-modules-commonjs',
               '@babel/plugin-proposal-class-properties',
               '@babel/plugin-proposal-object-rest-spread',
               '@babel/plugin-transform-runtime',
-            ],
+            ].map(require.resolve),
             presets: [
               '@babel/preset-env',
               '@babel/preset-react',
-            ],
+            ].map(require.resolve),
           },
         }],
       }, {
         test: /\.coffee$/,
         exclude: [/node_modules/],
-        loader: 'coffee-loader',
+        loader: require.resolve('coffee-loader'),
       }, {
         test: /\.tsx?$/,
         exclude: [/node_modules/],
         use: [
           {
-            loader: 'ts-loader',
+            loader: require.resolve('ts-loader'),
             options: {
               transpileOnly: true,
               compiler: typescriptPath || 'typescript',
