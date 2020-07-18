7036e682d8999c3db4d15658fada13b31a5bfea1
Jessica Sachs <jess@jessicasachs.io>
2020-07-18 00:57:13 -0400

    chore: cleaning up spec

--- cypress/component/basic/options-spec.js
+++ cypress/component/basic/options-spec.js
@@ -6,14 +6,17 @@ const template = `
     </div>
   `
 
-const data = {
-  message: 'Hello Vue!',
-}
-
 describe('Mount component', () => {
   // hmm, there are no more options to pass
 
-  const component = { template, data() { return data } }
+  const component = {
+    template,
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    },
+  }
   beforeEach(mountCallback(component))
 
   it('shows hello', () => {
