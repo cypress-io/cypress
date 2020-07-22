691bb322d3fe08663c2e45a032c1961f96e21b6c
laistomazz <laisotomaz@gmail.com>
2020-07-22 17:05:14 +0200

    Move test from driver to serve e2e test

--- packages/driver/cypress/integration/issues/5909+spec.js
+++
@@ -1,6 +0,0 @@
-// https://github.com/cypress-io/cypress/issues/5909
-describe('issue 5909', () => {
-  it('runs specs with name containing "+"', () => {
-    cy.visit('/fixtures/issue+5909.html')
-  })
-})
