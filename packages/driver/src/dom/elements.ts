321797e5a792b951c66f284d18aa12dc793ffc6d
Chris Breiding <chrisbreiding@gmail.com>
2020-07-28 10:14:33 -0400

    fix: fix hanging when scrolling shadow dom element

--- packages/driver/src/dom/elements.ts
+++ packages/driver/src/dom/elements.ts
@@ -611,7 +611,7 @@ const isWithinShadowRoot = (node: HTMLElement) => {
   return isShadowRoot(node.getRootNode())
 }
 
-const getParent = (el) => {
+const getParentNode = (el) => {
   // if the element has a direct parent element,
   // simply return it.
   if (el.parentElement) {
@@ -629,9 +629,13 @@ const getParent = (el) => {
   return null
 }
 
+const getParent = ($el: JQuery): JQuery => {
+  return $(getParentNode($el[0]))
+}
+
 const getAllParents = (el) => {
   const collectParents = (parents, node) => {
-    const parent = getParent(node)
+    const parent = getParentNode(node)
 
     if (!parent) return parents
 
@@ -899,7 +903,7 @@ const isDescendent = ($el1, $el2) => {
 
 const findParent = (el, condition) => {
   const collectParent = (node) => {
-    const parent = getParent(node)
+    const parent = getParentNode(node)
 
     if (!parent) return null
 
@@ -923,7 +927,7 @@ const getFirstFocusableEl = ($el: JQuery<HTMLElement>) => {
     return $el
   }
 
-  const parent = $el.parent()
+  const parent = getParent($el)
 
   // if we have no parent then just return
   // the window since that can receive focus
@@ -933,7 +937,7 @@ const getFirstFocusableEl = ($el: JQuery<HTMLElement>) => {
     return $(win)
   }
 
-  return getFirstFocusableEl($el.parent())
+  return getFirstFocusableEl(getParent($el))
 }
 
 const getActiveElByDocument = ($el: JQuery<HTMLElement>): HTMLElement | null => {
@@ -1245,6 +1249,12 @@ const elementFromPoint = (doc, x, y) => {
   return elFromPoint
 }
 
+const getShadowRoot = ($el: JQuery): JQuery<Node> => {
+  const root = $el[0].getRootNode()
+
+  return $(root)
+}
+
 const findAllShadowRoots = (root: Node): Node[] => {
   const collectRoots = (roots, nodes, node) => {
     const currentRoot = roots.pop()
@@ -1359,4 +1369,5 @@ export {
   getFirstScrollableParent,
   getParent,
   getAllParents,
+  getShadowRoot,
 }
