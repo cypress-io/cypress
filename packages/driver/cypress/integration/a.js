fcc45adf675d5c8acb3e0b5f6487d49d9ac256d2
KHeo <sainthkh@naver.com>
2020-07-28 12:07:51 +0900

    Add working test.

--- packages/driver/cypress/integration/a.js
+++ packages/driver/cypress/integration/a.js
@@ -7,3 +7,12 @@ it('t', () => {
 
   cy.get('#result').should('contain', '{Ctrl}')
 })
+
+it('x', () => {
+  cy.visit('/fixtures/issue-486.html')
+
+  cy.get('#button').type('{ctrl}', { release: false })
+  cy.get('#button').click()
+
+  cy.get('#result').should('contain', '{Ctrl}')
+})
