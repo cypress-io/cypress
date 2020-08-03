536821675d900b8e5f4661d96ed00f4e2bdf8815
Jennifer Shehane <jennifer@cypress.io>
2020-08-03 14:19:53 +0630

    Pass 'headed' true, otherwise it will always pass + add some comments

--- packages/server/test/support/fixtures/projects/issue-8111-iframe-input/cypress/integration/iframe_input_spec.js
+++ packages/server/test/support/fixtures/projects/issue-8111-iframe-input/cypress/integration/iframe_input_spec.js
@@ -1,4 +1,4 @@
-it('can type into an input in an iframe', () => {
+it('can type into an input in an iframe that calls auto focus', () => {
   cy.visit('/outer.html')
   cy.get('iframe')
   .its('0.contentDocument.body').should('not.be.empty')
