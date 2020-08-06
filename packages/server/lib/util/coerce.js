2643d806deaf2a516b47247f2985c169436f383b
Donovan Moini <donovanmoini@gmail.com>
2020-08-06 03:03:52 -0700

    refactor: improved readability of code for coerce and added more comments
    Signed-off-by: Donovan Moini <donovanmoini@gmail.com>

--- packages/server/lib/util/coerce.js
+++ packages/server/lib/util/coerce.js
@@ -7,17 +7,24 @@ const isValue = (value) => {
   }
 }
 
+// https://github.com/cypress-io/cypress/issues/6810
 const toArray = (value) => {
-  if (typeof (value) !== 'string' || (value[0] !== '[' && value[value.length - 1] !== ']')) {
+  const valueIsNotStringOrArray = typeof (value) !== 'string' || (value[0] !== '[' && value[value.length - 1] !== ']')
+
+  if (valueIsNotStringOrArray) {
     return
   }
 
-  const arr = value.substring(1, value.length - 1).split(',')
+  // '[foo,bar]' => ['foo', 'bar']
+  const convertStringToArray = () => value.substring(1, value.length - 1).split(',')
+  const arr = convertStringToArray()
 
   // The default `toString` array method returns one string containing each array element separated
   // by commas, but without '[' or ']'. If an environment variable is intended to be an array, it
   // will begin and end with '[' and ']' respectively. To correctly compare the value argument to
   // the value in `process.env`, the `toString` method must be updated to include '[' and ']'.
+  // Default `toString()` on array: ['foo', 'bar'].toString() => 'foo,bar'
+  // Custom `toString()` on array: ['foo', 'bar'].toString() => '[foo,bar]'
   arr.toString = () => `[${arr.join(',')}]`
 
   return arr
