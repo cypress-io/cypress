17f39265ef3517081139766da64892b7dda3f57e
Chris Breiding <chrisbreiding@gmail.com>
2020-08-07 12:55:12 -0400

    improve esmoduleinterop e2e tests

--- packages/server/test/e2e/1_typescript_plugins_spec.ts
+++ packages/server/test/e2e/1_typescript_plugins_spec.ts
@@ -11,9 +11,16 @@ describe('e2e typescript in plugins file', function () {
   })
 
   // https://github.com/cypress-io/cypress/issues/7575
-  it('allows esModuleInterop to be overridden', function () {
+  it('defaults to esModuleInterop: false', function () {
     return e2e.exec(this, {
-      project: Fixtures.projectPath('ts-proj-esmoduleinterop-false'),
+      project: Fixtures.projectPath('ts-proj'),
+    })
+  })
+
+  // https://github.com/cypress-io/cypress/issues/7575
+  it('allows esModuleInterop to be overridden with true via tsconfig.json', function () {
+    return e2e.exec(this, {
+      project: Fixtures.projectPath('ts-proj-esmoduleinterop-true'),
     })
   })
 })
--- packages/server/test/support/fixtures/projects/ts-proj/cypress/plugins/index.ts
+++ packages/server/test/support/fixtures/projects/ts-proj/cypress/plugins/index.ts
@@ -1,12 +1,11 @@
 /// <reference types="cypress" />
 
-// https://github.com/cypress-io/cypress/issues/7575
-import * as add from './add'
+import fn from './commonjs-export-function'
 
-// if esModuleInterop is forced to be true, this will error // with 'add is
+// if esModuleInterop is forced to be true, this will error // with 'fn is
 // not a function'. instead, we allow the tsconfig.json to determine the value
 // of esModuleInterop
-add(1, 2)
+fn()
 
 // Default Cypress plugin function
 export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
