487e4f9b5fa9f14fe6182817288f4eed6fda68c4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-11 22:27:27 -0400

    more comments for selector example for #376

--- cypress/component/advanced/react-book-example/src/components/ProductsList.spec.js
+++ cypress/component/advanced/react-book-example/src/components/ProductsList.spec.js
@@ -18,24 +18,45 @@ it('renders without crashing', () => {
       }),
     })
   mount(<ProductsList />)
+
+  // query DOM elements
   cy.contains('First item').should('be.visible')
   cy.get('.product').should('have.length', 2)
 
   // use https://github.com/abhinaba-ghosh/cypress-react-selector
-  // to find DOM elements by React component constructor name or state
+  // to find DOM elements by React component constructor name, props, or state
   cy.waitForReact(1000, '#cypress-root')
+
+  cy.log('**cy.react**')
+  // find the top level <ProductsContainer> that we have mounted
+  // under imported name "ProductsList"
   cy.react('ProductsContainer').should('have.class', 'product-container')
+
+  // for more ways to find components, see
+  // https://github.com/abhinaba-ghosh/cypress-react-selector#how-to-use-react-selector
+  // find all instances of <AProduct> component
   cy.react('AProduct').should('have.length', 2)
+
+  // find a single instance with prop
+  // <AProduct name={'Second item'} />
   cy.react('AProduct', { name: 'Second item' })
     .should('be.visible')
     .and('have.text', 'Second item')
+
+  cy.log('**cy.getReact**')
+
+  // returns React component wrapper with props
   cy.getReact('AProduct', { name: 'Second item' })
     .getProps()
-    .should('have.property', 'name')
+    .should('deep.equal', { name: 'Second item' })
+
   cy.getReact('AProduct', { name: 'First item' })
+    // get single prop
     .getProps('name')
     .should('eq', 'First item')
+
+  cy.log('**.getCurrentState**')
   cy.getReact('AProduct', { name: 'Second item' })
     .getCurrentState()
-    .should('not.empty')
+    .should('deep.equal', { name: 'Second item' })
 })
