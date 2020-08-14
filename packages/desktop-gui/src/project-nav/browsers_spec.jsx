d398acb353d1b5533210e8aef906b9e8d252c842
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-14 15:20:15 -0400

    add tests for loading and saving of the chosen browser

--- packages/desktop-gui/src/project-nav/browsers_spec.jsx
+++ packages/desktop-gui/src/project-nav/browsers_spec.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Browsers from './browsers'
+import Project from '../project/project-model'
 import { mount } from 'cypress-react-unit-test'
 import browsers from '../../cypress/fixtures/browsers.json'
 
 /* global cy */
 describe('Browsers', () => {
+  /**
+   * Mounts the Browsers component surrounded
+   * by the right markup for realistic styles
+   */
   const mnt = (props) => {
     const BrowserNav = () =>
       (<nav className="project-nav navbar navbar-default">
@@ -15,6 +20,17 @@ describe('Browsers', () => {
     mount(<BrowserNav />, {
       stylesheets: '/__root/dist/app.css',
     })
+  }
+
+  /**
+   * Creates an instance of the full Project Model class
+   */
+  const mntProjectModel = () => {
+    const project = new Project({})
+
+    project.setBrowsers(browsers)
+
+    mnt({ project })
   }
 
   const browserListClosed = () => {
@@ -63,5 +79,38 @@ describe('Browsers', () => {
     cy.log('**Canary was picked**')
     cy.get('@setChosenBrowser').should('have.been.calledWith', browsers[2])
     browserListClosed()
+  })
+
+  it('saves the selected browser', () => {
+    mntProjectModel()
+    cy.get('.dropdown-chosen').click()
+    cy.contains('li', 'Canary 48').click()
+    // TODO later one we should save the entire browser object
+    // with name and channel
+    // https://github.com/cypress-io/cypress/issues/8281
+    cy.wrap(window.localStorage)
+    .invoke('getItem', 'chosenBrowser').should('equal', 'chrome')
+  })
+
+  context('previously chosen browser', () => {
+    it('picks the first browser if there is nothing saved', () => {
+      window.localStorage.clear()
+      mntProjectModel()
+      cy.contains('li', 'Chrome 50').should('be.visible')
+    })
+
+    it('finds browser by name', () => {
+      // TODO update this to full browser with channel
+      // https://github.com/cypress-io/cypress/issues/8281
+      window.localStorage.setItem('chosenBrowser', 'chrome')
+      mntProjectModel()
+      cy.contains('li', 'Chrome 50').should('be.visible')
+    })
+
+    it('sets default browser if cannot find', () => {
+      window.localStorage.setItem('chosenBrowser', 'no-no-no')
+      mntProjectModel()
+      cy.contains('li', 'Chrome 50').should('be.visible')
+    })
   })
 })
