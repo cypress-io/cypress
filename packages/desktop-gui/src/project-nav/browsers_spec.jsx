39e02ba087392cd9dd129cdb42a78a80830c7cfd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-14 14:52:43 -0400

    expand browsers component spec

--- packages/desktop-gui/src/project-nav/browsers_spec.jsx
+++ packages/desktop-gui/src/project-nav/browsers_spec.jsx
@@ -5,6 +5,26 @@ import browsers from '../../cypress/fixtures/browsers.json'
 
 /* global cy */
 describe('Browsers', () => {
+  const mnt = (props) => {
+    const BrowserNav = () =>
+      (<nav className="project-nav navbar navbar-default">
+        <div className="spacer" />
+        <Browsers {...props} />
+      </nav>)
+
+    mount(<BrowserNav />, {
+      stylesheets: '/__root/dist/app.css',
+    })
+  }
+
+  const browserListClosed = () => {
+    cy.get('.browser-icon:visible').should('have.length', 1)
+  }
+
+  const browserListOpened = () => {
+    cy.get('.browser-icon:visible').should('have.length', 3)
+  }
+
   it('renders list', () => {
     const project = {
       browsers,
@@ -12,16 +32,36 @@ describe('Browsers', () => {
       otherBrowsers: browsers.slice(1),
     }
 
-    mount(<Browsers project={project} />, {
-      stylesheets: '/__root/dist/app.css',
-    })
+    mnt({ project })
 
     // only single browser icon visible at the start
     cy.get('.browser-icon').should('have.length', 3)
-    cy.get('.browser-icon:visible').should('have.length', 1)
+    browserListClosed()
 
     // now all the browser icons should be visible
     cy.get('.dropdown-chosen').click()
-    cy.get('.browser-icon:visible').should('have.length', 3)
+    browserListOpened()
+
+    cy.log('**closing**')
+    cy.get('.spacer').click()
+  })
+
+  it('picks the browser', () => {
+    const project = {
+      browsers,
+      chosenBrowser: browsers[0],
+      otherBrowsers: browsers.slice(1),
+      setChosenBrowser: cy.stub().as('setChosenBrowser'),
+    }
+
+    mnt({ project })
+
+    // now all the browser icons should be visible
+    cy.get('.dropdown-chosen').click()
+    cy.contains('li', 'Canary 48').click()
+
+    cy.log('**Canary was picked**')
+    cy.get('@setChosenBrowser').should('have.been.calledWith', browsers[2])
+    browserListClosed()
   })
 })
