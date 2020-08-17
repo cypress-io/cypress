093f3e7505096e24b2d49d53dbf2e9b3a31dd6fe
Chris Breiding <chrisbreiding@users.noreply.github.com>
2020-08-17 12:59:55 -0400

    fix: Avoid looking up remote jquery instance if subject is not an element (#8300)

--- packages/driver/src/cy/jquery.js
+++ packages/driver/src/cy/jquery.js
@@ -14,14 +14,16 @@ const create = function (state) {
 
   return {
     getRemotejQueryInstance (subject) {
-      const remoteJQuery = jquery()
-
       // we make assumptions that you cannot have
       // an array of mixed types, so we only look at
       // the first item (if there's an array)
       const firstSubject = $utils.unwrapFirst(subject)
 
-      if ($dom.isElement(firstSubject) && remoteJQueryisNotSameAsGlobal(remoteJQuery)) {
+      if (!$dom.isElement(firstSubject)) return
+
+      const remoteJQuery = jquery()
+
+      if (remoteJQueryisNotSameAsGlobal(remoteJQuery)) {
         const remoteSubject = remoteJQuery(subject)
 
         return remoteSubject
