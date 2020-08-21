c6b095caa6aea3cd479364f2f2f115543460dbe5
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2020-08-21 16:56:59 -0400

    rename e2e -> issues test folder

--- packages/runner/cypress/integration/issues/issue-8350.js
+++ packages/runner/cypress/integration/issues/issue-8350.js
@@ -1,6 +1,7 @@
 const { createCypress } = require('../support/helpers')
 const { runIsolatedCypress } = createCypress()
 
+// https://github.com/cypress-io/cypress/issues/8350
 describe('issue-8350', () => {
   it('does not hang on nested hook', () => {
     runIsolatedCypress(() => {
