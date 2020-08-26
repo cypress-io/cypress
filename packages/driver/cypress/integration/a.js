b2b60ed42bc1c83a37d0ba48349aa6e3e5049efa
KHeo <sainthkh@naver.com>
2020-08-26 16:25:46 +0900

    Refine test.

--- packages/driver/cypress/integration/a.js
+++ packages/driver/cypress/integration/a.js
@@ -1,12 +1,7 @@
 /// <reference types="cypress" />
 
-// There is uncaught exception on the editor side, when using cy.type
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
-})
-
-describe('page', () => {
-  it('works in Firefox, fails in Chrome', () => {
+describe('slatejs', () => {
+  it('test', () => {
     cy.visit('http://www.slatejs.org/examples/plaintext')
 
     const testString = 'Hello world'
