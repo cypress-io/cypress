63512bae436329ea4962fb3bb3a81b9bad0d01ba
KHeo <sainthkh@naver.com>
2020-08-27 16:56:42 +0900

    Create local version of the test fixture.

--- packages/driver/cypress/integration/issues/7088_spec.js
+++ packages/driver/cypress/integration/issues/7088_spec.js
@@ -2,15 +2,19 @@
 
 describe('slatejs', () => {
   it('test', () => {
-    cy.visit('http://www.slatejs.org/examples/plaintext')
+    cy.visit('fixtures/issue-7088.html')
 
     const testString = 'Hello world'
 
     cy.get('[data-slate-editor="true"]')
-    .type('{ctrl}{shift}{backspace}', { release: false, noUpdate: true })
     .type(testString, { noUpdate: true })
 
     cy.contains('[data-slate-string="true"]', testString)
     .should('be.visible')
+
+    cy.get('[data-slate-editor="true"]')
+    .type('{ctrl}{shift}{backspace}', { release: false, noUpdate: true })
+
+    cy.get('span[contenteditable="false"]').should('have.text', 'Enter some plain text...')
   })
 })
