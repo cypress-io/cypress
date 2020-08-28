ce992c6bd21a4f91827c1e7a4c54971199049ae1
KHeo <sainthkh@naver.com>
2020-08-28 15:07:17 +0900

    Move options_spec.js to log_spec.js Simple test for displayProps.

--- packages/driver/cypress/integration/commands/log_spec.js
+++ packages/driver/cypress/integration/commands/log_spec.js
@@ -302,3 +302,194 @@ describe('log options on success', () => {
     cy.writeFile('./cypress/_test-output/test.txt', 'test', options)
   })
 })
+
+describe('last arg can be an object, but not an option', () => {
+  before(() => {
+    cy.visit('/fixtures/command-log.html')
+  })
+
+  beforeEach(() => {
+    this.logs = []
+
+    cy.on('log:added', (attrs, log) => {
+      this.logs.push(log)
+    })
+  })
+
+  afterEach(() => {
+    cy.removeAllListeners('log:added')
+  })
+
+  it('check', () => {
+    cy.on('fail', () => {
+      expect(this.logs[1].get('options')).to.deep.eq(null)
+    })
+
+    cy.get('#b').check(['us', 'ca'])
+  })
+
+  it('clearLocalStorage', () => {
+    cy.on('log:added', () => {
+      cy.removeAllListeners('log:added')
+      expect(this.logs[0].get('message')).to.eq('/app-/')
+    })
+
+    cy.clearLocalStorage(/app-/)
+  })
+
+  it('clock', () => {
+    cy.on('fail', () => {
+      expect(this.logs[0].get('options')).to.deep.eq(null)
+    })
+
+    cy.clock('invoke failure', ['setTimeout', 'clearTimeout'])
+  })
+
+  it('contains', () => {
+    cy.on('fail', () => {
+      expect(this.logs[0].get('options')).to.deep.eq({})
+    })
+
+    cy.contains('', /app-/)
+  })
+
+  it('invoke', () => {
+    cy.on('fail', () => {
+      expect(this.logs[1].get('options')).to.deep.eq({
+        log: true,
+      })
+    })
+
+    cy.wrap({}).invoke({ log: true }, 'non-exist')
+  })
+
+  it('nextUntil - jquery', () => {
+    cy.on('log:added', (attrs, log) => {
+      if (log.get('name') === 'nextUntil') {
+        cy.removeAllListeners('log:added')
+        expect(log.get('options')).to.deep.eq({})
+      }
+    })
+
+    cy.get('div').nextUntil(cy.$$('.warning'))
+  })
+
+  it('nextUntil - DOM', () => {
+    cy.on('log:added', (attrs, log) => {
+      if (log.get('name') === 'nextUntil') {
+        cy.removeAllListeners('log:added')
+        expect(log.get('options')).to.deep.eq({})
+      }
+    })
+
+    cy.get('div').nextUntil(document.getElementsByClassName('warning')[0])
+  })
+
+  it('parentsUntil - jquery', () => {
+    cy.on('log:added', (attrs, log) => {
+      if (log.get('name') === 'parentsUntil') {
+        cy.removeAllListeners('log:added')
+        expect(log.get('options')).to.deep.eq({})
+      }
+    })
+
+    cy.get('div').parentsUntil(cy.$$('.warning'))
+  })
+
+  it('parentsUntil - DOM', () => {
+    cy.on('log:added', (attrs, log) => {
+      if (log.get('name') === 'parentsUntil') {
+        cy.removeAllListeners('log:added')
+        expect(log.get('options')).to.deep.eq({})
+      }
+    })
+
+    cy.get('div').parentsUntil(document.getElementsByClassName('warning')[0])
+  })
+
+  it('prevUntil - jquery', () => {
+    cy.on('log:added', (attrs, log) => {
+      if (log.get('name') === 'prevUntil') {
+        cy.removeAllListeners('log:added')
+        expect(log.get('options')).to.deep.eq({})
+      }
+    })
+
+    cy.get('div').prevUntil(cy.$$('.warning'))
+  })
+
+  it('prevUntil - DOM', () => {
+    cy.on('log:added', (attrs, log) => {
+      if (log.get('name') === 'prevUntil') {
+        cy.removeAllListeners('log:added')
+        expect(log.get('options')).to.deep.eq({})
+      }
+    })
+
+    cy.get('div').prevUntil(document.getElementsByClassName('warning')[0])
+  })
+
+  it('select', () => {
+    cy.on('log:added', (attrs) => {
+      if (attrs.name === 'select') {
+        expect(this.logs[1].get('options')).to.deep.eq({})
+      }
+    })
+
+    cy.get('#auto-test').select(['Cypress', 'Jest'])
+  })
+
+  it('spread', () => {
+    cy.on('fail', () => {
+      expect(this.logs[1].get('options')).to.deep.eq({
+        timeout: 1000,
+      })
+    })
+
+    cy.wrap([1, 2, 3]).spread({ timeout: 1000 }, (a, b, c) => {
+      throw new Error()
+    })
+  })
+
+  it('task', () => {
+    cy.on('fail', () => {
+      expect(this.logs[0].get('options')).to.deep.eq({})
+    })
+
+    cy.task('throw', { a: 1, b: 2 })
+  })
+
+  it('then', () => {
+    cy.on('fail', () => {
+      expect(this.logs[1].get('options')).to.deep.eq({
+        timeout: 1000,
+      })
+    })
+
+    cy.wrap('a').then({ timeout: 1000 }, () => {
+      throw new Error('hi?')
+    })
+  })
+
+  it('uncheck', () => {
+    cy.on('fail', () => {
+      expect(this.logs[1].get('options')).to.deep.eq(null)
+    })
+
+    cy.get('#b').uncheck(['us', 'ca'])
+  })
+})
+
+const log = require('../../../src/cypress/log')
+
+describe('display props are relayed correctly', () => {
+  it('timeout and wallClockStartedAt', () => {
+    const props = log.getDisplayProps({
+      timeout: 4000,
+      wallClockStartedAt: new Date().toJSON(),
+    })
+
+    expect(props).to.haveOwnProperty('timeout')
+    expect(props).to.haveOwnProperty('wallClockStartedAt')
+  })
+})
