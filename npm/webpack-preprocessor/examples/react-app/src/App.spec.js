43478e2d1f0d508eee7f22fd43993f2babf4b76b
Zach Panzarino <zachary@panzarino.com>
2020-09-14 16:01:03 -0400

    Fix webpack preprocessor eslint issues

--- npm/webpack-preprocessor/examples/react-app/src/App.spec.js
+++ npm/webpack-preprocessor/examples/react-app/src/App.spec.js
@@ -1,6 +1,6 @@
-import React from "react";
-import App, {toggleOneTodo} from "./App";
-import {mount} from 'cypress-react-unit-test'
+import React from 'react'
+import App, { toggleOneTodo } from './App'
+import { mount } from 'cypress-react-unit-test'
 
 describe('App', () => {
   beforeEach(() => {
@@ -8,9 +8,9 @@ describe('App', () => {
       <App />,
       {
         stylesheets: [
-          'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css'
-        ]
-      }
+          'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css',
+        ],
+      },
     )
   })
 
@@ -18,64 +18,66 @@ describe('App', () => {
     cy.get('.todo').should('have.length', 3)
     cy.get('input.input').type('Test with Cypress{enter}')
     cy.get('.todo').should('have.length', 4)
-      .contains('Meet friend for lunch')
-      .find('[data-cy=remove]').click()
-
+    .contains('Meet friend for lunch')
+    .find('[data-cy=remove]').click()
 
     cy.get('.todo').should('have.length', 3)
   })
 
   it('toggles correctly', () => {
     const todos = [{
-      isCompleted: false
+      isCompleted: false,
     }, {
-      isCompleted: false
+      isCompleted: false,
     }, {
-      isCompleted: false
+      isCompleted: false,
     }]
     const newTodos = toggleOneTodo(todos, 2)
+
     expect(newTodos).to.deep.equal([{
-        isCompleted: false
-      }, {
-        isCompleted: false
-      }, {
-        isCompleted: true
-      }])
+      isCompleted: false,
+    }, {
+      isCompleted: false,
+    }, {
+      isCompleted: true,
+    }])
   })
 
   it('ignores invalid index', () => {
     const todos = [{
-      isCompleted: false
+      isCompleted: false,
     }, {
-      isCompleted: false
+      isCompleted: false,
     }, {
-      isCompleted: false
+      isCompleted: false,
     }]
     const newTodos = toggleOneTodo(todos, 20)
+
     expect(newTodos).to.deep.equal(todos)
   })
 
   it('toggles an item', () => {
     cy.get('.todo').should('have.length', 3)
-      .contains('Learn about React')
-      .contains('button', 'Complete').click()
-    cy.contains('Learn about React')
-      .should('have.css', 'text-decoration',
-        'line-through solid rgb(74, 74, 74)')
-      .then(() => {
-        cy.wrap(window.todos[0]).should('deep.equal', {
-          text: 'Learn about React',
-          isCompleted: true
-        })
-      })
+    .contains('Learn about React')
+    .contains('button', 'Complete').click()
 
     cy.contains('Learn about React')
-      .contains('button', 'Redo').click()
-      .then(() => {
-        cy.wrap(window.todos[0]).should('deep.equal', {
-          text: 'Learn about React',
-          isCompleted: false
-        })
+    .should('have.css', 'text-decoration',
+      'line-through solid rgb(74, 74, 74)')
+    .then(() => {
+      cy.wrap(window.todos[0]).should('deep.equal', {
+        text: 'Learn about React',
+        isCompleted: true,
       })
+    })
+
+    cy.contains('Learn about React')
+    .contains('button', 'Redo').click()
+    .then(() => {
+      cy.wrap(window.todos[0]).should('deep.equal', {
+        text: 'Learn about React',
+        isCompleted: false,
+      })
+    })
   })
 })
