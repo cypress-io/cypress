a0b70aba8b0151ed12ab641facaf3ff5135f6424
KHeo <sainthkh@naver.com>
2020-09-14 15:18:13 +0900

    Fix firefox test.

--- packages/driver/cypress/integration/issues/7088_spec.js
+++ packages/driver/cypress/integration/issues/7088_spec.js
@@ -7,13 +7,19 @@ describe('slatejs', () => {
     const testString = 'Hello world'
 
     cy.get('[data-slate-editor="true"]')
-    .type(testString, { })
+    .type(testString)
 
     cy.contains('[data-slate-string="true"]', testString)
     .should('be.visible')
 
     cy.get('[data-slate-editor="true"]')
     .type('{ctrl}{shift}{backspace}', { release: false })
+
+    // In Firefox, {ctrl}{shift}{backspace} deletes only one word.
+    if (Cypress.isBrowser('firefox')) {
+      cy.get('[data-slate-editor="true"]')
+      .type('{ctrl}{shift}{backspace}', { release: false })
+    }
 
     cy.get('span[contenteditable="false"]').should('have.text', 'Enter some plain text...')
   })
