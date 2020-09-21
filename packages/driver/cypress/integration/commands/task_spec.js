379ad6498925ae4c53da09ee8c5cb97e1b59f32f
Kukhyeon Heo <sainthkh@naver.com>
2020-09-21 23:34:05 +0900

    fix: cy.task() can pass undefined as default argument. (#8600)
    Co-authored-by: Gleb Bahmutov <gleb.bahmutov@gmail.com>

--- packages/driver/cypress/integration/commands/task_spec.js
+++ packages/driver/cypress/integration/commands/task_spec.js
@@ -38,6 +38,10 @@ describe('src/cy/commands/task', () => {
       cy.task('return:arg', 'works').should('eq', 'works')
     })
 
+    it('returns the default value when no argument is given', () => {
+      cy.task('arg:is:undefined').should('eq', 'arg was undefined')
+    })
+
     describe('.log', () => {
       beforeEach(function () {
         this.logs = []
@@ -205,7 +209,7 @@ describe('src/cy/commands/task', () => {
           expect(lastLog.get('error')).to.eq(err)
           expect(lastLog.get('state')).to.eq('failed')
 
-          expect(err.message).to.eq(`\`cy.task('bar')\` failed with the following error:\n\nThe task 'bar' was not handled in the plugins file. The following tasks are registered: return:arg, wait, create:long:file\n\nFix this in your plugins file here:\n${Cypress.config('pluginsFile')}\n\nhttps://on.cypress.io/api/task`)
+          expect(err.message).to.eq(`\`cy.task('bar')\` failed with the following error:\n\nThe task 'bar' was not handled in the plugins file. The following tasks are registered: return:arg, arg:is:undefined, wait, create:long:file\n\nFix this in your plugins file here:\n${Cypress.config('pluginsFile')}\n\nhttps://on.cypress.io/api/task`)
 
           done()
         })
