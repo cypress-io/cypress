48fcfdf1e527aeb12deaab8f1878521ffb901bca
Zach Panzarino <zachary@panzarino.com>
2020-09-23 15:12:33 -0400

    Add semantic release

---
+++ scripts/semantic-release.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+
+const { execSync } = require('child_process')
+const argv = require('minimist')(process.argv.slice(2))
+
+function exec (command, args = {}) {
+  console.log(command)
+  execSync(command, { stdio: 'inherit', ...args })
+}
+
+const pack = argv._[0]
+
+console.log(`Running semantic release for ${pack}`)
+
+exec(`lerna exec --scope ${pack} -- npx --no-install semantic-release`)
