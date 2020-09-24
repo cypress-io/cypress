7f79c3de74c9511884796c1cc41e66963140c3bc
Zach Panzarino <zachary@panzarino.com>
2020-09-24 19:04:51 -0400

    chore: set up monorepo (#8663)

---
+++ scripts/semantic-release.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+const { execSync } = require('child_process')
+const path = require('path')
+const argv = require('minimist')(process.argv.slice(2))
+
+function exec (command, args = {}) {
+  console.log(command)
+  execSync(command, { stdio: 'inherit', ...args })
+}
+
+const pack = argv._[0]
+
+console.log(`Running semantic release for ${pack}`)
+
+exec(`node ${path.join(__dirname, 'inject-npm-version.js')} ${pack}`)
+exec(`lerna exec --scope ${pack} -- npx --no-install semantic-release`)
