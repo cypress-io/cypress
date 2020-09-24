c94e43a7f30c00cb2ca589b9f8ae052fae60933e
Zach Panzarino <zachary@panzarino.com>
2020-09-24 15:42:49 -0400

    update some cases of injecting npm version

--- scripts/inject-npm-version.js
+++ scripts/inject-npm-version.js
@@ -24,24 +24,50 @@ const main = async () => {
   const pack = packages.find((p) => p.name === name)
 
   if (!pack) {
-    return console.log(`Couldn't find package ${name}`)
+    console.log(`Couldn't find package ${name}`)
+    process.exit(1)
   }
 
   const packagePath = path.join(pack.location, 'package.json')
   const packageJson = JSON.parse(fs.readFileSync(packagePath))
+
+  if (!packageJson.dependencies) {
+    console.log(`No dependencies so we're done here`)
+    process.exit(0)
+  }
+
   const dependencies = Object.keys(packageJson.dependencies).filter((d) => localPackages.includes(d))
 
   for (let dep of dependencies) {
     process.stdout.write(`${dep}: `)
 
+    if (dep === 'cypress') {
+      const { stdout: root } = await execa('git', ['rev-parse', '--show-toplevel'])
+      const rootPath = path.join(root, 'package.json')
+      const rootPackage = JSON.parse(fs.readFileSync(rootPath))
+
+      packageJson.dependencies[dep] = rootPackage.version
+
+      break
+    }
+
+    if (packages.find((p) => p.name === dep).private) {
+      console.log(`ERROR`)
+      console.log(`Cannot add ${dep} as a dependency since it is private`)
+      process.exit(1)
+    }
+
     const { stdout: semrel } = await execa('npx', ['lerna', 'exec', '--scope', dep, '--', 'npx', '--no-install', 'semantic-release', '--dry-run'])
-    const currentVersion = semrel.match(/associated with version (\d+\.\d+\.\d+)/)[1]
-    const nextVersion = semrel.match(/The next release version is (\d+\.\d+\.\d+)/)[1]
+    const currentVersion = (semrel.match(/associated with version (\d+\.\d+\.\d+)/) || [])[1]
+    const nextVersion = (semrel.match(/The next release version is (\d+\.\d+\.\d+)/) || [])[1]
 
     const version = nextVersion || currentVersion
 
     if (!version) {
-      return console.log(`Couldn't find a current or next version for ${dep}.`)
+      console.log(`ERROR`)
+      console.log(`Couldn't find a current or next version for ${dep}`)
+
+      process.exit(1)
     }
 
     console.log(version)
--- scripts/semantic-release.js
+++ scripts/semantic-release.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 const { execSync } = require('child_process')
+const path = require('path')
 const argv = require('minimist')(process.argv.slice(2))
 
 function exec (command, args = {}) {
@@ -12,5 +13,5 @@ const pack = argv._[0]
 
 console.log(`Running semantic release for ${pack}`)
 
-exec(`node ./scripts/inject-npm-version.js ${pack}`)
+exec(`node ${path.join(__dirname, 'inject-npm-version.js')} ${pack}`)
 exec(`lerna exec --scope ${pack} -- npx --no-install semantic-release`)
