66f7b92966bad6fffb02323c29ce0b3b0d6a71a8
Zach Bloomquist <github@chary.us>
2020-09-29 15:50:15 -0400

    use vfr with wallclock timestamps on FF

--- packages/server/lib/video_capture.js
+++ packages/server/lib/video_capture.js
@@ -152,6 +152,7 @@ module.exports = {
         })
         .videoCodec('libx264')
         .outputOptions('-preset ultrafast')
+        .inputOptions('-use_wallclock_as_timestamps 1')
         .on('start', (command) => {
           debug('capture started %o', { command })
 
@@ -181,13 +182,9 @@ module.exports = {
           cmd
           .inputFormat('webm')
 
-          // assume 18 fps. This number comes from manual measurement of avg fps coming from firefox.
-          // TODO: replace this with the 'vfr' option below when dropped frames issue is fixed.
-          .inputFPS(18)
-
-          // 'vsync vfr' (variable framerate) works perfectly but fails on top page navigation
-          // since video timestamp resets to 0, timestamps already written will be dropped
-          // .outputOption('-vsync vfr')
+          // 'vsync vfr' (variable framerate) works because timestamps are discarded via
+          // of use_wallclock_as_timestamps
+          .outputOption('-vsync vfr')
 
           // this is to prevent the error "invalid data input" error
           // when input frames have an odd resolution
@@ -198,7 +195,6 @@ module.exports = {
         } else {
           cmd
           .inputFormat('image2pipe')
-          .inputOptions('-use_wallclock_as_timestamps 1')
         }
 
         return cmd.save(name)
--- packages/server/test/support/fixtures/projects/e2e/cypress/integration/video_compression_spec.coffee
+++ packages/server/test/support/fixtures/projects/e2e/cypress/integration/video_compression_spec.coffee
@@ -1,3 +1,13 @@
-Cypress._.times Cypress.env('NUM_TESTS'), (i) ->
+halfTests = Cypress.env('NUM_TESTS') / 2
+msPerTest = Cypress.env('MS_PER_TEST')
+
+Cypress._.times halfTests, (i) ->
   it "num: #{i+1} makes some long tests", ->
-    cy.wait(Cypress.env('MS_PER_TEST'))
+    cy.wait(msPerTest)
+
+it "top-level navigation", ->
+  cy.visit('http://localhost:38883')
+
+Cypress._.times halfTests, (i) ->
+  it "num: #{i+1 + halfTests} makes some long tests", ->
+    cy.wait(msPerTest)
