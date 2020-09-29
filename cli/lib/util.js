4da8d180d900f967515c59d548a68491d8505d74
Zach Bloomquist <github@chary.us>
2020-09-29 13:28:29 -0400

    update invalid HTTP header workaround
    node 12.16.3 removed `--http-parser=legacy`, but now we have `insecureHTTPParser` available on `ClientRequest`, which does the same thing but with `llhttp`

--- cli/lib/util.js
+++ cli/lib/util.js
@@ -295,7 +295,7 @@ const util = {
     }
 
     // https://github.com/cypress-io/cypress/issues/5431
-    const NODE_OPTIONS = `--max-http-header-size=${1024 ** 2} --http-parser=legacy`
+    const NODE_OPTIONS = `--max-http-header-size=${1024 ** 2}`
 
     if (_.isString(process.env.NODE_OPTIONS)) {
       return {
