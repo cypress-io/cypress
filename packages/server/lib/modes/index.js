1c974b74d1271008c23816dc73d226152fdc17ba
Thorsten Lorenz <thlorenz@gmx.de>
2020-10-01 15:56:57 -0500

    bundle: hardcoding interactive mode to simplify entry

--- packages/server/lib/modes/index.js
+++ packages/server/lib/modes/index.js
@@ -1,14 +1,16 @@
 module.exports = (mode, options) => {
+  return require('./interactive').run(options)
+  /*
   switch (mode) {
     case 'record':
       return require('./record').run(options)
     case 'run':
       return require('./run').run(options)
     case 'interactive':
-      return require('./interactive').run(options)
     case 'smokeTest':
       return require('./smoke_test').run(options)
     default:
       break
   }
+*/
 }
