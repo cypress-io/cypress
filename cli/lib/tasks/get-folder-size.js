63acb266534c595ebed85aeb2571483345202047
KHeo <sainthkh@naver.com>
2020-10-05 13:56:53 +0900

    Add JSDoc.

--- cli/lib/tasks/get-folder-size.js
+++ cli/lib/tasks/get-folder-size.js
@@ -2,6 +2,15 @@ const fs = require('../fs')
 const { join } = require('path')
 const Bluebird = require('bluebird')
 
+/**
+ * Get the size of a folder or a file.
+ *
+ * This function returns the actual file size of the folder (size), not the allocated space on disk (size on disk).
+ * For more details between the difference, check this link:
+ * https://www.howtogeek.com/180369/why-is-there-a-big-difference-between-size-and-size-on-disk/
+ *
+ * @param {string} path path to the file or the folder.
+ */
 async function getSize (path) {
   const stat = await fs.lstat(path)
 
