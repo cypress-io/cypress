ae5cd841f60dc149354efdd50b77a7226929d350
Chris Breiding <chrisbreiding@users.noreply.github.com>
2020-10-06 10:06:55 -0400

    feat: Redesign Desktop GUI updates modal (#8751)
    Co-authored-by: CypressAdam <adam@cypress.io>
    Co-authored-by: Jennifer Shehane <jennifer@cypress.io>

--- packages/desktop-gui/cypress/integration/updates_spec.js
+++ packages/desktop-gui/cypress/integration/updates_spec.js
@@ -5,6 +5,7 @@ const NEW_VERSION = '1.3.4'
 
 describe('Updates', function () {
   beforeEach(function () {
+    cy.viewport(800, 500)
     cy.fixture('user').as('user')
     cy.fixture('projects').as('projects')
     cy.fixture('projects_statuses').as('projectStatuses')
@@ -21,6 +22,7 @@ describe('Updates', function () {
       cy.stub(this.ipc, 'getCurrentUser').resolves(this.user)
       cy.stub(this.ipc, 'windowOpen')
       cy.stub(this.ipc, 'externalOpen')
+      cy.stub(this.ipc, 'setClipboardText')
 
       this.updaterCheck = this.util.deferred()
 
@@ -88,6 +90,9 @@ describe('Updates', function () {
   })
 
   describe('in project mode', function () {
+    const npmCommand = `npm install --save-dev cypress@${NEW_VERSION}`
+    const yarnCommand = `yarn upgrade cypress@${NEW_VERSION}`
+
     beforeEach(function () {
       cy.stub(this.ipc, 'getOptions').resolves({ version: OLD_VERSION, projectRoot: '/foo/bar' })
       this.start()
@@ -96,22 +101,63 @@ describe('Updates', function () {
       cy.get('.footer .version').click()
     })
 
-    it('modal has info about updating package.json', function () {
-      cy.get('.modal').contains(`npm install --save-dev cypress@${NEW_VERSION}`)
-
-      cy.get('.modal').contains(`yarn upgrade cypress@${NEW_VERSION}`)
+    it('modal has info about upgrading via package manager', function () {
+      cy.get('.modal').contains(npmCommand)
+      cy.get('.modal').contains(yarnCommand)
       cy.percySnapshot()
     })
 
-    it('links to \'open\' doc on click of open command', function () {
-      cy.get('.modal').contains('cypress open').click().then(() => {
-        expect(this.ipc.externalOpen).to.be.calledWith('https://on.cypress.io/how-to-open-cypress')
+    it('copies npm upgrade command to clipboard', function () {
+      cy.contains(npmCommand).find('button').click()
+      .then(() => {
+        expect(this.ipc.setClipboardText).to.be.calledWith(npmCommand)
       })
     })
 
-    it('opens changelog when new version is clicked', function () {
-      cy.get('.modal').contains(NEW_VERSION).click().then(() => {
-        expect(this.ipc.externalOpen).to.be.calledWith('https://on.cypress.io/changelog?source=dgui_footer')
+    it('changes npm upgrade button icon after copying', function () {
+      cy.contains(npmCommand).find('button').click()
+      cy.contains(npmCommand).find('button i').should('have.class', 'fa-check')
+    })
+
+    it('disables npm upgrade button after copying', function () {
+      cy.contains(npmCommand).find('button').click().should('be.disabled')
+    })
+
+    it('resets npm upgrade button after 5 seconds', function () {
+      cy.clock()
+      cy.contains(npmCommand).find('button').click()
+      cy.tick(5000)
+      cy.contains(npmCommand).find('button i').should('have.class', 'fa-copy')
+      cy.contains(npmCommand).find('button').should('not.be.disabled')
+    })
+
+    it('copies yarn upgrade command to clipboard', function () {
+      cy.contains(yarnCommand).find('button').click()
+      .then(() => {
+        expect(this.ipc.setClipboardText).to.be.calledWith(yarnCommand)
+      })
+    })
+
+    it('changes yarn upgrade button icon after copying', function () {
+      cy.contains(yarnCommand).find('button').click()
+      cy.contains(yarnCommand).find('button i').should('have.class', 'fa-check')
+    })
+
+    it('disables yarn upgrade button after copying', function () {
+      cy.contains(yarnCommand).find('button').click().should('be.disabled')
+    })
+
+    it('resets yarn upgrade button after 5 seconds', function () {
+      cy.clock()
+      cy.contains(yarnCommand).find('button').click()
+      cy.tick(5000)
+      cy.contains(yarnCommand).find('button i').should('have.class', 'fa-copy')
+      cy.contains(yarnCommand).find('button').should('not.be.disabled')
+    })
+
+    it('links to \'open\' doc on click of open command', function () {
+      cy.contains('cypress open').click().then(() => {
+        expect(this.ipc.externalOpen).to.be.calledWith('https://on.cypress.io/how-to-open-cypress')
       })
     })
   })
--- packages/desktop-gui/src/update/update-modal.jsx
+++ packages/desktop-gui/src/update/update-modal.jsx
@@ -1,14 +1,12 @@
-import { observer } from 'mobx-react'
+import cs from 'classnames'
+import { observer, useLocalStore } from 'mobx-react'
 import React from 'react'
+import { action } from 'mobx'
 import BootstrapModal from 'react-bootstrap-modal'
 
 import appStore from '../lib/app-store'
 import ipc from '../lib/ipc'
-
-const openChangelog = (e) => {
-  e.preventDefault()
-  ipc.externalOpen('https://on.cypress.io/changelog?source=dgui_footer')
-}
+import { useLifecycle } from '../lib/use-lifecycle'
 
 const openDownload = (e) => {
   e.preventDefault()
@@ -20,39 +18,83 @@ const openCyOpenDoc = (e) => {
   ipc.externalOpen('https://on.cypress.io/how-to-open-cypress')
 }
 
+const UpgradeCommand = observer(({ packageManager, command }) => {
+  const state = useLocalStore(() => ({
+    wasCopied: false,
+    setWasCopied: action((wasCopied) => {
+      state.wasCopied = wasCopied
+    }),
+  }))
+
+  let resetId
+
+  const copyToClipboard = () => {
+    if (state.wasCopied) return
+
+    ipc.setClipboardText(command)
+    state.setWasCopied(true)
+
+    resetId = setTimeout(() => {
+      state.setWasCopied(false)
+    }, 5000)
+  }
+
+  useLifecycle({
+    onUnmount () {
+      // clear timeout when component is unmounted
+      if (resetId) {
+        clearTimeout(resetId)
+      }
+    },
+  })
+
+  const icon = state.wasCopied ? 'fas fa-check' : 'far fa-copy'
+
+  return (
+    <span className={cs({ 'was-copied': state.wasCopied })}>
+      {packageManager}:
+      <code>
+        {command}
+        <button className='copy-to-clipboard' onClick={copyToClipboard} disabled={state.wasCopied}>
+          <i className={icon}></i>
+        </button>
+      </code>
+    </span>
+  )
+})
+
 const Instructions = observer(() => {
   if (appStore.isGlobalMode) {
     return (
-      <ol>
-        <li>
-          <span>
-            <a href='#' onClick={openDownload}><i className='fas fa-download'></i> Download the new version.</a>
-          </span>
-        </li>
-        <li>
-          <span>Quit this app.</span>
-        </li>
-        <li>
-          <span>Extract the download and replace the existing app.</span>
-        </li>
-      </ol>
+      <div className='global-mode-instructions'>
+        <p>To update:</p>
+        <ol>
+          <li>
+            <a href='#' onClick={openDownload}>Download the new version.</a>
+          </li>
+          <li>
+            <span>Extract the download and replace the existing app.<strong>*</strong></span>
+          </li>
+        </ol>
+        <p><i className='fas fa-asterisk' /> Remember to <strong>close this app</strong> before replacing.</p>
+      </div>
     )
   }
 
   return (
-    <ol>
-      <li>
-        <span>Quit this app.</span>
-      </li>
-      <li>
-        <span>If using npm, run <code>npm install --save-dev cypress@{appStore.newVersion}</code></span>
-        <br/>
-        <span>If using yarn, run <code>yarn upgrade cypress@{appStore.newVersion}</code></span>
-      </li>
-      <li>
-        <span>Run <a href='#' onClick={openCyOpenDoc}><code>node_modules/.bin/cypress open</code></a> to open the new version.</span>
-      </li>
-    </ol>
+    <div className='project-mode-instructions'>
+      <p>To get the latest, run the following command<strong>*</strong>:</p>
+      <ol>
+        <li>
+          <UpgradeCommand packageManager='npm' command={`npm install --save-dev cypress@${appStore.newVersion}`} />
+          <UpgradeCommand packageManager='yarn' command={`yarn upgrade cypress@${appStore.newVersion}`} />
+          <span className='footnote'><i className='fas fa-asterisk' /> Remember to <strong>close this app</strong> before installing.</span>
+        </li>
+        <li>
+          <span>Run <a href='#' onClick={openCyOpenDoc}><code>node_modules/.bin/cypress open</code></a> to open the new version.</span>
+        </li>
+      </ol>
+    </div>
   )
 })
 
@@ -60,14 +102,16 @@ const UpdateModal = observer(({ show, onClose }) => {
   return (
     <BootstrapModal show={show} onHide={onClose} backdrop='static'>
       <div className='update-modal modal-body os-dialog'>
-        <BootstrapModal.Dismiss className='btn btn-link close'>x</BootstrapModal.Dismiss>
-        <h4><i className='fas fa-download'></i> Update Available</h4>
-        <p>
-          <a href='#' onClick={openChangelog}><strong>Version {appStore.newVersion}</strong></a> is now available (currently running <strong>Version {appStore.displayVersion}</strong>)
-        </p>
-        <hr />
-        <p><strong>To update Cypress:</strong></p>
-        <Instructions />
+        <BootstrapModal.Dismiss className='close'>
+          <i className='fas fa-times' />
+        </BootstrapModal.Dismiss>
+        <header>
+          <h4><i className='fas fa-arrow-alt-circle-up'></i> Update to Version {appStore.newVersion}</h4>
+        </header>
+        <section>
+          <p>You are currently running <strong>Version {appStore.displayVersion}</strong></p>
+          <Instructions />
+        </section>
       </div>
     </BootstrapModal>
   )
