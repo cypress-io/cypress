ff5244a1b8c41c220c42ee31c18a626d16676578
Zach Panzarino <zachary@panzarino.com>
2020-10-08 17:45:30 -0400

    chore: Add changelog generation for semantic release (#8767)

--- .releaserc.base.js
+++ .releaserc.base.js
@@ -2,6 +2,15 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+    }],
+    ['@semantic-release/git', {
+      assets: [
+        '**/CHANGELOG.md',
+      ],
+      message: 'chore: release ${nextRelease.gitTag}\n\n[skip ci]',
+    }],
     '@semantic-release/npm',
   ],
   extends: 'semantic-release-monorepo',
