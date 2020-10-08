66920a002f7411dab3c0a247cf3021a3891a021b
Greg Lahaye <greg@lahayes.net>
2020-10-08 17:59:08 +0800

    remove space to fix test

--- cli/__snapshots__/cli_spec.js
+++ cli/__snapshots__/cli_spec.js
@@ -112,7 +112,7 @@ exports['cli unknown option shows help for cache command - unknown option --foo 
     list        list cached binary versions
     path        print the path to the binary cache
     clear       delete all cached binaries
-    prune       deletes all cached binaries except for the version currently in 
+    prune       deletes all cached binaries except for the version currently in
                 use
     --size      Used with the list command to show the sizes of the cached
                 folders
@@ -146,7 +146,7 @@ exports['cli unknown option shows help for cache command - unknown sub-command f
     list        list cached binary versions
     path        print the path to the binary cache
     clear       delete all cached binaries
-    prune       deletes all cached binaries except for the version currently in 
+    prune       deletes all cached binaries except for the version currently in
                 use
     --size      Used with the list command to show the sizes of the cached
                 folders
@@ -178,7 +178,7 @@ exports['cli unknown option shows help for cache command - no sub-command 1'] = 
     list        list cached binary versions
     path        print the path to the binary cache
     clear       delete all cached binaries
-    prune       deletes all cached binaries except for the version currently in 
+    prune       deletes all cached binaries except for the version currently in
                 use
     --size      Used with the list command to show the sizes of the cached
                 folders
