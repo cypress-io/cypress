4b03c5750caa96d60c6c03a20c5c0c6479c6e9f3
Jessica Sachs <jess@jessicasachs.io>
2020-10-13 15:00:54 -0400

    chore: refactor support.spec.js

--- npm/vue/cypress/component/support.spec.js
+++ npm/vue/cypress/component/support.spec.js
@@ -6,40 +6,29 @@ describe('support.js', () => {
    * they must be executed *together* to be meaningful.
    */
   describe('beforeEach', () => {
-    describe('body & head target cleanup', () => {
-      const renderAndTestHead = (id) => {
-        const el = Cypress.$(`<style
-                  class="before-each-head-${id} before-each-head">
-                  .before-each-body { color: deeppink; }
-                  </style>`)
+    let count = 0
+    const $ = Cypress.$
 
-        Cypress.$('head').append(el)
+    beforeEach(() => {
+      count++
+      $('body').append(`<h1 class="before-each-body">beforeEach's DOM cleanup: ${count}</h1>`)
+      $('head').append(`<style class="before-each-head">
+          h1 { color: forestgreen; }
+          h1:nth-of-type(2) { color: darkred; }
+         </style>`)
 
-        cy.get(`.before-each-head-${id}`).should('exist')
-        cy.get(`.before-each-head`).should('have.length', 1)
-      }
+      cy.get('head')
+      .find('.before-each-head')
+      .should('exist').and('have.length', 1)
 
-      const renderAndTestBody = (id) => {
-        const el = Cypress.$(`<h1
-                  class="before-each-body-${id} before-each-body">
-                  testing beforeEach's target cleanup: ${id}
-                  </h1>`)
-
-        Cypress.$('body').append(el)
-
-        cy.get(`.before-each-body-${id}`).should('exist')
-        cy.get(`.before-each-body`).should('have.length', 1)
-      }
-
-      it('Setup-only: cleans up the target between it blocks', () => {
-        renderAndTestHead('first')
-        renderAndTestBody('first')
-      })
-
-      it('Actual assertion: should have cleared out the first specs DOM', () => {
-        renderAndTestHead('second')
-        renderAndTestBody('second')
-      })
+      cy.get('body')
+      .find('h1')
+      .should('exist')
+      .and('have.length', 1)
     })
+
+    it('Setup-only: cleans up the target between it blocks', () => {})
+
+    it('Actual assertion: should have cleared out the first specs DOM', () => {})
   })
 })
