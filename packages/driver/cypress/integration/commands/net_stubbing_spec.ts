fc50f59e227c59fcd1a2bf8581f4ed0e1065a431
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2020-10-16 10:22:03 -0400

    add retries to net_stubbing_spec

--- packages/driver/cypress/integration/commands/net_stubbing_spec.ts
+++ packages/driver/cypress/integration/commands/net_stubbing_spec.ts
@@ -1,4 +1,4 @@
-describe('network stubbing', function () {
+describe('network stubbing', { retries: { runMode: 2 } }, function () {
   const { $, _, sinon, state, Promise } = Cypress
 
   beforeEach(function () {
@@ -1013,7 +1013,6 @@ describe('network stubbing', function () {
 
       it('can timeout in request handler', {
         defaultCommandTimeout: 50,
-        retries: 1,
       }, function (done) {
         cy.on('fail', (err) => {
           expect(err.message).to.match(/^A request callback passed to `cy.route2\(\)` timed out after returning a Promise that took more than the `defaultCommandTimeout` of `50ms` to resolve\./)
@@ -1518,7 +1517,6 @@ describe('network stubbing', function () {
 
       it('can timeout in req.reply handler', {
         defaultCommandTimeout: 50,
-        retries: 1,
       }, function (done) {
         cy.on('fail', (err) => {
           expect(err.message).to.match(/^A response callback passed to `req.reply\(\)` timed out after returning a Promise that took more than the `defaultCommandTimeout` of `50ms` to resolve\./)
