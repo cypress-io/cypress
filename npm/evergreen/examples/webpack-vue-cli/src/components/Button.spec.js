775d498358bc1d60ef34e211730a51e0b99d3611
Jessica Sachs <jess@jessicasachs.io>
2020-10-23 21:33:10 -0400

    retryable expect

--- npm/evergreen/examples/webpack-vue-cli/src/components/Button.spec.js
+++ npm/evergreen/examples/webpack-vue-cli/src/components/Button.spec.js
@@ -2,14 +2,28 @@ import Button from './Button'
 import { mount } from '@vue/test-utils'
 import $ from 'cash-dom'
 
-window.mouse = mouse;
+async function retry(fn, n = 0) {
+  try {
+    await fn()
+  } catch (err) {
+    if (n > 20) throw err
+    await new Promise((resolve) => setTimeout(resolve, 10))
+    return retry(fn, n + 1)
+  }
+}
+
 describe.only('Button',  () => {
   it('works', async () => {
     const wrapper = mount(Button, { attachTo: '#root' })
     expect(wrapper.exists()).to.be.ok
 
-    await mouse.click($cyDom.getElementCoordinatesByPosition($(wrapper.vm.$el)).fromElViewport)
+    await mouse.click(
+      $cyDom.getElementCoordinatesByPosition($(wrapper.vm.$el))
+        .fromElViewport
+    )
 
-    expect(wrapper.html()).to.contain('Goodbye')
+    const getElementByTestId = id => document.querySelectorAll(`[data-testid=${id}`)[0]
+
+    await retry(() => expect(getElementByTestId('byeButton').innerText).to.contain('Goodbye'))
   })
 })
--- npm/evergreen/examples/webpack-vue-cli/src/components/Button.vue
+++ npm/evergreen/examples/webpack-vue-cli/src/components/Button.vue
@@ -1,20 +1,29 @@
 <template>
-  <button @click="changeText">{{ text }}</button>
+  <div @click="changeText">
+    <h1>Hello World!</h1>
+    <button data-testid="byeButton" :key="random">{{ text }}</button>
+  </div>
 </template>
 
-<script>
+<script lang="js">
   export default {
     data() {
       return {
-        text: 'Hello'
+        text: 'Hello',
+        random: 0
       }
     },
     methods: {
       changeText() {
         setTimeout(() => {
           this.text = this.text === 'Hello' ? 'Goodbye' : 'Hello'
-        }, 1000)
+        }, 1)
       }
+    },
+    created() {
+      // setInterval(() => {
+      //   this.random = this.random + 1
+      // }, 10)
     }
   }
 </script>
