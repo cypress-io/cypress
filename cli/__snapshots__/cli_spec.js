dc949493844c52e6ea32d0538ece657d2b2c5303
Jennifer Shehane <jennifer@cypress.io>
2020-10-29 17:15:47 +0630

    Add better error message and exit 0 when no binary cache exists for cypress cache list

--- cli/__snapshots__/cli_spec.js
+++ cli/__snapshots__/cli_spec.js
@@ -451,3 +451,7 @@ exports['cli version and binary version with npm log warn'] = `
 Cypress package version: 1.2.3
 Cypress binary version: X.Y.Z
 `
+
+exports['prints explanation when no cache'] = `
+No binary cached versions were found.
+`
