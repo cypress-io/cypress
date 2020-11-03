b53fb1a9d159757ea88055159d58761b4e2b4fab
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-11-03 14:53:43 +0200

    Fix snapshot tests

--- packages/example/cypress/plugins/index.js
+++ packages/example/cypress/plugins/index.js
@@ -15,7 +15,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
