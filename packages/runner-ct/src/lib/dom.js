a6a4dee7cef12ac0a0f64d2877d9dcc3e79a81b6
Brian Mann <brian.mann86@gmail.com>
2020-11-09 04:37:13 -0500

    reference the right paths

--- packages/runner-ct/src/lib/dom.js
+++ packages/runner-ct/src/lib/dom.js
@@ -1,10 +1,10 @@
 import _ from 'lodash'
 
 import { $ } from '@packages/driver'
-import selectorPlaygroundHighlight from '../selector-playground/highlight'
+import selectorPlaygroundHighlight from '@packages/runner/src/selector-playground/highlight'
 // The '!' tells webpack to disable normal loaders, and keep loaders with `enforce: 'pre'` and `enforce: 'post'`
 // This disables the CSSExtractWebpackPlugin and allows us to get the CSS as a raw string instead of saving it to a separate file.
-import selectorPlaygroundCSS from '!../selector-playground/selector-playground.scss'
+import selectorPlaygroundCSS from '!@packages/runner/src/selector-playground/selector-playground.scss'
 
 const styles = (styleString) => {
   return styleString.replace(/\s*\n\s*/g, '')
