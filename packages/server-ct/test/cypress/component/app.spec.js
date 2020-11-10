69671f8629c693b8f208bed91a80406c2d589d94
Brian Mann <brian.mann86@gmail.com>
2020-11-09 21:37:46 -0500

    add some example spec code

---
+++ packages/server-ct/test/cypress/component/app.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+console.log('app.spec.js')
+
+describe('app.spec.js', () => {
+  it('t1', () => {
+
+  })
+})
---
+++ packages/server-ct/test/cypress/component/foo.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+console.log('foo.spec.js')
+
+describe('foo.spec.js', () => {
+  it('t2', () => {
+
+  })
+})
