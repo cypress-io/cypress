af26ac977e966ac29565bdee76a0757390ec9325
Zach Bloomquist <github@chary.us>
2020-11-17 10:28:50 -0500

    Update packages/server/lib/errors.js
    Co-authored-by: Jennifer Shehane <jennifer@cypress.io>

--- packages/server/lib/errors.js
+++ packages/server/lib/errors.js
@@ -903,7 +903,8 @@ const getMsgByType = function (type, arg1 = {}, arg2, arg3) {
         You can safely remove this option from your config.`
     case 'EXPERIMENTAL_NETWORK_STUBBING_REMOVED':
       return stripIndent`\
-        The \`experimentalNetworkStubbing\` configuration option was removed in Cypress version \`6.0.0\`. It is no longer necessary for using \`cy.http()\` (formerly \`cy.route2()\`).
+        The \`experimentalNetworkStubbing\` configuration option was removed in Cypress version \`6.0.0\`. 
+        It is no longer necessary for using \`cy.http()\` (formerly \`cy.route2()\`).
 
         You can safely remove this option from your config.`
     case 'INCOMPATIBLE_PLUGIN_RETRIES':
