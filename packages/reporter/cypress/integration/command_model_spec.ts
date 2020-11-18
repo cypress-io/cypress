427f1d9e56cd3791430c0332d6865019c58cb2ca
Chris Breiding <chrisbreiding@gmail.com>
2020-11-18 10:27:51 -0500

    fix command model spec

--- packages/reporter/cypress/integration/command_model_spec.ts
+++ packages/reporter/cypress/integration/command_model_spec.ts
@@ -1,6 +1,6 @@
 import sinon, { SinonFakeTimers } from 'sinon'
 
-import CommandModel, { CommandProps } from '../../../src/commands/command-model'
+import CommandModel, { CommandProps } from '../../src/commands/command-model'
 
 const LONG_RUNNING_THRESHOLD = 1000
 
