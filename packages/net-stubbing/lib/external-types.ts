3392544e7c4b8bed0f2a4aa6e998e0f20cb2c1b7
Zach Bloomquist <github@chary.us>
2020-11-23 12:47:27 -0500

    Update packages/net-stubbing/lib/external-types.ts
    Co-authored-by: Jennifer Shehane <jennifer@cypress.io>

--- packages/net-stubbing/lib/external-types.ts
+++ packages/net-stubbing/lib/external-types.ts
@@ -406,7 +406,7 @@ declare global {
       // without changing or stubbing its response
       cy.intercept('https://api.example.com/accounts/*').as('getAccount')
       cy.visit('/accounts/123')
-      cy.wait('@getAccount').then((intercepted) => {
+      cy.wait('@getAccount').then((interception) => {
         // we can now access the low level request
         // that contains the request body,
         // response body, status, etc
