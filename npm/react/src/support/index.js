3cc3947fc16b4af751fa84e9b74c7f0f47494a81
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-11-23 16:02:40 +0200

    Remove plugins from src

--- npm/react/src/support/index.js
+++
@@ -1,5 +0,0 @@
-// should be loaded from cypress.json as a support file
-// "supportFile": "node_modules/@cypress/react/support"
-// adds commands from @cypress/react
-require('../hooks')
-require('@cypress/code-coverage/support')
