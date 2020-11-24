398a7ff77a74f5fd51de606ce8b42b733ef72f1c
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-11-24 14:41:55 +0200

    Add vue-cli template

---
+++ npm/create-cypress-tests/src/component-testing/templates/vue/vueCli.ts
@@ -0,0 +1,29 @@
+import * as babel from '@babel/core'
+import { scanFSForAvailableDependency } from '../../../findPackageJson'
+import { Template } from '../Template'
+
+export const VueCliTemplate: Template = {
+  message:
+    'It looks like you are using vue-cli-service to run and build an application.',
+  getExampleUrl: () => 'https://github.com/cypress-io/cypress/tree/develop/npm/vue/examples/cli',
+  recommendedComponentFolder: 'cypress/component',
+  getPluginsCodeAst: () => {
+    return {
+      Require: babel.template.ast(
+        'const preprocessor = require("@cypress/vue/dist/plugins/webpack");',
+      ),
+      ModuleExportsBody: babel.template.ast([
+        'preprocessor(on, config);',
+        '// IMPORTANT return the config object',
+        'return config',
+      ].join('\n'), { preserveComments: true }),
+    }
+  },
+  test: (root) => {
+    const hasVueCliService = scanFSForAvailableDependency(root, ['@vue/cli-service'])
+
+    return {
+      success: hasVueCliService,
+    }
+  },
+}
