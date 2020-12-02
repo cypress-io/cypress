b0dbe8e69de35df07c3ee1216c638f94db75d7d3
Jennifer Shehane <shehane.jennifer@gmail.com>
2020-12-02 15:46:02 +0630

    add test title + link to issue

--- packages/driver/cypress/integration/issues/8998_spec.js
+++ packages/driver/cypress/integration/issues/8998_spec.js
@@ -1,4 +1,5 @@
-it('t', () => {
+// https://github.com/cypress-io/cypress/issues/8998
+it('issue 8998', () => {
   cy.visit('fixtures/issue-8998.html')
   cy.get('.option').then((el) => {
     const x = Cypress.dom.isVisible(el[8])
