c632cd8ccef78a0a27152e22dfc85435c575e759
ElevateBart <ledouxb@gmail.com>
2020-12-03 15:23:54 -0600

    fix check start

--- packages/server-ct/scripts/checkExample.js
+++ packages/server-ct/scripts/checkExample.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-if (!fs.existsSync(path.resolve(__dirname, './examples/node_modules/@vue/cli-service'))) {
+if (!fs.existsSync(path.resolve(__dirname, '../example/node_modules/@vue/cli-service'))) {
   throw new Error(['This script needs "example" to be installed locally. Run the following commands before retrying.', '', 'cd example', 'yarn install'].join('\n'))
 }
