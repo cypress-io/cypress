b7a79030224a614ed6d9f56db5a3b6a463e0cb82
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-12-09 02:55:53 +1000

    chore(cypress-vue): comment out i18n tests momentarily

--- npm/vue/cypress/component/advanced/i18n/TranslatedI18nMessage.vue
+++ npm/vue/cypress/component/advanced/i18n/TranslatedI18nMessage.vue
@@ -1,7 +1,9 @@
 <!-- uses special loader to load i18n blocks -->
 <!-- see https://github.com/intlify/vue-i18n-loader -->
 <!-- You can also inline the translations like <i18n>{...}</i18n> -->
+<!--
 <i18n src="./translations.json"/>
+-->
 
 <template>
   <div id="app">
