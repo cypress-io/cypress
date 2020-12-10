42b111a9bf78bee2b5698f95337c2b94b37a31ca
Zach Panzarino <zachary@panzarino.com>
2020-12-10 15:33:34 -0500

    Update save to file

--- packages/server/lib/util/spec_writer.js
+++ packages/server/lib/util/spec_writer.js
@@ -2,10 +2,12 @@ const fs = require('./fs')
 const { builders, visit } = require('ast-types')
 const recast = require('recast')
 
-const generateCypressCommand = (cmd) => {
+const studioComment = builders.block(' ==== Generated with Cypress Studio ==== ', true, false)
+
+const generateCypressCommand = (cmd, first) => {
   const { selector, command, value } = cmd
 
-  return builders.expressionStatement(
+  const expressionStatement = builders.expressionStatement(
     builders.callExpression(
       builders.memberExpression(
         builders.callExpression(
@@ -21,41 +23,63 @@ const generateCypressCommand = (cmd) => {
       value ? [builders.stringLiteral(value)] : [],
     ),
   )
+
+  if (first) {
+    expressionStatement.comments = [studioComment]
+  }
+
+  return expressionStatement
 }
 
 module.exports = {
   appendStudioCommandsToTest: (fileDetails, commandLog) => {
+    const { absoluteFile, line, column } = fileDetails
+
     const astRules = {
       visitCallExpression (path) {
         const { node } = path
         const { callee } = node
 
-        if (callee.name === 'it' && callee.loc.start.line === fileDetails.line) {
-          const fn = node.arguments[1]
+        let identifier
 
-          if (!fn) {
+        if (callee.type === 'Identifier') {
+          identifier = callee
+        } else if (callee.type === 'MemberExpression') {
+          identifier = callee.object
+        }
+
+        if (identifier) {
+          const columnStart = identifier.loc.start.column + 1
+          const columnEnd = identifier.loc.end.column + 2
+
+          if (identifier.name === 'it' && identifier.loc.start.line === line && columnStart <= column && column <= columnEnd) {
+            const fn = node.arguments[1]
+
+            if (!fn) {
+              return false
+            }
+
+            const body = fn.body.body
+
+            commandLog.forEach((command, index) => {
+              body.push(generateCypressCommand(command, index === 0))
+            })
+
             return false
           }
-
-          const body = fn.body.body
-
-          commandLog.forEach((command) => {
-            body.push(generateCypressCommand(command))
-          })
-
-          return false
         }
 
         this.traverse(path)
       },
     }
 
-    const path = fileDetails.absoluteFile
+    const path = absoluteFile
 
     return fs.readFile(path)
     .then((contents) => {
       const ast = recast.parse(contents, {
         parser: require('@babel/parser'),
+        wrapColumn: 180,
       })
 
       visit(ast, astRules)
