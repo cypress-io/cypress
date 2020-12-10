1249e69549eb27b08d7572c6f86493e13085d2d9
ElevateBart <ledouxb@gmail.com>
2020-12-10 12:28:03 -0600

    fix: debug log names

--- npm/webpack-dev-server/src/index.ts
+++ npm/webpack-dev-server/src/index.ts
@@ -1,13 +1,15 @@
 import { debug as debugFn } from 'debug'
+import { AddressInfo } from 'net'
 import { start as createDevServer } from './startServer'
-const debug = debugFn('@cypress/webpack-dev-server:webpack')
+const debug = debugFn('cypress:webpack-dev-server:webpack')
 
 export async function startDevServer (config, webpackConfig) {
   const webpackDevServer = await createDevServer(webpackConfig, config)
 
   return new Promise((resolve) => {
     const httpSvr = webpackDevServer.listen(0, '127.0.0.1', () => {
-      const port = httpSvr.address().port
+      // FIXME: handle address returning a string
+      const port = (httpSvr.address() as AddressInfo).port
 
       debug('Component testing webpack server started on port', port)
       resolve(port)
--- npm/webpack-dev-server/src/startServer.ts
+++ npm/webpack-dev-server/src/startServer.ts
@@ -1,16 +1,23 @@
 import webpack from 'webpack'
-import webpackDevServer from 'webpack-dev-server'
+import { debug as debugFn } from 'debug'
+import WebpackDevServer from 'webpack-dev-server'
+const debug = debugFn('cypress:webpack-dev-server:start')
 
 import { makeWebpackConfig } from './makeWebpackConfig'
 
-export async function start (userWebpackConfig = {}, { specs, config, devserverEvents }) {
+export async function start (userWebpackConfig = {}, { specs, config, devserverEvents }): Promise<WebpackDevServer> {
   const webpackConfig = await makeWebpackConfig(userWebpackConfig, {
     files: specs,
     projectRoot: config.projectRoot,
     support: '',
     devserverEvents,
   })
+
+  debug('compiling webpack')
+
   const compiler = webpack(webpackConfig)
 
-  return new webpackDevServer(compiler, { hot: true, noInfo: true })
+  debug('starting webpack dev server')
+
+  return new WebpackDevServer(compiler, { hot: true, noInfo: !debug.enabled })
 }
