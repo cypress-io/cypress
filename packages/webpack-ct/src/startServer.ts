1d7654e09ab4af2f2c304c16bc894b941e0bc755
Jessica Sachs <jess@jessicasachs.io>
2020-12-10 20:06:51 -0500

    resolving the webserver promise after 5 seconds to simulate webpack plugin compilation hook

--- packages/webpack-ct/src/startServer.ts
+++ packages/webpack-ct/src/startServer.ts
@@ -3,9 +3,24 @@ import webpackDevServer from 'webpack-dev-server'
 
 import { makeWebpackConfig } from './makeWebpackConfig'
 
-export async function start (userWebpackConfig = {}, testConfig, cypressWebsocket) {
+export async function start (userWebpackConfig = {}, testConfig) {
   const webpackConfig = await makeWebpackConfig(userWebpackConfig, testConfig)
   const compiler = webpack(webpackConfig)
 
-  return new webpackDevServer(compiler, { hot: true })
+  const devserver = new webpackDevServer(compiler, { hot: true })
+
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(devserver), 5000)
+    // const cb = (line) => {
+    //   console.log('on data?', line.toString())
+    //   if (line.toString().contains('Compiled successfully.')) {
+    //     console.log('FOUND compiled successfully')
+    //     process.stdout.removeListener('data', cb)
+    //     resolve(devserver)
+    //   }
+    // }
+    //
+    // process.stderr.on('data', (l) => console.error(`stderr: ${l.toString()}`))
+    // process.stdout.on('data', cb)
+  })
 }
