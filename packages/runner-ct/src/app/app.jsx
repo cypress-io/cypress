7199227550872f7ee9ec46e2b1e1634f3d98fc71
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-12-15 20:28:21 +0200

    Resizeable layout for command log
    (cherry picked from commit 5bde607d99720ded228961b5e6cff3eeb55121c8)

--- packages/runner-ct/src/app/app.jsx
+++ packages/runner-ct/src/app/app.jsx
@@ -1,9 +1,9 @@
 import { observer } from 'mobx-react'
 import PropTypes from 'prop-types'
 import React from 'react'
-// import { Reporter } from '@packages/reporter'
+import { Reporter } from '@packages/reporter'
 
-// import errorMessages from '../errors/error-messages'
+import errorMessages from '../errors/error-messages'
 import util from '../lib/util'
 import State from '../lib/state'
 
@@ -12,35 +12,40 @@ import SplitPane from 'react-split-pane'
 import Header from '../header/header'
 import Iframes from '../iframe/iframes'
 import Message from '../message/message'
+import { BottomPane } from './BottomPane'
 import './app.scss'
-
-// {this.props.state.spec && (
-//   <Reporter
-//     runMode={this.props.state.runMode}
-//     runner={this.props.eventManager.reporterBus}
-//     spec={this.props.state.spec}
-//     allSpecs={this.props.state.multiSpecs}
-//     autoScrollingEnabled={this.props.config.state.autoScrollingEnabled}
-//     error={errorMessages.reporterError(this.props.state.scriptError, spec.relative)}
-//     firefoxGcInterval={this.props.config.firefoxGcInterval}
-//     resetStatsOnSpecChange={this.props.state.runMode === 'single'}
-//   />
-// )}
 
 const App = observer(
   function App (props) {
-    const { state, children } = props
+    const { state, eventManager, config } = props
 
     return (
       <>
         <SplitPane split="vertical" minSize={250} defaultSize="20%" >
           <SpecsList state={state} />
-          <div className="runner runner-ct container">
-            <Header {...props} />
-            <Iframes {...props} />
-            <Message state={state} />
-            {children}
-          </div>
+          <SplitPane split="horizontal" defaultSize="60%" minSize="20%" maxSize="80%">
+            <div className="runner runner-ct container">
+              <Header {...props} />
+              <Iframes {...props} />
+              <Message state={state} />
+
+            </div>
+
+            <BottomPane>
+              {state.spec && (
+                <Reporter
+                  runMode={state.runMode}
+                  runner={eventManager.reporterBus}
+                  spec={state.spec}
+                  allSpecs={state.multiSpecs}
+                  autoScrollingEnabled={config.state.autoScrollingEnabled}
+                  error={errorMessages.reporterError(state.scriptError, state.spec.relative)}
+                  firefoxGcInterval={config.firefoxGcInterval}
+                  resetStatsOnSpecChange={state.runMode === 'single'}
+                />
+              )}
+            </BottomPane>
+          </SplitPane>
         </SplitPane>
 
         {/* these pixels help ensure the browser has painted when taking a screenshot */}
