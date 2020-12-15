34dd697b8cacbf2f7cf08010810d999e2ce5f585
Chris Breiding <chrisbreiding@gmail.com>
2020-12-15 09:59:41 -0500

    ensure config is an object
    mostly for the run_spec.js test's sake, so they don't fail because they don't pass config in

--- packages/server/lib/plugins/run_events.js
+++ packages/server/lib/plugins/run_events.js
@@ -4,7 +4,7 @@ const errors = require('../errors')
 const plugins = require('../plugins')
 
 module.exports = {
-  execute: Promise.method((eventName, config, ...args) => {
+  execute: Promise.method((eventName, config = {}, ...args) => {
     if (!config.experimentalRunEvents) return
 
     if (!plugins.has(eventName)) return
