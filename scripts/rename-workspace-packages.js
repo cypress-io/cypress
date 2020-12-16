3d674c4a9e75ce55528b0cbde0a745d628c26fa9
Brian Mann <brian.mann86@gmail.com>
2020-12-15 21:05:07 -0500

    added vscode task to rename relative workspace packages to their properly name spaced @packages/[name]

---
+++ scripts/rename-workspace-packages.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const glob = require('glob')
+const minimist = require('minimist')
+// const { workspaces } = require('@packages/root')
+const path = require('path')
+
+// const { packages } = workspaces
+
+// const PATH_REGEX = /('|")(.*\.\.\/)(.+\/)(lib|src)/g
+
+const args = minimist(process.argv.slice(2))
+
+const packages = glob.sync('*/', {
+  cwd: path.join(process.cwd(), 'packages'),
+})
+.map((str) => {
+  return str.replace('/', '\\/')
+})
+.join('|')
+
+const pattern = `(\'|")(.*\\.\\.\\/)(${packages})`
+
+const re = new RegExp(pattern, 'g')
+
+const text = fs.readFileSync(args.file, 'utf8')
+
+const replacedText = text.replace(re, '$1@packages/$3')
+
+fs.writeFileSync(args.file, replacedText)
