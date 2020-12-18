fca96c7c681959cf057a693b12d638e63e7808e1
Zach Panzarino <zachary@panzarino.com>
2020-12-18 01:49:55 -0500

    remove old snapshot

--- __snapshots__/packages-spec.js
+++ __snapshots__/packages-spec.js
@@ -27,57 +27,6 @@ exports['packages can copy files from package.json 1'] = {
   }
 }
 
-exports['packages can copy files with local npm dependencies 1'] = {
-  "[cwd]": {
-    "npm": {
-      "package-a": {
-        "package.json": "{\"main\":\"src/index.js\",\"name\":\"@cypress/package-a\",\"files\":[\"lib\"]}",
-        "src": {
-          "index.js": "console.error()"
-        },
-        "lib": {
-          "bar.js": "{}"
-        }
-      }
-    },
-    "packages": {
-      "coffee": {
-        "package.json": "{\"main\":\"src/main.js\",\"name\":\"foo\",\"files\":[\"lib\"],\"dependencies\":{\"@cypress/package-a\":\"0.0.0-development\"}}",
-        "src": {
-          "main.js": "console.log()"
-        },
-        "lib": {
-          "foo.js": "{}"
-        }
-      }
-    }
-  },
-  "tmp": {
-    "npm": {
-      "package-a": {
-        "package.json": "{\"main\":\"src/index.js\",\"name\":\"@cypress/package-a\",\"files\":[\"lib\"]}",
-        "src": {
-          "index.js": "console.error()"
-        },
-        "lib": {
-          "bar.js": "{}"
-        }
-      }
-    },
-    "packages": {
-      "coffee": {
-        "package.json": "{\"main\":\"src/main.js\",\"name\":\"foo\",\"files\":[\"lib\"],\"dependencies\":{\"@cypress/package-a\":\"0.0.0-development\"}}",
-        "src": {
-          "main.js": "console.log()"
-        },
-        "lib": {
-          "foo.js": "{}"
-        }
-      }
-    }
-  }
-}
-
 exports['transformRequires can find and replace symlink requires 1'] = {
   "[cwd]": {
     "build": {
