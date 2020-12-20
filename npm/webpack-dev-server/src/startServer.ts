e2baa42dae458475e61961fed08fb089b75fd8ed
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-12-20 22:49:34 +1000

    feat: use find-webpack in webpack-dev-server to locate user webpack config

--- npm/webpack-dev-server/src/startServer.ts
+++ npm/webpack-dev-server/src/startServer.ts
@@ -1,12 +1,14 @@
 import webpack from 'webpack'
 import { debug as debugFn } from 'debug'
 import WebpackDevServer from 'webpack-dev-server'
+import fw from 'find-webpack'
 const debug = debugFn('cypress:webpack-dev-server:start')
 
 import { makeWebpackConfig } from './makeWebpackConfig'
 
 export async function start (userWebpackConfig = {}, { specs, config, devserverEvents }): Promise<WebpackDevServer> {
-  const webpackConfig = await makeWebpackConfig(userWebpackConfig, {
+  const foundConfig = fw.getWebpackOptions()
+  const webpackConfig = await makeWebpackConfig(foundConfig || userWebpackConfig, {
     files: specs,
     projectRoot: config.projectRoot,
     support: '',
