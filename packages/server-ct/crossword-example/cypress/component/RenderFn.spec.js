4b08a26bfeed155223db416868fa4ac53440478d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-12-23 11:36:47 +1000

    update render fn example

--- packages/server-ct/crossword-example/cypress/component/RenderFn.spec.js
+++ packages/server-ct/crossword-example/cypress/component/RenderFn.spec.js
@@ -1,9 +1,9 @@
-import Foo from '@/components/Foo'
+import RenderFn from '@/components/RenderFn'
 import { mount } from '@cypress/vue'
 
 describe('Foo', () => {
   it('works', () => {
-    mount(Foo)
+    mount(RenderFn)
     cy.get('h3').contains('Count is: 0')
     cy.get('button').contains('Increment').click()
     cy.get('h3').contains('Count is: 1')
