a0cd07fe661ad0e6d1529f053c998f82e2f033e3
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-12-23 11:28:34 +1000

    annotate and improve typings in loader

--- npm/webpack-dev-server/src/loader.ts
+++ npm/webpack-dev-server/src/loader.ts
@@ -1,10 +1,19 @@
+/* global Cypress */
+/// <reference types="cypress" />
+
 import * as path from 'path'
 
-const makeImport = (file, fileKey, chunkName, projectRoot) => {
+/**
+ * @param {ComponentSpec} file spec to create import string from.
+ * @param {string} filename name of the spec file - this is the same as file.name
+ * @param {string} chunkName webpack chunk name. eg: 'spec-0'
+ * @param {string} projectRoot absolute path to the project root. eg: /Users/<username>/my-app
+ */
+const makeImport = (file: Cypress.Cypress['spec'], filename: string, chunkName: string, projectRoot: string) => {
   // If we want to rename the chunks, we can use this
   const magicComments = chunkName ? `/* webpackChunkName: "${chunkName}" */` : ''
 
-  return `"${fileKey}": {
+  return `"${filename}": {
     shouldLoad: () => document.location.pathname.includes(${JSON.stringify(file.relative)}),
     load: () => {
       return import(${JSON.stringify(path.resolve(projectRoot, file.relative), null, 2)} ${magicComments})
@@ -13,8 +22,23 @@ const makeImport = (file, fileKey, chunkName, projectRoot) => {
   }`
 }
 
-function buildSpecs (files, projectRoot) {
-  if (!files || !Array.isArray(files)) return `{}`
+/**
+ * Creates a object maping a spec file to an object mapping
+ * the spec name to the result of `makeImport`.
+ *
+ * @returns {Record<string, ReturnType<makeImport>}
+ * {
+ *   "App.spec.js": {
+ *     shouldLoad: () => document.location.pathname.includes("cypress/component/App.spec.js"),
+ *     load: () => {
+ *       return import("/Users/projects/my-app/cypress/component/App.spec.js" \/* webpackChunkName: "spec-0" *\/)
+ *     },
+ *     chunkName: "spec-0"
+ *   }
+ * }
+ */
+function buildSpecs (projectRoot: string, files: Cypress.Cypress['spec'][] = []): string {
+  if (!Array.isArray(files)) return `{}`
 
   return `{${files.map((f, i) => {
     return makeImport(f, f.name, `spec-${i}`, projectRoot)
@@ -22,15 +46,15 @@ function buildSpecs (files, projectRoot) {
 }
 
 // Runs the tests inside the iframe
-export default function loader () {
-  const { files, projectRoot } = this._cypress
+export default async function loader (): Promise<string> {
+  const { files, projectRoot } = this._cypress as { files: Cypress.Cypress['spec'][], projectRoot: string }
 
   return `
-  var allTheSpecs = ${buildSpecs(files, projectRoot)};
+  var allTheSpecs = ${buildSpecs(projectRoot, files)};
 
   const { init } = require(${JSON.stringify(require.resolve('./aut-runner'))})
 
-  init(Object.keys(allTheSpecs)
+  const { onHMR } = init(Object.keys(allTheSpecs)
     .filter(key => allTheSpecs[key].shouldLoad())
     .map(a => allTheSpecs[a].load())
   )
