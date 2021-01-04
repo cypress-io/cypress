19f46156b39f24af788ffb8a62d508021f65c188
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-01-04 17:27:53 +1000

    refactor: use BaseProject effectively

--- packages/server/lib/base_project.ts
+++ packages/server/lib/base_project.ts
@@ -1,4 +1,80 @@
 import { EventEmitter } from 'events'
+import path from 'path'
+
+const { escapeFilenameInUrl } = require('./util/escape_filename')
+
+interface GetSpecUrl {
+  absoluteSpecPath: string
+  specType: string
+  integrationFolder: string
+  componentFolder: string
+  projectRoot: string
+  browserUrl: string
+}
+
+const multipleForwardSlashesRe = /[^:\/\/](\/{2,})/g
 
 export class BaseProject extends EventEmitter {
+  normalizeSpecUrl (browserUrl, specUrl) {
+    const replacer = (match) => match.replace('//', '/')
+
+    return [
+      browserUrl,
+      '#/tests',
+      escapeFilenameInUrl(specUrl),
+    ].join('/').replace(multipleForwardSlashesRe, replacer)
+  }
+
+  getPrefixedPathToSpec (options: GetSpecUrl) {
+    // for now hard code the 'type' as integration
+    // but in the future accept something different here
+
+    // strip out the integration folder and prepend with "/"
+    // example:
+    //
+    // /Users/bmann/Dev/cypress-app/.projects/cypress/integration
+    // /Users/bmann/Dev/cypress-app/.projects/cypress/integration/foo.js
+    //
+    // becomes /integration/foo.js
+
+    const folderToUse = options.specType === 'integration'
+      ? options.integrationFolder
+      : options.componentFolder
+
+    const url = `/${path.join(options.specType, path.relative(
+      folderToUse,
+      path.resolve(options.projectRoot, options.absoluteSpecPath),
+    ))}`
+
+    // debug('prefixed path for spec %o', { absoluteSpecPath, specType, url })
+
+    return url
+  }
+
+  getSpecUrl (options: GetSpecUrl) {
+    // debug('get spec url: %s for spec type %s', options.absoluteSpecPath, options.specType)
+
+    // if we don't have a absoluteSpecPath or its __all
+    if (!options.absoluteSpecPath || (options.absoluteSpecPath === '__all')) {
+      const url = this.normalizeSpecUrl(options.browserUrl, '/__all')
+
+      // debug('returning url to run all specs: %s', url)
+
+      return url
+    }
+
+    // TODO:
+    // to handle both unit + integration tests we need
+    // to figure out (based on the config) where this absoluteSpecPath
+    // lives. does it live in the integrationFolder or
+    // the unit folder?
+    // once we determine that we can then prefix it correctly
+    // with either integration or unit
+    const prefixedPath = this.getPrefixedPathToSpec(options)
+    const url = this.normalizeSpecUrl(options.browserUrl, prefixedPath)
+
+    // debug('return path to spec %o', options.specType, options.absoluteSpecPath, prefixedPath, url)
+
+    return url
+  }
 }
--- packages/server/lib/project.js
+++ packages/server/lib/project.js
@@ -1,6 +1,5 @@
 const _ = require('lodash')
 const R = require('ramda')
-const EE = require('events')
 const path = require('path')
 const Promise = require('bluebird')
 const commitInfo = require('@cypress/commit-info')
@@ -28,13 +27,11 @@ const fs = require('./util/fs')
 const keys = require('./util/keys')
 const settings = require('./util/settings')
 const specsUtil = require('./util/specs')
-const { escapeFilenameInUrl } = require('./util/escape_filename')
+const { BaseProject } = require('./base_project')
 
 const localCwd = cwd()
 
-const multipleForwardSlashesRe = /[^:\/\/](\/{2,})/g
-
-class Project extends EE {
+class Project extends BaseProject {
   constructor (projectRoot) {
     super()
 
@@ -477,78 +474,6 @@ class Project extends EE {
 
       return cfg
     })
-  }
-
-  getSpecUrl (options) {
-    // interface GetSpecUrl {
-    //   absoluteSpecPath: string
-    //   specType: string
-    //   integrationFolder: string
-    //   componentFolder: string
-    //   projectRoot: string
-    //   browserUrl: string
-    // }
-
-    debug('get spec url: %s for spec type %s', options.absoluteSpecPath, options.specType)
-
-    // if we don't have a absoluteSpecPath or its __all
-    if (!options.absoluteSpecPath || (options.absoluteSpecPath === '__all')) {
-      const url = this.normalizeSpecUrl(options.browserUrl, '/__all')
-
-      debug('returning url to run all specs: %s', url)
-
-      return url
-    }
-
-    // TODO:
-    // to handle both unit + integration tests we need
-    // to figure out (based on the config) where this absoluteSpecPath
-    // lives. does it live in the integrationFolder or
-    // the unit folder?
-    // once we determine that we can then prefix it correctly
-    // with either integration or unit
-    const prefixedPath = this.getPrefixedPathToSpec(options.absoluteSpecPath, options.specType)
-    const url = this.normalizeSpecUrl(options.browserUrl, prefixedPath)
-
-    debug('return path to spec %o', options.specType, options.absoluteSpecPath, prefixedPath, url)
-
-    return url
-  }
-
-  getPrefixedPathToSpec (cfg, absoluteSpecPath, specType = 'integration') {
-    const { integrationFolder, componentFolder, projectRoot } = cfg
-
-    // for now hard code the 'type' as integration
-    // but in the future accept something different here
-
-    // strip out the integration folder and prepend with "/"
-    // example:
-    //
-    // /Users/bmann/Dev/cypress-app/.projects/cypress/integration
-    // /Users/bmann/Dev/cypress-app/.projects/cypress/integration/foo.js
-    //
-    // becomes /integration/foo.js
-
-    const folderToUse = specType === 'integration' ? integrationFolder : componentFolder
-
-    const url = `/${path.join(specType, path.relative(
-      folderToUse,
-      path.resolve(projectRoot, absoluteSpecPath),
-    ))}`
-
-    debug('prefixed path for spec %o', { absoluteSpecPath, specType, url })
-
-    return url
-  }
-
-  normalizeSpecUrl (browserUrl, specUrl) {
-    const replacer = (match) => match.replace('//', '/')
-
-    return [
-      browserUrl,
-      '#/tests',
-      escapeFilenameInUrl(specUrl),
-    ].join('/').replace(multipleForwardSlashesRe, replacer)
   }
 
   scaffold (cfg) {
