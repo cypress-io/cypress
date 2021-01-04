434e7a802076927c6ec91cc81349a462ebcb61c2
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2021-01-04 09:37:05 -0500

    fix test expected exit code

--- packages/server/test/e2e/3_retries_spec.ts
+++ packages/server/test/e2e/3_retries_spec.ts
@@ -14,6 +14,7 @@ describe('retries', () => {
 
   it('completes a run of many retries in a reasonable time', {
     spec: 'hanging_retries_spec.js',
+    expectedExitCode: 10,
   })
 
   it('warns about retries plugin', {
