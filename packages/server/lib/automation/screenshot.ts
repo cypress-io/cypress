ca0a7d134e659e54178fbf93a088d0568fa2264c
Brian Mann <brian.mann86@gmail.com>
2021-01-05 17:09:35 -0500

    convert automation/screenshot into typescript

---
+++ packages/server/lib/automation/screenshot.ts
@@ -0,0 +1,25 @@
+import screenshots from '../screenshots'
+
+export class Screenshot {
+  constructor (private screenshotsFolder) { }
+
+  capture (data, automate) {
+    return screenshots.capture(data, automate)
+    .then((details) => {
+      // if there are no details, this is part of a multipart screenshot
+      // and should not be saved
+      if (!details) {
+        return
+      }
+
+      return screenshots.save(data, details, this.screenshotsFolder)
+    })
+    .then((savedDetails) => {
+      return screenshots.afterScreenshot(data, savedDetails)
+    })
+    .catch((err) => {
+      screenshots.clearMultipartState()
+      throw err
+    })
+  }
+}
