c76ce37734ae69c26d9f31bb2716e25e6ef5c3b7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-01-07 17:58:24 +1000

    chore: remove build files unintionally committed

--- packages/server/lib/automation/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-function __export (m) {
-  for (let p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p]
-}
-Object.defineProperty(exports, '__esModule', { value: true })
-__export(require('./automation'))
