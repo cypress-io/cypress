5397cfc3a4103a6b01cc7e6439d04bd54c3f3141
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-01-08 19:15:58 +0200

    Fix socket hangup on intiial build

--- packages/runner-ct/src/lib/event-manager.js
+++ packages/runner-ct/src/lib/event-manager.js
@@ -195,6 +195,17 @@ const eventManager = {
       this._clearAllCookies()
     })
 
+    const hmrSocket = connectWebpackHmr({
+      url: `${window.location.origin}/cypress-webpack-hmr-socket`,
+      onReload: () => {
+        if (!state.isInitialBuildSucceed) {
+          state.initialBuildFired()
+        } else {
+          rerun()
+        }
+      },
+    })
+
     // when our window triggers beforeunload
     // we know we've change the URL and we need
     // to clear our cookies
