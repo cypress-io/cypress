bafa1bd2606e4d48843bb3825a7bd9d89b062305
ElevateBart <ledouxb@gmail.com>
2021-01-11 12:25:39 -0600

    style: add comments on the helpers.js file

--- packages/runner-ct/test/helper.js
+++ packages/runner-ct/test/helper.js
@@ -1,12 +1,15 @@
 import { register, returnMockRequire } from '@packages/web-config/node-jsdom-setup'
 import sinon from 'sinon'
 
+// import scss files will break mocha tests
+// if we do not add this
 import mochaIgnoreStyles from 'ignore-styles'
 
 mochaIgnoreStyles(['.scss'])
 
 const driverMock = {}
 
+// allows use of JSX
 register({
   enzyme: require('enzyme'),
   EnzymeAdapter: require('enzyme-adapter-react-16'),
@@ -18,10 +21,12 @@ register({
   },
 })
 
+// ensure that mocha exits after all tests pass
 const io = returnMockRequire('@packages/socket/lib/browser', { client: {} })
 
 io.client.connect = sinon.stub().returns({ emit: () => {}, on: () => {} })
 
+// setup fake timers to make the tests faster
 const _useFakeTimers = sinon.useFakeTimers
 let timers = []
 
