b4def32579cd5fee6161bb082d59fe66f0a16c6d
ElevateBart <ledouxb@gmail.com>
2021-01-12 10:39:32 -0600

    test: make fast using stub/spy + container tests

---
+++ packages/runner-ct/src/app/container.spec.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import sinon from 'sinon'
+
+import App from './app'
+import automation from '../lib/automation'
+import AutomationDisconnected from '../errors/automation-disconnected'
+import NoAutomation from '../errors/no-automation'
+import State from '../lib/state'
+
+import * as SockJS from 'sockjs-client/dist/sockjs'
+
+import Container, { automationElementId } from './container'
+
+const createProps = () => ({
+  config: {
+    browsers: [],
+    integrationFolder: '',
+    numTestsKeptInMemory: 1,
+    projectName: '',
+    viewportHeight: 0,
+    viewportWidth: 0,
+  },
+  eventManager: {
+    addGlobalListeners: sinon.spy(),
+    launchBrowser: sinon.spy(),
+    notifyRunningSpec: () => {},
+    reporterBus: {
+      emit: () => {},
+      on: () => {},
+    },
+  },
+  state: new State({ reporterWidth: 2 }),
+  util: {
+    hasSpecFile: sinon.stub(),
+  },
+})
+
+SockJS.default = function () {
+}
+
+describe('<Container />', () => {
+  it('initializes the event manager when mounted', () => {
+    const props = createProps()
+
+    mount(<Container {...props} />)
+    expect(props.eventManager.addGlobalListeners).to.have.been.called
+    expect(props.eventManager.addGlobalListeners.firstCall.args[0]).to.equal(props.state)
+    expect(props.eventManager.addGlobalListeners.firstCall.args[1].element).to.equal(automationElementId)
+    expect(props.eventManager.addGlobalListeners.firstCall.args[1].string).to.be.a('string')
+  })
+
+  it('connects to the webpack dev server socket', () => {
+    const props = createProps()
+
+    mount(<Container {...props} />)
+    expect(props.eventManager.addGlobalListeners).to.have.been.called
+    expect(props.eventManager.addGlobalListeners.firstCall.args[0]).to.equal(props.state)
+    expect(props.eventManager.addGlobalListeners.firstCall.args[1].element).to.equal(automationElementId)
+    expect(props.eventManager.addGlobalListeners.firstCall.args[1].string).to.be.a('string')
+  })
+
+  describe('when automation is connecting', () => {
+    let component
+
+    beforeEach(() => {
+      const props = createProps()
+
+      props.state.automation = automation.CONNECTING
+      component = shallow(<Container {...props} />)
+    })
+
+    it('renders the automation element alone', () => {
+      expect(component.find(`#${automationElementId}`)).to.exist
+      expect(component.find(`#${automationElementId}`).parent()).not.to.exist
+    })
+  })
+
+  describe('when automation is missing', () => {
+    let props
+    let component
+
+    beforeEach(() => {
+      props = createProps()
+      props.state.automation = automation.MISSING
+      component = shallow(<Container {...props} />)
+    })
+
+    it('renders <NoAutomation /> with browsers', () => {
+      expect(component.find(NoAutomation)).to.have.prop('browsers', props.config.browsers)
+    })
+
+    it('does not render the automation element', () => {
+      expect(component.find(`#${automationElementId}`)).not.to.exist
+    })
+
+    it('launches the browser when onLaunchBrowser prop is called', () => {
+      component.find(NoAutomation).prop('onLaunchBrowser')('chrome')
+      expect(props.eventManager.launchBrowser).to.have.been.calledWith('chrome')
+    })
+  })
+
+  describe('when automation is disconnected', () => {
+    let props
+    let component
+
+    beforeEach(() => {
+      props = createProps()
+      props.state.automation = automation.DISCONNECTED
+      component = shallow(<Container {...props} />)
+    })
+
+    it('renders <AutomationDisconnected />', () => {
+      expect(component.find(AutomationDisconnected)).to.exist
+    })
+
+    it('does not render the automation element', () => {
+      expect(component.find(`#${automationElementId}`)).not.to.exist
+    })
+
+    it('launches the browser when onReload prop is called', () => {
+      component.find(AutomationDisconnected).prop('onReload')('chrome')
+      expect(props.eventManager.launchBrowser).to.have.been.calledWith('chrome')
+    })
+  })
+
+  describe('when automation is connected, but there is no spec file', () => {
+    let props
+    let component
+
+    beforeEach(() => {
+      props = createProps()
+      props.state.automation = automation.CONNECTED
+      props.util.hasSpecFile.returns(false)
+      component = shallow(<Container {...props} />)
+    })
+
+    it('renders the automation element', () => {
+      expect(component.find(`#${automationElementId}`)).to.exist
+    })
+  })
+
+  describe('when automation is connected and there is a spec file', () => {
+    let props
+    let component
+
+    beforeEach(() => {
+      props = createProps()
+      props.state.automation = automation.CONNECTED
+      props.util.hasSpecFile.returns(true)
+      component = shallow(<Container {...props} />)
+    })
+
+    it('renders <App />', () => {
+      expect(component.find(App)).to.exist
+    })
+
+    it('renders the automation element', () => {
+      expect(component.find(`#${automationElementId}`)).to.exist
+    })
+  })
+})
