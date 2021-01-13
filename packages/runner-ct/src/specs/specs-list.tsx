c3237f7f5e61cf94f58f0c46c28afac921d09be7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-01-13 10:59:12 +1000

    chore: tweak style to match mock up

--- packages/runner-ct/src/specs/specs-list.tsx
+++ packages/runner-ct/src/specs/specs-list.tsx
@@ -24,22 +24,15 @@ export const SpecsList: React.FC<SpecsListProps> = observer(
         </header>
         <div className="specs-list-search-input-container">
           <input
-            placeholder='Select tests to run...'
+            placeholder='Find a spec'
             value={state.specSearchText}
             onChange={(e) => state.setSearchSpecText(e.currentTarget.value.toLowerCase())}
           />
         </div>
         <div className="specs-list-scroll-container">
           <ul className="specs-list-container">{
-            specGroups.map((item) => {
-              { // The `active` prop here is used only to
-                // force repaint of the tree when selecting a spec
-                // It is not used for anything else than to patch react
-                // not comparing members of an object (state in this case)
-              }
-
-              return <SpecItem key={item.shortName} item={item} state={state} />
-            })}
+            specGroups.map((item) =>
+              <SpecItem key={item.shortName} item={item} state={state} />)}
           </ul>
         </div>
       </div>
