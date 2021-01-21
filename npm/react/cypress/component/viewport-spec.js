0a9ea0c7fab17ff7fce28b14dc14d3ef68599b32
ElevateBart <ledouxb@gmail.com>
2021-01-21 11:46:49 -0600

    fix: error in tests

--- npm/react/cypress/component/viewport-spec.js
+++ npm/react/cypress/component/viewport-spec.js
@@ -1,14 +1,24 @@
+import React from 'react'
+import { mount } from '@cypress/react'
+
 const viewportWidth = 200
 const viewportHeight = 100
 
-describe('cypress.json viewport', { viewportWidth, viewportHeight }, () => {
-  it('should have the correct dimensions', () => {
-    cy.should(() => {
-      expect(window.innerWidth).to.eq(viewportWidth)
-      expect(window.innerHeight).to.eq(viewportHeight)
+describe('cypress.json viewport',
+  { viewportWidth, viewportHeight },
+  () => {
+    it('should have the correct dimensions', () => {
+      const Greeting = () => <div>Hello!</div>
+
+      mount(<Greeting />)
+      cy.get('@Greeting')
+      // cy.should cannot be the first cy command we run
+      cy.should(() => {
+        expect(window.innerWidth).to.eq(viewportWidth)
+        expect(window.innerHeight).to.eq(viewportHeight)
+      })
     })
   })
-})
 
 describe('cy.viewport', () => {
   it('should resize the viewport', () => {
