c39b42012d71594def99c7e05d2923cdf5d6b77e
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-01-26 13:31:38 +1000

    wip

--- my-project/cypress/component/Button.spec.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { mount } from '../../../npm/react/dist'
-import { Button } from '../../src/Components'
-
-it('Button', () => {
-  mount(<Button>Test button</Button>)
-  cy.get('button').contains('Test button').click()
-})
