939e4c5942b4cc175bfb86b2bae8eeb9aac2be6d
ElevateBart <ledouxb@gmail.com>
2021-02-11 15:14:46 -0600

    run the hooks only in support

--- npm/react/src/index.ts
+++ npm/react/src/index.ts
@@ -1,3 +1,5 @@
 export * from './mount'
 
 export * from './mountHook'
+
+export * from './hooks'
--- npm/react/support/index.js
+++ npm/react/support/index.js
@@ -1,6 +1,6 @@
 // should be loaded from cypress.json as a support file
 // "supportFile": "node_modules/@cypress/react/support"
 // adds commands from @cypress/react
-require('../dist/hooks').setupHooks()
+require('../dist').setupHooks()
 require('cypress-react-selector')
 require('@cypress/code-coverage/support')
