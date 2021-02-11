a524e95a8b421ae7db9b0b16344b9af056a7a8b9
Jessica Sachs <jess@jessicasachs.io>
2021-02-11 16:06:12 -0500

    fixing support to immediately invoke the new method on hooks

--- npm/react/support/index.js
+++ npm/react/support/index.js
@@ -1,6 +1,6 @@
 // should be loaded from cypress.json as a support file
 // "supportFile": "node_modules/@cypress/react/support"
 // adds commands from @cypress/react
-require('../dist/hooks')
+require('../dist/hooks').setupHooks()
 require('cypress-react-selector')
 require('@cypress/code-coverage/support')
