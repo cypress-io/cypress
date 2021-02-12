195b820f18927d6978bbcbebd56f7a9cd4cc0913
ElevateBart <ledouxb@gmail.com>
2021-02-12 17:36:37 -0600

    router specs are passing

--- npm/vue/cypress/component/router-example/PizzaShop/Home.vue
+++ npm/vue/cypress/component/router-example/PizzaShop/Home.vue
@@ -1,38 +1,34 @@
 <template>
   <div class="shop-home">
-    <router-link
-      :to="{ name: 'order' }"
-      tag="button"
-      class="order"
-    >
-      Place Your Order
+    <router-link :to="{ name: 'order' }" custom v-slot="{ navigate }">
+      <button class="order" @click="navigate">Place Your Order</button>
     </router-link>
-    <br>
+    <br />
     <h4>Or select a popular option:</h4>
     <router-link
       :to="{ name: 'order', params: { preset: 'veggie' } }"
       class="order-veggie"
     >
-      🥗 Veggie
-    </router-link><br>
+      🥗 Veggie </router-link
+    ><br />
     <router-link
       :to="{ name: 'order', params: { preset: 'vegan' } }"
       class="order-vegan"
     >
-      🥕 Vegan
-    </router-link><br>
+      🥕 Vegan </router-link
+    ><br />
     <router-link
       :to="{ name: 'order', params: { preset: 'meatlover' } }"
       class="order-meatlover"
     >
-      🍖 Meatlover
-    </router-link><br>
+      🍖 Meatlover </router-link
+    ><br />
     <router-link
       :to="{ name: 'order', params: { preset: 'hawaian' } }"
       class="order-hawaian"
     >
-      🍍 Hawain
-    </router-link><br>
+      🍍 Hawain </router-link
+    ><br />
     <router-link
       :to="{ name: 'order', query: { cheese: true } }"
       class="order-cheese"
