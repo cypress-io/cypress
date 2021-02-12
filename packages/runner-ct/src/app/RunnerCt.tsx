63f06ed71ec7b259f304c73b84afd5b17216060e
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-02-12 16:20:00 +1000

    fix: update specs

--- packages/runner-ct/src/app/RunnerCt.tsx
+++ packages/runner-ct/src/app/RunnerCt.tsx
@@ -58,7 +58,6 @@ const App: React.FC<AppProps> = observer(
     const runSpec = (spec: Cypress.Cypress['spec']) => {
       setIsSpecsListOpen(false)
       state.setSingleSpec(spec)
-      hidePane()
     }
 
     function monitorWindowResize () {
@@ -88,22 +87,51 @@ const App: React.FC<AppProps> = observer(
       monitorWindowResize()
     }, [])
 
-    const hidePane = () => {
-      runInAction(() => {
-        state.setScreenshotting(true)
+    React.useEffect(() => {
+      eventManager.on('before:screenshot', (config) => {
+        runInAction(() => {
+          state.setScreenshotting(true)
+          hidePane()
+        })
       })
 
+      const revertFromScreenshotting = () => {
+        runInAction(() => {
+          state.setScreenshotting(false)
+          showPane()
+        })
+      }
+
+      eventManager.on('after:screenshot', (config) => {
+        revertFromScreenshotting()
+      })
+
+      eventManager.on('run:start', () => {
+        revertFromScreenshotting()
+      })
+    }, [])
+
+    // SplitPane hierarchy looks like this:
+    // <div class="SplitPane">
+    //    <div class="Pane vertical Pane1  ">..</div>
+    //    <span role="presentation" class="Resizer vertical  ">...</span>
+    // </div>
+    //
+    // we need to set these to display: none during cy.screenshot.
+    const showPane = () => {
       if (!splitPaneRef.current) {
         return
       }
 
-      // SplitPane hierarchy looks like this:
-      // <div class="SplitPane">
-      //    <div class="Pane vertical Pane1  ">..</div>
-      //    <span role="presentation" class="Resizer vertical  ">...</span>
-      // </div>
-      //
-      // we need to set these to display: none during cy.screenshot.
+      splitPaneRef.current.splitPane.firstElementChild.classList.remove('d-none')
+      splitPaneRef.current.splitPane.querySelector('[role="presentation"]').classList.remove('d-none')
+    }
+
+    const hidePane = () => {
+      if (!splitPaneRef.current) {
+        return
+      }
+
       splitPaneRef.current.splitPane.firstElementChild.classList.add('d-none')
       splitPaneRef.current.splitPane.querySelector('[role="presentation"]').classList.add('d-none')
     }
