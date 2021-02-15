3f294df936a579975a2219a8b5ed77e1f159b517
Zach Panzarino <zachary@panzarino.com>
2021-02-15 00:22:39 -0500

    fix: update connect to dashboard graphic on runs tab

--- packages/desktop-gui/src/runs/dashboard-banner.jsx
+++ packages/desktop-gui/src/runs/dashboard-banner.jsx
@@ -4,15 +4,14 @@ import React from 'react'
 const DashboardBanner = () => {
   return (
     <div className='dashboard-banner'>
-      <div className='left-vector' />
-      <div className='right-vector' />
-      <div className='dashboard-banner-db'>
+      <div className='dashboard-banner-tr'>
+        <div className='dashboard-banner-tr-title'>Test Runner</div>
         {_.map(['pass', 'fail', 'pass'], (test, i) => (
           <div key={`test-${i}`} className={`dashboard-banner-test ${test}`}>
             <div className='dashboard-banner-test-left'>
               <div className='dashboard-banner-test-title'>
                 <div className='test-title-top'>
-                  <i className='fas fa-check' />
+                  <i className={`fas ${test === 'pass' ? 'fa-check' : 'fa-times'}`} />
                   {_.capitalize(test)}ed test
                 </div>
                 <div className='test-title-bottom'>
@@ -22,16 +21,26 @@ const DashboardBanner = () => {
                 </div>
               </div>
             </div>
-            <div className='dashboard-banner-test-mid'>
-              <i className='far fa-clock' />
-              <div className='fake-text' />
-            </div>
-            <div className='dashboard-banner-test-end'>
-              <i className='fas fa-hourglass-end' />
-              <div className='fake-text' />
-            </div>
           </div>
         ))}
+      </div>
+      <div className='dashboard-banner-dashboard'>
+        <div className='dashboard-banner-dashboard-title'>Cypress Dashboard</div>
+        <div className='dashboard-banner-dashboard-content'>
+          <div className='dashboard-banner-graph'>
+            <div className='fake-text graph-title' />
+            <div className='graph-bars'>
+              {_.map(_.range(4), () => (
+                <div className='graph-bar'>
+                  <div className='graph-bar-section' />
+                  <div className='graph-bar-section' />
+                  <div className='graph-bar-section' />
+                </div>
+              ))}
+            </div>
+            <div className='fake-text graph-footer' />
+          </div>
+        </div>
       </div>
     </div>
   )
