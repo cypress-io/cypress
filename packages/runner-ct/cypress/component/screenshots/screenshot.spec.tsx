749177e3e5bdc8aa666909a9cf12cc6968f30596
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-02-15 14:46:20 +1000

    tests: add screenshot tests

---
+++ packages/runner-ct/cypress/component/screenshots/screenshot.spec.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react'
+import { mount } from '@cypress/react'
+import * as Size from 'image-size'
+
+type ImageSize = typeof Size.imageSize
+type CompareImagesResult = [Buffer, Buffer, ImageSize, ImageSize]
+
+const styles = `
+  body {
+    margin: 0; 
+  }
+  #wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  #body {
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #header, #footer {
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: silver;
+  }
+`
+
+const Layout: React.FC = () => {
+  return (
+    <div id='wrapper'>
+      <div id='header'>Header</div>
+      <div id='body'>Body</div>
+      <div id='footer'>Footer</div>
+    </div>
+  )
+}
+
+describe('screenshot', () => {
+  beforeEach(() => {
+    cy.task('clearScreenshots')
+  })
+
+  afterEach(() => {
+    cy.task('clearScreenshots')
+  })
+
+  it('takes a standard screenshot', () => {
+    mount(<Layout />, {
+      styles,
+    })
+
+    cy.screenshot('1_takes_a_screenshot')
+    cy.task<CompareImagesResult>('compareImages', '1_takes_a_screenshot').then((result) => {
+      expect(result[0]).to.eql(result[1])
+    })
+  })
+
+  it('takes a screenshot with a custom viewport', () => {
+    cy.viewport(750, 750)
+    mount(<Layout />, {
+      styles,
+    })
+
+    cy.screenshot('2_screenshot_custom_viewport')
+    cy.task<CompareImagesResult>('compareImages', '2_screenshot_custom_viewport').then((result) => {
+      expect(result[0]).to.eql(result[1])
+      expect(result[2]).to.deep.eq({ height: 750, width: 750, type: 'png' })
+      expect(result[2]).to.deep.eq(result[3])
+    })
+  })
+
+  // TODO: This will technically pass, but the screenshot is not correct.
+  // AUT transform appears to be buggy for extreme viewports.
+  xit('screenshot with a really long viewport', () => {
+    cy.viewport(200, 2000)
+    mount(<Layout />, {
+      styles,
+    })
+
+    cy.screenshot('3_screenshot_long_viewport')
+    cy.task<CompareImagesResult>('compareImages', '3_screenshot_long_viewport').then((result) => {
+      expect(result[0]).to.eql(result[1])
+      expect(result[2]).to.deep.eq({ height: 2000, width: 200, type: 'png' })
+      expect(result[2]).to.deep.eq(result[3])
+    })
+  })
+})
--- packages/runner-ct/cypress/plugins/index.js
+++ packages/runner-ct/cypress/plugins/index.js
@@ -1,7 +1,12 @@
 /// <reference types="cypress" />
 const path = require('path')
+const fs = require('fs')
 const percyHealthCheck = require('@percy/cypress/task')
 const { startDevServer } = require('@cypress/webpack-dev-server')
+const Promise = require('bluebird')
+let sizeOf = require('image-size')
+
+sizeOf = Promise.promisify(sizeOf)
 
 function injectStylesInlineForPercyInPlace (webpackConfig) {
   webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
@@ -22,6 +27,41 @@ function injectStylesInlineForPercyInPlace (webpackConfig) {
  */
 module.exports = (on, config) => {
   on('task', percyHealthCheck)
+  on('task', {
+    compareImages (screenshotName) {
+      const readAsync = (p) => {
+        return new Promise((res, rej) => {
+          fs.readFile(p, {}, (err, data) => {
+            if (err) {
+              rej(err)
+            }
+
+            res(data)
+          })
+        })
+      }
+
+      return Promise.all([
+        readAsync(path.resolve(__dirname, '..', 'screenshots', 'All Specs', `${screenshotName}.png`)),
+        readAsync(path.resolve(__dirname, '..', 'component', 'screenshots', `${screenshotName}.png`)),
+        sizeOf((path.resolve(__dirname, '..', 'screenshots', 'All Specs', `${screenshotName}.png`))),
+        sizeOf((path.resolve(__dirname, '..', 'component', 'screenshots', `${screenshotName}.png`))),
+      ])
+    },
+
+    clearScreenshots () {
+      return new Promise((res, rej) => {
+        fs.rmdir(path.resolve(__dirname, '..', 'screenshots'), { recursive: true }, (err) => {
+          if (err) {
+            rej(err)
+          }
+
+          res('ok')
+        })
+      })
+    },
+  })
+
   on('dev-server:start', (options) => {
     /** @type {import('webpack').Configuration} */
     const { default: webpackConfig } = require(path.resolve(__dirname, '..', '..', 'webpack.config.ts'))
