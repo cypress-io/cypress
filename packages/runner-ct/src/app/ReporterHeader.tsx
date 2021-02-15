72a8d9d89269e1e35bfbf27c7ebdcc63d2e35c06
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-02-15 15:20:00 +0200

    Remove useless code

--- packages/runner-ct/src/app/ReporterHeader.tsx
+++ packages/runner-ct/src/app/ReporterHeader.tsx
@@ -3,13 +3,12 @@ import { observer } from 'mobx-react'
 import { ReporterHeaderProps } from '@packages/reporter/src/header/header'
 import Stats from '@packages/reporter/src/header/stats'
 import Controls from '@packages/reporter/src/header/controls'
+import { StatsStore } from '@packages/reporter/src/header/stats-store'
 
 export const EmptyReporterHeader: React.FC = () => {
   return (
     <header>
-      <Stats stats={{ numPassed: 0, numFailed: 0, numPending: 0, duration: 0 }} />
-      <div className='spacer' />
-      {/* <Controls appState={appState} /> */}
+      <Stats stats={{ numPassed: 0, numFailed: 0, numPending: 0, duration: 0 } as StatsStore} />
     </header>
   )
 }
