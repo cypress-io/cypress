2b51ba54a4a1e5f97fcd9215a946f7e969d977ec
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-02-17 16:19:41 +0200

    Fix react errors

--- npm/react/cypress/component/basic/react-tutorial/pretty-snapshots-spec.js
+++ npm/react/cypress/component/basic/react-tutorial/pretty-snapshots-spec.js
@@ -7,7 +7,7 @@ import pretty from 'pretty'
 it('says hello world', () => {
   mount(<Hello name="world" />)
   cy.contains('Hello, world!').should('be.visible')
-  cy.get('#cypress-root')
+  cy.get('#__cy_root')
   .invoke('html')
   .then(pretty)
   .should('equal', '<h1>Hello, world!</h1>')
