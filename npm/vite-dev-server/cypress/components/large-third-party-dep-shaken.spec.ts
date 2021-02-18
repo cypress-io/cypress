135a52b93e0a9255c08ab5315689cfda07c7b02e
ElevateBart <ledouxb@gmail.com>
2021-02-18 17:48:58 -0600

    fix: wait for build to complete before test

--- npm/vite-dev-server/cypress/components/large-third-party-dep-shaken.spec.ts
+++ npm/vite-dev-server/cypress/components/large-third-party-dep-shaken.spec.ts
@@ -2,6 +2,6 @@ import { isBoolean } from 'lodash'
 
 describe('Large 3rd party library with tree-shaking', () => {
   it('successfully imports isBoolean from lodash', () => {
-    expect(isBoolean(true)).to.be.true
+    cy.wrap(isBoolean(true)).should('equal', true)
   })
 })
