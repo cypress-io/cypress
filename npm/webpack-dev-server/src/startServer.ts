442fc14973a9cff605da837964ee3662cae323ae
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-02-19 16:04:04 +0200

    Fix ts error

--- npm/webpack-dev-server/src/startServer.ts
+++ npm/webpack-dev-server/src/startServer.ts
@@ -6,7 +6,7 @@ import { makeWebpackConfig } from './makeWebpackConfig'
 
 const debug = Debug('cypress:webpack-dev-server:start')
 
-export async function start ({ webpackConfig: userWebpackConfig, options, config, ...userOptions }: StartDevServer): Promise<WebpackDevServer> {
+export async function start ({ webpackConfig: userWebpackConfig, options, ...userOptions }: StartDevServer): Promise<WebpackDevServer> {
   if (!userWebpackConfig) {
     debug('User did not pass in any webpack configuration')
   }
