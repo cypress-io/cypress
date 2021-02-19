21e7b40dcdfd51bb041dd07a40b0a51ad3dae4a8
ElevateBart <ledouxb@gmail.com>
2021-02-19 10:59:05 -0600

    fix: remove failing vite tests from the ci

--- npm/vite-dev-server/cypress/components/large-third-party-dep.spec.ts
+++ npm/vite-dev-server/cypress/components/large-third-party-dep.spec.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 
-xdescribe('Large 3rd party library without tree-shaking', () => {
+describe('Large 3rd party library without tree-shaking', () => {
   it('successfully imports lodash', () => {
     expect(_.isBoolean(true)).to.be.true
   })
