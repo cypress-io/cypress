743695bca8ff20ba225c49ec2ef4092b460160bc
ElevateBart <ledouxb@gmail.com>
2021-02-26 17:48:54 -0600

    test: new project in plagrounds folder.

---
+++ playgrounds/react-scripts/project/cypress/plugins/index.js
@@ -0,0 +1,11 @@
+// load file preprocessor that comes with this plugin
+// https://github.com/bahmutov/cypress-react-unit-test#install
+const preprocessor = require('@cypress/react/plugins/react-scripts')
+
+module.exports = (on, config) => {
+  preprocessor(on, config)
+
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config
+}
---
+++ playgrounds/react-scripts/project/src/calc.js
@@ -0,0 +1,1 @@
+export const getRandomNumber = () => Math.round(Math.random() * 1000)
