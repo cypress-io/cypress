96e5d861eb7dde1630e69d67df4105ce215b9187
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-03-02 15:48:45 +0200

    Revert unnecessary changes

--- scripts/win-appveyor-build.js
+++ scripts/win-appveyor-build.js
@@ -25,7 +25,7 @@ const isRightBranch = () => {
     process.env.APPVEYOR_REPO_COMMIT_MESSAGE || ''
   ).includes('[build binary]')
 
-  const branchesToBuildBinary = ['develop', 'feature/cross-platform-wizard', 'sem-next-ver']
+  const branchesToBuildBinary = ['develop', 'sem-next-ver']
 
   return branchesToBuildBinary.includes(branch) || shouldForceBinaryBuild
 }
