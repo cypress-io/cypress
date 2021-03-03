3882d51e74b7d097193f0f26a799339814e98763
Zach Panzarino <zachary@panzarino.com>
2021-03-03 17:44:00 -0500

    Remove duplicated restart event Sneak that change in here since it's not worth a separate PR

--- packages/runner/src/lib/event-manager.js
+++ packages/runner/src/lib/event-manager.js
@@ -26,7 +26,7 @@ const driverToLocalAndReporterEvents = 'run:start run:end'.split(' ')
 const driverToSocketEvents = 'backend:request automation:request mocha recorder:frame'.split(' ')
 const driverTestEvents = 'test:before:run:async test:after:run'.split(' ')
 const driverToLocalEvents = 'viewport:changed config stop url:changed page:loading visit:failed'.split(' ')
-const socketRerunEvents = 'runner:restart watched:file:changed'.split(' ')
+const socketRerunEvents = 'runner:restart'.split(' ')
 const socketToDriverEvents = 'net:event script:error'.split(' ')
 const localToReporterEvents = 'reporter:log:add reporter:log:state:changed reporter:log:remove'.split(' ')
 
