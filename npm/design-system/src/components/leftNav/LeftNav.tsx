a0d613840416e1ffb5e92294a07b8e713b2dfea2
Jessica Sachs <jess@jessicasachs.io>
2021-03-04 03:53:14 -0500

    wip: active index

--- npm/design-system/src/components/leftNav/LeftNav.tsx
+++ npm/design-system/src/components/leftNav/LeftNav.tsx
@@ -7,14 +7,14 @@ import { LeftNavProps, NavButtonProps } from './types'
 
 export const LeftNav: React.FC<LeftNavProps> = ({ items, activeIndex, leftNavClasses, navButtonClasses }) => {
   return (<nav className={cs(styles.leftNav, leftNavClasses || '')}>
-    {items.map((item, i) => <NavButtonCell key={item.id} item={item} isActive={i === activeIndex} navButtonClasses={navButtonClasses} idx={i}/>)}
+    {items.map((item, i) => <NavButtonCell key={item.id} item={item} isActive={i === activeIndex} navButtonClasses={navButtonClasses} index={i}/>)}
   </nav>)
 }
 
-export const NavButtonCell: React.FC<NavButtonProps> = ({ item: { title, icon, interaction, itemClasses, itemClassesActive = '', itemClassesInactive = '' }, isActive, navButtonClasses, idx }) => {
+export const NavButtonCell: React.FC<NavButtonProps> = ({ item: { title, icon, interaction, itemClasses, itemClassesActive = '', itemClassesInactive = '' }, isActive, navButtonClasses, index }) => {
   return (
     <a
-      href={interaction.type === 'anchor' ? interaction.href : '#'}
+      href={interaction.type === 'anchor' ? interaction.href : 'javascript:;'}
       className={cs(styles.item, itemClasses, navButtonClasses, {
         [styles.active]: isActive,
         [styles.inactive]: !isActive,
@@ -23,7 +23,7 @@ export const NavButtonCell: React.FC<NavButtonProps> = ({ item: { title, icon, i
         [styles.itemAnchor]: interaction.type === 'anchor',
       })}
       title={title}
-      onClick={interaction.type === 'js' ? () => interaction.onClick(idx) : undefined}>
+      onClick={interaction.type === 'js' ? () => interaction.onClick(index) : undefined}>
       <FontAwesomeIcon className={styles.icon} icon={icon} size="1x" />
     </a>)
 }
