1380bcf95dadd59de09039cdf0ebbefe3b247853
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-05 18:54:07 +1000

    chore: build

--- npm/design-system/src/index.ts
+++ npm/design-system/src/index.ts
@@ -4,4 +4,4 @@ export * from './components/CypressLogo/CypressLogo'
 
 export * from './components/SearchInput/SearchInput'
 
-export * from './components/LeftNav'
+export * from './components/LeftNav/LeftNav'
