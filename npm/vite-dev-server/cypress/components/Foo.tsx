fe9d6d53dc0247a166f98d67660842f79cde7140
ElevateBart <ledouxb@gmail.com>
2021-03-09 16:18:38 -0600

    fix: make sure html is published

--- npm/vite-dev-server/cypress/components/Foo.tsx
+++ npm/vite-dev-server/cypress/components/Foo.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import _ from 'lodash'
+import SplitPane from 'react-split-pane'
 
 export const Foo: React.FC = () => {
-  return <div>Hello world!!!!</div>
+  const items = ['a', 'b', 'c']
+
+  return (<SplitPane>
+    {_.map(items, (letter) => <div>Hello world!!!! {letter}</div>)}
+  </SplitPane>)
 }
