e04070562aa79fdf12a1a10fd2979c9e336ece3e
ElevateBart <ledouxb@gmail.com>
2021-03-09 17:11:28 -0600

    simplify and rollback changes

--- npm/vite-dev-server/cypress/components/Foo.tsx
+++ npm/vite-dev-server/cypress/components/Foo.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import _ from 'lodash'
-import SplitPane from 'react-split-pane'
 
 export const Foo: React.FC = () => {
-  const items = ['a', 'b', 'c']
-
-  return (<SplitPane>
-    {_.map(items, (letter) => <div>Hello world!!!! {letter}</div>)}
-  </SplitPane>)
+  return <div>Hello world!!!!</div>
 }
