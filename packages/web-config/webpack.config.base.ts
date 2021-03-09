e69b9968912471b9ece6298afd47fc6f14728813
Jessica Sachs <jess@jessicasachs.io>
2021-03-09 01:16:43 -0500

    feat: Runner-CT UI Improvements (#15327)
    * feat: initial search input component
    
    * Ugly LeftNav control
    
    * chore: fixing the broken relative url specs
    
    * feat: cleaning up the left nav styles and adding a more interactive spec
    
    * chore: rebase
    
    * wip: nav bar external link fix
    
    * wip: active index
    
    * wip: active index
    
    * wip: more css modules
    
    * wip: dark mode/light mode support devtools
    
    * spwip: split left nav
    
    * wip: runner-ui-changes
    
    * wip: box shadows, borders, and more
    
    * fix: types
    
    * chore: extract component
    
    * chore: extract component for readability
    
    * chore: add checks for width on pane
    
    * chore: refactor to make it more easier to understand what is rendered in panes
    
    * chore: move things around
    
    * chore: move things around
    
    * chore: fix style
    
    * chore: fix shortcuts
    
    * tests: fix a test
    
    * fix: fix screenshots feature
    
    * chore: add placeholder
    
    * chore: rebase
    
    * fix: casing
    
    * chore: build
    
    * chore: old file
    
    * chore: update
    
    * chore: update name
    
    * wip: fixing bugs in UI
    
    * chore: improve resize logic
    
    * chore: improve resize logic
    
    * chore: remove unused code
    
    * chore: styling
    
    * chore: fix screenshotting
    
    * chore: style
    
    * chore: fix lint
    
    * chore: bump bin size
    
    * chore: revert changes in runnables
    
    * fix: remove padding during screenshot
    
    * chore: rebase and reauthor commit
    
    * fix: correctly open spec list when focusing
    
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>

--- packages/web-config/webpack.config.base.ts
+++ packages/web-config/webpack.config.base.ts
@@ -1,6 +1,7 @@
 import chalk from 'chalk'
 import CleanWebpackPlugin from 'clean-webpack-plugin'
-import webpack from 'webpack'
+const webpack = require('webpack')
+import { RuleSetRule, DefinePlugin, Configuration } from 'webpack'
 // @ts-ignore
 import LiveReloadPlugin from 'webpack-livereload-plugin'
 
@@ -8,6 +9,7 @@ import LiveReloadPlugin from 'webpack-livereload-plugin'
 import sassGlobImporter = require('node-sass-glob-importer')
 import HtmlWebpackPlugin = require('html-webpack-plugin')
 import MiniCSSExtractWebpackPlugin = require('mini-css-extract-plugin')
+// import { RuleSetRule } from 'webpack'
 
 const env = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 const args = process.argv.slice(2)
@@ -28,8 +30,50 @@ const evalDevToolPlugin = new webpack.EvalDevToolModulePlugin({
 
 evalDevToolPlugin.evalDevToolPlugin = true
 
+function makeSassLoaders ({ modules }): RuleSetRule {
+  const exclude = [/node_modules/]
+
+  if (!modules) exclude.push(/\.modules?\.s[ac]ss$/i)
+
+  return {
+    test: modules ? /\.modules?\.s[ac]ss$/i : /\.s[ac]ss$/i,
+    exclude,
+    enforce: 'pre',
+    use: [
+      {
+        loader: require.resolve('css-loader'),
+        options: {
+          // sourceMap: true,
+          modules,
+        },
+      }, // translates CSS into CommonJS
+      {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          plugins: [
+            require('autoprefixer')({ overrideBrowserslist: ['last 2 versions'], cascade: false }),
+          ],
+        },
+      },
+      {
+        loader: require.resolve('resolve-url-loader'),
+      },
+      {
+        loader: require.resolve('sass-loader'),
+        options: {
+          implementation: require('sass'),
+          sourceMap: true,
+          sassOptions: {
+            importer: sassGlobImporter(),
+          },
+        },
+      }, // compiles Sass to CSS, using Node Sass by default
+    ],
+  }
+}
+
 const getCommonConfig = () => {
-  const commonConfig: webpack.Configuration = {
+  const commonConfig: Configuration = {
     mode: 'none',
     node: {
       fs: 'empty',
@@ -84,41 +128,8 @@ const getCommonConfig = () => {
             { loader: MiniCSSExtractWebpackPlugin.loader },
           ],
         },
-        {
-          test: /\.s?css$/,
-          exclude: /node_modules/,
-          enforce: 'pre',
-          use: [
-            {
-              loader: require.resolve('css-loader'),
-              options: {
-                // sourceMap: true,
-                modules: false,
-              },
-            }, // translates CSS into CommonJS
-            {
-              loader: require.resolve('postcss-loader'),
-              options: {
-                plugins: [
-                  require('autoprefixer')({ overrideBrowserslist: ['last 2 versions'], cascade: false }),
-                ],
-              },
-            },
-            {
-              loader: require.resolve('resolve-url-loader'),
-            },
-            {
-              loader: require.resolve('sass-loader'),
-              options: {
-                implementation: require('sass'),
-                sourceMap: true,
-                sassOptions: {
-                  importer: sassGlobImporter(),
-                },
-              },
-            }, // compiles Sass to CSS, using Node Sass by default
-          ],
-        },
+        makeSassLoaders({ modules: false }),
+        makeSassLoaders({ modules: true }),
         {
           test: /\.(eot|svg|ttf|woff|woff2)$/,
           use: [
@@ -184,7 +195,7 @@ const getCommonConfig = () => {
 
       ...[
         (env === 'production'
-          ? new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') })
+          ? new DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') })
           : evalDevToolPlugin
         ),
       ],
