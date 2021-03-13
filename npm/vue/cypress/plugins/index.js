a94c9d5ef0da8559f20391fc14396d71fdca7a2f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-13 18:47:49 +1000

    feat: support ct/e2e specific overrides in cypress.json (#15444)
    Co-authored-by: ElevateBart <ledouxb@gmail.com>
    Co-authored-by: Jessica Sachs <jess@jessicasachs.io>

--- npm/vue/cypress/plugins/index.js
+++ npm/vue/cypress/plugins/index.js
@@ -5,7 +5,11 @@ const webpackConfig = require('../../webpack.config')
 /**
  * @type Cypress.PluginConfig
  */
-module.exports = (on, config) => {
+module.exports = (on, config, mode) => {
+  if (mode !== 'component') {
+    throw Error('This is a component testing project. mode should be `component`.')
+  }
+
   require('@cypress/code-coverage/task')(on, config)
   on('dev-server:start', (options) => startDevServer({ options, webpackConfig }))
 
--- packages/server/test/unit/settings_spec.js
+++ packages/server/test/unit/settings_spec.js
@@ -111,6 +111,42 @@ describe('lib/settings', () => {
         })
       })
 
+      it('promises cypress.json and merges CT specific properties for via testingType: component', function () {
+        return this.setup({ a: 'b', component: { a: 'c' } })
+        .then(() => {
+          return settings.read(projectRoot, { testingType: 'component' })
+        }).then((obj) => {
+          expect(obj).to.deep.eq({ a: 'c' })
+        })
+      })
+
+      it('promises cypress.json and merges CT specific properties for via componentTesting: true', function () {
+        return this.setup({ a: 'b', component: { a: 'c' } })
+        .then(() => {
+          return settings.read(projectRoot, { componentTesting: true })
+        }).then((obj) => {
+          expect(obj).to.deep.eq({ a: 'c' })
+        })
+      })
+
+      it('promises cypress.json and merges CT specific properties for via experimentalComponentTesting: true', function () {
+        return this.setup({ a: 'b', component: { a: 'c' } })
+        .then(() => {
+          return settings.read(projectRoot, { experimentalComponentTesting: true })
+        }).then((obj) => {
+          expect(obj).to.deep.eq({ a: 'c' })
+        })
+      })
+
+      it('promises cypress.json and merges e2e specific properties', function () {
+        return this.setup({ a: 'b', e2e: { a: 'c' } })
+        .then(() => {
+          return settings.read(projectRoot)
+        }).then((obj) => {
+          expect(obj).to.deep.eq({ a: 'c' })
+        })
+      })
+
       it('renames commandTimeout -> defaultCommandTimeout', function () {
         return this.setup({ commandTimeout: 30000, foo: 'bar' })
         .then(() => {
