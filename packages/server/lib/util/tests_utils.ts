52ed76110da96d1c8a5da0bd1a4d38db4f12d7d2
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2021-03-16 15:14:14 -0400

    fix not sending tests in some cases

--- packages/server/lib/util/tests_utils.ts
+++ packages/server/lib/util/tests_utils.ts
@@ -1,16 +1,16 @@
 import _ from 'lodash'
 
 export const flattenSuiteIntoRunnables = (suite, tests = [], hooks = []) => {
-  if (!suite || !suite.suites) {
-    return [tests, hooks]
-  }
-
   if (_.isArray(suite)) {
     return _.map(suite, (s) => flattenSuiteIntoRunnables(s))
     .reduce(
       (arr1, arr2) => [arr1[0].concat(arr2[0]), arr1[1].concat(arr2[1])],
       [tests, hooks],
     )
+  }
+
+  if (!suite || !suite.suites) {
+    return [tests, hooks]
   }
 
   tests = tests.concat(suite.tests)
