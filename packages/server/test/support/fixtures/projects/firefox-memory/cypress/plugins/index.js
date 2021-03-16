7bc0f0d6e444e8a23a66b297adf96d43b2f2a9df
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-17 01:50:35 +1000

    feat: cypress.json and plugin runner specific overrides

--- packages/server/test/support/fixtures/projects/firefox-memory/cypress/plugins/index.js
+++ packages/server/test/support/fixtures/projects/firefox-memory/cypress/plugins/index.js
@@ -20,7 +20,11 @@ let timings = []
 let rss = []
 let intervalId
 
-module.exports = (on, config) => {
+module.exports = (on, config, mode) => {
+  if (mode !== 'e2e') {
+    throw Error('This is an e2e project. mode should be `e2e`.')
+  }
+
   on('task', {
     'console' (...args) {
       console.log(...args)
