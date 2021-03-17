493eed38c4b6d1a358680726345af70d3f6a6cd7
ElevateBart <ledouxb@gmail.com>
2021-03-16 21:16:44 -0500

    adjusting for vue 3 tests too

---
+++ npm/vite-dev-server/cypress/components/vue/Hello.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <input v-model="username" data-cy="username" />
+    <div v-if="error" class="error">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  data() {
+    return {
+      username: "",
+    };
+  },
+
+  computed: {
+    error() {
+      console.log(this.username);
+      return this.username.trim().length < 7
+        ? "Please enter a longer username"
+        : "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  background: hotpink;
+  padding: 20px;
+}
+</style>
--- npm/vite-dev-server/cypress/plugins.js
+++ npm/vite-dev-server/cypress/plugins.js
@@ -1,7 +1,19 @@
 import { startDevServer } from '@cypress/vite-dev-server'
+import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react-refresh'
 
 module.exports = (on, config) => {
-  on('dev-server:start', async (options) => startDevServer({ options }))
+  on('dev-server:start', async (options) => {
+    return startDevServer({
+      options,
+      viteConfig: {
+        plugins: [
+          vue(),
+          react(),
+        ],
+      },
+    })
+  })
 
   return config
 }
