27cd9aa313be463d84e9225f77ae1a415d3a4dc7
Zach Bloomquist <github@chary.us>
2021-03-18 18:03:20 +0000

    chore: fix NEXT_VERSION override
    Wasn't actually returning NEXT_VERSION...

--- scripts/get-next-version.js
+++ scripts/get-next-version.js
@@ -9,9 +9,13 @@ const bump = Bluebird.promisify(bumpCb)
 const paths = ['packages', 'cli']
 
 // allow the semantic next version to be overridden by environment
-let nextVersion = process.env.NEXT_VERSION
+let nextVersion
 
 const getNextVersionForPath = async (path) => {
+  if (process.env.NEXT_VERSION) {
+    return process.env.NEXT_VERSION
+  }
+  
   const { releaseType } = await bump({ preset: 'angular', path })
 
   return semver.inc(currentVersion, releaseType || 'patch')
