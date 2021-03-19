c20a536d9d0c71930cb241a60e7d33ba58c2fa5f
Thorsten Lorenz <thlorenz@gmx.de>
2021-03-19 15:59:52 -0500

    snap: hook require uses snapshot when env var is set

--- packages/ts/hook-require.js
+++ packages/ts/hook-require.js
@@ -1,20 +1,57 @@
 'use strict'
 
-const path = require('path')
-const { DirtSimpleFileCache } = require('dirt-simple-file-cache')
-const { packherdRequire } = require('packherd/dist/src/require.js')
+// @ts-check
 
-const projectBaseDir = path.dirname(require.resolve('../../package.json'))
+const { createConfig } = require('@packages/snapshot')
+const env = process.env.CYPRESS_ENV === 'production' ? 'prod' : 'dev'
+const config = createConfig(env)
 
-packherdRequire(projectBaseDir, {
-  transpileOpts: {
-    supportTS: true,
-    initTranspileCache: DirtSimpleFileCache.initSync,
-    tsconfig: {
-      compilerOptions: {
-        useDefineForClassFields: false, // default
-        importsNotUsedAsValues: 'remove', // default
+const isDev = env === 'dev'
+
+if (process.env.USE_SNAPSHOT != null) {
+  runWithSnapshot()
+} else {
+  runWithoutSnapshot()
+}
+
+function runWithSnapshot () {
+  const { snapshotRequire } = require('v8-snapshot/dist/loading/snapshot-require')
+  const { projectBaseDir } = config
+
+  snapshotRequire(projectBaseDir, {
+    diagnostics: isDev,
+    useCache: true,
+    transpileOpts: {
+      supportTS: isDev,
+      initTranspileCache: isDev
+        ? require('dirt-simple-file-cache').DirtSimpleFileCache.initSync
+        : function () {},
+      tsconfig: {
+        compilerOptions: {
+          useDefineForClassFields: false, // default
+          importsNotUsedAsValues: 'remove', // default
+        },
       },
     },
-  },
-})
+
+  })
+}
+
+function runWithoutSnapshot () {
+  const { DirtSimpleFileCache } = require('dirt-simple-file-cache')
+  const { packherdRequire } = require('packherd/dist/src/require.js')
+  const { projectBaseDir } = config
+
+  packherdRequire(projectBaseDir, {
+    transpileOpts: {
+      supportTS: true,
+      initTranspileCache: DirtSimpleFileCache.initSync,
+      tsconfig: {
+        compilerOptions: {
+          useDefineForClassFields: false, // default
+          importsNotUsedAsValues: 'remove', // default
+        },
+      },
+    },
+  })
+}
