d7aa3362c4044ce913dd906d5f302983bd24112b
Adam Gastineau <adamg@cypress.io>
2021-03-19 10:35:31 -0700

    Added new linting rules to design-system

--- npm/design-system/src/components/Button/Button.tsx
+++ npm/design-system/src/components/Button/Button.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
 import { button } from './Button.module.scss'
 
-export const Button = () => (<button className={button}>Hello World</button>)
+export const Button = () => (
+  <button className={button}>
+Hello World
+  </button>
+)
