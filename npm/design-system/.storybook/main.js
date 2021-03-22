e1371726e586821bd836bf62dba41bb5472bb1d7
Adam Gastineau <adamg@cypress.io>
2021-03-22 13:12:43 -0700

    Basic Storybook palette rendering

--- npm/design-system/.storybook/main.js
+++ npm/design-system/.storybook/main.js
@@ -6,25 +6,17 @@ module.exports = {
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   'addons': [
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+          localIdentName: '[name]__[local]--[hash:base64:5]',
+        },
+      },
+    },
+    '@storybook/preset-typescript',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
   ],
-
-  // Export a function. Accept the base config as the only param.
-
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    })
-
-    // Return the altered config
-    return config
-  },
 }
--- npm/design-system/src/stories/Colors.stories.tsx
+++ npm/design-system/src/stories/Colors.stories.tsx
@@ -3,22 +3,42 @@ import { Story } from '@storybook/react'
 
 import { createStory, createStorybookConfig } from './util'
 
-import styles from './colors.scss'
-
-// export default {
-//   title: 'Example/Button',
-//   component: Button,
-//   argTypes: {
-//     backgroundColor: { control: 'color' },
-//   },
-// } as Meta;
+import styles from './colors.module.scss'
+import colors from '../css/colors.scss'
+import '../index.scss'
 
 export default createStorybookConfig({
   title: 'System/Colors',
 })
 
-const Template: Story = () => (<div>
-  <div className={`${styles.colorBlock} .bg-brand-00`}></div>
-</div>)
+const Color: React.FC<{
+  name: string
+  color: string
+}> = ({ name, color }) => {
+  const match = name.match(/([A-Z]+)([0-9]+)/i)
+
+  if (!match) {
+    return <div>{`Could not parse color ${name}`}</div>
+  }
+
+  // TODO: Group colors based on type
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [, type, number] = match
+
+  let textColor = parseInt(number, 10) < 50 ? '--metal-90' : '--metal-10'
+
+  if (name === 'brand01') {
+    // Override secondary brand
+    textColor = '--metal-10'
+  }
+
+  return <div className={styles.colorBlock} style={{ backgroundColor: color, color: `var(${textColor})` }}>{name}</div>
+}
+
+const Template: Story = () => (
+  <div>
+    {Object.keys(colors).map((name: string) => <Color key={name} name={name} color={colors[name]} />)}
+  </div>
+)
 
 export const Colors = createStory(Template)
