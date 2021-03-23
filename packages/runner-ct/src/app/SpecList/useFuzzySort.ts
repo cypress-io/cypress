9b1ca9256f9e4e689708d7dfa0cbf154a15ade9a
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-24 09:23:34 +1000

    simplify code

--- packages/runner-ct/src/app/SpecList/useFuzzySort.ts
+++ packages/runner-ct/src/app/SpecList/useFuzzySort.ts
@@ -11,7 +11,6 @@ interface UseFuzzySort<Input, Result> {
 export const useFuzzySort = <T, R>({ search, items, transformResult, options }: UseFuzzySort<T, R>) => {
   const [lastSearchInput, setLastSearchInput] = React.useState<string>(search)
   const [results, setResults] = React.useState<R[]>([])
-  const searchPromise = React.useRef<Fuzzysort.CancelablePromise<Fuzzysort.KeyResults<T>>>()
 
   React.useEffect(() => {
     // If no search was entered we just show all the items.
@@ -21,7 +20,6 @@ export const useFuzzySort = <T, R>({ search, items, transformResult, options }: 
 
     const promise = fuzzysort.goAsync(lastSearchInput, items, options)
 
-    searchPromise.current = promise
     promise.then((result) => {
       return setResults(result.map((res) => {
         return transformResult(res.obj, res.indexes)
@@ -29,7 +27,7 @@ export const useFuzzySort = <T, R>({ search, items, transformResult, options }: 
     })
 
     return () => {
-      searchPromise.current?.cancel()
+      promise.cancel()
     }
   }, [lastSearchInput, items, setResults, transformResult])
 
