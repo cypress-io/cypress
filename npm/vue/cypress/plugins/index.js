45cfe4d34fca6836e58f89936d045b4b4c357eab
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-03-23 14:23:28 +0200

    chore(component-testing): Correctly handle errors in plugins for dev-server events

--- npm/vue/cypress/plugins/index.js
+++ npm/vue/cypress/plugins/index.js
@@ -11,7 +11,9 @@ module.exports = (on, config) => {
   }
 
   require('@cypress/code-coverage/task')(on, config)
-  on('dev-server:start', (options) => startDevServer({ options, webpackConfig }))
+  on('dev-server:start', (options) => {
+    startDevServer({ options, webpackConfig })
+  })
 
   return config
 }
