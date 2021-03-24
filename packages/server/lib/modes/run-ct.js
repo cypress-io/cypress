52ff4034cb3ebf178012a88738572a166c6ed1e4
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-03-24 12:35:42 +0200

    Revert "fix(component-testing): Correctly exit process in run mode for compilation errors"
    This reverts commit 45f8e72101cc6aeb1b46273193e685a629eaf1b2.

--- packages/server/lib/modes/run-ct.js
+++ packages/server/lib/modes/run-ct.js
@@ -3,20 +3,6 @@ const run = (options) => {
   options.browser = options.browser || 'chrome'
   options.runAllSpecsInSameBrowserSession = true
 
-  require('../plugins/dev-server').emitter.on('dev-server:compile:error', (error) => {
-    options.onError(
-      new Error(`Dev-server compilation failed. We can not run tests if dev-server can not compile and emit assets, please make sure that all syntax errors resolved before running cypress. \n\n ${error}`),
-    )
-
-    // because in run mode we decided to build all the bundle and avoid lazy compilation
-    // if we get compilation error (e.g. from webpack) it means that no assets were emitted
-    // and we can not run any tests, even if the error belongs to the different module
-    // that's why the only way to avoid this is to close the process
-    require('../open_project').closeBrowser().then(() => {
-      process.exit(1)
-    })
-  })
-
   // if we're in run mode with component
   // testing then just pass this through
   // without waiting on electron to be ready
