0a301684d775b777836c452161330494c4d18bd6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-25 14:56:20 +1000

    fix: scale AUT correctly

---
+++ packages/runner-ct/cypress/component/ResizeSpecList.spec.tsx
@@ -0,0 +1,80 @@
+
+/// <reference types="@percy/cypress" />
+import React from 'react'
+import { mount } from '@cypress/react'
+import RunnerCt from '../../src/app/RunnerCt'
+import State from '../../src/lib/state'
+import '@packages/runner/src/main.scss'
+import eventManager from '../../src/lib/event-manager'
+
+const fakeConfig = { projectName: 'Project', env: {}, isTextTerminal: false } as any as Cypress.RuntimeConfigOptions
+const makeState = (options = {}) => (new State({
+  reporterWidth: 500,
+  spec: null,
+  specs: [{ relative: '/test.js', absolute: 'root/test.js', name: 'test.js' }],
+  ...options,
+}, fakeConfig))
+
+
+const testSpecFile = `
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width,initial-scale=1.0">
+      <title>Components App</title>
+    </head>
+    <body>
+      <div id="__cy_root"></div>
+    <script type="text/javascript">
+      // NO-OP is fine for now
+    </script></body>
+  </html>
+`
+
+describe('RunnerCt', () => {
+  beforeEach(() => {
+    cy.viewport(1000, 500)
+  })
+
+  it('resizes spec list', () => {
+    cy.intercept('GET', 'http://localhost:60728/undefined/iframes//test.js', {
+      statusCode: 200,
+      body: testSpecFile
+    })
+
+    mount(
+      <RunnerCt
+        state={makeState()}
+        eventManager={eventManager}
+        // @ts-ignore - TODO: define a good test config.
+        config={fakeConfig}
+      />,
+    )
+
+    // select spec.
+    cy.get('[data-item="/test.js"').click()
+
+    cy.get('.size-container').then(($el) => {
+      const style = $el.attr('style')
+      // extract inline transform value.
+      const [, initialScale] = style.match(/transform.\s(.+);/)
+
+      // resize reporter
+      cy.get('.Resizer.vertical').eq(1)
+        .trigger('mousedown', { eventConstructor: 'MouseEvent' })
+        .trigger('mousemove', -200, -200, { eventConstructor: 'MouseEvent', force: true })
+        .trigger('mouseup')
+
+
+      // AUT scale should be different.
+      cy.get('.size-container').then(($el) => {
+        const newStyle = $el.attr('style')
+        const [, newScale] = newStyle.match(/transform.\s(.+);/)
+        expect(newScale).not.to.eq(initialScale)
+      })
+    })
+
+    cy.percySnapshot()
+  })
+})
