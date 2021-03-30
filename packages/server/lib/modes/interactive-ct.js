ac33f36dabf576ae9a8f88cba2a2108d14181e38
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-30 12:09:24 +1000

    fix: default to electron for runner-ct. Co-authored-by: Brian Mann <brian.mann86@gmail.com>

--- packages/server/lib/modes/interactive-ct.js
+++ packages/server/lib/modes/interactive-ct.js
@@ -3,7 +3,7 @@ const serverCt = require('@packages/server-ct')
 const run = (options) => {
   const { projectRoot } = options
 
-  options.browser = options.browser || 'chrome'
+  options.browser = options.browser || 'electron'
 
   return serverCt.start(projectRoot, options)
 }
--- packages/server/lib/modes/run-ct.js
+++ packages/server/lib/modes/run-ct.js
@@ -1,21 +1,6 @@
 const run = (options) => {
   // TODO: make sure if we need to run this in electron by default to match e2e behavior?
-  options.browser = options.browser || 'chrome'
   options.runAllSpecsInSameBrowserSession = true
-
-  require('../plugins/dev-server').emitter.on('dev-server:compile:error', (error) => {
-    options.onError(
-      new Error(`Dev-server compilation failed. We can not run tests if dev-server can not compile and emit assets, please make sure that all syntax errors resolved before running cypress. \n\n ${error}`),
-    )
-
-    // because in run mode we decided to build all the bundle and avoid lazy compilation
-    // if we get compilation error (e.g. from webpack) it means that no assets were emitted
-    // and we can not run any tests, even if the error belongs to the different module
-    // that's why the only way to avoid this is to close the process
-    require('../open_project').closeBrowser().then(() => {
-      process.exit(1)
-    })
-  })
 
   // if we're in run mode with component
   // testing then just pass this through
