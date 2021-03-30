f524175a9241584ce3a29418d040bb7ec1871da4
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-30 13:11:39 +1000

    fix test.
    Co-authored-by: Brian Mann <brian.mann86@gmail.com>

--- cli/test/lib/cli_spec.js
+++ cli/test/lib/cli_spec.js
@@ -630,12 +630,14 @@ describe('cli', () => {
 
     it('spawns server with correct args for component-testing', () => {
       this.exec('open-ct --dev')
-      expect(spawn.start.firstCall.args[0]).to.include('--componentTesting')
+      expect(spawn.start.firstCall.args[0]).to.include('--testing-type')
+      expect(spawn.start.firstCall.args[0]).to.include('component')
     })
 
     it('runs server with correct args for component-testing', () => {
       this.exec('run-ct --dev')
-      expect(spawn.start.firstCall.args[0]).to.include('--componentTesting')
+      expect(spawn.start.firstCall.args[0]).to.include('--testing-type')
+      expect(spawn.start.firstCall.args[0]).to.include('component')
     })
 
     it('does not display open-ct command in the help', () => {
