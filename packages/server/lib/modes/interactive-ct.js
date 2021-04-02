e4f5bb7181aea3046563f630289a0c4684fcb1d9
Jessica Sachs <jess@jessicasachs.io>
2021-04-02 18:43:01 -0400

    fix: switching to use chrome as default in open mode to avoid element explorer issue (#15777)

--- packages/server/lib/modes/interactive-ct.js
+++ packages/server/lib/modes/interactive-ct.js
@@ -3,7 +3,7 @@ const serverCt = require('@packages/server-ct')
 const run = (options) => {
   const { projectRoot } = options
 
-  options.browser = options.browser || 'electron'
+  options.browser = options.browser || 'chrome'
 
   return serverCt.start(projectRoot, options)
 }
