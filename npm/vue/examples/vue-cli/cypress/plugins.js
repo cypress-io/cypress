4f09c29d5448c3ee22433c515f346ed238e8188d
ElevateBart <ledouxb@gmail.com>
2021-04-05 17:42:49 -0500

    test: fix CLI example

--- npm/vue/examples/vue-cli/cypress/plugins.js
+++ npm/vue/examples/vue-cli/cypress/plugins.js
@@ -1,14 +1,12 @@
 /// <reference types="cypress" />
 const { startDevServer } = require('@cypress/webpack-dev-server')
-const webpackConfig = require('@vue/cli-service/webpack.config').default
+const webpackConfig = require('@vue/cli-service/webpack.config')
 
 /**
  * @type Cypress.PluginConfig
  */
 module.exports = (on, config) => {
-  on('dev-server:start', async (options) => {
-    delete webpackConfig.output
-
+  on('dev-server:start', (options) => {
     return startDevServer({
       options,
       webpackConfig,
