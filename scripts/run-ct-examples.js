18713cf3e64f72e112114888e2a8b9f532fa52e1
ElevateBart <ledouxb@gmail.com>
2021-04-05 21:54:10 -0500

    more clear fix

--- scripts/run-ct-examples.js
+++ scripts/run-ct-examples.js
@@ -47,9 +47,7 @@ const runTests = async (dir) => {
 const main = async () => {
   const NODE_INDEX = process.env.CIRCLE_NODE_INDEX || 0
 
-  console.log('PROJECTS_FOR_CI.length', PROJECTS_FOR_CI.length)
-
-  if (PROJECTS_FOR_CI.length < (NODE_INDEX + 1)) {
+  if (NODE_INDEX > PROJECTS_FOR_CI.length - 1) {
     return
   }
 
