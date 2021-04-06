58ed385d9b0664feddc478c27728ee8435362fab
ElevateBart <ledouxb@gmail.com>
2021-04-05 19:37:56 -0500

    adjust the model for .env files making it easy to comment/uncomment the examples opened.

--- scripts/run-ct-examples.js
+++ scripts/run-ct-examples.js
@@ -9,7 +9,9 @@ const args = minimist(process.argv.slice(2))
 
 const filePath = path.resolve(process.cwd(), args.examplesList)
 
-const PROJECTS_FOR_CI = fs.readFileSync(filePath, { encoding: 'utf8' }).split('\n')
+const PROJECTS_FOR_CI = fs.readFileSync(filePath, { encoding: 'utf8' })
+.split('\n')
+.filter((a) => !/^\#/.test(a))
 
 const testResultsDestination = path.resolve(process.cwd(), 'test_results')
 
