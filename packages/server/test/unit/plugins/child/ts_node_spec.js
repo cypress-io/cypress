c0cd9f78c39b9ded9bf343706399b07dd811e195
Chris Breiding <chrisbreiding@users.noreply.github.com>
2021-04-07 10:51:06 -0400

    fix: Prevent a couple warnings from being displayed in stdout (#15828)

---
+++ packages/server/test/unit/plugins/child/ts_node_spec.js
@@ -0,0 +1,44 @@
+require('../../../spec_helper')
+
+const tsnode = require('ts-node')
+
+const resolve = require(`${root}../../lib/util/resolve`)
+
+const tsNodeUtil = require(`${root}../../lib/plugins/child/ts_node`)
+
+describe('lib/plugins/child/ts_node', () => {
+  beforeEach(() => {
+    sinon.stub(tsnode, 'register')
+    sinon.stub(resolve, 'typescript').returns('/path/to/typescript.js')
+  })
+
+  describe('typescript registration', () => {
+    it('registers ts-node if typescript is installed', () => {
+      tsNodeUtil.register('proj-root', '/path/to/plugins/file.js')
+
+      expect(tsnode.register).to.be.calledWith({
+        transpileOnly: true,
+        compiler: '/path/to/typescript.js',
+        dir: '/path/to/plugins',
+        compilerOptions: {
+          module: 'CommonJS',
+        },
+      })
+    })
+
+    it('does not register ts-node if typescript is not installed', () => {
+      resolve.typescript.returns(null)
+
+      tsNodeUtil.register('proj-root', '/path/to/plugins/file.js')
+
+      expect(tsnode.register).not.to.be.called
+    })
+
+    it('prevents tsconfig-paths from logging warning when there is no tsconfig.json', () => {
+      sinon.spy(console, 'warn')
+      tsNodeUtil.register('proj-root', '/path/to/plugins/file.js')
+
+      expect(console.warn).not.to.be.calledWith('Missing baseUrl in compilerOptions. tsconfig-paths will be skipped')
+    })
+  })
+})
