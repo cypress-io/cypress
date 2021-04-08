ce6447d3f7f9837c1157d999357d478dfee4c93f
djsilcock <djsilcock@users.noreply.github.com>
2021-04-08 18:39:39 +0100

    initialise nextjs webpack before importing devserver
    Fixes #15864

--- npm/react/plugins/next/index.js
+++ npm/react/plugins/next/index.js
@@ -1,10 +1,12 @@
-const { startDevServer } = require('@cypress/webpack-dev-server')
 const findNextWebpackConfig = require('./findNextWebpackConfig')
 
 module.exports = (on, config) => {
   on('dev-server:start', async (options) => {
-    return startDevServer({ options, webpackConfig: await findNextWebpackConfig(config) })
+    const webpackConfig=await findNextWebpackConfig(config)
+    const { startDevServer } = require('@cypress/webpack-dev-server')
+    return startDevServer({ options, webpackConfig})
   })
+
 
   config.env.reactDevtools = true
 
