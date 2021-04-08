a7c59b77eaa9212d5c1454b54ceba43d8f56abe4
djsilcock <djsilcock@users.noreply.github.com>
2021-04-08 19:54:21 +0100

    fix: get next config before requiring devserver

--- npm/react/plugins/next/index.js
+++ npm/react/plugins/next/index.js
@@ -1,9 +1,11 @@
-const { startDevServer } = require('@cypress/webpack-dev-server')
 const findNextWebpackConfig = require('./findNextWebpackConfig')
 
 module.exports = (on, config) => {
   on('dev-server:start', async (options) => {
-    return startDevServer({ options, webpackConfig: await findNextWebpackConfig(config) })
+    const webpackConfig = await findNextWebpackConfig(config)
+    //require('webpack') now points to nextjs bundled version
+    const { startDevServer } = require('@cypress/webpack-dev-server')
+    return startDevServer({ options, webpackConfig })
   })
 
   config.env.reactDevtools = true
