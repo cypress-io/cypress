b4847860c078642ce89a2aefdeb2241d4168427e
ElevateBart <ledouxb@gmail.com>
2021-04-08 15:17:25 -0500

    docs: document the vite server options in TSDoc

--- npm/vite-dev-server/src/startServer.ts
+++ npm/vite-dev-server/src/startServer.ts
@@ -14,10 +14,17 @@ interface Options {
 }
 
 export interface StartDevServer {
-  /* this is the Cypress options object */
+  /**
+   * the Cypress options object
+   */
   options: Options
-  /* support passing a path to the user's webpack config */
-  viteConfig?: UserConfig // TODO: implement taking in the user's vite configuration. Right now we don't
+  /**
+   * By default, vite will use your vite.config file to
+   * Start the server. If you need additional plugins or
+   * to override some options, you can do so using this.
+   * @optional
+   */
+  viteConfig?: UserConfig
 }
 
 const resolveServerConfig = async ({ viteConfig, options }: StartDevServer): Promise<InlineConfig> => {
