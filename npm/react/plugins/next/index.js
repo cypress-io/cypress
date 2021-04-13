be95b904ac494f6e466b8146b2987ed331f26a86
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-04-13 10:38:57 +1000

    lint

--- npm/react/plugins/next/index.js
+++ npm/react/plugins/next/index.js
@@ -3,8 +3,10 @@ const findNextWebpackConfig = require('./findNextWebpackConfig')
 module.exports = (on, config) => {
   on('dev-server:start', async (options) => {
     const webpackConfig = await findNextWebpackConfig(config)
-    //require('webpack') now points to nextjs bundled version
+
+    // require('webpack') now points to nextjs bundled version
     const { startDevServer } = require('@cypress/webpack-dev-server')
+
     return startDevServer({ options, webpackConfig })
   })
 
