4aa988fd30ecdb8934d4183bfbae4b9ecbbd1dca
Tim Griesser <tgriesser10@gmail.com>
2021-04-19 13:06:16 -0400

    WIP on better build workflow

--- scripts/circle-cache.js
+++ scripts/circle-cache.js
@@ -6,6 +6,7 @@ const minimist = require('minimist')
 const crypto = require('crypto')
 const fs = require('fs')
 const stringify = require('fast-json-stable-stringify')
+const rootPackageJson = require('../package.json')
 
 const opts = minimist(process.argv.slice(2))
 
@@ -21,7 +22,11 @@ async function circleCache () {
 }
 
 const BASE_DIR = path.join(__dirname, '..')
+const CACHE_DIR = path.join(BASE_DIR, 'globbed_node_modules')
 const p = (str) => path.join(BASE_DIR, str)
+
+const workspacePaths = rootPackageJson.workspaces.packages
+const packageGlobs = workspacePaths.filter((s) => s.endsWith('/*'))
 
 // Gets the sha of all of the patch-package files we have, so we can use this in the cache key.
 // Otherwise, adding/editing a patch will not invalidate the CI cache we have for the yarn install
@@ -31,13 +36,20 @@ async function cacheKey () {
     ignore: ['**/node_modules/**', '**/*_node_modules/**'],
   })
   // TODO: base on workspaces or lerna
-  const packageJsons = await globby([p('package.json'), p('cli/package.json'), p('{npm,packages}/*/package.json')], {
+  const packageJsons = await globby([
+    p('package.json'),
+    ...workspacePaths.map((dir) => p(`${dir}/package.json`)),
+  ], {
     ignore: ['**/node_modules/**', '**/*_node_modules/**'],
   })
 
   // Concat the stable stringify of all of the package.json dependencies that make up
   const hashedPackageDeps = packageJsons.sort().map((abs) => require(abs)).map(
-    ({ name, dependencies, devDependencies, peerDependencies }) => hashString(stringify({ name, dependencies, devDependencies, peerDependencies })),
+    ({ name, dependencies, devDependencies, peerDependencies }) => {
+      return hashString(
+        stringify({ name, dependencies, devDependencies, peerDependencies }),
+      )
+    },
   ).join('')
 
   const filesToHash = yarnLocks.concat(patchFiles).sort()
@@ -53,7 +65,7 @@ async function cacheKey () {
 // https://discuss.circleci.com/t/dynamic-or-programmatic-caching-of-directories/1455
 async function prepareCircleCache () {
   const paths = await globby(
-    p(`{packages,npm}/*/node_modules`),
+    packageGlobs.map((dir) => p(`${dir}/node_modules`)),
     { onlyDirectories: true },
   )
 
@@ -62,16 +74,18 @@ async function prepareCircleCache () {
       await fsExtra.move(
         src,
         src
-        .replace('/packages/', '/packages_node_modules/')
-        .replace('/npm/', '/npm_node_modules/'),
+        .replace(/(.*?)\/node_modules/, '$1_node_modules')
+        .replace(BASE_DIR, CACHE_DIR),
       )
     }),
   )
+
+  console.log(`Moved globbed node_modules for ${packageGlobs.join(', ')} to ${CACHE_DIR}`)
 }
 
 async function unpackCircleCache () {
   const paths = await globby(
-    p(`{packages,npm}_node_modules/*/node_modules`),
+    p(`globbed_node_modules/*/*`),
     { onlyDirectories: true },
   )
 
@@ -80,14 +94,15 @@ async function unpackCircleCache () {
       await fsExtra.move(
         src,
         src
-        .replace('/packages_node_modules/', '/packages/')
-        .replace('/npm_node_modules/', '/npm/'),
+        .replace(CACHE_DIR, BASE_DIR)
+        .replace(/(.*?)_node_modules/, `$1/node_modules`),
       )
     }),
   )
 
-  await fsExtra.remove(p('packages_node_modules'))
-  await fsExtra.remove(p('npm_node_modules'))
+  console.log(`Unpacked globbed node_modules from ${CACHE_DIR} to ${packageGlobs.join(', ')}`)
+
+  await fsExtra.remove(CACHE_DIR)
 }
 
 function hashFile (filePath) {
