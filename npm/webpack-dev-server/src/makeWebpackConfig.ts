6c2bc2239d85b83f3acef4c36e30fad6476d9350
ElevateBart <ledouxb@gmail.com>
2021-04-20 15:40:38 -0500

    chore: avoid replacement when using posix filesystem

--- npm/webpack-dev-server/src/makeWebpackConfig.ts
+++ npm/webpack-dev-server/src/makeWebpackConfig.ts
@@ -23,6 +23,7 @@ interface MakeWebpackConfigOptions extends CypressCTOptionsPluginOptions, UserWe
 }
 
 const OsSeparatorRE = RegExp(`\\${path.sep}`, 'g')
+const posixSeparator = '/'
 
 export async function makeWebpackConfig (userWebpackConfig: webpack.Configuration, options: MakeWebpackConfigOptions): Promise<webpack.Configuration> {
   const { projectRoot, devServerPublicPathRoute, files, supportFile, devServerEvents } = options
@@ -34,11 +35,12 @@ export async function makeWebpackConfig (userWebpackConfig: webpack.Configuratio
   debug(`Support file`, supportFile)
 
   const entry = path.resolve(__dirname, './browser.js')
-
-  // The second line here replaces backslashes on windows with posix compatible slash
-  // See https://github.com/cypress-io/cypress/issues/16097
-  const publicPath = path.join(devServerPublicPathRoute, '/')
-  .replace(OsSeparatorRE, '/')
+  const publicPath = (path.sep === posixSeparator)
+    ? path.join(devServerPublicPathRoute, posixSeparator)
+    // The second line here replaces backslashes on windows with posix compatible slash
+    // See https://github.com/cypress-io/cypress/issues/16097
+    : path.join(devServerPublicPathRoute, posixSeparator)
+    .replace(OsSeparatorRE, posixSeparator)
 
   const dynamicWebpackConfig = {
     output: {
