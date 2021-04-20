58981f8b3c2e9fca592c92c37e1d44bffa61ca28
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-04-20 14:01:42 +1000

    wip

--- npm/react/examples/nextjs-webpack-5/cypress/components/Search.spec.jsx
+++ npm/react/examples/nextjs-webpack-5/cypress/components/Search.spec.jsx
@@ -1,11 +1,14 @@
 /// <reference types="cypress" />
 import * as React from 'react'
+import * as ReactDom from 'react-dom'
 import { mount } from '@cypress/react'
 import { Search } from '../../components/Search'
 
 describe('<Search /> NextJS component', () => {
   it('Renders component', () => {
-    mount(<Search />)
+    mount(<Search />, {
+      ReactDom,
+    })
 
     cy.get('input').type('124152')
     cy.contains('.search-text', '124152').should('be.visible')
