1ac7ae38563d55c72ed681ebf88a3689dfbefb7c
Thorsten Lorenz <thlorenz@gmx.de>
2021-05-04 11:49:34 -0600

    snap: consolidating lodash like we do for bluebird

--- packages/snapshot/lib/consolidate-deps.js
+++ packages/snapshot/lib/consolidate-deps.js
@@ -10,26 +10,20 @@ const debug = require('debug')
 const logInfo = debug('snapgen:info')
 const logError = debug('snapgen:error')
 
-/**
- * Remove all bluebird installations used by the app directly or indirectly,
- * except one.  This step currently runs during `./setup-{prod,dev}` but would
- * make sense as a postinstall as well.
- *
- * @param {Partial<import('../snapconfig').SnapshotConfig>} opts
- */
-function consolidateBluebird ({ projectBaseDir }) {
-  logInfo('Looking for bluebird duplicates ...')
+function consolidateDep ({ projectBaseDir, dep }) {
+  logInfo('Looking for %s duplicates ...', dep)
 
   const matches = glob(
-    '{packages,node_modules/@cypress,node_modules/@packages}/**/node_modules/bluebird',
+    `{packages,node_modules/@cypress,node_modules/@packages}/**/node_modules/${dep}`,
     {
       cwd: projectBaseDir,
     },
   ).map((x) => path.join(projectBaseDir, x))
 
   logInfo(
-    'Found %d bluebird duplicates%s',
+    'Found %d %s duplicates%s',
     matches.length,
+    dep,
     matches.length > 0 ? ', removing them' : '',
   )
 
@@ -38,15 +32,23 @@ function consolidateBluebird ({ projectBaseDir }) {
       fs.rmdirSync(dir, { recursive: true })
     }
 
-    logInfo('Done removing bluebird duplicates')
+    logInfo('Done removing %s duplicates', dep)
   } catch (err) {
     logError(err)
     throw err
   }
 }
 
+/**
+ * Remove all bluebird + lodash installations used by the app directly or indirectly,
+ * except one.  This step currently runs during `./setup-{prod,dev}` but would
+ * make sense as a postinstall as well.
+ *
+ * @param {Partial<import('../snapconfig').SnapshotConfig>} opts
+ */
 function consolidateDeps ({ projectBaseDir }) {
-  return consolidateBluebird({ projectBaseDir })
+  consolidateDep({ projectBaseDir, dep: 'bluebird' })
+  consolidateDep({ projectBaseDir, dep: 'lodash' })
 }
 
 module.exports = { consolidateDeps }
