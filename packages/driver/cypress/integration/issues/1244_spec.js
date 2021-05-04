42358a97ec7f4f0f169cba965736e657949ca123
Tim Griesser <tgriesser10@gmail.com>
2021-05-04 10:42:52 -0400

    improve tests for 1244

--- packages/driver/cypress/integration/issues/1244_spec.js
+++ packages/driver/cypress/integration/issues/1244_spec.js
@@ -1,7 +1,21 @@
 describe('issue 1244', () => {
-  it('correctly redirects when target=_top', () => {
+  it('correctly redirects when target=_top with target.target =', () => {
     cy.visit('/fixtures/issue-1244.html')
-    cy.get('button').click()
+    cy.get('#setTarget').click()
+    cy.get('#dom').should('contain', 'DOM')
+    cy.url().should('include', 'dom.html')
+  })
+
+  it('correctly redirects when target=_top with setAttribute', () => {
+    cy.visit('/fixtures/issue-1244.html')
+    cy.get('#setAttr').click()
+    cy.get('#dom').should('contain', 'DOM')
+    cy.url().should('include', 'dom.html')
+  })
+
+  it('correctly redirects when target=_top inline in dom', () => {
+    cy.visit('/fixtures/issue-1244.html')
+    cy.get('#inline').click()
     cy.get('#dom').should('contain', 'DOM')
     cy.url().should('include', 'dom.html')
   })
