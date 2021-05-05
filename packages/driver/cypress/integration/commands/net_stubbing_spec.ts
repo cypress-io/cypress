8dd114e3976d58645250e58ed0b2d15214632f62
Zach Bloomquist <git@chary.us>
2021-05-05 15:54:34 -0400

    add more assertions

--- packages/driver/cypress/integration/commands/net_stubbing_spec.ts
+++ packages/driver/cypress/integration/commands/net_stubbing_spec.ts
@@ -1565,10 +1565,10 @@ describe('network stubbing', { retries: { runMode: 2, openMode: 0 } }, function 
           const third = sinon.stub()
 
           cy
-          .intercept({ url: '/foo*', times: 3 }, 'fourth')
-          .intercept({ url: '/foo*', times: 2 }, third)
-          .intercept({ url: '/foo*', times: 2 }, 'second')
-          .intercept({ url: '/foo*', times: 1 }, 'first')
+          .intercept({ url: '/foo*', times: 3 }, 'fourth').as('4')
+          .intercept({ url: '/foo*', times: 2 }, third).as('3')
+          .intercept({ url: '/foo*', times: 2 }, 'second').as('2')
+          .intercept({ url: '/foo*', times: 1 }, 'first').as('1')
           .then(async () => {
             const expectGet = (expected) => $.get('/foo').then((res) => expect(res).to.eq(expected))
 
@@ -1582,7 +1582,18 @@ describe('network stubbing', { retries: { runMode: 2, openMode: 0 } }, function 
             ], expectGet)
 
             expect(third).to.be.calledTwice
+
+            // now that matches are exhausted, it should fall through
+            await $.get('/foo').catch((xhr) => {
+              expect(xhr).to.include({
+                status: 404,
+              })
+            })
           })
+          .get('@1.all').should('have.length', 1)
+          .get('@2.all').should('have.length', 2)
+          .get('@3.all').should('have.length', 2)
+          .get('@4.all').should('have.length', 3)
         })
       })
     })
