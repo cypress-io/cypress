4702a5aeef184ff96ce4105b8568b9618e46ee48
ElevateBart <ledouxb@gmail.com>
2021-05-07 11:45:51 -0500

    tests: fix tests for filetree

--- npm/design-system/cypress/support/index.js
+++ npm/design-system/cypress/support/index.js
@@ -7,6 +7,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
+import '../../src/global.scss'
+
 library.add(fas)
 library.add(fab)
 
