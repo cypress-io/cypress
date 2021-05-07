0c69fceeb48da9deea4c66777bbe6be1a57f004c
Tim Griesser <tgriesser10@gmail.com>
2021-05-07 10:40:42 -0400

    Splitting out changes for #1244 into separate PR

--- packages/driver/src/cy/listeners.js
+++ packages/driver/src/cy/listeners.js
@@ -20,10 +20,10 @@ const removeAllListeners = () => {
   return null
 }
 
-const addListener = (win, event, fn, capture) => {
+const addListener = (win, event, fn) => {
   events.push([win, event, fn])
 
-  win.addEventListener(event, fn, capture)
+  win.addEventListener(event, fn)
 }
 
 const eventHasReturnValue = (e) => {
@@ -101,52 +101,7 @@ module.exports = {
       return callbacks.onSubmit(e)
     })
 
-    addListener(contentWindow, 'submit', handleInvalidFormSubmitTarget, true)
-
     contentWindow.alert = callbacks.onAlert
     contentWindow.confirm = callbacks.onConfirm
   },
-}
-
-// Handling the situation where "_top" is set on the <form> element, either in
-// html or dynamically, by tapping in at the capture phase of the events
-function handleInvalidFormSubmitTarget (e) {
-  let targetValue = e.target.target
-
-  if (e.target.target !== '') {
-    e.target.target = ''
-  }
-
-  const { getAttribute, setAttribute } = e.target
-  const targetDescriptor = Object.getOwnPropertyDescriptor(e.target, 'target')
-
-  e.target.getAttribute = function (k) {
-    if (k === 'target') {
-      return targetValue
-    }
-
-    return getAttribute.call(this, k)
-  }
-
-  e.target.setAttribute = function (k, v) {
-    if (k === 'target') {
-      targetValue = v
-
-      return setAttribute.call(this, 'cyTarget', v)
-    }
-
-    return setAttribute.call(this, k, v)
-  }
-
-  if (!targetDescriptor) {
-    Object.defineProperty(e.target, 'target', {
-      configurable: false,
-      set (value) {
-        return this.setAttribute('target', value)
-      },
-      get () {
-        return this.getAttribute('target')
-      },
-    })
-  }
 }
