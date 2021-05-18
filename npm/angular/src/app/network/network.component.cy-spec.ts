0d15cf6b8f76eb8cf8059a7b5cd61994e3a1df9d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-18 10:35:02 +1000

    add test

--- npm/angular/src/app/network/network.component.cy-spec.ts
+++ npm/angular/src/app/network/network.component.cy-spec.ts
@@ -4,15 +4,32 @@ import { NetworkService } from '../network.service'
 import { NetworkComponent } from './network.component'
 
 describe('Network', () => {
-  beforeEach(() => {
-    initEnv(NetworkComponent, {
-      providers: [NetworkService],
-      imports: [HttpClientModule],
+  describe('No mock', () => {
+    beforeEach(() => {
+      initEnv(NetworkComponent, {
+        providers: [NetworkService],
+        imports: [HttpClientModule],
+      })
+    })
+
+    it('fetches 3 users from remote API', () => {
+      mount(NetworkComponent)
+      cy.get('li', { timeout: 20000 }).should('have.length', 3)
     })
   })
 
-  it('fetches 3 users from remote API', () => {
-    mount(NetworkComponent)
-    cy.get('li', { timeout: 20000 }).should('have.length', 3)
+  describe('With mock', () => {
+    beforeEach(() => {
+      cy.intercept('/users?_limit=3', { body: [{ id: 1, name: 'foo' }] })
+      initEnv(NetworkComponent, {
+        providers: [NetworkService],
+        imports: [HttpClientModule],
+      })
+    })
+
+    it('fetch 1 user stubed', () => {
+      mount(NetworkComponent)
+      cy.get('li').should('have.length', 1).first().contains('foo')
+    })
   })
 })
