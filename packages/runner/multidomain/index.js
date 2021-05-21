2aafbb91d3c0d06ff3b7e046620347a5e7abaaa5
Chris Breiding <chrisbreiding@gmail.com>
2021-05-21 10:25:13 -0400

    move majority of multidomain entry point into driver

--- packages/runner/multidomain/index.js
+++ packages/runner/multidomain/index.js
@@ -1,46 +1,8 @@
-import $Cypress from '@packages/driver/src/cypress'
-import $Cy from '@packages/driver/src/cypress/cy'
-import $Commands from '@packages/driver/src/cypress/commands'
-import $Log from '@packages/driver/src/cypress/log'
+import { initialize } from '@packages/driver/src/multidomain'
 
 const autWindow = window.parent.frames[0]
-const specWindow = {
-  Error,
-}
-const Cypress = $Cypress.create({
-  browser: {
-    channel: 'stable',
-    displayName: 'Chrome',
-    family: 'chromium',
-    isChosen: true,
-    isHeaded: true,
-    isHeadless: false,
-    majorVersion: 90,
-    name: 'chrome',
-    path: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    version: '90.0.4430.212',
-  },
-})
-const log = (...args) => {
-  return Cypress.log.apply(this, args)
-}
-const cy = $Cy.create(specWindow, Cypress, Cypress.Cookies, Cypress.state, Cypress.config, log)
 
-Cypress.log = $Log.create(Cypress, cy, Cypress.state, Cypress.config)
-Cypress.runner = {
-  addLog () {},
-}
-
-Cypress.state('window', autWindow)
-Cypress.state('document', autWindow.document)
-Cypress.state('runnable', {
-  ctx: {},
-  clearTimeout () {},
-  resetTimeout () {},
-  timeout () {},
-})
-
-$Commands.create(Cypress, cy, Cypress.state)
+const { cy } = initialize(autWindow)
 
 autWindow.onReady = () => {
   cy.now('get', 'p').then(($el) => {
