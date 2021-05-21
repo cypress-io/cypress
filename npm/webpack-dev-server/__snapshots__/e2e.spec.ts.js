177c4a2e432b3182591161359d739c7a7003ca3b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-21 17:00:49 +1000

    simplify test

--- npm/webpack-dev-server/__snapshots__/e2e.spec.ts.js
+++
@@ -1,8 +0,0 @@
-exports['#startDevServer emits dev-server:compile:error event on error compilation 1'] = `
-./test/fixtures/compilation-fails.spec.js 1:5
-Module parse failed: Unexpected token (1:5)
-You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
-> this is an invalid spec file 
- @ ./dist/browser.js (./dist/loader.js!./dist/browser.js) 5:16-155
- @ ./dist/browser.js
-`
--- npm/webpack-dev-server/test/e2e.spec.ts
+++ npm/webpack-dev-server/test/e2e.spec.ts
@@ -5,7 +5,6 @@ import { expect } from 'chai'
 import { EventEmitter } from 'events'
 import http from 'http'
 import fs from 'fs'
-import snapshot from 'snap-shot-it'
 
 import { startDevServer } from '../'
 
@@ -119,7 +118,10 @@ describe('#startDevServer', () => {
 
     return new Promise((res) => {
       devServerEvents.on('dev-server:compile:error', (err: string) => {
-        snapshot(err)
+        expect(err).to.contain('./test/fixtures/compilation-fails.spec.js 1:5')
+        expect(err).to.contain('Module parse failed: Unexpected token (1:5)')
+        expect(err).to.contain('You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders')
+        expect(err).to.contain('> this is an invalid spec file')
         expect(exitSpy.calledOnce).to.be.true
         close(() => res())
       })
