5ad3c2a02444e1c19070954e69a5aef806b16012
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-28 15:02:30 +1000

    fix types

--- packages/springboard/vue-shims.d.ts
+++ packages/springboard/vue-shims.d.ts
@@ -1,16 +1,6 @@
 import { DefineComponent } from 'vue'
 
-declare global {
-  interface Window {
-    App: {
-      start: () => void
-    }
-  }
-}
-
 declare module '*.vue' {
   const component: DefineComponent
   export default component
 }
-
-export {}
