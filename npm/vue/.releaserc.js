d4a92c66d3b50810896022a8a9bad44bb9f577a2
ElevateBart <ledouxb@gmail.com>
2021-06-01 12:56:23 -0500

    ci: deliver only vue2 on npm/vue/v2

--- npm/vue/.releaserc.js
+++ npm/vue/.releaserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ...require('../../.releaserc.base'),
   branches: [
-    { name: 'npm/vue/v2' },
+    { name: 'npm/vue/v2', range: '2.x' },
     { name: 'master', channel: 'next', prerelease: 'beta' },
   ],
 }
