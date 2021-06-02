1cdf57391fb8f9c10f89fdceb66fb11fdeb505e0
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-02 10:10:20 +1000

    chore: configure windicss

--- packages/springboard/src/main.ts
+++ packages/springboard/src/main.ts
@@ -2,14 +2,6 @@ import { createApp } from 'vue'
 import 'windi.css'
 import App from './App.vue'
 
-declare global {
-  interface Window {
-    App: {
-      start: () => void
-    }
-  }
-}
-
 window.App = {
   start () {
     const app = createApp(App)
