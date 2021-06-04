1da5ea716cc545acc61cd36ce7a692c7518cf5c8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-04 12:40:06 +1000

    chore: add package detection util

---
+++ packages/springboard/test/unit/utils/detectPackageManager.spec.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+import path from 'path'
+import fs from 'fs'
+import { getPackageManager, clearCache } from '../../../src/utils/detectPackageManager'
+
+describe('detectPackageManager', () => {
+  beforeEach(() => {
+    clearCache()
+    try {
+      fs.unlinkSync(path.resolve('.', 'yarn.lock'))
+    /* eslint-disable no-empty */
+    } catch {}
+
+    try {
+      fs.unlinkSync(path.resolve('.', 'package-lock.json'))
+    /* eslint-disable no-empty */
+    } catch {}
+  })
+
+  it('returns yarn when yarn.lock is present', async () => {
+    fs.writeFileSync(path.resolve('.', 'yarn.lock'), '')
+    const res = await getPackageManager()
+
+    expect(res).to.eq('yarn')
+  })
+
+  it('returns npm when package-lock.json file is present', async () => {
+    fs.writeFileSync(path.resolve('.', 'package-lock.json'), '')
+    const res = await getPackageManager({ cwd: '.' })
+
+    expect(res).to.eq('npm')
+  })
+
+  it('uses yarn if globally installed', async () => {
+    // yarn is globally installed since Cypress uses it.
+    const res = await getPackageManager()
+
+    expect(res).to.eq('yarn')
+  })
+})
