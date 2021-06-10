2a15f45d9cffd12e767b2371aa27ea73e0ad7cfe
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-10 14:15:58 +1000

    revert changed test

--- packages/runner/cypress/integration/issues/issue-9162.js
+++ packages/runner/cypress/integration/issues/issue-9162.js
@@ -11,20 +11,5 @@ describe('issue 9162', () => {
 
   it('tests does not hang even if there is a fail in before().', function () {
     cy.contains('expected true to be false')
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('.runnable-err-name > .fas').click()
-    cy.get('.runnable-err-name > .fas').click()
-    cy.get('.command-pin').click()
-    cy.get('.runnable-failed > :nth-child(1) > .runnable-wrapper > .collapsible-header > .collapsible-header-inner > .collapsible-header-text > .runnable-controls > :nth-child(1) > .fas').click()
-    /* ==== End Cypress Studio ==== */
-  })
-})
-
-describe('studio', () => {
-  it('create new transaction', () => {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('https://google.com')
-    cy.get('[href="https://about.google/?fg=1&utm_source=google-AU&utm_medium=referral&utm_campaign=hp-header"]').click()
-    /* ==== End Cypress Studio ==== */
   })
 })
