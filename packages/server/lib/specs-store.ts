389a7a3d4af5283d3f29d516b1e2944222d16d46
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-16 15:34:31 +1000

    do not use private property

--- packages/server/lib/specs-store.ts
+++ packages/server/lib/specs-store.ts
@@ -26,27 +26,27 @@ export class SpecsStore {
     cypressConfig: Record<string, any>,
     private runner: RunnerType,
   ) {
-    this.#cypressConfig = cypressConfig
+    this.cypressConfig = cypressConfig
   }
 
   get specDirectory () {
     if (this.runner === 'e2e') {
-      return this.#cypressConfig.resolved.integrationFolder.value
+      return this.cypressConfig.resolved.integrationFolder.value
     }
 
     if (this.runner === 'ct') {
-      return this.#cypressConfig.resolved.componentFolder.value
+      return this.cypressConfig.resolved.componentFolder.value
     }
   }
 
   get testFiles () {
-    return this.#cypressConfig.resolved.testFiles.value
+    return this.cypressConfig.resolved.testFiles.value
   }
 
   get watchOptions (): chokidar.WatchOptions {
     return {
       cwd: this.specDirectory,
-      ignored: this.#cypressConfig.ignoreTestFiles,
+      ignored: this.cypressConfig.ignoreTestFiles,
       ignoreInitial: true,
     }
   }
@@ -59,7 +59,7 @@ export class SpecsStore {
   }
 
   getSpecFiles (): Bluebird<SpecFiles> {
-    const searchOptions = _.pick(this.#cypressConfig, COMMON_SEARCH_OPTIONS)
+    const searchOptions = _.pick(this.cypressConfig, COMMON_SEARCH_OPTIONS)
 
     searchOptions.searchFolder = this.specDirectory
     searchOptions.testFiles = this.testFiles
@@ -68,7 +68,7 @@ export class SpecsStore {
   }
 
   watch (options?: SpecsWatcherOptions) {
-    this.watcher = chokidar.watch(this.#cypressConfig.testFiles, this.watchOptions)
+    this.watcher = chokidar.watch(this.cypressConfig.testFiles, this.watchOptions)
 
     if (options?.onSpecsChanged) {
       const onSpecsChanged = debounce(async () => {
