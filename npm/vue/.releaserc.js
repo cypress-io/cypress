e327426c34db4af0efe28cb2a7d9ecd4d0d7ddae
ElevateBart <ledouxb@gmail.com>
2021-06-17 17:31:00 -0500

    fix: make sure we always deliver 2 on npm/vue/v2

--- npm/vue/.releaserc.js
+++ npm/vue/.releaserc.js
@@ -1,7 +1,12 @@
 module.exports = {
   ...require('../../.releaserc.base'),
   branches: [
-    'npm/vue/v2',
+    // we need to keep this branch in here even if no used because semantic-release demands
+    // that we have at least one branch that has no config
+    'next/npm/vue',
+    // this line forces releasing 2.X releases on the latest channel
+    { name: 'npm/vue/v2', range: '2.X.X' },
+    // this one releases v3 on master as beta on the next channel
     { name: 'master', channel: 'next', prerelease: 'beta' },
   ],
 }
