8e454f725ebb4dc3671685c032b8c0bd794b607f
Tim Griesser <tgriesser10@gmail.com>
2021-06-21 19:25:16 -0400

    refactor: remove unused findSpecs code

--- packages/server/lib/project-base.ts
+++ packages/server/lib/project-base.ts
@@ -4,10 +4,8 @@ import Bluebird from 'bluebird'
 import check from 'check-more-types'
 import Debug from 'debug'
 import EE from 'events'
-import la from 'lazy-ass'
 import _ from 'lodash'
 import path from 'path'
-import R from 'ramda'
 
 import commitInfo from '@cypress/commit-info'
 import pkg from '@packages/root'
@@ -32,7 +30,6 @@ import { escapeFilenameInUrl } from './util/escape_filename'
 import { fs } from './util/fs'
 import keys from './util/keys'
 import settings from './util/settings'
-import specsUtil from './util/specs'
 import Watchers from './watchers'
 
 interface CloseOptions {
@@ -920,30 +917,5 @@ export class ProjectBase<TServer extends ServerE2E | ServerCt> extends EE {
         })
       })
     })
-  }
-
-  // Given a path to the project, finds all specs
-  // returns list of specs with respect to the project root
-  static findSpecs (projectRoot, specPattern) {
-    debug('finding specs for project %s', projectRoot)
-    la(check.unemptyString(projectRoot), 'missing project path', projectRoot)
-    la(check.maybe.unemptyString(specPattern), 'invalid spec pattern', specPattern)
-
-    // if we have a spec pattern
-    if (specPattern) {
-      // then normalize to create an absolute
-      // file path from projectRoot
-      // ie: **/* turns into /Users/bmann/dev/project/**/*
-      specPattern = path.resolve(projectRoot, specPattern)
-      debug('full spec pattern "%s"', specPattern)
-    }
-
-    return new ProjectBase(projectRoot)
-    .getConfig()
-    // TODO: handle wild card pattern or spec filename
-    .then((cfg) => {
-      return specsUtil.find(cfg, specPattern)
-    }).then(R.prop('integration'))
-    .then(R.map(R.prop('name')))
   }
 }
