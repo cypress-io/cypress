9e13cc89c83210bbdbc9f4d29aea0beb493ac263
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-25 14:51:44 +1000

    collapse project-ct and project-e2e into project

--- packages/server-ct/src/project-ct.ts
+++ packages/server-ct/src/project-ct.ts
@@ -1,87 +1,10 @@
-import Debug from 'debug'
-import { Cfg, ProjectBase } from '@packages/server/lib/project-base'
+import { ProjectBase } from '@packages/server/lib/project-base'
 import { ServerCt } from './server-ct'
-import { SocketCt } from './socket-ct'
-import { createRoutes } from './routes-ct'
 
 export * from '@packages/server/lib/project-base'
-
-const debug = Debug('cypress:server-ct:project')
 
 export class ProjectCt extends ProjectBase<ServerCt> {
   get projectType (): 'ct' {
     return 'ct'
-  }
-
-  /**
-   * override default viewport for component testing
-   * 1. if user specified viewport values in their cypress.json, use those.
-   * 2. otherwise, use 500/500 by default.
-   */
-  addComponentTestingUniqueDefaults (cfg: Record<string, unknown>) {
-    const rawJson = cfg.rawJson as Cfg
-
-    return {
-      ...cfg,
-      viewportHeight: rawJson.viewportHeight ?? 500,
-      viewportWidth: rawJson.viewportWidth ?? 500,
-    }
-  }
-
-  changeToUrl (targetUrl: string) {
-    this.server.socket.changeToUrl(targetUrl)
-  }
-
-  open (options: Record<string, unknown>) {
-    this._server = new ServerCt()
-
-    return super.open(options, {
-      onOpen: (cfg) => {
-        const cfgForComponentTesting = this.addComponentTestingUniqueDefaults(cfg)
-
-        return this._initPlugins(cfgForComponentTesting, options)
-        .then(({ cfg, specsStore, startSpecWatcher }) => {
-          return this.server.open(cfg, {
-            project: this,
-            onError: options.onError,
-            onWarning: options.onWarning,
-            shouldCorrelatePreRequests: this.shouldCorrelatePreRequests,
-            projectType: 'e2e',
-            SocketCtor: SocketCt,
-            createRoutes,
-            specsStore,
-          })
-          .then(([port, warning]) => {
-            return {
-              cfg,
-              port,
-              warning,
-              specsStore,
-              startSpecWatcher,
-            }
-          })
-        })
-      },
-
-      onAfterOpen ({ cfg }) {
-        cfg.proxyServer = cfg.proxyUrl
-
-        return cfg
-      },
-    })
-  }
-
-  _onError<Options extends Record<string, any>> (err: Error, options: Options) {
-    debug('plugins failed with error', err)
-
-    options.onError(err)
-  }
-
-  _initPlugins (cfg, options) {
-    // only init plugins with the
-    // allowed config values to
-    // prevent tampering with the
-    // internals and breaking cypress
-    return super._initPlugins(cfg, options)
   }
 }
--- packages/server/lib/project-e2e.ts
+++ packages/server/lib/project-e2e.ts
@@ -1,72 +1,8 @@
-import Debug from 'debug'
-import browsers from './browsers'
-import preprocessor from './plugins/preprocessor'
 import { ProjectBase } from './project-base'
 import { ServerE2E } from './server-e2e'
-import { SocketE2E } from './socket-e2e'
-import { createRoutes } from './routes'
-
-const debug = Debug('cypress:server:project')
 
 export class ProjectE2E extends ProjectBase<ServerE2E> {
   get projectType (): 'e2e' {
     return 'e2e'
-  }
-
-  open (options: Record<string, unknown>) {
-    this._server = new ServerE2E()
-
-    return super.open(options, {
-      onOpen: (cfg) => {
-        return this._initPlugins(cfg, options)
-        .then(({ cfg, specsStore, startSpecWatcher }) => {
-          return this.server.open(cfg, {
-            project: this,
-            onError: options.onError,
-            onWarning: options.onWarning,
-            shouldCorrelatePreRequests: this.shouldCorrelatePreRequests,
-            projectType: 'e2e',
-            SocketCtor: SocketE2E,
-            createRoutes,
-            specsStore,
-          })
-          .then(([port, warning]) => {
-            return {
-              cfg,
-              port,
-              warning,
-              specsStore,
-              startSpecWatcher,
-            }
-          })
-        })
-      },
-
-      onAfterOpen ({ cfg }) {
-        cfg.proxyServer = cfg.proxyUrl
-
-        return cfg
-      },
-    })
-  }
-
-  _onError<Options extends Record<string, any>> (err: Error, options: Options) {
-    debug('got plugins error', err.stack)
-
-    browsers.close()
-
-    options.onError(err)
-  }
-
-  _initPlugins (cfg, options) {
-    return super._initPlugins(cfg, options)
-  }
-
-  close () {
-    return super.close({
-      onClose () {
-        preprocessor.close()
-      },
-    })
   }
 }
