97e313194fbbe924d791df6f81e31d6bbeccd4b2
ElevateBart <ledouxb@gmail.com>
2021-06-29 14:53:14 -0500

    fix comment on cache helper

--- packages/server/test/cache_helper.js
+++ packages/server/test/cache_helper.js
@@ -4,10 +4,10 @@ const supportedConfigFiles = [
 ]
 
 /**
-   * Since we load the cypress.json, cypress.e2e.config.js or cypress.component.config.js
-   * via `require`, we need to clear the `require.cache` before/after some tests
-   * to ensure we are not using a cached configuration file.
-   */
+ * Since we load the cypress.json via `require`,
+ * we need to clear the `require.cache` before/after some tests
+ * to ensure we are not using a cached configuration file.
+ */
 const clearCypressJsonCache = () => {
   Object.keys(require.cache).forEach((key) => {
     if (supportedConfigFiles.some((file) => key.includes(file))) {
