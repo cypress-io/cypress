682ed9fe02de7db9e5a17c33089ffcaa9c1df942
ElevateBart <ledouxb@gmail.com>
2021-07-05 09:18:39 -0500

    remove commands comments

--- packages/launchpad/cypress/support/commands.ts
+++ packages/launchpad/cypress/support/commands.ts
@@ -1,29 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 import { mount } from '@cypress/vue'
 import { VClickOutside } from '../../src/plugins/ClickOutside'
 import { createStoreApp, StoreApp } from '../../src/store/app'
