2e83daa2dad5aa3b170fabd1c6fd1f78a5161998
Jessica Sachs <jess@jessicasachs.io>
2021-07-08 01:58:45 -0400

    Styling commands more

--- packages/reporter-vue/src/hooks/Hook.vue
+++ packages/reporter-vue/src/hooks/Hook.vue
@@ -2,18 +2,22 @@
   <div v-if="hook.commands.length">
     <span>{{hook.hookName}} {{ parens }}</span>
     <div class="grid">
-      <div v-for="command in hook.commands" :key="command.id">
+      <Command v-for="command, idx in hook.commands" :command="command" :key="command.id" :idx="idx">
         <strong>{{ command.name }}</strong> {{ command.message }}
-      </div>
+      </Command>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, ref, PropType, } from 'vue'
+import Command from '../commands/Command.vue'
 
 export default defineComponent({
   props: ['hook', 'count', 'idx'],
+  components: {
+    Command,
+  },
   setup(props) {
     return {
       parens: computed(() => {
