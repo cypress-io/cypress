6b0d280e3a626995dd2361d607d096d758b5fab7
Zach Panzarino <zachary@panzarino.com>
2021-07-08 21:12:08 -0400

    Add assertions to e2e test

--- packages/server/__snapshots__/3_studio_spec.ts.js
+++ packages/server/__snapshots__/3_studio_spec.ts.js
@@ -80,11 +80,24 @@ describe('extends test', () => {
 
     cy.get('.link', { log: false }).click({ log: false })
     cy.get('.input-text', { log: false }).type('testing', { log: false })
+    cy.get('.input-radio', { log: false }).rightclick({ log: false })
+    cy.get('.__cypress-studio-assertions-menu', { log: false }).shadow({ log: false }).contains('not.be.checked', { log: false }).click({ log: false })
     cy.get('.input-radio', { log: false }).click({ log: false })
+    cy.get('.input-radio', { log: false }).rightclick({ log: false })
+    cy.get('.__cypress-studio-assertions-menu', { log: false }).shadow({ log: false }).contains('be.checked', { log: false }).click({ log: false })
     cy.get('.input-checkbox', { log: false }).click({ log: false })
     cy.get('.input-checkbox', { log: false }).click({ log: false })
     cy.get('.select', { log: false }).select('1', { log: false })
     cy.get('.multiple', { log: false }).select(['0', '2'], { log: false })
+    cy.get('.link', { log: false }).rightclick({ log: false })
+    cy.get('.__cypress-studio-assertions-menu', { log: false })
+    .shadow({ log: false })
+    .contains('have.class', { log: false })
+    .parents('.assertion-type', { log: false })
+    .trigger('mouseover', { log: false })
+    .find('.assertion-options', { log: false })
+    .contains('link', { log: false })
+    .click({ log: false })
 
     verifyCommandLog(1, {
       selector: '.link',
@@ -104,29 +117,47 @@ describe('extends test', () => {
 
     verifyCommandLog(4, {
       selector: '.input-radio',
-      name: 'check',
+      name: 'assert',
+      message: 'expect <input.input-radio> to not be checked',
     })
 
     verifyCommandLog(5, {
-      selector: '.input-checkbox',
+      selector: '.input-radio',
       name: 'check',
     })
 
     verifyCommandLog(6, {
+      selector: '.input-radio',
+      name: 'assert',
+      message: 'expect <input.input-radio> to be checked',
+    })
+
+    verifyCommandLog(7, {
+      selector: '.input-checkbox',
+      name: 'check',
+    })
+
+    verifyCommandLog(8, {
       selector: '.input-checkbox',
       name: 'uncheck',
     })
 
-    verifyCommandLog(7, {
+    verifyCommandLog(9, {
       selector: '.select',
       name: 'select',
       message: '1',
     })
 
-    verifyCommandLog(8, {
+    verifyCommandLog(10, {
       selector: '.multiple',
       name: 'select',
       message: '[0, 2]',
+    })
+
+    verifyCommandLog(11, {
+      selector: '.link',
+      name: 'assert',
+      message: 'expect <a.link> to have class link',
     })
 
     saveStudio()
@@ -134,11 +165,14 @@ describe('extends test', () => {
     cy.get('.link').click();
     cy.get('.input-text').clear();
     cy.get('.input-text').type('testing');
+    cy.get('.input-radio').should('not.be.checked');
     cy.get('.input-radio').check();
+    cy.get('.input-radio').should('be.checked');
     cy.get('.input-checkbox').check();
     cy.get('.input-checkbox').uncheck();
     cy.get('.select').select('1');
     cy.get('.multiple').select(['0', '2']);
+    cy.get('.link').should('have.class', 'link');
     /* ==== End Cypress Studio ==== */
   })
 })
