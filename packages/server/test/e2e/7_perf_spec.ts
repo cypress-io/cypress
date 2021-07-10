9aa2d46ecff21266d9e923c714d0c99cfb937b67
Tim Griesser <tgriesser10@gmail.com>
2021-07-09 21:27:35 -0400

    Undo perf spec

--- packages/server/test/e2e/7_perf_spec.ts
+++
@@ -1,9 +0,0 @@
-import e2e from '../support/helpers/e2e'
-
-describe('performance test', () => {
-  e2e.it('perf check', {
-    spec: 'perf_spec.js',
-    timeout: 60000,
-    headed: true,
-  })
-})
