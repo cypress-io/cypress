1c053b7bc1577f2e160d1198e7313c8a1eaef3e3
ElevateBart <ledouxb@gmail.com>
2021-07-17 09:53:29 -0500

    add some output to childprocess

--- packages/server/lib/util/require_async.ts
+++ packages/server/lib/util/require_async.ts
@@ -8,7 +8,7 @@ import Debug from 'debug'
 
 const debug = Debug('cypress:server:require_async')
 
-let configProcess: cp.ChildProcess
+let requireProcess: cp.ChildProcess | null
 
 interface RequireAsyncOptions{
     projectRoot: string
@@ -24,40 +24,51 @@ interface ChildOptions{
     execArgv?: string[]
 }
 
+const killChildProcess = () => {
+  requireProcess && requireProcess.kill()
+  requireProcess = null
+}
+
 export default async function requireAsync (filePath: string, options: RequireAsyncOptions): Promise<any> {
-  if (configProcess) {
-    debug('kill existing config process')
-    configProcess.kill()
-  }
-
-  const childOptions: ChildOptions = {
-    stdio: 'inherit',
-  }
-
-  if (inspector.url()) {
-    childOptions.execArgv = _.chain(process.execArgv.slice(0))
-    .remove('--inspect-brk')
-    .push(`--inspect=${process.debugPort + 1}`)
-    .value()
-  }
-
-  const childArguments = ['--projectRoot', options.projectRoot, '--file', filePath, '--loadErrorCode', options.loadErrorCode]
-
-  debug('fork child process', path.join(__dirname, 'require_async_child.js'), childArguments, childOptions)
-  configProcess = cp.fork(path.join(__dirname, 'require_async_child.js'), childArguments, childOptions)
-  const ipc = util.wrapIpc(configProcess)
-
   return new Promise((resolve, reject) => {
+    if (requireProcess) {
+      debug('kill existing config process')
+      killChildProcess()
+    }
+
+    const childOptions: ChildOptions = {
+      stdio: 'inherit',
+    }
+
+    if (inspector.url()) {
+      childOptions.execArgv = _.chain(process.execArgv.slice(0))
+      .remove('--inspect-brk')
+      .push(`--inspect=${process.debugPort + 1}`)
+      .value()
+    }
+
+    const childArguments = ['--projectRoot', options.projectRoot, '--file', filePath, '--loadErrorCode', options.loadErrorCode]
+
+    debug('fork child process', path.join(__dirname, 'require_async_child.js'), childArguments, childOptions)
+    requireProcess = cp.fork(path.join(__dirname, 'require_async_child.js'), childArguments, childOptions)
+    const ipc = util.wrapIpc(requireProcess)
+
+    if (requireProcess.stdout && requireProcess.stderr) {
+      // manually pipe plugin stdout and stderr for dashboard capture
+      // @see https://github.com/cypress-io/cypress/issues/7434
+      requireProcess.stdout.on('data', (data) => process.stdout.write(data))
+      requireProcess.stderr.on('data', (data) => process.stderr.write(data))
+    }
+
     ipc.on('loaded', ({ result, functionNames }) => {
       debug('resolving with result %o', result)
       debug('resolving with functions %o', functionNames)
-      configProcess.kill()
       resolve({ result, functionNames })
     })
 
     ipc.on('load:error', (type, ...args) => {
       debug('load:error %s, rejecting', type)
-      configProcess.kill()
+      killChildProcess()
 
       reject(errors.get(type, ...args))
     })
