99518d2036ef69ac61a3cbe078d64a9b8b68dbf4
ElevateBart <ledouxb@gmail.com>
2021-07-17 22:47:09 -0500

    protect project base against configFile:false

--- packages/server/lib/project-base.ts
+++ packages/server/lib/project-base.ts
@@ -402,7 +402,9 @@ export class ProjectBase<TServer extends ServerE2E | ServerCt> extends EE {
     // desktop-gui receives the proper config file, even if it is one of the defaults
     // we can't know in advance which one json, js or ts file will be present
     // but we need it to be forwarded to the gui for display and help
-    finalConfig.configFile = path.relative(this.projectRoot, finalConfig.configFile ?? configFile)
+    if (configFile !== false) {
+      finalConfig.configFile = path.relative(this.projectRoot, finalConfig.configFile ?? configFile)
+    }
 
     return finalConfig
   }
