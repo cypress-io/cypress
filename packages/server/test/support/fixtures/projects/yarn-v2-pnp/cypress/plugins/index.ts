7333a7bb8762442e430138b119cf93e7e5b6b9a0
Michael Luo <mluo@splunk.com>
2021-07-21 11:58:23 -0700

    add pnp-webpack-plugin plugin to our default wp config

--- packages/server/test/support/fixtures/projects/yarn-v2-pnp/cypress/plugins/index.ts
+++ packages/server/test/support/fixtures/projects/yarn-v2-pnp/cypress/plugins/index.ts
@@ -1,38 +1,9 @@
-const wp = require('@cypress/webpack-preprocessor')
-const PnpWebpackPlugin = require('pnp-webpack-plugin')
+import * as head from 'lodash/head'
 
-const options = {
-  webpackOptions: {
-    module: {
-      rules: [
-        {
-          test: /\.(j|t)sx?$/,
-          use: [{
-            loader: require.resolve('babel-loader'),
-            options: {
-              presets: [require.resolve('@babel/preset-env'), require.resolve('@babel/preset-typescript')],
-            },
-          }],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.ts'],
-      plugins: [
-        PnpWebpackPlugin,
-      ],
-    },
-    resolveLoader: {
-      plugins: [
-        PnpWebpackPlugin.moduleLoader(module),
-      ],
-    },
-
-  },
-}
-
+// Default Cypress plugin function
 export default (on, config) => {
-  on('file:preprocessor', wp(options))
+  // make sure plugin can access dependencies
+  head([1, 2, 3])
 
   return config
 }
