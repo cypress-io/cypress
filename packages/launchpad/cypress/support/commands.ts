f7b33d0c5d3e86ac01cbb26aa1b8a5191739d722
ElevateBart <ledouxb@gmail.com>
2021-07-24 15:18:10 -0500

    test: make launchpad tests work

--- packages/launchpad/cypress/support/commands.ts
+++ packages/launchpad/cypress/support/commands.ts
@@ -1,5 +1,8 @@
 import { mount } from '@cypress/vue'
+import { provideApolloClient } from '@vue/apollo-composable'
+import './setupWindowVars'
 import { createStoreApp, StoreApp } from '../../src/store/app'
+import { apolloClient } from '../../src/graphql/apolloClient'
 import { createStoreConfig, StoreConfig } from '../../src/store/config'
 
 Cypress.Commands.add(
@@ -17,6 +20,9 @@ Cypress.Commands.add(
     options.global.plugins = options.global.plugins || []
     options.global.plugins.push(storeApp)
     options.global.plugins.push(storeConfig)
+    options.global.plugins.push({ install (app) {
+      provideApolloClient(apolloClient)
+    } })
 
     return mount(comp, options)
   },
---
+++ packages/launchpad/cypress/support/setupWindowVars.ts
@@ -0,0 +1,4 @@
+(window as any).ipc = {
+  on: () => {},
+  send: () => {},
+}
