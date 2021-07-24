94446050d0e93703f4ae7990374a436407726d31
ElevateBart <ledouxb@gmail.com>
2021-07-23 21:12:05 -0500

    test adjust e2e test for function

--- packages/server/test/support/fixtures/projects/plugin-config-js/cypress.config.js
+++ packages/server/test/support/fixtures/projects/plugin-config-js/cypress.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  e2e: (on, config) => {
-    return new Promise((resolve) => {
-      setTimeout(resolve, 100)
-    })
-    .then(() => {
-      config.defaultCommandTimeout = 500
-      config.videoCompression = 20
-      config.env = config.env || {}
-      config.env.foo = 'bar'
+  e2e: {
+    plugins (on, config) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, 100)
+      })
+      .then(() => {
+        config.defaultCommandTimeout = 500
+        config.videoCompression = 20
+        config.env = config.env || {}
+        config.env.foo = 'bar'
 
-      return config
-    })
+        return config
+      })
+    },
   },
 }
