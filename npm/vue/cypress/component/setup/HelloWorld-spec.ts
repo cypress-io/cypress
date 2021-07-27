8184667dd351f026c3c49605aa94f16bc9b35057
ElevateBart <ledouxb@gmail.com>
2021-07-27 16:52:21 -0500

    chore: add setup example to cypress vue

---
+++ npm/vue/cypress/component/setup/HelloWorld-spec.ts
@@ -0,0 +1,22 @@
+import { mount } from '@cypress/vue'
+import HelloWorld from './HelloWorld.vue'
+
+describe('HelloWorld', () => {
+  it('playground', () => {
+    mount(HelloWorld, { props: { msg: 'Hello Cypress' } })
+  })
+
+  it('renders properly', () => {
+    mount(HelloWorld, { props: { msg: 'Hello Cypress' } })
+    cy.get('h1').should('contain', 'Hello Cypress')
+  })
+
+  it('adds 1 when clicking the plus button', () => {
+    mount(HelloWorld, { props: { msg: 'Hello Cypress' } })
+
+    cy.get('button')
+    .should('contain', '0')
+    .click()
+    .should('contain', '1')
+  })
+})
