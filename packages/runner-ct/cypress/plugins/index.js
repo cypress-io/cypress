7074ab37435a9ef793de5f6224167a732fec82b4
Jessica Sachs <jess@jessicasachs.io>
2021-07-26 20:15:13 -0400

    fixing plugins after modifying loader

--- packages/runner-ct/cypress/plugins/index.js
+++ packages/runner-ct/cypress/plugins/index.js
@@ -4,15 +4,16 @@ const { startDevServer } = require('@cypress/webpack-dev-server')
 
 function injectStylesInlineForPercyInPlace (webpackConfig) {
   webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
-    if (rule?.use[0]?.loader.includes('mini-css-extract-plugin')) {
-      return {
-        ...rule,
-        use: [{
-          loader: 'style-loader',
-        }],
+    if (rule && rule.use && rule[0] && rule[0].loader) {
+      if (rule.use[0].loader.includes('mini-css-extract-plugin')) {
+        return {
+          ...rule,
+          use: [{
+            loader: 'style-loader',
+          }],
+        }
       }
     }
-
     return rule
   })
 }
