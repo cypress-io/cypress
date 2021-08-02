d089776d12e93130b40c5bfce5d1f5ebbaee6c7d
Jessica Sachs <jess@jessicasachs.io>
2021-08-02 03:54:55 -0400

    Adding additional comments

--- packages/launchpad/src/composables/index.ts
+++ packages/launchpad/src/composables/index.ts
@@ -1,5 +1,11 @@
 import { computed } from 'vue'
-export function useModelWrapper(props, emit, name = 'modelValue') { 
+
+/**
+ * This snippet comes from Thorsten Lünborg and is explained in this blog post https://www.vuemastery.com/blog/vue-3-data-down-events-up/
+ * @example const localValue = useModelWrapper(props, emit, 'modelValue')
+ * @param {string} [name='modelValue'] - The name of the model property to use. modelValue is the default.
+ */
+export function useModelWrapper(props, emit, name = 'modelValue') {
   return computed({ 
     get: () => props[name], 
     set: (value) => emit(`update:${name}`, value) 
