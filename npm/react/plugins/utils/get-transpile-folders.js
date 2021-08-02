77b21f337426759030aec244db93d0c32f4fb3fe
ElevateBart <ledouxb@gmail.com>
2021-08-02 16:00:22 -0500

    test: weird webpack bug

--- npm/react/plugins/utils/get-transpile-folders.js
+++ npm/react/plugins/utils/get-transpile-folders.js
@@ -7,15 +7,15 @@ function getTranspileFolders (config) {
 
   // user can disable folders, so check first
   if (config.componentFolder) {
-    folders.push(config.componentFolder)
+    folders.push(path.resolve(config.projectRoot, config.componentFolder))
   }
 
   if (config.fixturesFolder) {
-    folders.push(config.fixturesFolder)
+    folders.push(path.resolve(config.projectRoot, config.fixturesFolder))
   }
 
   if (config.supportFolder) {
-    folders.push(config.supportFolder)
+    folders.push(path.resolve(config.projectRoot, config.supportFolder))
   }
 
   return folders
