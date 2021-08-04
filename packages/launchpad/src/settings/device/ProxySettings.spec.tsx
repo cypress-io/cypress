5867b2375d57bb9111503fba989d6b603a0103b1
Jessica Sachs <jess@jessicasachs.io>
2021-08-04 05:12:19 -0400

    chore: adding tests for the proxy

--- packages/launchpad/src/settings/device/ProxySettings.spec.tsx
+++ packages/launchpad/src/settings/device/ProxySettings.spec.tsx
@@ -2,6 +2,11 @@ import ProxySettings from './ProxySettings.vue'
 
 describe('<ProxySettings />', () => {
   it('renders', () => {
-    cy.viewport(400, 400).mount(() => <ProxySettings />)
+    cy.viewport(400, 400)
+    .mount(() => <ProxySettings />).get('body')
+    .findByText('Proxy Server').get('body')
+    .findByText('Proxy Bypass List')
+    .get('[data-testid=bypass-list]').should('be.visible')
+    .get('[data-testid=proxy-server]').should('be.visible')
   })
 })
--- packages/launchpad/src/settings/device/ProxySettings.vue
+++ packages/launchpad/src/settings/device/ProxySettings.vue
@@ -5,11 +5,11 @@
     <div class="bg-gray-50 border-1 rounded py-2 px-4 grid gap-1 w-364px">
       <div class="flex justify-between text-sm">
         <span class="font-medium text-gray-800">{{ t('settingsPage.proxy.proxyServer') }}</span>
-        <span class="text-gray-500">{{proxyServer}}</span>
+        <span class="text-gray-500" data-testid="proxy-server">{{proxyServer}}</span>
       </div>
       <div class="flex justify-between text-sm">
         <span class="font-medium text-gray-800">{{ t('settingsPage.proxy.bypassList') }}</span>
-        <span class="text-gray-500">{{bypassList}}</span>
+        <span class="text-gray-500" data-testid="bypass-list">{{bypassList}}</span>
       </div>
     </div>
   </SettingsSection>
