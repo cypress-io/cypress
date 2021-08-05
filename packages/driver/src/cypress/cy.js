171791b0177f711dc8013155679e8eedb6a1593c
Chris Breiding <chrisbreiding@users.noreply.github.com>
2021-08-05 12:57:21 -0400

    chore: Refactor command queue (#17448)

--- packages/driver/src/cypress/cy.js
+++ packages/driver/src/cypress/cy.js
@@ -1,6 +1,5 @@
 /* eslint-disable prefer-rest-params */
 const _ = require('lodash')
-const $ = require('jquery')
 const Promise = require('bluebird')
 const debugErrors = require('debug')('cypress:driver:errors')
 
@@ -27,19 +26,12 @@ const $Retries = require('../cy/retries')
 const $Stability = require('../cy/stability')
 const $selection = require('../dom/selection')
 const $Snapshots = require('../cy/snapshots')
+const $Command = require('./command')
 const $CommandQueue = require('./command_queue')
 const $VideoRecorder = require('../cy/video-recorder')
 const $TestConfigOverrides = require('../cy/testConfigOverrides')
 
 const { registerFetch } = require('unfetch')
-
-const noArgsAreAFunction = (args) => {
-  return !_.some(args, _.isFunction)
-}
-
-const isPromiseLike = (ret) => {
-  return ret && _.isFunction(ret.then)
-}
 
 const returnedFalse = (result) => {
   return result === false
@@ -121,60 +113,15 @@ const setTopOnError = function (Cypress, cy) {
   top.__alreadySetErrorHandlers__ = true
 }
 
-const commandRunningFailed = (Cypress, state, err) => {
-  // allow for our own custom onFail function
-  if (err.onFail) {
-    err.onFail(err)
-
-    // clean up this onFail callback after it's been called
-    delete err.onFail
-
-    return
-  }
-
-  const current = state('current')
-
-  return Cypress.log({
-    end: true,
-    snapshot: true,
-    error: err,
-    consoleProps () {
-      if (!current) return
-
-      const obj = {}
-      const prev = current.get('prev')
-
-      // if type isnt parent then we know its dual or child
-      // and we can add Applied To if there is a prev command
-      // and it is a parent
-      if (current.get('type') !== 'parent' && prev) {
-        const ret = $dom.isElement(prev.get('subject')) ?
-          $dom.getElements(prev.get('subject'))
-          :
-          prev.get('subject')
-
-        obj['Applied To'] = ret
-
-        return obj
-      }
-    },
-  })
-}
-
 // NOTE: this makes the cy object an instance
 // TODO: refactor the 'create' method below into this class
 class $Cy {}
 
 const create = function (specWindow, Cypress, Cookies, state, config, log) {
   let cy = new $Cy()
-  let stopped = false
   const commandFns = {}
 
   state('specWindow', specWindow)
-
-  const isStopped = () => {
-    return stopped
-  }
 
   const onFinishAssertions = function () {
     return assertions.finishAssertions.apply(window, arguments)
@@ -196,11 +143,10 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
     return $dom.query(selector, context)
   }
 
-  const queue = $CommandQueue.create()
-
   $VideoRecorder.create(Cypress)
   const timeouts = $Timeouts.create(state)
   const stability = $Stability.create(Cypress, state)
+
   const retries = $Retries.create(Cypress, state, timeouts.timeout, timeouts.clearTimeout, stability.whenStable, onFinishAssertions)
   const assertions = $Assertions.create(Cypress, cy)
 
@@ -220,6 +166,10 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
 
   const snapshots = $Snapshots.create($$, state)
   const testConfigOverrides = $TestConfigOverrides.create()
+
+  const isStopped = () => {
+    return queue.stopped
+  }
 
   const isCy = (val) => {
     return (val === cy) || $utils.isInstanceOf(val, $Chainer)
@@ -354,7 +304,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
 
   const enqueue = function (obj) {
     // if we have a nestedIndex it means we're processing
-    // nested commands and need to splice them into the
+    // nested commands and need to insert them into the
     // index past the current index as opposed to
     // pushing them to the end we also dont want to
     // reset the run defer because splicing means we're
@@ -365,22 +315,21 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
     // we had a bug that would bomb on custom commands when it was the
     // first command. this was due to nestedIndex being undefined at that
     // time. so we have to ensure to check that its any kind of number (even 0)
-    // in order to know to splice into the existing array.
+    // in order to know to insert it into the existing array.
     let nestedIndex = state('nestedIndex')
 
-    // if this is a number then we know
-    // we're about to splice this into our commands
-    // and need to reset next + increment the index
+    // if this is a number, then we know we're about to insert this
+    // into our commands and need to reset next + increment the index
     if (_.isNumber(nestedIndex)) {
       state('nestedIndex', (nestedIndex += 1))
     }
 
     // we look at whether or not nestedIndex is a number, because if it
-    // is then we need to splice inside of our commands, else just push
+    // is then we need to insert inside of our commands, else just push
     // it onto the end of the queu
     const index = _.isNumber(nestedIndex) ? nestedIndex : queue.length
 
-    queue.splice(index, 0, obj)
+    queue.insert(index, $Command.create(obj))
 
     return Cypress.action('cy:command:enqueued', obj)
   }
@@ -399,298 +348,6 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
     }
 
     return getCommandsUntilFirstParentOrValidSubject(command.get('prev'), memo)
-  }
-
-  const runCommand = function (command) {
-    // bail here prior to creating a new promise
-    // because we could have stopped / canceled
-    // prior to ever making it through our first
-    // command
-    if (stopped) {
-      return
-    }
-
-    state('current', command)
-    state('chainerId', command.get('chainerId'))
-
-    return stability.whenStable(() => {
-      // TODO: handle this event
-      // @trigger "invoke:start", command
-
-      state('nestedIndex', state('index'))
-
-      return command.get('args')
-    })
-
-    .then((args) => {
-      // store this if we enqueue new commands
-      // to check for promise violations
-      let ret
-      let enqueuedCmd = null
-
-      const commandEnqueued = (obj) => {
-        return enqueuedCmd = obj
-      }
-
-      // only check for command enqueing when none
-      // of our args are functions else commands
-      // like cy.then or cy.each would always fail
-      // since they return promises and queue more
-      // new commands
-      if (noArgsAreAFunction(args)) {
-        Cypress.once('command:enqueued', commandEnqueued)
-      }
-
-      // run the command's fn with runnable's context
-      try {
-        ret = __stackReplacementMarker(command.get('fn'), state('ctx'), args)
-      } catch (err) {
-        throw err
-      } finally {
-        // always remove this listener
-        Cypress.removeListener('command:enqueued', commandEnqueued)
-      }
-
-      state('commandIntermediateValue', ret)
-
-      // we cannot pass our cypress instance or our chainer
-      // back into bluebird else it will create a thenable
-      // which is never resolved
-      if (isCy(ret)) {
-        return null
-      }
-
-      if (!(!enqueuedCmd || !isPromiseLike(ret))) {
-        return $errUtils.throwErrByPath(
-          'miscellaneous.command_returned_promise_and_commands', {
-            args: {
-              current: command.get('name'),
-              called: enqueuedCmd.name,
-            },
-          },
-        )
-      }
-
-      if (!(!enqueuedCmd || !!_.isUndefined(ret))) {
-        // TODO: clean this up in the utility function
-        // to conditionally stringify functions
-        ret = _.isFunction(ret) ?
-          ret.toString()
-          :
-          $utils.stringify(ret)
-
-        // if we got a return value and we enqueued
-        // a new command and we didn't return cy
-        // or an undefined value then throw
-        return $errUtils.throwErrByPath(
-          'miscellaneous.returned_value_and_commands_from_custom_command', {
-            args: {
-              current: command.get('name'),
-              returned: ret,
-            },
-          },
-        )
-      }
-
-      return ret
-    }).then((subject) => {
-      state('commandIntermediateValue', undefined)
-
-      // we may be given a regular array here so
-      // we need to re-wrap the array in jquery
-      // if that's the case if the first item
-      // in this subject is a jquery element.
-      // we want to do this because in 3.1.2 there
-      // was a regression when wrapping an array of elements
-      const firstSubject = $utils.unwrapFirst(subject)
-
-      // if ret is a DOM element and its not an instance of our own jQuery
-      if (subject && $dom.isElement(firstSubject) && !$utils.isInstanceOf(subject, $)) {
-        // set it back to our own jquery object
-        // to prevent it from being passed downstream
-        // TODO: enable turning this off
-        // wrapSubjectsInJquery: false
-        // which will just pass subjects downstream
-        // without modifying them
-        subject = $dom.wrap(subject)
-      }
-
-      command.set({ subject })
-
-      // end / snapshot our logs
-      // if they need it
-      command.finishLogs()
-
-      // reset the nestedIndex back to null
-      state('nestedIndex', null)
-
-      // also reset recentlyReady back to null
-      state('recentlyReady', null)
-
-      // we're finished with the current command
-      // so set it back to null
-      state('current', null)
-
-      state('subject', subject)
-
-      return subject
-    })
-  }
-
-  const run = function () {
-    const next = function () {
-      // bail if we've been told to abort in case
-      // an old command continues to run after
-      if (stopped) {
-        return
-      }
-
-      // start at 0 index if we dont have one
-      let index = state('index') || state('index', 0)
-
-      const command = queue.at(index)
-
-      // if the command should be skipped
-      // just bail and increment index
-      // and set the subject
-      // TODO DRY THIS LOGIC UP
-      if (command && command.get('skip')) {
-        // must set prev + next since other
-        // operations depend on this state being correct
-        command.set({ prev: queue.at(index - 1), next: queue.at(index + 1) })
-        state('index', index + 1)
-        state('subject', command.get('subject'))
-
-        return next()
-      }
-
-      // if we're at the very end
-      if (!command) {
-        // trigger queue is almost finished
-        Cypress.action('cy:command:queue:before:end')
-
-        // we need to wait after all commands have
-        // finished running if the application under
-        // test is no longer stable because we cannot
-        // move onto the next test until its finished
-        return stability.whenStable(() => {
-          Cypress.action('cy:command:queue:end')
-
-          return null
-        })
-      }
-
-      // store the previous timeout
-      const prevTimeout = timeouts.timeout()
-
-      // store the current runnable
-      const runnable = state('runnable')
-
-      Cypress.action('cy:command:start', command)
-
-      return runCommand(command)
-      .then(() => {
-        // each successful command invocation should
-        // always reset the timeout for the current runnable
-        // unless it already has a state.  if it has a state
-        // and we reset the timeout again, it will always
-        // cause a timeout later no matter what.  by this time
-        // mocha expects the test to be done
-        let fn
-
-        if (!runnable.state) {
-          timeouts.timeout(prevTimeout)
-        }
-
-        // mutate index by incrementing it
-        // this allows us to keep the proper index
-        // in between different hooks like before + beforeEach
-        // else run will be called again and index would start
-        // over at 0
-        state('index', (index += 1))
-
-        Cypress.action('cy:command:end', command)
-
-        fn = state('onPaused')
-
-        if (fn) {
-          return new Promise((resolve) => {
-            return fn(resolve)
-          }).then(next)
-        }
-
-        return next()
-      })
-    }
-
-    let inner = null
-
-    // this ends up being the parent promise wrapper
-    const promise = new Promise((resolve, reject) => {
-      // bubble out the inner promise
-      // we must use a resolve(null) here
-      // so the outer promise is first defined
-      // else this will kick off the 'next' call
-      // too soon and end up running commands prior
-      // to promise being defined
-      inner = Promise
-      .resolve(null)
-      .then(next)
-      .then(resolve)
-      .catch(reject)
-
-      // can't use onCancel argument here because
-      // its called asynchronously
-
-      // when we manually reject our outer promise we
-      // have to immediately cancel the inner one else
-      // it won't be notified and its callbacks will
-      // continue to be invoked
-      // normally we don't have to do this because rejections
-      // come from the inner promise and bubble out to our outer
-      //
-      // but when we manually reject the outer promise we
-      // have to go in the opposite direction from outer -> inner
-      const rejectOuterAndCancelInner = function (err) {
-        inner.cancel()
-
-        return reject(err)
-      }
-
-      state('resolve', resolve)
-      state('reject', rejectOuterAndCancelInner)
-    })
-    .catch((err) => {
-      if (state('onCommandFailed')) {
-        return state('onCommandFailed')(err, queue, next)
-      }
-
-      debugErrors('caught error in promise chain: %o', err)
-
-      // since this failed this means that a
-      // specific command failed and we should
-      // highlight it in red or insert a new command
-      err.name = err.name || 'CypressError'
-      commandRunningFailed(Cypress, state, err)
-
-      return fail(err)
-    })
-    .finally(cleanup)
-
-    // cancel both promises
-    const cancel = function () {
-      promise.cancel()
-      inner.cancel()
-
-      // notify the world
-      return Cypress.action('cy:canceled')
-    }
-
-    state('cancel', cancel)
-    state('promise', promise)
-
-    // return this outer bluebird promise
-    return promise
   }
 
   const removeSubject = () => {
@@ -736,7 +393,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
   }
 
   const doneEarly = function () {
-    stopped = true
+    queue.stop()
 
     // we only need to worry about doneEarly when
     // it comes from a manual event such as stopping
@@ -795,7 +452,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
 
     let rets
 
-    stopped = true
+    queue.stop()
 
     if (typeof err === 'string') {
       err = new Error(err)
@@ -874,6 +531,8 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
     // else figure out how to finish this failure
     return finish(err)
   }
+
+  const queue = $CommandQueue.create(state, timeouts, stability, cleanup, fail, isCy)
 
   _.extend(cy, {
     id: _.uniqueId('cy'),
@@ -1032,7 +691,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
 
     stop () {
       // don't do anything if we've already stopped
-      if (stopped) {
+      if (queue.stopped) {
         return
       }
 
@@ -1040,8 +699,6 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
     },
 
     reset (attrs, test) {
-      stopped = false
-
       const s = state()
 
       const backup = {
@@ -1059,6 +716,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
       state(backup)
 
       queue.reset()
+      queue.clear()
       timers.reset()
       testConfigOverrides.restoreAndSetTestConfigOverrides(test, Cypress.config, Cypress.env)
 
@@ -1130,7 +788,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
           const current = state('current')
 
           // if this is a custom promise
-          if (isPromiseLike(ret) && noArgsAreAFunction(current.get('args'))) {
+          if ($utils.isPromiseLike(ret) && $utils.noArgsAreAFunction(current.get('args'))) {
             $errUtils.throwErrByPath(
               'miscellaneous.command_returned_promise_and_commands', {
                 args: {
@@ -1149,7 +807,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
             warnMixingPromisesAndCommands()
           }
 
-          run()
+          queue.run()
         }
 
         return chain
@@ -1326,7 +984,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
     setRunnable (runnable, hookId) {
       // when we're setting a new runnable
       // prepare to run again!
-      stopped = false
+      queue.reset()
 
       // reset the promise again
       state('promise', undefined)
@@ -1397,7 +1055,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
           if (ret &&
             (queue.length > currentLength) &&
               (!isCy(ret)) &&
-                (!isPromiseLike(ret))) {
+                (!$utils.isPromiseLike(ret))) {
             // TODO: clean this up in the utility function
             // to conditionally stringify functions
             ret = _.isFunction(ret) ?
@@ -1423,7 +1081,7 @@ const create = function (specWindow, Cypress, Cookies, state, config, log) {
           }
 
           // if we returned a promise like object
-          if ((!isCy(ret)) && isPromiseLike(ret)) {
+          if ((!isCy(ret)) && $utils.isPromiseLike(ret)) {
             // indicate we've returned a custom promise
             state('returnedCustomPromise', true)
 
