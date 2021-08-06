c50fb7d281eb99a9949034a257b167836889f1bc
Jessica Sachs <jess@jessicasachs.io>
2021-08-06 11:24:50 -0400

    feat: add global mode empty

--- packages/launchpad/src/global/GlobalPage.vue
+++ packages/launchpad/src/global/GlobalPage.vue
@@ -1,20 +1,20 @@
 <template>
   <template v-if="projects.length">
     <!-- Welcome Guide can fetch its own information for if it should render -->
-    <WelcomeGuide/>
+    <WelcomeGuide />
 
     <!-- If there are projects -->
     <div class="grid grid-cols-1 gap-6 pt-6 grid-cols-2">
       <div class="min-w-full col-start-1 col-end-3 flex items-center gap-6">
-        <GlobalPageHeader v-model="match"/>
+        <GlobalPageHeader v-model="match" />
       </div>
 
-      <GlobalProjectCard v-for="project, idx in filteredProjects" :key="idx" :project="project"/>
+      <GlobalProjectCard v-for="project, idx in filteredProjects" :key="idx" :project="project" />
     </div>
   </template>
 
   <!-- Else, show the empty state -->
-  <GlobalEmpty v-else/>
+  <GlobalEmpty v-else />
 </template>
 
 <script setup lang="ts">
