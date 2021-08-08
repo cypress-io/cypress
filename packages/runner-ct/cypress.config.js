f81c67cf26446b1ce4c4c60fc9ad039fdabbaadc
ElevateBart <ledouxb@gmail.com>
2021-08-07 23:07:48 -0500

    fix: runner-ct config needs to follow the full migration

--- packages/runner-ct/cypress.config.js
+++ packages/runner-ct/cypress.config.js
@@ -1,7 +1,38 @@
+/// <reference types="cypress" />
+const path = require('path')
+const { startDevServer } = require('@cypress/webpack-dev-server')
+
+function injectStylesInlineForPercyInPlace (webpackConfig) {
+  webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
+    if (rule?.use[0]?.loader.includes('mini-css-extract-plugin')) {
+      return {
+        ...rule,
+        use: [{
+          loader: 'style-loader',
+        }],
+      }
+    }
+
+    return rule
+  })
+}
+
 module.exports = {
   testFiles: '**/*spec.{ts,tsx}',
   video: true,
   env: {
     reactDevtools: true,
+  },
+  component: {
+    setupDevServer (options) {
+      const { default: webpackConfig } = require(path.resolve(__dirname, 'webpack.config.ts'))
+
+      injectStylesInlineForPercyInPlace(webpackConfig)
+
+      return startDevServer({
+        webpackConfig,
+        options,
+      })
+    },
   },
 }
