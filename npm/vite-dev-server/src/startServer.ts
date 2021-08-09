cdb2a5232eeba6383400979feacf2f9d8c4ab405
ElevateBart <ledouxb@gmail.com>
2021-08-09 13:39:00 -0500

    fix: when there are specs but the supportFile is undefined

--- npm/vite-dev-server/src/startServer.ts
+++ npm/vite-dev-server/src/startServer.ts
@@ -59,8 +59,13 @@ const resolveServerConfig = async ({ viteConfig, options }: StartDevServerOption
   // Ask vite to pre-optimize all dependencies of the specs
   finalConfig.optimizeDeps = finalConfig.optimizeDeps || {}
 
-  if ((options.specs && options.specs.length) || supportFile) {
-    finalConfig.optimizeDeps.entries = [...options.specs.map((spec) => spec.relative), supportFile]
+  // pre-optimizea all the specs
+  if ((options.specs && options.specs.length)) {
+    finalConfig.optimizeDeps.entries = [...options.specs.map((spec) => spec.relative)]
+    // only optimize a supportFile is it is not false or undefined
+    if (supportFile) {
+      finalConfig.optimizeDeps.entries.push(supportFile)
+    }
   }
 
   debug(`the resolved server config is ${JSON.stringify(finalConfig, null, 2)}`)
