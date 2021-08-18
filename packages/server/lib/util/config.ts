407c931c1a2f9dd7f86f8cf6169ea7fd4a6c833d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-08-18 14:23:38 +1000

    add config util file

---
+++ packages/server/lib/util/config.ts
@@ -0,0 +1,34 @@
+import _ from 'lodash'
+import {
+  CYPRESS_ENV_PREFIX,
+  CYPRESS_ENV_PREFIX_LENGTH,
+  CYPRESS_RESERVED_ENV_VARS,
+} from '../config'
+import coerce from './coerce'
+
+export const isDefault = (config: Record<string, any>, prop: string) => {
+  return config.resolved[prop].from === 'default'
+}
+
+const isCypressEnvLike = (key) => {
+  return _.chain(key)
+  .invoke('toUpperCase')
+  .startsWith(CYPRESS_ENV_PREFIX)
+  .value() &&
+  !_.includes(CYPRESS_RESERVED_ENV_VARS, key)
+}
+
+const removeEnvPrefix = (key: string) => {
+  return key.slice(CYPRESS_ENV_PREFIX_LENGTH)
+}
+
+export const getProcessEnvVars = (obj: NodeJS.ProcessEnv) => {
+  return _.reduce(obj, (memo, value, key) => {
+    if (isCypressEnvLike(key)) {
+      memo[removeEnvPrefix(key)] = coerce(value)
+    }
+
+    return memo
+  }
+  , {})
+}
