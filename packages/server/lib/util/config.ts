8393708fda71dd5897e20afb80f640a131c4d8ec
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-08-18 14:43:44 +1000

    move function

--- packages/server/lib/util/config.ts
+++ packages/server/lib/util/config.ts
@@ -10,6 +10,17 @@ export const isDefault = (config: Record<string, any>, prop: string) => {
   return config.resolved[prop].from === 'default'
 }
 
+export const getProcessEnvVars = (obj: NodeJS.ProcessEnv) => {
+  return _.reduce(obj, (memo, value, key) => {
+    if (isCypressEnvLike(key)) {
+      memo[removeEnvPrefix(key)] = coerce(value)
+    }
+
+    return memo
+  }
+  , {})
+}
+
 const isCypressEnvLike = (key) => {
   return _.chain(key)
   .invoke('toUpperCase')
@@ -20,15 +31,4 @@ const isCypressEnvLike = (key) => {
 
 const removeEnvPrefix = (key: string) => {
   return key.slice(CYPRESS_ENV_PREFIX_LENGTH)
-}
-
-export const getProcessEnvVars = (obj: NodeJS.ProcessEnv) => {
-  return _.reduce(obj, (memo, value, key) => {
-    if (isCypressEnvLike(key)) {
-      memo[removeEnvPrefix(key)] = coerce(value)
-    }
-
-    return memo
-  }
-  , {})
 }
