51d780b4ed0b6bb4c235d423a6cb7253fdb02558
Ollie Relph <ollie@relph.me>
2021-08-23 15:01:10 +0100

    feat: migrate to new webpack-dev-server public api

--- npm/webpack-dev-server/src/index.ts
+++ npm/webpack-dev-server/src/index.ts
@@ -2,6 +2,7 @@ import { debug as debugFn } from 'debug'
 import { AddressInfo } from 'net'
 import { Server } from 'http'
 import { start as createDevServer, StartDevServer } from './startServer'
+import { webpackDevServerFacts } from './webpackDevServerFacts'
 
 const debug = debugFn('cypress:webpack-dev-server:webpack')
 
@@ -17,26 +18,43 @@ export { StartDevServer }
 export async function startDevServer (startDevServerArgs: StartDevServer, exitProcess = process.exit) {
   const webpackDevServer = await createDevServer(startDevServerArgs, exitProcess)
 
-  return new Promise<ResolvedDevServerConfig>((resolve) => {
-    const httpSvr = webpackDevServer.listen(0, '127.0.0.1', () => {
-      // webpack-dev-server v3 returns `http.Server`.
-      // v4 returns a Promise that resolves `http.Server`.
-      // use Promise.resolve to make sure we get the `http.Server`,
-      // regardless of webpack-dev-server version.
-      Promise.resolve(httpSvr).then((server: Server) => {
+  return new Promise<ResolvedDevServerConfig>(async (resolve, reject) => {
+    if (webpackDevServerFacts.isV3()) {
+      const server: Server = webpackDevServer.listen(0, '127.0.0.1', () => {
         // FIXME: handle address returning a string
         const port = (server.address() as AddressInfo).port
 
         debug('Component testing webpack server started on port', port)
 
-        return resolve({
+        resolve({
           port,
           close: (done?: DoneCallback) => {
-            httpSvr.close()
+            server.close()
             done?.()
           },
         })
       })
-    })
+
+      return
+    }
+
+    if (webpackDevServerFacts.isV4()) {
+      // @ts-expect-error @types do not yet support v4
+      await webpackDevServer.start()
+
+      resolve({
+        // @ts-expect-error @types do not yet support v4
+        port: webpackDevServer.options.port,
+        close: (done?: DoneCallback) => {
+          // @ts-expect-error @types do not yet support v4
+          webpackDevServer.stop()
+          .then(done)
+        },
+      })
+
+      return
+    }
+
+    reject(webpackDevServerFacts.unsupported())
   })
 }
