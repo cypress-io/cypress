7f1a63598c84f65286670021bf48258b8417af4c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-08-24 14:00:00 +1000

    update gql config

--- packages/graphql/src/context/BaseContext.ts
+++ packages/graphql/src/context/BaseContext.ts
@@ -1,8 +1,7 @@
 import type { LaunchArgs } from '@packages/server/lib/open_project'
-import type { OpenProjectLaunchOptions } from '../../../server/lib/project-base'
+import type { OpenProjectLaunchOptions } from '@packages/server/lib/project-base'
 import type { BaseActions } from '../actions/BaseActions'
-import type { LocalProject, Viewer, DashboardProject } from '../entities'
-import { App, Wizard, NavigationMenu } from '../entities'
+import { App, Wizard, NavigationMenu, LocalProject, Viewer, DashboardProject } from '../entities'
 
 /**
  * The "Base Context" is the class type that we will use to encapsulate the server state.
@@ -17,7 +16,7 @@ export abstract class BaseContext {
   abstract dashboardProjects: DashboardProject[]
   abstract viewer: null | Viewer
 
-  constructor(private _launchArgs: LaunchArgs, private _launchOptions: OpenProjectLaunchOptions) {}
+  constructor (private _launchArgs: LaunchArgs, private _launchOptions: OpenProjectLaunchOptions) {}
 
   wizard = new Wizard()
   navigationMenu = new NavigationMenu()
@@ -26,7 +25,7 @@ export abstract class BaseContext {
   get activeProject () {
     return this.app.activeProject
   }
-  
+
   get launchArgs () {
     return this._launchArgs
   }
