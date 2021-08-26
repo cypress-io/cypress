665416338138ecafc9b5e46edf4e3ce4ca6c81d3
KHeo <sainthkh@naver.com>
2021-08-26 10:04:26 +0900

    Add unit test.

---
+++ packages/runner-shared/src/logger.spec.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon')
+const { logger } = require('./logger')
+
+describe('logger', () => {
+  it('cy.log() shows all arguments in each line when there are multiple args', () => {
+    const spy = sinon.spy(logger, 'log')
+
+    logger.logFormatted({ args: [1, 2, 3] })
+
+    expect(spy).to.have.been.calledWith(`%cArgs:`, 'font-weight: bold')
+    expect(spy).to.have.been.calledWith(`%c  [0]:`, 'font-weight: bold', 1)
+    expect(spy).to.have.been.calledWith(`%c  [1]:`, 'font-weight: bold', 2)
+    expect(spy).to.have.been.calledWith(`%c  [2]:`, 'font-weight: bold', 3)
+  })
+})
