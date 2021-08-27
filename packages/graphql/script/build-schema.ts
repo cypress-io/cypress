c12cd70befe76eb65f9bbb0cd3da9cafe457c3c5
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-08-27 15:50:06 +1000

    fix build

--- packages/graphql/script/build-schema.ts
+++ packages/graphql/script/build-schema.ts
@@ -4,5 +4,4 @@ process.env.GRAPHQL_CODEGEN_EXIT = 'true'
 // TODO: we should be upgrading ts-node or moving to Thorsten's work soon
 // and then this can be removed
 process.env.TS_NODE_CACHE = 'false'
-require('@packages/ts/register')
-require('../src/schema')
+import '../src/schema'
