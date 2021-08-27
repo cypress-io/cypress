ff8d937edfc409f682d3e8d457702ced1916cad6
Tim Griesser <tgriesser10@gmail.com>
2021-08-27 15:04:15 -0400

    More fixes

--- packages/graphql/src/constants/browserConstants.ts
+++ packages/graphql/src/constants/browserConstants.ts
@@ -1,5 +1,5 @@
 import { enumType } from 'nexus'
-import { BROWSER_FAMILY } from '@packages/types'
+import { BROWSER_FAMILY } from '@packages/types/src/browser'
 
 export const BrowserFamilyEnum = enumType({
   name: 'BrowserFamily',
