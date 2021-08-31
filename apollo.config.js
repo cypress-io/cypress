98b80be59a49fc0f35bcaa136ed9d3d695e1fccb
Tim Griesser <tgriesser10@gmail.com>
2021-08-31 18:36:18 -0400

    fix: rename testApolloClient -> testUrqlClient, add apollo.config.js (#17948)

---
+++ apollo.config.js
@@ -0,0 +1,12 @@
+// For use with Apollo Extension for VSCode:
+// https://www.apollographql.com/docs/devtools/editor-plugins/
+module.exports = {
+  client: {
+    service: {
+      name: 'cypress-io',
+      localSchemaFile: './packages/graphql/schema.graphql',
+    },
+    tagName: 'gql',
+    includes: ['./packages/launchpad/src/**/*.vue'],
+  },
+}
