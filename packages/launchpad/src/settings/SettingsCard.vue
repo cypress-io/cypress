c59c68330efa97853d77908baa3527beb45ff340
Mark Noonan <markthomasnoonan@gmail.com>
2021-09-09 08:31:49 -0400

    update slot syntax, tweak css, tweak formatting

--- packages/launchpad/src/settings/SettingsCard.vue
+++ packages/launchpad/src/settings/SettingsCard.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="p-4 min-w-650px mx-auto my-0">
-    <Disclosure as="section" class="border-1 border-gray-300 rounded" v-slot="{ open }">
-      <DisclosureButton data-testid="settings-card-header" class="settings-card-header bg-cool-gray-100 py-4 pl-6 pr-3 select-none cursor-pointer grid gap-4">
+    <Disclosure 
+      as="section" 
+      #="{ open }"
+      class="border-1 border-gray-300 rounded">
+      <DisclosureButton 
+        data-testid="settings-card-header" 
+        class="settings-card-header bg-cool-gray-100 py-4 pl-6 pr-3 select-none cursor-pointer grid gap-4">
         <span class="grid self-center h-full">
           <Icon :icon="icon" class="self-center text-indigo-600 text-xl"/>
         </span>
         <div class="w-0.5px bg-gray-300 h-full"/>
         <div>
-          <h2 data-testid="settings-card-title" class="col-start-3 text-xl text-indigo-600">{{ title }}</h2>
-          <p data-testid="settings-card-description" class="text-gray-500 text-sm">{{ description }}</p>
+          <h2 
+            data-testid="settings-card-title" 
+            class="col-start-3 text-xl text-indigo-600">
+            {{ title }}
+          </h2>
+          <p data-testid="settings-card-description" class="text-gray-500 text-sm">
+            {{ description }}
+          </p>
         </div>
-        <Icon :icon="IconCaret" class="transform transition-transform col-start-4 self-center text-xl text-gray-500" :class="{ 'rotate-90': !open, 'rotate-180': open }"/>
+        <Icon 
+          :icon="IconCaret" 
+          class="transform transition-transform col-start-4 self-center text-xl text-gray-500" 
+          :class="{ 'rotate-90': !open, 'rotate-180': open }"/>
       </DisclosureButton>
 
       <!-- Content of the Settings Card -->
