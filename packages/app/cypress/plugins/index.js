3932360fe7f99dced7131e14d76a9d7d2623e92d
Jessica Sachs <jess@jessicasachs.io>
2021-09-09 22:17:45 -0400

    chore: adding the new app package for the unified app experience (#18053)
    * adding the app folder
    
    * frontend app unified as a copy of launchpad
    
    * removing unnecessary files
    
    * fixing build step by removing package codegen
    
    * chore: regenerate schema
    
    * update readme
    
    * add diagram
    
    * add app diagram
    
    * add callback to renderRunner function
    
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>

---
+++ packages/app/cypress/plugins/index.js
@@ -0,0 +1,42 @@
+/**
+ * @type {import('@cypress/vite-dev-server')}
+ */
+const { startDevServer } = require('@cypress/vite-dev-server')
+
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+
+  if (config.testingType === 'component') {
+    on('dev-server:start', async (options) => {
+      return startDevServer({
+        options,
+        viteConfig: {
+          // TODO(tim): Figure out why this isn't being picked up
+          optimizeDeps: {
+            include: ['@headlessui/vue'],
+          },
+        },
+      })
+    })
+  }
+
+  return config // IMPORTANT to return a config
+}
---
+++ packages/app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { renderRunner } from './runner/renderRunner'
+
+const target = ref(null)
+
+onMounted(async () => {
+  const config = await (await fetch('/__/api')).json()
+  renderRunner(() => {
+    // @ts-ignore - yes it does
+    window.Runner.start(target.value, config.base64Config)
+  })
+})
+</script>
+
+<template>
+  <div id="target" ref="target"></div>
+</template>
