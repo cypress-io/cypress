bd1888521a940a71002741af3c9544db1e2313e7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-09-15 00:42:01 +1000

    remove unused files

--- packages/frontend-shared/cypress/support/commands.ts
+++ packages/frontend-shared/cypress/support/commands.ts
@@ -2,7 +2,7 @@ import '@testing-library/cypress/add-commands'
 import { mount, CyMountOptions } from '@cypress/vue'
 import urql, { TypedDocumentNode, useQuery } from '@urql/vue'
 import { print, FragmentDefinitionNode } from 'graphql'
-import { testUrqlClient } from '@packages/frontend-shared'
+import { testUrqlClient } from '@packages/frontend-shared/src/graphql/testUrqlClient'
 import { Component, computed, defineComponent, h } from 'vue'
 
 import { ClientTestContext } from '../../src/graphql/ClientTestContext'
