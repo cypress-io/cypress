25f9a6ec01b0009fd4949d69213d6117528ac8a4
Mark Noonan <mark@cypress.io>
2021-09-20 10:15:46 -0400

    small html tweaks

--- packages/launchpad/src/global/GlobalEmpty.vue
+++ packages/launchpad/src/global/GlobalEmpty.vue
@@ -2,7 +2,7 @@
   <main class="text-center">
     <h1 class="text-2rem mb-2">{{ t('globalPage.empty.title') }}</h1>
     <p class="text-lg font-light text-gray-600 mb-6">{{ t('globalPage.empty.helper') }}</p>
-    <button
+    <div
       type="button"
       @click="selectProject"
       class="min-w-220px relative block w-full border-2 bg-gray-50 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 text-center"
@@ -12,18 +12,19 @@
       />
       <span class="mt-13px block text-lg font-medium text-gray-700 font-light">
         <i18n-t keypath="globalPage.empty.dropText">
-          <a class="font-normal" @click="selectProject">{{ t('globalPage.empty.browseManually') }}</a>
+          <button class="font-normal text-indigo-600 hover:underline" @click.stop="selectProject">{{ t('globalPage.empty.browseManually') }}</button>
         </i18n-t>
       </span>
-    </button>
+    </div>
   </main>
 </template>
 
 <script lang="ts" setup>
 import { useI18n } from "../composables"
 import IconPlaceholder from 'virtual:vite-icons/icons8/circle-thin'
+import Button from '../components/button/Button.vue'
 
 const { t } = useI18n()
 
-const selectProject = () => { }
+const selectProject = () => { console.log('test')}
 </script>
