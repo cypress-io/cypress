f42aa40471a4c6527001ac47f682bc869a4e6ec3
Mark Noonan <mark@cypress.io>
2021-09-21 17:14:09 -0400

    get some initial colors set up

--- packages/launchpad/src/global/GlobalEmpty.vue
+++ packages/launchpad/src/global/GlobalEmpty.vue
@@ -2,20 +2,39 @@
   <main class="text-center">
     <h1 class="text-2rem mb-2">{{ t('globalPage.empty.title') }}</h1>
     <p class="text-lg font-light text-gray-600 mb-6">{{ t('globalPage.empty.helper') }}</p>
-    <button
-      type="button"
-      @click="selectProject"
-      class="min-w-220px relative block w-full border-2 bg-gray-50 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 text-center"
-    >
-      <IconPlaceholder
-        class="-mb-8px mx-auto max-w-65px h-full relative justify-center w-full text-indigo-600"
-      />
-      <span class="mt-13px block text-lg font-medium text-gray-700 font-light">
-        <i18n-t keypath="globalPage.empty.dropText">
-          <a class="font-normal" @click="selectProject">{{ t('globalPage.empty.browseManually') }}</a>
-        </i18n-t>
-      </span>
-    </button>
+    <FileSelector v-model="files" v-slot="{ openDialog }" allow-multiple >
+      <Dropzone v-slot="{ hovered }" @click="openDialog">
+        <div
+          class="min-w-220px relative block w-full border-2 bg-gray-50 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400"
+          :class="{ 'border-blue-200': hovered }"
+        >
+          <IconPlaceholder
+            class="mx-auto max-w-65px h-full reålative justify-center w-full text-primary"
+          />
+          <i18n-t keypath="globalPage.empty.dropText">
+            <button
+              class="text-primary hover:underline"
+            >
+            <!-- 
+              This button allows keyboard users to fire a click event with the Enter or Space keys, 
+              which will be handled by the dropzone's existing click handler.
+             -->
+            {{ t('globalPage.empty.browseManually') }}</button>
+          </i18n-t>
+        </div>
+      </Dropzone>
+    </FileSelector>
+    <div data-testid="upload-name" class="hidden">{{uploadName}}</div>    
+    <div>
+      <div class="text-primary">primary</div>
+      <div class="text-secondary">secondary</div>
+      <div class="text-warning-light">warning-light</div>
+      <div class="text-warning">warning</div>
+      <div class="text-success">success</div>
+      <div class="text-error">error</div>
+      <div class="text-confirm">confirm</div>
+      <div class="body-text">confirm</div>
+    </div>
   </main>
 </template>
 
--- packages/launchpad/windi.config.ts
+++ packages/launchpad/windi.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'windicss/helpers'
 import Colors from 'windicss/colors'
+import { customColors } from './src/design/colors'
 import { map, reduce, kebabCase } from 'lodash'
 
-const safelist = reduce(Colors, (acc, variants, colorName) => {
+const colors = Object.assign(Colors, customColors)
+
+const safelist = reduce(colors, (acc, variants, colorName) => {
   const name = kebabCase(colorName)
 
   return `${acc}
@@ -20,6 +23,17 @@ export default defineConfig({
       gridTemplateRows: {
         launchpad: '64px 1fr',
       },
+      colors: {
+        primary: colors.indigo[500],
+        'secondary-light': colors.jade[300],
+        secondary: colors.indigo[50],
+        error: colors.red[400],
+        'warning-light': colors.orange[400],
+        warning: colors.orange[500],
+        success: colors.jade[400],
+        confirm: colors.jade[500],
+        caution: colors.red[500],
+      },
     },
   },
   safelist,
@@ -27,5 +41,8 @@ export default defineConfig({
     // accepts globs and file paths relative to project root
     include: ['index.html', 'src/**/*.{vue,html,tsx}'],
     exclude: ['node_modules/**/*', '.git/**/*'],
+  },
+  shortcuts: {
+    'body-text': 'text-gray-600',
   },
 })
