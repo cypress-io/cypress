88f9971d09b659c46d069e446af1d0b3b70ee479
Jessica Sachs <jess@jessicasachs.io>
2021-09-22 00:41:04 -0400

    custom-icons wip

--- packages/app/src/navigation/SidebarNavigationRow.spec.tsx
+++ packages/app/src/navigation/SidebarNavigationRow.spec.tsx
@@ -1,7 +1,5 @@
 import SidebarNavigationRow from './SidebarNavigationRow.vue'
-import SettingsIcon from 'virtual:vite-icons/mdi/settings'
-import HeartIcon from 'virtual:vite-icons/mdi/heart'
-import BookmarkIcon from 'virtual:vite-icons/bi/bookmark-star'
+import BookIcon from '~icons/cy/book_x16'
 
 describe('SidebarNavigationRow', () => {
   it('renders something', () => {
@@ -9,9 +7,9 @@ describe('SidebarNavigationRow', () => {
       class="resize-x overflow-auto space-y-4 p-4 h-500px bg-gray-900"
     >
       <h2 class="text-white text-sm uppercase truncate">Tab Style</h2>
-      <SidebarNavigationRow icon={SettingsIcon} />
-      <SidebarNavigationRow icon={HeartIcon} />
-      <SidebarNavigationRow icon={BookmarkIcon} active/>
+      <SidebarNavigationRow icon={BookIcon} />
+      <SidebarNavigationRow icon={BookIcon} />
+      <SidebarNavigationRow icon={BookIcon} active/>
     </div>))
   })
 })
