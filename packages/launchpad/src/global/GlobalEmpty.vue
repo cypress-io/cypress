1f62f2c39ef023eb836d8601d5e283041dbf77aa
Mark Noonan <mark@cypress.io>
2021-09-23 14:30:30 -0400

    cleanup example

--- packages/launchpad/src/global/GlobalEmpty.vue
+++ packages/launchpad/src/global/GlobalEmpty.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="text-center" ref="projectUpload">
     <h1 class="mb-2 text-2rem">{{ t('globalPage.empty.title') }}</h1>
-    <p class="mb-6 text-lg font-light text-gray-600">{{ t('globalPage.empty.helper') }}</p>
+    <p class="mb-6 text-lg font-light text-body-gray">{{ t('globalPage.empty.helper') }}</p>
     <FileSelector v-model="files" v-slot="{ openDialog }" allow-multiple>
       <Dropzone v-slot="{ hovered }" @click="openDialog">
         <div
@@ -24,33 +24,6 @@
       </Dropzone>
     </FileSelector>
     <div data-testid="upload-name" class="hidden">{{ uploadName }}</div>
-    <div class="flex gap-6">
-      <div>
-        <div class="text-primary">primary</div>
-        <div class="text-secondary bg-body-gray">secondary</div>
-        <div class="text-warning">warning</div>
-        <div class="text-warning-light">warning-light</div>
-        <div class="text-success">success</div>
-        <div class="text-success-light">success-light</div>
-        <div class="text-error">error</div>
-        <div class="text-caution">caution</div>
-        <div class="text-confirm">confirm</div>
-        <div class="text-body-gray">body-gray</div>
-      </div>
-      <div class="flex flex-col gap-4">
-        <button class="block text-white pa-2 bg-primary">primary</button>
-        <button class="block pa-2 bg-secondary text-primary">secondary</button>
-        <button class="block pa-2 bg-warning">warning</button>
-        <button class="block pa-2 bg-warning-light">warning-light</button>
-        <button class="block text-white pa-2 bg-success">success</button>
-        <button class="block pa-2 bg-success-light">success-light</button>
-        <button class="block text-white pa-2 bg-error">error</button>
-        <button class="block text-white pa-2 bg-caution">caution</button>
-        <button class="block text-white pa-2 bg-confirm">confirm</button>
-        <button class="block text-white pa-2 bg-body-gray">body-gray</button>
-        <button class="block text-white pa-2 bg-body-gray">body-gray</button>
-      </div>
-    </div>
   </main>
 </template>
 
