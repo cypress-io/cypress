474b8bffcc3a10fff9a8678ac524113fd5bf0ab0
Jessica Sachs <jess@jessicasachs.io>
2021-09-25 20:37:32 -0400

    adding icons with some duplication

---
+++ packages/app/src/specs/SpecsList.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="h-40px"><SpecsListHeader v-model="searchString"/></header>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import SpecsListHeader from './SpecsListHeader.vue'
+
+const searchString = ref('my search')
+</script>
---
+++ packages/app/src/specs/SpecsListHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <Input
+    type="search"
+    class="min-w-200px w-[80%] flex-grow"
+    :placeholder="t('globalPage.searchPlaceholder')"
+    v-model="localValue"
+  />
+  <Button
+    @click="$emit('new-project')"
+    :prefixIcon="IconPlus"
+    prefixIconClass="text-center justify-center text-lg"
+    class="w-[20%] min-w-120px text-size-16px h-full focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+  >{{ t('globalPage.newProjectButton') }}</Button>
+</template>
+
+<script lang="ts" setup>
+import Button from '../components/button/Button.vue'
+import Input from '../components/input/Input.vue'
+import IconPlus from '~icons/mdi/plus'
+import { useI18n, useModelWrapper } from '../composables'
+
+const { t } = useI18n()
+
+const props = defineProps<{
+  modelValue: string
+}>()
+
+const emits = defineEmits(['update:modelValue', 'new-project'])
+
+const localValue = useModelWrapper(props, emits, 'modelValue')
+</script>
