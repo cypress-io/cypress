a2776563c8f551c1510341e7f9051f7a00af4e9d
Luca Pizzini <lpizzini7@gmail.com>
2021-09-29 14:39:51 +0200

    refactor: added async await functions in React examples

--- npm/react/cypress/component/advanced/mocking-imports/services.js
+++ npm/react/cypress/component/advanced/mocking-imports/services.js
@@ -1,6 +1,4 @@
 export const fetchIngredients = async () => {
-  const r = await fetch(
-    'https://httpbin.org/anything?ingredients=bacon&ingredients=mozzarella&ingredients=pineapples',
-  )
+  const r = await fetch('https://httpbin.org/anything?ingredients=bacon&ingredients=mozzarella&ingredients=pineapples')
   return r.json()
 }
