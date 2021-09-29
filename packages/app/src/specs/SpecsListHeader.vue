b87e477cc75fb97869b25df0ea1eb10922d3a73d
Jessica Sachs <jess@jessicasachs.io>
2021-09-29 17:36:53 -0400

    Adding required icons and strings for spec list

--- packages/app/src/specs/SpecsListHeader.vue
+++ packages/app/src/specs/SpecsListHeader.vue
@@ -1,17 +1,28 @@
 <template>
-  <Button>
-  <input :value="inputValue" @input="onInput">
+  <Button @click="emit('new-spec')">{{ t('specPage.newSpecButton') }}</Button>
+  <Input
+    type="search"
+    :prefix-icon="MagnifyingGlass"
+    :value="inputValue"
+    :placeholder="t('specPage.searchPlaceholder')" @input="onInput"/>
 </template>
 
 <script lang="ts" setup>
-import { useVModel } from '@vueuse/core''
+import { useVModel } from '@vueuse/core'
+import { useI18n } from '@cy/i18n'
+import Button from '@cy/components/Button.vue'
+import Input from '@cy/components/Input.vue'
+import MagnifyingGlass from '~icons/cy/magnifying-glass_x16'
+
+const { t } = useI18n()
 
 const props = defineProps<{
   modelValue: string
 }>()
 
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: string): void
+  (e: 'update:modelValue', value: string): void,
+  (e: 'new-spec'): void
 }>()
 
 const inputValue = useVModel(props, 'modelValue', emit)
