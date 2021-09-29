a4b67d6682822f1a1c2317065940de5bf5439ae4
Mark Noonan <mark@cypress.io>
2021-09-29 11:33:02 -0400

    dialing in styles and content for docs menu

--- packages/launchpad/src/components/topnav/TopNav.vue
+++ packages/launchpad/src/components/topnav/TopNav.vue
@@ -1,46 +1,88 @@
 <template>
-    <TopNavList v-if="versionList">
-        <template #heading>{{ versionList[0].version }}</template>
-        <TopNavListItem v-for="(item, index) in versionList" :key="item.version">
-            <div>
-                <a :href="`${releasesUrl}/tag/v${item.version}`" target="_blank">{{ item.version }}</a>
-                <br />
-                {{ item.released }}
-            </div>
-            <template v-if="!index" #suffix>check</template>
-        </TopNavListItem>
-        <TopNavListItem class="text-center p-4">
-            <a :href="releasesUrl">See all releases</a>
-        </TopNavListItem>
-    </TopNavList>
+  <TopNavList v-if="versionList">
+    <template #heading>
+      {{ versionList[0].version }}
+    </template>
+    <TopNavListItem
+      v-for="(item, index) in versionList"
+      :key="item.version"
+    >
+      <div>
+        <a
+          :href="`${releasesUrl}/tag/v${item.version}`"
+          target="_blank"
+        >{{ item.version }}</a>
+        <br>
+        {{ item.released }}
+      </div>
+      <template
+        v-if="!index"
+        #suffix
+      >
+        check
+      </template>
+    </TopNavListItem>
+    <TopNavListItem class="text-center p-4">
+      <a :href="releasesUrl">See all releases</a>
+    </TopNavListItem>
+  </TopNavList>
 
-    <TopNavList v-if="browsers">
-        <template #heading>browser</template>
-        <TopNavListItem v-for="browser in browsers">
-            <template #prepend>hey</template>
-            <span>{{ browser.displayName }}</span>
-            <template #append>append</template>
-        </TopNavListItem>
-    </TopNavList>
+  <TopNavList v-if="browsers">
+    <template #heading>
+      browser
+    </template>
+    <TopNavListItem
+      v-for="browser in browsers"
+      :key="browser.id"
+    >
+      <template #prefix>
+        hey
+      </template>
+      <span>{{ browser.displayName }}</span>
+      <template #suffix>
+        append
+      </template>
+    </TopNavListItem>
+  </TopNavList>
 
-    <TopNavList variant="panel">
-        <template #heading>I have no UL</template>
-        <div>
-            Columns right here
-        </div>
-    </TopNavList>
+  <TopNavList variant="panel">
+    <template #heading>
+      Docs
+    </template>
+    <div class="flex p-4 gap-24px">
+      <div
+        v-for="list in docsMenu"
+        :key="list.title"
+        class="min-w-164px"
+      >
+        <h2 class="text-gray-800 font-semibold">
+          {{ list.title }}
+        </h2>
+        <hr class="border-gray-50 my-2.5">
+        <ul>
+          <li
+            v-for="item in list.children"
+            :key="item.text"
+            class="text-indigo-500 flex mb-2 items-center"
+          >
+            <i-cy-book_x16 class="icon-dark-indigo-500 icon-light-indigo-50" />
+            <span class="whitespace-nowrap font-normal ml-2">{{ item.text }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </TopNavList>
 </template>
 
 <script setup lang="ts">
 
-import { useMutation, gql, useQuery } from "@urql/vue";
+import { gql, useQuery } from '@urql/vue'
 
 import TopNavListItem from './TopNavListItem.vue'
-import TopNavList from "./TopNavList.vue"
+import TopNavList from './TopNavList.vue'
 
-import { TopNavDocument } from "../../generated/graphql"
+import { TopNavDocument } from '../../generated/graphql'
 import { computed } from 'vue'
-
 
 gql`
 query TopNav {
@@ -55,20 +97,117 @@ const browsers = computed(() => query?.data?.value?.app?.browsers)
 
 // will come from gql
 const versionList = [
-    {
-        version: '8.4.1',
-        released: '2 days ago',
-    },
-    {
-        version: '8.4.1',
-        released: '2 days ago',
-    },
-    {
-        version: '8.4.1',
-        released: '2 days ago',
-    }
+  {
+    version: '8.4.1',
+    released: '2 days ago',
+  },
+  {
+    version: '8.4.1',
+    released: '2 days ago',
+  },
+  {
+    version: '8.4.1',
+    released: '2 days ago',
+  },
 ]
 
-const releasesUrl = "https://github.com/cypress-io/cypress/releases/"
+const releasesUrl = 'https://github.com/cypress-io/cypress/releases/'
+
+const utm_medium = 'Docs Menu'
+
+const docsMenu = [{
+  title: 'Getting Started',
+  children: [{
+    text: 'Write your first test',
+    link: {
+      url: 'https://on.cypress.io/writing-first-test',
+      params: {
+        utm_medium,
+        utm_content: 'First Test',
+      },
+    },
+  }, {
+    text: 'Testing your app',
+    link: {
+      url: 'https://on.cypress.io/testing-your-app',
+      params: {
+        utm_medium,
+        utm_content: 'Testing Your App',
+      },
+    },
+  },
+  {
+    text: 'Organizing Tests',
+    link: {
+      url: 'https://docs.cypress.io/guides/core-concepts/writing-and-organizing-testsp',
+      params: {
+        utm_medium,
+        utm_content: 'Organizing Tests',
+      },
+    },
+  }],
+}, {
+  title: 'References',
+  children: [{
+    text: 'Best practices',
+    link: {
+      url: 'https://on.cypress.io/best-practices',
+      params: {
+        utm_medium,
+        utm_content: 'Best Practices',
+      },
+    },
+  }, {
+    text: 'Configuration',
+    link: {
+      url: 'https://on.cypress.io/configuration',
+      params: {
+        utm_medium,
+        utm_content: 'Configuration',
+      },
+    },
+  }, {
+    text: 'API',
+    link: {
+      url: 'https://on.cypress.io/api',
+      params: {
+        utm_medium,
+        utm_content: 'API',
+      },
+    },
+  }],
+}, {
+  title: 'Run in CI/CD',
+  itemIcon: 'far fa-lightbulb',
+  children: [{
+    text: 'Set up CI',
+    link: {
+      url: 'https://on.cypress.io/ci',
+      params: {
+        utm_medium,
+        utm_content: 'Set Up CI',
+      },
+    },
+  }, {
+    text: 'Run tests faster',
+    link: {
+      url: 'https://on.cypress.io/parallelization',
+      params: {
+        utm_medium,
+        utm_content: 'Parallelization',
+      },
+    },
+  },
+  {
+    text: 'Smart Orchestration',
+    link: {
+      url: 'https://docs.cypress.io/guides/dashboard/smart-orchestration',
+      params: {
+        utm_medium,
+        utm_content: 'Smart Orchestration',
+      },
+    },
+  }],
+}]
 
 </script>
--- packages/launchpad/src/components/topnav/TopNavList.vue
+++ packages/launchpad/src/components/topnav/TopNavList.vue
@@ -1,26 +1,30 @@
 <template>
-    <Popover class="relative">
-        <PopoverButton>
-            <slot name="heading">default</slot>
-        </PopoverButton>
-        <TransitionQuickFade>
-            <PopoverPanel class="absolute right-0 z-10 bg-white w-240px shadow">
-                <ul v-if="variant !== 'panel'" class="flex flex-col">
-                    <slot></slot>
-                </ul>
-                <slot v-else></slot>
-            </PopoverPanel>
-        </TransitionQuickFade>
-    </Popover>
+  <Popover class="relative bg-white">
+    <PopoverButton>
+      <slot name="heading">
+        default
+      </slot>
+    </PopoverButton>
+    <TransitionQuickFade>
+      <PopoverPanel class="absolute right-0 z-10 bg-white shadow rounded">
+        <ul
+          v-if="variant !== 'panel'"
+          class="flex flex-col"
+        >
+          <slot />
+        </ul>
+        <slot v-else />
+      </PopoverPanel>
+    </TransitionQuickFade>
+  </Popover>
 </template>
 
 <script setup lang="ts">
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
-import TransitionQuickFade from '../transitions/TransitionQuickFade.vue';
+import TransitionQuickFade from '../transitions/TransitionQuickFade.vue'
 
 const props = defineProps<{
-  variant: String,
+  variant?: String,
 }>()
-
 
 </script>
