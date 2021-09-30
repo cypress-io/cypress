c97b807a631588f97af551b3d020a70bfacb5146
Tim Griesser <tgriesser10@gmail.com>
2021-09-30 14:02:37 -0400

    chore: improve e2e tests for launchpad

--- packages/app/cypress/integration/basic.spec.ts
+++ packages/app/cypress/integration/basic.spec.ts
@@ -1,5 +1,7 @@
-describe('Launchpad', () => {
+describe('App', () => {
   it('resolves the home page', () => {
     cy.visit('http://localhost:5556')
+    cy.get('[href="#/runs"]').click()
+    cy.get('[href="#/settings"]').click()
   })
 })
