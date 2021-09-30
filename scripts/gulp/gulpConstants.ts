340a09417cd5b769208827ddd28d1992a2f7b651
Tim Griesser <tgriesser10@gmail.com>
2021-09-30 14:18:58 -0400

    ability to set XVFB display num

--- scripts/gulp/gulpConstants.ts
+++ scripts/gulp/gulpConstants.ts
@@ -21,7 +21,7 @@ export const ENV_VARS = {
     CYPRESS_INTERNAL_CLOUD_ENV: DEFAULT_INTERNAL_CLOUD_ENV, // staging for now, until we get an e2e workflow w/ cloud project
     CYPRESS_INTERNAL_GQL_PORT: `52300`,
     CYPRESS_INTERNAL_ENV: 'staging', // Different than DEV, which will default to "development". TODO: Make this do less things internall
-    CYPRESS_INTERNAL_E2E_TESTING_SELF: `true`,
+    XVFB_DISPLAY_NUM: `44`,
   },
 
   // Uses the "built" vite assets, not the served ones
