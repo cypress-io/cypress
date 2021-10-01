0abf33c20bb07dc4706729330823723c96ba4c4c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-01 13:02:14 +1000

    log

--- packages/app/src/components/iframes/IFrameCt.vue
+++ packages/app/src/components/iframes/IFrameCt.vue
@@ -6,8 +6,12 @@
 // import { eventManager } from '@packages/runner-shared/src/event-manager'
 import * as Driver from '@packages/driver'
 
+/* eslint-disable no-console */
+console.log(Driver.default.create)
+/* eslint-disable no-console */
+console.log(Driver.default.$)
+
 const run = (spec: any) => {
-  Driver
   // eventManager
 }
 
