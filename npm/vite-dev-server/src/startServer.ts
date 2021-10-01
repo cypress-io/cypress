acceebf2ebaaedd3abde9f73792add2cb9b215dd
userquin <joaquin.sanchez@fi2net.es>
2021-10-02 00:06:22 +0200

    fix(vite-dev-server): windows `supportFile` + preserve optimize entries (#18286)
    Co-authored-by: Jessica Sachs <jess@jessicasachs.io>

--- npm/vite-dev-server/src/startServer.ts
+++ npm/vite-dev-server/src/startServer.ts
@@ -52,12 +52,21 @@ const resolveServerConfig = async ({ viteConfig, options }: StartDevServerOption
   // Ask vite to pre-optimize all dependencies of the specs
   finalConfig.optimizeDeps = finalConfig.optimizeDeps || {}
 
-  // pre-optimizea all the specs
+  // pre-optimize all the specs
   if ((options.specs && options.specs.length)) {
-    finalConfig.optimizeDeps.entries = [...options.specs.map((spec) => spec.relative)]
+    // fix: we must preserve entries configured on target project
+    const existingOptimizeDepsEntries = finalConfig.optimizeDeps.entries
+
+    if (existingOptimizeDepsEntries) {
+      finalConfig.optimizeDeps.entries = [...existingOptimizeDepsEntries, ...options.specs.map((spec) => spec.relative)]
+    } else {
+      finalConfig.optimizeDeps.entries = [...options.specs.map((spec) => spec.relative)]
+    }
+
     // only optimize a supportFile is it is not false or undefined
     if (supportFile) {
-      finalConfig.optimizeDeps.entries.push(supportFile)
+      // fix: on windows we need to replace backslashes with slashes
+      finalConfig.optimizeDeps.entries.push(supportFile.replace(/\\/g, '/'))
     }
   }
 
