44c0619b1d1e077c3665798a6e29b8ad8bbca76f
Mark Noonan <mark@cypress.io>
2021-10-03 21:14:56 -0400

    remove unsed slot code

--- packages/launchpad/src/components/topnav/TopNavListItem.vue
+++ packages/launchpad/src/components/topnav/TopNavListItem.vue
@@ -3,7 +3,7 @@
     class="flex transition-colors duration-300 border-b border-b-gray-50"
     :class="selectable ? 'hover:bg-indigo-50' : ''"
   >
-    <div v-if="hasPrefix">
+    <div v-if="$slots.prefix">
       <div class="flex items-center h-full align-middle">
         <slot name="prefix" />
       </div>
@@ -13,7 +13,7 @@
     </div>
     <div>
       <div
-        v-if="hasSuffix"
+        v-if="$slots.suffix"
         class="flex items-center h-full align-middle"
       >
         <slot name="suffix" />
@@ -22,16 +22,6 @@
   </li>
 </template>
 <script setup lang="ts">
-import { useSlots, computed } from 'vue'
-const slots = useSlots()
-
-const hasPrefix = computed(() => {
-  return Boolean(slots.prefix)
-})
-
-const hasSuffix = computed(() => {
-  return Boolean(slots.suffix)
-})
 
 const props = defineProps<{
   selectable?: Boolean,
