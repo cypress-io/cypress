e60f76a981fa36ffc327ae8ca9b9ae54845e417a
Cesar Avitia <cesar@cypress.io>
2021-10-04 13:28:17 -0700

    no custom query

--- packages/frontend-shared/src/composables/useSetActiveProject.ts
+++ packages/frontend-shared/src/composables/useSetActiveProject.ts
@@ -1,33 +1,27 @@
 import { gql } from '@urql/core'
 import { useMutation } from '@urql/vue'
 
-function makeSetActiveProjectQuery (query?: string) {
-  const activeProjectQuery = `
-    activeProject {
-      id
-      title
-      projectId
-      projectRoot
-      isFirstTimeCT
-      isFirstTimeE2E
-    }
-  `
-
-  return gql`
-    mutation setActiveProject($path: String!) {
-      setActiveProject(path: $path) {
-        ${query ? query : activeProjectQuery}
+const setActiveProjectMutation = gql`
+  mutation setActiveProject($path: String!) {
+    setActiveProject(path: $path) {
+      activeProject {
+        id
+        title
+        projectId
+        projectRoot
+        isFirstTimeCT
+        isFirstTimeE2E
       }
     }
-  `
-}
+  }
+`
 
 /**
  * @param query Graphql query to select data from the App node in graphql
  * @returns setActiveProject function to execute setActiveProject mutation with provided path
  */
 export function useSetActiveProject (query?: string) {
-  const activeProjectMutation = useMutation(makeSetActiveProjectQuery(query))
+  const activeProjectMutation = useMutation(setActiveProjectMutation)
 
   const setActiveProject = (path: string) => activeProjectMutation.executeMutation({ path })
 
