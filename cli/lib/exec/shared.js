90c4b63e95d3bc47375c6d88e00fa0277618507f
BlueWinds <blue@everblue.info>
2021-10-05 10:02:54 -0700

    Add --e2e and --component CLI options

--- cli/lib/exec/shared.js
+++ cli/lib/exec/shared.js
@@ -23,16 +23,16 @@ const throwInvalidOptionError = (details) => {
  * @param {string} testingType The type of tests being executed
  * @returns {string[]} The array of new exec arguments
  */
-const processTestingType = (testingType) => {
-  if (testingType) {
-    if (testingType === 'e2e') {
-      return ['--testing-type', 'e2e']
-    }
+const processTestingType = (options) => {
+  if (options.testingType === 'component' || options.component) {
+    return ['--testing-type', 'component']
+  }
 
-    if (testingType === 'component') {
-      return ['--testing-type', 'component']
-    }
+  if (options.testingType === 'e2e' || options.e2e) {
+    return ['--testing-type', 'e2e']
+  }
 
+  if (options.testingType) {
     return throwInvalidOptionError(errors.invalidTestingType)
   }
 
