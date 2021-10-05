2e18a14c360d5284c933b8e4c01e3290edb452fc
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-05 15:28:35 +1000

    wip: spec

--- packages/app/src/HelloWorld2.spec.tsx
+++ packages/app/src/HelloWorld2.spec.tsx
@@ -1,5 +1,20 @@
+import { mount } from '@cypress/vue'
+import { defineComponent, h, ref } from 'vue'
+
 describe('hello 2', () => {
   it('works', () => {
-    expect(2).to.eq(0)
+    const Comp = defineComponent({
+      setup() {
+        const count = ref(0)
+        return () => h('div', [
+          h('button', { onClick: () => count.value++ }, `Count: ${count.value}`)
+        ])
+      }
+    })
+    mount(Comp)
+    .then(() => cy.get('button').click())
+    .then(() => {
+      cy.get('button').contains('Count: 1')
+    })
   })
 })
