867c770c9588ef7d6c848112d05bd9370ad38160
Cesar Avitia <cesar@cypress.io>
2021-10-04 18:18:20 -0700

    use optional chaining for path

--- packages/launchpad/src/global/GlobalPageHeader.vue
+++ packages/launchpad/src/global/GlobalPageHeader.vue
@@ -60,7 +60,7 @@ type WebkitFile = File & { path: string }
 function getDirectoryPath (files: FileList | null) {
   if (files) {
     const file = files[0] as WebkitFile
-    const path = file.path
+    const path = file?.path ?? ''
 
     return path.substring(0, path.lastIndexOf('/'))
   }
