6c124b8d121de45a5eaf3b26c3eabceec1a56e87
ElevateBart <ledouxb@gmail.com>
2021-10-06 10:04:56 -0500

    feat: normalized signatures webpack & vite servers

--- npm/vite-dev-server/src/index.ts
+++ npm/vite-dev-server/src/index.ts
@@ -1,4 +1,5 @@
 import { debug as debugFn } from 'debug'
+import { InlineConfig } from 'vite'
 import { start as createDevServer, StartDevServerOptions } from './startServer'
 const debug = debugFn('cypress:vite-dev-server:vite')
 
@@ -13,4 +14,12 @@ export async function startDevServer (startDevServerArgs: StartDevServerOptions)
   debug('Component testing vite server started on port', port)
 
   return { port, close: app.httpServer!.close }
+}
+
+export function devServer (cypressDevServerConfig: Cypress.DevServerConfig, devServerConfig?: Omit<InlineConfig, 'base' | 'root'>) {
+  return startDevServer({ options: cypressDevServerConfig, viteConfig: devServerConfig })
+}
+
+export function defineDevServerConfig (devServerConfig: Omit<InlineConfig, 'base' | 'root'>) {
+  return devServerConfig
 }
