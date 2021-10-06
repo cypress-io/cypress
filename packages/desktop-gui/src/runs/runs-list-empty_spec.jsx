610accc42ef3aff4c81dcd759c37a202ad970734
ElevateBart <ledouxb@gmail.com>
2021-10-06 16:36:49 -0500

    add tests for runs list

--- packages/desktop-gui/src/runs/runs-list-empty_spec.jsx
+++ packages/desktop-gui/src/runs/runs-list-empty_spec.jsx
@@ -4,15 +4,38 @@ import { mount } from '@cypress/react'
 import '../main.scss'
 
 /* global cy */
-describe('RunsList', () => {
-  it('retries pinging API server', () => {
+describe('RunsListEmpty', () => {
+  it('should display only a loder is orgId is missing', () => {
+    mount(<RunsListEmpty project={{
+      clientDetails: cy.stub(),
+    }}/>)
+
+    cy.get('.loader').should('exist')
+  })
+
+  it('Displays a tutorial to record your first run', () => {
     mount(<RunsListEmpty project={{
       id: 'id1234',
       clientDetails: cy.stub(),
       orgId: 'appl',
-      getTestGroup: cy.stub(),
+      getTestGroup: cy.stub().returns(false),
       configFile: 'cypress.config.js',
     }}
     recordKey="123456-456789-789123"/>)
+
+    cy.get('.first-run-instructions').should('exist')
+  })
+
+  it('Displays another tutorial to record your first run', () => {
+    mount(<RunsListEmpty project={{
+      id: 'id1234',
+      clientDetails: cy.stub(),
+      orgId: 'appl',
+      getTestGroup: cy.stub().returns(true),
+      configFile: 'cypress.config.js',
+    }}
+    recordKey="123456-456789-789123"/>)
+
+    cy.get('.new-first-run-instructions').should('exist')
   })
 })
