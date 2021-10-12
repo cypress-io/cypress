9ee43b3a050d868b3832c3880c8b0a7cdad318af
ElevateBart <ledouxb@gmail.com>
2021-10-12 15:52:45 -0500

    fix badges colors

--- packages/frontend-shared/src/components/Badge.spec.tsx
+++ packages/frontend-shared/src/components/Badge.spec.tsx
@@ -1,13 +1,13 @@
 import Badge from './Badge.vue'
 
 describe('<Badge />', () => {
-  it('playground', { viewportWidth: 200, viewportHeight: 300 }, () => {
+  it('playground', { viewportWidth: 100, viewportHeight: 300 }, () => {
     cy.mount(() => (
-      <div class="p-6 grid gap-2">
+      <div class="p-4 text-center">
         <Badge status="success" label="Success" />
+        <Badge status="warning" label="Warning" />
         <Badge status="error" label="Error" />
         <Badge status="disabled" label="Disabled" />
-        <Badge status="warning" label="Warning" />
         <Badge status="skipped" label="Skipped" />
       </div>
     ))
--- packages/frontend-shared/src/components/Badge.vue
+++ packages/frontend-shared/src/components/Badge.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-
 const props = defineProps<{
     label: string,
     status: 'success' | 'warning' | 'error' | 'disabled' | 'skipped'
 }>()
 
 const statusClasses = {
-  success: '',
-  warning: '',
-  error: '',
-  disabled: '',
-  skipped: '',
+  disabled: 'bg-gray-50 text-gray-500',
+  error: 'bg-error-100 text-error-600',
+  skipped: 'bg-gray-100 text-gray-700',
+  success: 'bg-success-100 text-success-600',
+  warning: 'bg-warning-100 text-warning-600',
 }
 </script>
+
 <template>
   <div
-    class="text-sm inline-block rounded px-1 whitespace-nowrap"
+    class="text-sm inline-block rounded px-1 m-1 whitespace-nowrap"
     :class="statusClasses[status]"
   >
     {{ label }}
