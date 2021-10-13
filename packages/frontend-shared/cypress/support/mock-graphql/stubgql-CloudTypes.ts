3101b2e0b3cea76406f7dacaa3b8bf4aabefcaf5
ElevateBart <ledouxb@gmail.com>
2021-10-13 11:45:39 -0500

    use ryans icons for cloud test status

--- packages/frontend-shared/cypress/support/mock-graphql/stubgql-CloudTypes.ts
+++ packages/frontend-shared/cypress/support/mock-graphql/stubgql-CloudTypes.ts
@@ -122,6 +122,7 @@ export function createCloudRun (config: Partial<CloudRun>): CloudRun {
     status: 'PASSED',
     totalFailed: 0,
     totalSkipped: 0,
+    totalPending: 0,
     totalRunning: 0,
     totalTests: 10,
     totalPassed: 10,
