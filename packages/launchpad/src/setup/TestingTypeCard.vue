3e002bb798fbe381df5a45a3f3e61306ccfb7961
Cesar <cesaravitia@outlook.com>
2021-10-12 21:54:26 -0700

    feat: add configured state to TestingTypeCard (#18461)
    * add configured state to TestingTypeCard
    
    * remove unused style attribute
    
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>

--- packages/launchpad/src/setup/TestingTypeCard.vue
+++ packages/launchpad/src/setup/TestingTypeCard.vue
@@ -18,8 +18,21 @@
       v-html="description"
     />
     <div class="absolute left-0 right-0 flex justify-between bottom-30px px-30px">
-      <span class="flex items-center px-3 py-1 border rounded-full">
-        <i-oi-clock class="mr-2 text-gray-300 w-14px h-14px" />8 min
+      <span
+        v-if="configured"
+        class="flex items-center px-3 py-1 border text-jade-500 rounded-full"
+      >
+        <GrommetIcon
+          class="fill-current stroke-current text-jade-500"
+        />
+        {{ t('setupPage.testingCard.configured') }}
+      </span>
+      <span
+        v-else
+        class="flex items-center px-3 py-1 border rounded-full"
+      >
+        <GrommetIcon />
+        {{ t('setupPage.testingCard.notConfigured') }}
       </span>
       <span class="border rounded-full text-body-gray-400 w-32px h-32px flex items-center justify-center">
         <i-akar-icons-triangle-right class="h-24px w-24px" />
@@ -29,13 +42,18 @@
 </template>
 
 <script setup lang="ts">
+import { useI18n } from '@cy/i18n'
 import type { TestingTypeEnum } from '../generated/graphql'
+import GrommetIcon from '@packages/frontend-shared/src/assets/icons/grommet.svg'
 import { TestingTypeIcons } from '../utils/icons'
+
+const { t } = useI18n()
 
 const props = defineProps<{
   id: TestingTypeEnum
   title: string
   description: string
+  configured: boolean
 }>()
 
 const icon = TestingTypeIcons[props.id]
