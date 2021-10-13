ae4cf071fc8d404e51431f38a4ff6879eb925771
Jessica Sachs <jess@jessicasachs.io>
2021-10-13 18:24:54 -0400

    wip

--- packages/app/src/specs/CreateSpecCard.spec.tsx
+++ packages/app/src/specs/CreateSpecCard.spec.tsx
@@ -9,7 +9,7 @@ const shortDescription = `We'll walk you through generating your first spec from
 describe('<CreateSpecCard />', { viewportWidth: 400, viewportHeight: 400 }, () => {
   it('renders with long text', () => {
     const longHeader = 'Create from very long header content alsowhenwordshaveno-linebreaks'
-    const longDescription = `This is some description text to explain how we import stuff using this button. Specifically, we're going to create specs from your user input. Also, this is a vary long description to test how our CreateSpecCard resizes.`
+    const longDescription = `This is some description text to explain how we import stuff using this button. Specifically, we're going to create specs from your user input. Also, this is a very long description to test how our CreateSpecCard resizes.`
 
     cy.mount(() => (
       <div class="m-12">
