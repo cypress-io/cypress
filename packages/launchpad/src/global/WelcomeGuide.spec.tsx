8cbfdd577ad02f6a56bc723975444093f4a3214d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-13 13:40:09 +1000

    update test

--- packages/launchpad/src/global/WelcomeGuide.spec.tsx
+++ packages/launchpad/src/global/WelcomeGuide.spec.tsx
@@ -1,12 +1,22 @@
 import { defaultMessages } from '@cy/i18n'
 import WelcomeGuide from './WelcomeGuide.vue'
+import { RecentProjects_WelcomeGuideFragmentDoc } from '../generated/graphql-test'
 
 const welcomeGuide = defaultMessages.welcomeGuide.header
 
 describe('<WelcomeGuide />', () => {
   it('should be dismissable when clicking on the Dismiss button', () => {
     cy.viewport(1200, 800)
-    cy.mount(() => <div class="min-w-400px max-w-1100px resize-x overflow-auto"><WelcomeGuide /></div>)
+    cy.mountFragment(RecentProjects_WelcomeGuideFragmentDoc, {
+      render: (gqlVal) => {
+        return (
+          <div class="min-w-400px max-w-1100px resize-x overflow-auto">
+            <WelcomeGuide gql={gqlVal} />
+          </div>
+        )
+      },
+    })
+
     cy.findByText(welcomeGuide.title).should('be.visible')
     cy.findByText(welcomeGuide.description).should('be.visible')
     cy.findByText(defaultMessages.components.modal.dismiss).click()
@@ -16,6 +26,14 @@ describe('<WelcomeGuide />', () => {
 
   it('renders', () => {
     cy.viewport(1200, 800)
-    cy.mount(() => <div class="min-w-400px max-w-1100px resize-x overflow-auto"><WelcomeGuide /></div>)
+    cy.mountFragment(RecentProjects_WelcomeGuideFragmentDoc, {
+      render: (gqlVal) => {
+        return (
+          <div class="min-w-400px max-w-1100px resize-x overflow-auto">
+            <WelcomeGuide gql={gqlVal} />
+          </div>
+        )
+      },
+    })
   })
 })
