b85277967eb93d563d77e5c652d71e4e9597e04f
ElevateBart <ledouxb@gmail.com>
2021-10-15 13:29:16 -0500

    fix barrel issue

--- packages/data-context/src/codegen/index.ts
+++ packages/data-context/src/codegen/index.ts
@@ -1,6 +1,4 @@
 /* eslint-disable padding-line-between-statements */
 // created by autobarrel, do not modify directly
 
-export * from './config-file'
 export * from './sample-config-files'
-export * from './template'
