463cc03ca1257e50819cba73ef47ad59f1a38629
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-16 02:27:23 +1000

    fix(launchpad): fix bugs and update global mode component (#18465)

--- packages/data-context/src/actions/ProjectActions.ts
+++ packages/data-context/src/actions/ProjectActions.ts
@@ -30,14 +30,13 @@ export class ProjectActions {
   }
 
   async clearActiveProject () {
+    this.ctx.appData.activeProject = null
     await this.api.closeActiveProject()
 
     // TODO(tim): Improve general state management w/ immutability (immer) & updater fn
     this.ctx.coreData.app.isInGlobalMode = true
     this.ctx.coreData.app.activeProject = null
     this.ctx.coreData.app.activeTestingType = null
-    this.ctx.coreData.wizard.history = ['welcome']
-    this.ctx.coreData.wizard.currentStep = 'welcome'
   }
 
   private get projects () {
@@ -49,6 +48,9 @@ export class ProjectActions {
   }
 
   async setActiveProject (projectRoot: string) {
+    this.clearActiveProject()
+    await this.api.closeActiveProject()
+
     this.ctx.coreData.app.activeProject = {
       projectRoot,
       title: '',
@@ -92,6 +94,7 @@ export class ProjectActions {
     }
 
     try {
+      await this.api.closeActiveProject()
       await this.api.initializeProject(launchArgs, {
         ...this.ctx.launchOptions,
         ...options,
