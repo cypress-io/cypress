ad3bb95a71d0ef426ef3004058d9989716eb77a5
Mark Noonan <oddlyaromatic@gmail.com>
2021-10-15 12:03:31 -0400

    feat: (launchpad) add modal to display testing type differences (#18499)
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>

--- packages/launchpad/src/Main.vue
+++ packages/launchpad/src/Main.vue
@@ -5,14 +5,37 @@
       <template v-if="query.error.value">
         <BaseError />
       </template>
-      <template v-else-if="query.data.value.app.isInGlobalMode && !query.data.value?.app.activeProject">
+      <template
+        v-else-if="query.data.value.app.isInGlobalMode && !query.data.value?.app.activeProject"
+      >
         <GlobalPage :gql="query.data.value.app" />
       </template>
 
       <template v-else>
         <template v-if="query.data.value?.wizard.step === 'welcome'">
           <WizardHeader :gql="query.data.value.wizard" />
-          <TestingTypeCards :gql="query.data.value" />
+          <StandardModal
+            v-model="isTestingTypeModalOpen"
+            modal-classes="w-960px"
+          >
+            <template #title>
+              Key Differences
+            </template>
+
+            <CompareTestingTypes />
+          </StandardModal>
+          <button
+            class="block mx-auto text-indigo-500 text-18px focus:outline-transparent hocus:underline group"
+            @click="isTestingTypeModalOpen = true"
+          >
+            {{ t('welcomePage.review') }}<i-cy-arrow-right_x16
+              class="inline-block transition-transform duration-200 ease-in transform -translate-y-1px ml-4px icon-dark-current group-hocus:translate-x-2px"
+            />
+          </button>
+          <TestingTypeCards
+            :gql="query.data.value"
+            @open-compare="isTestingTypeModalOpen = true"
+          />
         </template>
         <Wizard
           v-else
@@ -35,6 +58,13 @@ import WizardHeader from './setup/WizardHeader.vue'
 import HeaderBar from './layouts/HeaderBar.vue'
 import GlobalPage from './global/GlobalPage.vue'
 import BaseError from './error/BaseError.vue'
+import StandardModal from '@cy/components/StandardModal.vue'
+import CompareTestingTypes from './setup/CompareTestingTypes.vue'
+import { useI18n } from '@cy/i18n'
+import { ref } from 'vue'
+
+const { t } = useI18n()
+const isTestingTypeModalOpen = ref(false)
 
 gql`
 query MainLaunchpadQuery {
--- packages/launchpad/src/setup/TestingTypeCards.spec.tsx
+++ packages/launchpad/src/setup/TestingTypeCards.spec.tsx
@@ -60,4 +60,18 @@ describe('TestingTypeCards', () => {
       cy.contains('Click here to configure end-to-end testing with Cypress.').should('be.visible')
     })
   })
+
+  it('emits an `openCompare` event when the question mark is clicked in cards', () => {
+    cy.mountFragment(TestingTypeCardsFragmentDoc, {
+      render: (gqlVal) => {
+        return <TestingTypeCards gql={gqlVal} />
+      },
+    }).then(() => {
+      cy.findAllByLabelText(defaultMessages.welcomePage.review).as('buttons')
+      cy.get('@buttons').eq(0).click()
+      cy.get('@buttons').eq(1).click().then(() =>
+        cy.wrap(Cypress.vueWrapper.findComponent(TestingTypeCards).emitted('openCompare'))
+        .should('have.length', 2))
+    })
+  })
 })
