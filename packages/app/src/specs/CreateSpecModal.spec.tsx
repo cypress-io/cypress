124348d59b35ecce9a8a9983cc5f18ab10e155f4
Jessica Sachs <jess@jessicasachs.io>
2021-10-15 19:04:40 -0400

    finishing the tests for CreateSpecModal

--- packages/app/src/specs/CreateSpecModal.spec.tsx
+++ packages/app/src/specs/CreateSpecModal.spec.tsx
@@ -1,23 +1,24 @@
 import CreateSpecModal from './CreateSpecModal.vue'
 import { ImportFromComponentGenerator } from './generators'
 import { ref } from 'vue'
+import { defaultMessages } from '@cy/i18n'
 
 const modalCloseSelector = '[aria-label=Close]'
 const triggerButtonSelector = '[data-testid=trigger]'
 const modalSelector = '[data-testid=create-spec-modal]'
-const descriptionSelector = '[data-testid=create-generator-description]'
-const titleSelector = '[data-testid=create-generator-title]'
-const overlaySelector = '[data-testid=create-spec-modal-overlay]'
+
+const messages = defaultMessages.createSpec.component.importFromComponent
 
 describe('<CreateSpecModal />', () => {
   beforeEach(() => {
     const show = ref(true)
-    cy.mount(() => <div>
+
+    cy.mount(() => (<div>
       <CreateSpecModal
-        show={ show }
-        onClose={() => show.value = false }
-        currentGenerator={ ImportFromComponentGenerator }/>
-    </div>)
+        show={show}
+        onClose={() => show.value = false}
+        currentGenerator={ImportFromComponentGenerator}/>
+    </div>))
   })
 
   it('renders a modal', () => {
@@ -27,49 +28,43 @@ describe('<CreateSpecModal />', () => {
   describe('dismissing', () => {
     it('is not dismissed when you press escape or click outside', () => {
       cy.get(modalSelector)
-        .click(0, 0)
-        .get(modalSelector)
-        .should('be.visible')
-        .type('{esc}')
-        .get(modalSelector)
-        .should('be.visible')
+      .click(0, 0)
+      .get(modalSelector)
+      .should('be.visible')
+      .type('{esc}')
+      .get(modalSelector)
+      .should('be.visible')
     })
 
     it('is dismissed when the X button is clicked', () => {
       cy.get(modalSelector)
-        .get(modalCloseSelector)
-        .click()
-        .get(modalSelector)
-        .should('not.be.visible')
+      .get(modalCloseSelector)
+      .click()
+      .get(modalSelector)
+      .should('not.exist')
     })
   })
-  
-})
 
-it('can be opened and closed via the show prop', () => {
-  const show = ref(false)
-  cy.mount(() => <>
-    <button data-testid="trigger" onClick={ () => show.value = true }>Open Modal</button>
-    <br/>
-    <CreateSpecModal
-      show={ show }
-      onClose={() => show.value = false }
-      currentGenerator={ ImportFromComponentGenerator }/>
-  </>).get(triggerButtonSelector)
+  describe('generator', () => {
+    it('renders the generator', () => {
+      cy.contains(messages.chooseAComponentHeader).should('be.visible')
+    })
+  })
+
+  it('can be opened and closed via the show prop', () => {
+    const show = ref(false)
+
+    cy.mount(() => (<>
+      <button data-testid="trigger" onClick={() => show.value = true}>Open Modal</button>
+      <br/>
+      <CreateSpecModal
+        show={show}
+        onClose={() => show.value = false}
+        currentGenerator={ImportFromComponentGenerator}/>
+    </>)).get(triggerButtonSelector)
     .click()
     .get(modalSelector)
     .should('be.visible')
     .get(modalCloseSelector)
-})
-
-it('renders the currently active generator', () => {
-  const show = ref(false)
-  cy.mount(() => <>
-    <button onClick={ () => show.value = true }>Open Modal</button>
-    <br/>
-    <CreateSpecModal
-      show={ show }
-      onClose={() => show.value = false }
-      currentGenerator={ ImportFromComponentGenerator }/>
-  </>)
+  })
 })
--- packages/app/src/specs/CreateSpecModal.vue
+++ packages/app/src/specs/CreateSpecModal.vue
@@ -1,32 +1,40 @@
 <template>
-  <StandardModal :clickOutside="false"
-    :modelValue="show"
-    @update:modelValue="close"
+  <StandardModal
+    :click-outside="false"
+    :model-value="show.value"
     variant="bare"
     data-testid="create-spec-modal"
-    >
-
+    @update:modelValue="close"
+  >
     <template #overlay="{classes}">
-      <DialogOverlay :class="classes" class="bg-gray-900 opacity-[0.97]" data-testid="create-spec-modal-overlay"/>
+      <DialogOverlay
+        :class="classes"
+        class="bg-gray-900 opacity-[0.97]"
+      />
     </template>
     <template #title>
-      <h2 class="inline-block font-medium text-gray-900 text-size-18px" data-testid="create-generator-title">
+      <h2 class="inline-block font-medium text-gray-900 text-size-18px">
         {{ title }}
       </h2>
     </template>
     <!-- The presence of the description slot within StandardModal adds
 additional style, classes, and padding. Make sure to render it intentionally -->
 
-    <template #description v-if="description" data-testid="create-generator-description">{{ description }}</template>
+    <template
+      v-if="description"
+      #description
+    >
+      {{ description }}
+    </template>
 
     <!-- Each generator contains the entry component which leads the
 user through the flow to generate a spec. When a generator is finished, it
 will emit a success or failure event -->
     <component
-      v-model:title="title" 
+      :is="currentGenerator?.entry"
+      v-model:title="title"
       v-model:description="description"
-      :is="currentGenerator?.entry">
-    </component>
+    />
   </StandardModal>
 </template>
 
@@ -41,7 +49,7 @@ withDefaults(defineProps<{
   currentGenerator?: SpecGenerator | null,
 }>(), {
   currentGenerator: null,
-  show: false
+  show: false,
 })
 
 const emits = defineEmits<{
