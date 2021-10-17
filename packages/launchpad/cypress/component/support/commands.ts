ec2808b19aaef6e7a81223fc016211d977c177a9
Mark Noonan <mark@cypress.io>
2021-10-17 14:18:31 -0400

    Add cypress vue command to find components to test

--- packages/launchpad/cypress/component/support/commands.ts
+++ packages/launchpad/cypress/component/support/commands.ts
@@ -1,3 +1,11 @@
 import { registerMountFn } from '@packages/frontend-shared/cypress/support/common'
 
 registerMountFn()
+
+Cypress.Commands.add('vue', (componentToFind = null) => {
+  if (componentToFind) {
+    return cy.wrap(Cypress.vueWrapper.findComponent(componentToFind))
+  }
+
+  return cy.wrap(Cypress.vueWrapper)
+})
