41a96ed1739d401a947d6ceb2f264ab9815ba18b
Mark Noonan <mark@cypress.io>
2021-10-18 11:41:32 -0400

    no-results-wip

---
+++ packages/frontend-shared/src/components/NoResults.vue
@@ -0,0 +1,30 @@
+<template>
+  <p v-if="search">
+    {{ message }}:
+    <span class="text-purple-300">{{ search }}</span>
+  </p>
+
+  <Button
+    size="lg"
+    variant="outline"
+    @click="emit('clear')"
+  >
+    Clear Search
+  </Button>
+</template>
+
+<script setup lang="ts">
+import Button from '@cy/components/Button.vue'
+
+withDefaults(defineProps<{
+    search?: string,
+    message?: string
+}>(), {
+  message: 'No results matched your search',
+})
+
+const emit = defineEmits<{
+    (e: 'clear'): void
+}>()
+
+</script>
