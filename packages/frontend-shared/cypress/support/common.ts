cbda6a79b1d0a2cc844782bf10fd27bf70274646
Mark Noonan <mark@cypress.io>
2021-10-18 15:22:10 -0400

    add vue command in both command files

--- packages/frontend-shared/cypress/support/common.ts
+++ packages/frontend-shared/cypress/support/common.ts
@@ -1,2 +1,25 @@
 // Add cy.mount, cy.mountFragment, cy.mountFragmentList
 export * from './mock-graphql/mountFragment'
+
+import type { Component } from 'vue'
+
+export function addVueCommand () {
+  Cypress.Commands.add('vue', (componentToFind = null) => {
+    if (componentToFind) {
+      return cy.wrap(Cypress.vueWrapper.findComponent(componentToFind))
+    }
+
+    return cy.wrap(Cypress.vueWrapper)
+  })
+}
+
+declare global {
+    namespace Cypress {
+      interface Chainable {
+        /**
+         * return Vue Test Utils wrapper wrapped in a cypress chainable
+         */
+        vue(componentToFind?: Component): Cypress.Chainable<any>
+      }
+    }
+  }
--- packages/launchpad/cypress/component/support/commands.ts
+++ packages/launchpad/cypress/component/support/commands.ts
@@ -1,24 +1,4 @@
-import { registerMountFn } from '@packages/frontend-shared/cypress/support/common'
-
-import type { Component } from 'vue'
+import { registerMountFn, addVueCommand } from '@packages/frontend-shared/cypress/support/common'
 
 registerMountFn()
-
-Cypress.Commands.add('vue', (componentToFind = null) => {
-  if (componentToFind) {
-    return cy.wrap(Cypress.vueWrapper.findComponent(componentToFind))
-  }
-
-  return cy.wrap(Cypress.vueWrapper)
-})
-
-declare global {
-    namespace Cypress {
-      interface Chainable {
-        /**
-         * return Vue Test Utils wrapper wrapped in a cypress chainable
-         */
-        vue(componentToFind?: Component): Cypress.Chainable<any>
-      }
-    }
-  }
+addVueCommand()
