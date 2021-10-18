fb79dbdf76148573942a1e45ed3e4892fba177e6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-18 11:22:14 +1000

    await clearing of active project

--- packages/data-context/src/actions/ProjectActions.ts
+++ packages/data-context/src/actions/ProjectActions.ts
@@ -48,7 +48,7 @@ export class ProjectActions {
   }
 
   async setActiveProject (projectRoot: string) {
-    this.clearActiveProject()
+    await this.clearActiveProject()
 
     this.ctx.coreData.app.activeProject = {
       projectRoot,
