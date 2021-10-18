a2a8836d5e056b284db62c3a6e153727b4bb6553
Mark <mark@cypress.io>
2021-10-18 18:54:55 -0400

    reduce button markup with v-for

--- packages/launchpad/src/global/GlobalProjectCard.vue
+++ packages/launchpad/src/global/GlobalProjectCard.vue
@@ -3,24 +3,22 @@
     class="relative w-full rounded border border-gray-100 bg-white pr-16px pt-13px pb-13px flex items-center space-x-3 group hocus-default focus-within-default"
     @click="setActiveProject(props.gql.projectRoot)"
   >
-    <div class="w-73px h-40px text-center flex items-center justify-center border-r border-r-gray-100 mr-4px">
+    <div
+      class="w-73px h-40px text-center flex items-center justify-center border-r border-r-gray-100 mr-4px"
+    >
       <i-cy-bookmark_x24
         class="w-24px h-28px icon-dark-gray-500 icon-light-gray-50 group-hocus:icon-dark-indigo-400 group-hocus:icon-light-indigo-200"
       />
     </div>
 
     <div class="flex-1 min-w-0">
-      <button class="focus:outline-none underline-transparent grid w-full text-left children:truncate">
+      <button
+        class="focus:outline-none underline-transparent grid w-full text-left children:truncate"
+      >
         <span
           class="text-16px row-[1] leading-normal font-medium text-indigo-500"
-        >
-          {{ props.gql.title }}
-        </span>
-        <span
-          class="text-sm text-gray-500 relative"
-        >
-          {{ props.gql.projectRoot }}
-        </span>
+        >{{ props.gql.title }}</span>
+        <span class="text-sm text-gray-500 relative">{{ props.gql.projectRoot }}</span>
       </button>
     </div>
 
@@ -31,35 +29,23 @@
       >
         <i-cy-vertical-dots_x16
           class="icon-dark-current transition transition-color duration-300"
-          :class="open? 'icon-dark-gray-700' : 'group-hocus:icon-dark-gray-300'"
+          :class="open ? 'icon-dark-gray-700' : 'group-hocus:icon-dark-gray-300'"
         />
       </MenuButton>
-      <MenuItems class="absolute bg-gray-900 text-white flex flex-col right-0 -bottom-100px right-18px rounded outline-transparent z-40">
-        <MenuItem #="{ active }">
+      <MenuItems
+        class="absolute bg-gray-900 text-white flex flex-col right-0 -bottom-100px right-18px outline-transparent z-40"
+      >
+        <MenuItem
+          v-for="item in menuItems"
+          :key="item.name"
+          #="{ active }"
+        >
           <button
             :class="{ 'bg-gray-700': active }"
-            class="text-left px-16px py-8px border-b border-b-gray-800"
-            @click="emit('removeProject', props.gql.projectRoot)"
+            class="rounded text-left px-16px py-8px border-b border-b-gray-800"
+            @click.stop="emit(item.event, props.gql.projectRoot)"
           >
-            Remove Project
-          </button>
-        </MenuItem>
-        <MenuItem #="{ active }">
-          <button
-            :class="{ 'bg-gray-700': active }"
-            class="text-left px-16px py-8px border-b border-b-gray-800"
-            @click="emit('openInIDE', props.gql.projectRoot)"
-          >
-            Open In IDE
-          </button>
-        </MenuItem>
-        <MenuItem #="{ active }">
-          <button
-            :class="{ 'bg-gray-700': active }"
-            class="text-left px-16px py-8px border-b border-b-gray-800 rounded-b"
-            @click="emit('openInFinder', props.gql.projectRoot)"
-          >
-            Open In Finder
+            {{ item.name }}
           </button>
         </MenuItem>
       </MenuItems>
@@ -71,6 +57,7 @@
 import { gql, useMutation } from '@urql/vue'
 import { GlobalProjectCardFragment, GlobalProjectCard_SetActiveProjectDocument } from '../generated/graphql'
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import { useI18n } from '@cy/i18n'
 
 gql`
 mutation GlobalProjectCard_setActiveProject($path: String!) {
@@ -107,4 +94,12 @@ const emit = defineEmits<{
   (event: 'openInIDE', path: string): void
   (event: 'openInFinder', path: string): void
 }>()
+
+const { t } = useI18n()
+
+const menuItems = [
+  { name: t('globalPage.removeProject'), event: 'removeProject' },
+  { name: t('globalPage.openInIDE'), event: 'openInIDE' },
+  { name: t('globalPage.openInFinder'), event: 'openInFinder' },
+]
 </script>
