526d65c4f81eb9af6b15b34c0cc612d20a5e930b
Cesar Avitia <cesar@cypress.io>
2021-10-18 15:34:54 -0700

    hide dock icon when window is hidden

--- packages/data-context/src/actions/ElectronActions.ts
+++ packages/data-context/src/actions/ElectronActions.ts
@@ -1,4 +1,5 @@
-import type { BrowserWindow } from 'electron'
+import { app, BrowserWindow } from 'electron'
+import os from 'os'
 import type { DataContext } from '..'
 
 export class ElectronActions {
@@ -8,16 +9,31 @@ export class ElectronActions {
     return this.ctx.coreData.electron
   }
 
+  private get isMac () {
+    return os.platform() === 'darwin'
+  }
+
   setBrowserWindow (window: BrowserWindow) {
     this.electron.browserWindow = window
   }
 
   hideBrowserWindow () {
     this.electron.browserWindow?.hide()
-    // this.electron.browserWindow?.setIcon(nativeImage.createEmpty())
+
+    if (this.isMac) {
+      app.dock.hide()
+    } else {
+      this.electron.browserWindow?.setSkipTaskbar(true)
+    }
   }
 
   showBrowserWindow () {
     this.electron.browserWindow?.show()
+
+    if (this.isMac) {
+      app.dock.show()
+    } else {
+      this.electron.browserWindow?.setSkipTaskbar(false)
+    }
   }
 }
