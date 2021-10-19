1e21f37e14b01028f4d528f18d219d57a6fce2c0
David Munechika <davidam1203@gmail.com>
2021-10-19 08:57:16 -0400

    change vf

--- packages/server/lib/video_capture.ts
+++ packages/server/lib/video_capture.ts
@@ -60,14 +60,11 @@ export function getMsFromDuration (duration) {
 
 export function getCodecData (src) {
   return new Bluebird((resolve, reject) => {
-    return ffmpeg({ logger: true })
-    .outputOptions('-pix_fmt yuv420p')
+    return ffmpeg()
+    .outputOptions('-vf format=yuv420p')
     .on('stderr', (stderr) => {
       return debug('get codecData stderr log %o', { message: stderr })
     }).on('codecData', resolve)
-    .on('error', function (err) {
-      return debug(`An error occurred: ${ err.message}`)
-    })
     .input(src)
     .format('null')
     .output(new BlackHoleStream())
