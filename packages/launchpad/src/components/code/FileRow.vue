492adccaf0186c7a9971e6588f29126e3c006a1d
Jessica Sachs <jess@jessicasachs.io>
2021-10-20 16:54:27 -0400

    fix: opening link is not working. location is undefined

--- packages/launchpad/src/components/code/FileRow.vue
+++ packages/launchpad/src/components/code/FileRow.vue
@@ -43,8 +43,8 @@
         <p class="flex-grow ml-1 text-left">
           Please merge the code below with your existing <span class="inline-block px-1 rounded bg-warning-200 text-warning-600">{{ filePath }}</span>
         </p>
-        <Button @click="document.location.assign('https://docs.cypress.io/config')">
-          Learn more
+        <Button @click="openDocs">
+          {{ t('links.learnMore') }}
         </Button>
       </div>
       <ShikiHighlight
@@ -62,7 +62,10 @@ import { computed, ref } from 'vue'
 import ListRow from '@cy/components/ListRow.vue'
 import Button from '@cy/components/Button.vue'
 import Badge from '@cy/components/Badge.vue'
+import { useI18n } from '@cy/i18n'
 import ShikiHighlight, { CyLangType, langsSupported } from '@cy/components/ShikiHighlight.vue'
+
+const { t } = useI18n()
 
 const props = defineProps<{
     status: 'changes' | 'valid' | 'skipped' | 'error'
@@ -85,6 +88,10 @@ const language = computed(() => {
 const open = ref(!['valid', 'skipped'].includes(props.status))
 const handleToggle = () => {
   open.value = !open.value
+}
+
+const openDocs = () => {
+  window.open('https://docs.cypress.io/config')
 }
 
 const statusLabel = computed(() => props.status === 'skipped' ? 'Skipped' : props.status === 'changes' ? 'Changes required' : undefined)
