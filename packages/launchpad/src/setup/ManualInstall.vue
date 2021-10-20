84425e80ade32737cf0baf4d9e9c9cbdc081d6c7
Mark <mark@cypress.io>
2021-10-20 15:52:02 -0400

    wrap up install dependencies style

--- packages/launchpad/src/setup/ManualInstall.vue
+++ packages/launchpad/src/setup/ManualInstall.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="relative p-24px">
-    <div class="absolute h-40px w-160px bg-gradient-to-r from-transparent to-white via-white right-25px top-25px rounded pointer-events-none" />
+  <div class="relative px-24px py-22px">
+    <div class="absolute h-38px w-160px bg-gradient-to-r from-transparent to-white via-white right-25px top-25px rounded pointer-events-none" />
     <div class="code-area text-left p-8px text-purple-500 border rounded flex items-center overflow-x-scroll">
       <i-cy-dollar_x16 class="icon-dark-gray-500 mr-12px" /> <pre>{{ dependenciesCode }}</pre>
     </div>
     <CopyButton
       :text="dependenciesCode"
-      class="top-29px right-30px absolute"
+      class="top-27px right-30px absolute"
     />
   </div>
-  <div class="border-t border-t-gray-100 px-24px pt-4px">
-    <dl>
-      <div
+  <div class="border-t border-t-gray-100 px-24px">
+    <ul>
+      <li
         v-for="dep in props.gql.packagesToInstall"
         :key="dep.id"
-        class="py-20px border-b border-b-gray-100 last-of-type:border-b-0"
+        class="py-16px border-b border-b-gray-100 last-of-type:border-b-0"
       >
-        <dt class="text-indigo-500 text-14px">
-          {{ dep.package }}
-        </dt>
-        <dd
-          class="text-gray-500 text-14px mt-10px leading-5"
+        <a
+          :href="`https://www.npmjs.com/package/${dep.package}`"
+          target="_blank"
+          class="text-indigo-500 text-14px hocus-link-default"
+        >{{ dep.package }}</a>
+        <p
+          class="text-gray-500 text-14px leading-5"
           v-html="dep.description"
         />
-      </div>
-    </dl>
+      </li>
+    </ul>
   </div>
 </template>
 
