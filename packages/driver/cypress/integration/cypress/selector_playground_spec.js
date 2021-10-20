2be65ce427cbdc732638fe7bbd4f7df6b5e51633
mjhenkes <mjhenkes@gmail.com>
2021-10-20 13:33:46 -0500

    update test name

--- packages/driver/cypress/integration/cypress/selector_playground_spec.js
+++ packages/driver/cypress/integration/cypress/selector_playground_spec.js
@@ -55,7 +55,7 @@ describe('src/cypress/selector_playground', () => {
       .and.include('`Cypress.SelectorPlayground.defaults()` called with invalid `selectorPriority` property. It must be one of: `data-*`, `id`, `class`, `tag`, `attributes`, `nth-child`. You passed: `name`')
     })
 
-    it('throws if selector:playground:priority if selectorPriority contains an unsupported priority that contains a valid priority', () => {
+    it('throws if selector:playground:priority has an unsupported priority that contains a substring of a valid priority', () => {
       const fn = () => {
         SelectorPlayground.defaults({
           selectorPriority: [
