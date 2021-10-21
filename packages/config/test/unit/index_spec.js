8fb03ab9529a12700696a3313c443f5c3cfb1f21
Emily Rohrbough <emilyrohrbough@yahoo.com>
2021-10-21 13:28:08 -0500

    tests

--- packages/config/test/unit/index_spec.js
+++ packages/config/test/unit/index_spec.js
@@ -1,16 +1,136 @@
-const validator = require('../../src/index')
-const { expect } = require('chai')
+const chai = require('chai')
+const snapshot = require('snap-shot-it')
+const sinon = require('sinon')
+const sinonChai = require('@cypress/sinon-chai')
+
+const configUtil = require('../../src/index')
+
+chai.use(sinonChai)
+const { expect } = chai
 
 describe('src/index', () => {
+  describe('.allowed', () => {
+    it('returns filter config only containing allowed keys', () => {
+      const keys = configUtil.allowed({
+        'baseUrl': 'https://url.com',
+        'blacklistHosts': 'breaking option',
+        'devServerPublicPathRoute': 'internal key',
+        'random': 'not a config option',
+      })
+
+      expect(keys).to.deep.eq({
+        'baseUrl': 'https://url.com',
+        'blacklistHosts': 'breaking option',
+      })
+    })
+  })
+
+  describe('.getBreakingKeys', () => {
+    it('returns list of breaking config keys', () => {
+      const breakingKeys = configUtil.getBreakingKeys()
+
+      expect(breakingKeys).to.include('blacklistHosts')
+      snapshot(breakingKeys)
+    })
+  })
+
+  describe('.getDefaultValues', () => {
+    it('returns list of public config keys', () => {
+      const defaultValues = configUtil.getDefaultValues()
+
+      expect(defaultValues).to.deep.include({
+        defaultCommandTimeout: 4000,
+        env: {},
+        scrollBehavior: 'top',
+        watchForFileChanges: true,
+      })
+
+      snapshot(defaultValues)
+    })
+  })
+
   describe('.getPublicConfigKeys', () => {
-    beforeEach(function () {
-      this.includes = (key) => {
-        expect(validator.getPublicConfigKeys()).to.include(key)
-      }
+    it('returns list of public config keys', () => {
+      const publicConfigKeys = configUtil.getPublicConfigKeys()
+
+      expect(publicConfigKeys).to.include('blockHosts')
+      expect(publicConfigKeys).to.not.include('devServerPublicPathRoute')
+      snapshot(publicConfigKeys)
+    })
+  })
+
+  describe('.matchesConfigKey', () => {
+    it('returns key when config key has a default value', () => {
+      const normalizedKey = configUtil.matchesConfigKey('testFiles')
+
+      expect(normalizedKey).to.eq('testFiles')
     })
 
-    it('includes blockHosts', function () {
-      return this.includes('blockHosts')
+    it('returns normalized key when config key has a default value', () => {
+      let normalizedKey = configUtil.matchesConfigKey('EXEC_TIMEOUT')
+
+      expect(normalizedKey).to.eq('execTimeout')
+
+      normalizedKey = configUtil.matchesConfigKey('Base-url')
+      expect(normalizedKey).to.eq('baseUrl')
+
+      normalizedKey = configUtil.matchesConfigKey('TEST-FILES')
+
+      expect(normalizedKey).to.eq('testFiles')
+    })
+
+    it('returns nothing when config key does not has a default value', () => {
+      let normalizedKey = configUtil.matchesConfigKey('random')
+
+      expect(normalizedKey).to.be.undefined
+    })
+  })
+
+  describe('.validate', () => {
+    it('validates config', () => {
+      const errorFn = sinon.spy()
+
+      configUtil.validate({
+        'baseUrl': 'https://',
+      }, errorFn)
+
+      expect(errorFn).to.have.been.callCount(0)
+    })
+
+    it('calls error callback if config is invalid', () => {
+      const errorFn = sinon.spy()
+
+      configUtil.validate({
+        'baseUrl': ' ',
+      }, errorFn)
+
+      expect(errorFn).to.have.been.calledWithMatch(/Expected `baseUrl`/)
+    })
+  })
+
+  describe('.validateNoBreakingConfig', () => {
+    it('calls warning callback if config contains breaking option that warns', () => {
+      const warningFn = sinon.spy()
+      const errorFn = sinon.spy()
+
+      configUtil.validateNoBreakingConfig({
+        'experimentalNetworkStubbing': 'should break',
+      }, warningFn, errorFn)
+
+      expect(warningFn).to.have.been.calledOnceWith('EXPERIMENTAL_NETWORK_STUBBING_REMOVED', 'experimentalNetworkStubbing', undefined)
+      expect(errorFn).to.have.been.callCount(0)
+    })
+
+    it('calls error callback if config contains breaking option that should throw an error', () => {
+      const warningFn = sinon.spy()
+      const errorFn = sinon.spy()
+
+      configUtil.validateNoBreakingConfig({
+        'blacklistHosts': 'should break',
+      }, warningFn, errorFn)
+
+      expect(warningFn).to.have.been.callCount(0)
+      expect(errorFn).to.have.been.calledOnceWith('RENAMED_CONFIG_OPTION', 'blacklistHosts', 'blockHosts')
     })
   })
 })
