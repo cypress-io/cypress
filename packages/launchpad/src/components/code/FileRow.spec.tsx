f58d9233fe4970b793fdf7f245a439eee7de5f20
Jessica Sachs <jess@jessicasachs.io>
2021-10-22 05:18:39 -0400

    feat: create collapsible component (#18602)
    * wip
    
    * adding a collapsible component
    
    * restructuring the file row component
    
    * fixing the runcard
    
    * fix type
    
    * dont show duplicate copy button
    
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>

--- packages/launchpad/src/components/code/FileRow.spec.tsx
+++ packages/launchpad/src/components/code/FileRow.spec.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-irregular-whitespace */
 import FileRow from './FileRow.vue'
+import faker from 'faker'
+import { defaultMessages } from '@cy/i18n'
 
-const content = `import { defineConfig } from ’cypress'
-import { devServer, defineDevServerConfig } from ’@cypress/vite-dev-server’
+const content = `import { defineConfig } from 'cypress'
+import { devServer, defineDevServerConfig } from '@cypress/vite-dev-server'
 
 export default defineConfig({
   component: {
@@ -13,6 +15,11 @@ export default defineConfig({
   },
 })`
 
+const description = faker.hacker.phrase()
+const messages = defaultMessages.setupPage.configFile
+
+const changesRequiredDescription = messages.changesRequiredDescription.replace('{0}', '')
+
 describe('FileRow', () => {
   it('renders each status', () => {
     cy.mount(() => (
@@ -21,25 +28,25 @@ describe('FileRow', () => {
           status="valid"
           content={content}
           filePath="cypress/integration/support.ts"
-          description="Lorem ipsum dolor sit"
+          description={description}
         />
         <FileRow
           status="changes"
           content={content}
           filePath="cypress/integration/command.js"
-          description="Aenean lacinia bibendum nulla sed consectetur."
+          description={description}
         />
         <FileRow
           status="skipped"
           content={content}
           filePath="cypress.config.js"
-          description="Lorem ipsum dolor sit"
+          description={description}
         />
         <FileRow
           status="error"
           content={content}
           filePath="cypress/integration/index.js"
-          description="Lorem ipsum dolor sit"
+          description={description}
         />
       </div>
     ))
@@ -52,38 +59,43 @@ describe('FileRow', () => {
           status="valid"
           content={content}
           filePath="cypress/integration/support.js"
-          description="Lorem ipsum dolor sit"
+          description={description}
         />
         <FileRow
           status="changes"
           content={content}
           filePath="cypress/integration/command.js"
-          description="Lorem ipsum dolor sit"
+          description={description}
         />
       </div>
     ))
 
     cy.contains('cypress/integration/command.js')
-    cy.contains('Changes required').should('exist')
-    cy.get('pre').should('exist')
+    cy.contains(messages.changesRequiredLabel).should('be.visible')
+    cy.contains(messages.changesRequiredBadge).should('not.exist') // Hide badge when row is expanded
+    cy.contains(changesRequiredDescription).should('be.visible')
+    cy.get('pre').should('have.length', 2)
     cy.contains('cypress/integration/command.js').click()
-    cy.get('pre').should('not.exist')
   })
 
   it('responds nice to small screens', { viewportWidth: 500 }, () => {
+    const lorem = faker.lorem.paragraphs(3)
+
     cy.mount(() => (
       <div class="p-5 w-full">
         <FileRow
           status="changes"
           content={content}
           filePath="cypress/integration/command.js"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam at temporibus nulla ratione a nam inventore esse facere vel nemo est veniam dolore, ullam fuga quidem, cum dolor quibusdam officiis."
+          description={lorem}
         />
       </div>
     ))
 
     cy.contains('cypress/integration/command.js')
-    cy.contains('Changes required').should('exist')
+    cy.contains(messages.changesRequiredLabel).should('be.visible')
+    cy.contains(messages.changesRequiredBadge).should('not.exist')
+    cy.contains(changesRequiredDescription).should('be.visible')
     cy.get('pre').should('exist')
   })
 })
