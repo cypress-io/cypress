776b7301fd1ccedca1999ca744fbf4c67b3857a1
Blue F <blue@cypress.io>
2021-10-25 11:49:32 -0700

    feat: Add 'slowTestThreshold' and fix this.slow() inside specs (#18496)
    Co-authored-by: Chris Breiding <chrisbreiding@users.noreply.github.com>

--- packages/server/test/unit/config_spec.js
+++ packages/server/test/unit/config_spec.js
@@ -1079,6 +1079,14 @@ describe('lib/config', () => {
       }
     })
 
+    it('slowTestThreshold=10000 for e2e', function () {
+      return this.defaults('slowTestThreshold', 10000, {}, { testingType: 'e2e' })
+    })
+
+    it('slowTestThreshold=250 for component', function () {
+      return this.defaults('slowTestThreshold', 250, {}, { testingType: 'component' })
+    })
+
     it('port=null', function () {
       return this.defaults('port', null)
     })
@@ -1462,6 +1470,7 @@ describe('lib/config', () => {
             retries: { value: { runMode: 0, openMode: 0 }, from: 'default' },
             screenshotOnRunFailure: { value: true, from: 'default' },
             screenshotsFolder: { value: 'cypress/screenshots', from: 'default' },
+            slowTestThreshold: { value: 10000, from: 'default' },
             supportFile: { value: 'cypress/support', from: 'default' },
             taskTimeout: { value: 60000, from: 'default' },
             testFiles: { value: '**/*.*', from: 'default' },
@@ -1568,6 +1577,7 @@ describe('lib/config', () => {
             retries: { value: { runMode: 0, openMode: 0 }, from: 'default' },
             screenshotOnRunFailure: { value: true, from: 'default' },
             screenshotsFolder: { value: 'cypress/screenshots', from: 'default' },
+            slowTestThreshold: { value: 10000, from: 'default' },
             supportFile: { value: 'cypress/support', from: 'default' },
             taskTimeout: { value: 60000, from: 'default' },
             testFiles: { value: '**/*.*', from: 'default' },
