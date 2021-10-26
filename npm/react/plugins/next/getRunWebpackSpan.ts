115652ad834b93af04ee63e1fba152e841e23484
Pavlo Bilyk <pavlo.bilyk@gmail.com>
2021-10-26 22:09:22 +0300

    update nextjs version to 12

--- npm/react/plugins/next/getRunWebpackSpan.ts
+++ npm/react/plugins/next/getRunWebpackSpan.ts
@@ -1,23 +1,15 @@
-import type { Span } from 'next/dist/telemetry/trace/trace'
+import type { Span } from 'next/dist/trace/trace'
 
 // Starting with v11.1.1, a trace is required.
-// 'next/dist/telemetry/trace/trace' only exists since v10.0.9
+// 'next/dist/trace/trace' only exists since v12.0.0
 // and our peerDeps support back to v8 so try-catch this import
-// Starting from 12.0 trace is now located in 'next/dist/trace/trace'
 export async function getRunWebpackSpan (): Promise<{ runWebpackSpan?: Span }> {
   let trace: (name: string) => Span
 
   try {
-    try {
-      trace = await import('next/dist/telemetry/trace/trace').then((m) => m.trace)
+    trace = await import('next/dist/trace/trace').then((m) => m.trace)
 
-      return { runWebpackSpan: trace('cypress') }
-    }
-    catch (_){
-      trace = await import('next/dist/trace/trace').then((m) => m.trace)
-
-      return { runWebpackSpan: trace('cypress') }
-    }
+    return { runWebpackSpan: trace('cypress') }
   } catch (_) {
     return {}
   }
