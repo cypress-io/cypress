025ea23b6e320d00bedebdd883a3defc786add16
Chris Breiding <chrisbreiding@gmail.com>
2021-10-26 15:35:35 -0400

    refactor and add component tests

--- packages/launchpad/src/setup/OpenBrowserList.spec.tsx
+++ packages/launchpad/src/setup/OpenBrowserList.spec.tsx
@@ -5,8 +5,11 @@ import { longBrowsersList } from '@packages/frontend-shared/cypress/support/mock
 const launchButtonSelector = 'button[data-testid=launch-button]'
 
 describe('<OpenBrowserList />', () => {
-  it('renders a long list of found browsers correctly', () => {
+  beforeEach(() => {
     cy.viewport(1000, 750)
+  })
+
+  it('renders a long list of found browsers correctly', () => {
     cy.mountFragment(OpenBrowserListFragmentDoc, {
       onResult: (result) => {
         result.selectedBrowser = null
@@ -23,12 +26,30 @@ describe('<OpenBrowserList />', () => {
   })
 
   it('renders launch button when a browser is selected', () => {
-    cy.viewport(1000, 750)
     cy.mountFragment(OpenBrowserListFragmentDoc, {
       render: (gqlVal) => <div class="resize overflow-auto border-current border-1"><OpenBrowserList gql={gqlVal} /></div>,
     })
 
     cy.get(launchButtonSelector).should('be.visible').and('have.text', 'Launch Electron')
     cy.contains('button', 'different browser').should('not.exist')
+  })
+
+  it('renders browser error if there is one', () => {
+    cy.mountFragment(OpenBrowserListFragmentDoc, {
+      onResult: (result) => {
+        result.browserErrorMessage = 'There was an error'
+      },
+      render: (gqlVal) => <div class="resize overflow-auto border-current border-1"><OpenBrowserList gql={gqlVal} /></div>,
+    })
+
+    cy.contains('There was an error').should('be.visible')
+  })
+
+  it('does not render browser error if there is not one', () => {
+    cy.mountFragment(OpenBrowserListFragmentDoc, {
+      render: (gqlVal) => <div class="resize overflow-auto border-current border-1"><OpenBrowserList gql={gqlVal} /></div>,
+    })
+
+    cy.contains('[data-test-id="browser-error-message"]').should('not.exist')
   })
 })
