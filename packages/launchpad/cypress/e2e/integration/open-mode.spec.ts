393892fd93c692723c06d8af9ed439d1a8fb5776
Chris Breiding <chrisbreiding@gmail.com>
2021-10-27 14:42:02 -0400

    add e2e test

--- packages/launchpad/cypress/e2e/integration/open-mode.spec.ts
+++ packages/launchpad/cypress/e2e/integration/open-mode.spec.ts
@@ -10,6 +10,24 @@ describe('Launchpad: Open Mode', () => {
     cy.get('h1').should('contain', defaultMessages.globalPage.empty.title)
   })
 
+  it('auto-selects the browser when launched with --browser', () => {
+    cy.withCtx(async (ctx) => {
+      ctx.launchArgs.testingType = 'e2e'
+      ctx.launchArgs.browser = 'firefox'
+      await ctx.initializeData()
+    })
+
+    cy.withCtx(async (ctx, o) => {
+      await ctx.actions.project.setActiveProject(o.projectDir('todos'))
+      ctx.emitter.toLaunchpad()
+    })
+
+    cy.contains('Next Step').click()
+    cy.get('h1').should('contain', 'Choose a Browser')
+    cy.contains('Firefox').parent().should('have.class', 'border-jade-300')
+    cy.get('button[data-testid=launch-button]').invoke('text').should('include', 'Launch Firefox')
+  })
+
   describe('when there is a list of projects', () => {
     it('goes to an active project if one is added', () => {
       cy.withCtx(async (ctx, o) => {
