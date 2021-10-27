a741f4a28e4e660ce4405e0db648e70889e6ac8b
ElevateBart <ledouxb@gmail.com>
2021-10-27 17:01:44 -0500

    all test shells are ready

--- packages/app/cypress/e2e/integration/runs.spec.ts
+++ packages/app/cypress/e2e/integration/runs.spec.ts
@@ -3,10 +3,10 @@ describe('App', () => {
     cy.setupE2E('component-tests')
 
     cy.initializeApp()
-    cy.loginUser()
   })
 
   it('resolves the runs page', () => {
+    cy.loginUser()
     cy.visitApp()
     cy.wait(1000)
     cy.get('[href="#/runs"]').click()
@@ -14,6 +14,7 @@ describe('App', () => {
   })
 
   it('shows the loader', () => {
+    cy.loginUser()
     cy.remoteGraphQLIntercept(async (obj) => {
       await new Promise((resolve) => setTimeout(resolve, 200))
 
@@ -27,6 +28,7 @@ describe('App', () => {
   })
 
   it('when no runs, shows call to action', () => {
+    cy.loginUser()
     cy.remoteGraphQLIntercept(async (obj) => {
       // Currently, all remote requests go through here, we want to use this to modify the
       // remote request before it's used and avoid touching the login query
@@ -44,5 +46,22 @@ describe('App', () => {
     cy.visitApp()
     cy.get('[href="#/runs"]').click()
     cy.get('[data-cy="no-runs"]')
+  })
+
+  it('when logged out, shows call to action', () => {
+    cy.visitApp()
+    cy.get('[href="#/runs"]').click()
+    cy.contains('Connect the current user').should('exist')
+  })
+
+  it('when no project Id in the config file, shows call to action', () => {
+    cy.loginUser()
+    cy.visitApp()
+    cy.withCtx(async (ctx) => {
+      await ctx.actions.file.writeFileInProject('cypress.json', '{}')
+    })
+
+    cy.get('[href="#/runs"]').click()
+    cy.contains('Connect the current project').should('exist')
   })
 })
--- packages/app/src/runs/RunsConnect.vue
+++ packages/app/src/runs/RunsConnect.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Connect the current project to the cloud (Placeholder)</div>
+  <div v-if="!isLoggedIn">
+    Connect the current user to the cloud (Placeholder)
+  </div>
+  <div v-else-if="!hasProjectId">
+    Connect the current project to the cloud (Placeholder)
+  </div>
+  <div v-else>
+    Something went wrong
+  </div>
 </template>
 
 <script lang="ts" setup>
