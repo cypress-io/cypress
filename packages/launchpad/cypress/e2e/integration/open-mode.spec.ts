55c46aadb9dc9832b301043d91e9a4d3c6341e6d
BlueWinds <blue@everblue.info>
2021-10-27 09:50:33 -0700

    Add e2e tests for testingType launchArgs

--- packages/launchpad/cypress/e2e/integration/open-mode.spec.ts
+++ packages/launchpad/cypress/e2e/integration/open-mode.spec.ts
@@ -6,8 +6,42 @@ describe('Launchpad: Open Mode', () => {
     cy.visitLaunchpad()
   })
 
-  it('Shows the open page', () => {
+  it('shows the open page when no testingType is not specified', () => {
     cy.get('h1').should('contain', defaultMessages.globalPage.empty.title)
+  })
+
+  it('goes directly to e2e tests when launched with --e2e', () => {
+    cy.withCtx(async (ctx) => {
+      // Though the data context is previously initialized,
+      // we re-initialize it here so that it reflects the new launchArg
+      ctx.launchArgs.testingType = 'e2e'
+      await ctx.initializeData()
+    })
+
+    cy.withCtx(async (ctx, o) => {
+      await ctx.actions.project.setActiveProject(o.projectDir('todos'))
+      ctx.emitter.toLaunchpad()
+    })
+
+    // e2e testing is configured for the todo project, so we don't expect an error.
+    cy.get('h1').should('contain', 'Initializing Config...')
+  })
+
+  it('goes directly to component tests when launched with --component', () => {
+    cy.withCtx(async (ctx) => {
+      // Though the data context is previously initialized,
+      // we re-initialize it here so that it reflects the new launchArg
+      ctx.launchArgs.testingType = 'component'
+      await ctx.initializeData()
+    })
+
+    cy.withCtx(async (ctx, o) => {
+      await ctx.actions.project.setActiveProject(o.projectDir('todos'))
+      ctx.emitter.toLaunchpad()
+    })
+
+    // Component testing is not configured for the todo project
+    cy.get('h1').should('contain', 'Cypress Configuration Error')
   })
 
   describe('when there is a list of projects', () => {
