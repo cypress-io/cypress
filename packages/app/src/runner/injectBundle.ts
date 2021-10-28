90c6859ca232e164057a9631282b6d01b4e04211
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-28 12:43:00 +1000

    state in mobx

--- packages/app/src/runner/injectBundle.ts
+++ packages/app/src/runner/injectBundle.ts
@@ -1,4 +1,4 @@
-import { initializeMobxStore } from '../store'
+import { initializeMobxStore, useAutStore } from '../store'
 
 export async function injectBundle () {
   const src = '/__cypress/runner/cypress_runner.js'
@@ -29,12 +29,18 @@ export async function injectBundle () {
       // just stick config on window until we figure out how we are
       // going to manage it
       const config = window.UnifiedRunner.decodeBase64(data.base64Config) as any
+      const autStore = useAutStore()
+
+      autStore.updateDimensions(config.viewportWidth, config.viewportHeight)
 
       window.UnifiedRunner.config = config
 
       window.UnifiedRunner.MobX.runInAction(() => {
         const store = initializeMobxStore(window.UnifiedRunner.config.testingType)
 
+        // TODO(lachlan): use GraphQL to get and set the configuration once
+        // it is more practical to do so
+        // find out if we need to continue managing viewportWidth/viewportHeight in MobX at all.
         store.updateDimensions(config.viewportWidth, config.viewportHeight)
       })
 
