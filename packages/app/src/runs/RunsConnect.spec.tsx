cad38cb0ef82f2187f0e75ccea5b9235adcc8b4e
ElevateBart <ledouxb@gmail.com>
2021-10-28 15:18:25 -0500

    fix some typescript

--- packages/app/src/runs/RunsConnect.spec.tsx
+++ packages/app/src/runs/RunsConnect.spec.tsx
@@ -1,17 +1,28 @@
 import RunsConnect from './RunsConnect.vue'
 
+const gql = {
+  app: {
+    isAuthBrowserOpened: true,
+  },
+  cloudViewer: {
+    id: '2',
+    email: 't@me.com',
+    fullName: 'John Appleseed',
+  },
+}
+
 describe('<RunsConnect />', () => {
   it('playground', () => {
-    cy.mount(<RunsConnect isLoggedIn={false} />)
+    cy.mount(<RunsConnect isLoggedIn={false} gql={gql} />)
   })
 
   it('show user connect if not connected', () => {
-    cy.mount(<RunsConnect isLoggedIn={false} />)
+    cy.mount(<RunsConnect isLoggedIn={false} gql={gql}/>)
     cy.contains('button', 'Log in').should('be.visible')
   })
 
   it('show project connect if not connected', () => {
-    cy.mount(<RunsConnect isLoggedIn={true} />)
+    cy.mount(<RunsConnect isLoggedIn={true} gql={gql}/>)
     cy.contains('button', 'Connect your project').should('be.visible')
   })
 })
