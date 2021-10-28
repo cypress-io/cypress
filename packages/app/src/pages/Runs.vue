a5c3b50763125fa558c208c03f0245aa5600dbe1
ElevateBart <ledouxb@gmail.com>
2021-10-28 14:53:46 -0500

    fix scroll

--- packages/app/src/pages/Runs.vue
+++ packages/app/src/pages/Runs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative m-24px h-full">
+  <div class="relative p-24px h-full overflow-y-scroll">
     <transition
       v-if="query.fetching.value || activeProject?.cloudProject?.runs?.nodes"
       name="fade"
