6b49cee589254587c243edcf9faaf92aa914d5ca
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-28 12:52:13 +1000

    revert

--- packages/app/cypress/e2e/integration/files.spec.ts
+++ packages/app/cypress/e2e/integration/files.spec.ts
@@ -11,6 +11,7 @@ describe('App', () => {
 
   it('resolves the home page', () => {
     cy.visitApp()
+    cy.wait(1000)
     cy.get('[href="#/runs"]').click()
     cy.get('[href="#/settings"]').click()
   })
