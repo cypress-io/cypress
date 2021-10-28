281f28706707b8fc74a7d74ad15806d485a77e51
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-10-28 11:27:39 +1000

    update state

--- packages/app/src/spec/SpecRunnerHeader.vue
+++ packages/app/src/spec/SpecRunnerHeader.vue
@@ -8,9 +8,19 @@
       Selector
     </button>
 
-    <div class="rounded-md bg-white flex shadow-md mx-2 url px-4">
-      URL: {{ autStore.url }}
+    <div
+      class="rounded-md flex shadow-md mx-2 url px-4"
+      :class="{
+        'bg-yellow-50': autStore.isLoadingUrl,
+        'bg-white': !autStore.isLoadingUrl,
+      }"
+    >
+      <div>
+        {{ autStore.url }}
+      </div>
     </div>
+
+    <div>Loading URL: {{ autStore.isLoadingUrl }}</div>
 
     <Select
       v-model="browser"
@@ -21,11 +31,11 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
-import { useAutStore } from '../store';
+import { computed } from 'vue'
+import { useAutStore } from '../store'
 import Select from '@packages/frontend-shared/src/components/Select.vue'
 import { gql } from '@urql/vue'
-import type { SpecRunnerHeaderFragment } from '../generated/graphql';
+import type { SpecRunnerHeaderFragment } from '../generated/graphql'
 
 gql`
 fragment SpecRunnerHeader on App {
@@ -47,14 +57,14 @@ const props = defineProps<{
 
 const browser = computed(() => {
   if (!props.gql.selectedBrowser) {
-    return 
+    return
   }
 
   const dimensions = `${autStore.viewportDimensions.width}x${autStore.viewportDimensions.height}`
 
   return {
     id: props.gql.selectedBrowser.id,
-    name: `${props.gql.selectedBrowser.displayName} ${dimensions}`
+    name: `${props.gql.selectedBrowser.displayName} ${dimensions}`,
   }
 })
 
@@ -67,11 +77,11 @@ const isDisabled = computed(() => autStore.isRunning || autStore.isLoading)
 
 <style scoped lang="scss">
 button, .url {
-  @apply rounded-md bg-white flex shadow-md ml-2;
   @apply flex items-center justify-center;
 }
 
 button {
+  @apply rounded-md bg-white flex shadow-md ml-2;
   @apply w-20 hover:bg-gray-50;
 }
 </style>
