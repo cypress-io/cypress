e17a044e136d2d549336d9df82a405f0c43806e2
Tim Griesser <tgriesser10@gmail.com>
2021-10-29 10:37:56 -0400

    feat: prerendering data for app load

---
+++ packages/data-context/src/sources/HtmlDataSource.ts
@@ -0,0 +1,56 @@
+/**
+ * Forms the HTML pages rendered to the client for Component / E2E testing,
+ * including the pre-hydration we use to bootstrap the script data for fast
+ * initial loading
+ */
+import type { DataContextShell } from '../DataContextShell'
+import { getPathToDist } from '@packages/resolve-dist'
+
+export class HtmlDataSource {
+  constructor (private ctx: DataContextShell) {}
+
+  async fetchAppInitialData () {
+    await Promise.all([
+      this.ctx.graphql.executeQuery('AppQueryDocument', {}),
+      this.ctx.graphql.executeQuery('NewSpec_NewSpecQueryDocument', {}),
+      this.ctx.graphql.executeQuery('ProjectSettingsDocument', {}),
+      this.ctx.graphql.executeQuery('SpecsPageContainerDocument', {}),
+      this.ctx.graphql.executeQuery('HeaderBar_HeaderBarQueryDocument', {}),
+    ])
+
+    return this.ctx.graphql.getSSRData()
+  }
+
+  async fetchAppHtml () {
+    if (this.ctx.env.CYPRESS_INTERNAL_VITE_APP_PORT) {
+      const response = await this.ctx.util.fetch(`http://localhost:${process.env.CYPRESS_INTERNAL_VITE_APP_PORT}/`, { method: 'GET' })
+      const html = await response.text()
+
+      return html
+    }
+
+    return this.ctx.fs.readFile(getPathToDist('app'), 'utf8')
+  }
+
+  /**
+   * The app html includes the SSR'ed data to bootstrap the page for the app
+   */
+  async appHtml () {
+    const [appHtml, appInitialData] = await Promise.all([
+      this.fetchAppHtml(),
+      this.fetchAppInitialData(),
+    ])
+
+    return this.replaceBody(appHtml, appInitialData)
+  }
+
+  private replaceBody (html: string, initialData: object) {
+    return html.replace('<body>', `
+      <body>
+        <script>
+          window.__CYPRESS_GRAPHQL_PORT__ = ${JSON.stringify(this.ctx.gqlServerPort)};
+          window.__CYPRESS_INITIAL_DATA__ = ${JSON.stringify(initialData)};
+        </script>
+    `)
+  }
+}
