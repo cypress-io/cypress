2b0be873e7b0d86d901de1b8a46cf983d68c0dd7
estrada9166 <estrada9166@hotmail.com>
2021-10-30 10:43:11 -0500

    Fix tests

---
+++ system-tests/lib/support/helpers/electron_stub.js
@@ -0,0 +1,11 @@
+// a stubbed out version of electron
+// for using in all of our tests :-)
+module.exports = {
+  shell: {},
+  app: {
+    dock: {
+      show () {},
+      hide () {},
+    },
+  },
+}
