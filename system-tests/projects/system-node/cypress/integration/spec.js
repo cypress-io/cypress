21815fb73b7f327e9ceff6c7960eaea2bb495c8f
mjhenkes <mjhenkes@gmail.com>
2021-11-01 16:17:54 -0500

    try to fix system test

--- system-tests/projects/system-node/cypress/integration/spec.js
+++ system-tests/projects/system-node/cypress/integration/spec.js
@@ -1,13 +1,8 @@
 it('has expected resolvedNodePath and resolvedNodeVersion', () => {
   expect(Cypress.config('nodeVersion')).to.eq('system')
 
-  // TODO: make this assertion a strict equality check again
-  // for some reason, `yarn` is messing up the node path in CI and making it `/tmp/yarn---12345/node`
-  // but the e2e test does not get that same node path for `expectedNodePath`
-  // however since it still works as it should this is fine - this is an issue with the tests
-  // expect(Cypress.config('resolvedNodePath')).to.eq(Cypress.env('expectedNodePath'))
-  expect(Cypress.config('resolvedNodePath')).to.match(/.*\/node$/)
-  expect(Cypress.env('expectedNodePath')).to.match(/.*\/node$/)
+  // Since this test is not run via the cli, resolvedNodePath will always be undefined and fall back to bundled node.
+  expect(Cypress.config('resolvedNodePath')).to.be.undefined
 
   expect(Cypress.config('resolvedNodeVersion')).to.eq(Cypress.env('expectedNodeVersion'))
 })
