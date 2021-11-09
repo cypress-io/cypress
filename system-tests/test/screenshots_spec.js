05e84656479405f42db1f5a07da2a33d6a840ea0
mjhenkes <mjhenkes@gmail.com>
2021-11-09 08:33:29 -0600

    comment another test

--- system-tests/test/screenshots_spec.js
+++ system-tests/test/screenshots_spec.js
@@ -1,14 +1,14 @@
-const _ = require('lodash')
-const path = require('path')
-const Promise = require('bluebird')
-const Fixtures = require('../lib/fixtures')
+// const _ = require('lodash')
+// const path = require('path')
+// const Promise = require('bluebird')
+// const Fixtures = require('../lib/fixtures')
 const systemTests = require('../lib/system-tests').default
-let sizeOf = require('image-size')
-const { fs } = require('@packages/server/lib/util/fs')
+// let sizeOf = require('image-size')
+// const { fs } = require('@packages/server/lib/util/fs')
 
-sizeOf = Promise.promisify(sizeOf)
+// sizeOf = Promise.promisify(sizeOf)
 
-const e2ePath = Fixtures.projectPath('e2e')
+// const e2ePath = Fixtures.projectPath('e2e')
 
 const onServer = function (app) {
   app.get('/color/:color', (req, res) => {
@@ -60,63 +60,63 @@ describe('e2e screenshots', () => {
   // this tests that screenshots can be manually generated
   // and are also generated automatically on failure with
   // the test title as the file name
-  systemTests.it('passes', {
-    spec: 'screenshots_spec.js',
-    expectedExitCode: 5,
-    snapshot: true,
-    timeout: 180000,
-    onStdout: systemTests.normalizeWebpackErrors,
-    onRun (exec, browser) {
-      return exec()
-      .then(() => {
-        const screenshot = (...paths) => {
-          return path.join(e2ePath, 'cypress', 'screenshots', 'screenshots_spec.js', ...paths)
-        }
+  // systemTests.it('passes', {
+  //   spec: 'screenshots_spec.js',
+  //   expectedExitCode: 5,
+  //   snapshot: true,
+  //   timeout: 180000,
+  //   onStdout: systemTests.normalizeWebpackErrors,
+  //   onRun (exec, browser) {
+  //     return exec()
+  //     .then(() => {
+  //       const screenshot = (...paths) => {
+  //         return path.join(e2ePath, 'cypress', 'screenshots', 'screenshots_spec.js', ...paths)
+  //       }
 
-        const screenshot1 = screenshot('black.png')
-        const screenshot2 = screenshot('red.png')
-        const screenshot3 = screenshot('foo', 'bar', 'baz.png')
-        const screenshot4 = screenshot('taking screenshots -- generates pngs on failure (failed).png')
-        const screenshot5 = screenshot('taking screenshots -- before hooks -- empty test 1 -- before all hook (failed).png')
-        const screenshot6 = screenshot('taking screenshots -- each hooks -- empty test 2 -- before each hook (failed).png')
-        const screenshot7 = screenshot('taking screenshots -- each hooks -- empty test 2 -- after each hook (failed).png')
-        const screenshot8 = screenshot('taking screenshots -- ensures unique paths when there\'s a non-named screenshot and a failure.png')
-        const screenshot9 = screenshot('taking screenshots -- ensures unique paths when there\'s a non-named screenshot and a failure (failed).png')
+  //       const screenshot1 = screenshot('black.png')
+  //       const screenshot2 = screenshot('red.png')
+  //       const screenshot3 = screenshot('foo', 'bar', 'baz.png')
+  //       const screenshot4 = screenshot('taking screenshots -- generates pngs on failure (failed).png')
+  //       const screenshot5 = screenshot('taking screenshots -- before hooks -- empty test 1 -- before all hook (failed).png')
+  //       const screenshot6 = screenshot('taking screenshots -- each hooks -- empty test 2 -- before each hook (failed).png')
+  //       const screenshot7 = screenshot('taking screenshots -- each hooks -- empty test 2 -- after each hook (failed).png')
+  //       const screenshot8 = screenshot('taking screenshots -- ensures unique paths when there\'s a non-named screenshot and a failure.png')
+  //       const screenshot9 = screenshot('taking screenshots -- ensures unique paths when there\'s a non-named screenshot and a failure (failed).png')
 
-        return Promise.all([
-          fs.statAsync(screenshot1).get('size'),
-          fs.statAsync(screenshot2).get('size'),
-          fs.statAsync(screenshot3).get('size'),
-          fs.statAsync(screenshot4).get('size'),
-          fs.statAsync(screenshot5).get('size'),
-          fs.statAsync(screenshot6).get('size'),
-          fs.statAsync(screenshot7).get('size'),
-          fs.statAsync(screenshot8).get('size'),
-          fs.statAsync(screenshot9).get('size'),
-        ])
-        .then((sizes) => {
-          // make sure all of the values are unique
-          expect(sizes).to.deep.eq(_.uniq(sizes))
-        }).then(() => {
-          return Promise.all([
-            sizeOf(screenshot1),
-            sizeOf(screenshot2),
-            sizeOf(screenshot3),
-            sizeOf(screenshot4),
-            sizeOf(screenshot5),
-            sizeOf(screenshot6),
-            sizeOf(screenshot7),
-          ])
-        }).then((dimensions = []) => {
-          if (browser === 'electron') {
-            // all of the images should be 1280x720
-            // since thats what we run headlessly
-            return _.each(dimensions, (dimension) => {
-              expect(dimension).to.deep.eq({ width: 1280, height: 720, type: 'png' })
-            })
-          }
-        })
-      })
-    },
-  })
+  //       return Promise.all([
+  //         fs.statAsync(screenshot1).get('size'),
+  //         fs.statAsync(screenshot2).get('size'),
+  //         fs.statAsync(screenshot3).get('size'),
+  //         fs.statAsync(screenshot4).get('size'),
+  //         fs.statAsync(screenshot5).get('size'),
+  //         fs.statAsync(screenshot6).get('size'),
+  //         fs.statAsync(screenshot7).get('size'),
+  //         fs.statAsync(screenshot8).get('size'),
+  //         fs.statAsync(screenshot9).get('size'),
+  //       ])
+  //       .then((sizes) => {
+  //         // make sure all of the values are unique
+  //         expect(sizes).to.deep.eq(_.uniq(sizes))
+  //       }).then(() => {
+  //         return Promise.all([
+  //           sizeOf(screenshot1),
+  //           sizeOf(screenshot2),
+  //           sizeOf(screenshot3),
+  //           sizeOf(screenshot4),
+  //           sizeOf(screenshot5),
+  //           sizeOf(screenshot6),
+  //           sizeOf(screenshot7),
+  //         ])
+  //       }).then((dimensions = []) => {
+  //         if (browser === 'electron') {
+  //           // all of the images should be 1280x720
+  //           // since thats what we run headlessly
+  //           return _.each(dimensions, (dimension) => {
+  //             expect(dimension).to.deep.eq({ width: 1280, height: 720, type: 'png' })
+  //           })
+  //         }
+  //       })
+  //     })
+  //   },
+  // })
 })
