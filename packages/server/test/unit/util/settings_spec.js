86f1cade3ddf620fdf6fd86de167baf4ea06318e
Tim Griesser <tgriesser10@gmail.com>
2021-11-15 14:47:11 -0500

    Revert setting_spec

--- packages/server/test/unit/util/settings_spec.js
+++ packages/server/test/unit/util/settings_spec.js
@@ -1,44 +1,24 @@
 const path = require('path')
-const { makeLegacyDataContext, clearLegacyDataContext } = require('../../../lib/makeDataContext')
 
 require('../../spec_helper')
 const { fs } = require('../../../lib/util/fs')
 const settings = require(`../../../lib/util/settings`)
-const Fixtures = require('@tooling/system-tests/lib/fixtures')
 
+const projectRoot = process.cwd()
 const defaultOptions = {
   configFile: 'cypress.config.js',
 }
 
 describe('lib/util/settings', () => {
-  let ctx
-  let projectRoot
-
-  beforeEach(() => {
-    ctx = makeLegacyDataContext()
-
-    Fixtures.scaffold()
-
-    projectRoot = Fixtures.projectPath('empty-folders')
-  })
-
-  afterEach(() => {
-    clearLegacyDataContext()
-    Fixtures.remove()
-  })
-
   context('with default configFile option', () => {
     beforeEach(function () {
       this.setup = (obj = {}) => {
-        return fs.writeFileAsync(path.join(projectRoot, 'cypress.config.js'), `module.exports = ${JSON.stringify(obj)}`)
-        .then(() => {
-          return ctx.actions.project.setActiveProject(projectRoot)
-        })
+        return fs.writeFileAsync('cypress.config.js', `module.exports = ${JSON.stringify(obj)}`)
       }
     })
 
     afterEach(() => {
-      return fs.removeAsync(path.join(projectRoot, 'cypress.config.js'))
+      return fs.removeAsync('cypress.config.js')
     })
 
     context('nested cypress object', () => {
@@ -58,13 +38,13 @@ describe('lib/util/settings', () => {
 
     context('.readEnv', () => {
       afterEach(() => {
-        return fs.removeAsync(path.join(projectRoot, 'cypress.env.json'))
+        return fs.removeAsync('cypress.env.json')
       })
 
       it('parses json', () => {
         const json = { foo: 'bar', baz: 'quux' }
 
-        fs.writeJsonSync(path.join(projectRoot, 'cypress.env.json'), json)
+        fs.writeJsonSync('cypress.env.json', json)
 
         return settings.readEnv(projectRoot)
         .then((obj) => {
@@ -73,7 +53,7 @@ describe('lib/util/settings', () => {
       })
 
       it('throws when invalid json', () => {
-        fs.writeFileSync(path.join(projectRoot, 'cypress.env.json'), '{\'foo;: \'bar}')
+        fs.writeFileSync('cypress.env.json', '{\'foo;: \'bar}')
 
         return settings.readEnv(projectRoot)
         .catch((err) => {
@@ -89,7 +69,7 @@ describe('lib/util/settings', () => {
         .then((obj) => {
           expect(obj).to.deep.eq({})
         }).then(() => {
-          return fs.pathExists(path.join(projectRoot, 'cypress.env.json'))
+          return fs.pathExists('cypress.env.json')
         }).then((found) => {
           expect(found).to.be.false
         })
@@ -97,19 +77,22 @@ describe('lib/util/settings', () => {
     })
 
     context('.id', () => {
-      const emptyFolderPath = Fixtures.projectPath('empty-folders')
+      beforeEach(function () {
+        this.projectRoot = path.join(projectRoot, '_test-output/path/to/project/')
+
+        return fs.ensureDirAsync(this.projectRoot)
+      })
 
       afterEach(function () {
-        return fs.removeAsync(path.join(emptyFolderPath, 'cypress.config.js'))
+        return fs.removeAsync(`${this.projectRoot}cypress.config.js`)
       })
 
       it('returns project id for project', function () {
-        return fs.writeFileAsync(path.join(emptyFolderPath, 'cypress.config.js'), `module.exports = { projectId: 'id-123' }`)
+        return fs.writeFileAsync(`${this.projectRoot}cypress.config.js`, `module.exports = {
+          projectId: 'id-123',
+        }`)
         .then(() => {
-          return ctx.actions.project.setActiveProject(emptyFolderPath)
-        })
-        .then(() => {
-          return settings.id(emptyFolderPath, defaultOptions)
+          return settings.id(this.projectRoot, defaultOptions)
         }).then((id) => {
           expect(id).to.equal('id-123')
         })
@@ -154,11 +137,11 @@ describe('lib/util/settings', () => {
       })
 
       it('renames supportFolder -> supportFile', function () {
-        return this.setup({ supportFolder: 'support', foo: 'bar' })
+        return this.setup({ supportFolder: 'foo', foo: 'bar' })
         .then(() => {
           return settings.read(projectRoot, defaultOptions)
         }).then((obj) => {
-          expect(obj).to.deep.eq({ supportFile: 'support', foo: 'bar' })
+          expect(obj).to.deep.eq({ supportFile: 'foo', foo: 'bar' })
         })
       })
 
@@ -181,13 +164,13 @@ describe('lib/util/settings', () => {
       })
 
       it('errors if in run mode and can\'t find file', function () {
-        return settings.read(projectRoot, { configFile: 'foo.config.js', args: { runProject: 'path' } })
+        return settings.read(projectRoot, { ...defaultOptions, args: { runProject: 'path' } })
         .then(() => {
           throw Error('read should have failed with no config file in run mode')
         }).catch((err) => {
-          expect(err.type).to.equal('ERROR_READING_FILE')
+          expect(err.type).to.equal('CONFIG_FILE_NOT_FOUND')
 
-          return fs.access(path.join(projectRoot, 'foo.config.js'))
+          return fs.access(path.join(projectRoot, 'cypress.config.js'))
           .then(() => {
             throw Error('file should not have been created here')
           }).catch((err) => {
@@ -200,17 +183,16 @@ describe('lib/util/settings', () => {
     context('.write', () => {
       it('promises cypress.config.js updates', function () {
         return this.setup().then(() => {
-          return settings.writeOnly(projectRoot, { foo: 'bar' }, defaultOptions)
+          return settings.write(projectRoot, { foo: 'bar' }, defaultOptions)
         }).then((obj) => {
           expect(obj).to.deep.eq({ foo: 'bar' })
         })
       })
 
-      // TODO: (Alejandro) - update when we write correctly to config files
-      it.skip('only writes over conflicting keys', function () {
+      it('only writes over conflicting keys', function () {
         return this.setup({ projectId: '12345', autoOpen: true })
         .then(() => {
-          return settings.writeOnly(projectRoot, { projectId: 'abc123' }, defaultOptions)
+          return settings.write(projectRoot, { projectId: 'abc123' }, defaultOptions)
         }).then((obj) => {
           expect(obj).to.deep.eq({ projectId: 'abc123', autoOpen: true })
         })
@@ -228,7 +210,7 @@ describe('lib/util/settings', () => {
     })
 
     it('.write does not create a file', function () {
-      return settings.writeOnly(this.projectRoot, {}, this.options)
+      return settings.write(this.projectRoot, {}, this.options)
       .then(() => {
         return fs.access(path.join(this.projectRoot, 'cypress.config.js'))
         .then(() => {
@@ -249,17 +231,16 @@ describe('lib/util/settings', () => {
 
   context('with js files', () => {
     it('.read returns from configFile when its a JavaScript file', function () {
-      return fs.writeFileAsync(path.join(projectRoot, 'cypress.config.js'), `module.exports = { baz: 'lurman' }`)
+      this.projectRoot = path.join(projectRoot, '_test-output/path/to/project/')
+
+      return fs.writeFile(path.join(this.projectRoot, 'cypress.custom.js'), `module.exports = { baz: 'lurman' }`)
       .then(() => {
-        return ctx.actions.project.setActiveProject(projectRoot)
-      })
-      .then(() => {
-        return settings.read(projectRoot, { configFile: 'cypress.config.js' })
-      })
-      .then((settings) => {
-        expect(settings).to.deep.equal({ baz: 'lurman' })
-      }).then(() => {
-        return fs.remove(path.join(projectRoot, 'cypress.config.js'))
+        return settings.read(this.projectRoot, { configFile: 'cypress.custom.js' })
+        .then((settings) => {
+          expect(settings).to.deep.equal({ baz: 'lurman' })
+        }).then(() => {
+          return fs.remove(path.join(this.projectRoot, 'cypress.custom.js'))
+        })
       })
     })
   })
