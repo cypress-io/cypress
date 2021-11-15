0579ef6516a12f1c955be8e6b3f8a0f131a193e2
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-11-15 22:02:38 +1000

    add tree list to spec page

---
+++ packages/app/src/specs/SpecsListRowItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="grid grid-cols-2 outline-none children:cursor-pointer group h-full"
+    data-testid="specs-list-row"
+    role="link"
+    tabindex="0"
+  >
+    <div>
+      <slot name="file" />
+    </div>
+
+    <div>
+      <slot name="git-info" />
+    </div>
+  </div>
+</template>
