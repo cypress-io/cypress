90f33fecd1c201c0f09431f834d4151e0d91e7c9
mjhenkes <mjhenkes@gmail.com>
2021-11-15 11:07:47 -0500

    enable proxy logging spec

--- packages/driver/cypress/integration/cypress/proxy-logging-spec.ts
+++ packages/driver/cypress/integration/cypress/proxy-logging-spec.ts
@@ -103,41 +103,41 @@ describe('Proxy Logging', () => {
     })
 
     // @see https://github.com/cypress-io/cypress/issues/17656
-    // it('xhr log has response body/status code', (done) => {
-    //   cy.window()
-    //   .then((win) => {
-    //     cy.on('log:changed', (log) => {
-    //       try {
-    //         expect(log.snapshots.map((v) => v.name)).to.deep.eq(['request', 'response'])
-    //         expect(log.consoleProps['Response Headers']).to.include({
-    //           'x-powered-by': 'Express',
-    //         })
+    it('xhr log has response body/status code', (done) => {
+      cy.window()
+      .then((win) => {
+        cy.on('log:changed', (log) => {
+          try {
+            expect(log.snapshots.map((v) => v.name)).to.deep.eq(['request', 'response'])
+            expect(log.consoleProps['Response Headers']).to.include({
+              'x-powered-by': 'Express',
+            })
 
-    //         expect(log.consoleProps['Response Body']).to.include('Cannot GET /some-url')
-    //         expect(log.consoleProps['Response Status Code']).to.eq(404)
+            expect(log.consoleProps['Response Body']).to.include('Cannot GET /some-url')
+            expect(log.consoleProps['Response Status Code']).to.eq(404)
 
-    //         expect(log.renderProps).to.include({
-    //           indicator: 'bad',
-    //           message: 'GET 404 /some-url',
-    //         })
+            expect(log.renderProps).to.include({
+              indicator: 'bad',
+              message: 'GET 404 /some-url',
+            })
 
-    //         expect(Object.keys(log.consoleProps)).to.deep.eq(
-    //           ['Event', 'Resource Type', 'Method', 'URL', 'Request went to origin?', 'XHR', 'groups', 'Request Headers', 'Response Status Code', 'Response Headers', 'Response Body'],
-    //         )
+            expect(Object.keys(log.consoleProps)).to.deep.eq(
+              ['Event', 'Resource Type', 'Method', 'URL', 'Request went to origin?', 'XHR', 'groups', 'Request Headers', 'Response Status Code', 'Response Headers', 'Response Body'],
+            )
 
-    //         done()
-    //       } catch (err) {
-    //         // eslint-disable-next-line no-console
-    //         console.log('assertion error, retrying', err)
-    //       }
-    //     })
+            done()
+          } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log('assertion error, retrying', err)
+          }
+        })
 
-    //     const xhr = new win.XMLHttpRequest()
+        const xhr = new win.XMLHttpRequest()
 
-    //     xhr.open('GET', '/some-url')
-    //     xhr.send()
-    //   })
-    // })
+        xhr.open('GET', '/some-url')
+        xhr.send()
+      })
+    })
 
     it('does not log an unintercepted non-xhr/fetch request', (done) => {
       const img = new Image()
