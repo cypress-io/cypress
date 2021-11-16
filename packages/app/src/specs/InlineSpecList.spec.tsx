033bd555d0bf9d9126c85a5f05e7a509e85ed565
Mark Noonan <mark@cypress.io>
2021-11-16 13:50:03 -0500

    add component test for inline spec toggle button interaction

--- packages/app/src/specs/InlineSpecList.spec.tsx
+++ packages/app/src/specs/InlineSpecList.spec.tsx
@@ -13,15 +13,31 @@ describe('InlineSpecList', () => {
       },
       render: (gqlValue) => {
         return (
-          <div class="bg-gray-1000">
-            <InlineSpecList gql={gqlValue}></InlineSpecList>
+          <div >
+            <div id="unified-runner-vue-wrapper">
+              <div id="focus-tests-vue-teleport-target"></div>
+            </div>
+            <div class="bg-gray-1000">
+              <InlineSpecList gql={gqlValue}></InlineSpecList>
+            </div>
           </div>
         )
       },
     })
   })
 
-  it('should render a list of spec', () => {
+  it('should render a list of specs', () => {
     cy.get('a').should('exist').and('have.length', specs.length)
+  })
+
+  it('can be opened/closed with an external button if teleport target exists', () => {
+    // selecting with an ID as this is what associates the button with what it controls
+    const specListSelector = '[id=inline-spec-list-aria-id]'
+
+    cy.get(specListSelector).should('be.visible')
+    cy.contains('button', 'Specs').click()
+    cy.get(specListSelector).should('not.exist')
+    cy.contains('button', 'Specs').click()
+    cy.get(specListSelector).should('be.visible')
   })
 })
