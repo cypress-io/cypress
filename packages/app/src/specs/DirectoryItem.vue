651b91b247675b772b1cf300d1f1a7562ec90b39
Cesar <cesaravitia@outlook.com>
2021-11-16 06:18:12 -0700

    feat: implement spec list tree (#18901)
    * feat: create spec tree
    
    * improve types
    
    * feat(app): add tree list to spec page (#18913)
    
    Co-authored-by: Zachary Williams <zachjw34@gmail.com>
    
    * address comments
    
    * update style
    
    * better types
    
    * remove unused code
    
    * remove old code
    
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>
    Co-authored-by: Zachary Williams <zachjw34@gmail.com>

---
+++ packages/app/src/specs/DirectoryItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex items-center py-4px text-sm">
+    <i-cy-chevron-down-small_x16
+      class="icon-dark-gray-700 mr-8px group-hocus:(icon-dark-gray-300) group-hover:children:(transition-all ease-in-out duration-250)"
+      :class="{'transform rotate-270': !expanded}"
+    />
+    <i-cy-folder_x16 class="mr-8px w-16px h-16px" />
+    <template
+      v-for="(directory, idx) in directories"
+      :key="directory"
+    >
+      <span class="font-medium text-gray-400">{{ directory }}</span>
+      <span
+        v-if="idx !== directories.length - 1"
+        class="font-medium text-gray-800 px-4px"
+      >
+        /
+      </span>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  directories: string[],
+  expanded: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  directories: () => [],
+  expanded: false,
+})
+</script>
