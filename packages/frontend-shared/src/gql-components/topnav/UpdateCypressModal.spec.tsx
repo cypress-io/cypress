878fbec46b28b62c34ee92d72304a1414486c97b
Mark Noonan <mark@cypress.io>
2021-11-17 11:43:34 -0500

    add more detail to test for update modal

--- packages/frontend-shared/src/gql-components/topnav/UpdateCypressModal.spec.tsx
+++ packages/frontend-shared/src/gql-components/topnav/UpdateCypressModal.spec.tsx
@@ -1,15 +1,26 @@
 import UpdateCypressModal from './UpdateCypressModal.vue'
+import { defaultMessages } from '@cy/i18n'
 
 describe('<UpdateCypressModal />', { viewportWidth: 1000, viewportHeight: 750 }, () => {
-  it('renders', () => {
+  it('renders expected content & emits expected events', () => {
+    const installedVersion = '8.2.0'
+    const latestVersion = '10.0.0'
+
     cy.mount(UpdateCypressModal, {
       props: {
-        installedVersion: '8.2.0',
-        latestVersion: '10.0.0',
+        installedVersion,
+        latestVersion,
         show: true,
       },
     })
 
-    cy.contains('Paste the command')
+    cy.contains(`${defaultMessages.topNav.updateCypress.title} ${latestVersion}`).should('be.visible')
+    cy.contains(`${defaultMessages.topNav.updateCypress.currentlyRunning}`.replace('{0}', installedVersion)).should('be.visible')
+    cy.contains(`${defaultMessages.topNav.updateCypress.pasteToUpgrade}`).should('be.visible')
+    cy.contains('Remember to close').should('be.visible')
+    cy.findByLabelText('Close').click().then(() => {
+      cy.wrap(Cypress.vueWrapper.emitted('close')?.[0])
+      .should('deep.equal', [])
+    })
   })
 })
