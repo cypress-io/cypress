9587c303885107d1316a4a31320389593880568f
Mark Noonan <mark@cypress.io>
2021-11-17 15:33:21 -0500

    remove unnecessary span

--- packages/frontend-shared/src/components/InlineCodeFragment.vue
+++ packages/frontend-shared/src/components/InlineCodeFragment.vue
@@ -1,7 +1,5 @@
 <template>
-  <code>
-    <span class="text-purple-600 bg-purple-100 rounded p-2px">
-      <slot />
-    </span>
+  <code class="text-purple-600 bg-purple-100 rounded p-2px">
+    <slot />
   </code>
 </template>
