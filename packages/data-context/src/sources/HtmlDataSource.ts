866264b3af0584b13b750dc8d813a338f5b23e89
Tim Griesser <tgriesser10@gmail.com>
2021-11-18 11:10:48 -0500

    fix: CYPRESS_INTERNAL_VITE_DEV for development

--- packages/data-context/src/sources/HtmlDataSource.ts
+++ packages/data-context/src/sources/HtmlDataSource.ts
@@ -35,7 +35,28 @@ export class HtmlDataSource {
   }
 
   async fetchAppHtml () {
-    return this.ctx.fs.readFile(getPathToDist('app', 'index.html'), 'utf8')
+    if (process.env.CYPRESS_INTERNAL_VITE_DEV) {
+      const response = await this.ctx.util.fetch(`http://localhost:${process.env.CYPRESS_INTERNAL_VITE_APP_PORT}/`, { method: 'GET' })
+      const html = await response.text()
+
+      return html
+    }
+
+    // Check if the file exists. If it doesn't, it probably means that Vite is re-building
+    // and we should retry a few times until the file exists
+    let retryCount = 0
+    let err
+
+    while (retryCount < 5) {
+      try {
+        return await this.ctx.fs.readFile(getPathToDist('app', 'index.html'), 'utf8')
+      } catch (e) {
+        err = e
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+      }
+    }
+
+    throw err
   }
 
   /**
