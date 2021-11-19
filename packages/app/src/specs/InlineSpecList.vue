d8309ea11dabe7bfdc1a7744f5be48143c9306ae
Zachary Williams <ZachJW34@gmail.com>
2021-11-18 19:08:00 -0600

    feat: use fuzzy search (#18966)
    * feat: add fuzzy sort to inline spec-list
    
    * delete flat list
    
    * add fuzzy sort to home spec list
    
    * address comments
    
    * fix: virtual list
    
    * types
    
    Co-authored-by: Jessica Sachs <jess@jessicasachs.io>
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>

--- packages/app/src/specs/InlineSpecList.vue
+++ packages/app/src/specs/InlineSpecList.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="w-280px">
     <InlineSpecListHeader
-      v-model:tab="tab"
       v-model:search="search"
     />
     <div class="h-[calc(100vh-65px)] overflow-y-auto overflow-x-hidden pt-16px">
-      <div v-if="tab === 'flat'">
-        <InlineSpecListRow
-          v-for="spec in specs"
-          :key="spec.node.id"
-          :spec="spec.node"
-          :selected="isCurrentSpec(spec)"
-        />
-      </div>
       <InlineSpecListTree
-        v-else
-        :specs="specs.map(spec => spec.node)"
+        :specs="specs"
       />
     </div>
   </div>
@@ -24,12 +14,12 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { gql } from '@urql/vue'
-import type { SpecNode_InlineSpecListFragment, Specs_InlineSpecListFragment } from '../generated/graphql'
-import { useSpecStore } from '../store'
+import type { Specs_InlineSpecListFragment } from '../generated/graphql'
 import InlineSpecListHeader from './InlineSpecListHeader.vue'
-import InlineSpecListRow from './InlineSpecListRow.vue'
 import InlineSpecListTree from './InlineSpecListTree.vue'
-import type { SpecViewType } from './SpecsList.vue'
+
+import fuzzySort from 'fuzzysort'
+import type { FuzzyFoundSpec } from '@packages/frontend-shared/src/utils/spec-utils'
 
 gql`
 fragment SpecNode_InlineSpecList on SpecEdge {
@@ -63,28 +53,18 @@ const props = defineProps<{
   gql: Specs_InlineSpecListFragment
 }>()
 
-const specStore = useSpecStore()
-
-const isCurrentSpec = (spec: SpecNode_InlineSpecListFragment) => {
-  return spec.node.relative === specStore.activeSpec?.relative
-}
-
-const tab = ref<SpecViewType>('flat')
 const search = ref('')
 
-const specs = computed(() => {
+const specs = computed<FuzzyFoundSpec[]>(() => {
+  const specs = props.gql.specs?.edges.map((x) => ({ ...x.node, indexes: [] })) || []
+
   if (!search.value) {
-    return props.gql.specs?.edges || []
+    return specs
   }
 
-  return (
-    props.gql.specs?.edges.filter((edge) => {
-      return (
-        edge.node.fileName.toLowerCase() +
-        edge.node.specFileExtension.toLowerCase()
-      ).includes(search.value.toLocaleLowerCase())
-    }) || []
-  )
+  return fuzzySort
+  .go(search.value, specs || [], { key: 'relative' })
+  .map(({ obj, indexes }) => ({ ...obj, indexes }))
 })
 
 </script>
