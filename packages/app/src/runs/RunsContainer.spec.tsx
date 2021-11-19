5eeada238addd656658f4bf5fb90be0b03a1d2d5
ElevateBart <ledouxb@gmail.com>
2021-11-19 09:34:45 -0600

    test: fix one last issue on container

--- packages/app/src/runs/RunsContainer.spec.tsx
+++ packages/app/src/runs/RunsContainer.spec.tsx
@@ -3,11 +3,15 @@ import { RunsContainerFragmentDoc } from '../generated/graphql-test'
 
 describe('<RunsContainer />', { keystrokeDelay: 0 }, () => {
   const cloudViewer = {
-    __typename: 'CloudUser',
+    __typename: 'CloudUser' as const,
     id: '1',
     email: 'test@test.test',
     fullName: 'Tester Test',
-  } as const
+    organizations: {
+      __typename: 'CloudOrganizationConnection' as const,
+      nodes: [],
+    },
+  }
 
   it('playground', () => {
     cy.mountFragment(RunsContainerFragmentDoc, {
