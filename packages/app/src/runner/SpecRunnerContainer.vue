0a57c91dfd1bfc7abb96e824808fd2386ea0bdd8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-11-24 10:42:13 +1000

    types

--- packages/app/src/runner/SpecRunnerContainer.vue
+++ packages/app/src/runner/SpecRunnerContainer.vue
@@ -19,7 +19,6 @@ import { getAutIframeModel, UnifiedRunnerAPI } from '../runner'
 import { useSpecStore } from '../store'
 import { useSelectorPlaygroundStore } from '../store/selector-playground-store'
 import SpecRunner from './SpecRunner.vue'
-import type { GqlWithCurrentProject } from '../pages/Runner.vue'
 
 const initialized = ref(false)
 const specStore = useSpecStore()
@@ -32,11 +31,11 @@ onMounted(async () => {
 })
 
 const props = defineProps<{
-  gql: GqlWithCurrentProject
+  gql: SpecRunnerFragment
 }>()
 
 watch(() => route.query.file, (queryParam) => {
-  const spec = props.gql.currentProject.specs?.edges.find((x) => x.node.relative === queryParam)?.node
+  const spec = props.gql.currentProject?.specs?.edges.find((x) => x.node.relative === queryParam)?.node
 
   if (selectorPlaygroundStore.show) {
     const autIframe = getAutIframeModel()
