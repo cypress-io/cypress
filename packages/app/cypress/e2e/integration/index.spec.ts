9c4a9cfcd9931588e858579acfd220c1224ce67c
Mark Noonan <mark@cypress.io>
2021-11-29 22:52:25 -0500

    wip changes for spec patterns in graphql

--- packages/app/cypress/e2e/integration/index.spec.ts
+++ packages/app/cypress/e2e/integration/index.spec.ts
@@ -9,15 +9,23 @@ describe('Index', () => {
   context('with no specs', () => {
     beforeEach(() => {
       cy.visitApp()
+    })
+
+    it('shows "Create your first spec" title if default specPattern is present in config', () => {
+      cy.withCtx((ctx, o) => {
+        ctx.actions.file.removeFileInProject('cypress/integration/integration-spec.js')
+        ctx.currentProject?.config
+      })
+
+      cy.contains(defaultMessages.createSpec.page.defaultPatternNoSpecs.title).should('be.visible')
+    })
+
+    it('shows "No specs found" title if custom specPattern is present in config', () => {
       cy.withCtx((ctx, o) => {
         ctx.actions.file.removeFileInProject('cypress/integration/integration-spec.js')
       })
-    })
 
-    it('shows "Create your first spec" title', () => {
-    // after removing the default scaffolded spec, we should be prompted to create a first spec
-      cy.visitApp()
-      cy.contains(defaultMessages.createSpec.page.defaultPatternNoSpecs.title).should('be.visible')
+      cy.contains(defaultMessages.createSpec.page.customPatternNoSpecs.title).should('be.visible')
     })
   })
 })
