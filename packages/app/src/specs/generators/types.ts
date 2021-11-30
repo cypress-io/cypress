8d8fb5c86cf5869aef12ea6a547b2875f2fa640a
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-11-30 16:57:34 +1000

    types

--- packages/app/src/specs/generators/types.ts
+++ packages/app/src/specs/generators/types.ts
@@ -1,11 +1,12 @@
 import type { Component } from 'vue'
+import type { TestingTypeEnum, CreateSpecCardsFragment } from '../../generated/graphql'
 
 export type GeneratorId = 'component-generator' | 'empty-generator' | 'scaffold-generator' | 'story-generator'
 
 export interface SpecGenerator {
   card: Component
   entry: Component
-  matches: (testingType) => boolean
-  disabled: (currentProject?) => boolean | void
+  matches: (testingType: TestingTypeEnum) => boolean
+  disabled: (currentProject?: CreateSpecCardsFragment['currentProject'] | null) => boolean
   id: GeneratorId
 }
