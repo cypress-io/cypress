b83a5f48c6036a2ac20dfe14f6ac67cb989ce711
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-11-30 14:00:20 +1000

    wip: specs

--- packages/app/cypress/e2e/integration/index.spec.ts
+++ packages/app/cypress/e2e/integration/index.spec.ts
@@ -1,21 +1,27 @@
 describe('Index', () => {
-  beforeEach(() => {
-    cy.setupE2E('component-tests')
+  function initializeAndVisit (project: Parameters<typeof cy.setupE2E>[0]) {
+    cy.setupE2E(project)
     cy.initializeApp()
+    cy.visitApp()
+  }
+
+  it('shows "Create your first spec"', () => {
+    initializeAndVisit('component-tests')
+    cy.withCtx((ctx, o) => {
+      ctx.actions.file.removeFileInProject('cypress/integration/integration-spec.js')
+    })
+
+    // after removing the default scaffolded spec, we should be prompted to create a first spec
+    cy.visitApp()
+    cy.contains('Create your first spec')
   })
 
-  context('with no specs', () => {
-    beforeEach(() => {
-      cy.visitApp()
-      cy.withCtx((ctx, o) => {
-        ctx.actions.file.removeFileInProject('cypress/integration/integration-spec.js')
-      })
+  it('creates a spec from component', () => {
+    initializeAndVisit('webpack-dev-server-react')
+    cy.withCtx((ctx, o) => {
+      ctx.actions.file.removeFileInProject('src/Hello.cy.jsx')
     })
 
-    it('shows "Create your first spec"', () => {
-    // after removing the default scaffolded spec, we should be prompted to create a first spec
-      cy.visitApp()
-      cy.contains('Create your first spec')
-    })
+    cy.visitApp()
   })
 })
