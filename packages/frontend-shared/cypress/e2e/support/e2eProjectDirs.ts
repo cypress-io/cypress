736dfd48c56d389b6a824be7a7d35405c9929ee7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-01 10:51:19 +1000

    update schema

--- packages/frontend-shared/cypress/e2e/support/e2eProjectDirs.ts
+++ packages/frontend-shared/cypress/e2e/support/e2eProjectDirs.ts
@@ -83,6 +83,7 @@ export const e2eProjectDirs = [
   'unify-onboarding-with-config',
   'unify-plugin-errors',
   'various-file-types',
+  'webpack-dev-server-react',
   'webpack-preprocessor',
   'webpack-preprocessor-awesome-typescript-loader',
   'webpack-preprocessor-ts-loader',
