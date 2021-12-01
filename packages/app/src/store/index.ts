addf357c25c62d9c7407f2e92668bc3cf17f474f
Mark Noonan <mark@cypress.io>
2021-12-01 12:01:12 -0500

    cleanup

--- packages/app/src/store/index.ts
+++ packages/app/src/store/index.ts
@@ -7,8 +7,6 @@ export * from './specs-store'
 
 export * from './aut-store'
 
-export * from './runner-store'
-
 export * from './runner-ui-store'
 
 // Mobx Store Wrapper from @packages/runner-shared
