d26297ad6c1b0ceabd45df5757a1aee2f0e8c10a
Chris Breiding <chrisbreiding@gmail.com>
2021-12-02 10:15:01 -0500

    add hook to parallelized system test

--- system-tests/projects/e2e/cypress/integration/record_pass_spec.js
+++ system-tests/projects/e2e/cypress/integration/record_pass_spec.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
 describe('record pass', () => {
-  it('passes', { env: { foo: true } }, () => {
+  beforeEach(() => {
     cy.visit('/scrollable.html')
+  })
+
+  it('passes', { env: { foo: true } }, () => {
     cy.viewport(400, 400)
     cy.get('#box')
     cy.screenshot('yay it passes')
