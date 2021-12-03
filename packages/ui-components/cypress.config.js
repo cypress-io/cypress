85517965da388a5bbb3cf230afe6cde8c85614d9
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-03 11:47:47 +1000

    update spec pattern for ui components

--- packages/ui-components/cypress.config.js
+++ packages/ui-components/cypress.config.js
@@ -64,6 +64,7 @@ module.exports = {
     'openMode': 0,
   },
   'e2e': {
+    'specPattern': 'cypress/integration/**/*.jsx',
     setupNodeEvents (on, config) {
       on('file:preprocessor', wp({ webpackOptions }))
 
