af933e7462ff81e449cdb07473e947d42f6ea851
Jessica Sachs <jess@jessicasachs.io>
2021-12-03 18:18:57 -0500

    fix typo, add hover state

--- packages/app/src/settings/SettingsContainer.vue
+++ packages/app/src/settings/SettingsContainer.vue
@@ -31,10 +31,10 @@
       {{ t('settingsPage.footer.text') }}
     </p>
     <Button
-      class="mx-auto"
+      class="mx-auto group"
       variant="outline"
       :prefix-icon="SettingsIcon"
-      prefix-icon-class="icon-dark-gray-500 icon-light-gray-50"
+      prefix-icon-class="icon-dark-gray-500 icon-light-gray-50 group-hocus:icon-dark-indigo-400 group-hocus:icon-light-indigo-50"
       @click="reconfigure"
     >
       {{ t('settingsPage.footer.button') }}
