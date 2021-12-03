590eb0e831f1aeed65bd849e651681eb89274ac7
David Munechika <davidam1203@gmail.com>
2021-12-03 11:47:59 -0500

    fix logic for test config override options

--- packages/config/lib/index.js
+++ packages/config/lib/index.js
@@ -20,7 +20,7 @@ const breakingKeys = _.map(breakingOptions, 'name')
 const defaultValues = createIndex(options, 'name', 'defaultValue')
 const publicConfigKeys = _(options).reject({ isInternal: true }).map('name').value()
 const validationRules = createIndex(options, 'name', 'validation')
-const readOnlyConfigOptions = options.filter((option) => !option.canUpdateDuringTestTime).map((option) => option.name).reduce((name, val) => ({ ...name, [val]: true }), {})
+const testConfigOverrideOptions = createIndex(options, 'name', 'canUpdateDuringTestTime')
 
 module.exports = {
   allowed: (obj = {}) => {
@@ -107,7 +107,7 @@ module.exports = {
     let errProperty
 
     Object.keys(config).some((option) => {
-      return errProperty = readOnlyConfigOptions[option] ? option : undefined
+      return errProperty = !testConfigOverrideOptions[option] ? option : undefined
     })
 
     if (errProperty) {
