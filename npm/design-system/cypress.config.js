9ca7b81c5bdb58e029643602bdfdd3c9234bf037
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-03 12:15:14 +1000

    update design system config

--- npm/design-system/cypress.config.js
+++ npm/design-system/cypress.config.js
@@ -3,7 +3,6 @@ module.exports = {
   viewportHeight: 800,
   video: false,
   projectId: 'z9dxah',
-  testFiles: '**/*spec.{js,jsx,ts,tsx}',
   env: {
     reactDevtools: true,
   },
@@ -11,9 +10,9 @@ module.exports = {
     '**/__snapshots__/*',
     '**/__image_snapshots__/*',
   ],
-  componentFolder: 'src',
   fixturesFolder: false,
   component: {
+    specPattern: 'src/**/*spec.{js,jsx,ts,tsx}',
     devServer (cypressConfig) {
       const { startDevServer } = require('@cypress/vite-dev-server')
 
