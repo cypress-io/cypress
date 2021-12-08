b4fd2ae2ec21a805de0981b09f07505a12ccae1a
David Munechika <davidam1203@gmail.com>
2021-12-08 12:08:45 -0500

    fix error ui spec

--- packages/driver/src/cypress.ts
+++ packages/driver/src/cypress.ts
@@ -144,7 +144,7 @@ class $Cypress {
     this.state = $SetterGetter.create({})
     this.originalConfig = _.cloneDeep(config)
     this.config = $SetterGetter.create(config, (config) => {
-      if (!window.top.__cySkipValidateConfig) {
+      if (!window.top.__cySkipValidateConfig && process.env.CYPRESS_INTERNAL_ENV !== 'development') {
         validateNoReadOnlyConfig(config, (errProperty) => {
           let errMessage
 
--- system-tests/test/error_ui_spec.ts
+++ system-tests/test/error_ui_spec.ts
@@ -32,6 +32,9 @@ describe('e2e error ui', function () {
       onRun (exec) {
         return exec().then(verifyPassedAndFailedAreSame(1))
       },
+      processEnv: {
+        CYPRESS_INTERNAL_ENV: 'development',
+      },
     })
   })
 
@@ -44,6 +47,9 @@ describe('e2e error ui', function () {
       Fixtures.scaffoldProject('integration-outside-project-root')
 
       return exec().then(verifyPassedAndFailedAreSame(1))
+    },
+    processEnv: {
+      CYPRESS_INTERNAL_ENV: 'development',
     },
   })
 })
