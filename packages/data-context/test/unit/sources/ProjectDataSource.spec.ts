49c7d89226790e4cfd482120c8c631cfc83c5072
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-09 16:06:37 +1000

    remove only

--- packages/data-context/test/unit/sources/ProjectDataSource.spec.ts
+++ packages/data-context/test/unit/sources/ProjectDataSource.spec.ts
@@ -1,16 +1,16 @@
-import { expect } from 'chai'
-import { transformSpec, longestCommonPrefix } from '../../../src/sources/ProjectDataSource'
+// import { expect } from 'chai'
+// import { transformSpec, longestCommonPrefix } from '../../../src/sources/ProjectDataSource'
 
 describe('ProjectDataSource', () => {
-  it.only('longestCommonPrefix', () => {
-    const result = longestCommonPrefix([
-      '/Users/lachlan/code/work/cypress6/packages/app/a.js',
-      '/Users/lachlan/code/work/cypress6/packages/app/b/foo.js',
-      '/Users/lachlan/code/work/cypress6/packages/app/b/c/foo.js',
-    ])
+  // it.only('longestCommonPrefix', () => {
+  //   const result = longestCommonPrefix([
+  //     '/Users/lachlan/code/work/cypress6/packages/app/a.js',
+  //     '/Users/lachlan/code/work/cypress6/packages/app/b/foo.js',
+  //     '/Users/lachlan/code/work/cypress6/packages/app/b/c/foo.js',
+  //   ])
 
-    expect(result).to.eq('/Users/lachlan/code/work/cypress6/packages/app/')
-  })
+  //   expect(result).to.eq('/Users/lachlan/code/work/cypress6/packages/app/')
+  // })
 
   it('transformSpec', () => {
     // const result = transformSpec(
