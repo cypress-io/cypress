85a95ded15707c4dbe32192bf00ad7cdcc448679
ElevateBart <ledouxb@gmail.com>
2021-12-09 10:02:07 -0600

    feat: add config file updater and unit tests

---
+++ packages/server/lib/util/config-file-updater.ts
@@ -0,0 +1,342 @@
+import _ from 'lodash'
+import { parse } from '@babel/parser'
+import type { File } from '@babel/types'
+import type { NodePath } from 'ast-types/lib/node-path'
+import { visit } from 'recast'
+import type { namedTypes } from 'ast-types'
+import Debug from 'debug'
+import { fs } from './fs'
+import errors from '../errors'
+import { pathToConfigFile, read } from './settings'
+import type { SettingsOptions } from '@packages/types'
+
+const debug = Debug('cypress:server:config-file-updater')
+
+function _err (type, file, err) {
+  const e = errors.get(type, file, err) as any
+
+  e.code = err.code
+  e.errno = err.errno
+  throw e
+}
+
+function _logWriteErr (file, err) {
+  return _err('ERROR_WRITING_FILE', file, err)
+}
+
+export async function insertValuesInConfigFile (projectRoot: string, obj = {}, options: SettingsOptions = {}) {
+  const { configFile } = options
+
+  if (configFile === false) {
+    return {}
+  }
+
+  if (configFile && /\.json$/.test(configFile)) {
+    return await insertValuesInJSON(projectRoot, obj, options)
+  }
+
+  return await insertValuesInJavaScript(projectRoot, obj, options)
+}
+
+export async function insertValuesInJSON (projectRoot: string, obj, options: SettingsOptions) {
+  const config = await read(projectRoot, options)
+
+  obj = _.extend(config, obj)
+
+  const file = pathToConfigFile(projectRoot, options)
+
+  debug('writing json file')
+  try {
+    await fs.outputJson(file, obj, { spaces: 2 })
+  } catch (err) {
+    return _logWriteErr(file, err)
+  }
+
+  return obj
+}
+
+export async function insertValuesInJavaScript (projectRoot: string, obj, options: SettingsOptions) {
+  const file = pathToConfigFile(projectRoot, options)
+
+  const config = await read(projectRoot, options)
+  const fileContents = await fs.readFile(file, { encoding: 'utf8' })
+
+  await fs.writeFile(file, await insertValueInJSString(fileContents, obj))
+
+  return { ...config, ...obj }
+}
+
+export async function insertValueInJSString (fileContents: string, obj: Record<string, any>): Promise<string> {
+  const ast = parse(fileContents, { plugins: ['typescript'], sourceType: 'module' })
+
+  let objectLiteralNode: namedTypes.ObjectExpression | undefined
+
+  function handleExport (nodePath: NodePath<namedTypes.CallExpression, any> | NodePath<namedTypes.ObjectExpression, any>) {
+    if (nodePath.node.type === 'CallExpression'
+        && nodePath.node.callee.type === 'Identifier') {
+      const functionName = nodePath.node.callee.name
+
+      if (isDefineConfigFunction(ast, functionName)) {
+        return handleExport(nodePath.get('arguments', 0))
+      }
+    }
+
+    if (nodePath.node.type === 'ObjectExpression' && !nodePath.node.properties.find((prop) => prop.type !== 'ObjectProperty')) {
+      objectLiteralNode = nodePath.node
+      debug('found object litteral %o', objectLiteralNode)
+
+      return
+    }
+
+    throw errors.get('COULD_NOT_UPDATE_CONFIG_FILE', obj, 'Exported object is not an object literal')
+  }
+
+  visit(ast, {
+    visitAssignmentExpression (nodePath) {
+      if (nodePath.node.left.type === 'MemberExpression') {
+        if (nodePath.node.left.object.type === 'Identifier' && nodePath.node.left.object.name === 'module'
+        && nodePath.node.left.property.type === 'Identifier' && nodePath.node.left.property.name === 'exports') {
+          handleExport(nodePath.get('right'))
+        }
+      }
+
+      return false
+    },
+    visitExportDefaultDeclaration (nodePath) {
+      handleExport(nodePath.get('declaration'))
+
+      return false
+    },
+  })
+
+  const splicers: Splicer[] = []
+
+  if (!objectLiteralNode) {
+    // if the export is no object litteral
+    throw errors.get('COULD_NOT_UPDATE_CONFIG_FILE', obj, 'No export could be found')
+  }
+
+  setRootKeysSplicers(splicers, obj, objectLiteralNode, '  ')
+  setSubKeysSplicers(splicers, obj, objectLiteralNode, '  ', '  ')
+
+  // sort splicers to keep the order of the original file
+  const sortedSplicers = splicers.sort((a, b) => a.start === b.start ? 0 : a.start > b.start ? 1 : -1)
+
+  debug('sortedSplicers %o', sortedSplicers)
+
+  if (!sortedSplicers.length) return fileContents
+
+  let nextStartingIndex = 0
+  let resultCode = ''
+
+  sortedSplicers.forEach((splicer) => {
+    resultCode += fileContents.slice(nextStartingIndex, splicer.start) + splicer.replaceString
+    nextStartingIndex = splicer.end
+  })
+
+  return resultCode + fileContents.slice(nextStartingIndex)
+}
+
+export function isDefineConfigFunction (ast: File, functionName: string): boolean {
+  let value = false
+
+  visit(ast, {
+    visitVariableDeclarator (nodePath) {
+      // if this is a require of cypress
+      if (nodePath.node.init?.type === 'CallExpression'
+      && nodePath.node.init.callee.type === 'Identifier'
+      && nodePath.node.init.callee.name === 'require'
+      && nodePath.node.init.arguments[0].type === 'StringLiteral'
+      && nodePath.node.init.arguments[0].value === 'cypress') {
+        if (nodePath.node.id?.type === 'ObjectPattern') {
+          const defineConfigFunctionNode = nodePath.node.id.properties.find((prop) => {
+            return prop.type === 'ObjectProperty'
+          && prop.key.type === 'Identifier'
+          && prop.key.name === 'defineConfig'
+          })
+
+          if (defineConfigFunctionNode) {
+            value = (defineConfigFunctionNode as any).value?.name === functionName
+          }
+        }
+      }
+
+      return false
+    },
+    visitImportDeclaration (nodePath) {
+      if (nodePath.node.source.type === 'StringLiteral'
+      && nodePath.node.source.value === 'cypress') {
+        const defineConfigFunctionNode = nodePath.node.specifiers?.find((specifier) => {
+          return specifier.type === 'ImportSpecifier'
+          && specifier.imported.type === 'Identifier'
+        && specifier.imported.name === 'defineConfig'
+        })
+
+        if (defineConfigFunctionNode) {
+          value = (defineConfigFunctionNode as any).local?.name === functionName
+        }
+      }
+
+      return false
+    },
+  })
+
+  return value
+}
+
+function setRootKeysSplicers (
+  splicers: Splicer[],
+  obj: Record<string, any>,
+  objectLiteralNode: namedTypes.ObjectExpression,
+  lineStartSpacer: string,
+) {
+  const objectLiteralStartIndex = (objectLiteralNode as any).start + 1
+  // add values
+  const objKeys = Object.keys(obj).filter((key) => ['boolean', 'number', 'string'].includes(typeof obj[key]))
+
+  // update values
+  const keysToUpdate = objKeys.filter((key) => {
+    return objectLiteralNode.properties.find((prop) => {
+      return prop.type === 'ObjectProperty'
+        && prop.key.type === 'Identifier'
+        && prop.key.name === key
+    })
+  })
+
+  debug('keys to update %O', keysToUpdate)
+
+  keysToUpdate.forEach(
+    (key) => {
+      const propertyToUpdate = propertyFromKey(objectLiteralNode, key)
+
+      if (propertyToUpdate) {
+        setSplicerToUpdateProperty(splicers, propertyToUpdate, obj[key], key, obj)
+      }
+    },
+  )
+
+  const keysToInsert = objKeys.filter((key) => !keysToUpdate.includes(key))
+
+  debug('keys to instert %O', keysToInsert)
+
+  if (keysToInsert.length) {
+    const valuesInserted = `\n${lineStartSpacer}${ keysToInsert.map((key) => `${key}: ${JSON.stringify(obj[key])},`).join(`\n${lineStartSpacer}`)}`
+
+    splicers.push({
+      start: objectLiteralStartIndex,
+      end: objectLiteralStartIndex,
+      replaceString: valuesInserted,
+    })
+  }
+}
+
+function setSubKeysSplicers (
+  splicers: Splicer[],
+  obj: Record<string, any>,
+  objectLiteralNode: namedTypes.ObjectExpression,
+  lineStartSpacer: string,
+  parentLineStartSpacer: string,
+) {
+  const objectLiteralStartIndex = (objectLiteralNode as any).start + 1
+
+  const keysToUpdateWithObjects: string[] = []
+
+  const objSubkeys = Object.keys(obj).filter((key) => typeof obj[key] === 'object').reduce((acc: Array<{parent: string, subkey: string}>, key) => {
+    keysToUpdateWithObjects.push(key)
+    Object.entries(obj[key]).forEach(([subkey, value]) => {
+      if (['boolean', 'number', 'string'].includes(typeof value)) {
+        acc.push({ parent: key, subkey })
+      }
+    })
+
+    return acc
+  }, [])
+
+  // add values where the parent key needs to be created
+  const subkeysToInsertWithoutKey = objSubkeys.filter(({ parent }) => {
+    return !objectLiteralNode.properties.find((prop) => {
+      return prop.type === 'ObjectProperty'
+        && prop.key.type === 'Identifier'
+        && prop.key.name === parent
+    })
+  })
+  const keysToInsertForSubKeys: Record<string, string[]> = {}
+
+  subkeysToInsertWithoutKey.forEach((keyTuple) => {
+    const subkeyList = keysToInsertForSubKeys[keyTuple.parent] || []
+
+    subkeyList.push(keyTuple.subkey)
+    keysToInsertForSubKeys[keyTuple.parent] = subkeyList
+  })
+
+  let subvaluesInserted = ''
+
+  for (const key in keysToInsertForSubKeys) {
+    subvaluesInserted += `\n${parentLineStartSpacer}${key}: {`
+    keysToInsertForSubKeys[key].forEach((subkey) => {
+      subvaluesInserted += `\n${parentLineStartSpacer}${lineStartSpacer}${subkey}: ${JSON.stringify(obj[key][subkey])},`
+    })
+
+    subvaluesInserted += `\n${parentLineStartSpacer}},`
+  }
+
+  if (subkeysToInsertWithoutKey.length) {
+    splicers.push({
+      start: objectLiteralStartIndex,
+      end: objectLiteralStartIndex,
+      replaceString: subvaluesInserted,
+    })
+  }
+
+  // add/update values where parent key already exists
+  keysToUpdateWithObjects.filter((parent) => {
+    return objectLiteralNode.properties.find((prop) => {
+      return prop.type === 'ObjectProperty'
+        && prop.key.type === 'Identifier'
+        && prop.key.name === parent
+    })
+  }).forEach((key) => {
+    const propertyToUpdate = propertyFromKey(objectLiteralNode, key)
+
+    if (propertyToUpdate?.value.type === 'ObjectExpression') {
+      setRootKeysSplicers(splicers, obj[key], propertyToUpdate.value, parentLineStartSpacer + lineStartSpacer)
+    }
+  })
+}
+
+function setSplicerToUpdateProperty (splicers: Splicer[],
+  propertyToUpdate: namedTypes.ObjectProperty,
+  updatedValue: any,
+  key: string,
+  obj: Record<string, any>) {
+  if (propertyToUpdate && (isPrimitive(propertyToUpdate.value) || isUndefinedOrNull(propertyToUpdate.value))) {
+    splicers.push({
+      start: (propertyToUpdate.value as any).start,
+      end: (propertyToUpdate.value as any).end,
+      replaceString: JSON.stringify(updatedValue),
+    })
+  } else {
+    debug('error', propertyToUpdate?.value)
+    throw errors.get('COULD_NOT_UPDATE_CONFIG_FILE', obj, `Value for \`${key}\` is not a primitive. Updating this value could break your config.`)
+  }
+}
+
+function propertyFromKey (objectLiteralNode: namedTypes.ObjectExpression | undefined, key: string): namedTypes.ObjectProperty | undefined {
+  return objectLiteralNode?.properties.find((prop) => {
+    return prop.type === 'ObjectProperty' && prop.key.type === 'Identifier' && prop.key.name === key
+  }) as namedTypes.ObjectProperty
+}
+
+function isPrimitive (value: NodePath['node']): value is namedTypes.NumericLiteral | namedTypes.StringLiteral | namedTypes.BooleanLiteral {
+  return value.type === 'NumericLiteral' || value.type === 'StringLiteral' || value.type === 'BooleanLiteral'
+}
+
+function isUndefinedOrNull (value: NodePath['node']): value is namedTypes.Identifier {
+  return value.type === 'Identifier' && ['undefined', 'null'].includes(value.name)
+}
+
+interface Splicer{
+  start: number
+  end: number
+  replaceString: string
+}
---
+++ packages/server/test/unit/util/config-file-updater_spec.ts
@@ -0,0 +1,429 @@
+import * as path from 'path'
+import { expect } from 'chai'
+import '../../spec_helper'
+
+import { fs } from '../../../lib/util/fs'
+import { insertValueInJSString, insertValuesInConfigFile } from '../../../lib/util/config-file-updater'
+import { stripIndent } from '../../../lib/util/strip_indent'
+const projectRoot = process.cwd()
+const defaultOptions = {
+  configFile: 'cypress.json',
+}
+
+describe('lib/util/config-file-updater', () => {
+  context('with default configFile option', () => {
+    beforeEach(function () {
+      this.setup = (obj = {}) => {
+        return fs.writeJson('cypress.json', obj)
+      }
+    })
+
+    afterEach(() => {
+      return fs.removeAsync('cypress.json')
+    })
+
+    context('.insertValuesInConfigFile', () => {
+      it('promises cypress.json updates', function () {
+        return this.setup().then(() => {
+          return insertValuesInConfigFile(projectRoot, { foo: 'bar' }, defaultOptions)
+        }).then((obj) => {
+          expect(obj).to.deep.eq({ foo: 'bar' })
+        })
+      })
+
+      it('only writes over conflicting keys', function () {
+        return this.setup({ projectId: '12345', autoOpen: true })
+        .then(() => {
+          return insertValuesInConfigFile(projectRoot, { projectId: 'abc123' }, defaultOptions)
+        }).then((obj) => {
+          expect(obj).to.deep.eq({ projectId: 'abc123', autoOpen: true })
+        })
+      })
+    })
+  })
+
+  context('with configFile: false', () => {
+    beforeEach(function () {
+      this.projectRoot = path.join(projectRoot, '_test-output/path/to/project/')
+
+      this.options = {
+        configFile: false,
+      }
+    })
+
+    it('.insertValuesInConfigFile does not create a file', function () {
+      return insertValuesInConfigFile(this.projectRoot, {}, this.options)
+      .then(() => {
+        return fs.access(path.join(this.projectRoot, 'cypress.json'))
+        .then(() => {
+          throw Error('file shuold not have been created here')
+        }).catch((err) => {
+          expect(err.code).to.equal('ENOENT')
+        })
+      })
+    })
+  })
+
+  context('with js files', () => {
+    describe('#insertValueInJSString', () => {
+      describe('es6 vs es5', () => {
+        it('finds the object litteral and adds the values to it es6', async () => {
+          const src = stripIndent`\
+              export default {
+                foo: 42,
+              }
+            `
+
+          const expectedOutput = stripIndent`\
+            export default {
+              projectId: "id1234",
+              viewportWidth: 400,
+              foo: 42,
+            }
+          `
+
+          const output = await insertValueInJSString(src, { projectId: 'id1234', viewportWidth: 400 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('finds the object litteral and adds the values to it es5', async () => {
+          const src = stripIndent`\
+              module.exports = {
+                foo: 42,
+              }
+            `
+
+          const expectedOutput = stripIndent`\
+            module.exports = {
+              projectId: "id1234",
+              viewportWidth: 400,
+              foo: 42,
+            }
+          `
+
+          const output = await insertValueInJSString(src, { projectId: 'id1234', viewportWidth: 400 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('works with and without the quotes around keys', async () => {
+          const src = stripIndent`\
+              export default {
+                "foo": 42,
+              }
+            `
+
+          const expectedOutput = stripIndent`\
+              export default {
+                projectId: "id1234",
+                viewportWidth: 400,
+                "foo": 42,
+              }
+            `
+
+          const output = await insertValueInJSString(src, { projectId: 'id1234', viewportWidth: 400 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+      })
+
+      describe('defineConfig', () => {
+        it('skips defineConfig and add to the object inside', async () => {
+          const src = stripIndent`\
+              import { defineConfig } from "cypress"
+              export default defineConfig({
+                foo: 42,
+              })
+            `
+
+          const expectedOutput = stripIndent`\
+              import { defineConfig } from "cypress"
+              export default defineConfig({
+                projectId: "id1234",
+                viewportWidth: 400,
+                foo: 42,
+              })
+            `
+
+          const output = await insertValueInJSString(src, { projectId: 'id1234', viewportWidth: 400 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('skips defineConfig even if it renamed in an import (es6)', async () => {
+          const src = stripIndent`\
+              import { defineConfig as cy_defineConfig } from "cypress"
+              export default cy_defineConfig({
+                foo: 42,
+              })
+            `
+
+          const expectedOutput = stripIndent`\
+              import { defineConfig as cy_defineConfig } from "cypress"
+              export default cy_defineConfig({
+                projectId: "id1234",
+                viewportWidth: 400,
+                foo: 42,
+              })
+            `
+
+          const output = await insertValueInJSString(src, { projectId: 'id1234', viewportWidth: 400 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('skips defineConfig even if it renamed in a require (es5)', async () => {
+          const src = stripIndent`\
+              const { defineConfig: cy_defineConfig } = require("cypress")
+              module.exports = cy_defineConfig({
+                foo: 42,
+              })
+            `
+
+          const expectedOutput = stripIndent`\
+              const { defineConfig: cy_defineConfig } = require("cypress")
+              module.exports = cy_defineConfig({
+                projectId: "id1234",
+                viewportWidth: 400,
+                foo: 42,
+              })
+            `
+
+          const output = await insertValueInJSString(src, { projectId: 'id1234', viewportWidth: 400 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+      })
+
+      describe('updates', () => {
+        it('updates a value if the same value is found in resolved config', async () => {
+          const src = stripIndent`\
+              export default {
+                foo: 42,
+              }
+            `
+          const expectedOutput = stripIndent`\
+              export default {
+                foo: 1000,
+              }
+            `
+
+          const output = await insertValueInJSString(src, { foo: 1000 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('accepts inline comments', async () => {
+          const src = stripIndent`\
+              export default {
+                foo: 12, // will do this later
+                viewportWidth: 800,
+              }
+            `
+          const expectedOutput = stripIndent`\
+              export default {
+                foo: 1000, // will do this later
+                viewportWidth: 800,
+              }
+            `
+
+          const output = await insertValueInJSString(src, { foo: 1000 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('updates a value even when this value is explicitely undefined', async () => {
+          const src = stripIndent`\
+              export default {
+                foo: undefined, // will do this later
+                viewportWidth: 800,
+              }
+            `
+          const expectedOutput = stripIndent`\
+              export default {
+                foo: 1000, // will do this later
+                viewportWidth: 800,
+              }
+            `
+
+          const output = await insertValueInJSString(src, { foo: 1000 })
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('updates values and inserts config', async () => {
+          const src = stripIndent`\
+            export default {
+              foo: 42,
+              bar: 84,
+              component: {
+                devServer() {
+                  return null
+                }
+              }
+            }
+          `
+
+          const expectedOutput = stripIndent`\
+            export default {
+              projectId: "id1234",
+              foo: 1000,
+              bar: 3000,
+              component: {
+                devServer() {
+                  return null
+                }
+              }
+            }
+          `
+
+          const output = await insertValueInJSString(src, { foo: 1000, bar: 3000, projectId: 'id1234' })
+
+          expect(output).to.equal(expectedOutput)
+        })
+      })
+
+      describe('subkeys', () => {
+        it('inserts nested values', async () => {
+          const src = stripIndent`\
+          module.exports = {
+            foo: 42
+          }
+        `
+
+          const output = await insertValueInJSString(src, { component: { specFilePattern: 'src/**/*.spec.cy.js' } })
+
+          const expectedOutput = stripIndent`\
+              module.exports = {
+                component: {
+                  specFilePattern: "src/**/*.spec.cy.js",
+                },
+                foo: 42
+              }
+            `
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('inserts nested values into existing keys', async () => {
+          const src = stripIndent`\
+              module.exports = {
+                component: {
+                  viewportWidth: 800
+                },
+                foo: 42
+              }
+            `
+
+          const output = await insertValueInJSString(src, { component: { specFilePattern: 'src/**/*.spec.cy.js' } })
+
+          const expectedOutput = stripIndent`\
+            module.exports = {
+              component: {
+                specFilePattern: "src/**/*.spec.cy.js",
+                viewportWidth: 800
+              },
+              foo: 42
+            }
+          `
+
+          expect(output).to.equal(expectedOutput)
+        })
+
+        it('updates nested values', async () => {
+          const src = stripIndent`\
+            module.exports = {
+              foo: 42,
+              component: {
+                specFilePattern: 'components/**/*.spec.cy.js',
+                foo: 82
+              }
+            }`
+
+          const output = await insertValueInJSString(src, { component: { specFilePattern: 'src/**/*.spec.cy.js' } })
+
+          const expectedOutput = stripIndent`\
+          module.exports = {
+            foo: 42,
+            component: {
+              specFilePattern: "src/**/*.spec.cy.js",
+              foo: 82
+            }
+          }`
+
+          expect(output).to.equal(expectedOutput)
+        })
+      })
+
+      describe('failures', () => {
+        it('fails if not an object litteral', () => {
+          const src = [
+            'const foo = {}',
+            'export default foo',
+          ].join('\n')
+
+          return insertValueInJSString(src, { bar: 10 })
+          .then(() => {
+            throw Error('this should not succeed')
+          })
+          .catch((err) => {
+            expect(err).to.have.property('type', 'COULD_NOT_UPDATE_CONFIG_FILE')
+          })
+        })
+
+        it('fails if one of the values to update is not a literal', () => {
+          const src = [
+            'const bar = 12',
+            'export default {',
+            '  foo: bar',
+            '}',
+          ].join('\n')
+
+          return insertValueInJSString(src, { foo: 10 })
+          .then(() => {
+            throw Error('this should not succeed')
+          })
+          .catch((err) => {
+            expect(err).to.have.property('type', 'COULD_NOT_UPDATE_CONFIG_FILE')
+          })
+        })
+
+        it('fails with inlined values', () => {
+          const src = stripIndent`\
+            const foo = 12
+            export default {
+              foo
+            }
+            `
+
+          return insertValueInJSString(src, { foo: 10 })
+          .then(() => {
+            throw Error('this should not succeed')
+          })
+          .catch((err) => {
+            expect(err).to.have.property('type', 'COULD_NOT_UPDATE_CONFIG_FILE')
+          })
+        })
+
+        it('fails if there is a spread', () => {
+          const src = stripIndent`\
+            const foo = { bar: 12 }
+            export default {
+              bar: 8,
+              ...foo
+            }
+            `
+
+          return insertValueInJSString(src, { bar: 10 })
+          .then(() => {
+            throw Error('this should not succeed')
+          })
+          .catch((err) => {
+            expect(err).to.have.property('type', 'COULD_NOT_UPDATE_CONFIG_FILE')
+          })
+        })
+      })
+    })
+  })
+})
