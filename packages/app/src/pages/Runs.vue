706b4d6376b5b6b9a7fb3400df9d55e4354f1ba3
ElevateBart <ledouxb@gmail.com>
2021-12-09 12:23:22 -0600

    refactor: rename component in runs page

--- packages/app/src/pages/Runs.vue
+++ packages/app/src/pages/Runs.vue
@@ -2,7 +2,7 @@
   <div class="h-full p-24px relative">
     <TransitionQuickFade>
       <RunsSkeleton v-if="query.fetching.value || !query.data.value" />
-      <RunsPage
+      <RunsContainer
         v-else
         :gql="query.data.value"
       />
@@ -14,7 +14,7 @@
 import { gql, useQuery } from '@urql/vue'
 import { RunsDocument } from '../generated/graphql'
 import RunsSkeleton from '../runs/RunsSkeleton.vue'
-import RunsPage from '../runs/RunsContainer.vue'
+import RunsContainer from '../runs/RunsContainer.vue'
 import TransitionQuickFade from '@cy/components/transitions/TransitionQuickFade.vue'
 
 gql`
