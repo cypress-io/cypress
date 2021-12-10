ba32823a61cb2e4d3cfa1bbd74d9fd05cff78aee
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-10 13:01:56 +1000

    update config

--- packages/runner/cypress.config.js
+++ packages/runner/cypress.config.js
@@ -13,7 +13,6 @@ module.exports = {
     'configFile': '../../mocha-reporter-config.json',
   },
   'e2e': {
-    'specPattern': 'cypress/integration/**/*',
     'setupNodeEvents': require('./cypress/plugins'),
   },
 }
