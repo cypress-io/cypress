235c89649655fff65903f3a7b5f6ab0af45a17dc
Ryan Manuel <ryanm@cypress.io>
2021-12-10 14:59:27 -0600

    test: spec file item

--- packages/app/src/specs/SpecFileItem.spec.tsx
+++ packages/app/src/specs/SpecFileItem.spec.tsx
@@ -1,10 +1,14 @@
 import SpecFileItem from './SpecFileItem.vue'
 
 describe('SpecFileItem', () => {
-  it('should render SpecFileItem', () => {
+  it('renders a SpecFileItem with the base filename and extension having different highlights', () => {
     cy.mount(() =>
       (<div class="bg-gray-1000 group">
         <SpecFileItem fileName="Hello" extension=".spec.jsx" selected={false}></SpecFileItem>
       </div>))
+
+    cy.contains('Hello.spec.jsx').should('be.visible')
+
+    cy.percySnapshot()
   })
 })
