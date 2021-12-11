c03a65048a55221a5f912559a31c913cc3643b00
ElevateBart <ledouxb@gmail.com>
2021-12-10 21:14:01 -0600

    feat: Start working on the first step WIP

---
+++ packages/launchpad/src/migration/RenameSpecsAuto.spec.tsx
@@ -0,0 +1,7 @@
+import RenameSpecsAuto from './RenameSpecsAuto.vue'
+
+describe('RenameSpecsAuto', () => {
+  it('renders expected content', () => {
+    cy.mount(RenameSpecsAuto)
+  })
+})
---
+++ packages/launchpad/src/migration/RenameSpecsAuto.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h3>We'll automatically rename your specs in this step — here's why:</h3>
+    <ul class="list-disc pl-24px">
+      <li>
+        <span>
+          We've changed the default spec folder from:
+        </span>
+        <Tag
+          class="text-red-500"
+          code
+        >cypress/integration</Tag>
+        <i-cy-arrow-right_x16 class="h-16px w-16px inline-block icon-dark-gray-300" />
+        <Tag
+          class="text-jade-500"
+          code
+        >cypress/e2e</Tag>
+      </li>
+      <li>
+        <span>
+          We've changed the default spec file extension from:
+        </span>
+        <Tag
+          class="text-red-500"
+          code
+        >[filename].spec.[ext]</Tag>
+        <i-cy-arrow-right_x16 class="h-16px w-16px inline-block icon-dark-gray-300" />
+        <Tag
+          class="text-blue-500"
+          code
+        >[filename].cy.[ext]</Tag>
+      </li>
+      <li>
+        <span>
+          Since you're currently using a custom directory for the <Tag code>integrationFolder</Tag>, we'll temporarily be moving your specs back to the default directory.
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Tag from '@cy/components/Tag.vue'
+
+defineProps<{
+
+}>()
+</script>
