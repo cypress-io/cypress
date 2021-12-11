6579fee503a5cb6dc742b1b02a8ed337420a04eb
Mark Noonan <mark@cypress.io>
2021-12-11 17:54:08 -0500

    wip begin using new component in spec-runner

--- packages/app/src/runner/SpecRunner.vue
+++ packages/app/src/runner/SpecRunner.vue
@@ -2,91 +2,78 @@
   <RemovePositioningDuringScreenshot
     id="main-pane"
     class="flex border-gray-900 border-l-1"
-    :class="{'select-none': specsListIsDragging || reporterIsDragging}"
   >
-    <HideDuringScreenshot
-      id="inline-spec-list"
-      class="bg-gray-1000"
-    >
-      <div
-        v-if="props.gql.currentProject"
-        v-show="runnerUiStore.isSpecsListOpen"
-        class="relative"
-        :style="{width: `${specsListWidth - 64}px`}"
-      >
-        <InlineSpecList
-          id="reporter-inline-specs-list"
-          :gql="props.gql"
-        />
+    <ResizablePanels :offset-left="64">
+      <template #panel1>
+        <HideDuringScreenshot
+          id="inline-spec-list"
+          class="bg-gray-1000"
+        >
+          <div
+            v-if="props.gql.currentProject"
+            v-show="runnerUiStore.isSpecsListOpen"
+          >
+            <InlineSpecList
+              id="reporter-inline-specs-list"
+              :gql="props.gql"
+            />
+          </div>
+
+          <ChooseExternalEditorModal
+            :open="runnerUiStore.showChooseExternalEditorModal"
+            :gql="props.gql"
+            @close="runnerUiStore.setShowChooseExternalEditorModal(false)"
+            @selected="openFile"
+          />
+        </HideDuringScreenshot>
+      </template>
+      <template #panel2>
+        <HideDuringScreenshot
+          class="h-full"
+        >
+          <div
+            v-once
+            :id="REPORTER_ID"
+            class="w-full"
+          />
+        </HideDuringScreenshot>
+      </template>
+      <template #panel3>
         <div
-          v-show="runnerUiStore.isSpecsListOpen && !screenshotStore.isScreenshotting"
-          ref="specsListResizeHandle"
-          class="cursor-ew-resize h-full top-0 -right-6px w-16px z-30 absolute"
-        />
-      </div>
+          ref="runnerPane"
+          class="w-full relative"
+        >
+          <HideDuringScreenshot class="bg-white p-16px">
+            <SpecRunnerHeader
+              v-if="props.gql.currentProject"
+              :gql="props.gql.currentProject"
+              :event-manager="eventManager"
+              :get-aut-iframe="getAutIframeModel"
+            />
+          </HideDuringScreenshot>
 
-      <ChooseExternalEditorModal
-        :open="runnerUiStore.showChooseExternalEditorModal"
-        :gql="props.gql"
-        @close="runnerUiStore.setShowChooseExternalEditorModal(false)"
-        @selected="openFile"
-      />
-    </HideDuringScreenshot>
-
-    <HideDuringScreenshot>
-      <div
-        class="h-full relative"
-        :style="{width: `${reporterWidth}px`}"
-      >
-        <div
-          v-once
-          :id="REPORTER_ID"
-          :class="{'select-none': specsListIsDragging}"
-          class="w-full"
-        />
-
-        <div
-          v-show="!screenshotStore.isScreenshotting"
-          ref="reporterResizeHandle"
-          class="cursor-ew-resize h-full top-0 -right-6px w-16px z-30 absolute"
-        />
-      </div>
-    </HideDuringScreenshot>
-
-    <div
-      ref="runnerPane"
-      class="w-full relative"
-      :class="{'pointer-events-none': specsListIsDragging || reporterIsDragging}"
-    >
-      <HideDuringScreenshot class="bg-white p-16px">
-        <SpecRunnerHeader
-          v-if="props.gql.currentProject"
-          :gql="props.gql.currentProject"
-          :event-manager="eventManager"
-          :get-aut-iframe="getAutIframeModel"
-        />
-      </HideDuringScreenshot>
-
-      <RemoveClassesDuringScreenshotting
-        class="h-full bg-gray-100 p-16px"
-      >
-        <ScriptError
-          v-if="autStore.scriptError"
-          :error="autStore.scriptError.error"
-        />
-        <div
-          v-show="!autStore.scriptError"
-          :id="RUNNER_ID"
-          class="origin-top-left viewport"
-          :style="viewportStyle"
-        />
-      </RemoveClassesDuringScreenshotting>
-      <SnapshotControls
-        :event-manager="eventManager"
-        :get-aut-iframe="getAutIframeModel"
-      />
-      <ScreenshotHelperPixels />
-    </div>
+          <RemoveClassesDuringScreenshotting
+            class="h-full bg-gray-100 p-16px"
+          >
+            <ScriptError
+              v-if="autStore.scriptError"
+              :error="autStore.scriptError.error"
+            />
+            <div
+              v-show="!autStore.scriptError"
+              :id="RUNNER_ID"
+              class="origin-top-left viewport"
+              :style="viewportStyle"
+            />
+          </RemoveClassesDuringScreenshotting>
+          <SnapshotControls
+            :event-manager="eventManager"
+            :get-aut-iframe="getAutIframeModel"
+          />
+          <ScreenshotHelperPixels />
+        </div>
+      </template>
+    </ResizablePanels>
   </RemovePositioningDuringScreenshot>
 </template>
 
@@ -110,7 +97,8 @@ import { OpenFileInIdeDocument } from '@packages/data-context/src/gen/all-operat
 import type { SpecRunnerFragment } from '../generated/graphql'
 import { usePreferences } from '../composables/usePreferences'
 import ScriptError from './ScriptError.vue'
-import { useWindowSize, useDraggable } from '@vueuse/core'
+import { useWindowSize } from '@vueuse/core'
+import ResizablePanels from './ResizablePanels.vue'
 
 const { height: windowHeight, width: windowWidth } = useWindowSize()
 
@@ -161,108 +149,12 @@ preferences.update('specsListWidth', initialSpecsListWidth)
 
 const runnerPane = ref<HTMLDivElement>()
 
-const MIN_AUT_WIDTH = 200
-
-const specsListResizeHandle = ref<HTMLElement | null>(null)
-const reporterResizeHandle = ref<HTMLElement | null>(null)
-const lastReporterWidth = ref(initialReporterWidth)
-const lastSpecsListWidth = ref(initialSpecsListWidth)
-
-const { x: specsListHandleX, isDragging: specsListIsDragging } = useDraggable(specsListResizeHandle, {
-  initialValue: { x: initialSpecsListWidth, y: 0 },
-  exact: true,
-  preventDefault: true,
-  onMove: () => {
-    lastSpecsListWidth.value = specsListWidth.value
-  },
-  onEnd: () => {
-    // console.log('end drag specs list x val', specsListHandleX.value)
-    // console.log('end drag specs list width val', specsListWidth.value)
-    specsListHandleX.value = specsListWidth.value
-    handleResizeEnd('specsList')
-  },
-})
-
-const { x: reporterHandleX, isDragging: reporterIsDragging } = useDraggable(reporterResizeHandle, {
-  initialValue: { x: initialReporterWidth + initialSpecsListWidth, y: 0 },
-  exact: true,
-  preventDefault: true,
-  onMove: () => {
-    lastReporterWidth.value = reporterWidth.value
-  },
-  onEnd: () => {
-    reporterHandleX.value = reporterWidth.value + specsListWidth.value
-    lastReporterWidth.value = reporterWidth.value
-    handleResizeEnd('reporter')
-  },
-})
-const specsListWidth = computed(() => {
-  if (!runnerUiStore.isSpecsListOpen) {
-    return 0
-  }
-
-  if (!specsListIsDragging) {
-    return lastSpecsListWidth.value
-  }
-
-  const nonSpecsListWidth = reporterWidth.value + MIN_AUT_WIDTH
-  const maxSpecsListWidth = windowWidth.value - nonSpecsListWidth
-
-  if (specsListHandleX.value <= maxSpecsListWidth && specsListHandleX.value > 300) {
-    return specsListHandleX.value
-  }
-
-  if (specsListHandleX.value > maxSpecsListWidth) {
-    return maxSpecsListWidth
-  }
-
-  return 300
-})
-
-const reporterWidth = computed(() => {
-  if (!reporterIsDragging.value) {
-    // console.log('reporter is not dragging, using last value ', lastReporterWidth.value)
-
-    return lastReporterWidth.value
-  }
-
-  const unavailableWidth = specsListWidth.value + MIN_AUT_WIDTH + 50
-  const maxReporterWidth = windowWidth.value - unavailableWidth
-  const currentReporterWidth = reporterHandleX.value - specsListWidth.value
-
-  if (!maxReporterWidth || !currentReporterWidth) {
-    // console.log('missing maxReporterWidth or currentReporterWidth')
-
-    return initialReporterWidth
-  }
-
-  if (currentReporterWidth <= maxReporterWidth && currentReporterWidth > 200) {
-    // console.log('safe zone, currentReporterWidth was less than maxReporterWidth and greater than 200', currentReporterWidth)
-
-    return currentReporterWidth
-  }
-
-  if (currentReporterWidth > maxReporterWidth) {
-    // console.log('currentReporterWidth was greater than maxReporterWidth, return max')
-
-    return maxReporterWidth
-  }
-
-  if (currentReporterWidth < 200) {
-    // console.log('currentReporterWidth less than 200')
-
-    return 200
-  }
-
-  // console.log('something else', reporterIsDragging.value, currentReporterWidth, maxReporterWidth)
-
-  return currentReporterWidth
-})
 const autMargin = 16
 
 const containerWidth = computed(() => {
-  const miscBorders = 4
-  const nonAutWidth = reporterWidth.value + specsListWidth.value + (autMargin * 2) + miscBorders
+  // const miscBorders = 4
+  // const nonAutWidth = reporterWidth.value + specsListWidth.value + (autMargin * 2) + miscBorders
+  const nonAutWidth = 600
 
   return windowWidth.value - nonAutWidth
 })
@@ -295,14 +187,6 @@ const viewportStyle = computed(() => {
 function runSpec () {
   autStore.setScriptError(null)
   UnifiedRunnerAPI.executeSpec(props.activeSpec)
-}
-
-function handleResizeEnd (pane: 'reporter' | 'specsList') {
-  if (pane === 'reporter') {
-    preferences.update('reporterWidth', Math.round(reporterWidth.value))
-  } else {
-    preferences.update('specsListWidth', Math.round(specsListWidth.value))
-  }
 }
 
 let fileToOpen: FileDetails
@@ -359,7 +243,8 @@ onMounted(() => {
 
   eventManager.on('save:app:state', (state) => {
     preferences.update('isSpecsListOpen', state.isSpecsListOpen)
-    reporterHandleX.value = state.isSpecsListOpen ? runnerUiStore.specsListWidth : 0
+    // TODO: set initial values here from saved state
+    // reporterHandleX.value = state.isSpecsListOpen ? runnerUiStore.specsListWidth : 0
     preferences.update('autoScrollingEnabled', state.autoScrollingEnabled)
   })
 
