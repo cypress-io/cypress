1f80ed6211ec60e05c14d1173a5598b7903e6bfc
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-13 12:08:29 +1000

    remove old script

--- npm/react/examples/run.sh
+++
@@ -1,4 +0,0 @@
-find ./ -depth -name "*spec.js" -exec sh -c 'mv "$1" "${1%spec.js}cy.js"' _ {} \;
-find ./ -depth -name "*spec.ts" -exec sh -c 'mv "$1" "${1%spec.ts}cy.ts"' _ {} \;
-find ./ -depth -name "*spec.jsx" -exec sh -c 'mv "$1" "${1%spec.jsx}cy.jsx"' _ {} \;
-find ./ -depth -name "*spec.tsx" -exec sh -c 'mv "$1" "${1%spec.tsx}cy.tsx"' _ {} \;
