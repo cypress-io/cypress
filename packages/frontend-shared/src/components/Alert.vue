a2a7bb30de432ae4d1f191972208d16c198bd50d
Tyler Biethman <tbiethman@gmail.com>
2021-12-13 10:12:04 -0600

    Updating tests with data-cy usage

--- packages/frontend-shared/src/components/Alert.vue
+++ packages/frontend-shared/src/components/Alert.vue
@@ -27,7 +27,7 @@
           :prefix-icon-class="open ? prefix?.classes + ' rotate-180' : prefix?.classes"
           :suffix-icon-aria-label="props.dismissible ? t('components.alert.dismissAriaLabel') : ''"
           :suffix-icon="props.dismissible ? DeleteIcon : null"
-          data-testid="alert"
+          data-cy="alert"
           class="rounded min-w-200px p-16px"
           @suffixIconClicked="$emit('update:modelValue', !modelValue)"
         >
@@ -52,7 +52,7 @@
         </AlertHeader>
         <div
           v-if="open"
-          data-testid="alert-body-divider"
+          data-cy="alert-body-divider"
           class="mx-auto h-1px transform w-[calc(100%-32px)] translate-y-1px"
           :class="[classes.dividerClass]"
         />
