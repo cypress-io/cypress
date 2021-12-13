40720678cae7cec3659a641637aea8a47048aa21
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-13 18:29:24 +1000

    snaps

--- packages/config/__snapshots__/index_spec.js
+++ packages/config/__snapshots__/index_spec.js
@@ -16,11 +16,13 @@ exports['src/index .getDefaultValues returns list of public config keys 1'] = {
   "blockHosts": null,
   "chromeWebSecurity": true,
   "clientCertificates": [],
-  "component": {},
+  "component": {
+    "specPattern": "**/*.cy.{js,jsx,ts,tsx}"
+  },
   "defaultCommandTimeout": 4000,
   "downloadsFolder": "cypress/downloads",
   "e2e": {
-    "specPattern": "cypress/integration/**/*"
+    "specPattern": "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
   },
   "env": {},
   "execTimeout": 60000,
