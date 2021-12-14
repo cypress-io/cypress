660be23f1f47b9016492360b9d623e73541ce79c
Mark Noonan <mark@cypress.io>
2021-12-14 09:44:24 -0500

    improve test organization

--- packages/app/src/runner/ResizablePanels.spec.tsx
+++ packages/app/src/runner/ResizablePanels.spec.tsx
@@ -1,13 +1,16 @@
 import ResizablePanels from './ResizablePanels.vue'
 
-const panelSlots = {
-  panel1: () => <div class="h-full bg-emerald-100">panel1</div>,
-  panel2: () => <div class="h-full bg-purple-300">panel2</div>,
-  panel3: () => <div class="flex-grow h-full bg-indigo-100">panel3</div>,
-}
-
+// default values
 const defaultPanel1Width = 280
 const defaultPanel2Width = 320
+const minPanel1Width = 100
+const minPanel2Width = 100
+const minPanel3Width = 500
+
+// helpers
+const assertWidth = (panel: 'panel1' | 'panel2' | 'panel3', width: number) => {
+  cy.contains(panel).invoke('outerWidth').should('eq', width)
+}
 
 const dragHandleToClientX = (panel: 'panel1' | 'panel2', x: number) => {
   cy.get(`[data-cy="${panel}ResizeHandle"]`).trigger('mousedown', { eventConstructor: 'MouseEvent' })
@@ -15,131 +18,116 @@ const dragHandleToClientX = (panel: 'panel1' | 'panel2', x: number) => {
   .trigger('mouseup', { eventConstructor: 'MouseEvent' })
 }
 
-const assertWidth = (panel: 'panel1' | 'panel2' | 'panel3', width: number) => {
-  cy.contains(panel).invoke('outerWidth').should('eq', width)
+// slot content
+const slotContents = {
+  panel1: () => <div class="h-full bg-emerald-100">panel1</div>,
+  panel2: () => <div class="h-full bg-purple-300">panel2</div>,
+  panel3: () => <div class="flex-grow h-full bg-indigo-100">panel3</div>,
 }
 
 describe('<ResizablePanels />', { viewportWidth: 1500, defaultCommandTimeout: 4000 }, () => {
-  const minPanel1Width = 100
-  const minPanel2Width = 100
-  const minPanel3Width = 500
-
-  it('playground with default slot and props', () => {
-    cy.mount(() => <div class="h-screen"><ResizablePanels/></div>)
-  })
-
-  it('the panels can be resized', () => {
-    cy.mount(() => (
-      <div class="h-screen">
-        <ResizablePanels
-          maxTotalWidth={1500}
-          v-slots={panelSlots}
-        />
-      </div>))
-
-    assertWidth('panel1', defaultPanel1Width)
-    dragHandleToClientX('panel1', 500)
-    assertWidth('panel1', 500)
-    dragHandleToClientX('panel1', 400)
-    assertWidth('panel1', 400)
-
-    assertWidth('panel2', defaultPanel2Width)
-    dragHandleToClientX('panel2', 800)
-    assertWidth('panel2', 400)
-    dragHandleToClientX('panel2', 700)
-    assertWidth('panel2', 300)
-  })
-
-  it('the minium values are respected across a range of user actions', () => {
-    cy.mount(() => (
-      <div class="h-screen">
-        <ResizablePanels
-          maxTotalWidth={1500}
-          v-slots={panelSlots}
-          minPanel1Width={minPanel1Width}
-          minPanel2Width={minPanel2Width}
-          minPanel3Width={minPanel3Width}
-        />
-      </div>))
-
-    // drag panel 1 to its minimum width and attempt to go below it
-    assertWidth('panel1', defaultPanel1Width)
-    dragHandleToClientX('panel1', 100)
-    dragHandleToClientX('panel1', 99)
-    assertWidth('panel1', minPanel1Width)
-    dragHandleToClientX('panel1', 50)
-    assertWidth('panel1', minPanel1Width)
-
-    // drag panel 2 to its minimum width and attempt to go below it
-    assertWidth('panel2', defaultPanel2Width)
-    dragHandleToClientX('panel2', 200)
-    dragHandleToClientX('panel2', 199)
-    assertWidth('panel2', minPanel2Width)
-    dragHandleToClientX('panel2', 50)
-    assertWidth('panel2', minPanel2Width)
-
-    // panels 1 and 2 are at the minimum of 100 each,
-    // so panel 3 should be 1300px wide
-    assertWidth('panel3', 1300)
-
-    // drag panel 1 to its maximum width
-    // this should compress panel 3 to its minimum
-    // and have no effect on panel 2
-    dragHandleToClientX('panel1', 900)
-    assertWidth('panel3', minPanel3Width)
-    dragHandleToClientX('panel1', 901)
-    assertWidth('panel3', minPanel3Width)
-    dragHandleToClientX('panel1', 1000)
-    assertWidth('panel3', minPanel3Width)
-    assertWidth('panel2', minPanel2Width)
-
-    // even though we attempted to drag it beyond 900px
-    // panel 1 should not have gotten wider
-    assertWidth('panel1', 900)
-
-    // now we reduce the size of panel 1 so that we can
-    // expand panel 2, and make sure it does not
-    // allow expanding so much that it shrinks panel 3
-    // below the minimum
-    dragHandleToClientX('panel1', 700)
-    assertWidth('panel3', 700)
-    assertWidth('panel2', minPanel2Width)
-    assertWidth('panel1', 700)
-    dragHandleToClientX('panel2', 1000)
-    assertWidth('panel2', 300)
-    assertWidth('panel3', minPanel3Width)
-    dragHandleToClientX('panel2', 1200)
-
-    // attempting to drag the handle of panel 2 from 1000px
-    // to 1200px should have no effect on the size of anything
-    assertWidth('panel1', 700)
-    assertWidth('panel2', 300)
-    assertWidth('panel3', minPanel3Width)
-  })
-
-  it('handles being offset by some distance on the left', () => {
-    cy.mount(() => (
-      <div class="flex">
-        <div class='bg-green-600 text-white w-100px'>sidebar</div>
-        <div class="flex-grow h-screen">
+  describe('the panels resize as expected', () => {
+    beforeEach(() => {
+      cy.mount(() => (
+        <div class="h-screen">
           <ResizablePanels
             maxTotalWidth={1500}
-            v-slots={panelSlots}
-            offsetLeft={100}
+            v-slots={slotContents}
+            initialPanel1Width={defaultPanel1Width}
+            initialPanel2Width={defaultPanel2Width}
+            minPanel1Width={minPanel1Width}
+            minPanel2Width={minPanel2Width}
+            minPanel3Width={minPanel3Width}
           />
-        </div>
-      </div>))
+        </div>))
+    })
 
-    assertWidth('panel1', defaultPanel1Width)
-    assertWidth('panel2', defaultPanel2Width)
-    dragHandleToClientX('panel1', 400)
-    assertWidth('panel1', 300)
-    dragHandleToClientX('panel1', 300)
-    assertWidth('panel1', 200)
+    it('the panels can be resized', () => {
+      assertWidth('panel1', defaultPanel1Width)
+      dragHandleToClientX('panel1', 500)
+      assertWidth('panel1', 500)
+      dragHandleToClientX('panel1', 400)
+      assertWidth('panel1', 400)
 
-    dragHandleToClientX('panel2', 600)
-    assertWidth('panel2', 300)
-    dragHandleToClientX('panel2', 580)
-    assertWidth('panel2', 280)
+      assertWidth('panel2', defaultPanel2Width)
+      dragHandleToClientX('panel2', 800)
+      assertWidth('panel2', 400)
+      dragHandleToClientX('panel2', 700)
+      assertWidth('panel2', 300)
+    })
+
+    it('panel 1 can be resized between its minimum allowed width and maximum available space', () => {
+      // drag panel 1 to its minimum width and attempt to go below it
+      assertWidth('panel1', defaultPanel1Width)
+      dragHandleToClientX('panel1', 100)
+      dragHandleToClientX('panel1', 99)
+      assertWidth('panel1', minPanel1Width)
+      dragHandleToClientX('panel1', 50)
+      assertWidth('panel1', minPanel1Width)
+
+      // drag panel 1 to the maximum space available and attempt to go above it
+      dragHandleToClientX('panel1', 680)
+      dragHandleToClientX('panel1', 691)
+      assertWidth('panel1', 680)
+      dragHandleToClientX('panel1', 700)
+      assertWidth('panel1', 680)
+
+      // panel 2 was not reduced
+      assertWidth('panel2', defaultPanel2Width)
+
+      // panel 3 reached its minimum allowed size
+      assertWidth('panel3', minPanel3Width)
+    })
+
+    it('panel 2 can be resized between its minimum allowed width and maximum available space', () => {
+      // drag panel 2 to its minimum width and attempt to go below it
+      assertWidth('panel2', defaultPanel2Width)
+      dragHandleToClientX('panel2', 380)
+      dragHandleToClientX('panel2', 279)
+      assertWidth('panel2', minPanel2Width)
+      dragHandleToClientX('panel2', 50)
+      assertWidth('panel2', minPanel2Width)
+
+      // drag panel 2 to the maximum space available and attempt to go above it
+      dragHandleToClientX('panel2', 1000)
+      dragHandleToClientX('panel2', 1001)
+      assertWidth('panel2', 720)
+      dragHandleToClientX('panel2', 1100)
+      assertWidth('panel2', 720)
+
+      // panel 1 was not reduced
+      assertWidth('panel1', defaultPanel1Width)
+
+      // panel 3 reached its minimum allowed size
+      assertWidth('panel3', minPanel3Width)
+    })
+  })
+
+  describe('when there is a side nav', () => {
+    it('handles being offset by some distance on the left', () => {
+      cy.mount(() => (
+        <div class="flex">
+          <div class='bg-green-600 text-white w-100px'> 100px wide sidebar</div>
+          <div class="flex-grow h-screen">
+            <ResizablePanels
+              maxTotalWidth={1500}
+              v-slots={slotContents}
+              offsetLeft={100}
+            />
+          </div>
+        </div>))
+
+      assertWidth('panel1', defaultPanel1Width)
+      assertWidth('panel2', defaultPanel2Width)
+      dragHandleToClientX('panel1', 400)
+      assertWidth('panel1', 300)
+      dragHandleToClientX('panel1', 300)
+      assertWidth('panel1', 200)
+
+      dragHandleToClientX('panel2', 600)
+      assertWidth('panel2', 300)
+      dragHandleToClientX('panel2', 580)
+      assertWidth('panel2', 280)
+    })
   })
 })
