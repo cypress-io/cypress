f8582163659245d0f6367567f8c5aaa5503e5ec5
ElevateBart <ledouxb@gmail.com>
2021-12-15 16:13:28 -0600

    test: add a test for CodeTag bg

--- packages/frontend-shared/src/components/CodeTag.spec.tsx
+++ packages/frontend-shared/src/components/CodeTag.spec.tsx
@@ -7,9 +7,15 @@ describe('<CodeTag />', () => {
     </p>))
   })
 
-  it('looks good for code colored', () => {
+  it('displays colored code', () => {
     cy.mount(() => (<p>
       Create a <CodeTag class="text-red-500">sample_spec.js</CodeTag> file.
+    </p>))
+  })
+
+  it('displays colored code on a background', () => {
+    cy.mount(() => (<p>
+      Create a <CodeTag class="bg-red-50 text-red-500" bg>sample_spec.js</CodeTag> file.
     </p>))
   })
 })
