a04a15da1c0e50a12b4a5d870bca900fb356067f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-15 10:52:48 +1000

    fix react example

--- npm/react/examples/webpack-file/cypress.config.js
+++ npm/react/examples/webpack-file/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = {
   'viewportWidth': 500,
   'viewportHeight': 500,
   'component': {
+    'specPattern': 'src/**/*.cy.js',
     setupNodeEvents (on, config) {
       require('@cypress/react/plugins/load-webpack')(on, config, {
         // from the root of the project (folder with cypress.config.{ts|js} file)
