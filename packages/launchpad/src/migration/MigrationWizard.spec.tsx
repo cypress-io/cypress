0a96da946c83d2b3dc420507aad9ae32ecbeb6b8
ElevateBart <ledouxb@gmail.com>
2021-12-15 09:56:50 -0600

    test: add test for all three steps

--- packages/launchpad/src/migration/MigrationWizard.spec.tsx
+++ packages/launchpad/src/migration/MigrationWizard.spec.tsx
@@ -1,9 +1,38 @@
 import { MigrationWizardFragmentDoc } from '../generated/graphql-test'
 import MigrationWizard from './MigrationWizard.vue'
 
-describe('<MigrationWizard/>', { viewportWidth: 1119 }, () => {
-  it('renders expected content', () => {
+describe('<MigrationWizard/>', { viewportWidth: 1280, viewportHeight: 1100 }, () => {
+  it('renders Automatic rename', () => {
     cy.mountFragment(MigrationWizardFragmentDoc, {
+      onResult (res) {
+        res.step = 'renameAuto'
+      },
+      render (gql) {
+        return (<div class="p-16px">
+          <MigrationWizard gql={gql} />
+        </div>)
+      },
+    })
+  })
+
+  it('renders Manual rename', () => {
+    cy.mountFragment(MigrationWizardFragmentDoc, {
+      onResult (res) {
+        res.step = 'renameManual'
+      },
+      render (gql) {
+        return (<div class="p-16px">
+          <MigrationWizard gql={gql} />
+        </div>)
+      },
+    })
+  })
+
+  it('renders Config File migration', () => {
+    cy.mountFragment(MigrationWizardFragmentDoc, {
+      onResult (res) {
+        res.step = 'configFile'
+      },
       render (gql) {
         return (<div class="p-16px">
           <MigrationWizard gql={gql} />
