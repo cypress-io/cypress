094d39054ffc4a74d97984ff3f16300251b9d73d
Emily Rohrbough <emilyrohrbough@yahoo.com>
2021-12-16 11:48:33 -0600

    does windows like this better? some reason passing --cwd /tmp/testing as C:/Users/circleci/AppData/Local/Temp/testing instead of C:/tmp/testing

--- scripts/test-unique-npm-and-binary.js
+++ scripts/test-unique-npm-and-binary.js
@@ -1,11 +1,27 @@
+/* eslint-disable no-console */
 const minimist = require('minimist')
 const options = minimist(process.argv)
 const la = require('lazy-ass')
+const fs = require('fs-extra')
 const is = require('check-more-types')
 const execa = require('execa')
 const { getNameAndBinary } = require('./utils')
 
-/* eslint-disable no-console */
+const cwd = options.cwd || '/tmp/testing'
+
+fs.ensureDirSync(cwd)
+
+console.log('Create Dummy Project')
+execa('npm init -y', {
+  cwd,
+  shell: true,
+  stdio: 'inherit',
+})
+.then(console.log)
+.catch((e) => {
+  console.error(e)
+  process.exit(1)
+})
 
 const { npm, binary } = getNameAndBinary(process.argv)
 
@@ -14,10 +30,8 @@ la(is.unemptyString(binary), 'missing binary url')
 
 console.log('testing NPM from', npm)
 console.log('and binary from', binary)
-const cwd = options.cwd
 
-console.log('in (provided', cwd)
-console.log('in (node cwd)', process.cwd())
+console.log('in', cwd)
 
 execa(`npm install ${npm}`, {
   cwd,
@@ -34,7 +48,7 @@ execa(`npm install ${npm}`, {
 })
 
 console.log('Verify Cypress binary')
-execa(`$(yarn bin cypress) verify`, {
+execa('$(yarn bin cypress) verify', {
   cwd,
   shell: true,
   stdio: 'inherit',
