816dac8a956c7ae0a91611120b145fe13444bef0
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-17 03:25:47 +1000

    refactor: inject websocket to event manager (#19396)
    * update
    
    * inject web socket
    
    * yarn lock
    
    * fix types
    
    * update test code

--- packages/app/cypress/component/support/ctSupport.ts
+++ packages/app/cypress/component/support/ctSupport.ts
@@ -4,8 +4,15 @@ import { logger } from '@packages/runner-shared/src/logger'
 import { blankContents } from '@packages/runner-shared/src/blank-contents'
 import _ from 'lodash'
 import { visitFailure } from '@packages/runner-shared/src/visit-failure'
+import type { Socket } from '@packages/socket/lib/browser'
 
 class StudioRecorderMock {}
+
+export const StubWebsocket = new Proxy<Socket>(Object.create(null), {
+  get: (obj, prop) => {
+    throw Error(`Cannot access ${String(prop)} on StubWebsocket!`)
+  },
+})
 
 // Event manager with Cypress driver dependencies stubbed out
 // Useful for component testing
@@ -16,6 +23,7 @@ export const createEventManager = () => {
     null, // MobX, also not needed in Vue CT tests,
     null, // selectorPlaygroundModel,
     StudioRecorderMock, // needs to be a valid class
+    StubWebsocket,
   )
 }
 
