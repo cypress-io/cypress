f5673a20545849b2270b4643c24a7db3058c04c0
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-12-17 02:35:48 +1000

    update test code

--- packages/runner-shared/test/utils.ts
+++ packages/runner-shared/test/utils.ts
@@ -1,9 +1,15 @@
 import $Cypress from '@packages/driver'
 import { EventManager } from '@packages/app/src/runner/event-manager'
-import { StubWebsocket } from '@packages/app/cypress/component/support/ctSupport'
+import type { Socket } from '@packages/socket/lib/browser'
 import { StudioRecorder } from '../src/studio'
 import { selectorPlaygroundModel } from '../src/selector-playground'
 import * as MobX from 'mobx'
+
+export const StubWebsocket = new Proxy<Socket>(Object.create(null), {
+  get: (obj, prop) => {
+    throw Error(`Cannot access ${String(prop)} on StubWebsocket!`)
+  },
+})
 
 export const createEventManager = () => {
   return new EventManager(
