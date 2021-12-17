2008a61e62532af8511071d73062c5d953dd95b3
ElevateBart <ledouxb@gmail.com>
2021-12-17 14:50:48 -0600

    feat: add a favicon to the app

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,4 +1,5 @@
 import { makeConfig } from '../frontend-shared/vite.config'
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon'
 import Layouts from 'vite-plugin-vue-layouts'
 import Pages from 'vite-plugin-pages'
 
@@ -18,5 +19,18 @@ export default makeConfig({
   plugins: [
     Layouts(),
     Pages({ extensions: ['vue'] }),
+    ViteFaviconsPlugin({
+      logo: require.resolve('@packages/frontend-shared/src/assets/logos/cypress-dark.png'),
+      favicons: {
+        icons: {
+          android: false, // Create Android homescreen icon. `boolean` or `{ offset, background }` or an array of sources
+          appleIcon: false, // Create Apple touch icons. `boolean` or `{ offset, background }` or an array of sources
+          appleStartup: false, // Create Apple startup images. `boolean` or `{ offset, background }` or an array of sources
+          favicons: true, // Create regular favicons. `boolean` or `{ offset, background }` or an array of sources
+          windows: false, // Create Windows 8 tile icons. `boolean` or `{ offset, background }` or an array of sources
+          yandex: false, // Create Yandex browser icon. `boolean` or `{ offset, background }` or an array of sources
+        },
+      },
+    }),
   ],
 })
