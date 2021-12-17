04c6f13b2c528512b2a1a76e84f4e0bf8bf187ca
BlueWinds <blue@everblue.info>
2021-12-17 11:36:18 -0800

    Bring up to date with 10.0-release changes

--- packages/launchpad/cypress/e2e/integration/project-setup.spec.ts
+++ packages/launchpad/cypress/e2e/integration/project-setup.spec.ts
@@ -2,27 +2,28 @@ import { WIZARD_STEPS } from '@packages/types/src/constants'
 import defaultMessages from '@packages/frontend-shared/src/locales/en-US.json'
 
 describe('Launchpad: Setup Project', () => {
-  it('no initial setup displays welcome page', () => {
-    cy.setupE2E('todos')
-    cy.visitLaunchpad()
+  before(() => {
+    cy.scaffoldProject('todos')
+  })
 
+  beforeEach(() => {
+    cy.openProject('todos')
+    cy.visitLaunchpad()
+  })
+
+  it('no initial setup displays welcome page', () => {
     cy.contains(WIZARD_STEPS.find((s) => s.type === 'welcome').title).should('be.visible')
     cy.contains('E2E Testing').should('be.visible')
     cy.contains('Component Testing').should('be.visible')
   })
 
-  it('welcome page has button to learn about testing types', () => {
-    cy.setupE2E('todos')
-    cy.visitLaunchpad()
+  it('welcome page has link to learn about testing types', () => {
+    cy.intercept('POST', 'mutation-ExternalLink_OpenExternal').as('OpenExternal')
 
     cy.contains(defaultMessages.welcomePage.review).click()
-
     cy.contains('Key Differences').should('be.visible')
+    cy.contains('Need help?').click()
 
-    cy.intercept('POST', 'mutation-GlobalPage_OpenInFinder').as('OpenInFinder')
-
-    cy.contais('button', 'Need help?').click()
-
-    cy.wait('@OpenInFinder')
+    cy.wait('@OpenExternal')
   })
 })
