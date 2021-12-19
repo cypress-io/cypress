e7cfc351cf18cc5c8b426977cc88a4a09db143a1
ElevateBart <ledouxb@gmail.com>
2021-12-18 21:52:25 -0600

    fix: make vite-dev-server config detect ct

--- npm/vite-dev-server/cypress.config.ts
+++ npm/vite-dev-server/cypress.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  'pluginsFile': 'cypress/plugins.js',
-  'video': false,
-  'fixturesFolder': false,
-  'testFiles': '**/*.spec.*',
-  'componentFolder': 'cypress/components',
-  'component': {
+  pluginsFile: 'cypress/plugins.js',
+  video: false,
+  fixturesFolder: false,
+  testFiles: '**/*.spec.*',
+  componentFolder: 'cypress/components',
+  component: {
+    testFiles: '**/*.spec.*',
     devServer (cypressConfig) {
       const path = require('path')
       const { startDevServer } = require('./dist')
