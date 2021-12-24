bc0832ed4a303f6da9e9e6f5f694ac82148fb9f6
ElevateBart <ledouxb@gmail.com>
2021-12-24 16:20:19 -0600

    add tests for codetag

--- packages/frontend-shared/src/components/CodeTag.spec.tsx
+++ packages/frontend-shared/src/components/CodeTag.spec.tsx
@@ -5,11 +5,15 @@ describe('<CodeTag />', () => {
     cy.mount(() => (<p>
       Create a <CodeTag>sample_spec.js</CodeTag> file.
     </p>))
+
+    cy.contains('sample_spec.js').should('be.visible')
   })
 
   it('looks good for code colored', () => {
     cy.mount(() => (<p>
       Create a <CodeTag class="text-red-500">sample_spec.js</CodeTag> file.
     </p>))
+
+    cy.contains('sample_spec.js').should('be.visible')
   })
 })
