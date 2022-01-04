65973b16f2eb3718c038122e75fbf21a5b618222
Paul Jaffre <jaffrepaul@gmail.com>
2022-01-04 13:55:04 -0500

    added index ref & cleanup

--- packages/driver/src/cy/commands/actions/mount.ts
+++ packages/driver/src/cy/commands/actions/mount.ts
@@ -1,7 +1,7 @@
 import $errUtils from '../../../cypress/error_utils'
 
 export default (Commands) => {
-  return Commands.addAll({ prevSubject: 'element' }, {
+  return Commands.addAll({
     mount () {
       return $errUtils.throwErrByPath('mount.not_implemented')
     },
