157dab25d98ca4877ca9da3b7d6f0c7328125ee4
Mark Noonan <mark@cypress.io>
2022-01-05 13:28:18 -0500

    tweak flaky assertion

--- packages/app/cypress/e2e/integration/sidebar_navigation.spec.ts
+++ packages/app/cypress/e2e/integration/sidebar_navigation.spec.ts
@@ -31,7 +31,7 @@ describe('Sidebar Navigation', () => {
   it('has unlabeled menu item that shows the keyboard shortcuts modal (unexpanded state)', () => {
     cy.get('[data-cy="keyboard-shortcuts"]').should('be.visible')
     cy.get('[data-cy="keyboard-shortcuts"]').click()
-    cy.get('h2').findByText('Keyboard Shortcuts').should('be.visible')
+    cy.contains('h2', 'Keyboard Shortcuts').should('be.visible')
     cy.get('li p').contains('Re-run tests').should('be.visible')
     cy.get('li p').contains('Stop tests').should('be.visible')
     cy.get('li p').contains('Toggle specs list').should('be.visible')
