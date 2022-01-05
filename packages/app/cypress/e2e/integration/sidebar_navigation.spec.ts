6c4f595c3b4808c3081bd9534597fb902fb95dde
Mark Noonan <mark@cypress.io>
2022-01-05 13:26:48 -0500

    tweak flaky assertion

--- packages/app/cypress/e2e/integration/sidebar_navigation.spec.ts
+++ packages/app/cypress/e2e/integration/sidebar_navigation.spec.ts
@@ -104,7 +104,7 @@ describe('Sidebar Navigation', () => {
     cy.get('[aria-expanded]').should('have.attr', 'aria-expanded', 'true')
     cy.get('[data-cy="keyboard-shortcuts"]').should('be.visible')
     cy.get('[data-cy="keyboard-shortcuts"]').click()
-    cy.findByText('Keyboard Shortcuts').should('be.visible')
+    cy.contains('h2', 'Keyboard Shortcuts').should('be.visible')
     cy.get('li p').contains('Re-run tests').should('be.visible')
     cy.get('li p').contains('Stop tests').should('be.visible')
     cy.get('li p').contains('Toggle specs list').should('be.visible')
