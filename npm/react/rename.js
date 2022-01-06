fb9116d53419fce4c21296868905c50520a1e2c1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-07 09:35:34 +1000

    wip

--- npm/react/rename.js
+++ npm/react/rename.js
@@ -5,7 +5,7 @@ const glob = require('globby')
 const torep = [
   { old: '/cy-cy.js', new: '/spec.cy.js', },
   { old: '.cy-cy.js', new: '.cy.js', },
-  { old: '-cy.js', new: '-cy.js', },
+  { old: '-cy.js', new: '.cy.js', },
   { old: '-cy.jsx', new: '.cy.jsx', },
   { old: '-cy.tsx', new: '.cy.tsx', },
   { old: '-cy.ts', new: '.cy.ts', },
@@ -17,8 +17,10 @@ glob('./**/*cy.js', { onlyFiles: true }).then(arr => {
   for (const a of arr) {
     for (const repl of torep) {
       if (a.endsWith(repl.old)) {
-      const n = a.replace(repl.old, repl.new)
-      towrite.push({ a, n })
+        const n = a.replace(repl.old, repl.new)
+        if (a !== n) {
+          towrite.push({ a, n })
+        }
       }
     }
   }
