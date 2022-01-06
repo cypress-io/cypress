2c0536174b74723c219223484d075ad82dc6ef9c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-07 09:29:01 +1000

    wip

--- npm/react/rename.js
+++ npm/react/rename.js
@@ -2,22 +2,22 @@ const fs = require('fs')
 const path = require('path')
 const glob = require('globby')
 
+const torep = [
+  { old: '/cy-cy.js', new: '/spec.cy.js', },
+  { old: '.cy-cy.js', new: '.cy.js', },
+  { old: '-cy.js', new: '-cy.js', },
+  { old: '-cy.jsx', new: '.cy.jsx', },
+  { old: '-cy.tsx', new: '.cy.tsx', },
+  { old: '-cy.ts', new: '.cy.ts', },
+]
 glob('./**/*cy.js').then(arr => {
   console.log(arr)
 
-  let i = 0
   const towrite = []
   for (const a of arr) {
-  console.log(i)
-  i++
-    if (a.endsWith('/cy-cy.js')) {
-      const n = a.replace('/cy-cy.js', '/spec.cy.js')
+    for (const repl of torep) {
+      const n = a.replace(repl.old, repl.new)
       towrite.push({ a, n })
-    } else if (a.endsWith('.cy-cy.js')) {
-      const n = a.replace('.cy-cy.js', '.cy.js')
-      towrite.push({ a, n })
-    } else {
-      towrite.push({ a, n: a })
     }
   }
 
