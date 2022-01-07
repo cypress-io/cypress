b6b23030f2e2be454d646c254b849c9dd5c5c1f1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-07 10:04:39 +1000

    mass rename

--- npm/react/rename.js
+++ npm/react/rename.js
@@ -1,30 +1,30 @@
 const fs = require('fs')
-const path = require('path')
 const glob = require('globby')
 
 const torep = [
-  { old: '/cy-cy.jsx', new: '/spec.cy.jsx', },
-  { old: '/cy-cy.js', new: '/spec.cy.js', },
-  { old: 'cy-cy.jsx', new: 'cy.jsx', },
-  { old: 'cy-cy.js', new: 'cy.js', },
-  { old: 'cy-cy.jsx', new: 'cy.jsx', },
-  { old: '.cy-cy.tsx', new: '.cy.tsx', },
-  { old: '-cy.jsx', new: '.cy.jsx', },
-  { old: '-cy.js', new: '.cy.js', },
-  { old: '-cy.tsx', new: '.cy.tsx', },
-  { old: '-cy.ts', new: '.cy.ts', },
+  { old: '/cy-cy.jsx', new: '/spec.cy.jsx' },
+  { old: '/cy-cy.js', new: '/spec.cy.js' },
+  { old: 'cy-cy.jsx', new: 'cy.jsx' },
+  { old: 'cy-cy.js', new: 'cy.js' },
+  { old: 'cy-cy.jsx', new: 'cy.jsx' },
+  { old: '.cy-cy.tsx', new: '.cy.tsx' },
+  { old: '-cy.jsx', new: '.cy.jsx' },
+  { old: '-cy.js', new: '.cy.js' },
+  { old: '-cy.tsx', new: '.cy.tsx' },
+  { old: '-cy.ts', new: '.cy.ts' },
 ]
 
-glob(['./**/*cy-{js,jsx,ts,tsx}', './**/*-cy.{js,jsx,ts,jsx}'], { onlyFiles: true }).then(arr => {
+glob(['./**/*cy-{js,jsx,ts,tsx}', './**/*-cy.{js,jsx,ts,jsx}'], { onlyFiles: true }).then((arr) => {
   // console.log(arr)
   const f = new Set()
 
   const towrite = []
+
   for (const a of new Set(arr)) {
     for (const repl of torep) {
-
       if (a.endsWith(repl.old) && !f.has(a)) {
         let n = a.replace(repl.old, repl.new)
+
         if (a !== n) {
           f.add(a)
           towrite.push({ a, n })
@@ -34,14 +34,12 @@ glob(['./**/*cy-{js,jsx,ts,tsx}', './**/*-cy.{js,jsx,ts,jsx}'], { onlyFiles: tru
     }
   }
 
-
   for (const { a, n } of towrite) {
     try {
       console.log(`Renaming: \n${a}\n${n}`)
-      // fs.renameSync(a, n)
+      fs.renameSync(a, n)
     } catch (e) {
       console.log('error for ', a, n)
     }
   }
 })
-
