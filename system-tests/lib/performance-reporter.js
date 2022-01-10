3f369e21aaa14de786bd080adac4849e89bd1dc7
BlueWinds <blue@everblue.info>
2022-01-10 14:01:51 -0800

    Run test timing reporter for all system tests

--- system-tests/lib/performance-reporter.js
+++ system-tests/lib/performance-reporter.js
@@ -2,16 +2,16 @@ const path = require('path')
 const chalk = require('chalk')
 const mochaReporters = require('mocha-7.0.1/lib/reporters')
 const StatsD = require('hot-shots')
+const version = require('../../package.json').version
 
 class StatsdReporter extends mochaReporters.Spec {
   constructor (runner) {
     super(runner)
     const statsD = new StatsD({})
-    let startTime; let testTime
+    let testTime
 
     runner.on('start', () => {
       console.log(chalk.bold.green('  Reporting performance to statsd'))
-      startTime = Date.now()
     })
 
     runner.on('test', () => {
@@ -21,19 +21,9 @@ class StatsdReporter extends mochaReporters.Spec {
     runner.on('test end', (test) => {
       const specName = path.basename(runner.suite.file).replace(/\W/g, '_')
       const testTitle = test.title.replace(/\W/g, '_')
-      const statsdPath = `performance.${specName}.${testTitle}`
+      const statsdPath = `performance.${version}.${specName}.${testTitle}`
 
-      statsD.timing(`${statsdPath }.hooks`, test.timings.lifecycle)
-      statsD.timing(`${statsdPath }.test`, test.timings.test.fnDuration)
-      statsD.timing(`${statsdPath }.total`, Date.now() - testTime)
-    })
-
-    runner.on('end', (spec) => {
-      const specName = path.basename(runner.suite.file).replace(/\W/g, '_')
-      const statsdPath = `performance.${specName}`
-
-      statsD.timing(`${statsdPath }.mocha`, runner.stats.wallClockEndedAt - runner.stats.wallClockStartedAt)
-      statsD.timing(`${statsdPath }.total`, Date.now() - startTime)
+      statsD.timing(`${statsdPath}`, Date.now() - testTime)
     })
   }
 }
