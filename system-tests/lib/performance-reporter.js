15a9195b11dd10e8493a3bfb56cc1b36728b1dff
BlueWinds <blue@everblue.info>
2022-01-11 11:39:34 -0800

    Tweaks

--- system-tests/lib/performance-reporter.js
+++ system-tests/lib/performance-reporter.js
@@ -2,29 +2,31 @@ const path = require('path')
 const chalk = require('chalk')
 const mochaReporters = require('mocha-7.0.1/lib/reporters')
 const StatsD = require('hot-shots')
-const version = require('../../package.json').version
 
 class StatsdReporter extends mochaReporters.Spec {
   constructor (runner) {
     super(runner)
-    const statsD = new StatsD({})
-    let testTime
 
-    runner.on('start', () => {
-      console.log(chalk.bold.green('  Reporting performance to statsd'))
-    })
+    if (process.env.CYPRESS_STATSD_TIMINGS) {
+      const statsD = new StatsD({})
+      let testTime
 
-    runner.on('test', () => {
-      testTime = Date.now()
-    })
+      runner.on('start', () => {
+        console.log(chalk.bold.green('  Reporting performance to statsd'))
+      })
 
-    runner.on('test end', (test) => {
-      const specName = path.basename(runner.suite.file).replace(/\W/g, '_')
-      const testTitle = test.title.replace(/\W/g, '_')
-      const statsdPath = `performance.${version}.${specName}.${testTitle}`
+      runner.on('test', () => {
+        testTime = Date.now()
+      })
 
-      statsD.timing(`${statsdPath}`, Date.now() - testTime)
-    })
+      runner.on('test end', (test) => {
+        const specName = path.basename(runner.suite.file).replace(/\W/g, '_')
+        const testTitle = test.title.replace(/\W/g, '_')
+        const statsdPath = `cy_system_tests.${'branch'}.${specName}.${testTitle}`
+
+        statsD.timing(`${statsdPath}`, Date.now() - testTime)
+      })
+    }
   }
 }
 
