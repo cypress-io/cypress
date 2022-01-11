37ca062e3da7d1e0f1c6362cb46f9144825ae02d
Tyler Biethman <tbiethman@gmail.com>
2022-01-11 11:56:41 -0600

    Few more tweaks following up on config changes

--- system-tests/projects/no-specs/cypress.config.js
+++ system-tests/projects/no-specs/cypress.config.js
@@ -2,10 +2,8 @@ module.exports = {
   pluginsFile: false,
   component: {
     supportFile: false,
-    specPattern: 'cypress/component/**/*',
   },
   e2e: {
     supportFile: false,
-    specPattern: 'cypress/integration/**/*',
   },
 }
