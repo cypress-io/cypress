e0c1554cf7a840f6400a66f445b3d60319e31f6e
Ryan Manuel <ryanm@cypress.io>
2022-01-12 23:16:33 -0600

    Cleanup code

--- packages/extension/app/init.js
+++ packages/extension/app/init.js
@@ -1,4 +1,3 @@
-const browser = require('webextension-polyfill')
 const background = require('./background')
 
 const HOST = 'CHANGE_ME_HOST'
@@ -6,11 +5,3 @@ const PATH = 'CHANGE_ME_PATH'
 
 // immediately connect
 background.connect(HOST, PATH)
-
-browser.runtime.onMessageExternal.addListener((message) => {
-  if (message.host && message.path) {
-    return new Promise((resolve) => {
-      background.connect(message.host, message.path, {}, resolve)
-    })
-  }
-})
