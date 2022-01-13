d17f1ae3ef7cbccaa74afd62674bd52bf1854bd8
Emily Rohrbough <emilyr@cypress.io>
2022-01-13 17:21:42 -0600

    steal Tyler's change to modal dialog from his PR

--- packages/frontend-shared/src/components/StandardModalHeader.vue
+++ packages/frontend-shared/src/components/StandardModalHeader.vue
@@ -2,8 +2,11 @@
   <div
     class="sticky top-0 flex items-center justify-between bg-white rounded-t border-b-1px border-b-gray-100 min-h-56px px-24px z-1"
   >
-    <DialogTitle class="text-gray-900 text-18px">
-      <slot /> <span
+    <div>
+      <DialogTitle class="text-gray-900 text-18px inline-block">
+        <slot />
+      </DialogTitle>
+      <span
         v-if="helpLink.length"
         class="inline-block border-t border-t-gray-100 w-32px h-6px mx-8px"
       /> <ExternalLink
@@ -14,11 +17,10 @@
         <span class="group-hocus:underline">{{ helpText }}</span>
         <i-cy-circle-bg-question-mark_x16 class="relative inline-block icon-dark-indigo-500 icon-light-indigo-100 -top-2px ml-8px" />
       </ExternalLink>
-    </DialogTitle>
+    </div>
     <button
       aria-label="Close"
       class="border-transparent rounded-full p-5px border-1 hover:border-indigo-300 hocus-default"
-      type="button"
       @click="$emit('close')"
     >
       <i-cy-delete_x12 class="icon-dark-gray-400 w-12px h-12px m-4px" />
