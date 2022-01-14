94ca5ad169dbb2447964ea5766379766894da5d9
Mark Noonan <mark@cypress.io>
2022-01-14 12:37:30 -0500

    UNIFY-722

--- packages/frontend-shared/src/components/StatusBadge.cy.tsx
+++ packages/frontend-shared/src/components/StatusBadge.cy.tsx
@@ -3,7 +3,7 @@ import StatusBadge from './StatusBadge.vue'
 import Button from './Button.vue'
 
 describe('<StatusBadge />', () => {
-  it('playground', { viewportWidth: 500, viewportHeight: 200 }, () => {
+  it('renders and toggles', { viewportWidth: 500, viewportHeight: 200 }, () => {
     const status = ref(false)
 
     cy.mount(() => (
@@ -13,14 +13,16 @@ describe('<StatusBadge />', () => {
           titleOff="not configured"
           status={status.value}
         />
-        <br/>
+        <br /><br />
         {// @ts-ignore
         }<Button onClick={() => status.value = !status.value}>toggle</Button>
       </div>
     ))
 
     cy.findByText('not configured').should('be.visible')
+    cy.percySnapshot()
     cy.findByText('toggle').click()
     cy.findByText('configured').should('be.visible')
+    cy.percySnapshot()
   })
 })
