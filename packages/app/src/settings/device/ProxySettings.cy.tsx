135c9d9e42eebabc178931be6fbff46f86450e9e
ElevateBart <ledouxb@gmail.com>
2022-01-14 11:52:40 -0600

    test: UNIFY-704 proxy settings

--- packages/app/src/settings/device/ProxySettings.cy.tsx
+++ packages/app/src/settings/device/ProxySettings.cy.tsx
@@ -1,21 +1,37 @@
 import { ProxySettingsFragmentDoc } from '../../generated/graphql-test'
 import ProxySettings from './ProxySettings.vue'
 
-describe('<ProxySettings />', () => {
+describe('<ProxySettings />', {
+  viewportHeight: 400,
+  viewportWidth: 600,
+}, () => {
   it('renders', () => {
-    cy.viewport(400, 400)
-    .mountFragment(ProxySettingsFragmentDoc, {
+    cy.mountFragment(ProxySettingsFragmentDoc, {
       onResult: (ctx) => {
         ctx.localSettings.preferences.proxyServer = 'proxy-server'
         ctx.localSettings.preferences.proxyBypass = 'proxy-bypass'
       },
-      render: (gql) => <ProxySettings gql={gql} />,
+      render: (gql) => <div class="p-24px"><ProxySettings gql={gql} /></div>,
     })
 
     cy.findByText('Proxy Bypass List')
-    .get('[data-testid=bypass-list]').contains('proxy-bypass')
+    .get('[data-testid=bypass-list]').should('have.text', 'proxy-bypass')
 
     cy.findByText('Proxy Server')
-    .get('[data-testid=proxy-server]').contains('proxy-server')
+    .get('[data-testid=proxy-server]').should('have.text', 'proxy-server')
+
+    cy.percySnapshot()
+  })
+
+  it('renders empty', () => {
+    cy.mountFragment(ProxySettingsFragmentDoc, {
+      render: (gql) => <div class="p-24px"><ProxySettings gql={gql} /></div>,
+    })
+
+    cy.findByText('Proxy Bypass List')
+    .get('[data-testid=bypass-list]').should('have.text', '-')
+
+    cy.findByText('Proxy Server')
+    .get('[data-testid=proxy-server]').should('have.text', '-')
   })
 })
