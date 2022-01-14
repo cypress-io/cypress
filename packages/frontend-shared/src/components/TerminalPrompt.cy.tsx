7d21a5716377a1a46914be162d21d57ab3238a68
Mark Noonan <mark@cypress.io>
2022-01-14 11:27:48 -0500

    UNIFY-718

--- packages/frontend-shared/src/components/TerminalPrompt.cy.tsx
+++ packages/frontend-shared/src/components/TerminalPrompt.cy.tsx
@@ -1,19 +1,29 @@
 import TerminalPrompt from './TerminalPrompt.vue'
+import { defaultMessages } from '@cy/i18n'
 
 describe('<TerminalPrompt />', () => {
-  it('playground', { viewportWidth: 800, viewportHeight: 120 }, () => {
+  it('renders without overflow', { viewportWidth: 800, viewportHeight: 120 }, () => {
     cy.mount(() => (
       <div class="p-6">
         <TerminalPrompt projectFolderName="design-system" command="git add cypress.config.js"/>
       </div>
     ))
+    .percySnapshot()
   })
 
   it('overflows nicely', { viewportWidth: 800, viewportHeight: 120 }, () => {
+    const command = 'yarn workspace @packages/frontend-shared cypress:run --record --key 123as4d56asda987das'
+    const projectFolderName = 'design-system'
+
     cy.mount(() => (
       <div class="p-6">
-        <TerminalPrompt projectFolderName="design-system" command="yarn workspace @packages/frontend-shared cypress:run --record --key 123as4d56asda987das"/>
+        <TerminalPrompt projectFolderName={projectFolderName} command={command}/>
       </div>
     ))
+    .percySnapshot()
+
+    cy.contains(command)
+    cy.contains(projectFolderName)
+    cy.contains('button', defaultMessages.clipboard.copy).should('be.visible')
   })
 })
