7b8bff03dc9ef213ceb6d3183c4a3fdb4a8beb30
ElevateBart <ledouxb@gmail.com>
2022-01-14 17:46:28 -0600

    fix: update design for Alert.vue

--- packages/frontend-shared/cypress/e2e/support/e2eProjectDirs.ts
+++ packages/frontend-shared/cypress/e2e/support/e2eProjectDirs.ts
@@ -25,6 +25,7 @@ export const e2eProjectDirs = [
   'ids',
   'integration-outside-project-root',
   'issue-8111-iframe-input',
+  'kill-child-process',
   'launchpad',
   'max-listeners',
   'multiple-task-registrations',
