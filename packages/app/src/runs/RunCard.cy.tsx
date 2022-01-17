75ec63c53bb3ac460d0bbede856dbeb1219c2d80
Mark Noonan <mark@cypress.io>
2022-01-17 17:15:11 -0500

    UNIFY-734

--- packages/app/src/runs/RunCard.cy.tsx
+++ packages/app/src/runs/RunCard.cy.tsx
@@ -3,24 +3,62 @@ import { RunCardFragmentDoc } from '../generated/graphql-test'
 import RunCard from './RunCard.vue'
 
 describe('<RunCard />', { viewportHeight: 400 }, () => {
-  it('displays last commit info', () => {
-    cy.mountFragment(RunCardFragmentDoc, {
-      onResult (result) {
-        Object.keys(result).forEach((key) => {
-          result[key] = CloudRunStubs.allPassing[key]
-        })
-      },
-      render: (gqlVal) => {
-        return (
-          <div class="h-screen bg-gray-100 p-3">
-            <RunCard gql={gqlVal} />
-          </div>
-        )
-      },
-    })
+  context('when there is full commit info', () => {
+    it('displays last commit info', () => {
+      cy.mountFragment(RunCardFragmentDoc, {
+        onResult (result) {
+          Object.keys(result).forEach((key) => {
+            result[key] = CloudRunStubs.allPassing[key]
+          })
+        },
+        render: (gqlVal) => {
+          return (
+            <div class="h-screen bg-gray-100 p-3">
+              <RunCard gql={gqlVal} />
+            </div>
+          )
+        },
+      })
 
-    cy.contains(CloudRunStubs.allPassing.commitInfo?.authorName || '')
-    cy.contains(CloudRunStubs.allPassing.commitInfo?.summary || '')
-    cy.contains(CloudRunStubs.allPassing.commitInfo?.branch || '')
+      if (!CloudRunStubs.allPassing.commitInfo) {
+        throw new Error('RunCard spec did not successfully import commit info, so the test cannot be completed')
+      }
+
+      cy.contains(CloudRunStubs.allPassing.commitInfo.authorName as string)
+      .should('be.visible')
+
+      cy.contains(CloudRunStubs.allPassing.commitInfo.summary as string)
+      .should('be.visible')
+
+      cy.contains(CloudRunStubs.allPassing.commitInfo.branch as string)
+      .should('be.visible')
+
+      cy.percySnapshot()
+    })
+  })
+
+  context('when there is missing commit info', () => {
+    it('renders without errors', () => {
+      cy.mountFragment(RunCardFragmentDoc, {
+        onResult (result) {
+          Object.keys(result).forEach((key) => {
+            result[key] = CloudRunStubs.allPassing[key]
+          })
+
+          result.commitInfo = null
+        },
+        render: (gqlVal) => {
+          return (
+            <div class="h-screen bg-gray-100 p-3">
+              <RunCard gql={gqlVal} />
+            </div>
+          )
+        },
+      })
+
+      cy.contains('3:17:00 AM').should('be.visible')
+
+      cy.percySnapshot()
+    })
   })
 })
