9e4c04d7201557218fdec3ebca22579f3e2c3c9a
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2022-01-18 15:27:33 -0600

    test(launchpad): add e2e tests for Project Setup - Round 2 (#19580)
    Co-authored-by: Tyler Biethman <tbiethman@gmail.com>
    Co-authored-by: Zachary Williams <ZachJW34@gmail.com>

--- packages/frontend-shared/src/components/StandardModalHeader.vue
+++ packages/frontend-shared/src/components/StandardModalHeader.vue
@@ -2,8 +2,11 @@
   <div
     class="sticky top-0 flex items-center justify-between bg-white rounded-t border-b-1px border-b-gray-100 min-h-56px px-24px z-1"
   >
-    <DialogTitle class="text-gray-900 text-18px">
-      <slot /> <span
+    <div>
+      <DialogTitle class="text-gray-900 text-18px inline-block">
+        <slot />
+      </DialogTitle>
+      <span
         v-if="helpLink.length"
         class="inline-block border-t border-t-gray-100 w-32px h-6px mx-8px"
       /> <ExternalLink
@@ -14,7 +17,7 @@
         <span class="group-hocus:underline">{{ helpText }}</span>
         <i-cy-circle-bg-question-mark_x16 class="relative inline-block icon-dark-indigo-500 icon-light-indigo-100 -top-2px ml-8px" />
       </ExternalLink>
-    </DialogTitle>
+    </div>
     <button
       aria-label="Close"
       class="border-transparent rounded-full p-5px border-1 hover:border-indigo-300 hocus-default"
