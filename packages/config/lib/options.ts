c5c0b00d45d0e8f8e9a0eaf6f01f9a9b7a293934
Zachary Williams <ZachJW34@gmail.com>
2022-01-19 10:19:42 -0600

    feat: use real spec pattern and wire up no specs page (#19703)

--- packages/config/lib/options.ts
+++ packages/config/lib/options.ts
@@ -46,7 +46,7 @@ interface BreakingOption {
   isWarning?: boolean
 }
 
-const isValidConfig = (key, config) => {
+const isValidConfig = (key: string, config: any) => {
   const status = validate.isPlainObject(key, config)
 
   if (status !== true) {
