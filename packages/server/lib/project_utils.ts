77e19ffdb216e879c088bb9221c4e579d389aca5
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-20 12:03:15 +1000

    correctly normalize url

--- packages/server/lib/project_utils.ts
+++ packages/server/lib/project_utils.ts
@@ -3,6 +3,7 @@ import path from 'path'
 
 import errors from './errors'
 import { fs } from './util/fs'
+import { escapeFilenameInUrl } from './util/escape_filename'
 
 const debug = Debug('cypress:server:project_utils')
 
@@ -10,6 +11,7 @@ const multipleForwardSlashesRe = /[^:\/\/](\/{2,})/g
 const multipleForwardSlashesReplacer = (match: string) => match.replace('//', '/')
 const backSlashesRe = /\\/g
 
+// format is: http://localhost:<port>/__/#/specs/runner?file=<relative_url>
 export const getSpecUrl = ({
   spec,
   browserUrl,
@@ -28,10 +30,12 @@ export const getSpecUrl = ({
     return browserUrl
   }
 
-  const relativeSpecPath = path.relative(projectRoot, path.resolve(projectRoot, spec.absolute))
+  const relativeSpecPath = path.relative(projectRoot, path.resolve(projectRoot, spec.relative))
   .replace(backSlashesRe, '/')
 
-  const normalized = `${browserUrl}/#/specs/runner?file=${relativeSpecPath}`
+  const escapedRelativePath = escapeFilenameInUrl(relativeSpecPath)
+
+  const normalized = `${browserUrl}/#/specs/runner?file=${escapedRelativePath}`
   .replace(multipleForwardSlashesRe, multipleForwardSlashesReplacer)
 
   debug('returning spec url %s', normalized)
