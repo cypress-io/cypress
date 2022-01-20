6320d7f1f3c7b4b8c4b93190b661329a2013ce6e
Mark Noonan <mark@cypress.io>
2022-01-20 09:24:38 -0500

    add component test

--- packages/frontend-shared/src/components/NoInternetConnection.vue
+++ packages/frontend-shared/src/components/NoInternetConnection.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col h-full text-center justify-center items-center">
     <i-cy-no-internet_x48
+      data-cy="no-connection-icon"
+      aria-hidden
       class="h-48px w-48px icon-dark-gray-500 icon-light-gray-100 icon-dark-secondary-jade-400 icon-light-secondary-jade-200"
     />
     <h2 class="mt-24px mb-4px text-gray-900 text-18px leading-24px w-640px">
