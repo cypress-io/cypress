fbfe5901c953a023d3568107dfbf7a1cfd6e7755
ElevateBart <ledouxb@gmail.com>
2022-01-20 23:09:26 -0600

    feat: make the rename support component

--- packages/launchpad/src/migration/RenameSupport.vue
+++ packages/launchpad/src/migration/RenameSupport.vue
@@ -1,10 +1,49 @@
 <template>
-  <div>SupportFile</div>
+  <MigrationTitle :title="t('migration.renameSupport.title')" />
+  <MigrationList>
+    <template #line-1>
+      {{ t('migration.renameSupport.serveDifferentTypes') }}
+    </template>
+    <template #line-2>
+      {{ t('migration.renameSupport.changedSupportFile') }}
+      <CodeTag
+        class="text-red-500"
+      >cypress/support/index.js</CodeTag>
+      <i-cy-arrow-right_x16 class="h-16px w-16px inline-block icon-dark-gray-300" />
+      <CodeTag
+        class="text-jade-500"
+      >cypress/support/e2e.js</CodeTag>
+    </template>
+  </MigrationList>
+  <BeforeAfter>
+    <template #before>
+      <HighlightedFilesList
+        :files="[props.gql.supportFileBefore]"
+        :highlight-reg-exp="/index/gi"
+        highlight-class="text-red-500"
+      />
+    </template>
+    <template #after>
+      <HighlightedFilesList
+        :files="[props.gql.supportFileAfter]"
+        :highlight-reg-exp="/e2e/gi"
+        highlight-class="text-jade-500"
+      />
+    </template>
+  </BeforeAfter>
 </template>
 
 <script lang="ts" setup>
 import { gql } from '@urql/vue'
+import CodeTag from '@cy/components/CodeTag.vue'
+import MigrationTitle from './fragments/MigrationTitle.vue'
+import MigrationList from './fragments/MigrationList.vue'
+import HighlightedFilesList from './fragments/HighlightedFilesList.vue'
+import BeforeAfter from './fragments/BeforeAfter.vue'
 import type { RenameSupportFragment } from '../generated/graphql'
+import { useI18n } from '@cy/i18n'
+
+const { t } = useI18n()
 
 gql`
 fragment RenameSupport on Migration {
@@ -12,7 +51,7 @@ fragment RenameSupport on Migration {
   supportFileAfter
 }`
 
-defineProps<{
+const props = defineProps<{
   gql: RenameSupportFragment
 }>()
 </script>
