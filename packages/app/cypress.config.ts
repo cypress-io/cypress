5647a087ce8c5866fc8eb82a78ed928126375aec
Tim Griesser <tgriesser10@gmail.com>
2022-01-21 09:10:35 -0500

    fix

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     },
   },
   'e2e': {
-    baseUrl: 'http://localhost:5555',
+    baseUrl: 'http://localhost:4455',
     pluginsFile: 'cypress/e2e/plugins/index.ts',
     supportFile: 'cypress/e2e/support/e2eSupport.ts',
     async setupNodeEvents (on, config) {
