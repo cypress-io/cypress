3246a64507baf2ec4270651a16ae09ea68b31fc8
Adam Murray <adamm@cypress.io>
2022-01-24 09:28:38 -0600

    chore: remove default config values

--- npm/cypress-schematic/src/schematics/ng-add/files/cypress.config.ts
+++ npm/cypress-schematic/src/schematics/ng-add/files/cypress.config.ts
@@ -2,10 +2,4 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   'baseUrl': '<%= baseUrl%>',
-  'e2e': {
-    'supportFile': '<%= root%>cypress/support/e2e.ts',
-    'videosFolder': '<%= root%>cypress/videos',
-    'screenshotsFolder': '<%= root%>cypress/screenshots',
-    'fixturesFolder': '<%= root%>cypress/fixtures',
-  },
 })
