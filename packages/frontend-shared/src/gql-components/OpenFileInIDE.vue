dd1ee9af037b56e0aa1d095c0a7a6d5745ad1051
Tim Griesser <tgriesser10@gmail.com>
2022-01-25 17:14:33 -0500

    trying to make OpenFileInIDE generic

---
+++ packages/frontend-shared/src/gql-components/OpenFileInIDE.vue
@@ -0,0 +1,74 @@
+<template>
+  <slot @click="showCypressConfigInIDE()" />
+  <ChooseExternalEditorModal
+    v-if="isChooseEditorOpen"
+    :open="isChooseEditorOpen"
+    :gql="query.data?.value"
+    @close="isChooseEditorOpen = false"
+    @selected="openFile"
+  />
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+import { gql, useMutation, useQuery } from '@urql/vue'
+import { OpenConfigFileDocument } from '@packages/data-context/src/gen/all-operations.gen'
+import { OpenConfigFileInIdeDocument } from '../generated/graphql'
+import ChooseExternalEditorModal from './ChooseExternalEditorModal.vue'
+
+gql`
+query OpenFileInIDE {
+  localSettings {
+    preferences {
+      preferredEditorBinary
+    }
+  }
+  ...ChooseExternalEditorModal
+}
+`
+
+gql`
+mutation OpenFileInIDE_Mutation ($input: FileDetailsInput!) {
+  openFileInIDE (input: $input)
+}
+`
+
+defineProps<{
+  absolutePath: string
+}>()
+
+const query = useQuery({ query: OpenConfigFileInIdeDocument, requestPolicy: 'network-only' })
+
+const configFile = computed(() => query.data?.value?.currentProject?.configFile ?? 'cypress.config.js')
+
+const OpenConfigFileInIDE = useMutation(OpenConfigFileDocument)
+
+const openConfigFileInIDE = (absolute: string) => {
+  OpenConfigFileInIDE.executeMutation({
+    input: {
+      absolute,
+      line: 1,
+      column: 1,
+    },
+  })
+}
+
+const openFile = () => {
+  isChooseEditorOpen.value = false
+
+  if (query.data?.value?.currentProject?.configFileAbsolutePath) {
+    openConfigFileInIDE(query.data?.value?.currentProject.configFileAbsolutePath)
+  }
+}
+
+const showCypressConfigInIDE = () => {
+  if (query.data?.value?.localSettings.preferences.preferredEditorBinary && query.data?.value?.currentProject?.configFileAbsolutePath) {
+    openConfigFileInIDE(query.data?.value?.currentProject.configFileAbsolutePath)
+  } else {
+    isChooseEditorOpen.value = true
+  }
+}
+
+const isChooseEditorOpen = ref(false)
+
+</script>
