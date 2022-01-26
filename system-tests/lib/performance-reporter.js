c718fa2e84b2b3c011e4e3b869c4aaa589a73f40
BlueWinds <blue@everblue.info>
2022-01-26 13:03:02 -0800

    Review feedback

--- system-tests/lib/performance-reporter.js
+++ system-tests/lib/performance-reporter.js
@@ -13,9 +13,19 @@ class StatsdReporter {
     }
 
     console.log(chalk.green('Reporting to honeycomb'))
+
+    let branch; let commitSha
+
     this.honey = new Libhoney({
       dataset: 'systemtest-performance',
       writeKey: process.env.HONEYCOMB_API_KEY,
+    })
+
+    commitInfo().then((commitInformation) => {
+      const ciInformation = ciProvider.commitParams() || {}
+
+      this.branch = commitInformation.branch || ciInformation.branch
+      this.commitSha = commitInformation.sha || ciInformation.sha
     })
 
     runner.on('test', (test) => {
@@ -24,40 +34,47 @@ class StatsdReporter {
 
     runner.on('test end', (test, done) => {
       // Skipped tests never get a 'start' event, but they still get 'test end' somehow.
-      if (test.state === 'skipped') {
+      if (!test.state || test.state === 'skipped') {
         return
       }
 
-      commitInfo().then((commitInformation) => {
-        const ciInformation = ciProvider.commitParams() || {}
-        const [, testTitle, browser] = test.title.match(/(.+?)(?: \[([a-z]+)\])?$/)
+      const title = test.titlePath().join(' / ')
+      // This regex pulls apart a string like `e2e async timeouts / failing1 [electron]`
+      // into `e2e async timeouts / failing1` and `electron`, letting us use the same
+      // test name for all browsers, with the browser as a separate field.
+      // The browser capture group is optional because some tests aren't browser specific,
+      // in which case it will be undefined and not passed as a field to honeycomb.
+      const [, testTitle, browser] = title.match(/(.+?)(?: \[([a-z]+)\])?$/)
 
-        const honeycombEvent = this.honey.newEvent()
+      const honeycombEvent = this.honey.newEvent()
 
-        honeycombEvent.timestamp = test.wallclockStart
-        honeycombEvent.add({
-          test: testTitle,
-          specFile: path.basename(test.file),
-          browser,
-          state: test.state,
-          durationMs: Date.now() - test.wallclockStart,
-          mochaDurationMs: test.duration,
-          branch: commitInformation.branch || ciInformation.branch,
-          commitSha: commitInformation.sha || ciInformation.sha,
-          buildUrl: process.env.CIRCLE_BUILD_URL,
-          platform: process.platform,
-          arch: process.arch,
-          version: pkg.version,
-        })
-
-        honeycombEvent.send()
+      honeycombEvent.timestamp = test.wallclockStart
+      honeycombEvent.add({
+        test: testTitle,
+        specFile: path.basename(test.file),
+        browser,
+        state: test.state,
+        err: test.err && test.err.message,
+        errStack: test.err && test.err.stack,
+        durationMs: Date.now() - test.wallclockStart,
+        mochaDurationMs: test.duration,
+        branch,
+        commitSha,
+        buildUrl: process.env.CIRCLE_BUILD_URL,
+        platform: process.platform,
+        arch: process.arch,
+        version: pkg.version,
       })
+
+      //       honeycombEvent.send()
     })
   }
 
   // If there is no done callback, then mocha-multi-reporter will kill the process without waiting for our honeycomb post to complete.
   done (failures, callback) {
-    this.honey.flush().then(callback)
+    if (this.honey) {
+      this.honey.flush().then(callback)
+    }
   }
 }
 
