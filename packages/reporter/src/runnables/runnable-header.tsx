7f0e30de879af73930ca5064ea1f1d4ee2fc45a3
Tyler Biethman <tbiethman@gmail.com>
2022-01-26 22:11:16 -0600

    Open in IDE runner changes + tests

--- packages/reporter/src/runnables/runnable-header.tsx
+++ packages/reporter/src/runnables/runnable-header.tsx
@@ -3,9 +3,7 @@ import React, { Component, ReactElement } from 'react'
 
 import { StatsStore } from '../header/stats-store'
 import { formatDuration, getFilenameParts } from '../lib/util'
-import OpenFileInIDE from '../lib/open-file-in-ide'
 import FileNameOpener from '../lib/file-name-opener'
-import TextIcon from '-!react-svg-loader!@packages/frontend-shared/src/assets/icons/document-text_x16.svg'
 
 const renderRunnableHeader = (children: ReactElement) => <div className="runnable-header" data-cy="runnable-header">{children}</div>
 
@@ -52,20 +50,9 @@ class RunnableHeader extends Component<RunnableHeaderProps> {
       relativeFile: relativeSpecPath,
     }
 
-    const openInIde = '__vite__' in window
-      ? (
-        <OpenFileInIDE fileDetails={fileDetails}>
-          <a href="#" onClick={(e) => e.preventDefault()}>
-            <TextIcon />
-            {fileDetails.displayFile || fileDetails.originalFile}
-          </a>
-        </OpenFileInIDE>
-      )
-      : <FileNameOpener fileDetails={fileDetails} hasIcon />
-
     return renderRunnableHeader(
       <>
-        {openInIde}
+        <FileNameOpener fileDetails={fileDetails} hasIcon />
         {Boolean(statsStore.duration) && (
           <span className='duration' data-cy="spec-duration">{formatDuration(statsStore.duration)}</span>
         )}
--- system-tests/projects/runner-e2e-specs/cypress.config.js
+++ system-tests/projects/runner-e2e-specs/cypress.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   projectId: 'abc123',
-  // experimentalInteractiveRunEvents: true,
   e2e: {
     supportFile: false,
   },
