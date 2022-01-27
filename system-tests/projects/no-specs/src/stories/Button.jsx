0c3958bafec6038ce7958806fb7b238a86605cec
ElevateBart <ledouxb@gmail.com>
2022-01-27 14:43:45 -0600

    fix some errors in e2e test

--- system-tests/projects/no-specs/src/stories/Button.jsx
+++ system-tests/projects/no-specs/src/stories/Button.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
 
 /**
  * Primary UI component for user interaction
@@ -17,29 +15,6 @@ export default Button = ({ primary, backgroundColor, size, label, ...props }) =>
       {label}
     </button>
   );
-};
-
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
