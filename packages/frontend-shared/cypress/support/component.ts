2805b3480122d2f9bbdda695857aba73f3a31cd4
Mark Noonan <mark@cypress.io>
2022-01-27 18:37:55 -0500

    add cypress-real-events

--- packages/frontend-shared/cypress/support/component.ts
+++ packages/frontend-shared/cypress/support/component.ts
@@ -1,6 +1,7 @@
 import { registerMountFn, addVueCommand } from './common'
 import '../../src/styles/shared.scss'
 import 'virtual:windi.css'
+import 'cypress-real-events/support'
 import installCustomPercyCommand from '@packages/ui-components/cypress/support/customPercyCommand'
 import { addNetworkCommands } from './onlineNetwork'
 
