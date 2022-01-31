36d361022aa419cd98f7b409c082a4a30eaac074
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-31 20:46:46 +1000

    system test

--- system-tests/projects/migration-e2e-fully-custom/expected-cypress.config.js
+++ system-tests/projects/migration-e2e-fully-custom/expected-cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents (on, config) {
-      return require('src/the-plugins-file.js')(on, config)
+      return require('./src/the-plugins-file.js')(on, config)
     },
     specPattern: 'src/**/*.spec.js',
     supportFile: 'src/some-support-file.js',
