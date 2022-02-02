ee08f4ae95ca2c1c442c5c699395710ff1c145e6
Barthélémy Ledoux <bart@cypress.io>
2022-02-02 14:19:33 -0600

    feat: waiting for dependencies to be installed in wizard (#19955)

--- packages/data-context/src/sources/WizardDataSource.ts
+++ packages/data-context/src/sources/WizardDataSource.ts
@@ -1,6 +1,11 @@
+import Debug from 'debug'
 import { BUNDLERS, CODE_LANGUAGES, FRONTEND_FRAMEWORKS, PACKAGES_DESCRIPTIONS } from '@packages/types'
 import type { NexusGenObjects } from '@packages/graphql/src/gen/nxs.gen'
 import type { DataContext } from '..'
+import path from 'path'
+import resolve from 'resolve-from'
+
+const debug = Debug('cypress:data-context:wizard-data-source')
 
 export class WizardDataSource {
   constructor (private ctx: DataContext) {}
@@ -35,6 +40,41 @@ export class WizardDataSource {
     }
 
     return packages
+  }
+
+  async resolvePackagesToInstall (): Promise<string[]> {
+    const packagesInitial = await this.packagesToInstall() || []
+
+    if (!this.ctx.currentProject) {
+      throw Error('currentProject is not defined')
+    }
+
+    debug('packages to install: %O', packagesInitial)
+
+    const installedPackages: (string|null)[] = packagesInitial.map((p) => {
+      if (this.ctx.currentProject) {
+        debug('package checked: %s', p.package)
+
+        // At startup, node will only resolve the main files of packages it knows of.
+        // Adding a package after the app started will not be resolved in the same way.
+        // It will only be resolved as a package whose main is `index.js`, ignoring the "main" field
+        // to avoid this bug, we resolve a file we know has to be in a node module:
+        // `package.json`
+        const packageJsonPath = path.join(p.package, 'package.json')
+
+        try {
+          resolve(this.ctx.currentProject, packageJsonPath)
+
+          return p.package
+        } catch (e) {
+          debug('ERROR - resolving package "%s": %O', p.package, e)
+        }
+      }
+
+      return null
+    })
+
+    return installedPackages.filter((p) => p !== null) as string[]
   }
 
   get chosenFramework () {
