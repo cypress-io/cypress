e2f0a66a1f4cb5168c37805617627de04ad39e7f
Zach Bloomquist <git@chary.us>
2022-02-03 07:44:27 -0500

    Fix create-cypress-tests

--- packages/example/bin/build.js
+++ packages/example/bin/build.js
@@ -13,4 +13,7 @@ shell.cp('-r', join(resolvePkg('cypress-example-kitchensink'), 'app'), '.')
 shell.rm('-rf', 'cypress')
 shell.cp('-r', join(resolvePkg('cypress-example-kitchensink'), 'cypress'), '.')
 
+// TODO: remove once example-kitchensink has this
+shell.touch(join('cypress', 'support', 'e2e.js'))
+
 shell.exec('node ./bin/convert.js')
