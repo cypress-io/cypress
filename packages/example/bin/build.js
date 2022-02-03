edc3042d3b7d849842e7c49936590b17f7437c4e
ElevateBart <ledouxb@gmail.com>
2022-02-03 11:07:50 -0600

    avoid moving what is already there

--- packages/example/bin/build.js
+++ packages/example/bin/build.js
@@ -12,7 +12,6 @@ shell.cp('-r', join(resolvePkg('cypress-example-kitchensink'), 'app'), '.')
 
 shell.rm('-rf', 'cypress')
 shell.cp('-r', join(resolvePkg('cypress-example-kitchensink'), 'cypress'), '.')
-shell.mv(join('cypress', 'integration'), join('cypress', 'e2e'))
 shell.exec(`
   for f in cypress/e2e/**/*.spec.js; do
   mv -- "$f" "\${f%.spec.js}.cy.js"
