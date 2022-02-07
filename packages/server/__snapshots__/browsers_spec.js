d48d3f96b4dde4f2320a5af905480d35c79aab07
Brian Mann <brian.mann86@gmail.com>
2022-02-07 10:38:34 -0500

    fix failing tests

--- packages/server/__snapshots__/browsers_spec.js
+++ packages/server/__snapshots__/browsers_spec.js
@@ -4,11 +4,11 @@ Can't run because you've entered an invalid browser name.
 Browser: browserNotGonnaBeFound was not found on your system or is not supported by Cypress.
 
 Cypress supports the following browsers:
-- chrome
-- chromium
-- edge
-- electron
-- firefox
+ - electron
+ - chrome
+ - chromium
+ - edge
+ - firefox
 
 You can also use a custom browser: https://on.cypress.io/customize-browsers
 
@@ -16,6 +16,10 @@ Available browsers found on your system are:
 - chrome
 - firefox
 - electron
+ - chrome
+ - chromium
+ - firefox
+ - electron
 `
 
 exports['lib/browsers/index .ensureAndGetByNameOrPath throws a special error when canary is passed 1'] = `
@@ -24,20 +28,20 @@ Can't run because you've entered an invalid browser name.
 Browser: canary was not found on your system or is not supported by Cypress.
 
 Cypress supports the following browsers:
-- chrome
-- chromium
-- edge
-- electron
-- firefox
+ - electron
+ - chrome
+ - chromium
+ - edge
+ - firefox
 
 You can also use a custom browser: https://on.cypress.io/customize-browsers
 
 Available browsers found on your system are:
-- chrome
-- chrome:canary
-- firefox
+ - chrome
+ - chrome:canary
+ - firefox
 
-Note: In Cypress version 4.0.0, Canary must be launched as \`chrome:canary\`, not \`canary\`.
+Note: In Cypress version 4.0.0, Canary must be launched as chrome:canary, not canary.
 
 See https://on.cypress.io/migration-guide for more information on breaking changes in 4.0.0.
 `
