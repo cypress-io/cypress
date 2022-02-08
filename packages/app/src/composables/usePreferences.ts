e75d3130baf33548d9484a9b2fd3a809fe0ade7b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-02-08 14:38:36 +1000

    nest store accessor in fn

--- packages/app/src/composables/usePreferences.ts
+++ packages/app/src/composables/usePreferences.ts
@@ -2,8 +2,6 @@ import { useRunnerUiStore, RunnerUiState } from '../store'
 import { useMutation, gql } from '@urql/vue'
 import { Preferences_SetPreferencesDocument } from '@packages/app/src/generated/graphql'
 
-const runnerUiStore = useRunnerUiStore()
-
 gql`
 mutation Preferences_SetPreferences ($value: String!) {
   setPreferences (value: $value) {
@@ -13,6 +11,7 @@ mutation Preferences_SetPreferences ($value: String!) {
 }`
 
 export function usePreferences () {
+  const runnerUiStore = useRunnerUiStore()
   const setPreferences = useMutation(Preferences_SetPreferencesDocument)
 
   function update<K extends keyof RunnerUiState> (preference: K, value: RunnerUiState[K]) {
