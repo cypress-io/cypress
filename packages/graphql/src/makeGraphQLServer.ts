8b7eadb409e87666bf3589036a6b07279bd72cb7
Tim Griesser <tgriesser10@gmail.com>
2022-02-08 14:12:38 -0500

    feat: Better logging utilities for GraphQL requests & slow fields

--- packages/graphql/src/makeGraphQLServer.ts
+++ packages/graphql/src/makeGraphQLServer.ts
@@ -10,9 +10,13 @@ import serverDestroy from 'server-destroy'
 import send from 'send'
 import { getPathToDist } from '@packages/resolve-dist'
 import httpProxy from 'http-proxy'
+import debugLib from 'debug'
 
 import { graphqlSchema } from './schema'
-import { parse } from 'graphql'
+import { execute, parse } from 'graphql'
+import { plugin } from 'nexus'
+
+const debugOperation = debugLib(`cypress-verbose:graphql:operation`)
 
 const SHOW_GRAPHIQL = process.env.CYPRESS_INTERNAL_ENV !== 'production'
 
@@ -109,6 +113,16 @@ export const graphQLHTTP = graphqlHTTP((req, res, params) => {
     schema: graphqlSchema,
     graphiql: SHOW_GRAPHIQL,
     context: ctx,
+    customExecuteFn: (args) => {
+      const date = new Date()
+      const prefix = `${args.operationName ?? '(anonymous)'}`
+
+      return plugin.completeValue(execute(args), (val) => {
+        debugOperation(`${prefix} completed in ${new Date().valueOf() - date.valueOf()} with ${val.errors?.length ?? 0} errors`)
+
+        return val
+      })
+    },
   }
 })
 
