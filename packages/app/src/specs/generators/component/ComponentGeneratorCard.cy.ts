7e4977d75d45fbf0884a8e52c874b57221afaa91
Jessica Sachs <jess@jessicasachs.io>
2022-02-09 15:20:13 -0500

    removing files from scaffold

--- packages/app/src/specs/generators/component/ComponentGeneratorCard.cy.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@cypress/vue'
-import Componentgeneratorcard from './ComponentGeneratorCard.vue'
-
-describe('<Componentgeneratorcard />', () => {
-  it('renders', () => {
-    // see: https://vue-test-utils.vuejs.org/
-    mount(Componentgeneratorcard, { props: {} })
-  })
-})
--- packages/app/src/specs/generators/scaffold/ScaffoldGeneratorStepOne.cy.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@cypress/vue'
-import Scaffoldgeneratorstepone from './ScaffoldGeneratorStepOne.vue'
-
-describe('<Scaffoldgeneratorstepone />', () => {
-  it('renders', () => {
-    // see: https://vue-test-utils.vuejs.org/
-    mount(Scaffoldgeneratorstepone, { props: {} })
-  })
-})
