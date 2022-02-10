68d8a306c6d9cdded88657ba81db2afdc11f3062
Jessica Sachs <jess@jessicasachs.io>
2022-02-10 00:36:37 -0500

    fix: line height for file match indicator

--- packages/app/src/components/FileMatchIndicator.vue
+++ packages/app/src/components/FileMatchIndicator.vue
@@ -3,7 +3,7 @@
     class="inline-flex items-center"
     data-cy="file-match-indicator"
   >
-    <span class="rounded font-medium bg-jade-100 h-24px text-center px-8px text-jade-600 truncate select-none">
+    <span class="rounded font-medium bg-jade-100 h-24px text-center px-8px text-jade-600 leading-24px truncate select-none">
       <slot /></span>
   </div>
 </template>
