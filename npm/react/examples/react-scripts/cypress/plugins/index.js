ab771dd95bdf734c360d19b1767f18108ecf4ef5
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-02-10 15:33:00 +1000

    fix: set default webpack path if empty config object provided

--- npm/react/examples/react-scripts/cypress/plugins/index.js
+++ npm/react/examples/react-scripts/cypress/plugins/index.js
@@ -8,7 +8,7 @@ const devServer = require('@cypress/react/plugins/react-scripts')
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  devServer(on, config)
+  devServer(on, config, {})
 
   // IMPORTANT to return the config object
   // with the any changed environment variables
