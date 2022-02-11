9608b021fda02d23684e17ed4d2cba16fe853738
estrada9166 <estrada9166@hotmail.com>
2022-02-11 12:24:49 -0500

    Fix tests

--- packages/data-context/test/unit/util/migration.spec.ts
+++ packages/data-context/test/unit/util/migration.spec.ts
@@ -295,7 +295,7 @@ describe('reduceConfig', () => {
     const config = { testFiles: '**/**.cy.js', componentFolder: 'src', integrationFolder: 'cypress/integration' }
     const newConfig = reduceConfig(config)
 
-    expect(newConfig.component.specPattern).to.eq(config.testFiles)
+    expect(newConfig.component.specPattern).to.eq(`${config.componentFolder}/${config.testFiles}`)
     expect(newConfig.e2e.specPattern).to.eq(`${config.integrationFolder}/${config.testFiles}`)
   })
 
@@ -311,7 +311,7 @@ describe('reduceConfig', () => {
     }
     const newConfig = reduceConfig(config)
 
-    expect(newConfig.component.specPattern).to.eq(config.testFiles)
+    expect(newConfig.component.specPattern).to.eq(`${config.component.componentFolder}/${config.testFiles}`)
     expect(newConfig.e2e.specPattern).to.eq(`${config.e2e.integrationFolder}/${config.testFiles}`)
   })
 
@@ -336,7 +336,7 @@ describe('reduceConfig', () => {
     }
     const newConfig = reduceConfig(config)
 
-    expect(newConfig.component.specPattern).to.eq(config.testFiles)
+    expect(newConfig.component.specPattern).to.eq(`${config.component.componentFolder}/${config.testFiles}`)
     expect(newConfig.e2e.specPattern).to.eq(`${config.e2e.integrationFolder}/${config.testFiles}`)
   })
 
