ac14cce8b813623c2b0d73e72e32decf5c923412
Brian Mann <brian.mann86@gmail.com>
2022-02-10 23:44:31 -0500

    Update packages/errors/src/errors.ts
    Co-authored-by: Ryan Manuel <ryanm@cypress.io>

--- packages/errors/src/errors.ts
+++ packages/errors/src/errors.ts
@@ -1241,11 +1241,9 @@ export const cloneError = function (err: CypressError | GenericError, options: {
 
   // and any own (custom) properties
   // of the err object
-  for (let prop of Object.keys(err || {})) {
-    const val = err[prop]
-
+  Object.entries(err || {}).forEach(([prop, val]) => {
     obj[prop] = val
-  }
+  })
 
   if (err.stackWithoutMessage) {
     obj.stack = err.stackWithoutMessage
