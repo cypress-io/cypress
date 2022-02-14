c65f5e421d33b3cd58189d472d69b591be699e71
Mark Noonan <mark@cypress.io>
2022-02-14 14:44:04 -0500

    remove unused code

--- packages/app/src/store/aut-store.ts
+++ packages/app/src/store/aut-store.ts
@@ -1,6 +1,4 @@
 import { defineStore } from 'pinia'
-import { useRunnerStyle } from '../runner/useRunnerStyle'
-import { useScreenshotStore } from './screenshot-store'
 
 export type ScriptError = { type: string, error: string } | null
 
@@ -91,21 +89,6 @@ export const useAutStore = defineStore({
         height: state.viewportHeight,
         width: state.viewportWidth,
       }
-    },
-
-    autScale (state) {
-      const { containerHeight, containerWidth } = useRunnerStyle()
-      const screenshotStore = useScreenshotStore()
-
-      if (screenshotStore.isScreenshotting) {
-        return 1
-      }
-
-      return Math.min(
-        containerWidth.value / state.viewportWidth,
-        containerHeight.value / state.viewportHeight,
-        1,
-      )
     },
   },
 })
