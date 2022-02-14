c7b9e3e03f370f23dfc64570197a7560e16943f1
Mark Noonan <mark@cypress.io>
2022-02-14 17:31:52 -0500

    add e2e test simulating run mode (CT testing type)

--- packages/app/cypress/e2e/cypress-in-cypress-run-mode.cy.ts
+++ packages/app/cypress/e2e/cypress-in-cypress-run-mode.cy.ts
@@ -1,17 +1,15 @@
 describe('Cypress In Cypress - run mode', { viewportWidth: 1200 }, () => {
-  beforeEach(() => {
+  it('e2e run mode spec runner header is correct', () => {
     cy.scaffoldProject('cypress-in-cypress')
     cy.findBrowsers()
     cy.openProject('cypress-in-cypress')
     cy.startAppServer()
     cy.visitApp()
-  })
 
-  it('test e2e', () => {
     // this simulates run mode enough for this test
     cy.window().then((win) => {
       win.__CYPRESS_MODE__ = 'run'
-      cy.findAllByTestId('navbar-wrapper').then(($el) => {
+      cy.get('[data-cy="navbar-wrapper"]').then(($el) => {
         $el.remove()
       })
     })
@@ -21,7 +19,6 @@ describe('Cypress In Cypress - run mode', { viewportWidth: 1200 }, () => {
       expect(location.hash).to.contain('dom-content.spec')
     })
 
-    cy.get('[data-model-state="passed"]').should('contain', 'renders the test content')
     cy.findByTestId('aut-url').should('be.visible')
     cy.findByTestId('playground-activator').should('not.exist')
 
@@ -30,6 +27,42 @@ describe('Cypress In Cypress - run mode', { viewportWidth: 1200 }, () => {
     cy.contains('84%').should('be.visible')
     cy.contains('Chrome 1').should('be.visible')
     cy.contains('http://localhost:4455/cypress/e2e/dom-content.html').should('be.visible')
+
+    cy.percySnapshot()
+
+    // confirm no interactions are implemented
+    cy.findByTestId('viewport').click()
+    cy.contains('The viewport determines').should('not.exist')
+    cy.contains('Chrome 1').click()
+    cy.contains('Firefox').should('not.exist')
+  })
+
+  it('component testing run mode spec runner header is correct', () => {
+    cy.scaffoldProject('cypress-in-cypress')
+    cy.findBrowsers()
+    cy.openProject('cypress-in-cypress')
+    cy.startAppServer('component')
+    cy.visitApp()
+
+    // this simulates run mode enough for this test
+    cy.window().then((win) => {
+      win.__CYPRESS_MODE__ = 'run'
+      cy.get('[data-cy="navbar-wrapper"]').then(($el) => {
+        $el?.remove()
+      })
+    })
+
+    cy.contains('TestComponent.spec').click()
+    cy.location().should((location) => {
+      expect(location.hash).to.contain('TestComponent.spec')
+    })
+
+    cy.findByTestId('aut-url').should('not.exist')
+    cy.findByTestId('playground-activator').should('not.exist')
+
+    // confirm expected content is rendered
+    cy.contains('500x500').should('be.visible')
+    cy.contains('Chrome 1').should('be.visible')
 
     cy.percySnapshot()
 
