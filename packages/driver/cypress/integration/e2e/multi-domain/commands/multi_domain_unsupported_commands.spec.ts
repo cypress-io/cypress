3bdd6342205c43ed083a37973f2bc2e5344569e5
Bill Glesias <bglesias@gmail.com>
2022-02-16 17:31:25 -0500

    update test description for unsupported commands

--- packages/driver/cypress/integration/e2e/multi-domain/commands/multi_domain_unsupported_commands.spec.ts
+++ packages/driver/cypress/integration/e2e/multi-domain/commands/multi_domain_unsupported_commands.spec.ts
@@ -5,7 +5,7 @@ context('multi-domain unsupported commands', { experimentalSessionSupport: true,
     cy.get('a[data-cy="multi-domain-secondary-link"]').click()
   })
 
-  it('throws an error that the `cy.route()` method is deprecated and forbidden when attempted use is in multi-domain', (done) => {
+  it('throws an error that the `cy.route()` method is deprecated and unsupported when attempted use is in multi-domain', (done) => {
     cy.on('fail', (err) => {
       expect(err.message).to.equal('`cy.route()` has been deprecated and use is not supported in `cy.switchToDomain()`. Consider using `cy.intercept()` instead.')
       done()
@@ -16,7 +16,7 @@ context('multi-domain unsupported commands', { experimentalSessionSupport: true,
     })
   })
 
-  it('throws an error that the `cy.server()` method is deprecated and forbidden when attempted use is in multi-domain', (done) => {
+  it('throws an error that the `cy.server()` method is deprecated and unsupported when attempted use is in multi-domain', (done) => {
     cy.on('fail', (err) => {
       expect(err.message).to.equal('`cy.server()` has been deprecated and use is not supported in `cy.switchToDomain()`. Consider using `cy.intercept()` instead.')
       done()
