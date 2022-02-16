3b1f997ea3e4d2c6b4f57759470e13aa0f048725
Bill Glesias <bglesias@gmail.com>
2022-02-15 22:35:23 -0500

    remove preprocessing of thrown errors in secondary domain (meant to remove this)

--- packages/driver/src/cy/multi-domain/index.ts
+++ packages/driver/src/cy/multi-domain/index.ts
@@ -136,7 +136,7 @@ export function addCommands (Commands, Cypress: Cypress.Cypress, cy: Cypress.cy,
             }
 
             cleanup()
-            reject(correctStackForCrossDomainError(err, userInvocationStack))
+            reject(err)
 
             return
           }
