5f905ee49b8ce285ae2e86fecfdef65fe832474b
Matt Schile <mschile@gmail.com>
2022-02-17 12:53:45 -0700

    feat: (multi-domain) add support for pausing timers during screenshots (#20209)

--- packages/runner/injection/multi-domain.js
+++ packages/runner/injection/multi-domain.js
@@ -7,31 +7,7 @@
  * dependencies.
  */
 
-// TODO: uncomment the following code once the timing is worked out to have
-// the sibling Cypress global available before this injection code runs
-
-//  import { createTimers } from './timers'
-
-//  const Cypress = window.Cypress = parent.Cypress
-
-//  if (!Cypress) {
-//    throw new Error('Something went terribly wrong and we cannot proceed. We expected to find the global Cypress in the parent window but it is missing!. This should never happen and likely is a bug. Please open an issue!')
-//  }
-
-//  // We wrap timers in the injection code because if we do it in the driver (like
-//  // we used to do), any uncaught errors thrown in the timer callbacks would
-//  // get picked up by the top frame's 'error' handler instead of the AUT's.
-//  // We need to wrap the timer callbacks in the AUT itself for errors to
-//  // propagate properly.
-//  const timers = createTimers()
-
-//  Cypress.removeAllListeners('app:timers:reset')
-//  Cypress.removeAllListeners('app:timers:pause')
-
-//  Cypress.on('app:timers:reset', timers.reset)
-//  Cypress.on('app:timers:pause', timers.pause)
-
-//  timers.wrap()
+import { createTimers } from './timers'
 
 // TODO: don't hard-code the index. need it to be predictable or need
 // to search for the right one somehow. will need to be fixed when we
@@ -39,5 +15,21 @@
 const cyBridgeFrame = window.parent.frames[2]
 
 const Cypress = cyBridgeFrame.Cypress
+
+if (!Cypress) {
+  throw new Error('Something went terribly wrong and we cannot proceed. We expected to find the global \
+    Cypress in the spec bridge window but it is missing. This should never happen and likely is a bug. Please open an issue.')
+}
+
+// the timers are wrapped in the injection code similar to the primary domain
+const timers = createTimers()
+
+Cypress.removeAllListeners('app:timers:reset')
+Cypress.removeAllListeners('app:timers:pause')
+
+Cypress.on('app:timers:reset', timers.reset)
+Cypress.on('app:timers:pause', timers.pause)
+
+timers.wrap()
 
 Cypress.action('app:window:before:load', window)
