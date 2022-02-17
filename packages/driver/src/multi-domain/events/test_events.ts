dcd76899b1952385d34530cbb5fae34cc6986b35
Matt Schile <mschile@cypress.io>
2022-02-17 10:42:52 -0700

    fixing references

--- packages/driver/src/multi-domain/events/test_events.ts
+++ packages/driver/src/multi-domain/events/test_events.ts
@@ -1,4 +1,4 @@
-import type { SpecBridgeDomainCommunicator } from './communicator'
+import type { SpecBridgeDomainCommunicator } from '../communicator'
 
 export const handleTestEvents = (Cypress: Cypress.Cypress, specBridgeCommunicator: SpecBridgeDomainCommunicator) => {
   specBridgeCommunicator.on('test:before:run', (...args) => {
