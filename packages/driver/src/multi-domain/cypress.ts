bf22062bd26a78f4919c34aec0590bed34271bd8
Matt Schile <mschile@cypress.io>
2022-02-18 13:30:48 -0700

    removing dup events

--- packages/driver/src/multi-domain/cypress.ts
+++ packages/driver/src/multi-domain/cypress.ts
@@ -73,23 +73,6 @@ const setup = (cypressConfig: Cypress.Config, env: Cypress.ObjectLike) => {
 
   cy.onBeforeAppWindowLoad = onBeforeAppWindowLoad(Cypress, cy)
 
-  // TODO Should state syncing be built into cy.state instead of being explicitly called?
-  specBridgeCommunicator.on('sync:state', async (state) => {
-    cy.state(state)
-  })
-
-  // Listen for window load events from the primary window to resolve page loads
-  specBridgeCommunicator.on('window:load', ({ url }) => {
-    cy.isStable(true, 'load')
-    Cypress.emit('internal:window:load', { type: 'cross:domain', url })
-  })
-
-  // Forward url:changed Message to the primary domain to enable changing the url displayed in the AUT
-  // @ts-ignore
-  Cypress.on('url:changed', (url) => {
-    specBridgeCommunicator.toPrimary('url:changed', url)
-  })
-
   return cy
 }
 
