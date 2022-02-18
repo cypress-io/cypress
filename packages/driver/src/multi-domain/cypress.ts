adeb2d7268f2f08fff0a54d3b6995eefdf8fd068
Matt Schile <mschile@cypress.io>
2022-02-18 13:17:13 -0700

    rename misc_events file

--- packages/driver/src/multi-domain/cypress.ts
+++ packages/driver/src/multi-domain/cypress.ts
@@ -18,7 +18,7 @@ import { handleSpecWindowEvents } from './events/spec_window_events'
 import { handleErrorEvent } from './events/errors'
 import { handleScreenshots } from './events/screenshots'
 import { handleTestEvents } from './events/test_events'
-import { handleMiscEvents } from './events/misc_events'
+import { handleMiscEvents } from './events/misc'
 
 const specBridgeCommunicator = new SpecBridgeDomainCommunicator()
 
