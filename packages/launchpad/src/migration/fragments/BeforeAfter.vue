313aae9d0e2ccd8492d67c76750f174f212ceb3b
ElevateBart <ledouxb@gmail.com>
2022-02-17 22:21:43 -0600

    fix placement of identifier

--- packages/launchpad/src/migration/fragments/BeforeAfter.vue
+++ packages/launchpad/src/migration/fragments/BeforeAfter.vue
@@ -3,26 +3,26 @@
     class="border rounded bg-gray-100 border-gray-100 mt-16px
       w-full grid gap-1px grid-rows-[48px,1fr] grid-cols-2 overflow-hidden"
   >
-    <div
-      class="flex font-semibold bg-gray-50 px-16px text-gray-800 items-center"
-      data-cy="migrate-before"
-    >
+    <div class="flex font-semibold bg-gray-50 px-16px text-gray-800 items-center">
       <slot name="beforeHeader">
         {{ t('migration.before') }}
       </slot>
     </div>
-    <div
-      class="flex font-semibold bg-gray-50 px-16px text-gray-800 items-center"
-      data-cy="migrate-after"
-    >
+    <div class="flex font-semibold bg-gray-50 px-16px text-gray-800 items-center">
       <slot name="afterHeader">
         {{ t('migration.after') }}
       </slot>
     </div>
-    <div class="bg-white">
+    <div
+      class="bg-white"
+      data-cy="migrate-before"
+    >
       <slot name="before" />
     </div>
-    <div class="bg-white">
+    <div
+      class="bg-white"
+      data-cy="migrate-after"
+    >
       <slot name="after" />
     </div>
   </div>
