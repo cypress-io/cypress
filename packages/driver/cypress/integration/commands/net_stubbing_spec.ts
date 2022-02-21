f7e83a4be964af1ae309dd71a58be88fe8bf1b7b
Ryan Manuel <ryanm@cypress.io>
2022-02-21 13:27:40 -0600

    chore: fix net stubbing flake

--- packages/driver/cypress/integration/commands/net_stubbing_spec.ts
+++ packages/driver/cypress/integration/commands/net_stubbing_spec.ts
@@ -1649,7 +1649,7 @@ describe('network stubbing', function () {
         })
       }).then(() => {
         return $.get('/timeout').then((responseText) => {
-          expect(Date.now() - this.start).to.be.closeTo(250 + 100, 100)
+          expect(Date.now() - this.start).to.be.closeTo(delayMs + 100, 100)
 
           done()
         })
@@ -1664,7 +1664,7 @@ describe('network stubbing', function () {
         const start = Date.now()
 
         return $.get('/timeout').then((responseText) => {
-          expect(Date.now()).to.be.gte(start + delay)
+          expect(Date.now() - start).to.be.closeTo(delay + 100, 100)
           expect(responseText).to.eq('foo')
         })
       }
@@ -2583,16 +2583,18 @@ describe('network stubbing', function () {
     })
 
     it('can delay a proxy response using res.setDelay', function (done) {
+      const delay = 1000
+
       cy.intercept('/timeout*', (req) => {
         req.reply((res) => {
           this.start = Date.now()
 
-          res.setDelay(1000).send('delay worked')
+          res.setDelay(delay).send('delay worked')
         })
       }).then(() => {
         $.get('/timeout')
         .done((responseText) => {
-          expect(Date.now() - this.start).to.be.closeTo(1100, 100)
+          expect(Date.now() - this.start).to.be.closeTo(delay + 100, 100)
           expect(responseText).to.include('delay worked')
 
           done()
@@ -2601,18 +2603,20 @@ describe('network stubbing', function () {
     })
 
     it('can \'delay\' a proxy response using Promise.delay', function (done) {
+      const delay = 1000
+
       cy.intercept('/timeout*', (req) => {
         req.reply((res) => {
           this.start = Date.now()
 
-          return Promise.delay(1000)
+          return Promise.delay(delay)
           .then(() => {
             res.send('Promise.delay worked')
           })
         })
       }).then(() => {
         $.get('/timeout').then((responseText) => {
-          expect(Date.now() - this.start).to.be.closeTo(1000, 100)
+          expect(Date.now() - this.start).to.be.closeTo(delay + 100, 100)
           expect(responseText).to.eq('Promise.delay worked')
 
           done()
