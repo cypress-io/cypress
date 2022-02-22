4f9457f8dedaf198d67652a7a585fecda8c15d11
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-02-22 18:46:09 +1000

    work on nextjs

--- packages/scaffold-config/src/types.ts
+++ packages/scaffold-config/src/types.ts
@@ -1,7 +1,7 @@
 import type { BUNDLERS, STORYBOOK_DEPS, CODE_GEN_FRAMEWORKS } from './constants'
 import type { FRONTEND_FRAMEWORKS } from './configFiles'
 
-export type AllPackages = FrontendFramework['package']['name'] | Bundler['package'] | typeof STORYBOOK_DEPS[number]
+export type AllPackages = FrontendFramework['packages'][number]['name'] | Bundler['package'] | typeof STORYBOOK_DEPS[number]
 
 export type AllPackageTypes = FrontendFramework['type'] | Bundler['type']
 
