d35843402aaac3890cc3548fd8e3992b6c5570e5
Zach Bloomquist <git@chary.us>
2022-02-23 18:08:46 -0500

    bump timeout

--- system-tests/test-binary/module_api_spec.ts
+++ system-tests/test-binary/module_api_spec.ts
@@ -2,6 +2,7 @@ import systemTests from '../lib/system-tests'
 
 describe('module API', () => {
   systemTests.it('can run module API Mocha spec', {
+    timeout: 240000,
     dockerImage: 'cypress/base:12',
     withBinary: true,
     project: 'module-api',
