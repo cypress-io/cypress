0b1707bb234d1f7429a650d64328c6442b733872
ElevateBart <ledouxb@gmail.com>
2022-02-23 10:31:50 -0600

    add semicolons...

--- npm/icons/scripts/build.sh
+++ npm/icons/scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 rm -rf dist && mkdir dist && mkdir dist/icons
-if [[ "$OSTYPE" == "darwin"* ]]
-  iconutil -c icns src/cypress.iconset -o dist/icons/cypress.icns
+if [[ "$OSTYPE" == "darwin"* ]];
+  iconutil -c icns src/cypress.iconset -o dist/icons/cypress.icns;
 fi
 cp -r src/* dist
 mv dist/cypress.iconset/* dist/icons
