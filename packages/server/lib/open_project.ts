61fae04435b44bc38d6e8c6b3c1a23b1a1349460
Tyler Biethman <tbiethman@gmail.com>
2022-02-23 16:50:15 -0600

    Moving browser status handling into server so it gets updated in all browser loading situations

--- packages/server/lib/open_project.ts
+++ packages/server/lib/open_project.ts
@@ -180,7 +180,13 @@ export class OpenProject {
           return browsers.connectToNewSpec(browser, { onInitializeNewBrowserTab, ...options }, automation)
         }
 
+        this._ctx?.browser.setBrowserStatus('opening')
+
         return browsers.open(browser, options, automation, this._ctx)
+      }).then((browserInstance) => {
+        this._ctx?.browser.setBrowserStatus('open')
+
+        return browserInstance
       })
     }
 
@@ -299,10 +305,14 @@ export class OpenProject {
   async sendFocusBrowserMessage () {
     const isRunnerConnected = this.projectBase?.isRunnerSocketConnected()
 
+    // If the runner's socket is active and connected, we focus the active window
     if (isRunnerConnected) {
-      this.projectBase?.sendFocusBrowserMessage()
-    } else if (this.relaunchBrowser) {
-      this.relaunchBrowser()
+      return this.projectBase?.sendFocusBrowserMessage()
+    }
+
+    // Otherwise, we relaunch the app in the current browser
+    if (this.relaunchBrowser) {
+      return this.relaunchBrowser()
     }
   }
 }
