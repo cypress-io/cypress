727b4352c00a296a8b3c4fd47ac0b381a57b3a03
David Munechika <david.munechika@gatech.edu>
2022-02-23 14:41:46 -0500

    fix types

--- packages/frontend-shared/src/utils/sortBrowsers.ts
+++ packages/frontend-shared/src/utils/sortBrowsers.ts
@@ -1,5 +1,3 @@
-import type { Browser } from '@packages/types/src/browser'
-
-export default function sortBrowsers (browsers: Browser[]) {
+export default function sortBrowsers (browsers) {
   return browsers.sort((a, b) => a.displayName > b.displayName ? 1 : -1)
 }
