23885443fc091635cc9becd05549b44389be207d
ElevateBart <ledouxb@gmail.com>
2022-02-23 10:30:46 -0600

    skip step on all platforms except darwin

--- npm/icons/scripts/build.sh
+++ npm/icons/scripts/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 rm -rf dist && mkdir dist && mkdir dist/icons
-iconutil -c icns src/cypress.iconset -o dist/icons/cypress.icns
+if [[ "$OSTYPE" == "darwin"* ]]
+  iconutil -c icns src/cypress.iconset -o dist/icons/cypress.icns
+fi
 cp -r src/* dist
 mv dist/cypress.iconset/* dist/icons
 rm -r dist/cypress.iconset
