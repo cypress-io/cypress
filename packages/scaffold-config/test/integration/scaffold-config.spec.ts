da535c2fdb4cf33b5843733bb35fe44ae2940ab2
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-02-24 17:39:54 +1000

    rebase pt1

--- packages/scaffold-config/test/integration/scaffold-config.spec.ts
+++ packages/scaffold-config/test/integration/scaffold-config.spec.ts
@@ -12,57 +12,65 @@ export async function scaffoldMigrationProject (project: typeof e2eProjectDirs[n
 }
 
 describe('detect', () => {
-  it(`vuecli v4 vue 2`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-vuecli4-vue2-configured')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('vuecli4-vue2')
+  it(`Vue CLI w/ Vue 2`, async () => {
+    const dir = await scaffoldMigrationProject('vueclivue2-unconfigured')
+    const actual = detect(dir)
+
+    expect(actual.type).to.eq('vueclivue2')
   })
 
-  it(`vuecli v4 vue 3`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-vuecli-v4-vue-3')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('vuecli4-vue3')
+  it('Create React App', async () => {
+    const dir = await scaffoldMigrationProject('create-react-app-unconfigured')
+    const actual = detect(dir)
+
+    expect(actual.type).to.eq('cra')
   })
 
-  it(`vuecli v5 vue 2`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-vuecli-v5-vue-2')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('vuecli5-vue2')
-  })
+  // it(`vuecli v4 vue 3`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-vuecli-v4-vue-3')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('vuecli4-vue3')
+  // })
 
-  it(`vuecli v5 vue 3`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-vuecli-v5-vue-3')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('vuecli5-vue3')
-  })
-  
-  it(`next v11+`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-nextjs-configured')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('nextjs')
-  })
+  // it(`vuecli v5 vue 2`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-vuecli-v5-vue-2')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('vuecli5-vue2')
+  // })
 
-  it(`create react app v4`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-create-react-app-js')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('create-react-app')
-  })
+  // it(`vuecli v5 vue 3`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-vuecli-v5-vue-3')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('vuecli5-vue3')
+  // })
 
-  it(`vite and react`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-react-vite')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('react-vite')
-  })
+  // it(`next v11+`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-nextjs-configured')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('nextjs')
+  // })
 
-  it(`vite and vue 3`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-vue3-vite')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('vue3-vite')
-  })
+  // it(`create react app v4`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-create-react-app-js')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('create-react-app')
+  // })
 
-  it(`nuxt 2`, async () => {
-    const dir = await  scaffoldMigrationProject('pristine-nuxtjs-vue2-configured')
-    const actual = await detect(dir)
-    expect(actual.id).to.eq('nuxt-2')
-  })
+  // it(`vite and react`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-react-vite')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('react-vite')
+  // })
+
+  // it(`vite and vue 3`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-vue3-vite')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('vue3-vite')
+  // })
+
+  // it(`nuxt 2`, async () => {
+  //   const dir = await  scaffoldMigrationProject('pristine-nuxtjs-vue2-configured')
+  //   const actual = await detect(dir)
+  //   expect(actual.id).to.eq('nuxt-2')
+  // })
 })
