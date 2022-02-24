6c482bf758af4b1e6b7f82aa2bb62d63f802e5bd
ElevateBart <ledouxb@gmail.com>
2022-02-24 09:44:30 -0600

    make mocha types load properly

--- packages/data-context/test/unit/helper.ts
+++ packages/data-context/test/unit/helper.ts
@@ -1,3 +1,5 @@
+// necessary to have mocha types working correctly
+import 'mocha'
 import { e2eProjectDirs } from '@packages/frontend-shared/cypress/e2e/support/e2eProjectDirs'
 import Fixtures from '@tooling/system-tests/lib/fixtures'
 
