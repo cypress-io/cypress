faef1815a3205bee1ea09867fed764835d724e93
Tyler Biethman <tbiethman@gmail.com>
2022-02-23 18:05:43 -0600

    fix: Updating Focus behavior to relaunch browser if runner has been closed
    Also updating firefox executable used to ensure focus behavior continues working for firefox

--- packages/server/lib/open_project.ts
+++ packages/server/lib/open_project.ts
@@ -180,7 +180,13 @@ export class OpenProject {
           return browsers.connectToNewSpec(browser, { onInitializeNewBrowserTab, ...options }, automation)
         }
 
+        this._ctx?.browser.setBrowserStatus('opening')
+
         return browsers.open(browser, options, automation, this._ctx)
+      }).then((browserInstance) => {
+        this._ctx?.browser.setBrowserStatus('open')
+
+        return browserInstance
       })
     }
 
@@ -294,6 +300,20 @@ export class OpenProject {
   // for testing purposes
   __reset () {
     this.resetOpenProject()
+  }
+
+  async sendFocusBrowserMessage () {
+    const isRunnerConnected = this.projectBase?.isRunnerSocketConnected()
+
+    // If the runner's socket is active and connected, we focus the active window
+    if (isRunnerConnected) {
+      return this.projectBase?.sendFocusBrowserMessage()
+    }
+
+    // Otherwise, we relaunch the app in the current browser
+    if (this.relaunchBrowser) {
+      return this.relaunchBrowser()
+    }
   }
 }
 
