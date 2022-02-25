7f8a810e3dd6e7c7e900d6e797ee5608262ab6e8
Zachary Williams <ZachJW34@gmail.com>
2022-02-25 17:29:17 -0600

    fix(launchpad): scaffold correct config file (#20372)

--- packages/data-context/test/unit/helper.ts
+++ packages/data-context/test/unit/helper.ts
@@ -2,6 +2,11 @@
 import 'mocha'
 import { e2eProjectDirs } from '@packages/frontend-shared/cypress/e2e/support/e2eProjectDirs'
 import Fixtures from '@tooling/system-tests/lib/fixtures'
+import { DataContext, DataContextConfig } from '../../src'
+import { graphqlSchema } from '@packages/graphql/src/schema'
+import type { BrowserApiShape } from '../../src/sources/BrowserDataSource'
+import type { AppApiShape, AuthApiShape, ElectronApiShape, LocalSettingsApiShape, ProjectApiShape } from '../../src/actions'
+import { InjectedConfigApi } from '../../src/data'
 
 export async function scaffoldMigrationProject (project: typeof e2eProjectDirs[number]) {
   Fixtures.removeProject(project)
@@ -9,4 +14,21 @@ export async function scaffoldMigrationProject (project: typeof e2eProjectDirs[n
   await Fixtures.scaffoldProject(project)
 
   return Fixtures.projectPath(project)
+}
+
+export function createTestDataContext (mode: DataContextConfig['mode'] = 'run') {
+  return new DataContext({
+    schema: graphqlSchema,
+    mode,
+    modeOptions: {},
+    appApi: {} as AppApiShape,
+    localSettingsApi: {} as LocalSettingsApiShape,
+    authApi: {} as AuthApiShape,
+    configApi: {
+      getServerPluginHandlers: () => [],
+    } as InjectedConfigApi,
+    projectApi: {} as ProjectApiShape,
+    electronApi: {} as ElectronApiShape,
+    browserApi: {} as BrowserApiShape,
+  })
 }
---
+++ system-tests/projects/vue3-vite-ts-configured/src/components/HelloWorld.cy.ts
@@ -0,0 +1,12 @@
+import { mount } from '@cypress/vue'
+import HelloWorld from './HelloWorld.vue'
+
+it('works', () => {
+  mount(HelloWorld, {
+    props: {
+      msg: 'Hello!'
+    }
+  })
+
+  cy.contains('Hello!')
+})
