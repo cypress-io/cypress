2ba40a0d4bc434051e580f2683a044bac4694739
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2022-02-25 16:15:34 -0600

    chore: Refactor Command Log UI (#20202)

--- packages/reporter/cypress.config.ts
+++ packages/reporter/cypress.config.ts
@@ -1,27 +1,25 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  'projectId': 'ypt4pf',
-  'viewportWidth': 400,
-  'viewportHeight': 450,
-  'reporter': '../../node_modules/cypress-multi-reporters/index.js',
-  'reporterOptions': {
-    'configFile': '../../mocha-reporter-config.json',
+  projectId: 'ypt4pf',
+  reporter: '../../node_modules/cypress-multi-reporters/index.js',
+  reporterOptions: {
+    configFile: '../../mocha-reporter-config.json',
   },
-  'retries': {
-    'runMode': 2,
-    'openMode': 0,
+  retries: {
+    runMode: 2,
+    openMode: 0,
   },
-  'e2e': {
-    'baseUrl': 'http://localhost:5006',
-    setupNodeEvents (on, config) {
+  e2e: {
+    baseUrl: 'http://localhost:5006',
+    setupNodeEvents (_on, config) {
       const express = require('express')
 
       express().use(express.static('dist')).listen(5006)
 
       return config
     },
-    'viewportHeight': 660,
-    'viewportWidth': 1000,
+    viewportHeight: 660,
+    viewportWidth: 400,
   },
 })
