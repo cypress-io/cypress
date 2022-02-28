735ec628b43ebf9da59ea91fd9ac132e2e4b9f8c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-02-28 16:59:21 +1000

    update path for requiring plugins

--- system-tests/projects/migration-specs-already-migrated/expected-cypress.config.js
+++ system-tests/projects/migration-specs-already-migrated/expected-cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents (on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('cypress/plugins/index.js')(on, config)
     },
     specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
   },
