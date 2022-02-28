49112cac4d8b7b900cecb39f0ac916445fc04f4c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-02-28 17:13:24 +1000

    fix test

--- system-tests/projects/migration-e2e-fully-custom/expected-cypress.config.js
+++ system-tests/projects/migration-e2e-fully-custom/expected-cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents (on, config) {
-      return require('./src/the-plugins-file.js')(on, config)
+      return require('src/the-plugins-file.js')(on, config)
     },
     specPattern: 'src/**/*.spec.js',
     supportFile: 'src/some-support-file.js',
