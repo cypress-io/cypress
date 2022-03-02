fc47ca4857e5d3f6bc8df471773911fa2edbd2e8
ElevateBart <ledouxb@gmail.com>
2022-03-02 09:52:29 -0600

    only cachebust when open mode

--- npm/webpack-dev-server/src/browser.ts
+++ npm/webpack-dev-server/src/browser.ts
@@ -1,5 +1,8 @@
-function render () {
-  require(`!!./loader.js!./browser.js?${(new Date()).getTime()}`)
+// In the last version of nextjs, the caching mechanism is hard to remove.
+// The cacheBust here allows us to disable it in open mode while keeping it in run mode.
+// When user will add a new spec in edit mode, there won't be any cache.
+function render (cacheBust: boolean) {
+  require(`!!./loader.js!./browser.js${cacheBust ? `?${(new Date()).getTime()}` : ''}`)
 }
 
-render()
+render(!parent.Cypress.config('isTextTerminal'))
