54fc59f629c3259559f730a9ec8106c93df6bfe1
Emily Rohrbough <emilyrohrbough@yahoo.com>
2022-03-03 14:09:57 -0600

    easier to use IMO

--- packages/driver/src/cy/logGroup.ts
+++ packages/driver/src/cy/logGroup.ts
@@ -1,50 +1,40 @@
 import Promise from 'bluebird'
 import $errUtils from '../cypress/error_utils'
 
-export default class LogGroup {
-  private cy
-  private subject
-  private options
+export default (Cypress, userOptions, fn) => {
+  let log
+  const cy = Cypress.cy
+  const current = cy.state('current')
 
-  log
-
-  constructor (cy, userOptions) {
-    this.cy = cy
-
-    const current = cy.state('current')
-
-    this.options = {
-      name: current.get('name'),
-      type: current.get('type'),
-      log: true,
-      ...userOptions,
-      groupStart: true,
-    }
-
-    this.options.emitOnly = !this.options.log
-    this.subject = userOptions.$el || null
+  const options = {
+    name: current.get('name'),
+    type: current.get('type'),
+    log: true,
+    ...userOptions,
+    groupStart: true,
   }
 
-  run (fn) {
-    if (!_.isFunction(fn)) {
-      // update this to be a logGroup error message
-      $errUtils.throwErrByPath('group.invalid_argument', { onFail: this.log })
+  options.emitOnly = !options.log
+
+  const subject = userOptions.$el || null
+
+  if (!_.isFunction(fn)) {
+    $errUtils.throwErrByPath('group.missing_fn', { onFail: log })
+  }
+
+  return cy.then(() => {
+    // pull forward the original command's subject to correctly yield the subject in the console
+    cy.state('current').set('subject', subject)
+
+    log = Cypress.log(options)
+
+    return Promise.try(() => fn(log))
+  })
+  .then((subject) => {
+    if (log) {
+      log.endGroup()
     }
 
-    return this.cy.then(() => {
-      // pull forward the original command's subject to correctly yield the subject in the console
-      cy.state('current').set('subject', this.subject)
-
-      this.log = Cypress.log(this.options)
-
-      return Promise.try(fn)
-    })
-    .then((subject) => {
-      if (this.log) {
-        this.log.endGroup()
-      }
-
-      return subject
-    })
-  }
+    return subject
+  })
 }
