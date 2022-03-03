f16d0ac6df7e9850334a00ef96e7b848b22d3293
Ryan Manuel <ryanm@cypress.io>
2022-03-03 12:00:51 -0600

    test: try out session experiment to fix net stubbing flake

--- packages/driver/cypress/integration/commands/net_stubbing_spec.ts
+++ packages/driver/cypress/integration/commands/net_stubbing_spec.ts
@@ -11,7 +11,7 @@ const testFail = (cb, expectedDocsUrl = 'https://on.cypress.io/intercept') => {
   })
 }
 
-describe('network stubbing', function () {
+describe('network stubbing', { experimentalSessionSupport: true }, function () {
   const { $, _, sinon, state, Promise } = Cypress
 
   beforeEach(function () {
