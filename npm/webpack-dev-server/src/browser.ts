fda170a6a38617c6d43ff3d8839f4a8baa863ba5
ElevateBart <ledouxb@gmail.com>
2022-03-03 08:45:52 -0600

    fix: cleanup unsafeCache to omit browser.js

--- npm/webpack-dev-server/src/browser.ts
+++ npm/webpack-dev-server/src/browser.ts
@@ -1,8 +1,5 @@
-// In the last version of nextjs, the caching mechanism is hard to remove.
-// The cacheBust here allows us to disable it in open mode while keeping it in run mode.
-// When user will add a new spec in edit mode, there won't be any cache.
-function render (cacheBust: boolean) {
-  require(`!!./loader.js!./browser.js${cacheBust ? `?${(new Date()).getTime()}` : ''}`)
+function render () {
+  require('!!./loader.js!./browser.js')
 }
 
-render(!parent.Cypress.config('isTextTerminal'))
+render()
