9506e6b46a8f0761cb9f20fa4c33dfb541dbf69d
Bill Glesias <bglesias@gmail.com>
2022-03-07 17:36:58 -0500

    add system test to verify stack trace is corrected for multi-domain errors

---
+++ system-tests/__snapshots__/multi_domain_error_spec.ts.js
@@ -0,0 +1,74 @@
+exports['e2e multi domain errors / captures the stack trace correctly for multi-domain errors to point users to their "switchToDomain" callback'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (multi_domain_error_spec.ts)                                               │
+  │ Searched:   cypress/integration/multi_domain_error_spec.ts                                     │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  multi_domain_error_spec.ts                                                      (1 of 1)
+
+
+  multi-domain
+    1) tries to find an element that doesn't exist and fails
+
+
+  0 passing
+  1 failing
+
+  1) multi-domain
+       tries to find an element that doesn't exist and fails:
+     AssertionError: Timed out retrying after 1000ms: Expected to find element: \`#doesnotexist\`, but never found it.
+      [stack trace lines]
+
+
+
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        1                                                                                │
+  │ Passing:      0                                                                                │
+  │ Failing:      1                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  1                                                                                │
+  │ Video:        true                                                                             │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     multi_domain_error_spec.ts                                                       │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+  (Screenshots)
+
+  -  /XXX/XXX/XXX/cypress/screenshots/multi_domain_error_spec.ts/multi-domain -- trie     (1280x720)
+     s to find an element that doesn't exist and fails (failed).png                                 
+
+
+  (Video)
+
+  -  Started processing:  Compressing to 32 CRF                                                     
+  -  Finished processing: /XXX/XXX/XXX/cypress/videos/multi_domain_error_spec.ts.mp4      (X second)
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+       Spec                                              Tests  Passing  Failing  Pending  Skipped  
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✖  multi_domain_error_spec.ts               XX:XX        1        -        1        -        - │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    ✖  1 of 1 failed (100%)                     XX:XX        1        -        1        -        -  
+
+
+`
---
+++ system-tests/projects/e2e/cypress/integration/multi_domain_error_spec.ts
@@ -0,0 +1,15 @@
+// @ts-ignore / session support is needed for visiting about:blank between tests
+describe('multi-domain', () => {
+  beforeEach(() => {
+    cy.visit('/multi_domain.html')
+    cy.get('a[data-cy="multi_domain_secondary_link"]').click()
+  })
+
+  it('tries to find an element that doesn\'t exist and fails', () => {
+    cy.switchToDomain('foobar.com', () => {
+      cy.get('#doesnotexist', {
+        timeout: 1000,
+      })
+    })
+  })
+})
