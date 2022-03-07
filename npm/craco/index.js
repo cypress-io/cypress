be47deea858066c8e07ae4f0b696a3d75f252b3e
Brian Mann <brian.mann86@gmail.com>
2022-03-07 17:51:37 -0500

    begin implementation of porting craco into its own devServer

---
+++ npm/craco/index.js
@@ -0,0 +1,17 @@
+const { startDevServer } = require('@cypress/webpack-dev-server')
+const { createWebpackDevConfig } = require('@craco/craco')
+
+const devServer = (cypressDevServerConfig) => {
+  process.env.FAST_REFRESH = 'false'
+
+  const { config } = cypressDevServerConfig
+
+  const cracoConfig = config.cracoConfig = config.cracoConfig || {}
+
+  return startDevServer({
+    options: cypressDevServerConfig,
+    webpackConfig: createWebpackDevConfig(cracoConfig),
+  })
+}
+
+module.exports.devServer = devServer
