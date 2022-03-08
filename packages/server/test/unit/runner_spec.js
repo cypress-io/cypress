bd3188e30a00d20465d912604d594389ac432b27
Mark Noonan <mark@cypress.io>
2022-03-08 16:50:37 +0000

    remove reference to global.root

--- packages/server/test/unit/runner_spec.js
+++ packages/server/test/unit/runner_spec.js
@@ -1,6 +1,6 @@
 require('../spec_helper')
 
-const { serveRunner } = require(`${root}/lib/controllers/runner`)
+const { serveRunner } = require(`../../lib/controllers/runner`)
 
 describe('controllers/runner', () => {
   describe('serveRunner', () => {
