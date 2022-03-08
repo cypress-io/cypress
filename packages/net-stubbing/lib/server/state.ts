25cdf6d8ba375b3e32c6bd80181d348dcd3ed745
Ryan Manuel <ryanm@cypress.io>
2022-03-08 12:39:24 -0600

    Tweak clean up a bit

--- packages/net-stubbing/lib/server/state.ts
+++ packages/net-stubbing/lib/server/state.ts
@@ -7,14 +7,13 @@ export function state (): NetStubbingState {
     routes: [],
     pendingEventHandlers: {},
     async reset () {
-      console.log('resetting', Object.keys(this.requests))
       await Promise.all(Object.values <InterceptedRequest>(this.requests).map((request) => {
-        const responseEvents = ['before:response', 'response:callback', 'response']
-        const inResponsePhase = responseEvents.includes(request.lastEvent!)
+        const responseOrErrorEvents = ['before:response', 'response:callback', 'response', 'network:error']
+        const inResponseOrErrorPhase = responseOrErrorEvents.includes(request.lastEvent!)
         const { res } = request
 
         if (res.destroyed) {
-          console.log('already destroyed')
+          delete this.requests[request.id]
 
           return Promise.resolve()
         }
@@ -24,8 +23,7 @@ export function state (): NetStubbingState {
         request.subscriptionsByRoute.forEach((subscriptionByRoute) => {
           if (!subscriptionByRoute.immediateStaticResponse) {
             subscriptionByRoute.subscriptions.forEach((subscription) => {
-              if (subscription.await && !subscription.skip && (responseEvents.includes(subscription.eventName) || !inResponsePhase)) {
-                console.log('Destroying due to', subscription)
+              if (subscription.await && !subscription.skip && (responseOrErrorEvents.includes(subscription.eventName) || !inResponseOrErrorPhase)) {
                 shouldDestroyResponse = true
               }
             })
@@ -38,14 +36,14 @@ export function state (): NetStubbingState {
 
           return new Promise<void>((resolve) => {
             res.once('finish', () => {
-              console.log('finished')
               res.destroy()
+              delete this.requests[request.id]
               resolve()
             })
 
             res.once('error', () => {
-              console.log('errored')
               res.destroy()
+              delete this.requests[request.id]
               resolve()
             })
 
@@ -53,13 +51,10 @@ export function state (): NetStubbingState {
           })
         }
 
-        console.log('not destroyed')
-
         return Promise.resolve()
       }))
 
       this.pendingEventHandlers = {}
-      this.requests = {}
       this.routes = []
     },
   }
