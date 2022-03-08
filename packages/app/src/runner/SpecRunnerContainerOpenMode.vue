69532d189adfd0090516270ec1de18471177d485
Mark Noonan <mark@cypress.io>
2022-03-08 09:20:18 +0000

    cleanup

--- packages/app/src/runner/SpecRunnerContainerOpenMode.vue
+++ packages/app/src/runner/SpecRunnerContainerOpenMode.vue
@@ -33,7 +33,7 @@ watchSpec(specs)
 watchEffect(() => {
   const specPath = router.currentRoute.value.query.file
 
-  if (specPath && specStore && !specStore.activeSpec) {
+  if (specPath && !specStore.activeSpec) {
     router.push({ name: 'Specs', params: {
       unrunnable: router.currentRoute.value.query.file as string,
     } })
