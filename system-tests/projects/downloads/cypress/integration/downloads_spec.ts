ec257f30c1b7e79cab2f4719239292c5b96ec580
Ryan Manuel <ryanm@cypress.io>
2022-03-09 11:37:58 -0600

    test: fix system-tests-firefox downloads flake

--- system-tests/projects/downloads/cypress/integration/downloads_spec.ts
+++ system-tests/projects/downloads/cypress/integration/downloads_spec.ts
@@ -6,7 +6,27 @@ describe('downloads', () => {
   })
 
   it('handles csv file download', () => {
-    cy.get('[data-cy=download-csv]').click()
+    // HACK: monkey-patching Cypress.downloads.end to ensure the download has
+    // finished before asserting on its contents. this test was flakey b/c
+    // sometimes file was read before the browser had written its contents.
+    // consider creating events for downloads or some API, so that this can
+    // be done without hacks
+    const awaitDownload = new Promise<void>((resolve) => {
+      // @ts-ignore
+      const end = Cypress.downloads.end
+
+      // @ts-ignore
+      Cypress.downloads.end = (arg) => {
+        resolve()
+
+        return end(arg)
+      }
+    })
+
+    cy.get('[data-cy=download-csv]').click().then(() => {
+      return awaitDownload
+    })
+
     cy
     .readFile(`${Cypress.config('downloadsFolder')}/records.csv`)
     .should('contain', '"Joe","Smith"')
