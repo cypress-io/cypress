b8ea5451c3da94172493d841def327a5cfcd30dd
Jessica Sachs <jess@jessicasachs.io>
2022-03-09 23:52:27 -0500

    adding another project example

--- system-tests/test/config_modules_spec.ts
+++ system-tests/test/config_modules_spec.ts
@@ -8,6 +8,7 @@ describe('cypress config with esm and cjs', function () {
     'config-cjs-and-esm/config-with-mjs',
     'config-cjs-and-esm/config-with-cjs',
     'config-cjs-and-esm/config-with-js-module',
+    'config-cjs-and-esm/config-with-ts-module',
 
     // This covers Vite and SvelteKit e2e projects
     'config-cjs-and-esm/config-with-ts-module-and-esbuild',
