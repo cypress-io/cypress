04d739f5deb414d34469d80d65e919d9aa78bcb5
Emily Rohrbough <emilyrohrbough@yahoo.com>
2022-03-10 10:42:04 -0600

    chore(unification): fix flakey percy test in packages/app

--- packages/app/cypress/e2e/support/e2eSupport.ts
+++ packages/app/cypress/e2e/support/e2eSupport.ts
@@ -1,2 +1,13 @@
 import '@packages/frontend-shared/cypress/e2e/support/e2eSupport'
+
+import { installCustomPercyCommand } from '@packages/ui-components/cypress/support/customPercyCommand'
+
+// @ts-ignore
+installCustomPercyCommand({
+  elementOverrides: {
+    // @ts-ignore
+    '.runnable-header .duration': ($el) => $el.text('XX:XX'),
+  },
+})
+
 import 'cypress-real-events/support'
