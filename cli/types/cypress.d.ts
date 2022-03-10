c68c06b72ca18ef85580715ee26e97a5508a075f
estrada9166 <estrada9166@hotmail.com>
2022-03-10 14:50:26 -0500

    Fix test/TS

--- cli/types/cypress.d.ts
+++ cli/types/cypress.d.ts
@@ -2625,12 +2625,6 @@ declare namespace Cypress {
     /**
      * A String or Array of glob patterns used to ignore test files that would otherwise be shown in your list of tests. Cypress uses minimatch with the options: {dot: true, matchBase: true}. We suggest using http://globtester.com to test what files would match.
      * @default "*.hot-update.js"
-     * @deprecated use `excludeSpecPattern` instead
-     */
-    ignoreTestFiles: string | string[]
-    /**
-     * A String or Array of glob patterns used to ignore test files that would otherwise be shown in your list of tests. Cypress uses minimatch with the options: {dot: true, matchBase: true}. We suggest using http://globtester.com to test what files would match.
-     * @default "*.hot-update.js"
      */
     excludeSpecPattern: string | string[]
     /**
