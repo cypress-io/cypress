3a4e835c55fd9b295c7596a7d8f95696bee78030
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-03-11 17:31:10 +1000

    add an extra test for migration

--- system-tests/projects/migration-e2e-component-default-everything/expected-cypress.config.js
+++ system-tests/projects/migration-e2e-component-default-everything/expected-cypress.config.js
@@ -1,14 +1,17 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  viewportWidth: 1200,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents (on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
+    viewportWidth: 1600,
   },
   component: {
     setupNodeEvents (on, config) {},
+    viewportWidth: 400,
   },
 })
