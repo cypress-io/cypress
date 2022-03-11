9d776b6dfddaf5e20596479eda2ea7be8096ef1e
Jessica Sachs <jess@jessicasachs.io>
2022-03-10 22:06:04 -0500

    reverting commented out tests

--- system-tests/test/config_modules_spec.ts
+++ system-tests/test/config_modules_spec.ts
@@ -5,14 +5,14 @@ describe('cypress config with esm and cjs', function () {
   systemTests.setup()
 
   ;[
-    // 'config-cjs-and-esm/config-with-mjs',
-    // 'config-cjs-and-esm/config-with-cjs',
-    // 'config-cjs-and-esm/config-with-js-module',
-    // 'config-cjs-and-esm/config-with-ts-module',
+    'config-cjs-and-esm/config-with-mjs',
+    'config-cjs-and-esm/config-with-cjs',
+    'config-cjs-and-esm/config-with-js-module',
+    'config-cjs-and-esm/config-with-ts-module',
 
-    // // This covers Vite and SvelteKit e2e projects
-    // 'config-cjs-and-esm/config-with-ts-module-and-esbuild',
-    // 'config-cjs-and-esm/config-with-ts-tsconfig-es5',
+    // This covers Vite and SvelteKit e2e projects
+    'config-cjs-and-esm/config-with-ts-module-and-esbuild',
+    'config-cjs-and-esm/config-with-ts-tsconfig-es5',
   ].forEach((project) => {
     systemTests.it(`supports modules and cjs in ${project}`, {
       project,
@@ -24,7 +24,7 @@ describe('cypress config with esm and cjs', function () {
   })
 
   ;[
-    // 'config-cjs-and-esm/config-with-ts-module-component',
+    'config-cjs-and-esm/config-with-ts-module-component',
   ].forEach((project) => {
     // This covers Vite and SvelteKit component testing projects
     systemTests.it(`supports modules and cjs in ${project}`, {
