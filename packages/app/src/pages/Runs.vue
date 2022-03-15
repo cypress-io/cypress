db21ad1c955a4b61349559190897e07656a4e562
estrada9166 <estrada9166@hotmail.com>
2022-03-15 10:10:41 -0500

    Fix test

--- packages/app/src/pages/Runs.vue
+++ packages/app/src/pages/Runs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full p-24px relative">
     <TransitionQuickFade>
-      <RunsSkeleton v-if="query.fetching.value || !query.data.value" />
+      <RunsSkeleton v-if="query.fetching.value || !query.data.value || !cloudProject" />
       <RunsContainer
         v-else
         :gql="query.data.value"
@@ -17,6 +17,7 @@ import { RunsDocument } from '../generated/graphql'
 import RunsSkeleton from '../runs/RunsSkeleton.vue'
 import RunsContainer from '../runs/RunsContainer.vue'
 import TransitionQuickFade from '@cy/components/transitions/TransitionQuickFade.vue'
+import { computed } from 'vue'
 
 gql`
 query Runs {
@@ -24,6 +25,10 @@ query Runs {
 }`
 
 const query = useQuery({ query: RunsDocument, requestPolicy: 'cache-and-network' })
+
+const cloudProject = computed(() => {
+  return query.data.value?.currentProject?.cloudProject
+})
 
 function reexecuteRunsQuery () {
   query.executeQuery()
