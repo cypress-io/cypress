1ae7359f2412f5cdc42eb8084fcc9d06293c6f78
Chris Breiding <chrisbreiding@gmail.com>
2022-03-16 16:25:16 -0400

    fix tests

--- packages/driver/cypress/integration/e2e/multi-domain/cookie_login_spec.ts
+++ packages/driver/cypress/integration/e2e/multi-domain/cookie_login_spec.ts
@@ -10,7 +10,7 @@ describe('multi-domain - cookie login', { experimentalSessionSupport: true }, ()
     cy.session('ZJohnson', () => {
       cy.visit('/fixtures/multi-domain.html')
       cy.get('[data-cy="cookie-login"]').click()
-      cy.switchToDomain('foobar.com', () => {
+      cy.switchToDomain('http://foobar.com:3500', () => {
         cy.get('[data-cy="username"]').type('ZJohnson')
         cy.get('[data-cy="login"]').click()
       })
@@ -31,7 +31,7 @@ describe('multi-domain - cookie login', { experimentalSessionSupport: true }, ()
     })
 
     it('works with no SameSite, no Secure', () => {
-      cy.switchToDomain('foobar.com', () => {
+      cy.switchToDomain('http://foobar.com:3500', () => {
         cy.get('[data-cy="username"]').type('AJohnson')
         cy.get('[data-cy="login"]').click()
       })
@@ -40,7 +40,7 @@ describe('multi-domain - cookie login', { experimentalSessionSupport: true }, ()
     })
 
     it('works with SameSite=None, Secure', () => {
-      cy.switchToDomain('foobar.com', () => {
+      cy.switchToDomain('http://foobar.com:3500', () => {
         cy.get('[data-cy="sameSite"]').select('None')
         cy.get('[data-cy="secure"]').check()
         cy.get('[data-cy="username"]').type('BJohnson')
@@ -51,7 +51,7 @@ describe('multi-domain - cookie login', { experimentalSessionSupport: true }, ()
     })
 
     it('works with SameSite=None, no Secure', () => {
-      cy.switchToDomain('foobar.com', () => {
+      cy.switchToDomain('http://foobar.com:3500', () => {
         cy.get('[data-cy="sameSite"]').select('None')
         cy.get('[data-cy="username"]').type('CJohnson')
         cy.get('[data-cy="login"]').click()
@@ -61,7 +61,7 @@ describe('multi-domain - cookie login', { experimentalSessionSupport: true }, ()
     })
 
     it('works with SameSite=Lax, Secure', () => {
-      cy.switchToDomain('foobar.com', () => {
+      cy.switchToDomain('http://foobar.com:3500', () => {
         cy.get('[data-cy="sameSite"]').select('Lax')
         cy.get('[data-cy="secure"]').check()
         cy.get('[data-cy="username"]').type('DJohnson')
@@ -72,7 +72,7 @@ describe('multi-domain - cookie login', { experimentalSessionSupport: true }, ()
     })
 
     it('works with SameSite=Strict, Secure', () => {
-      cy.switchToDomain('foobar.com', () => {
+      cy.switchToDomain('http://foobar.com:3500', () => {
         cy.get('[data-cy="sameSite"]').select('Strict')
         cy.get('[data-cy="secure"]').check()
         cy.get('[data-cy="username"]').type('EJohnson')
@@ -83,7 +83,7 @@ describe('multi-domain - cookie login', { experimentalSessionSupport: true }, ()
     })
 
     it('works with invalid SameSite, Secure', () => {
-      cy.switchToDomain('foobar.com', () => {
+      cy.switchToDomain('http://foobar.com:3500', () => {
         cy.get('[data-cy="sameSite"]').select('Invalid')
         cy.get('[data-cy="secure"]').check()
         cy.get('[data-cy="username"]').type('FJohnson')
