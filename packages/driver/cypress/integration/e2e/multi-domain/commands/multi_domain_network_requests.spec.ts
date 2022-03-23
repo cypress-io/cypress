44ec725319f805327df557987e91edceab308e3c
Chris Breiding <chrisbreiding@gmail.com>
2022-03-23 09:31:34 -0400

    chore: uncomment cy.request() test, add one more

--- packages/driver/cypress/integration/e2e/multi-domain/commands/multi_domain_network_requests.spec.ts
+++ packages/driver/cypress/integration/e2e/multi-domain/commands/multi_domain_network_requests.spec.ts
@@ -1,14 +1,21 @@
 // @ts-ignore / session support is needed for visiting about:blank between tests
-// FIXME: received cross-origin errors
-context.skip('multi-domain network requests', { experimentalSessionSupport: true }, () => {
+context('multi-domain network requests', { experimentalSessionSupport: true }, () => {
   beforeEach(() => {
     cy.visit('/fixtures/multi-domain.html')
     cy.get('a[data-cy="request-link"]').click()
   })
 
-  it('.request()', () => {
+  it('.request() to secondary domain', () => {
     cy.switchToDomain('http://foobar.com:3500', () => {
       cy.request('/fixtures/example.json').should((response) => {
+        expect(response.status).to.equal(200)
+      })
+    })
+  })
+
+  it('.request() to primary domain', () => {
+    cy.switchToDomain('http://foobar.com:3500', () => {
+      cy.request('http://localhost:3500/fixtures/example.json').should((response) => {
         expect(response.status).to.equal(200)
       })
     })
