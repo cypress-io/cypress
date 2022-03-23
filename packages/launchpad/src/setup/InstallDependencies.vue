2aa331ba5d4cf122adc095e15e6cf5a136058d57
estrada9166 <estrada9166@hotmail.com>
2022-03-23 15:21:42 -0500

    Fix TS

--- packages/launchpad/src/setup/InstallDependencies.vue
+++ packages/launchpad/src/setup/InstallDependencies.vue
@@ -6,7 +6,7 @@
     :next-fn="confirmInstalled"
     class="max-w-640px relative"
     :main-button-variant="canNavigateForward ? 'primary' : 'pending'"
-    :skip-fn="canNavigateForward ? null : confirmInstalled"
+    :skip-fn="canNavigateForward ? undefined : confirmInstalled"
   >
     <ManualInstall
       :gql="props.gql"
