92b9eb286b7298f0c32c01cb9bedf36accc73b02
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-03-24 16:22:41 +1000

    add logging

--- packages/app/cypress/e2e/specs_list_e2e.cy.ts
+++ packages/app/cypress/e2e/specs_list_e2e.cy.ts
@@ -113,12 +113,10 @@ describe('Spec List - Git Status', () => {
   beforeEach(() => {
     cy.scaffoldProject('cypress-in-cypress')
     .then((projectPath) => {
-      cy.task('initGitRepoForTestProject', projectPath).then(() => {
-        cy.wait(100)
-        cy.openProject('cypress-in-cypress')
-        cy.startAppServer('e2e')
-        cy.visitApp()
-      })
+      cy.task('initGitRepoForTestProject', projectPath)
+      cy.openProject('cypress-in-cypress')
+      cy.startAppServer('e2e')
+      cy.visitApp()
     })
   })
 
