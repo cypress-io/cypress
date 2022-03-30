8d08812db6209f40a8732e998275dc9bbb1cde2d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-03-30 11:40:49 +1000

    exclude scaffolded assets from linting

--- system-tests/projects/pristine/expected-cypress-ts-e2e/cypress.config.ts
+++ system-tests/projects/pristine/expected-cypress-ts-e2e/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents (on, config) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
