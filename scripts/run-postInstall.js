95869455a8632cdeca6076e94fce1974b30e3aee
estrada9166 <estrada9166@hotmail.com>
2022-03-31 17:06:42 -0500

    Update script

---
+++ scripts/run-postInstall.js
@@ -0,0 +1,12 @@
+const os = require('os')
+const { execSync } = require('child_process')
+
+if (os.platform() === 'win32' && !process.env.CI) {
+  execSync('patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build', {
+    stdio: 'inherit',
+  })
+} else {
+  execSync('patch-package && ./scripts/run-if-not-ci.sh yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && ./scripts/run-if-not-ci.sh yarn build', {
+    stdio: 'inherit',
+  })
+}
