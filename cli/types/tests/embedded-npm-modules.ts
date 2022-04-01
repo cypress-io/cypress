a018d9e7db644af12197fdfff48b963c6edc18a3
Tim Griesser <tgriesser10@gmail.com>
2022-04-01 13:43:45 -0400

    Attempting to get the dts tests passing

--- cli/types/tests/embedded-npm-modules.ts
+++ cli/types/tests/embedded-npm-modules.ts
@@ -1,6 +1,11 @@
-import * as mountUtils from '@cypress/mount-utils'
+import { mount } from '@cypress/react'
+import { mount as bundledMount } from 'cypress/react'
 
-import * as reactMount from '@cypress/react'
+import { StyleOptions } from '@cypress/mount-utils'
+import { StyleOptions as BundledStyleOptions } from 'cypress/mount-utils'
 
-mountUtils // $ExpectType
-reactMount // $ExpectType
+type StyleOptionsA = StyleOptions // $ExpectType StyleOptions
+type StyleOptionsB = BundledStyleOptions // $ExpectType StyleOptions
+
+type MountA = typeof mount // $ExpectType (jsx: ReactNode, options?: Partial<StyleOptions & MountReactComponentOptions> | undefined) => Chainable<MountReturn>
+type MountB = typeof bundledMount // $ExpectType (jsx: ReactNode, options?: Partial<StyleOptions & MountReactComponentOptions> | undefined) => Chainable<MountReturn>
