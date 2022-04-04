c82bbba0d5de15a2984fd71049bab500b6328e22
Mark Noonan <mark@cypress.io>
2022-04-04 07:48:10 -0400

    run different set of commands in CI for windows

--- scripts/run-postInstall.js
+++ scripts/run-postInstall.js
@@ -1,10 +1,16 @@
 const os = require('os')
 const { execSync } = require('child_process')
 
-if (os.platform() === 'win32' && !process.env.CI) {
-  execSync('patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build', {
-    stdio: 'inherit',
-  })
+if (os.platform() === 'win32') {
+  if (process.env.CI) {
+    execSync('patch-package && yarn clean && gulp postinstall', {
+      stdio: 'inherit',
+    })
+  } else {
+    execSync('patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build', {
+      stdio: 'inherit',
+    })
+  }
 } else {
   execSync('patch-package && ./scripts/run-if-not-ci.sh yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && ./scripts/run-if-not-ci.sh yarn build', {
     stdio: 'inherit',
