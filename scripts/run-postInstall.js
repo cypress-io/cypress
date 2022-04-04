9e121d88114988226425e1365faa0e7cc08d1cb9
estrada9166 <estrada9166@hotmail.com>
2022-04-04 09:57:53 -0500

    Update postinstall script

--- scripts/run-postInstall.js
+++ scripts/run-postInstall.js
@@ -1,12 +1,12 @@
-const os = require('os')
 const { execSync } = require('child_process')
 
-if (os.platform() === 'win32' && !process.env.CI) {
-  execSync('patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build', {
-    stdio: 'inherit',
-  })
-} else {
-  execSync('patch-package && ./scripts/run-if-not-ci.sh yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && ./scripts/run-if-not-ci.sh yarn build', {
-    stdio: 'inherit',
-  })
+const executionEnv = process.env.CI ? 'ci' : 'local'
+
+const postInstallCommands = {
+  local: 'patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build',
+  ci: 'patch-package && yarn clean && gulp postinstall',
 }
+
+execSync(postInstallCommands[executionEnv], {
+  stdio: 'inherit',
+})
