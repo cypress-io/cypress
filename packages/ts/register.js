a4ca74ab54af063641af5a75fe8c066ae1c1d91e
Barthélémy Ledoux <bart@cypress.io>
2022-04-05 16:30:47 -0500

    chore: add comment
    Co-authored-by: Zach Bloomquist <git@chary.us>

--- packages/ts/register.js
+++ packages/ts/register.js
@@ -1,1 +1,3 @@
+// This file can be used as a shortcut to invoke `registerDir` without a scope.
+// This is useful in Mocha tests with the `-r` option (`-r @packages/ts/register`)
 require('./registerDir')()
