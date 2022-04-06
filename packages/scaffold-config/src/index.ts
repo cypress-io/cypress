06fdf47bc5914ee7c201eb97dcc25a1569daa6b2
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-04-06 11:44:11 +1000

    move files from data-context to scaffold-config

--- packages/scaffold-config/src/index.ts
+++ packages/scaffold-config/src/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable padding-line-between-statements */
 // created by autobarrel, do not modify directly
 
+export * from './commandFile'
 export * from './dependencies'
 export * from './detect'
 export * from './frameworks'
+export * from './supportFile'
---
+++ packages/scaffold-config/src/supportFile.ts
@@ -0,0 +1,27 @@
+import type { CodeLanguage } from '@packages/types'
+import dedent from 'dedent'
+
+export function supportFileBody (fileName: 'e2e' | 'component', language: CodeLanguage['type']) {
+  return dedent`
+    // ***********************************************************
+    // This example support/${fileName}.${language} is processed and
+    // loaded automatically before your test files.
+    //
+    // This is a great place to put global configuration and
+    // behavior that modifies Cypress.
+    //
+    // You can change the location of this file or turn off
+    // automatically serving support files with the
+    // 'supportFile' configuration option.
+    //
+    // You can read more here:
+    // https://on.cypress.io/configuration
+    // ***********************************************************
+
+    // Import commands.js using ES2015 syntax:
+    import './commands'
+
+    // Alternatively you can use CommonJS syntax:
+    // require('./commands')
+  `
+}
