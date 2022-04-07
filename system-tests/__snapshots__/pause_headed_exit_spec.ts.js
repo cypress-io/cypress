27e6079295f20e94e31ca3402a232ece5c1ec341
David Munechika <david.munechika@gatech.edu>
2022-04-07 12:28:44 -0400

    add onSpawn callback to system tests

---
+++ system-tests/__snapshots__/pause_headed_exit_spec.ts.js
@@ -0,0 +1,181 @@
+exports['cy.pause() works with --headed and --no-exit / pauses with --headed and --no-exit'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (pause_spec.js)                                                            │
+  │ Searched:   cypress/integration/pause_spec.js                                                  │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  pause_spec.js                                                                   (1 of 1)
+
+
+  cy.pause()
+    ✓ pauses
+
+
+  1 passing
+
+not exiting due to options.exit being false
+
+`
+
+exports['cy.pause() works with --headed and --no-exit / does not pause if headless'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (no_pause_spec.js)                                                         │
+  │ Searched:   cypress/integration/no_pause_spec.js                                               │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  no_pause_spec.js                                                                (1 of 1)
+
+
+  cy.pause()
+    ✓ pauses
+
+
+  1 passing
+
+not exiting due to options.exit being false
+
+`
+
+exports['cy.pause() works with --headed and --no-exit / does not pause without --no-exit'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (no_pause_spec.js)                                                         │
+  │ Searched:   cypress/integration/no_pause_spec.js                                               │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  no_pause_spec.js                                                                (1 of 1)
+
+
+  cy.pause()
+    ✓ pauses
+
+
+  1 passing
+
+Warning: We failed processing this video.
+
+This error will not alter the exit code.
+
+TimeoutError: operation timed out
+      [stack trace lines]
+
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        1                                                                                │
+  │ Passing:      1                                                                                │
+  │ Failing:      0                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  0                                                                                │
+  │ Video:        false                                                                            │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     no_pause_spec.js                                                                 │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+       Spec                                              Tests  Passing  Failing  Pending  Skipped  
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✔  no_pause_spec.js                         XX:XX        1        1        -        -        - │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    ✔  All specs passed!                        XX:XX        1        1        -        -        -  
+
+
+`
+
+exports['cy.pause() works with --headed and --no-exit / does not pause without --headed and --no-exit'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:    1.2.3                                                                              │
+  │ Browser:    FooBrowser 88                                                                      │
+  │ Specs:      1 found (no_pause_spec.js)                                                         │
+  │ Searched:   cypress/integration/no_pause_spec.js                                               │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  no_pause_spec.js                                                                (1 of 1)
+
+
+  cy.pause()
+    ✓ pauses
+
+
+  1 passing
+
+Warning: We failed processing this video.
+
+This error will not alter the exit code.
+
+TimeoutError: operation timed out
+      [stack trace lines]
+
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        1                                                                                │
+  │ Passing:      1                                                                                │
+  │ Failing:      0                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  0                                                                                │
+  │ Video:        false                                                                            │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     no_pause_spec.js                                                                 │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+       Spec                                              Tests  Passing  Failing  Pending  Skipped  
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✔  no_pause_spec.js                         XX:XX        1        1        -        -        - │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    ✔  All specs passed!                        XX:XX        1        1        -        -        -  
+
+
+`
--- system-tests/test/pause_headed_exit_spec.ts
+++ system-tests/test/pause_headed_exit_spec.ts
@@ -3,33 +3,47 @@ import systemTests from '../lib/system-tests'
 describe('cy.pause() works with --headed and --no-exit', () => {
   systemTests.setup()
 
-  systemTests.it('pauses', {
+  systemTests.it('pauses with --headed and --no-exit', {
     spec: 'pause_spec.js',
-    snapshot: false,
+    snapshot: true,
     headed: true,
     noExit: true,
-    expectedExitCode: 0,
+    expectedExitCode: null,
+    onSpawn: (cp) => {
+      cp.stdout.on('data', (buf) => {
+        if (buf.toString().includes('not exiting due to options.exit being false')) {
+          cp.kill()
+        }
+      })
+    },
   })
 
-  systemTests.it('does not pause', {
+  systemTests.it('does not pause if headless', {
     spec: 'no_pause_spec.js',
-    snapshot: false,
+    snapshot: true,
     headed: false,
     noExit: true,
-    expectedExitCode: 0,
+    expectedExitCode: null,
+    onSpawn: (cp) => {
+      cp.stdout.on('data', (buf) => {
+        if (buf.toString().includes('not exiting due to options.exit being false')) {
+          cp.kill()
+        }
+      })
+    },
   })
 
-  systemTests.it('does not pause', {
+  systemTests.it('does not pause without --no-exit', {
     spec: 'no_pause_spec.js',
-    snapshot: false,
+    snapshot: true,
     headed: true,
     noExit: false,
     expectedExitCode: 0,
   })
 
-  systemTests.it('does not pause', {
+  systemTests.it('does not pause without --headed and --no-exit', {
     spec: 'no_pause_spec.js',
-    snapshot: false,
+    snapshot: true,
     headed: false,
     noExit: false,
     expectedExitCode: 0,
