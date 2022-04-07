62f394dddbc5a423a34dd0fc9c680d76f6b4079a
ElevateBart <ledouxb@gmail.com>
2022-04-07 08:56:04 -0500

    refactor: split e2e and component templates

--- packages/data-context/src/codegen/templates.ts
+++ packages/data-context/src/codegen/templates.ts
@@ -4,7 +4,7 @@ import cypressEx from '@packages/example'
 const getPath = (dir: string) => path.join(__dirname, dir)
 
 export default {
-  component: getPath('./templates/empty'),
-  e2e: getPath('./templates/empty'),
+  component: getPath('./templates/empty-component'),
+  e2e: getPath('./templates/empty-e2e'),
   scaffoldIntegration: cypressEx.getPathToE2E(),
 } as const
