602ca2001d38858e5fa06710d80d2b71b6372297
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-04-08 14:15:41 +1000

    add test for Vue CLI v4 and Vue 3

--- npm/webpack-dev-server-fresh/cypress/e2e/nuxt.cy.ts
+++ npm/webpack-dev-server-fresh/cypress/e2e/nuxt.cy.ts
@@ -1,14 +1,12 @@
 // <reference types="cypress" />
 /// <reference path="../support/e2e.ts" />
-import type { e2eProjectDirs } from '@packages/frontend-shared/cypress/e2e/support/e2eProjectDirs'
+import type { ProjectFixtureDir } from '@tooling/system-tests/lib/fixtureDirs'
 
-type ProjectDirs = typeof e2eProjectDirs
-
-const PROJECTS: ProjectDirs[number][] = ['nuxtjs2']
+const PROJECTS: ProjectFixtureDir[] = ['nuxtjs2']
 
 // Add to this list to focus on a particular permutation
 // TODO: run vuecli4-vue2 tests once cypress/vue-2 is bundled
-const ONLY_PROJECTS: ProjectDirs[number][] = []
+const ONLY_PROJECTS: ProjectFixtureDir[] = []
 
 for (const project of PROJECTS) {
   if (ONLY_PROJECTS.length && !ONLY_PROJECTS.includes(project)) {
--- npm/webpack-dev-server-fresh/cypress/e2e/vue-cli.cy.ts
+++ npm/webpack-dev-server-fresh/cypress/e2e/vue-cli.cy.ts
@@ -1,14 +1,12 @@
 // <reference types="cypress" />
 /// <reference path="../support/e2e.ts" />
-import type { e2eProjectDirs } from '@packages/frontend-shared/cypress/e2e/support/e2eProjectDirs'
+import type { ProjectFixtureDir } from '@tooling/system-tests/lib/fixtureDirs'
 
-type ProjectDirs = typeof e2eProjectDirs
-
-const PROJECTS: ProjectDirs[number][] = ['vuecli4-vue2', 'vuecli5-vue3']
+const PROJECTS: ProjectFixtureDir[] = ['vuecli4-vue2', 'vuecli4-vue3', 'vuecli5-vue3']
 
 // Add to this list to focus on a particular permutation
 // TODO: run vuecli4-vue2 tests once cypress/vue-2 is bundled
-const ONLY_PROJECTS: ProjectDirs[number][] = ['vuecli5-vue3']
+const ONLY_PROJECTS: ProjectFixtureDir[] = ['vuecli4-vue3', 'vuecli5-vue3']
 
 for (const project of PROJECTS) {
   if (ONLY_PROJECTS.length && !ONLY_PROJECTS.includes(project)) {
