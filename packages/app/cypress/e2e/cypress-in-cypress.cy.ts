e04639fa56e1286123ba2d8e9dc7c408a0fb02f3
Mark Noonan <mark@cypress.io>
2022-04-11 10:07:06 -0400

    add tests for dragging and resizing

--- packages/app/cypress/e2e/cypress-in-cypress.cy.ts
+++ packages/app/cypress/e2e/cypress-in-cypress.cy.ts
@@ -1,14 +1,26 @@
+import type { DraggablePanel } from '../../src/runner/useRunnerStyle'
+
+const testingTypes = ['component', 'e2e'] as const
+
+const dragHandleToClientX = (panel: DraggablePanel, x: number) => {
+  cy.get(`[data-cy="${panel}ResizeHandle"]`).trigger('mousedown', { eventConstructor: 'MouseEvent' })
+  .trigger('mousemove', { clientX: x })
+  .trigger('mouseup', { eventConstructor: 'MouseEvent' })
+}
+
+function startAtSpecsPage (testingType: typeof testingTypes[number]) {
+  cy.scaffoldProject('cypress-in-cypress')
+  cy.findBrowsers()
+  cy.openProject('cypress-in-cypress')
+  cy.startAppServer(testingType)
+  cy.visitApp()
+}
+
 // For Cypress-in-Cypress tests that do not vary based on testing type
 describe('Cypress in Cypress', { viewportWidth: 1500, defaultCommandTimeout: 10000 }, () => {
-  const testingTypes = ['component', 'e2e'] as const
-
   testingTypes.forEach((testingType) => {
     it(`handles automation disconnects in ${testingType}`, () => {
-      cy.scaffoldProject('cypress-in-cypress')
-      cy.findBrowsers()
-      cy.openProject('cypress-in-cypress')
-      cy.startAppServer(testingType)
-      cy.visitApp()
+      startAtSpecsPage(testingType)
 
       cy.get('[data-cy="spec-item"]').first().click()
       // Let runner stabilize
@@ -36,11 +48,7 @@ describe('Cypress in Cypress', { viewportWidth: 1500, defaultCommandTimeout: 100
     it(`handles automation missing in ${testingType}`, () => {
       let connectedCallback: any
 
-      cy.scaffoldProject('cypress-in-cypress')
-      cy.findBrowsers()
-      cy.openProject('cypress-in-cypress')
-      cy.startAppServer(testingType)
-      cy.visitApp()
+      startAtSpecsPage(testingType)
 
       cy.window().then((win) => {
         if (!win.ws) {
@@ -83,6 +91,42 @@ describe('Cypress in Cypress', { viewportWidth: 1500, defaultCommandTimeout: 100
         expect(ctx.coreData.activeBrowser?.displayName).eq('Electron')
         expect(ctx.actions.project.launchProject).to.have.been.called
       })
+    })
+
+    it(`scales the AUT correctly in ${testingType}`, () => {
+      startAtSpecsPage(testingType)
+      cy.get('[data-cy="spec-item"]').first().click()
+      // Let runner stabilize
+      cy.get('#unified-reporter').should('be.visible')
+      const dragPositions = [1000, 1090, 900, 600]
+      const testingTypeExpectedScales = {
+        component: ['93%', '75%'],
+        e2e: ['46%', '37%', '56%', '85%'],
+        componentShortViewport: '61%',
+        e2eShortViewport: '46%',
+      }
+
+      dragPositions.forEach((position, index) => {
+        dragHandleToClientX('panel2', position)
+        const expectedScale = testingTypeExpectedScales[testingType][index]
+
+        if (expectedScale) {
+          cy.contains(expectedScale).should('be.visible')
+        } else {
+          cy.contains('%)').should('not.exist')
+        }
+
+        cy.percySnapshot(`panel 2 at ${ position } px`)
+      })
+
+      cy.viewport(1500, 1300)
+      cy.contains('[aria-controls=reporter-inline-specs-list]', 'Specs').click()
+      cy.contains('%)').should('not.exist')
+      cy.percySnapshot('tall viewport')
+
+      cy.viewport(1500, 400)
+      cy.contains(testingTypeExpectedScales[`${ testingType }ShortViewport`]).should('exist')
+      cy.percySnapshot('short viewport')
     })
   })
 })
