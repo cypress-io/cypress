88a3a3b1dbb0d0f3cd5d1282d922aafc3318a061
Jessica Sachs <jess@jessicasachs.io>
2022-04-11 19:47:27 -0400

    use unreleased versions of vue

--- system-tests/test/component_testing_index_html_spec.ts
+++ system-tests/test/component_testing_index_html_spec.ts
@@ -19,7 +19,7 @@ describe('component testing projects', function () {
     expectedExitCode: 0,
   })
 
-  systemTests.it('vue3-vite-ts-custom-index-html', {
+  systemTests.it.only('vue3-vite-ts-custom-index-html', {
     project: 'vue3-vite-ts-custom-index-html',
     testingType: 'component',
     spec: 'src/components/HelloWorld.cy.ts',
