f54a94958df01202e8a1f39f82a87be29cbc05d7
Tyler Biethman <tbiethman@gmail.com>
2022-04-11 17:37:27 -0500

    Making the coordinate checks safer for firefox

--- packages/driver/cypress/integration/commands/actions/type_spec.js
+++ packages/driver/cypress/integration/commands/actions/type_spec.js
@@ -351,14 +351,13 @@ describe('src/cy/commands/actions/type - #type', () => {
     })
 
     it('passes options.animationDistanceThreshold to cy.ensureElementIsNotAnimating', () => {
-      const $txt = cy.$$(':text:first')
-
-      const { fromElWindow } = Cypress.dom.getElementCoordinatesByPosition($txt)
-
       cy.spy(cy, 'ensureElementIsNotAnimating')
 
       cy.get(':text:first').type('foo', { animationDistanceThreshold: 1000 }).then(() => {
         const { args } = cy.ensureElementIsNotAnimating.firstCall
+
+        const $txt = cy.$$(':text:first')
+        const { fromElWindow } = Cypress.dom.getElementCoordinatesByPosition($txt)
 
         expect(args[1]).to.deep.eq([fromElWindow, fromElWindow])
 
@@ -369,14 +368,13 @@ describe('src/cy/commands/actions/type - #type', () => {
     it('passes config.animationDistanceThreshold to cy.ensureElementIsNotAnimating', () => {
       const animationDistanceThreshold = Cypress.config('animationDistanceThreshold')
 
-      const $txt = cy.$$(':text:first')
-
-      const { fromElWindow } = Cypress.dom.getElementCoordinatesByPosition($txt)
-
       cy.spy(cy, 'ensureElementIsNotAnimating')
 
       cy.get(':text:first').type('foo').then(() => {
         const { args } = cy.ensureElementIsNotAnimating.firstCall
+
+        const $txt = cy.$$(':text:first')
+        const { fromElWindow } = Cypress.dom.getElementCoordinatesByPosition($txt)
 
         expect(args[1]).to.deep.eq([fromElWindow, fromElWindow])
 
