dcd9bff0135f81f9917b79fd359db40bd4cc07ff
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-04-12 22:43:40 +1000

    update timeouts

--- packages/app/cypress/tasks/git.ts
+++ packages/app/cypress/tasks/git.ts
@@ -2,7 +2,31 @@ import simpleGit from 'simple-git'
 import path from 'path'
 import fs from 'fs-extra'
 
+async function teardownGit (projectPath: string) {
+  const gitDir = path.join(projectPath, '.git')
+
+  try {
+    if (await fs.stat(gitDir)) {
+      return fs.remove(gitDir)
+    }
+  } catch (e) {
+    // git not initialized - no need to do anything
+  }
+}
+
 export async function initGitRepoForTestProject (projectPath: string) {
+  /**
+    * There's a potential race condition the task executes
+  * before the domain is updated in cypress-in-cypress e2e tests,
+  * and we end up in an incorrect state where the test project
+  * is not in a pristine, deterministic state.
+    *
+    * For this reason we check if the git repo
+  * exists, and if it does, remove it and re-create it,
+    * to ensure the test runs on a pristine project.
+    */
+  await teardownGit(projectPath)
+
   const git = simpleGit({ baseDir: projectPath })
 
   if (process.env.CI) {
@@ -14,6 +38,25 @@ export async function initGitRepoForTestProject (projectPath: string) {
   }
 
   const e2eFolder = path.join(projectPath, 'cypress', 'e2e')
+  const fooSpec = path.join(e2eFolder, 'foo.spec.js')
+
+  // Ensure that foo.spec.js file does not exist.
+  // As described above, it's possible that during e2e testing:
+  // 1. execute this task
+  // 2. create foo.spec.js
+  // 3. change domain (for e2e testing)
+  // 4. re-execute this task (foo.spec.js now exists)
+  // 5. foo.spec.js is incorrectly added to the git repo
+  // foo.spec.js is supposed to represent an untracked file,
+  // so we ensure it does not exist before initializing the
+  // git repo for the test project.
+  try {
+    await fs.stat(fooSpec)
+    await fs.remove(fooSpec)
+  } catch (e) {
+    // it doesn't exist, no problem
+  }
+
   const allSpecs = fs.readdirSync(e2eFolder)
 
   await git.init()
@@ -22,7 +65,6 @@ export async function initGitRepoForTestProject (projectPath: string) {
 
   // create a file and modify a file to express all
   // git states we are interested in (created, unmodified, modified)
-  const fooSpec = path.join(e2eFolder, 'foo.spec.js')
   const blankContentsSpec = path.join(e2eFolder, 'blank-contents.spec.js')
 
   fs.createFileSync(fooSpec)
