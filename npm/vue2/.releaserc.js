b169c9e05b39c9277d31e81c8cabb294b5ee33ea
Tim Griesser <tgriesser10@gmail.com>
2022-04-13 09:57:33 -0400

    cleanup unused code

--- npm/vue2/.releaserc.js
+++ npm/vue2/.releaserc.js
@@ -3,7 +3,5 @@ module.exports = {
   branches: [
     // this one releases v3 on master on the latest channel
     'master',
-    // this line forces releasing 2.X releases on the v2 channel
-    { name: 'npm/vue/v2', range: '2.X.X', channel: 'v2' },
   ],
 }
