9afcf26bf5b57e7fd254485178f821bd5360f6aa
Jessica Sachs <jess@jessicasachs.io>
2022-04-13 20:08:45 -0400

    removing more webpack-dev-server refrences

--- system-tests/projects/config-with-import-error/cypress.config.js
+++ system-tests/projects/config-with-import-error/cypress.config.js
@@ -1,12 +1,11 @@
-const { devServer } = require('@cypress/webpack-dev-server')
 // NOTE: ensure you are requiring your webpack config from the
 // correct location.
 const webpackConfig = require('./webpack.config.js')
 
 module.exports = {
   component: {
-    devServer,
-    devServerConfig: {
+    devServer: {
+      bundler: 'webpack',
       webpackConfig,
     },
   },
