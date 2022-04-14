330d0386cc7f4c64f694465a0fd0e4f8cf6d9e97
David Munechika <david.munechika@gatech.edu>
2022-04-14 12:36:28 -0400

    fix launchpad test

--- packages/launchpad/cypress/e2e/project-setup.cy.ts
+++ packages/launchpad/cypress/e2e/project-setup.cy.ts
@@ -602,30 +602,24 @@ describe('Launchpad: Setup Project', () => {
         cy.contains('Project Setup')
       })
 
-      it('can move forward to choose browser if e2e is configured and is selected from the dropdown list', () => {
+      it('can move forward to choose browser if e2e is configured', () => {
         cy.openProject('pristine-with-e2e-testing')
         cy.visitLaunchpad()
 
         verifyWelcomePage({ e2eIsConfigured: true, ctIsConfigured: false })
 
-        cy.get('[data-cy-testingtype="e2e"]').within(() => {
-          cy.get('[data-cy=status-badge-menu]').click()
-          cy.get('[data-cy="Choose a Browser"]').click()
-        })
+        cy.get('[data-cy-testingtype="e2e"]').click()
 
         verifyChooseABrowserPage()
       })
 
-      it('can move forward to choose browser if component is configured and is selected from the dropdown list', () => {
+      it('can move forward to choose browser if component is configured', () => {
         cy.openProject('pristine-with-ct-testing')
         cy.visitLaunchpad()
 
         verifyWelcomePage({ e2eIsConfigured: false, ctIsConfigured: true })
 
-        cy.get('[data-cy-testingtype="component"]').within(() => {
-          cy.get('[data-cy=status-badge-menu]').click()
-          cy.get('[data-cy="Choose a Browser"]').click()
-        })
+        cy.get('[data-cy-testingtype="component"]').click()
 
         verifyChooseABrowserPage()
       })
