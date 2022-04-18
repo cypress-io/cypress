cae0345284b69a437fecc260435fa95b82b1f87e
Zachary Williams <zachjw34@gmail.com>
2022-04-18 10:27:02 -0500

    remove unused components and styles

--- packages/frontend-shared/src/gql-components/StatusBadge.vue
+++ packages/frontend-shared/src/gql-components/StatusBadge.vue
@@ -1,18 +1,10 @@
 <template>
   <template v-if="props.status">
     <div class="border rounded-full font-medium p-5px pr-16px text-size-14px text-jade-500 leading-20px inline-flex items-center relative">
-      <Menu>
-        <MenuButton
-          data-cy="status-badge-menu"
-          class="flex items-center justify-center focus:outline-transparent"
-          @click.stop
-        >
-          <i-cy-grommet_x16
-            class="h-16px mr-4px ml-4px w-16px icon-light-jade-400 icon-dark-jade-400"
-          />
-          {{ props.titleOn }}
-        </MenuButton>
-      </Menu>
+      <i-cy-grommet_x16
+        class="h-16px mr-4px ml-4px w-16px icon-light-jade-400 icon-dark-jade-400"
+      />
+      {{ props.titleOn }}
     </div>
   </template>
   <template v-else>
@@ -29,7 +21,6 @@
 
 <script lang="ts" setup>
 
-import { Menu, MenuButton } from '@headlessui/vue'
 import type { TestingTypeEnum } from '../generated/graphql'
 
 const props = defineProps<{
