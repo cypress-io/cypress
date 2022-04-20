d2cd14be714a28d97122bab539126491d826cbfa
Rachel <Raquel.rudermano@gmail.com>
2022-04-20 12:05:45 -0700

    add scaffoldedFileOrder.ts

---
+++ packages/launchpad/src/utils/scaffoldedFileOrder.ts
@@ -0,0 +1,9 @@
+export const scaffoldedFileOrder: Array<string> = [
+  'cypress.config.',
+  'support/component.',
+  'support/e2e.',
+  'support/commands.',
+  'support/component-index.',
+  'fixtures/example.',
+  // for rest of files, order not important in UI
+]
