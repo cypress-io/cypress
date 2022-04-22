4d97836ca3eeb51ffc33ff8763d06502d8d89e34
Tyler Biethman <tbiethman@gmail.com>
2022-04-22 15:11:28 -0500

    Updating state cache key names to limit improper fallback matches. Sorting output for cache key for cross-arch determinism.

--- system-tests/scripts/cache-key.sh
+++ system-tests/scripts/cache-key.sh
@@ -5,4 +5,12 @@
 # cd to this "scripts" directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-cat ../projects/**/{package.json,yarn.lock}
+# Sort glob output, as it can vary based on architecture. LC_ALL=C required for locale-agnostic sort.
+file_list=$(ls ../projects/**/{package.json,yarn.lock} | LC_ALL=C sort -f)
+ 
+contents=''
+for t in ${file_list[@]}; do
+  contents+=$(<$t)
+done
+ 
+echo $contents
