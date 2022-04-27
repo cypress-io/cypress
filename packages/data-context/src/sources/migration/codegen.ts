a04c1f163c8d863605b0fb89f6d5a0f7af0b0710
estrada9166 <estrada9166@hotmail.com>
2022-04-27 10:24:28 -0500

    fix: use prettier to format migrated config

--- packages/data-context/src/sources/migration/codegen.ts
+++ packages/data-context/src/sources/migration/codegen.ts
@@ -3,6 +3,7 @@ import fs from 'fs-extra'
 import path from 'path'
 import globby from 'globby'
 import type { TestingType } from '@packages/types'
+import prettier from 'prettier'
 import { formatMigrationFile } from './format'
 import { substitute } from './autoRename'
 import { supportFileRegexps } from './regexps'
@@ -172,7 +173,6 @@ function createCypressConfig (config: ConfigOptions, pluginPath: string | undefi
         `import { defineConfig } from 'cypress'
   
         export default defineConfig({${globalString}${e2eString}${componentString}})`,
-        options.projectRoot,
       )
     }
 
@@ -180,15 +180,14 @@ function createCypressConfig (config: ConfigOptions, pluginPath: string | undefi
       `const { defineConfig } = require('cypress')
 
       module.exports = defineConfig({${globalString}${e2eString}${componentString}})`,
-      options.projectRoot,
     )
   }
 
   if (options.hasTypescript) {
-    return formatConfig(`export default {${globalString}${e2eString}${componentString}}`, options.projectRoot)
+    return formatConfig(`export default {${globalString}${e2eString}${componentString}}`)
   }
 
-  return formatConfig(`module.exports = {${globalString}${e2eString}${componentString}}`, options.projectRoot)
+  return formatConfig(`module.exports = {${globalString}${e2eString}${componentString}}`)
 }
 
 function formatObjectForConfig (obj: Record<string, unknown>) {
@@ -449,11 +448,8 @@ export function getSpecPattern (cfg: LegacyCypressConfigJson, testType: TestingT
   return specPattern
 }
 
-export function formatConfig (config: string, projectRoot: string): string {
+export function formatConfig (config: string): string {
   try {
-    const prettierPath = require.resolve('prettier', { paths: [projectRoot] })
-    const prettier = require(prettierPath)
-
     return prettier.format(config, {
       semi: false,
       singleQuote: true,
