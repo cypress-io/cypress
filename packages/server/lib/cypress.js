244c1cc5c08112bda6bd60d0cef096b26a8595b6
Zach Bloomquist <git@chary.us>
2022-04-27 19:08:17 -0400

    Revert "fix "not running in an electron environment""
    This reverts commit 573966f58d08bfdeea9d459f32572ec68b4911e6.

--- packages/server/lib/cypress.js
+++ packages/server/lib/cypress.js
@@ -20,6 +20,7 @@ require('@packages/server/lib/project-base')
 
 const Promise = require('bluebird')
 const debug = require('debug')('cypress:server:cypress')
+const { getPublicConfigKeys } = require('@packages/config')
 const argsUtils = require('./util/args')
 
 const warning = (code, args) => {
@@ -36,8 +37,6 @@ const exit = (code = 0) => {
 }
 
 const showWarningForInvalidConfig = (options) => {
-  const { getPublicConfigKeys } = require('@packages/config')
-
   const publicConfigKeys = getPublicConfigKeys()
   const invalidConfigOptions = require('lodash').keys(options.config).reduce((invalid, option) => {
     if (!publicConfigKeys.find((configKey) => configKey === option)) {
@@ -257,6 +256,9 @@ module.exports = {
 
       case 'interactive':
         return this.runElectron(mode, options)
+
+      case 'openProject':
+        throw new Error('Unused')
 
       default:
         throw new Error(`Cannot start. Invalid mode: '${mode}'`)
