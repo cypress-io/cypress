717e7e527379b0bb5cc196fd7b2f1e2ce43bdfa4
David Munechika <david.munechika@gatech.edu>
2022-04-27 10:58:49 -0400

    fix: expand code by default

--- packages/app/src/specs/generators/GeneratorSuccess.vue
+++ packages/app/src/specs/generators/GeneratorSuccess.vue
@@ -1,23 +1,37 @@
 <template>
-  <div
-    class="flex py-16px px-24px gap-8px items-center"
-    data-cy="file-row"
+  <Collapsible
+    class="rounded outline-none m-4px overflow-hidden"
+    :initially-open="true"
   >
-    <i-cy-status-passed-solid_x16 />
-    <span class="font-medium text-jade-500 truncate">{{ file.relative }}</span>
-  </div>
-  <div class="rounded border-1 mx-24px mb-24px">
-    <ShikiHighlight
-      :code="file.contents"
-      line-numbers
-      lang="js"
-      copy-button
-    />
-  </div>
+    <template #target="{open}">
+      <div
+        class="cursor-pointer flex py-16px px-24px gap-8px items-center"
+        data-cy="file-row"
+      >
+        <i-cy-status-passed-solid_x16 />
+        <span class="font-medium text-jade-500 truncate">{{ file.relative }}</span>
+        <div class="flex-grow flex justify-self-end justify-end">
+          <i-cy-chevron-down-small_x16
+            :class="{ 'rotate-180': open }"
+            class="max-w-16px transform transition duration-150 icon-dark-gray-400"
+          />
+        </div>
+      </div>
+    </template>
+    <div class="rounded border-1 mx-24px mb-24px">
+      <ShikiHighlight
+        :code="file.contents"
+        line-numbers
+        lang="js"
+        copy-button
+      />
+    </div>
+  </Collapsible>
 </template>
 
 <script lang="ts" setup>
 import ShikiHighlight from '@cy/components/ShikiHighlight.vue'
+import Collapsible from '@cy/components/Collapsible.vue'
 import { gql } from '@urql/core'
 import type { GeneratorSuccessFileFragment } from '../../generated/graphql'
 
