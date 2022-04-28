7daf8d054e316005e996ae523866355c6ffcd3e3
David Munechika <david.munechika@gatech.edu>
2022-04-28 14:04:03 -0400

    add tests

--- packages/app/cypress/e2e/specs_list_e2e.cy.ts
+++ packages/app/cypress/e2e/specs_list_e2e.cy.ts
@@ -112,4 +112,153 @@ describe('App: Spec List (E2E)', () => {
       })
     })
   })
+
+  describe('typing the filter', function () {
+    const runAllIntegrationSpecsLabel = 'Run 8 integration specs'
+
+    beforeEach(function () {
+      this.specs.integration.push({
+        name: 'a-b_c/d~e(f)g.spec.js',
+        absolute: '/user/project/cypress/integration/a-b_c/d~e(f)g.spec.js',
+        relative: 'cypress/integration/a-b_c/d~e(f)g.spec.js',
+      })
+
+      this.numSpecs = 16
+
+      this.ipc.getSpecs.yields(null, this.specs)
+      this.openProject.resolve(this.config)
+
+      cy.contains('.all-tests', runAllIntegrationSpecsLabel)
+      cy.get('.filter').type('new')
+    })
+
+    it('displays only matching spec', function () {
+      cy.get('.specs-list .file')
+      .should('have.length', 1)
+      .and('contain', 'account_new_spec.coffee')
+
+      cy.contains('.all-tests', 'Run 1 integration spec').click()
+      .find('.fa-dot-circle')
+      .then(() => {
+        expect(this.ipc.launchBrowser).to.have.property('called').equal(true)
+        const launchArgs = this.ipc.launchBrowser.lastCall.args
+
+        expect(launchArgs[0].specFilter, 'spec filter').to.eq('new')
+      })
+    })
+
+    it('only shows matching folders', () => {
+      cy.get('.specs-list .folder')
+      .should('have.length', 2)
+    })
+
+    it('ignores non-letter characters', function () {
+      cy.get('.filter').clear().type('appspec')
+
+      cy.get('.specs-list .file')
+      .should('have.length', 1)
+      .and('contain', 'app_spec.coffee')
+    })
+
+    it('ignores non-number characters', function () {
+      cy.get('.filter').clear().type('123spec')
+
+      cy.get('.specs-list .file')
+      .should('have.length', 1)
+      .and('contain', '123_spec.coffee')
+    })
+
+    it('ignores commonly used path characters', function () {
+      cy.get('.filter').clear().type('abcdefg')
+
+      cy.get('.specs-list .file')
+      .should('have.length', 1)
+      .and('contain', 'd~e(f)g.spec.js')
+    })
+
+    it('treats non-Latin characters as letters', function () {
+      cy.get('.filter').clear().type('日本語')
+
+      cy.get('.specs-list .file')
+      .should('have.length', 1)
+      .and('contain', '日本語_spec.coffee')
+    })
+
+    it('clears the filter on clear button click', function () {
+      cy.get('.clear-filter').click()
+      cy.get('.filter')
+      .should('have.value', '')
+
+      cy.get('.specs-list .file')
+      .should('have.length', this.numSpecs)
+
+      cy.contains('.all-tests', runAllIntegrationSpecsLabel)
+    })
+
+    it('clears the filter if the user press ESC key', function () {
+      cy.get('.filter').type('{esc}')
+      .should('have.value', '')
+
+      cy.get('.specs-list .file')
+      .should('have.length', this.numSpecs)
+
+      cy.contains('.all-tests', runAllIntegrationSpecsLabel)
+      .find('.fa-play')
+    })
+
+    it('shows empty message if no results', function () {
+      cy.get('.filter').clear().type('foobarbaz')
+      cy.get('.specs-list').should('not.exist')
+
+      cy.get('.empty-well').should('contain', 'No specs match your search: "foobarbaz"')
+      cy.percySnapshot()
+    })
+
+    it('removes run all tests buttons if no results', function () {
+      cy.get('.filter').clear().type('foobarbaz')
+
+      // the "Run ... tests" buttons should be gone
+      cy.get('.all-tests').should('not.exist')
+    })
+
+    it('clears and focuses the filter field when clear search is clicked', function () {
+      cy.get('.filter').clear().type('foobarbaz')
+      cy.get('.btn').contains('Clear search').click()
+      cy.focused().should('have.id', 'filter')
+
+      cy.get('.specs-list .file')
+      .should('have.length', this.numSpecs)
+    })
+
+    it('saves the filter to local storage for the project', function () {
+      cy.window().then((win) => {
+        expect(win.localStorage[`specsFilter-${this.config.projectId}-/foo/bar`]).to.be.a('string')
+
+        expect(JSON.parse(win.localStorage[`specsFilter-${this.config.projectId}-/foo/bar`])).to.equal('new')
+      })
+    })
+
+    it('does not update run button label while running', function () {
+      cy.contains('.all-tests', 'Run 1 integration spec').click()
+      // mock opened browser and running tests
+      // to force "Stop" button to show up
+      cy.window().its('__project').then((project) => {
+        project.browserOpened()
+      })
+
+      // the button has its its label reflect the running specs
+      cy.contains('.all-tests', 'Running integration tests')
+      .should('have.class', 'active')
+
+      // the button has its label unchanged while the specs are running
+      cy.get('.filter').clear()
+      cy.contains('.all-tests', 'Running integration tests')
+      .should('have.class', 'active')
+
+      // but once the project stops running tests, the button gets updated
+      cy.get('.close-browser').click()
+      cy.contains('.all-tests', runAllIntegrationSpecsLabel)
+      .should('not.have.class', 'active')
+    })
+  })
 })
