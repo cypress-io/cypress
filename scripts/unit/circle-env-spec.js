fe3e2afbea154117657c154d88dc1e7e107cc437
Zach Bloomquist <git@chary.us>
2022-04-28 21:44:15 -0400

    chore: add env var checking to verify-mocha-results (#21211)
    Co-authored-by: Ryan Manuel <ryanm@cypress.io>

---
+++ scripts/unit/circle-env-spec.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises
+const sinon = require('sinon')
+const { expect } = require('chai')
+const { _checkCanaries } = require('../circle-env')
+
+describe('circle-env', () => {
+  let cachedEnv = { ...process.env }
+
+  afterEach(() => {
+    sinon.restore()
+    Object.assign(process.env, cachedEnv)
+  })
+
+  beforeEach(() => {
+    process.env.CI = 'true'
+    process.env.CIRCLE_INTERNAL_CONFIG = '/foo.json'
+  })
+
+  it('fails with missing canaries', async () => {
+    sinon.stub(fs, 'readFile')
+    .withArgs('/foo.json').resolves(JSON.stringify({
+      Dispatched: { TaskInfo: { Environment: { somekey: 'someval' } } },
+    }))
+
+    try {
+      await _checkCanaries()
+      throw new Error('should not reach')
+    } catch (err) {
+      expect(err.message).to.include('Missing MAIN_CANARY')
+    }
+  })
+
+  it('passes with canaries', async () => {
+    sinon.stub(fs, 'readFile')
+    .withArgs('/foo.json').resolves(JSON.stringify({
+      Dispatched: { TaskInfo: { Environment: { MAIN_CANARY: 'true', CONTEXT_CANARY: 'true' } } },
+    }))
+
+    await _checkCanaries()
+  })
+})
