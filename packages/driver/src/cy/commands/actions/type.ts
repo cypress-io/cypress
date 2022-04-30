ed40e89196cb99d0c993778e8882b67c29cd38d4
Sam Kvale <sam.kvale@sap.com>
2022-04-30 10:23:23 -0500

    feat(type): Only wrap in Jquery where needed

--- packages/driver/src/cy/commands/actions/type.ts
+++ packages/driver/src/cy/commands/actions/type.ts
@@ -176,12 +176,12 @@ export default function (Commands, Cypress, cy, state, config) {
       const formId = form.attr('id')
       const nestedButtons = form.find('input, button')
 
-      const possibleDefaultButtons = formId ? $dom.wrap(_.uniq([
+      const possibleDefaultButtons: JQuery<any> = formId ? $dom.wrap(_.uniq([
         ...nestedButtons,
         ...cy.$$('body').find(`input[form='${formId}'], button[form='${formId}']`),
       ])) : nestedButtons
 
-      return $dom.wrap(possibleDefaultButtons).filter((__, el) => {
+      return possibleDefaultButtons.filter((__, el) => {
         const $el = $dom.wrap(el)
 
         return (
