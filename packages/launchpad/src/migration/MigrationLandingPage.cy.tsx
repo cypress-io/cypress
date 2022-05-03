360ea52408444c43fd39ee5f446787105a4eb7e8
Mark Noonan <mark@cypress.io>
2022-05-03 17:32:32 -0400

    add basic component and test

---
+++ packages/launchpad/src/migration/MigrationLandingPage.cy.tsx
@@ -0,0 +1,14 @@
+import { defaultMessages } from '@cy/i18n'
+import MigrationLandingPage from './MigrationLandingPage.vue'
+
+const text = defaultMessages.migration.landingPage
+
+describe('<MigrationLandingPage />', () => {
+  it('renders the content', () => {
+    cy.mount(MigrationLandingPage)
+    cy.contains('h1', text.title).should('be.visible')
+    cy.contains('p', text.description).should('be.visible')
+    cy.contains('button', text.actionContinue).should('be.visible')
+    cy.contains('a', text.linkReleaseNotes).should('be.visible')
+  })
+})
---
+++ packages/launchpad/src/migration/MigrationLandingPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ t('migration.landingPage.title') }}</h1>
+  <p>{{ t('migration.landingPage.description') }}</p>
+
+  <div class="text-center">
+    <div>
+      video
+    </div>
+    <div class="flex gap-8px">
+      <Button @click="handleClick">
+        {{ t('migration.landingPage.actionContinue') }}
+      </Button>
+      <Button
+        href="https://on.cypress.io/changelog"
+        variant="outline"
+      >
+        {{ t('migration.landingPage.linkReleaseNotes') }}
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Button from '@cy/components/Button.vue'
+
+import { useI18n } from '@cy/i18n'
+const { t } = useI18n()
+
+const handleClick = () => {
+  // emit event dismiss
+}
+
+</script>
