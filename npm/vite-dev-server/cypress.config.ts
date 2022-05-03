673a2389e887f6e76ccb35a0f5c52f6d27ac5660
Bill Glesias <bglesias@gmail.com>
2022-05-02 22:54:53 -0400

    update npm vite-dev-server config and frontend-shared cypress fixtures to use experimentalSessionAndOrigin flag

--- npm/vite-dev-server/cypress.config.ts
+++ npm/vite-dev-server/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   projectId: 'ypt4pf',
   e2e: {
-    experimentalSessionSupport: true,
+    experimentalSessionAndOrigin: true,
     defaultCommandTimeout: 10000, // these take a bit longer b/c they're e2e open mode test
     async setupNodeEvents (on, config) {
       if (!process.env.HTTP_PROXY_TARGET_FOR_ORIGIN_REQUESTS) {
