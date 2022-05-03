68ae38cabc9ce105b3d6299b5dc22922941caea1
Zach Bloomquist <git@chary.us>
2022-05-03 15:28:08 -0400

    try using getConfig

--- packages/data-context/src/data/ProjectLifecycleManager.ts
+++ packages/data-context/src/data/ProjectLifecycleManager.ts
@@ -103,7 +103,7 @@ export class ProjectLifecycleManager {
 
   async getProjectId (): Promise<string | null> {
     try {
-      const contents = await this.getConfigFileContents()
+      const contents = await this.ctx.project.getConfig()
 
       return contents.projectId ?? null
     } catch {
