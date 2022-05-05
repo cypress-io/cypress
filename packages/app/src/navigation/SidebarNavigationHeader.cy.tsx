c1de9545b4b6a330207e9894b9d035c13e10647d
estrada9166 <estrada9166@hotmail.com>
2022-05-05 14:03:00 -0500

    fix: CT app icon to have correct sizes

---
+++ packages/app/src/navigation/SidebarNavigationHeader.cy.tsx
@@ -0,0 +1,31 @@
+import SidebarNavigationHeader from './SidebarNavigationHeader.vue'
+import { SidebarNavigationHeaderFragmentDoc } from '../generated/graphql-test'
+
+describe('SidebarNavigationHeader', () => {
+  it('renders e2e icon', () => {
+    cy.mountFragment(SidebarNavigationHeaderFragmentDoc, {
+      render: (gql) => <div class="p-16px"><SidebarNavigationHeader gql={gql} /></div>,
+    })
+
+    cy.get('[data-cy="testing-type-e2e"]').should('exist')
+    cy.contains('test-project')
+    cy.percySnapshot()
+  })
+
+  it('renders component icon', () => {
+    cy.mountFragment(SidebarNavigationHeaderFragmentDoc, {
+      onResult: (res) => {
+        if (!res || !res.currentProject) {
+          return
+        }
+
+        res.currentProject.currentTestingType = 'component'
+      },
+      render: (gql) => <div class="p-16px"><SidebarNavigationHeader gql={gql} /></div>,
+    })
+
+    cy.get('[data-cy="testing-type-component"]').should('exist')
+    cy.contains('test-project')
+    cy.percySnapshot()
+  })
+})
