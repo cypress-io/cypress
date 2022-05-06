5a34def3046501b94e0dc1f0277049c0226ae0b8
Zach Bloomquist <git@chary.us>
2022-05-06 18:42:39 -0400

    chore: update ensure-dependencies to always fail in CI if integrity check fails

--- scripts/ensure-dependencies.sh
+++ scripts/ensure-dependencies.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-if [ $SKIP_DEPCHECK ]; then exit 0; fi
+if [ $SKIP_DEPCHECK ] && ! [ $CI ]; then exit 0; fi
 
 yarn check --integrity
 
 if [ $? -ne 0 ]; then
-  echo 'Your dependencies are out of date; installing the correct dependencies...'
-  yarn
+  echo 'yarn check --integrity failed.'
+  echo 'This means the package.json and/or yarn.lock does not match the installed packages.'
+
+  if [ $CI ]; then
+    echo 'Since we are in CI, we cannot continue.'
+    echo 'Check if your yarn.lock is up to date, and check why node_modules_install did not fail with --frozen-lockfile.'
+    exit 1
+  fi
+
+  echo 'To skip this check for local development, set SKIP_DEPCHECK=1.'
+  echo 'Installing the correct dependencies with "yarn --frozen-lockfile"...'
+  set +e
+  yarn --frozen-lockfile
 fi
