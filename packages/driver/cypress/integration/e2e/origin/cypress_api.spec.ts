b270b8b6d2e4d1e9c0ffd4fffc0b57cb19246936
Tyler Biethman <tbiethman@gmail.com>
2022-05-09 10:07:16 -0500

    Improving comments around test changes. Reverting changes to image comparison test to get better diff.

--- packages/driver/cypress/integration/e2e/origin/cypress_api.spec.ts
+++ packages/driver/cypress/integration/e2e/origin/cypress_api.spec.ts
@@ -7,9 +7,12 @@ describe('cy.origin Cypress API', () => {
   })
 
   afterEach(() => {
-    // Enqueuing another cy command after each test to ensure stability
-    // for the next test. This can be removed with the completion of:
-    // https://github.com/cypress-io/cypress/issues/21300
+    // FIXME: Tests that end with a cy.origin command and enqueue no further cy
+    // commands may have origin's unload event bleed into subsequent tests
+    // and prevent stability from being reached, causing those tests to hang.
+    // We enqueue another cy command after each test to ensure stability
+    // is reached for the next test. This additional command can be removed with the
+    // completion of: https://github.com/cypress-io/cypress/issues/21300
     cy.then(() => { /* ensuring stability */ })
   })
 
