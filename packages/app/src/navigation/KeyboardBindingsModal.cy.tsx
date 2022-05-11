00aebaf640b2e676b38c8bfaa5dae9c5d84dda99
Rachel <Raquel.rudermano@gmail.com>
2022-05-11 17:57:29 -0300

    remove axe test in ct

--- packages/app/src/navigation/KeyboardBindingsModal.cy.tsx
+++ packages/app/src/navigation/KeyboardBindingsModal.cy.tsx
@@ -13,11 +13,4 @@ describe('KeyboardBindingsModal', () => {
       cy.contains(text).should('be.visible')
     })
   })
-
-  context('accessibility', () => {
-    it('has no axe violations', () => {
-      cy.injectAxe()
-      cy.checkA11y()
-    })
-  })
 })
