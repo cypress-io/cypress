4b24622c5ab437bb0d12685aa0170ddf22b55e89
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-05-13 20:13:40 +1000

    wip - updates

--- system-tests/__snapshots__/cy_origin_retries_spec.ts.js
+++ system-tests/__snapshots__/cy_origin_retries_spec.ts.js
@@ -1,84 +1,20 @@
 exports['e2e cy.origin retries / Appropriately displays test retry errors without other side effects'] = `
+Can't run because you've entered an invalid browser name.
 
-====================================================================================================
+Browser: firefox was not found on your system or is not supported by Cypress.
 
-  (Run Starting)
+Cypress supports the following browsers:
+ - electron
+ - chrome
+ - chromium
+ - chrome:canary
+ - edge
+ - firefox
 
-  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ Cypress:      1.2.3                                                                            │
-  │ Browser:      FooBrowser 88                                                                    │
-  │ Specs:        1 found (cy_origin_retries.cy.ts)                                                │
-  │ Searched:     cypress/e2e/cy_origin_retries.cy.ts                                              │
-  │ Experiments:  experimentalSessionAndOrigin=true                                                │
-  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+You can also use a custom browser: https://on.cypress.io/customize-browsers
 
-
-────────────────────────────────────────────────────────────────────────────────────────────────────
-                                                                                                    
-  Running:  cy_origin_retries.cy.ts                                                         (1 of 1)
-
-
-  cy.origin test retries
-    (Attempt 1 of 3) appropriately retries test within "cy.origin" without propagating other errors errors
-    (Attempt 2 of 3) appropriately retries test within "cy.origin" without propagating other errors errors
-    1) appropriately retries test within "cy.origin" without propagating other errors errors
-
-
-  0 passing
-  1 failing
-
-  1) cy.origin test retries
-       appropriately retries test within "cy.origin" without propagating other errors errors:
-     AssertionError: expected true to be false
-      [stack trace lines]
-
-
-
-
-  (Results)
-
-  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ Tests:        1                                                                                │
-  │ Passing:      0                                                                                │
-  │ Failing:      1                                                                                │
-  │ Pending:      0                                                                                │
-  │ Skipped:      0                                                                                │
-  │ Screenshots:  3                                                                                │
-  │ Video:        true                                                                             │
-  │ Duration:     X seconds                                                                        │
-  │ Spec Ran:     cy_origin_retries.cy.ts                                                          │
-  └────────────────────────────────────────────────────────────────────────────────────────────────┘
-
-
-  (Screenshots)
-
-  -  /XXX/XXX/XXX/cypress/screenshots/cy_origin_retries.cy.ts/cy.origin test retries      (1280x720)
-     -- appropriately retries test within cy.origin without propagating other errors                
-     errors (failed).png                                                                            
-  -  /XXX/XXX/XXX/cypress/screenshots/cy_origin_retries.cy.ts/cy.origin test retries      (1280x720)
-     -- appropriately retries test within cy.origin without propagating other errors                
-     errors (failed) (attempt 2).png                                                                
-  -  /XXX/XXX/XXX/cypress/screenshots/cy_origin_retries.cy.ts/cy.origin test retries      (1280x720)
-     -- appropriately retries test within cy.origin without propagating other errors                
-     errors (failed) (attempt 3).png                                                                
-
-
-  (Video)
-
-  -  Started processing:  Compressing to 32 CRF                                                     
-  -  Finished processing: /XXX/XXX/XXX/cypress/videos/cy_origin_retries.cy.ts.mp4         (X second)
-
-
-====================================================================================================
-
-  (Run Finished)
-
-
-       Spec                                              Tests  Passing  Failing  Pending  Skipped  
-  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ ✖  cy_origin_retries.cy.ts                  XX:XX        1        -        1        -        - │
-  └────────────────────────────────────────────────────────────────────────────────────────────────┘
-    ✖  1 of 1 failed (100%)                     XX:XX        1        -        1        -        -  
-
-
+Available browsers found on your system are:
+- browser1
+- browser2
+- browser3
 `
