a3db56226748e8e867a64197dd8d5bea97aea165
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-05-13 22:28:17 +1000

    update spec

--- system-tests/projects/e2e/cypress.config.js
+++ system-tests/projects/e2e/cypress.config.js
@@ -4,7 +4,7 @@ const plugin = require('./cypress/plugins')
 module.exports = defineConfig({
   retries: null,
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,mjs,ts,tsx}',
     setupNodeEvents (on, config) {
       return plugin(on, config)
     },
