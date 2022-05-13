1e57b122f5d6ca79062f7a8c80f9d1f69054c1aa
Mark Noonan <mark@cypress.io>
2022-05-13 08:37:12 -0400

    update test for flake

--- packages/app/cypress/e2e/sidebar_navigation.cy.ts
+++ packages/app/cypress/e2e/sidebar_navigation.cy.ts
@@ -251,10 +251,6 @@ describe('Sidebar Navigation', () => {
     it('resize nav sends the correct value on the mutation', () => {
       cy.contains('fixture.js').click()
 
-      cy.get('.toggle-specs-text').click({ force: true })
-
-      cy.contains('Expand Specs List')
-
       cy.withCtx((ctx, o) => {
         o.sinon.stub(ctx.actions.localSettings, 'setPreferences').resolves()
       })
