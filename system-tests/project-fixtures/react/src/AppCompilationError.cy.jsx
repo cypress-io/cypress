865d69f9a7a36324214224ab93178d1800747f97
Ryan Manuel <ryanm@cypress.io>
2022-05-19 16:06:37 -0500

    Fix tests

--- system-tests/project-fixtures/react/src/AppCompilationError.cy.jsx
+++ system-tests/project-fixtures/react/src/AppCompilationError.cy.jsx
@@ -3,6 +3,7 @@ import { mount } from 'cypress/react'
 import { App } from './App'
 
 it('renders hello world', () => {
-  mount(<App />adf)
+  mount(<App />)
   cy.get('h1').contains('Hello World')
+}
 })
