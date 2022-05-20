91852e900f583bd0844e8e2bb7c02e1ce2783ad3
Ryan Manuel <ryanm@cypress.io>
2022-05-19 20:28:06 -0500

    Fix tests

--- system-tests/project-fixtures/react/cypress-vite.config.ts
+++ system-tests/project-fixtures/react/cypress-vite.config.ts
@@ -22,5 +22,5 @@ export default defineConfig({
   },
   // These tests should run quickly / fail quickly,
   // since we intentionally causing error states for testing
-  defaultCommandTimeout: 1000
+  defaultCommandTimeout: 20000
 })
