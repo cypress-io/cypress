067df920b1b311cce0233dd1d2c92448b4abf5b4
Muaz Othman <muaz@cypress.io>
2022-05-20 16:15:32 -0500

    Fixed GitDataSource tests

--- packages/app/cypress/e2e/specs_list_actual_git_repo.cy.ts
+++ packages/app/cypress/e2e/specs_list_actual_git_repo.cy.ts
@@ -1,10 +1,13 @@
 describe('Spec List - Git Status', () => {
+  let projectRoot: string
+
   beforeEach(() => {
     cy.scaffoldProject('cypress-in-cypress')
     .then((projectPath) => {
+      projectRoot = projectPath
+      cy.task('initGitRepoForTestProject', projectPath)
       cy.openProject('cypress-in-cypress')
       cy.startAppServer('e2e')
-      cy.task('initGitRepoForTestProject', projectPath)
       cy.visitApp()
     })
   })
@@ -55,29 +58,11 @@ describe('Spec List - Git Status', () => {
     cy.get('[data-cy-row="foo.spec.js"] [data-cy="git-info-row"] svg')
     .should('have.class', 'icon-light-jade-50')
 
-    if (Cypress.platform !== 'win32') {
-      // skip this test in Windows because of possible divergence in git behavior related to file permissions/stats
-      // TLDR: git config value `core.filemode` should be set to false
-      // See https://stackoverflow.com/questions/14564946/git-status-shows-changed-files-but-git-diff-doesnt and
-      // https://github.com/microsoft/WSL/issues/184 for reference
+    cy.task('resetGitRepoForTestProject', projectRoot)
 
-      cy.withCtx((ctx) => {
-        ctx.fs.writeFileSync(
-          ctx.path.join(ctx.currentProject!, 'cypress', 'e2e', 'dom-container.spec.js'),
-`describe('Dom Content', () => {
-  it('renders a container', () => {
-    cy.get('.container')
-  })
-})
-`,
-'utf-8',
-        )
-      })
+    cy.wait(500)
 
-      cy.wait(500)
-
-      cy.get('[data-cy-row="dom-container.spec.js"] [data-cy="git-info-row"] svg')
-      .should('have.class', 'icon-light-gray-500')
-    }
+    cy.get('[data-cy-row="dom-container.spec.js"] [data-cy="git-info-row"] svg')
+    .should('have.class', 'icon-light-gray-500')
   })
 })
