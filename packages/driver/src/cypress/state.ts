7e17f9cc7c3d9fdbd9113cb42b25f8733f410f3b
KHeo <sainthkh@naver.com>
2022-05-23 16:35:01 +0900

    xhr.ts

--- packages/driver/src/cypress/state.ts
+++ packages/driver/src/cypress/state.ts
@@ -3,9 +3,10 @@
 
 import type { RouteMap } from '../cy/net-stubbing/types'
 import type { $Command } from './command'
-import type { XHRResponse } from '../cy/commands/xhr'
+import type { XHRRequest, XHRResponse } from '../cy/commands/xhr'
 import type { KeyboardModifiers } from '../cy/keyboard'
 import type { MouseCoords } from '../cy/mouse'
+import type { Server } from './server'
 
 export interface StateFunc {
   (): Record<string, any>
@@ -40,6 +41,8 @@ export interface StateFunc {
   (k: 'keyboardModifiers', v?: KeyboardModifiers): KeyboardModifiers
   (k: 'mouseLastHoveredEl', v?: HTMLElement | null): HTMLElement | null
   (k: 'mouseCoords', v?: MouseCoords): MouseCoords
+  (k: 'server', v?: Server): Server
+  (k: 'requests', v?: XHRRequest[]): XHRRequest[]
   (k: string, v?: any): any
   state: StateFunc
   reset: () => Record<string, any>
