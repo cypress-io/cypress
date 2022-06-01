2e5b5d6c9bda0efd3245056f9746e920e00a5b93
estrada9166 <estrada9166@hotmail.com>
2022-06-01 16:21:35 -0500

    Update regexp

--- packages/data-context/src/sources/migration/utils.ts
+++ packages/data-context/src/sources/migration/utils.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 
 export const isDefaultSupportFile = (supportFile: string) => {
-  if (_.isNil(supportFile) || !_.isBoolean(supportFile) && supportFile.match(/^cypress\/support($|\/index($|\.(ts|js|coffee)$))/)) {
+  if (_.isNil(supportFile) || !_.isBoolean(supportFile) && supportFile.match(/(^|\.+\/)cypress\/support($|\/index($|\.(ts|js|coffee)$))/)) {
     return true
   }
 
