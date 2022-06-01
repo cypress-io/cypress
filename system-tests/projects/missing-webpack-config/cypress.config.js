ae8d4b2b05ea0a5beec7bdc09e0214d04c3837e1
Tim Griesser <tgriesser10@gmail.com>
2022-06-01 09:24:41 -0400

    fix: cleanup test flake (#21989)
    * fix: test flake in e2e tests
    
    * fix flake in GitDataSource.spec.ts

--- system-tests/projects/missing-webpack-config/cypress.config.js
+++ system-tests/projects/missing-webpack-config/cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
+module.exports = defineConfig({
   component: {
     supportFile: false,
     devServer: {
