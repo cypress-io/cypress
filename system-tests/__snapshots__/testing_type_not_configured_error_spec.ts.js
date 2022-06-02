85b8ca100230154afc534e4eb7fa18107d8df6f7
estrada9166 <estrada9166@hotmail.com>
2022-06-02 10:00:27 -0500

    Add tests

---
+++ system-tests/__snapshots__/testing_type_not_configured_error_spec.ts.js
@@ -0,0 +1,15 @@
+exports['testing type not configured error on run mode / reports correct exit code when failing attempting to run e2e'] = `
+The testing type selected (e2e) is not configured in your config file.
+
+Please update it, and try running it again.
+
+
+`
+
+exports['testing type not configured error on run mode / reports correct exit code when failing attempting to run component'] = `
+The testing type selected (component) is not configured in your config file.
+
+Please update it, and try running it again.
+
+
+`
