b08933398986f16f8554c3967aa8f1b7a6ba0317
estrada9166 <estrada9166@hotmail.com>
2022-06-06 17:37:53 -0500

    Update with code review

--- system-tests/__snapshots__/testing_type_not_configured_error_spec.ts.js
+++ system-tests/__snapshots__/testing_type_not_configured_error_spec.ts.js
@@ -1,7 +1,7 @@
 exports['testing type not configured error on run mode / reports correct exit code when failing attempting to run e2e'] = `
 The testing type selected (e2e) is not configured in your config file.
 
-Please update it, and try running it again.
+Please run ‘cypress open’ and choose your testing type to automatically update your configuration file.
 
 
 `
@@ -9,7 +9,7 @@ Please update it, and try running it again.
 exports['testing type not configured error on run mode / reports correct exit code when failing attempting to run component'] = `
 The testing type selected (component) is not configured in your config file.
 
-Please update it, and try running it again.
+Please run ‘cypress open’ and choose your testing type to automatically update your configuration file.
 
 
 `
