1dabcd67cf53533c1dce2db88dc61f02378ddbff
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-07 18:40:16 +1000

    remove obsolete test

--- packages/server/test/unit/plugins/child/run_require_async_child_spec.js
+++ packages/server/test/unit/plugins/child/run_require_async_child_spec.js
@@ -1,8 +1,6 @@
 require('../../../spec_helper')
 
-const tsNodeUtil = require('../../../../lib/plugins/child/ts_node')
 const runRequireAsyncChild = require('../../../../lib/plugins/child/run_require_async_child')
-const resolve = require('../../../../lib/util/resolve')
 
 describe('lib/plugins/child/run_require_async_child', () => {
   beforeEach(function () {
@@ -15,28 +13,6 @@ describe('lib/plugins/child/run_require_async_child', () => {
 
   afterEach(() => {
     mockery.deregisterMock('@cypress/webpack-batteries-included-preprocessor')
-  })
-
-  describe('typescript registration', () => {
-    beforeEach(() => {
-      sinon.stub(tsNodeUtil, 'register')
-      sinon.stub(resolve, 'typescript').returns('/path/to/typescript.js')
-    })
-
-    it('registers ts-node only once when typescript module found', function () {
-      runRequireAsyncChild(this.ipc, 'cypress.config.js', 'proj-root')
-      runRequireAsyncChild(this.ipc, 'cypress.config.js', 'proj-root')
-
-      expect(tsNodeUtil.register).to.be.calledWith(
-        'proj-root',
-        'cypress.config.js',
-      )
-
-      expect(tsNodeUtil.register).to.be.calledOnce
-    })
-
-    // FIXME: need to validate that TS is checked once when ts is not found as well
-    it.skip('checks for typescript only once if typescript module was not found')
   })
 
   describe('errors', () => {
