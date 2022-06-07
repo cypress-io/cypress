8664a91a068f47d847be4e4b36007bfdbc5198af
Ryan Manuel <ryanm@cypress.io>
2022-06-06 20:49:06 -0500

    fix: properly run multiple specs in run and headed mode on linux and windows

---
+++ system-tests/__snapshots__/headed_spec.ts.js
@@ -0,0 +1,96 @@
+exports['e2e headed runs multiple specs in headed mode 1'] = `
+
+====================================================================================================
+
+  (Run Starting)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Cypress:      1.2.3                                                                            │
+  │ Browser:      FooBrowser 88                                                                    │
+  │ Specs:        2 found (dom-content.spec.js, dom-container.spec.js)                             │
+  │ Searched:     cypress/e2e/dom-content.spec.js, cypress/e2e/dom-container.spec.js               │
+  │ Experiments:  experimentalInteractiveRunEvents=true                                            │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  dom-content.spec.js                                                             (1 of 2)
+
+
+  Dom Content
+    ✓ renders the test content
+
+
+  1 passing
+
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        1                                                                                │
+  │ Passing:      1                                                                                │
+  │ Failing:      0                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  0                                                                                │
+  │ Video:        true                                                                             │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     dom-content.spec.js                                                              │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+  (Video)
+
+  -  Started processing:  Compressing to 32 CRF                                                     
+  -  Finished processing: /XXX/XXX/XXX/cypress/videos/dom-content.spec.js.mp4             (X second)
+
+
+────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                    
+  Running:  dom-container.spec.js                                                           (2 of 2)
+
+
+  Dom Content
+    ✓ renders a container
+
+
+  1 passing
+
+
+  (Results)
+
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Tests:        1                                                                                │
+  │ Passing:      1                                                                                │
+  │ Failing:      0                                                                                │
+  │ Pending:      0                                                                                │
+  │ Skipped:      0                                                                                │
+  │ Screenshots:  0                                                                                │
+  │ Video:        true                                                                             │
+  │ Duration:     X seconds                                                                        │
+  │ Spec Ran:     dom-container.spec.js                                                            │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+  (Video)
+
+  -  Started processing:  Compressing to 32 CRF                                                     
+  -  Finished processing: /XXX/XXX/XXX/cypress/videos/dom-container.spec.js.mp4           (X second)
+
+
+====================================================================================================
+
+  (Run Finished)
+
+
+       Spec                                              Tests  Passing  Failing  Pending  Skipped  
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ ✔  dom-content.spec.js                      XX:XX        1        1        -        -        - │
+  ├────────────────────────────────────────────────────────────────────────────────────────────────┤
+  │ ✔  dom-container.spec.js                    XX:XX        1        1        -        -        - │
+  └────────────────────────────────────────────────────────────────────────────────────────────────┘
+    ✔  All specs passed!                        XX:XX        2        2        -        -        -  
+
+
+`
---
+++ system-tests/test/headed_spec.ts
@@ -0,0 +1,15 @@
+import systemTests from '../lib/system-tests'
+
+describe('e2e headed', function () {
+  systemTests.setup()
+
+  it('runs multiple specs in headed mode', async function () {
+    await systemTests.exec(this, {
+      project: 'cypress-in-cypress',
+      headed: true,
+      spec: 'dom-content.spec.js,dom-container.spec.js',
+      snapshot: true,
+      expectedExitCode: 0,
+    })
+  })
+})
