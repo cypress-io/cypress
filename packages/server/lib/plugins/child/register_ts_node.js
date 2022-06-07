c738da5bdb8171d056c06e123a1744f7117134f9
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-07 14:58:27 +1000

    be less aggressive with erroring

--- packages/server/lib/plugins/child/register_ts_node.js
+++ packages/server/lib/plugins/child/register_ts_node.js
@@ -1,6 +1,5 @@
 const minimist = require('minimist')
 const debugLib = require('debug')
-const dedent = require('dedent')
 const { register } = require('./ts_node')
 
 const debug = debugLib('cypress:server:register-ts-node')
@@ -11,13 +10,7 @@ debug('executing register_ts_node with args %o', args)
 
 const { projectRoot, file } = args
 
-if (!projectRoot || !file) {
-  throw Error(dedent`
-    Need to provide --projectRoot and --file args when using
-    register_ts_node, for example: 
-    node --require register_ts_node --projectRoot=/path/to/project --file=/path/to/project/cypress.config.ts.
-    You passed projectRoot: ${projectRoot} and file ${file}.
-  `)
+if (projectRoot && file) {
+  debug('registering ts-node for projectRoot: %s and file: %s', projectRoot, file)
+  register(projectRoot, file)
 }
-
-register(projectRoot, file)
