b70847c92d820b78f5d323fccf88667d90ebd17d
NinjaTester1997 <106650336+NinjaTester1997@users.noreply.github.com>
2022-06-08 14:46:00 +0530

    Add files via upload

---
+++ javaprogram.java
@@ -0,0 +1,14 @@
+public class PrintAsciiValueExample1   
+{  
+public static void main(String[] args)   
+{  
+// character whose ASCII value to be found  
+char ch1 = 'a';  
+char ch2 = 'b';  
+// variable that stores the integer value of the character  
+int asciivalue1 = ch1;  
+int asciivalue2 = ch2;  
+System.out.println("The ASCII value of " + ch1 + " is this: " + asciivalue1);  
+System.out.println("The ASCII value of " + ch2 + " is this: " + asciivalue2);  
+}  
+}  
