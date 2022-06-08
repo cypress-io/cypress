08af079d80bf39d48831f1f8f4f9756f26000387
Vinicius Pietscher <vini.914@gmail.com>
2022-06-08 16:23:37 -0400

    no longer necessary padding will be added to Main.vue

--- packages/launchpad/src/global/GlobalPage.vue
+++ packages/launchpad/src/global/GlobalPage.vue
@@ -13,7 +13,6 @@
       {{ t('globalPage.recentProjectsHeader') }}
     </h2>
     <div
-      class="pb-24px"
       :class="{ 'md:grid md:grid-cols-2 md:gap-24px mb-0': filteredProjects?.length > 1 }"
     >
       <GlobalProjectCard
