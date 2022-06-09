f1a0d6c7be006752591d18d80f0a2dfc6a92db82
Zach Bloomquist <git@chary.us>
2022-06-09 10:24:11 -0400

    debug

--- scripts/binary/smoke.js
+++ scripts/binary/smoke.js
@@ -94,6 +94,8 @@ const runProjectTest = function (buildAppExecutable, e2e) {
       stdio: 'inherit', env,
     }
 
+    env.DEBUG = 'cypress:*'
+
     console.log('running project test')
     console.log(buildAppExecutable, args.join(' '))
 
