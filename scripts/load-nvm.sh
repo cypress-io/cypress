0ae48dc7e655c01e1603ebda6cb6c17b3b3a3564
Zach Bloomquist <git@chary.us>
2022-06-10 16:14:59 -0400

    fix script

--- scripts/load-nvm.sh
+++ scripts/load-nvm.sh
@@ -3,14 +3,14 @@
 # some environments (like Arm on CircleCI) bring their own nvm
 if type nvm &>/dev/null; then
   echo 'nvm found, not doing anything in load-nvm.sh.'
-  exit 0
-fi
-
-if [ -s "${HOME}/.nvm/nvm.sh" ]; then
-  echo 'nvm found in home, sourcing...'
 else
-  echo "nvm not found. Installing nvm..."
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
+  if [ -s "${HOME}/.nvm/nvm.sh" ]; then
+    echo 'nvm found in home, sourcing...'
+  else
+    echo "nvm not found. Installing nvm..."
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
+  fi
+
+  source "${HOME}/.nvm/nvm.sh"
 fi
 
-source "${HOME}/.nvm/nvm.sh"
