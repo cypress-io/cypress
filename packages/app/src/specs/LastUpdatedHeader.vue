4296ebba9b98dcefbb7529edc77a7a583882b33b
Peter Stakoun <peter@stakoun.com>
2022-06-15 14:27:34 -0400

    Update ACI column behavior

--- packages/app/src/specs/LastUpdatedHeader.vue
+++ packages/app/src/specs/LastUpdatedHeader.vue
@@ -3,6 +3,7 @@
     placement="top"
     :is-interactive="true"
     :hide-delay="0"
+    class="bg-white z-10"
   >
     <div
       class="cursor-default decoration-dotted underline underline-gray-300 underline-offset-4"
