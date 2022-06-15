cb2cc2dc183864d8dbeafe8d361b7425c9f712be
Jordan <jordan@jpdesigning.com>
2022-06-15 13:47:20 -0400

    chore(system-tests): add angular 12, 13 & 14 apps

--- system-tests/projects/angular-14/src/app/app.component.ts
+++ system-tests/projects/angular-14/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'angular-14';
 }
