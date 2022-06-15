c999048a100dcb8356d0834da9dad2e5db53cbde
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-15 19:45:01 +1000

    add new test projects for npm-react

---
+++ system-tests/projects/npm-react-vite/cypress.config.ts
@@ -0,0 +1,18 @@
+export default {
+  viewportWidth: 400,
+  viewportHeight: 400,
+  video: false,
+  experimentalFetchPolyfill: true,
+  component: {
+    excludeSpecPattern: [
+      '**/__snapshots__/*',
+      '**/__image_snapshots__/*',
+      'examples/**/*',
+    ],
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+    specPattern: 'cypress/**/*.cy.jsx',
+  },
+}
