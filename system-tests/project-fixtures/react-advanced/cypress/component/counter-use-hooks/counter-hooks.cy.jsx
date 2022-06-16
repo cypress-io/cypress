88f3df4028b3d60fb6b2bf31254c4177a2a4a594
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-16 14:37:46 +1000

    add extension

--- system-tests/project-fixtures/react-advanced/cypress/component/counter-use-hooks/counter-hooks.cy.jsx
+++ system-tests/project-fixtures/react-advanced/cypress/component/counter-use-hooks/counter-hooks.cy.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { mount } from 'cypress/react'
-import Counter from './counter'
+import Counter from './counter.jsx'
 
 describe('Counter using hooks', () => {
   it('works', () => {
