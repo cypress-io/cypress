1a5f6683bf60a2cc96bf401491e4289d51a5c42d
Zachary Williams <ZachJW34@gmail.com>
2022-06-17 14:03:18 -0500

    fix: make virtual-list loading more efficient (#22384)

--- packages/app/src/specs/SpecsListRowItem.vue
+++ packages/app/src/specs/SpecsListRowItem.vue
@@ -2,7 +2,7 @@
   <div data-cy="specs-list-row">
     <component
       :is="isLeaf ? 'RouterLink' : 'div'"
-      class="h-full outline-none border-gray-50 pr-20px ring-inset grid grid-cols-7 md:grid-cols-9 group focus:outline-transparent focus-within:ring-indigo-300 focus-within:ring-1 children:cursor-pointer"
+      class="h-full outline-none border-gray-50 ring-inset grid pr-20px grid-cols-7 group md:grid-cols-9 focus:outline-transparent focus-within:ring-indigo-300 focus-within:ring-1 children:cursor-pointer"
       :to="route"
       :data-cy="isLeaf ? 'spec-item-link' : 'spec-item-directory'"
       @click="emit('toggleRow')"
@@ -15,23 +15,28 @@
       >
         <slot name="file" />
       </div>
-      <div
-        data-cy="specs-list-row-git-info"
-        class="col-span-2"
+      <template
+        v-if="lazyRender"
       >
-        <slot name="git-info" />
-      </div>
-      <div>
-        <slot name="latest-runs" />
-      </div>
-      <div class="invisible md:visible md:col-span-2">
-        <slot name="average-duration" />
-      </div>
+        <div
+          data-cy="specs-list-row-git-info"
+          class="col-span-2"
+        >
+          <slot name="git-info" />
+        </div>
+        <div>
+          <slot name="latest-runs" />
+        </div>
+        <div class="invisible md:col-span-2 md:visible">
+          <slot name="average-duration" />
+        </div>
+      </template>
     </component>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useTimeout } from '@vueuse/core'
 import type { RouteLocationRaw } from 'vue-router'
 
 defineProps<{
@@ -42,6 +47,8 @@ defineProps<{
 const emit = defineEmits<{
   (event: 'toggleRow'): void
 }>()
+
+const lazyRender = useTimeout(50)
 
 function handleCtrlClick (): void {
   // noop intended to reduce the chances of opening tests multiple tabs
