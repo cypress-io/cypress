8c82f89076c643fae507082bd4af375d372238b6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-24 00:01:19 +1000

    alias react-dom

--- system-tests/project-fixtures/next/cypress.config.js
+++ system-tests/project-fixtures/next/cypress.config.js
@@ -10,6 +10,7 @@ module.exports = {
         resolve: {
           alias: {
             'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
           }
         }
       }
