5293e3d0206e40b8f8af9cfe1e6491b8c1861a42
elevatebart <ledouxb@gmail.com>
2022-06-28 11:40:11 -0500

    use updated icons

--- packages/app/src/navigation/SidebarNavigationRow.vue
+++ packages/app/src/navigation/SidebarNavigationRow.vue
@@ -23,9 +23,23 @@
     >
       <component
         :is="icon"
-        :class="active ? 'icon-dark-indigo-300 icon-light-indigo-700' : 'icon-dark-gray-500 icon-light-gray-900 group-hover:(icon-dark-gray-300 icon-light-gray-800) group-focus:(icon-dark-gray-300 icon-light-gray-800)'"
-        class="flex-shrink-0 h-24px m-12px w-24px
-              children:transition children:duration-300"
+        v-if="active"
+        stroke-color="indigo-300"
+        fill-color="indigo-700"
+        class="flex-shrink-0
+        h-24px m-12px w-24px
+        children:transition children:duration-300
+        "
+      />
+      <component
+        :is="icon"
+        v-else
+        stroke-color="gray-500"
+        fill-color="gray-900"
+        class="flex-shrink-0
+        h-24px m-12px w-24px
+        children:transition children:duration-300
+        group-hover:(icon-dark-gray-300 icon-light-gray-800) group-focus:(icon-dark-gray-300 icon-light-gray-800) "
       />
       <span
         :class="[active ? 'text-indigo-300' : 'text-gray-500 group-hocus:text-gray-300']"
@@ -45,7 +59,7 @@ import type { FunctionalComponent, SVGAttributes } from 'vue'
 import Tooltip from '@packages/frontend-shared/src/components/Tooltip.vue'
 
 withDefaults(defineProps <{
-  icon: FunctionalComponent<SVGAttributes, {}>
+  icon: FunctionalComponent<SVGAttributes>
   name: string
   // Currently active row (generally the current route)
   active?: boolean
