a4deaee97b81e065c328fae2c7a0403c9dc9fd86
Bill Glesias <bglesias@gmail.com>
2022-06-28 17:16:18 -0400

    fix lint types

--- cli/types/cypress.d.ts
+++ cli/types/cypress.d.ts
@@ -2854,7 +2854,7 @@ declare namespace Cypress {
      * additional obstructive JS code in .js or .html files
      * that are leveraged by common authentication providers.
      * NOTE: Setting this flag to true currently removes Subresource Integrity (SRI).
-     * Please see https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity. 
+     * Please see https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity.
      * This option has no impact on experimentalSourceRewriting and is only used with the
      * non-experimental source rewriter.
      * This option also modifies ANY javascript, including cross-origin scripts, to be passed through the rewriter.
