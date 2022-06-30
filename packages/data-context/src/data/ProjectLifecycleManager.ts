5c32dd4cf57a97ebd54005bf2107de398c23c6a4
astone123 <adams@cypress.io>
2022-06-30 13:15:51 -0400

    fix: Catch refreshLifecycle errors in resetErrorAndLoadConfig mutation

--- packages/data-context/src/data/ProjectLifecycleManager.ts
+++ packages/data-context/src/data/ProjectLifecycleManager.ts
@@ -797,7 +797,7 @@ export class ProjectLifecycleManager {
    * centrally in the e2e tests, as well as notify the "pending initialization"
    * for run mode
    */
-  private onLoadError = (err: any) => {
+  onLoadError = (err: any) => {
     if (this.ctx.isRunMode && this._configManager) {
       this._configManager.onLoadError(err)
     } else {
