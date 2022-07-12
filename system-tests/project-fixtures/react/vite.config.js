6c22f1bd592bcc970b8f9107823904648256be2c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-12 14:19:01 +1000

    reduce duplication via fixtures

--- system-tests/project-fixtures/react/vite.config.js
+++ system-tests/project-fixtures/react/vite.config.js
@@ -1,5 +1,17 @@
 const {defineConfig} = require('vite')
+const react = require('@vitejs/plugin-react')
 
 module.exports = defineConfig({
-  logLevel: 'silent'
+  logLevel: 'silent',
+  plugins: [react()],
+  server: {
+    fs: {
+      // this is needed to run on CI since we
+      // do some magic with symlinks and caching
+      // to make everything fast, that Vite does
+      // not seem to like.
+      // https://vitejs.dev/config/#server-fs-allow
+      allow: ['/root/cypress/', '/root/.cache/', '/tmp/', '/Users/', '/private/'],
+    },
+  },
 })
