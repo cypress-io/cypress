5e695d180cc06eadacfacd75ace453487064cebb
Rachel <Raquel.rudermano@gmail.com>
2022-07-14 12:26:30 -0700

    organize test in context block

--- packages/frontend-shared/src/gql-components/HeaderBarContent.cy.tsx
+++ packages/frontend-shared/src/gql-components/HeaderBarContent.cy.tsx
@@ -162,39 +162,42 @@ describe('<HeaderBarContent />', { viewportWidth: 1000, viewportHeight: 750 }, (
     })
   })
 
-  // https://github.com/cypress-io/cypress/issues/21842
-  it('docs menu is shown correctly on small viewports', () => {
-    cy.mountFragment(HeaderBar_HeaderBarContentFragmentDoc, {
-      onResult: (result) => {
-        result.currentProject = null
-      },
-      render: (gqlVal) => (
-        <div class="border-current border-1 h-700px resize overflow-auto">
-          <HeaderBarContent gql={gqlVal} />
-        </div>
-      ),
+  context('responsive design', () => {
+    beforeEach(() => {
+      cy.mountFragment(HeaderBar_HeaderBarContentFragmentDoc, {
+        onResult: (result) => {
+          result.currentProject = null
+        },
+        render: (gqlVal) => (
+          <div class="border-current border-1 h-700px resize overflow-auto">
+            <HeaderBarContent gql={gqlVal} />
+          </div>
+        ),
+      })
     })
 
-    // Simulate the small viewport.
-    cy.viewport(767, 800)
+    // https://github.com/cypress-io/cypress/issues/21842
+    it('shows docs menu correctly on small viewports', () => {
+      // Simulate the small viewport.
+      cy.viewport(767, 800)
 
-    // show docs menu
-    cy.contains('button', text.docsMenu.docsHeading).click()
+      // show docs menu
+      cy.contains('button', text.docsMenu.docsHeading).click()
 
-    // docs menu flex direction is column when viewport width is small
-    cy.get('[data-cy=docs-menu-container]').should('have.css', 'flex-direction', 'column')
+      // docs menu flex direction is column when viewport width is small
+      cy.findByTestId('docs-menu-container').should('have.css', 'flex-direction', 'column')
+    })
 
-    // Hide docs menu
-    cy.contains('button', text.docsMenu.docsHeading).click()
+    it('shows docs menu correctly on wider viewports', () => {
+      // Change the viewport size to wide.
+      cy.viewport(768, 800)
 
-    // Change the viewport size to wide.
-    cy.viewport(768, 800)
+      // show docs menu
+      cy.contains('button', text.docsMenu.docsHeading).click()
 
-    // show docs menu
-    cy.contains('button', text.docsMenu.docsHeading).click()
-
-    // docs menu flex direction is row when viewport width is big enough.
-    cy.get('[data-cy=docs-menu-container]').should('have.css', 'flex-direction', 'row')
+      // docs menu flex direction is row when viewport width is big enough.
+      cy.findByTestId('docs-menu-container').should('have.css', 'flex-direction', 'row')
+    })
   })
 
   it('does not show hint when on latest version of Cypress', () => {
