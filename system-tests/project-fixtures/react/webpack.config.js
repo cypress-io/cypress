5212f9b60a3f3e9a9b5f95d3d5befa2b68535387
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-14 12:28:38 +1000

    reverted code

--- system-tests/project-fixtures/react/webpack.config.js
+++ system-tests/project-fixtures/react/webpack.config.js
@@ -1,14 +1,9 @@
-const path = require('path')
 /**
  * @type {import('webpack').Configuration}
  */
 module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx'],
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    }
   },
   module: {
     rules: [
