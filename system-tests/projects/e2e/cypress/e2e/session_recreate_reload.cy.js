e70d4296d56ec35df61fa88ef9104b00ce5b0341
Emily Rohrbough <emilyrohrbough@yahoo.com>
2022-07-15 15:30:03 -0500

    fix

--- system-tests/projects/e2e/cypress/e2e/session_recreate_reload.cy.js
+++ system-tests/projects/e2e/cypress/e2e/session_recreate_reload.cy.js
@@ -1,8 +1,8 @@
 top.count = top.count || 0
 
 // @see https://github.com/cypress-io/cypress/issues/17805
-describe('issue 17805', { experimentalSessionAndOrigin: true }, () => {
-  it('recreates session on spec reload in open mode', () => {
+describe('recreates session on spec reload in open mode', () => {
+  it('sets session', () => {
     let validateFlag = false
 
     cy.session('persist_session', () => {
@@ -28,7 +28,10 @@ describe('issue 17805', { experimentalSessionAndOrigin: true }, () => {
         Cypress.config().isTextTerminal = false
         Cypress.config().isInteractive = true
 
-        top.location.reload()
+        // this simulates user clicking the stop and reload button
+        // in the browser reporter gui
+        cy.$$('button.stop', top.document)[0].click()
+        cy.$$('button.restart', top.document)[0].click()
       })
     }
 
