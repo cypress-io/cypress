58753e8f405af5c8417ccef3b1c6d26f5fc08145
Mike Plummer <mikep@cypress.io>
2022-07-22 15:43:35 -0500

    Use `Badge` component from frontend-shared

--- packages/app/src/specs/flaky-badge/FlakyBadge.vue
+++ packages/app/src/specs/flaky-badge/FlakyBadge.vue
@@ -1,15 +1,14 @@
 <template>
-  <div
-    class="font-bold bg-orange-50 rounded-4px text-xs px-4px text-orange-600 items-center uppercase"
-    style="width: fit-content"
+  <Badge
+    :label="t('specPage.flaky.badgeLabel')"
+    status="warning"
+    class="font-bold uppercase"
     data-cy="flaky-badge"
-  >
-    {{ t('specPage.flaky.badgeLabel') }}
-  </div>
+  />
 </template>
 
 <script setup lang="ts">
-
+import Badge from '@packages/frontend-shared/src/components/Badge.vue'
 import { useI18n } from '@cy/i18n'
 
 const { t } = useI18n()
