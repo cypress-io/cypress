54b73b572b038f0e3d81d965cdf123535b47a2cc
KHeo <sainthkh@naver.com>
2022-08-01 11:15:49 +0900

    chore: Intregrate runner packages.

--- packages/runner-shared/src/studio/index.js
+++ packages/runner-shared/src/studio/index.js
@@ -1,3 +1,7 @@
+// Studio tests have been removed with v10 update.
+// You can find the tests in the PR below.
+// @see https://github.com/cypress-io/cypress/pull/9542
+
 export * from './studio'
 
 export * from './studio-modals'
