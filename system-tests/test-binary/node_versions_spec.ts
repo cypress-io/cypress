df4ab51d209e7aa24b85014e0e4d53b489574036
Zachary Williams <zachjw34@gmail.com>
2022-08-02 17:48:51 -0500

    test: binary system-test bug

--- system-tests/test-binary/node_versions_spec.ts
+++ system-tests/test-binary/node_versions_spec.ts
@@ -12,7 +12,7 @@ function smokeTestDockerImage (dockerImage: string) {
     })
   })
 
-  context('component', () => {
+  context.only('component', () => {
     systemTests.it(`can run in ${dockerImage}`, {
       withBinary: true,
       browser: 'electron',
@@ -26,9 +26,9 @@ function smokeTestDockerImage (dockerImage: string) {
 
 describe('binary node versions', () => {
   [
-    'cypress/base:12',
-    'cypress/base:14',
+    // 'cypress/base:12',
+    // 'cypress/base:14',
     'cypress/base:16.14.2',
-    'cypress/base:17.3.0',
+    // 'cypress/base:17.3.0',
   ].forEach(smokeTestDockerImage)
 })
