6e78919e65caa041f813eb947d8052115936397f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-08-03 11:49:23 +1000

    simplify code

--- packages/data-context/src/actions/ErrorActions.ts
+++ packages/data-context/src/actions/ErrorActions.ts
@@ -23,32 +23,8 @@ export class ErrorActions {
     this.ctx.update((d) => {
       const warningsIndex = d.diagnostics.warnings.findIndex((v) => v.id === id)
 
-      if (warningsIndex != null && warningsIndex !== -1) {
+      if (warningsIndex !== -1) {
         d.diagnostics.warnings.splice(warningsIndex, 1)
-
-        return
-      }
-
-      const projectWarningsIndex = d.diagnostics.warnings.findIndex((v) => v.id === id)
-
-      if (projectWarningsIndex != null && projectWarningsIndex !== -1) {
-        d.diagnostics.warnings.splice(projectWarningsIndex, 1)
-
-        return
-      }
-
-      const testingTypeWarningsIndex = d.diagnostics.warnings.findIndex((v) => v.id === id)
-
-      if (testingTypeWarningsIndex != null && testingTypeWarningsIndex !== -1) {
-        d.diagnostics.warnings.splice(testingTypeWarningsIndex, 1)
-
-        return
-      }
-
-      const appWarningsIndex = d.diagnostics.warnings.findIndex((v) => v.id === id)
-
-      if (appWarningsIndex != null && appWarningsIndex !== -1) {
-        d.diagnostics.warnings.splice(appWarningsIndex, 1)
       }
     })
   }
