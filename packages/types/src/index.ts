d498a534f9371b00215bd5a53d00a7aa6b9dcac6
Tyler Biethman <tbiethman@gmail.com>
2022-08-05 15:41:35 -0500

    Removing exports that no longer exist

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -20,10 +20,6 @@ export type { PlatformName } from './platform'
 
 export {
   BROWSER_FAMILY,
-  MIN_CHROME_VERSION,
-  MIN_FIREFOX_VERSION,
-  MIN_EDGE_VERSION,
-  browsers,
 } from './browser'
 
 export * from './config'
