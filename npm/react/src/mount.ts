f04e0718cec2f01343fc4ba77159ce63eb1d141e
Zachary Williams <zachjw34@gmail.com>
2022-08-08 15:48:37 -0500

    fix(react18): unmount component with react18 API

--- npm/react/src/mount.ts
+++ npm/react/src/mount.ts
@@ -1,34 +1,44 @@
+import { getContainerEl } from '@cypress/mount-utils'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {
   makeMountFn,
   makeUnmountFn,
-  lastMountedReactDom,
 } from './createMount'
 import type {
   MountOptions,
   InternalMountOptions,
-  InternalUnmountOptionsReact,
 } from './types'
+
+let lastReactDom: any
+
+const cleanup = () => {
+  if (lastReactDom) {
+    const root = getContainerEl()
+
+    lastReactDom.unmountComponentAtNode(root)
+
+    return true
+  }
+
+  return false
+}
 
 export function mount (jsx: React.ReactNode, options: MountOptions = {}, rerenderKey?: string) {
   const internalOptions: InternalMountOptions = {
     reactDom: ReactDOM,
     render: (reactComponent: ReturnType<typeof React.createElement>, el: HTMLElement, reactDomToUse: typeof ReactDOM) => {
-      return (reactDomToUse || ReactDOM).render(reactComponent, el)
+      lastReactDom = (reactDomToUse || ReactDOM)
+
+      return lastReactDom.render(reactComponent, el)
     },
     unmount,
+    cleanup,
   }
 
   return makeMountFn('mount', jsx, { ReactDom: ReactDOM, ...options }, rerenderKey, internalOptions)
 }
 
 export function unmount (options = { log: true }) {
-  const internalOptions: InternalUnmountOptionsReact = {
-    unmount: (el) => {
-      return (lastMountedReactDom || ReactDOM).unmountComponentAtNode(el)
-    },
-  }
-
-  return makeUnmountFn(options, internalOptions)
+  return makeUnmountFn(options)
 }
