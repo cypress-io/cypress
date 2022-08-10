d599a906d95a1d5a16c9bb747d154ebfda1b1eff
Mark Noonan <mark@cypress.io>
2022-08-10 18:26:20 -0400

    get more substring matches

--- packages/app/src/specs/spec-utils.ts
+++ packages/app/src/specs/spec-utils.ts
@@ -98,7 +98,7 @@ export function fuzzySortSpecs <T extends FuzzyFoundSpec> (specs: T[], searchVal
   const transformedSpecs = addDirectoryToSpecs(specs)
 
   return fuzzySort
-  .go(searchValue, transformedSpecs, { keys: ['baseName', 'directory'], allowTypo: false, threshold: -100 })
+  .go(searchValue, transformedSpecs, { keys: ['baseName', 'directory'], allowTypo: false, threshold: -3000 })
   .map((result) => {
     const [file, dir] = result
 
