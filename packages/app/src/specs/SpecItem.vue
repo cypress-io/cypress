c07cc69202c2c7102cd7563f59c67d03ae6e44eb
Zachary Williams <zachjw34@gmail.com>
2022-08-12 16:16:24 -0500

    fix: match full spec path and improve search results

--- packages/app/src/specs/SpecItem.vue
+++ packages/app/src/specs/SpecItem.vue
@@ -13,13 +13,13 @@
     >
       <HighlightedText
         :text="fileName"
-        :indexes="indexes.filter((idx) => idx < fileName.length)"
+        :indexes="split.fileNameIndexes"
         class="font-medium text-indigo-500 group-hocus:text-indigo-700"
         highlight-classes="text-gray-1000"
       />
       <HighlightedText
         :text="extension"
-        :indexes="indexes.filter((idx) => idx >= fileName.length).map(idx => idx - fileName.length)"
+        :indexes="split.extensionIndexes"
         class="font-light group-hocus:text-gray-400"
         highlight-classes="text-gray-1000"
       />
@@ -28,11 +28,16 @@
 </template>
 
 <script lang="ts" setup>
+import { toRefs } from '@vueuse/core'
 import HighlightedText from './HighlightedText.vue'
+import { useSplitIndexes } from './spec-utils'
 
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
   fileName: string
   extension: string
   indexes?: number[]
 }>(), { indexes: () => [] })
+
+const refs = toRefs(props)
+const split = useSplitIndexes(refs.fileName, refs.indexes)
 </script>
