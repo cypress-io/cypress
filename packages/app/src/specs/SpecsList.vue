e5e79cf3dcd03e698dcd91ce1fd4465519838a68
Mark Noonan <mark@cypress.io>
2022-08-13 15:48:03 -0400

    feat(app): show "connect project" after login if project is not connected (#23295)
    Co-authored-by: Mike Plummer <mikep@cypress.io>

--- packages/app/src/specs/SpecsList.vue
+++ packages/app/src/specs/SpecsList.vue
@@ -153,7 +153,9 @@
     v-model="isLoginOpen"
     :gql="props.gql"
     :utm-medium="loginUtmMedium"
-    @loggedin="refreshPage"
+    :show-connect-button-after-login="!props.gql?.currentProject?.projectId"
+    @loggedin="handleLoggedin"
+    @connect-project="isProjectConnectOpen = true"
   />
   <CloudConnectModals
     v-if="isProjectConnectOpen"
@@ -485,6 +487,15 @@ async function refetchFailedCloudData () {
 
 function refreshPage () {
   location.reload()
+}
+
+const handleLoggedin = () => {
+  // if there is no project id, there can be no cloud data to refresh -
+  // we want to stay on the page and let the `connect-project` event take the user
+  // to the next step to connect their project
+  if (props.gql.currentProject?.projectId) {
+    refreshPage()
+  }
 }
 
 </script>
