247c8b94314cecfa13f6292776d75c741f04e058
Mike Plummer <mikep@cypress.io>
2022-08-14 09:48:26 -0500

    Pluck projectId from config rather than directly query

--- packages/frontend-shared/src/gql-components/HeaderBarContent.vue
+++ packages/frontend-shared/src/gql-components/HeaderBarContent.vue
@@ -157,7 +157,7 @@
         v-model="isLoginOpen"
         :gql="props.gql"
         utm-medium="Nav"
-        :show-connect-button-after-login="isApp && !props.gql?.currentProject?.projectId"
+        :show-connect-button-after-login="isApp && !cloudProjectId"
         @connect-project="handleConnectProject"
       />
     </div>
@@ -234,7 +234,6 @@ fragment HeaderBar_HeaderBarContent on Query {
     savedState
     currentTestingType
     branch
-    projectId
   }
   isGlobalMode
   ...TopNav
@@ -252,6 +251,10 @@ const savedState = computed(() => {
 })
 
 const currentProject = computed(() => props.gql.currentProject)
+
+const cloudProjectId = computed(() => {
+  return props.gql?.currentProject?.config?.find((item: { field: string }) => item.field === 'projectId')?.value
+})
 
 const isLoginOpen = ref(false)
 const clearCurrentProjectMutation = useMutation(GlobalPageHeader_ClearCurrentProjectDocument)
@@ -336,7 +339,7 @@ function shouldShowPrompt (prompt: { slug: string, noProjectId: boolean, interva
 
   // if prompt requires no project id,
   // check if project id exists
-  if (prompt.noProjectId && props.gql?.currentProject?.projectId) {
+  if (prompt.noProjectId && cloudProjectId.value) {
     return false
   }
 
