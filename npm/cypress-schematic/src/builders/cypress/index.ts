f6d45297350865c189f9a2ef30d7f8d257b347a5
conradkirschner <conrad.kirschner@idealo.de>
2022-08-15 18:52:18 +0200

    fix: cypress-schematic only set watch mode if not nguniversal ssr dev server

--- npm/cypress-schematic/src/builders/cypress/index.ts
+++ npm/cypress-schematic/src/builders/cypress/index.ts
@@ -103,12 +103,16 @@ export function startDevServer ({
   // this is relevant for running Cypress against dev server target that does not support this option,
   // for instance @nguniversal/builders:ssr-dev-server.
   // see https://github.com/nrwl/nx/blob/f930117ed6ab13dccc40725c7e9551be081cc83d/packages/cypress/src/executors/cypress/cypress.impl.ts
-  if (!devServerTarget.includes('watch')) {
-    watch = undefined;
-  }
+  const angularJson = require(`${context.workspaceRoot }/angular.json`)
+  const { project, target } = targetFromTargetString(devServerTarget)
+  const devServerConfig = angularJson.projects[project].architect[target]
+  let overrides = {}
 
-  const overrides = {
-    watch,
+  if (devServerConfig.builder !== '@nguniversal/builders:ssr-dev-server') {
+    console.info('DevServer will be started in watch mode.')
+    overrides = {
+      watch,
+    }
   }
 
   //@ts-ignore
