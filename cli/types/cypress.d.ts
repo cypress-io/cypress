34d163fa238fbe2fefb89dc3a19ecd767ea1de74
Kukhyeon Heo <sainthkh@naver.com>
2022-08-15 23:46:24 +0900

    chore: add check-ts command to pkg/driver (#22912)
    Co-authored-by: Blue F <blue@everblue.info>
    Co-authored-by: Rachel <Raquel.rudermano@gmail.com>
    Co-authored-by: Rachel <rachel@cypress.io>

--- cli/types/cypress.d.ts
+++ cli/types/cypress.d.ts
@@ -3073,7 +3073,12 @@ declare namespace Cypress {
    * Takes ComponentDevServerOpts to track the signature of the devServerConfig for the provided `devServer`,
    * so we have proper completion for `devServerConfig`
    */
-  type ConfigOptions<ComponentDevServerOpts = any> = Partial<UserConfigOptions<ComponentDevServerOpts>>
+  type ConfigOptions<ComponentDevServerOpts = any> = Partial<UserConfigOptions<ComponentDevServerOpts>> & {
+    /**
+     * Hosts mappings to IP addresses.
+     */
+     hosts?: null | {[key: string]: string}
+  }
 
   interface PluginConfigOptions extends ResolvedConfigOptions, RuntimeConfigOptions {
     /**
