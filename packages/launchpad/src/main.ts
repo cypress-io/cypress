9c0b402663d86025462867fafb78a67b0d7ddce6
Rachel <Raquel.rudermano@gmail.com>
2022-08-17 16:56:41 -0700

    Revert "try new logic for header wait"
    This reverts commit bfed31edceb3433ef1d2d6732d3dddc4812e4526.

--- packages/launchpad/src/main.ts
+++ packages/launchpad/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
+import { HeaderBar_HeaderBarQueryDocument } from './generated/graphql'
 import './main.scss'
 import 'virtual:windi.css'
+import urql from '@urql/vue'
 import App from './App.vue'
 import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+import { makeUrqlClient } from '@packages/frontend-shared/src/graphql/urqlClient'
 import { createI18n } from '@cy/i18n'
 import { initHighlighter } from '@packages/frontend-shared/src/components/highlight'
 
@@ -17,6 +20,19 @@ app.use(Toast, {
 
 app.use(createI18n())
 
-// Make sure highlighter is initialized immediately at app
-// start, so it's available when we render code blocks
-initHighlighter().then(() => app.mount('#app'))
+Promise.all([
+  makeUrqlClient({ target: 'launchpad' }).then((launchpadClient) => {
+    app.use(urql, launchpadClient)
+
+    // Loading the Header Bar Query document prior to mounting leads to a better experience
+    // when doing things like taking snapshots of the DOM during testing, and it
+    // shouldn't be any different to the user
+    return launchpadClient
+    .query(HeaderBar_HeaderBarQueryDocument)
+  }),
+  // Make sure highlighter is initialized immediately at app
+  // start, so it's available when we render code blocks
+  initHighlighter(),
+]).then(() => {
+  app.mount('#app')
+})
