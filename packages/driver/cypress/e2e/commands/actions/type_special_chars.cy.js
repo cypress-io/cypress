47d1155219fb60948fc36f937928f05f4e9889b8
Tyler Biethman <tbiethman@gmail.com>
2022-08-23 21:28:23 -0500

    Simplifying workaround for webkit focus selection

--- packages/driver/cypress/e2e/commands/actions/type_special_chars.cy.js
+++ packages/driver/cypress/e2e/commands/actions/type_special_chars.cy.js
@@ -314,8 +314,8 @@ describe('src/cy/commands/actions/type - #type special chars', () => {
       attachKeyListeners({ input })
 
       cy.get(':text:first').invoke('val', 'ab')
-      .focus()
       .then(($input) => $input[0].setSelectionRange(0, 0))
+      .focus()
       .type('{backspace}')
       .should('have.value', 'ab')
 
@@ -357,8 +357,8 @@ describe('src/cy/commands/actions/type - #type special chars', () => {
       attachKeyListeners({ input })
 
       cy.get('textarea:first').invoke('val', 'ab')
-      .focus()
       .then(($textarea) => $textarea[0].setSelectionRange(0, 0))
+      .focus()
       .type('{backspace}')
       .should('have.value', 'ab')
 
@@ -452,8 +452,8 @@ describe('src/cy/commands/actions/type - #type special chars', () => {
       attachKeyListeners({ input })
 
       cy.get(':text:first').invoke('val', 'ab')
-      .focus()
       .then(($input) => $input[0].setSelectionRange(0, 0))
+      .focus()
       .type('{del}')
       .should('have.value', 'b')
 
@@ -467,7 +467,6 @@ describe('src/cy/commands/actions/type - #type special chars', () => {
       attachKeyListeners({ input })
 
       cy.get(':text:first').invoke('val', 'ab')
-
       .then(($input) => $input[0].setSelectionRange(0, 0))
       .focus()
       .type('{selectall}{del}')
@@ -497,8 +496,8 @@ describe('src/cy/commands/actions/type - #type special chars', () => {
       attachKeyListeners({ textarea })
 
       cy.get('textarea:first').invoke('val', 'ab')
-      .focus()
       .then(($textarea) => $textarea[0].setSelectionRange(0, 0))
+      .focus()
       .type('{del}')
       .should('have.value', 'b')
 
