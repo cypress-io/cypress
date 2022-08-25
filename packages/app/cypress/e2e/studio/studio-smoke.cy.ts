bd2084ce277593b93adc4d924e0c435311d92f10
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-08-25 16:36:24 +1000

    additional spec

--- packages/app/cypress/e2e/studio/studio-smoke.cy.ts
+++ packages/app/cypress/e2e/studio/studio-smoke.cy.ts
@@ -58,31 +58,4 @@ it('visits a basic html page', () => {
 })`.trim())
     })
   })
-
-  // TODO: Can we somehow do the "Create Test" workflow within Cypress in Cypress?
-  it.skip('creates a brand new test', () => {
-    cy.scaffoldProject('experimental-studio')
-    cy.openProject('experimental-studio')
-    cy.startAppServer('e2e')
-    cy.visitApp()
-    cy.visit(`http://localhost:4455/__/#/specs/runner?file=cypress/e2e/empty.cy.js`)
-
-    cy.waitForSpecToFinish()
-
-    cy.contains('Create test with Cypress Studio').click()
-    cy.get('[data-cy="aut-url"]').as('urlPrompt')
-
-    cy.get('@urlPrompt').within(() => {
-      cy.contains('Continue ➜').should('be.disabled')
-    })
-
-    cy.get('@urlPrompt').type('http://localhost:4455/cypress/e2e/index.html')
-
-    cy.get('@urlPrompt').within(() => {
-      cy.contains('Continue ➜').should('not.be.disabled')
-      cy.contains('Cancel').click()
-    })
-
-    // TODO: Can we somehow do the "Create Test" workflow within Cypress in Cypress?
-  })
 })
