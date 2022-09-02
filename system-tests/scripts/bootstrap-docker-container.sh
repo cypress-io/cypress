c219e2c6c740bb157e2823a3644b2d0bab4a6478
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-02 13:15:49 +1000

    update tests with docker images and docker script

--- system-tests/scripts/bootstrap-docker-container.sh
+++ system-tests/scripts/bootstrap-docker-container.sh
@@ -36,6 +36,9 @@ export CYPRESS_CACHE_FOLDER=/tmp/CYPRESS_CACHE_FOLDER/
 export npm_config_cache=/tmp/npm_config_cache/
 export npm_config_package_lock=false
 
+mkdir /tmp/npm_config_cache
+chown -R 1000:1000 /tmp/npm_config_cache
+
 npx npm@latest install --unsafe-perm --allow-root --force file:$CLI_PATH
 
 PATH=$PATH:./node_modules/.bin
