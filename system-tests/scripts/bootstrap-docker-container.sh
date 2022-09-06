ba268147e76eee186b79865af2efd0a0848761e8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-07 05:15:00 +1000

    Update system-tests/scripts/bootstrap-docker-container.sh
    Co-authored-by: Zach Bloomquist <git@chary.us>

--- system-tests/scripts/bootstrap-docker-container.sh
+++ system-tests/scripts/bootstrap-docker-container.sh
@@ -37,7 +37,7 @@ export npm_config_cache=/tmp/npm_config_cache/
 export npm_config_package_lock=false
 
 mkdir $npm_config_cache
-chown -R 1000:1000 $npm_config_cache
+chown -R $(id -u):$(id -g) $npm_config_cache
 
 npx npm@latest install --unsafe-perm --allow-root --force file:$CLI_PATH
 
