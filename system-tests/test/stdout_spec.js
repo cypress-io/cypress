53b99c471d3519b67a5996d276b190318db9b354
Rachel <Raquel.rudermano@gmail.com>
2022-09-09 01:26:23 -0700

    fix snapshots

--- system-tests/test/stdout_spec.js
+++ system-tests/test/stdout_spec.js
@@ -29,9 +29,9 @@ describe('e2e stdout', () => {
       expectedExitCode: 3,
       onStdout: (stdout) => {
         // assert stack trace line numbers/columns mirror source map
-        expect(stdout).to.include('Context.eval (webpack:///./cypress/e2e/stdout_failing.cy.js:7:13)')
-        expect(stdout).to.include('Context.eval (webpack:///./cypress/e2e/stdout_failing.cy.js:15:10)')
-        expect(stdout).to.include('Context.eval (webpack:///./cypress/e2e/stdout_failing.cy.js:27:10)')
+        expect(stdout).to.include('Context.eval (webpack:///./cypress/e2e/stdout_failing.cy.js:7:12)')
+        expect(stdout).to.include('Context.eval (webpack:///./cypress/e2e/stdout_failing.cy.js:15:9)')
+        expect(stdout).to.include('Context.eval (webpack:///./cypress/e2e/stdout_failing.cy.js:27:9)')
       },
     })
   })
