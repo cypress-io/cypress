de5aa453c4c9a5f66460b10332c044fd433aa907
astone123 <adams@cypress.io>
2022-09-09 21:23:52 +0000

    fix: force forward slash to backslash on Windows in spec search input

--- packages/app/src/specs/SpecsListHeader.vue
+++ packages/app/src/specs/SpecsListHeader.vue
@@ -68,6 +68,7 @@ import Button from '@cy/components/Button.vue'
 import Input from '@cy/components/Input.vue'
 import IconMagnifyingGlass from '~icons/cy/magnifying-glass_x16'
 import IconAdd from '~icons/cy/add-large_x16'
+import { getPathForPlatform } from '../paths'
 
 const { t } = useI18n()
 
@@ -91,7 +92,7 @@ const emit = defineEmits<{
 const onInput = (e: Event) => {
   const value = (e.target as HTMLInputElement).value
 
-  emit('update:modelValue', value)
+  emit('update:modelValue', getPathForPlatform(value))
 }
 
 const clearInput = (e: Event) => {
