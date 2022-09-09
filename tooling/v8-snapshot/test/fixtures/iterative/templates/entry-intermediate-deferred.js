f35b39c8a68d965a019cc548cf66362cd213708c
Ryan Manuel <ryanm@cypress.io>
2022-09-09 16:25:47 -0500

    Update tooling/v8-snapshot/test/fixtures/iterative/templates/entry-intermediate-deferred.js

--- tooling/v8-snapshot/test/fixtures/iterative/templates/entry-intermediate-deferred.js
+++ tooling/v8-snapshot/test/fixtures/iterative/templates/entry-intermediate-deferred.js
@@ -1,4 +1,4 @@
 exports.healthy = require('./healthy')
 exports.deferred = require('./deferred')
 exports.intermediate = require('./intermediate-deferred')
-exports.norwrite = require('./norewrite')
+exports.norewrite = require('./norewrite')
