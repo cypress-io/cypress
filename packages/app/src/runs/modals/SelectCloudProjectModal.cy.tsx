0c69a946ba7e2335398e7edf09422e815822b8df
Mark Noonan <mark@cypress.io>
2022-09-10 18:20:42 -0400

    make utm medium dynamic for new project creation

--- packages/app/src/runs/modals/SelectCloudProjectModal.cy.tsx
+++ packages/app/src/runs/modals/SelectCloudProjectModal.cy.tsx
@@ -25,7 +25,7 @@ describe('<SelectCloudProjectModal />', () => {
       },
       render (gql) {
         return (<div class="h-screen">
-          <SelectCloudProjectModal gql={gql}/>
+          <SelectCloudProjectModal utmMedium="test" gql={gql}/>
         </div>)
       },
     })
