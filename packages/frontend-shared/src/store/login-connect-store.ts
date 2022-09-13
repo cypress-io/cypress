d94df20b2ed57b98b2fdedc4c85736d16faad0d7
Mark Noonan <mark@cypress.io>
2022-09-13 17:15:23 -0400

    wip add status to store

--- packages/frontend-shared/src/store/login-connect-store.ts
+++ packages/frontend-shared/src/store/login-connect-store.ts
@@ -5,7 +5,13 @@ export interface LoginConnectState {
   utmMedium: string
   isLoggedIn: boolean
   isProjectConnected: boolean
+  isOrganizationLoaded: boolean
+  isMemberOfOrganization: boolean
+  hasNoRecordedRuns: boolean
+  error: boolean
 }
+
+export type UserStatus = 'isLoggedOut' | 'needsOrgConnect' | 'needsProjectConnect' | 'needsRecordedRun' | 'noActionableState'
 
 export const useLoginConnectStore = defineStore({
   id: 'loginConnect',
@@ -16,9 +22,12 @@ export const useLoginConnectStore = defineStore({
       utmMedium: '',
       isLoggedIn: false,
       isProjectConnected: false,
+      isOrganizationLoaded: false,
+      isMemberOfOrganization: false,
+      hasNoRecordedRuns: false,
+      error: false,
     }
   },
-
   actions: {
     openLoginConnectModal ({ utmMedium }: { utmMedium: string }) {
       this.isLoginConnectOpen = true
@@ -31,6 +40,27 @@ export const useLoginConnectStore = defineStore({
     },
     setStatus (name: 'isLoggedIn' | 'isProjectConnected', newVal: boolean) {
       this[name] = newVal
+    },
+  },
+  getters: {
+    userStatus (state) {
+      switch (true) {
+        // the switch here ensures the uniqueness of states as we don't allow duplicate case labels
+        // https://eslint.org/docs/latest/rules/no-duplicate-case
+        case !state.isLoggedIn:
+          return 'isLoggedOut'
+        case state.isLoggedIn && state.isOrganizationLoaded && !state.isMemberOfOrganization:
+          return 'needsOrgConnect'
+        case state.isLoggedIn && state.isMemberOfOrganization && !state.isProjectConnected:
+          return 'needsProjectConnect'
+        case state.isLoggedIn && state.isMemberOfOrganization && state.isProjectConnected && state.hasNoRecordedRuns:
+          return 'needsRecordedRun'
+        default:
+          return 'noActionableState'
+      }
+    },
+    userStatusMatches () {
+      return (status: UserStatus) => this.userStatus === status
     },
   },
 })
