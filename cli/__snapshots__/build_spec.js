03c71d900e9fda19593c1f4b6659e7b5c9e33617
Jordan <jordan@jpdesigning.com>
2022-09-15 16:26:03 -0400

    refactor: move linting to each lib to enable caching

--- cli/__snapshots__/build_spec.js
+++ cli/__snapshots__/build_spec.js
@@ -1,37 +1,37 @@
 exports['package.json build outputs expected properties 1'] = {
-  "name": "test",
-  "engines": "test engines",
-  "version": "x.y.z",
-  "buildInfo": "replaced by normalizePackageJson",
-  "description": "Cypress is a next generation front end testing tool built for the modern web",
-  "homepage": "https://github.com/cypress-io/cypress",
-  "license": "MIT",
-  "bugs": {
-    "url": "https://github.com/cypress-io/cypress/issues"
+  'name': 'test',
+  'engines': 'test engines',
+  'version': 'x.y.z',
+  'buildInfo': 'replaced by normalizePackageJson',
+  'description': 'Cypress is a next generation front end testing tool built for the modern web',
+  'homepage': 'https://github.com/cypress-io/cypress',
+  'license': 'MIT',
+  'bugs': {
+    'url': 'https://github.com/cypress-io/cypress/issues',
   },
-  "repository": {
-    "type": "git",
-    "url": "https://github.com/cypress-io/cypress.git"
+  'repository': {
+    'type': 'git',
+    'url': 'https://github.com/cypress-io/cypress.git',
   },
-  "keywords": [
-    "automation",
-    "browser",
-    "cypress",
-    "cypress.io",
-    "e2e",
-    "end-to-end",
-    "integration",
-    "component",
-    "mocks",
-    "runner",
-    "spies",
-    "stubs",
-    "test",
-    "testing"
+  'keywords': [
+    'automation',
+    'browser',
+    'cypress',
+    'cypress.io',
+    'e2e',
+    'end-to-end',
+    'integration',
+    'component',
+    'mocks',
+    'runner',
+    'spies',
+    'stubs',
+    'test',
+    'testing',
   ],
-  "types": "types",
-  "scripts": {
-    "postinstall": "node index.js --exec install",
-    "size": "t=\"$(npm pack .)\"; wc -c \"${t}\"; tar tvf \"${t}\"; rm \"${t}\";"
-  }
+  'types': 'types',
+  'scripts': {
+    'postinstall': 'node index.js --exec install',
+    'size': 't="$(npm pack .)"; wc -c "${t}"; tar tvf "${t}"; rm "${t}";',
+  },
 }
