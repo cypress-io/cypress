2a347938b87f9b093e54934db361600dd7c2b4cc
KHeo <sainthkh@naver.com>
2022-09-19 12:07:05 +0900

    Cleanup vite.config.mjs.

--- packages/driver/vite.config.mjs
+++ packages/driver/vite.config.mjs
@@ -1,25 +1,15 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-export default defineConfig({
-  define: {
-    // process: { env: {} },
-  },
-
+export const driverConfig: ReturnType<defineConfig> = {
   resolve: {
     alias: {
-      util: resolve('rollup-plugin-node-builtins/src/es6/util.js'),
-      stream: resolve('rollup-plugin-node-builtins/src/es6/stream.js'),
       buffer: resolve('buffer-es6'),
-      events: resolve('rollup-plugin-node-builtins/src/es6/events.js'),
       process: resolve('process-es6'),
-      path: resolve('rollup-plugin-node-builtins/src/es6/path.js'),
+      path: resolve('node_modules', 'rollup-plugin-node-builtins/src/es6/path.js'),
       setImmediate: resolve('rollup-plugin-node-builtins/src/es6/setImmediate.js'),
-      // 'mocha/lib/pending': resolve('mocha/lib/pending.js'),
-      mocha: resolve('mocha/mocha.js'),
     },
   },
-  optimizeDeps: {
-    // include: ['mocha/lib/pending.js'],
-  },
-})
+}
+
+export default defineConfig(driverConfig)
