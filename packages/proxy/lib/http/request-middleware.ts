27e639cf18286dcbb25e778cae5030d3813b9894
Bill Glesias <bglesias@gmail.com>
2022-09-19 14:54:49 -0400

    feat: fill out the ExtractCypressMetadataHeaders implementation

--- packages/proxy/lib/http/request-middleware.ts
+++ packages/proxy/lib/http/request-middleware.ts
@@ -3,6 +3,7 @@ import { blocked, cors } from '@packages/network'
 import { InterceptRequest } from '@packages/net-stubbing'
 import type { HttpMiddleware } from './'
 import { getSameSiteContext, addCookieJarCookiesToRequest } from './util/cookies'
+import { doesTopNeedToBeSimulated } from './util/top-simulation'
 
 // do not use a debug namespace in this file - use the per-request `this.debug` instead
 // available as cypress-verbose:proxy:http
@@ -23,6 +24,7 @@ const LogRequest: RequestMiddleware = function () {
 
 const ExtractCypressMetadataHeaders: RequestMiddleware = function () {
   this.req.isAUTFrame = !!this.req.headers['x-cypress-is-aut-frame']
+  const requestIsXhrOrFetch = this.req.headers['x-cypress-request']
 
   if (this.req.headers['x-cypress-is-aut-frame']) {
     delete this.req.headers['x-cypress-is-aut-frame']
@@ -33,6 +35,23 @@ const ExtractCypressMetadataHeaders: RequestMiddleware = function () {
     delete this.req.headers['x-cypress-request']
   }
 
+  if (!this.config.experimentalSessionAndOrigin ||
+    !doesTopNeedToBeSimulated(this) ||
+    // this should be unreachable happen as the x-cypress-request header is only attached if the resource type is 'xhr'
+    // inside the extension or electron equivalent. This should only be needed for defensive purposes
+    (requestIsXhrOrFetch !== 'true' && requestIsXhrOrFetch !== 'xhr' && requestIsXhrOrFetch !== 'fetch')) {
+    this.next()
+
+    return
+  }
+
+  this.debug(`looking up credentials for ${this.req.proxiedUrl}`)
+  let { resourceType, credentialStatus } = this.getCredentialLevelOfRequest(this.req.proxiedUrl, requestIsXhrOrFetch !== 'true' ? requestIsXhrOrFetch : undefined)
+
+  this.debug(`credentials calculated for ${resourceType}:${credentialStatus}`)
+
+  this.req.requestedWith = resourceType
+  this.req.credentialsLevel = credentialStatus
   this.next()
 }
 
