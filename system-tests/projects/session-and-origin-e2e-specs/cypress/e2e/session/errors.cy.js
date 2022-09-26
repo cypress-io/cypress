3b7a3975282509104be1dfe3119dbbedee7aeda8
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2022-09-26 12:35:49 -0500

    Update system-tests/projects/session-and-origin-e2e-specs/cypress/e2e/session/errors.cy.js
    Co-authored-by: Chris Breiding <chrisbreiding@users.noreply.github.com>

--- system-tests/projects/session-and-origin-e2e-specs/cypress/e2e/session/errors.cy.js
+++ system-tests/projects/session-and-origin-e2e-specs/cypress/e2e/session/errors.cy.js
@@ -3,6 +3,6 @@
  */
 it('setup has failing command', () => {
   cy.session('session_1', () => {
-    cy.get('does_not_exist', { timeout: 500 })
+    cy.get('does_not_exist', { timeout: 1 })
   })
 })
