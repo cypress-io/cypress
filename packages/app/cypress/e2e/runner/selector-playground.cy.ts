7284c3a26a169cb7619b400be54a157bda122020
KHeo <sainthkh@naver.com>
2022-09-26 09:53:27 +0900

    add test.

---
+++ packages/app/cypress/e2e/runner/selector-playground.cy.ts
@@ -0,0 +1,25 @@
+function launchApp () {
+  cy.scaffoldProject('selector-playground')
+  cy.openProject('selector-playground')
+  cy.startAppServer('e2e')
+  cy.visitApp()
+  cy.get(`[data-cy-row="spec.cy.js"]`).click()
+
+  cy.waitForSpecToFinish()
+}
+
+describe('selector playground', () => {
+  it('highlight the element when hover over it.', () => {
+    launchApp()
+
+    cy.get('[data-cy="playground-activator"]').click()
+
+    cy.getAutIframe().within(() => {
+      cy.get('h2').realHover()
+      cy.get('.__cypress-selector-playground').shadow().within(() => {
+        cy.get('.highlight').should('exist')
+        cy.get('.tooltip').should('have.text', '[data-cy="h2-contents"]')
+      })
+    })
+  })
+})
