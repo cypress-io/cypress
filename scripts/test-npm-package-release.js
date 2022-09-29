f8fd983144f22ade0b10ac9965fdd9a298e6105c
Chris Breiding <chrisbreiding@gmail.com>
2022-09-29 08:40:27 -0400

    chore: add test-npm-package-release script

---
+++ scripts/test-npm-package-release.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const execa = require('execa')
+
+const getNpmPackages = async () => {
+  const { stdout } = await execa('npx', ['lerna', 'la', '--json'])
+
+  const lernaPackages = JSON.parse(stdout)
+
+  // filter out private and binary packages
+  return lernaPackages.filter((pkg) => {
+    return !pkg.private && !pkg.name.includes('@packages')
+  })
+}
+
+const main = async () => {
+  const packages = await getNpmPackages()
+
+  for (const { name } of packages) {
+    console.log()
+    console.log('semantic-release dry run for:', name)
+
+    const { stdout } = await execa(
+      'npx',
+      ['lerna', 'exec', '--scope', name, '--', 'npx', '--no-install', 'semantic-release', '--dry-run'],
+    )
+
+    console.log(stdout)
+  }
+}
+
+// execute main function if called from command line
+if (require.main === module) {
+  main()
+}
