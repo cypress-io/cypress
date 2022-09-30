6509baa270df2471c958b1b8e70678b444a3fa05
Matt Schile <mschile@cypress.io>
2022-09-30 10:16:52 -0600

    updating snapshot

--- system-tests/__snapshots__/webpack_dev_server_fresh_spec.ts.js
+++ system-tests/__snapshots__/webpack_dev_server_fresh_spec.ts.js
@@ -19,7 +19,6 @@ exports['@cypress/webpack-dev-server react executes all of the tests for webpack
 ────────────────────────────────────────────────────────────────────────────────────────────────────
                                                                                                     
   Running:  App.cy.jsx                                                                      (1 of 7)
-ℹ ｢wdm｣: wait until bundle finished: /__cypress/src/index.html
 
 
   ✓ renders hello world
@@ -787,7 +786,6 @@ exports['@cypress/webpack-dev-server react executes all of the tests for webpack
 ────────────────────────────────────────────────────────────────────────────────────────────────────
                                                                                                     
   Running:  App.cy.jsx                                                                      (1 of 7)
-ℹ ｢wdm｣: wait until bundle finished: /__cypress/src/index.html
 
 
   ✓ renders hello world
