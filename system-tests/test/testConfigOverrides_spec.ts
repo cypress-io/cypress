7cf87423cecc560599be05d747d7cd273ba3dc4a
Emily Rohrbough <emilyrohrbough@yahoo.com>
2022-10-19 17:04:45 -0500

    fix system test

--- system-tests/test/testConfigOverrides_spec.ts
+++ system-tests/test/testConfigOverrides_spec.ts
@@ -16,6 +16,7 @@ describe('testConfigOverrides', () => {
     expectedExitCode: 0,
     browser: 'electron',
     config: {
+      experimentalSessionAndOrigin: true,
       video: false,
     },
   })
@@ -25,6 +26,7 @@ describe('testConfigOverrides', () => {
     snapshot: true,
     expectedExitCode: 1,
     config: {
+      experimentalSessionAndOrigin: true,
       video: false,
     },
   })
