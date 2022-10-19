6c38b35843c822d0335f7bde0f028bca293fedad
Ryan Manuel <ryanm@cypress.io>
2022-10-18 19:46:32 -0500

    Attempt at dynamic work

--- scripts/binary/binary-cleanup.js
+++ scripts/binary/binary-cleanup.js
@@ -5,151 +5,151 @@ const del = require('del')
 const esbuild = require('esbuild')
 const snapshotMetadata = require('@tooling/v8-snapshot/cache/prod-darwin/snapshot-meta.cache.json')
 
-const getEsbuildConfig = () => {
-  return {
-    entryPoints: [
-      './packages/server/lib/plugins/child/register_ts_node.js',
-      './packages/server/lib/plugins/child/require_async_child.js',
-      './packages/server/index.js',
-      './packages/server/hook-require.js',
-      require.resolve('ts-loader'),
-      require.resolve('babel-loader'),
-      require.resolve('@babel/plugin-transform-runtime'),
-      require.resolve('@babel/runtime/package'),
-      require.resolve('@babel/preset-env'),
-      require.resolve('@babel/preset-react'),
-      require.resolve('coffee-loader'),
-      require.resolve('typescript'),
-      require.resolve('@cypress/webpack-batteries-included-preprocessor'),
-      require.resolve('webpack'),
-      require.resolve('webpack/hot/only-dev-server'),
-      require.resolve('webpack/hot/dev-server'),
-      require.resolve('webpack-dev-server', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('webpack-dev-server/client/index', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('webpack-dev-server/client/clients/SockJSClient', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('webpack-dev-server/client/clients/WebSocketClient', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('terser-webpack-plugin/dist/minify', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('watchpack', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('html-webpack-plugin-4', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('html-webpack-plugin-5', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('html-webpack-plugin-5/lib/loader', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('buffer/'),
-      require.resolve('coffee-loader'),
-      './node_modules/worker-farm/lib/child/index.js',
-      './node_modules/webpack/buildin/global.js',
-      './node_modules/webpack/buildin/system.js',
-      require.resolve('node-libs-browser/mock/empty'),
-      './node_modules/ts-node/dist/transpilers/swc.js',
-      './node_modules/terser-webpack-plugin/dist/worker.js',
-      require.resolve('browserify-zlib'),
-      require.resolve('vm-browserify'),
-      require.resolve('url/'),
-      require.resolve('tty-browserify'),
-      require.resolve('timers-browserify'),
-      require.resolve('string_decoder/'),
-      require.resolve('readable-stream/writable.js'),
-      require.resolve('readable-stream/transform.js'),
-      require.resolve('readable-stream/readable.js'),
-      require.resolve('readable-stream/passthrough.js'),
-      require.resolve('readable-stream/duplex.js'),
-      require.resolve('stream-browserify'),
-      require.resolve('querystring-es3/'),
-      require.resolve('process/browser.js'),
-      require.resolve('util/util.js'),
-      require.resolve('punycode/'),
-      require.resolve('path-browserify'),
-      require.resolve('os-browserify/browser.js'),
-      require.resolve('https-browserify'),
-      require.resolve('stream-http'),
-      require.resolve('events/'),
-      require.resolve('domain-browser'),
-      require.resolve('crypto-browserify'),
-      require.resolve('constants-browserify'),
-      require.resolve('console-browserify'),
-      require.resolve('assert/'),
-      require.resolve('isexe'),
-      require.resolve('babel-plugin-add-module-exports'),
-      require.resolve('@babel/plugin-proposal-class-properties'),
-      require.resolve('@babel/plugin-proposal-object-rest-spread'),
-      require.resolve('lodash'),
-      ...['ibmi',
-        'sunos',
-        'android',
-        'darwin',
-        'freebsd',
-        'linux',
-        'openbsd',
-        'sunos',
-        'win32'].map((platform) => require.resolve(`default-gateway/${platform}`)),
-    ],
-    bundle: true,
-    outdir: 'out',
-    platform: 'node',
-    metafile: true,
-    external: [
-      './packages/server/server-entry',
-      'fsevents',
-      './npm/webpack-batteries-included-preprocessor/empty',
-      'coffee-loader',
-      '@babel/preset-react',
-      '@babel/preset-env',
-      '@babel/plugin-transform-runtime',
-      '@babel/runtime/package',
-      'babel-loader',
-      'ts-loader',
-      './child/index',
-      '../../buildin/global',
-      '../../buildin/system',
-      'node-libs-browser/mock/empty',
-      './transpilers/swc.js',
-      './worker',
-      'browserify-zlib',
-      'vm-browserify',
-      'url/',
-      'tty-browserify',
-      'timers-browserify',
-      'string_decoder/',
-      'readable-stream/writable.js',
-      'readable-stream/transform.js',
-      'readable-stream/readable.js',
-      'readable-stream/passthrough.js',
-      'readable-stream/duplex.js',
-      'stream-browserify',
-      'querystring-es3/',
-      'process/browser.js',
-      'util/util.js',
-      'punycode/',
-      'path-browserify',
-      'os-browserify/browser.js',
-      'https-browserify',
-      'stream-http',
-      'events/',
-      'domain-browser',
-      'crypto-browserify',
-      'constants-browserify',
-      'console-browserify',
-      'buffer/',
-      'esbuild',
-      '../../bin/coffee',
-      'assert/',
-      'babel-plugin-add-module-exports',
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-object-rest-spread',
-      './lib/loader.js',
-      'emitter',
-      'pnpapi',
-      '@swc/core',
-      '../client/index.js',
-      '../client/clients/SockJSClient',
-      '../client/clients/WebSocketClient',
-      'webpack/hot/only-dev-server',
-      'webpack/hot/dev-server',
-      'watchpack',
-      './minify',
-      './processChild',
-    ],
-  }
-}
+// const getEsbuildConfig = () => {
+//   return {
+//     entryPoints: [
+//       './packages/server/lib/plugins/child/register_ts_node.js',
+//       './packages/server/lib/plugins/child/require_async_child.js',
+//       './packages/server/index.js',
+//       './packages/server/hook-require.js',
+//       require.resolve('ts-loader'),
+//       require.resolve('babel-loader'),
+//       require.resolve('@babel/plugin-transform-runtime'),
+//       require.resolve('@babel/runtime/package'),
+//       require.resolve('@babel/preset-env'),
+//       require.resolve('@babel/preset-react'),
+//       require.resolve('coffee-loader'),
+//       require.resolve('typescript'),
+//       require.resolve('@cypress/webpack-batteries-included-preprocessor'),
+//       require.resolve('webpack'),
+//       require.resolve('webpack/hot/only-dev-server'),
+//       require.resolve('webpack/hot/dev-server'),
+//       require.resolve('webpack-dev-server', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('webpack-dev-server/client/index', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('webpack-dev-server/client/clients/SockJSClient', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('webpack-dev-server/client/clients/WebSocketClient', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('terser-webpack-plugin/dist/minify', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('watchpack', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('html-webpack-plugin-4', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('html-webpack-plugin-5', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('html-webpack-plugin-5/lib/loader', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+//       require.resolve('buffer/'),
+//       require.resolve('coffee-loader'),
+//       './node_modules/worker-farm/lib/child/index.js',
+//       './node_modules/webpack/buildin/global.js',
+//       './node_modules/webpack/buildin/system.js',
+//       require.resolve('node-libs-browser/mock/empty'),
+//       './node_modules/ts-node/dist/transpilers/swc.js',
+//       './node_modules/terser-webpack-plugin/dist/worker.js',
+//       require.resolve('browserify-zlib'),
+//       require.resolve('vm-browserify'),
+//       require.resolve('url/'),
+//       require.resolve('tty-browserify'),
+//       require.resolve('timers-browserify'),
+//       require.resolve('string_decoder/'),
+//       require.resolve('readable-stream/writable.js'),
+//       require.resolve('readable-stream/transform.js'),
+//       require.resolve('readable-stream/readable.js'),
+//       require.resolve('readable-stream/passthrough.js'),
+//       require.resolve('readable-stream/duplex.js'),
+//       require.resolve('stream-browserify'),
+//       require.resolve('querystring-es3/'),
+//       require.resolve('process/browser.js'),
+//       require.resolve('util/util.js'),
+//       require.resolve('punycode/'),
+//       require.resolve('path-browserify'),
+//       require.resolve('os-browserify/browser.js'),
+//       require.resolve('https-browserify'),
+//       require.resolve('stream-http'),
+//       require.resolve('events/'),
+//       require.resolve('domain-browser'),
+//       require.resolve('crypto-browserify'),
+//       require.resolve('constants-browserify'),
+//       require.resolve('console-browserify'),
+//       require.resolve('assert/'),
+//       require.resolve('isexe'),
+//       require.resolve('babel-plugin-add-module-exports'),
+//       require.resolve('@babel/plugin-proposal-class-properties'),
+//       require.resolve('@babel/plugin-proposal-object-rest-spread'),
+//       require.resolve('lodash'),
+//       ...['ibmi',
+//         'sunos',
+//         'android',
+//         'darwin',
+//         'freebsd',
+//         'linux',
+//         'openbsd',
+//         'sunos',
+//         'win32'].map((platform) => require.resolve(`default-gateway/${platform}`)),
+//     ],
+//     bundle: true,
+//     outdir: 'out',
+//     platform: 'node',
+//     metafile: true,
+//     external: [
+//       './packages/server/server-entry',
+//       'fsevents',
+//       './npm/webpack-batteries-included-preprocessor/empty',
+//       'coffee-loader',
+//       '@babel/preset-react',
+//       '@babel/preset-env',
+//       '@babel/plugin-transform-runtime',
+//       '@babel/runtime/package',
+//       'babel-loader',
+//       'ts-loader',
+//       './child/index',
+//       '../../buildin/global',
+//       '../../buildin/system',
+//       'node-libs-browser/mock/empty',
+//       './transpilers/swc.js',
+//       './worker',
+//       'browserify-zlib',
+//       'vm-browserify',
+//       'url/',
+//       'tty-browserify',
+//       'timers-browserify',
+//       'string_decoder/',
+//       'readable-stream/writable.js',
+//       'readable-stream/transform.js',
+//       'readable-stream/readable.js',
+//       'readable-stream/passthrough.js',
+//       'readable-stream/duplex.js',
+//       'stream-browserify',
+//       'querystring-es3/',
+//       'process/browser.js',
+//       'util/util.js',
+//       'punycode/',
+//       'path-browserify',
+//       'os-browserify/browser.js',
+//       'https-browserify',
+//       'stream-http',
+//       'events/',
+//       'domain-browser',
+//       'crypto-browserify',
+//       'constants-browserify',
+//       'console-browserify',
+//       'buffer/',
+//       'esbuild',
+//       '../../bin/coffee',
+//       'assert/',
+//       'babel-plugin-add-module-exports',
+//       '@babel/plugin-proposal-class-properties',
+//       '@babel/plugin-proposal-object-rest-spread',
+//       './lib/loader.js',
+//       'emitter',
+//       'pnpapi',
+//       '@swc/core',
+//       '../client/index.js',
+//       '../client/clients/SockJSClient',
+//       '../client/clients/WebSocketClient',
+//       'webpack/hot/only-dev-server',
+//       'webpack/hot/dev-server',
+//       'watchpack',
+//       './minify',
+//       './processChild',
+//     ],
+//   }
+// }
 
 async function removeEmptyDirectories (directory) {
   // lstat does not follow symlinks (in contrast to stat)
@@ -178,17 +178,109 @@ async function removeEmptyDirectories (directory) {
   }
 }
 
+const getDependencyPathsToRemove = async ({ entryPoints, external }) => {
+  const esbuildResult = await esbuild.build({
+    entryPoints,
+    bundle: true,
+    outdir: 'out',
+    platform: 'node',
+    metafile: true,
+    external,
+  })
+
+  const newEntryPoints = []
+  const newExternal = []
+
+  esbuildResult.warnings.forEach((warning) => {
+    const matches = warning.text.match(/"(.*)" should be marked as external for use with "require.resolve"/)
+    const warningSubject = matches && matches[1]
+
+    console.log(warningSubject)
+    console.log(warning.location.file)
+
+    if (warningSubject) {
+      let entryPoint
+
+      if (warningSubject.startsWith('.')) {
+        entryPoint = path.join(__dirname, '..', '..', path.dirname(warning.location.file), warningSubject)
+      } else {
+        entryPoint = require.resolve(warningSubject)
+      }
+
+      if (path.extname(entryPoint) !== '') {
+        newEntryPoints.push(entryPoint)
+      }
+
+      newExternal.push(warningSubject)
+    }
+  })
+
+  console.log('******* new entry points *******')
+  console.log([...entryPoints, ...newEntryPoints])
+  console.log('******* externals *******')
+  console.log([...external, ...newExternal])
+
+  if (newEntryPoints.length > 0) {
+    return getDependencyPathsToRemove({ entryPoints: [...entryPoints, ...newEntryPoints], external: [...external, ...newExternal] })
+  }
+
+  return [...Object.keys(esbuildResult.metafile.inputs), ...entryPoints]
+}
+
 const cleanup = async (buildAppDir) => {
-  const esbuildConfig = getEsbuildConfig()
-  const esbuildResult = await esbuild.build(esbuildConfig)
+  // const esbuildConfig = getEsbuildConfig()
+  // const esbuildResult = await esbuild.build(esbuildConfig)
+
+  const keptDependencies = await getDependencyPathsToRemove({
+    entryPoints: [
+      './packages/server/index.js',
+      './packages/server/hook-require.js',
+      './packages/server/lib/plugins/child/require_async_child.js',
+      './packages/server/lib/plugins/child/register_ts_node.js',
+      require.resolve('ts-loader'),
+      require.resolve('babel-loader'),
+      require.resolve('@babel/plugin-transform-runtime'),
+      require.resolve('@babel/runtime/package'),
+      require.resolve('@babel/preset-env'),
+      require.resolve('@babel/preset-react'),
+      require.resolve('coffee-loader'),
+      require.resolve('typescript'),
+      require.resolve('@cypress/webpack-batteries-included-preprocessor'),
+      require.resolve('babel-plugin-add-module-exports'),
+      require.resolve('@babel/plugin-proposal-class-properties'),
+      require.resolve('@babel/plugin-proposal-object-rest-spread'),
+      require.resolve('webpack'),
+      require.resolve('webpack-dev-server', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+      require.resolve('html-webpack-plugin-4', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+      require.resolve('html-webpack-plugin-5', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+      ...['ibmi',
+        'sunos',
+        'android',
+        'darwin',
+        'freebsd',
+        'linux',
+        'openbsd',
+        'sunos',
+        'win32'].map((platform) => require.resolve(`default-gateway/${platform}`)),
+    ],
+    external: [
+      './packages/server/server-entry',
+      'fsevents',
+      'pnpapi',
+      '@swc/core',
+      'emitter',
+    ],
+  })
 
   const potentiallyRemovedDependencies = [...snapshotMetadata.healthy, ...snapshotMetadata.deferred, ...snapshotMetadata.norewrite]
-  const keptDependencies = Object.keys(esbuildResult.metafile.inputs)
+  // const keptDependencies = Object.keys(esbuildResult.metafile.inputs)
+
+  console.log(potentiallyRemovedDependencies.length)
+  console.log(keptDependencies.length)
 
   await Promise.all(potentiallyRemovedDependencies.map(async (dependency) => {
-    if (dependency !== './package.json' && dependency !== './packages/server/server-entry.js' && !dependency.includes('marionette-client') && esbuildConfig.entryPoints.findIndex((externalDependency) => {
-      return dependency === externalDependency
-    }) === -1 && !keptDependencies.includes(dependency.slice(2))) {
+    if (![...keptDependencies, 'package.json', 'packages/server/server-entry.js'].includes(dependency.slice(2)) && !dependency.includes('marionette-client')) {
+      console.log('removing', dependency)
       await fs.remove(path.join(buildAppDir, dependency.replace(/.ts$/, '.js')))
     }
   }))
