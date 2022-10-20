9407d50d18984b4092a41f98cdad458e95b30e2f
Ryan Manuel <ryanm@cypress.io>
2022-10-19 21:18:42 -0500

    Clean up code

--- scripts/binary/binary-cleanup.js
+++ scripts/binary/binary-cleanup.js
@@ -4,152 +4,10 @@ const { consolidateDeps } = require('@tooling/v8-snapshot')
 const del = require('del')
 const esbuild = require('esbuild')
 const snapshotMetadata = require('@tooling/v8-snapshot/cache/prod-darwin/snapshot-meta.cache.json')
+const tempDir = require('temp-dir')
+const workingDir = path.join(tempDir, 'binary-cleanup-workdir')
 
-// const getEsbuildConfig = () => {
-//   return {
-//     entryPoints: [
-//       './packages/server/lib/plugins/child/register_ts_node.js',
-//       './packages/server/lib/plugins/child/require_async_child.js',
-//       './packages/server/index.js',
-//       './packages/server/hook-require.js',
-//       require.resolve('ts-loader'),
-//       require.resolve('babel-loader'),
-//       require.resolve('@babel/plugin-transform-runtime'),
-//       require.resolve('@babel/runtime/package'),
-//       require.resolve('@babel/preset-env'),
-//       require.resolve('@babel/preset-react'),
-//       require.resolve('coffee-loader'),
-//       require.resolve('typescript'),
-//       require.resolve('@cypress/webpack-batteries-included-preprocessor'),
-//       require.resolve('webpack'),
-//       require.resolve('webpack/hot/only-dev-server'),
-//       require.resolve('webpack/hot/dev-server'),
-//       require.resolve('webpack-dev-server', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('webpack-dev-server/client/index', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('webpack-dev-server/client/clients/SockJSClient', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('webpack-dev-server/client/clients/WebSocketClient', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('terser-webpack-plugin/dist/minify', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('watchpack', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('html-webpack-plugin-4', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('html-webpack-plugin-5', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('html-webpack-plugin-5/lib/loader', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-//       require.resolve('buffer/'),
-//       require.resolve('coffee-loader'),
-//       './node_modules/worker-farm/lib/child/index.js',
-//       './node_modules/webpack/buildin/global.js',
-//       './node_modules/webpack/buildin/system.js',
-//       require.resolve('node-libs-browser/mock/empty'),
-//       './node_modules/ts-node/dist/transpilers/swc.js',
-//       './node_modules/terser-webpack-plugin/dist/worker.js',
-//       require.resolve('browserify-zlib'),
-//       require.resolve('vm-browserify'),
-//       require.resolve('url/'),
-//       require.resolve('tty-browserify'),
-//       require.resolve('timers-browserify'),
-//       require.resolve('string_decoder/'),
-//       require.resolve('readable-stream/writable.js'),
-//       require.resolve('readable-stream/transform.js'),
-//       require.resolve('readable-stream/readable.js'),
-//       require.resolve('readable-stream/passthrough.js'),
-//       require.resolve('readable-stream/duplex.js'),
-//       require.resolve('stream-browserify'),
-//       require.resolve('querystring-es3/'),
-//       require.resolve('process/browser.js'),
-//       require.resolve('util/util.js'),
-//       require.resolve('punycode/'),
-//       require.resolve('path-browserify'),
-//       require.resolve('os-browserify/browser.js'),
-//       require.resolve('https-browserify'),
-//       require.resolve('stream-http'),
-//       require.resolve('events/'),
-//       require.resolve('domain-browser'),
-//       require.resolve('crypto-browserify'),
-//       require.resolve('constants-browserify'),
-//       require.resolve('console-browserify'),
-//       require.resolve('assert/'),
-//       require.resolve('isexe'),
-//       require.resolve('babel-plugin-add-module-exports'),
-//       require.resolve('@babel/plugin-proposal-class-properties'),
-//       require.resolve('@babel/plugin-proposal-object-rest-spread'),
-//       require.resolve('lodash'),
-//       ...['ibmi',
-//         'sunos',
-//         'android',
-//         'darwin',
-//         'freebsd',
-//         'linux',
-//         'openbsd',
-//         'sunos',
-//         'win32'].map((platform) => require.resolve(`default-gateway/${platform}`)),
-//     ],
-//     bundle: true,
-//     outdir: 'out',
-//     platform: 'node',
-//     metafile: true,
-//     external: [
-//       './packages/server/server-entry',
-//       'fsevents',
-//       './npm/webpack-batteries-included-preprocessor/empty',
-//       'coffee-loader',
-//       '@babel/preset-react',
-//       '@babel/preset-env',
-//       '@babel/plugin-transform-runtime',
-//       '@babel/runtime/package',
-//       'babel-loader',
-//       'ts-loader',
-//       './child/index',
-//       '../../buildin/global',
-//       '../../buildin/system',
-//       'node-libs-browser/mock/empty',
-//       './transpilers/swc.js',
-//       './worker',
-//       'browserify-zlib',
-//       'vm-browserify',
-//       'url/',
-//       'tty-browserify',
-//       'timers-browserify',
-//       'string_decoder/',
-//       'readable-stream/writable.js',
-//       'readable-stream/transform.js',
-//       'readable-stream/readable.js',
-//       'readable-stream/passthrough.js',
-//       'readable-stream/duplex.js',
-//       'stream-browserify',
-//       'querystring-es3/',
-//       'process/browser.js',
-//       'util/util.js',
-//       'punycode/',
-//       'path-browserify',
-//       'os-browserify/browser.js',
-//       'https-browserify',
-//       'stream-http',
-//       'events/',
-//       'domain-browser',
-//       'crypto-browserify',
-//       'constants-browserify',
-//       'console-browserify',
-//       'buffer/',
-//       'esbuild',
-//       '../../bin/coffee',
-//       'assert/',
-//       'babel-plugin-add-module-exports',
-//       '@babel/plugin-proposal-class-properties',
-//       '@babel/plugin-proposal-object-rest-spread',
-//       './lib/loader.js',
-//       'emitter',
-//       'pnpapi',
-//       '@swc/core',
-//       '../client/index.js',
-//       '../client/clients/SockJSClient',
-//       '../client/clients/WebSocketClient',
-//       'webpack/hot/only-dev-server',
-//       'webpack/hot/dev-server',
-//       'watchpack',
-//       './minify',
-//       './processChild',
-//     ],
-//   }
-// }
+fs.ensureDirSync(workingDir)
 
 async function removeEmptyDirectories (directory) {
   // lstat does not follow symlinks (in contrast to stat)
@@ -178,138 +36,152 @@ async function removeEmptyDirectories (directory) {
   }
 }
 
-const getDependencyPathsToRemove = async ({ entryPoints }) => {
-  const esbuildResult = await esbuild.build({
-    entryPoints,
-    bundle: true,
-    outdir: 'out',
-    platform: 'node',
-    metafile: true,
-    external: [
-      './packages/server/server-entry',
-      'fsevents',
-      'pnpapi',
-      '@swc/core',
-      'emitter',
-    ],
-  })
+const getDependencyPathsToKeep = async () => {
+  let entryPoints = new Set([
+    // This is the entry point for the server bundle. It will not have access to the snapshot yet. It needs to be kept in the binary
+    require.resolve('@packages/server/index.js'),
+    // This is a dynamic import that is used to load the snapshot require logic. It will not have access to the snapshot yet. They needs to be kept in the binary
+    require.resolve('@packages/server/hook-require.js'),
+    // These dependencies are started in a new process or thread and will not have access to the snapshot. It needs to be kept in the binary
+    require.resolve('@packages/server/lib/plugins/child/require_async_child.js'),
+    require.resolve('@packages/server/lib/plugins/child/register_ts_node.js'),
+    require.resolve('@packages/rewriter/lib/threads/worker.ts'),
+    // These dependencies use the `require.resolve(<dependency>, { paths: [<path>] })` pattern where <path> is a path within the cypress monorepo. These will not be
+    // pulled in by esbuild but still need to be kept in the binary.
+    require.resolve('webpack'),
+    require.resolve('webpack-dev-server', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+    require.resolve('html-webpack-plugin-4', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+    require.resolve('html-webpack-plugin-5', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
+    // These dependencies are completely dynamic using the pattern `require(`./${name}`)` and will not be pulled in by esbuild but still need to be kept in the binary.
+    ...['ibmi',
+      'sunos',
+      'android',
+      'darwin',
+      'freebsd',
+      'linux',
+      'openbsd',
+      'sunos',
+      'win32'].map((platform) => require.resolve(`default-gateway/${platform}`)),
+  ])
+  let esbuildResult
+  let newEntryPointsFound = true
 
-  const newEntryPoints = []
+  // The general idea here is to run esbuild with on entry points that are used outside of the snapshot. If, during the process,
+  // we find places where we do a require.resolve on a module, that should be treated as an additional entry point and we run
+  // esbuild again. We do this until we no longer find any new entry points. The resulting metafile inputs are
+  // the dependency paths that we need to ensure stay in the snapshot.
+  while (newEntryPointsFound) {
+    esbuildResult = await esbuild.build({
+      entryPoints: [...entryPoints],
+      bundle: true,
+      outdir: workingDir,
+      platform: 'node',
+      metafile: true,
+      external: [
+        './packages/server/server-entry',
+        'fsevents',
+        'pnpapi',
+        '@swc/core',
+        'emitter',
+      ],
+    })
 
-  esbuildResult.warnings.forEach((warning) => {
-    const matches = warning.text.match(/"(.*)" should be marked as external for use with "require.resolve"/)
-    const warningSubject = matches && matches[1]
+    newEntryPointsFound = false
+    esbuildResult.warnings.forEach((warning) => {
+      const matches = warning.text.match(/"(.*)" should be marked as external for use with "require.resolve"/)
+      const warningSubject = matches && matches[1]
 
-    console.log(warningSubject)
-    console.log(warning.location.file)
+      if (warningSubject) {
+        let entryPoint
 
-    if (warningSubject) {
-      let entryPoint
+        if (warningSubject.startsWith('.')) {
+          entryPoint = path.join(__dirname, '..', '..', path.dirname(warning.location.file), warningSubject)
+        } else {
+          entryPoint = require.resolve(warningSubject)
+        }
 
-      if (warningSubject.startsWith('.')) {
-        entryPoint = path.join(__dirname, '..', '..', path.dirname(warning.location.file), warningSubject)
-      } else {
-        entryPoint = require.resolve(warningSubject)
+        if (path.extname(entryPoint) !== '' && !entryPoints.has(entryPoint)) {
+          newEntryPointsFound = true
+          entryPoints.add(entryPoint)
+        }
       }
-
-      if (path.extname(entryPoint) !== '' && !entryPoints.includes(entryPoint)) {
-        newEntryPoints.push(entryPoint)
-      }
-    }
-  })
-
-  console.log('******* new entry points *******')
-  console.log([...entryPoints, ...newEntryPoints])
-
-  if (newEntryPoints.length > 0) {
-    return getDependencyPathsToRemove({ entryPoints: [...entryPoints, ...newEntryPoints] })
+    })
   }
 
   return [...Object.keys(esbuildResult.metafile.inputs), ...entryPoints]
 }
 
 const cleanup = async (buildAppDir) => {
-  // const esbuildConfig = getEsbuildConfig()
-  // const esbuildResult = await esbuild.build(esbuildConfig)
+  // 1. Retrieve all dependencies that still need to be kept in the binary. In theory, we could use the bundles generated here as single files within the binary,
+  // but for now, we just track on the dependencies that get pulled in
+  const keptDependencies = await getDependencyPathsToKeep()
 
-  const keptDependencies = await getDependencyPathsToRemove({
-    entryPoints: [
-      './packages/server/index.js',
-      './packages/server/hook-require.js',
-      './packages/server/lib/plugins/child/require_async_child.js',
-      './packages/server/lib/plugins/child/register_ts_node.js',
-      './packages/rewriter/lib/threads/worker.ts',
-      require.resolve('webpack'),
-      require.resolve('webpack-dev-server', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('html-webpack-plugin-4', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      require.resolve('html-webpack-plugin-5', { paths: [path.join(__dirname, '..', '..', 'npm', 'webpack-dev-server')] }),
-      ...['ibmi',
-        'sunos',
-        'android',
-        'darwin',
-        'freebsd',
-        'linux',
-        'openbsd',
-        'sunos',
-        'win32'].map((platform) => require.resolve(`default-gateway/${platform}`)),
-    ],
-  })
-
+  // 2. Gather the dependencies that could potentially be removed from the binary due to being in the snapshot
   const potentiallyRemovedDependencies = [...snapshotMetadata.healthy, ...snapshotMetadata.deferred, ...snapshotMetadata.norewrite]
-  // const keptDependencies = Object.keys(esbuildResult.metafile.inputs)
 
-  console.log(potentiallyRemovedDependencies.length)
-  console.log(keptDependencies.length)
-
+  // 3. Remove all dependencies that are in the snapshot but not in the list of kept dependencies from the binary
   await Promise.all(potentiallyRemovedDependencies.map(async (dependency) => {
     if (![...keptDependencies, 'package.json', 'packages/server/server-entry.js'].includes(dependency.slice(2)) && !dependency.includes('marionette-client')) {
-      console.log('removing', dependency)
       await fs.remove(path.join(buildAppDir, dependency.replace(/.ts$/, '.js')))
     }
   }))
 
+  // 4. Consolidate dependencies that are safe to consolidate (`lodash` and `bluebird`)
   await consolidateDeps({ projectBaseDir: buildAppDir })
+
+  // 5. Remove various unnecessary files from the binary to further clean things up. Likely, there is additional work that can be done here
   await del([
+    // Remove test files
     path.join(buildAppDir, '**', 'test'),
     path.join(buildAppDir, '**', 'tests'),
     // What we need of prettier is entirely encapsulated within the v8 snapshot, but has a few leftover large files
     path.join(buildAppDir, '**', 'prettier', 'esm'),
     path.join(buildAppDir, '**', 'prettier', 'standalone.js'),
     path.join(buildAppDir, '**', 'prettier', 'bin-prettier.js'),
+    // ESM files are mostly not needed currently
     path.join(buildAppDir, '**', '@babel', '**', 'esm'),
+    path.join(buildAppDir, '**', 'ramda', 'es'),
+    path.join(buildAppDir, '**', 'jimp', 'es'),
+    path.join(buildAppDir, '**', '@jimp', '**', 'es'),
+    path.join(buildAppDir, '**', 'nexus', 'dist-esm'),
+    path.join(buildAppDir, '**', '@graphql-tools', '**', '*.mjs'),
+    path.join(buildAppDir, '**', 'graphql', '**', '*.mjs'),
+    // We currently do not use any map files
     path.join(buildAppDir, '**', '*js.map'),
+    // License files need to be kept
     path.join(buildAppDir, '**', '!(LICENSE|license|License).md'),
+    // These are type related files that are not used within the binary
     path.join(buildAppDir, '**', '*.d.ts'),
+    path.join(buildAppDir, '**', 'ajv', 'lib', '**', '*.ts'),
     path.join(buildAppDir, '**', '*.flow'),
+    // Example files are not needed
     path.join(buildAppDir, '**', 'jimp', 'browser', 'examples'),
+    // Documentation files are not needed
     path.join(buildAppDir, '**', 'JSV', 'jsdoc-toolkit'),
     path.join(buildAppDir, '**', 'JSV', 'docs'),
     path.join(buildAppDir, '**', 'fluent-ffmpeg', 'doc'),
+    // Files used as part of prebuilding are not necessary
     path.join(buildAppDir, '**', 'registry-js', 'prebuilds'),
-    path.join(buildAppDir, '**', 'ramda', 'es'),
-    path.join(buildAppDir, '**', 'ramda', 'dist'),
     path.join(buildAppDir, '**', '*.cc'),
     path.join(buildAppDir, '**', '*.o'),
     path.join(buildAppDir, '**', '*.c'),
     path.join(buildAppDir, '**', '*.h'),
+    // Remove distributions that are not needed in the binary
+    path.join(buildAppDir, '**', 'ramda', 'dist'),
     path.join(buildAppDir, '**', 'jimp', 'browser'),
-    path.join(buildAppDir, '**', 'jimp', 'es'),
-    path.join(buildAppDir, '**', '@jimp', '**', 'es'),
     path.join(buildAppDir, '**', '@jimp', '**', 'src'),
     path.join(buildAppDir, '**', 'nexus', 'src'),
-    path.join(buildAppDir, '**', 'nexus', 'dist-esm'),
     path.join(buildAppDir, '**', 'source-map', 'dist'),
     path.join(buildAppDir, '**', 'source-map-js', 'dist'),
-    path.join(buildAppDir, '**', 'yarn.lock'),
     path.join(buildAppDir, '**', 'pako', 'dist'),
-    path.join(buildAppDir, '**', 'ajv', 'lib', '**', '*.ts'),
     path.join(buildAppDir, '**', 'node-forge', 'dist'),
-    path.join(buildAppDir, '**', '@graphql-tools', '**', '*.mjs'),
-    path.join(buildAppDir, '**', 'graphql', '**', '*.mjs'),
     path.join(buildAppDir, '**', 'pngjs', 'browser.js'),
     path.join(buildAppDir, '**', 'plist', 'dist'),
+    // Remove yarn locks
+    path.join(buildAppDir, '**', 'yarn.lock'),
   ], { force: true })
 
+  // 6. Remove any empty directories as a result of the rest of the cleanup
   await removeEmptyDirectories(buildAppDir)
 }
 
