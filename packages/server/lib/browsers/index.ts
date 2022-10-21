b0b55e0f29152e535127197c9f66f7aca94f2b81
Matt Henkes <mjhenkes@gmail.com>
2022-10-21 10:31:10 -0500

    Apply suggestions from code review
    Co-authored-by: Ryan Manuel <ryanm@cypress.io>

--- packages/server/lib/browsers/index.ts
+++ packages/server/lib/browsers/index.ts
@@ -171,7 +171,7 @@ export = {
       // We want to avoid adding signals in here that may intentionally be caused by a user.
       // For example exiting firefox through either force quitting or quitting via cypress will fire a 'SIGTERM' event which
       // would result in constantly relaunching the browser when the user actively wants to quit.
-      // On windows the crash produces 2147483651 as an exit node. We should add to the list of crashes we handle as we see them.
+      // On windows the crash produces 2147483651 as an exit code. We should add to the list of crashes we handle as we see them.
       // In the future we may consider delegating to the browsers to determine if an exit is a crash since it might be different
       // depending on what browser has crashed.
       if (code === null && ['SIGTRAP', 'SIGABRT'].includes(signal) || code === 2147483651 && signal === null) {
