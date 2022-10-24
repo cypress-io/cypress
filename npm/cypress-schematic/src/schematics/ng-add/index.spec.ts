5537c82044c3931d3ed2ef5a3e9edc2de5526605
Waldemar Penner <waldemar.penner91@gmail.com>
2022-10-24 16:52:15 +0200

    feat: add @cypress/schematic to schematicCollections

--- npm/cypress-schematic/src/schematics/ng-add/index.spec.ts
+++ npm/cypress-schematic/src/schematics/ng-add/index.spec.ts
@@ -3,6 +3,7 @@
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing'
 import { join } from 'path'
 import { expect } from 'chai'
+import { JsonObject } from '@angular-devkit/core'
 
 describe('@cypress/schematic: ng-add', () => {
   const schematicRunner = new SchematicTestRunner(
@@ -16,6 +17,7 @@ describe('@cypress/schematic: ng-add', () => {
     name: 'workspace',
     newProjectRoot: 'projects',
     version: '6.0.0',
+    packageManager: 'yarn',
   }
 
   const appOptions = {
@@ -24,6 +26,10 @@ describe('@cypress/schematic: ng-add', () => {
     routing: false,
     skipTests: false,
     skipPackageJson: false,
+  }
+
+  const readAngularJson = (tree: UnitTestTree) => {
+    return tree.readJson('/angular.json') as JsonObject
   }
 
   beforeEach(async () => {
@@ -56,6 +62,37 @@ describe('@cypress/schematic: ng-add', () => {
       expect(files).to.contain('/projects/sandbox/cypress/tsconfig.json')
       expect(files).to.contain('/projects/sandbox/cypress.config.ts')
       expect(files).to.contain('/projects/sandbox/cypress/fixtures/example.json')
+    })
+  })
+
+  it('should add @cypress/schematic to the schemaCollections array', async () => {
+    const tree = await schematicRunner.runSchematicAsync('ng-add', { 'component': true }, appTree).toPromise()
+    const angularJson = readAngularJson(tree)
+    const cliOptions = angularJson.cli as JsonObject
+
+    expect(cliOptions).to.eql({
+      packageManager: 'yarn',
+      schematicCollections: ['@cypress/schematic', '@schematics/angular'],
+    })
+  })
+
+  it('should not overwrite existing schemaCollections array', async () => {
+    let angularJson = readAngularJson(appTree)
+
+    appTree.overwrite('./angular.json', JSON.stringify({
+      ...angularJson,
+      cli: {
+        schematicCollections: ['@any/schematic'],
+      },
+    }))
+
+    const tree = await schematicRunner.runSchematicAsync('ng-add', { 'component': true }, appTree).toPromise()
+
+    angularJson = readAngularJson(tree)
+    const cliOptions = angularJson.cli as JsonObject
+
+    expect(cliOptions).to.eql({
+      schematicCollections: ['@cypress/schematic', '@any/schematic', '@schematics/angular'],
     })
   })
 })
