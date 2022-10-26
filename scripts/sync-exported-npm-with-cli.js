37f7708dea70b4b9ba985d3536ec5415d00a5516
Zachary Williams <zachjw34@gmail.com>
2022-10-26 16:32:17 -0500

    fix: remove dependence on @cypress/<dep> types

--- scripts/sync-exported-npm-with-cli.js
+++ scripts/sync-exported-npm-with-cli.js
@@ -29,6 +29,8 @@ packlist({ path: currentPackageDir })
   const exportName = currentPackageConfig.name.replace('@cypress/', '')
   const outDir = path.join(cliPath, exportName)
 
+  fs.removeSync(outDir)
+
   // 3. For each file, mkdir if not exists, and then copy the dist'd assets over
   // to write to the `cliPackageConfig` at the end
   files.forEach((f) => {
