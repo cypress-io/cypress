abb607d25f1af0a98795758a1b2692ace94a3c9e
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-11-14 12:43:32 +1000

    new branch from cypress 11

--- cli/lib/exec/spawn.js
+++ cli/lib/exec/spawn.js
@@ -172,6 +172,12 @@ module.exports = {
 
         if (stdioOptions.env.ELECTRON_RUN_AS_NODE) {
           // Since we are running electron as node, we need to add an entry point file.
+          if (isPlatform('darwin')) {
+            startScriptPath = path.join(state.getBinaryPkgPath(path.join(executable, '..', '..', '..')), '..', 'index.js')
+          } else {
+            startScriptPath = path.join(state.getBinaryPkgPath(path.dirname(executable)), '..', 'index.js')
+          }
+
           startScriptPath = path.join(state.getBinaryPkgPath(path.dirname(executable)), '..', 'index.js')
         } else {
           // Start arguments with "--" so Electron knows these are OUR
