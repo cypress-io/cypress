21e2ef1925e72da20d39f97b14bebc8d7a5f92b7
Ankit <ankit@cypress.io>
2022-11-16 10:57:26 -0500

    upper portion of header complete

--- packages/app/src/debug/DebugPageHeader.cy.tsx
+++ packages/app/src/debug/DebugPageHeader.cy.tsx
@@ -1,6 +1,10 @@
 import DebugPageHeader from './DebugPageHeader.vue'
 
-describe('<DebugPageHeader />', () => {
+describe('<DebugPageHeader />', {
+  viewportHeight: 1000,
+  viewportWidth: 1032,
+},
+() => {
   it('mounts the debug header component', () => {
     cy.mount(DebugPageHeader)
   })
--- packages/app/src/debug/DebugPageHeader.vue
+++ packages/app/src/debug/DebugPageHeader.vue
@@ -5,39 +5,63 @@
   >
     <div
       :data-cy="`debug-header-${run.id}`"
-      class="w-full px-24px block overflow-hidden flex items-center border border-color-red gap-y-8"
+      class="grid px-24px w-full overflow-hidden flex items-center border gap-y-2 py-24px"
     >
-      <div
+      <ul
         :data-cy="'header-top'"
-        class="flex flex-row self-stretch items-center gap-x-2"
+        class="flex self-stretch items-center gap-x-2 flex-row whitespace-nowrap"
       >
-        <span
-          class="font-medium text-lg text-gray-900"
-        >
-          {{ test.name }}
-        </span>
-        <div class="flex flex-row justify-center items-center border rounded border-gray-100">
-          <span
-            class=" text-sm font-medium text-gray-700 border-r-1px"
-          >
+        <li class="font-medium text-lg text-gray-900">
+          <span class="sr-only">Test Name:</span> {{ test.name }}
+        </li>
+        <div class="border rounded border-gray-100 items-center text-sm h-6">
+          <span class="font-medium text-gray-700 border-r-1px px-2 mx-px">
             Run #468
           </span>
-          <span
-            class="font-normal text-sm text-orange-500"
-          >
+          <span class="font-normal text-orange-500 px-2 mx-px">
             You are 2 commits ahead
           </span>
         </div>
-      </div>
-      <div
+        <li class="-mt-8px text-lg text-gray-400">
+          .
+        </li>
+        <li class="font-normal text-sm text-indigo-500">
+          <span class="sr-only">Dashboard Link:</span> View in the dashboard
+        </li>
+      </ul>
+      <ul
         :data-cy="'metadata'"
-        class="flex flex-row gap-x-2 items-center"
-      />
+        class="flex flex-wrap gap-x-2 items-center"
+      >
+        <ResultCounts
+          :total-passed="1"
+          :total-failed="7"
+          :total-skipped="6"
+          :total-pending="6"
+        />
+      </ul>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { computed } from 'vue'
+import ResultCounts from '@packages/frontend-shared/src/components/ResultCounts.vue'
+// import type { RunResultsFragment } from '../generated/graphql'
+import { gql } from '@urql/core'
+
+gql`
+fragment RunResults on CloudRun {
+  id
+  totalPassed
+  totalFailed
+  totalPending
+  totalSkipped
+  totalFlakyTests
+}
+`
+// const props = defineProps<{
+//   gql: RunResultsFragment
+// }>()
 
 const run = computed(() => {
   return ({ id: 1, url: 'something.cypress.com' })
@@ -48,3 +72,6 @@ const test = computed(() => {
 })
 
 </script>
+<style scoped>
+
+</style>
