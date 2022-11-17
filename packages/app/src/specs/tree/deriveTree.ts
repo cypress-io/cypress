d520d9d189a6c879d5353e84bda21155c142472d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-11-17 16:27:54 +1000

    vastly improve tree building algorithm [skip ci]

--- packages/app/src/specs/tree/deriveTree.ts
+++ packages/app/src/specs/tree/deriveTree.ts
@@ -1,16 +1,16 @@
-import fuzzysort from 'fuzzysort'
+import fuzzysort from "fuzzysort";
 
-type SearchFn<T extends BaseSpec> = (search: string, specs: T[]) => T[]
+type SearchFn<T extends BaseSpec> = (search: string, specs: T[]) => T[];
 
 export interface SpecListOptions<T extends BaseSpec> {
   /** either / for posix for \\ for windows */
-  sep?: string
+  sep?: string;
 
-  /** string to filter by. */
+  /** String to filter by. Passed to the searchFn. */
   search?: string;
 
-  /** sera */
-  searchFn?: SearchFn<T>
+  /** Search function. Defaults to a basic string filter. */
+  searchFn?: SearchFn<T>;
 
   /** Relative paths for all collapsed directories */
   collapsedDirs: Set<string>;
@@ -20,12 +20,22 @@ export interface SpecTreeDirectoryNode<T extends BaseSpec> {
   name: string;
   type: "directory";
   relative: string;
+  /**
+   * Only ever null for the root node.
+   */
+  parentPath: string | null;
   parent: SpecTreeDirectoryNode<T> | null;
-  /** This is technical derived data.
-   *  It's the count of parent.parent.parent... but we cache for perf. reasons.
+  /** 
+   * How deep is the node? Derive via parent.parent.parent...
    */
   depth: number;
-  children: Map<string, SpecTreeNode<T>>;
+  /**
+   * A directory can have other directories or files as children.
+   */
+  children: Array<SpecTreeNode<T>>;
+  /**
+   * Is it collapsed? This is purely a UI concern, but we manage it here for simplicity.
+   */
   collapsed: boolean;
 }
 
@@ -34,14 +44,14 @@ export interface SpecTreeFileNode<T extends BaseSpec> {
   name: string;
   data: T;
   parent: SpecTreeDirectoryNode<T>;
+  parentPath: string;
 }
 
-export type SpecTreeNode<T extends BaseSpec> = SpecTreeFileNode<T> | SpecTreeDirectoryNode<T>;
+export type SpecTreeNode<T extends BaseSpec> =
+  | SpecTreeFileNode<T>
+  | SpecTreeDirectoryNode<T>;
 
-type DirectoryMap<T extends BaseSpec> = Map<
-  string,
-  { node: SpecTreeDirectoryNode<T> }
->;
+type DirectoryMap<T extends BaseSpec> = Map<string, SpecTreeDirectoryNode<T>>;
 
 function splitIntoParts(
   path: string,
@@ -58,9 +68,11 @@ function splitIntoParts(
   return { name, path: p };
 }
 
-type BaseSpec = { relative: string }
+type BaseSpec = { relative: string };
 
-export function filterFileNodes<T extends BaseSpec>(node: SpecTreeNode<T>): node is SpecTreeFileNode<T> {
+export function filterFileNodes<T extends BaseSpec>(
+  node: SpecTreeNode<T>
+): node is SpecTreeFileNode<T> {
   return node.type === "file";
 }
 
@@ -70,12 +82,10 @@ export function filterDirectoryNodes<T extends BaseSpec>(
   return node.type === "directory";
 }
 
-
-function isRootLevelSpec(path: string, name: string) {
-  return path === name
-}
-
-export function basicSearch<T extends BaseSpec>(search: string, specs: T[]): T[]  {
+export function basicSearch<T extends BaseSpec>(
+  search: string,
+  specs: T[]
+): T[] {
   return specs.filter((x) => x.relative.includes(search));
 }
 
@@ -104,6 +114,19 @@ export function fuzzySortSpecs<T extends BaseSpec>(
   return fuzzySortResult;
 }
 
+function getParentPath(path: string, candidates: string[]) {
+  return candidates.reduce<string>((acc, curr) => {
+    if (
+      path.includes(curr) &&
+      path.indexOf(curr) === 0 &&
+      curr.length > acc.length
+    ) {
+      return curr;
+    }
+    return acc;
+  }, candidates[0]);
+}
+
 export function deriveSpecTree<T extends BaseSpec>(
   allSpecs: T[],
   options?: Partial<SpecListOptions<T>>
@@ -111,178 +134,144 @@ export function deriveSpecTree<T extends BaseSpec>(
   root: SpecTreeDirectoryNode<T>;
   map: DirectoryMap<T>;
 } {
-
   const sep = options?.sep ?? "/";
   const search = options?.search ?? null;
-  const searchFn = options?.searchFn ?? basicSearch
-  const specs = search
-    ? searchFn(search, allSpecs)
-    : allSpecs;
+  const searchFn = options?.searchFn ?? basicSearch;
+
+  let specs = search ? searchFn(search, allSpecs) : allSpecs;
+
+  specs.sort((x, y) => {
+    const a = x.relative.split(sep).length;
+    const b = y.relative.split(sep).length;
+    return a > b ? 1 : -1;
+  });
 
   const root: SpecTreeDirectoryNode<T> = {
     type: "directory",
     relative: "/",
     name: "/",
-    depth: 0,
     parent: null,
-    collapsed: options?.collapsedDirs?.has('/') ?? false,
-    children: new Map(),
+    parentPath: null,
+    depth: 0,
+    collapsed: options?.collapsedDirs?.has("/") ?? false,
+    children: [],
   };
 
+  const map: DirectoryMap<T> = new Map([["/", root]]);
 
-  const map: DirectoryMap<T> = new Map();
-  const dirNodes: Map<string, SpecTreeDirectoryNode<T>> = new Map();
-
-  map.set("/", { node: root });
-  dirNodes.set("/", root);
-
-  // Derive all the directories based on the specs.
+  // Get all directories that contain a spec as the immediate child.
+  // Eg given:
+  // foo/1.ts
+  // foo/bar/qux/2.ts
+  // The directories are:
+  // - foo
+  //    - 1.ts
+  // - foo/bar/qux (parent is foo)
+  //    - 2.ts
+  // This is because in the UI, we do **not** show empty directories. We flatten them.
   for (const spec of specs) {
-    const { path, name } = splitIntoParts(spec.relative, sep);
+    const { path } = splitIntoParts(spec.relative, sep);
 
-    const acc: string[] = [];
-    const parts = path.split(sep);
-
-    // Root level spec - no such directory to add, root is already added.
-    const isRootSpec = isRootLevelSpec(path, name)
-
-    if (isRootSpec) {
+    if (map.get(path)) {
       continue;
     }
 
-    for (let i = 0; i < parts.length; i++) {
-      acc.push(parts[i]);
-      const dirName = acc.join(sep);
+    const parentPath = getParentPath(path, Array.from(map.keys()));
+    const parentNode = map.get(parentPath);
 
-      // Since we add each directory in a depth first fashion,
-      // the "parent" will always exist, except in the case of directores
-      // under `/`, in which case we just default to root.
-      // eg for `cypress/e2e/foo.cy.ts
-      //
-      // Loop 0: n=undefined, dirName=cypress
-      // -> n=undefined, so use root as `parent`
-      // -> dirNodes.set("cypress", {...})
-      //
-      // Loop 1: dirName="cypress"
-      // -> parent = dirNodes.get("cypress") // exists!
-      // currentDir.parent = parent
-      // ... etc ...
-
-      const n = acc.slice(0, acc.length - 1).join(sep);
-      const existing = dirNodes.get(n);
-      const parent = existing ?? root;
-
-      let depth = 1
-      let p: SpecTreeDirectoryNode<T> | null = parent
-      while (p = p.parent) {
-        depth++
-      }
-
-      const node: SpecTreeDirectoryNode<T> = {
-        type: "directory",
-        relative: dirName,
-        name: parts[i],
-        parent,
-        depth,
-        children: new Map(),
-        collapsed: options?.collapsedDirs?.has(dirName) ?? false,
-      };
-
-      dirNodes.set(dirName, node);
-      map.set(acc.join(sep), { node });
-    }
-  }
-
-  const recursivelyAssignDirectories = (node: SpecTreeDirectoryNode<T>) => {
-    if (node.relative === 'cypress') {
-    }
-    if (node.parent) {
-      map.get(node.parent.relative)?.node.children.set(node.relative, node);
-      recursivelyAssignDirectories(node.parent);
-    }
-  };
-
-  // Next, we assign the children directories for each directory.
-  // We recurse *up* until parent is null, which means which reached the root.
-  // Mutating becuase it's way faster and easier to grok.
-  for (const childNode of dirNodes.values()) {
-    recursivelyAssignDirectories(childNode);
-  }
-
-  // Add specs to directory's children.
-  for (const spec of specs) {
-    let { name, path } = splitIntoParts(spec.relative, sep);
-    path ||= "/";
-
-    const parent = isRootLevelSpec(spec.relative, name)
-      ? root
-      : map.get(path)?.node;
-    
-    if (!parent) {
+    if (!parentNode) {
       throw Error(
-        `Could not find directory node with key '${path}'. This should never happen.`
+        `Could not find node with id ${parentPath} in map. Should be impossible.`
       );
     }
 
-    // Finally, add the spec to the correct directory.
-    const fileNode: SpecTreeFileNode<T> = {
-      name,
-      type: "file",
-      data: spec,
-      parent,
-    };
+    const newDirName =
+      parentPath === "/" ? path : path.slice(parentPath.length + sep.length);
 
-    parent.children.set(name, fileNode);
+    const node: SpecTreeDirectoryNode<T> = {
+      type: "directory",
+      name: newDirName,
+      relative: path,
+      parent: parentNode,
+      parentPath,
+      depth: parentNode.depth + 1,
+      children: [],
+      collapsed: options?.collapsedDirs?.has(path) ?? false,
+    };
+    parentNode.children.push(node);
+    map.set(path, node);
   }
 
-  const rootNode = map.get("/")?.node;
+  // Now we know all the directories and their parent,
+  // place each spec in the correct directory.
+  for (const spec of specs) {
+    const { path, name } = splitIntoParts(spec.relative, sep);
+    const parent = map.get(path);
 
-  if (!rootNode) {
-    throw Error("Could not find root node");
+    if (!parent) {
+      throw Error(
+        `Could not find node with id ${path} in map. Should be impossible.`
+      );
+    }
+
+    const fileNode: SpecTreeFileNode<T> = {
+      name,
+      parentPath: path,
+      parent,
+      type: "file",
+      data: spec,
+    };
+
+    parent.children.push(fileNode);
   }
 
   return {
-    root: rootNode,
+    root,
     map,
   };
 }
 
-interface GroupedNodes<T extends {relative: string}> {
-  files: SpecTreeFileNode<T>[]
-  directories: SpecTreeDirectoryNode<T>[]
+interface GroupedNodes<T extends { relative: string }> {
+  files: SpecTreeFileNode<T>[];
+  directories: SpecTreeDirectoryNode<T>[];
 }
 
-export function groupSpecTreeNodes<T extends BaseSpec> (node: SpecTreeDirectoryNode<T>): GroupedNodes<T> {
-  return array(node.children).reduce<GroupedNodes<T>>((acc, curr) => {
-    if (curr.type === 'file') {
-      acc.files.push(curr)
-    } else {
-      acc.directories.push(curr)
+export function groupSpecTreeNodes<T extends BaseSpec>(
+  node: SpecTreeDirectoryNode<T>
+): GroupedNodes<T> {
+  return node.children.reduce<GroupedNodes<T>>(
+    (acc, curr) => {
+      if (curr.type === "file") {
+        acc.files.push(curr);
+      } else {
+        acc.directories.push(curr);
+      }
+      return acc;
+    },
+    {
+      files: [],
+      directories: [],
     }
-    return acc
-  }, {
-    files: [],
-    directories: []
-  });
+  );
 }
 
-export function array<T>(list: Set<T> | Map<string, T>): T[] {
-  return Array.from(list.values());
-}
+export function getAllFileInDirectory<T extends BaseSpec>(
+  node: SpecTreeDirectoryNode<T>,
+  { depth }: { depth: number } = { depth: 9999 }
+): SpecTreeFileNode<T>[] {
+  const files: SpecTreeFileNode<T>[] = [];
 
-export function getAllFileInDirectory<T extends BaseSpec>(node: SpecTreeDirectoryNode<T>): SpecTreeFileNode<T>[] {
-  const files: SpecTreeFileNode<T>[] = []
-
-  function walk (node: SpecTreeNode<T>) {
-    if (node.type === 'file') {
-      files.push(node)
+  function walk(node: SpecTreeNode<T>) {
+    if (node.type === "file") {
+      files.push(node);
     } else {
-      // continue...
-      Array.from(node.children.values()).map(n => walk(n))
+      Array.from(node.children.values()).map((n) => walk(n));
     }
   }
 
-  walk(node)
+  walk(node);
 
-  return files
+  return files.filter(
+    (x) => x.parent.depth >= node.depth && x.parent.depth < node.depth + depth
+  );
 }
