4107a2a20f1f70143eff93acccaf52757c1d43b4
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-11-23 11:57:24 +1000

    add missing files [run ci]

---
+++ packages/app/cypress/e2e/runner/support/mochaEventsUtils.ts
@@ -0,0 +1,187 @@
+import type { CypressInCypressMochaEvent } from '../../../../src/runner/event-manager'
+import _ from 'lodash'
+import type { MochaLifecycleData, SanitizedMochaLifecycleData } from './mochaTypes'
+import EventEmitter from 'events'
+import disparity from 'disparity'
+
+const hooks = {
+  before: ['before all', 'before each'],
+  after: ['after each', 'after all'],
+} as const
+
+const stringifyShort = (obj: Record<string, any>) => {
+  const constructorName = _.get(obj, 'constructor.name')
+
+  if (constructorName && !_.includes(['Object', 'Array'], constructorName)) {
+    return `{${constructorName}}`
+  }
+
+  if (_.isArray(obj)) {
+    return `[Array ${obj.length}]`
+  }
+
+  if (_.isObject(obj)) {
+    return `{Object ${Object.keys(obj).length}}`
+  }
+
+  return obj
+}
+
+const eventCleanseMap = {
+  snapshots: stringifyShort,
+  parent: stringifyShort,
+  tests: stringifyShort,
+  commands: stringifyShort,
+  invocationDetails: stringifyShort,
+  body: () => '[body]',
+  wallClockStartedAt: () => 'match.date',
+  lifecycle: () => 'match.number',
+  fnDuration: () => 'match.number',
+  duration: () => 'match.number',
+  afterFnDuration: () => 'match.number',
+  wallClockDuration: () => 'match.number',
+  stack: () => 'match.string',
+  file: (arg: string) => arg ? 'relative/path/to/spec.js' : undefined,
+  message: () => '[error message]',
+  parsedStack: () => 'match.array',
+  name: (n: string) => n === 'Error' ? 'AssertionError' : n,
+  err: () => {
+    return {
+      message: '[error message]',
+      name: 'AssertionError',
+      stack: 'match.string',
+      parsedStack: 'match.array',
+    }
+  },
+  startTime: new Date(0),
+  start: () => 'match.date',
+  end: () => 'match.date',
+  timings: (arg: MochaLifecycleData): SanitizedMochaLifecycleData => {
+    let sanitizedLifecycleData: SanitizedMochaLifecycleData = {
+      lifecycle: 'match.number',
+    }
+
+    for (const hook of hooks.before) {
+      const hooksToSanitize = arg[hook]
+
+      if (!hooksToSanitize) {
+        continue
+      }
+
+      sanitizedLifecycleData[hook] = hooksToSanitize.map((oldHook) => {
+        return {
+          hookId: oldHook.hookId,
+          fnDuration: 'match.number',
+          afterFnDuration: 'match.number',
+        }
+      })
+    }
+
+    if (arg.test) {
+      sanitizedLifecycleData.test = {
+        fnDuration: 'match.number',
+        afterFnDuration: 'match.number',
+      }
+    }
+
+    for (const hook of hooks.after) {
+      const hooksToSanitize = arg[hook]
+
+      if (!hooksToSanitize) {
+        continue
+      }
+
+      sanitizedLifecycleData[hook] = hooksToSanitize.map((oldHook) => {
+        return {
+          hookId: oldHook.hookId,
+          fnDuration: 'match.number',
+          afterFnDuration: 'match.number',
+        }
+      })
+    }
+
+    return sanitizedLifecycleData
+  },
+}
+
+const keysToEliminate = ['codeFrame', '_testConfig'] as const
+
+function removeUnusedKeysForTestSnapshot<T> (obj: T): T {
+  for (const key of keysToEliminate) {
+    delete obj[key]
+  }
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (key in obj) {
+      const transform = eventCleanseMap[key]?.(value)
+
+      if (!transform) {
+        continue
+      }
+
+      obj[key] = transform
+    } else {
+      delete obj[key]
+    }
+  }
+
+  return obj
+}
+
+declare global {
+  interface Window {
+    bus: EventEmitter
+  }
+}
+
+export function runCypressInCypressMochaEventsTest<T> (snapshots: T, snapToCompare: keyof T, done: Mocha.Done) {
+  const bus = new EventEmitter()
+  const outerRunner = window.top!.window
+
+  outerRunner.bus = bus
+
+  // TODO: Can we automate writing the snapshots to disk?
+  // For some reason `cy.task('getSnapshot')` has problems when executed in
+  // "cypress in cypress"
+  bus.on('assert:cypress:in:cypress', (snapshot: CypressInCypressMochaEvent[]) => {
+    const expected = snapshots[snapToCompare]
+    const diff = disparity.unifiedNoColor(JSON.stringify(snapshot, null, 2), JSON.stringify(expected, null, 2), {})
+
+    if (diff !== '') {
+      /* eslint-disable no-console */
+      console.info('Received snapshot:', JSON.stringify(snapshot, null, 2))
+
+      return cy.fail(new Error(`The captured mocha events did not match the "${String(snapToCompare)}" snapshot.\n${diff}`), { async: false })
+    }
+
+    Cypress.log({
+      name: 'assert',
+      message: `The captured mocha events for the spec matched the "${String(snapToCompare)}" snapshot!`,
+      state: 'passed',
+    })
+
+    return done()
+  })
+
+  const assertMatchingSnapshot = (win: Cypress.AUTWindow) => {
+    win.getEventManager().on('cypress:in:cypress:run:complete', (args: CypressInCypressMochaEvent[]) => {
+      const data = sanitizeMochaEvents(args)
+
+      bus.emit('assert:cypress:in:cypress', data)
+    })
+  }
+
+  return { assertMatchingSnapshot }
+}
+
+function sanitizeMochaEvents (args: CypressInCypressMochaEvent[]) {
+  return args.map((mochaEvent) => {
+    return mochaEvent.map((payload) => {
+      if (typeof payload === 'string') {
+        return payload
+      }
+
+      return removeUnusedKeysForTestSnapshot(payload)
+    })
+  })
+}
---
+++ packages/app/cypress/e2e/runner/support/verify-failures.ts
@@ -0,0 +1,216 @@
+import _ from 'lodash'
+import defaultMessages from '@packages/frontend-shared/src/locales/en-US.json'
+
+// Assert that either the the dialog is presented or the mutation is emitted, depending on
+// whether the test has a preferred IDE defined.
+const verifyIdeOpen = ({ fileName, action, hasPreferredIde, line, column }) => {
+  if (hasPreferredIde) {
+    cy.withCtx((ctx, o) => {
+      // @ts-expect-error - check if we've stubbed it already, only need to stub it once
+      if (!ctx.actions.file.openFile.restore?.sinon) {
+        o.sinon.stub(ctx.actions.file, 'openFile')
+      }
+    })
+
+    action()
+
+    cy.withCtx((ctx, o) => {
+      expect(ctx.actions.file.openFile).to.have.been.calledWith(o.sinon.match(new RegExp(`${o.fileName}$`)), o.line, o.column)
+    }, { fileName, line, column })
+  } else {
+    action()
+
+    cy.contains(defaultMessages.globalPage.externalEditorPreferences).should('be.visible')
+    cy.findByRole('button', { name: defaultMessages.actions.close }).click()
+  }
+}
+
+const verifyFailure = (options) => {
+  const {
+    specTitle,
+    hasCodeFrame = true,
+    verifyOpenInIde,
+    hasPreferredIde,
+    column,
+    originalMessage,
+    message = [],
+    notInMessage = [],
+    command,
+    stack,
+    fileName,
+    uncaught = false,
+    uncaughtMessage,
+    line,
+    regex,
+    mode,
+  } = options
+  let { codeFrameText, stackRegex, codeFrameRegex } = options
+
+  if (!codeFrameText) {
+    codeFrameText = specTitle
+  }
+
+  const codeFrameColumnArray = [].concat(column)
+  const codeFrameColumn = codeFrameColumnArray[0]
+  const stackColumnArray = codeFrameColumnArray.map((col) => col - 1)
+  const stackColumn = stackColumnArray[0]
+
+  stackRegex = regex || stackRegex || new RegExp(`${fileName}:${line || '\\d+'}:(${stackColumnArray.join('|')})`)
+  codeFrameRegex = regex || codeFrameRegex || new RegExp(`${fileName}:${line || '\\d+'}:(${codeFrameColumnArray.join('|')})`)
+
+  cy.contains('.runnable-title', specTitle).closest('.runnable').as('Root')
+
+  cy.get('@Root').within(() => {
+    cy.contains('View stack trace').click()
+
+    const messageLines = [].concat(message)
+
+    if (messageLines.length) {
+      cy.log('message contains expected lines and stack does not include message')
+
+      _.each(messageLines, (msg) => {
+        cy.get('.runnable-err-message')
+        .should('include.text', msg)
+
+        cy.get('.runnable-err-stack-trace')
+        .should('not.include.text', msg)
+      })
+    }
+
+    if (originalMessage) {
+      cy.get('.runnable-err-message')
+      .should('include.text', originalMessage)
+    }
+
+    const notInMessageLines = [].concat(notInMessage)
+
+    if (notInMessageLines.length) {
+      cy.log('message does not contain the specified lines')
+
+      _.each(notInMessageLines, (msg) => {
+        cy.get('.runnable-err-message')
+        .should('not.include.text', msg)
+      })
+    }
+
+    cy.log('stack trace matches the specified pattern')
+    cy.get('.runnable-err-stack-trace')
+    .invoke('text')
+    .should('match', stackRegex)
+
+    if (stack) {
+      const stackLines = [].concat(stack)
+
+      if (stackLines.length) {
+        cy.log('stack contains the expected lines')
+      }
+
+      _.each(stackLines, (stackLine) => {
+        cy.get('.runnable-err-stack-trace')
+        .should('include.text', stackLine)
+      })
+    }
+
+    cy.get('.runnable-err-stack-trace')
+    .invoke('text')
+    .should('not.include', '__stackReplacementMarker')
+    .should((stackTrace) => {
+      // if this stack trace has the 'From Your Spec Code' addendum,
+      // it should only appear once
+      const match = stackTrace.match(/From Your Spec Code/g)
+
+      if (match && match.length) {
+        expect(match.length, `'From Your Spec Code' should only be in the stack once, but found ${match.length} instances`).to.equal(1)
+      }
+    })
+  })
+
+  if (verifyOpenInIde) {
+    verifyIdeOpen({
+      fileName,
+      hasPreferredIde,
+      action: () => {
+        cy.get('@Root').contains('.runnable-err-stack-trace .runnable-err-file-path a', fileName)
+        .click({ force: true })
+      },
+      line,
+      column: stackColumn,
+    })
+  }
+
+  cy.get('@Root').within(() => {
+    if (command) {
+      cy.log('the error is attributed to the correct command')
+      cy
+      .get('.command-state-failed')
+      .first()
+      .find('.command-method')
+      .invoke('text')
+      .should('equal', command)
+    }
+
+    if (uncaught) {
+      cy.log('uncaught error has an associated log for the original error')
+      cy.get('.command-name-uncaught-exception')
+      .should(mode === 'component' ? 'have.length.gte' : 'have.length', 1)
+      .find('.command-state-failed')
+      .find('.command-message-text')
+      .should('include.text', uncaughtMessage || originalMessage)
+    } else {
+      cy.log('"caught" error does not have an uncaught error log')
+      cy.get('.command-name-uncaught-exception').should('not.exist')
+    }
+
+    if (!hasCodeFrame) return
+
+    cy.log('code frame matches specified pattern')
+    cy
+    .get('.test-err-code-frame .runnable-err-file-path')
+    .invoke('text')
+    .should('match', codeFrameRegex)
+
+    cy.get('.test-err-code-frame span').should('include.text', codeFrameText)
+  })
+
+  if (verifyOpenInIde) {
+    verifyIdeOpen({
+      fileName,
+      hasPreferredIde,
+      action: () => {
+        cy.get('@Root').contains('.test-err-code-frame .runnable-err-file-path a', fileName)
+        .click()
+      },
+      line,
+      column: codeFrameColumn,
+    })
+  }
+}
+
+type ChainableVerify = (specTitle: string, props: any) => Cypress.Chainable
+
+export const createVerify = ({ fileName, hasPreferredIde, mode }): ChainableVerify => {
+  return (specTitle: string, props: any) => {
+    props.specTitle ||= specTitle
+    props.fileName ||= fileName
+    props.hasPreferredIde = hasPreferredIde
+    props.mode = mode
+
+    return cy.wrap(
+      (props.verifyFn || verifyFailure).call(null, props),
+    )
+  }
+}
+
+export const verifyInternalFailure = (props) => {
+  const { specTitle, method, stackMethod } = props
+
+  cy.contains('.runnable-title', specTitle).closest('.runnable').within(() => {
+    cy.get('.runnable-err-message')
+    .should('include.text', `thrown in ${method.replace(/\./g, '-')}`)
+
+    cy.get('.runnable-err-stack-expander > .collapsible-header').click()
+
+    cy.get('.runnable-err-stack-trace')
+    .should('include.text', stackMethod || method)
+  })
+}
