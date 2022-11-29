7961b6b43c6a2d91ac01807d4fdb8b56ea48c8a1
Bill Glesias <bglesias@gmail.com>
2022-11-29 17:47:05 -0500

    chore: update cross_origin_command to point to new v12 docs with cy.origin explainer

--- packages/driver/src/cypress/error_messages.ts
+++ packages/driver/src/cypress/error_messages.ts
@@ -913,10 +913,18 @@ export default {
     },
     test_stopped: 'Cypress test was stopped while running this command.',
     cross_origin_command ({ commandOrigin, autOrigin }) {
-      return stripIndent`\
-        The command was expected to run against origin \`${commandOrigin }\` but the application is at origin \`${autOrigin}\`.
+      return {
+        message: stripIndent`\
+        The command was expected to run against origin \`${commandOrigin}\` but the application is at origin \`${autOrigin}\`.
 
-        This commonly happens when you have either not navigated to the expected origin or have navigated away unexpectedly.`
+        This commonly happens when you have either not navigated to the expected origin, have navigated away unexpectedly, or likely forgot to use ${cmd('origin')}.
+
+        \`cy.origin('${autOrigin}', () => {\`
+        \`  <commands targeting ${autOrigin} go here>\`
+        \`})\`
+        `,
+        docsUrl: 'https://on.cypress.io/cy-visit-succeeded-but-commands-are-timing-out-because-you-visited-a-different-origin-domain-without-using-cy-origin',
+      }
     },
   },
 
