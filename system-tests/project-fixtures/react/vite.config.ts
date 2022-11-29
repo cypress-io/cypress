bdc96b585b202125b21291bce1e0e3052d057085
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-11-29 11:21:00 +1000

    dynamic alias for react-dom/client

--- system-tests/project-fixtures/react/vite.config.ts
+++ system-tests/project-fixtures/react/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
+import * as React from 'react'
+
+const alias: Record<string, string> = {
+  'react': require.resolve('react'),
+  'react-dom': require.resolve('react-dom'),
+}
+
+if (React.version.startsWith('18')) {
+  // Only needed for React 18+
+  alias['react-dom/client'] = require.resolve('react-dom/client')
+}
 
 export default defineConfig({
   resolve: {
-    alias: {
-      'react': require.resolve('react'),
-      'react-dom/client': require.resolve('react-dom/client'),
-      'react-dom': require.resolve('react-dom'),
-    },
+    alias,
   },
   logLevel: 'silent',
 })
