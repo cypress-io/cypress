1c80741a5829f6f65e4c2c7e23262064c88194f3
amehta265 <65267668+amehta265@users.noreply.github.com>
2022-12-02 14:20:36 -0500

    feat: IATR-M0 Spec and Test Component (#24877)
    Closes #24443 and #24444

---
+++ packages/app/src/debug/DebugFailedTest.cy.tsx
@@ -0,0 +1,38 @@
+import DebugFailedTest from './DebugFailedTest.vue'
+
+describe('<DebugFailedTest/>', () => {
+  it('mounts correctly', () => {
+    const testResult = {
+      id: '676df87878',
+      titleParts: ['Login', 'Should redirect unauthenticated user to signin page'],
+    }
+
+    cy.mount(() => (
+      <DebugFailedTest failedTestResult={testResult} />
+    ))
+
+    cy.findByTestId('test-row').children().should('have.length', 2)
+
+    testResult.titleParts.forEach((title, index) => {
+      cy.findByTestId(`titleParts-${index}`).should('have.text', `${title}`)
+    })
+  })
+
+  it('contains multiple titleParts segments', () => {
+    const multipleTitleParts = {
+      id: '676df87878',
+      titleParts: ['Login', 'Describe', 'it', 'context', 'Should redirect unauthenticated user to signin page'],
+    }
+
+    cy.mount(() => (
+      <DebugFailedTest failedTestResult={multipleTitleParts} />
+    ))
+
+    cy.findByTestId('test-row').children().should('have.length', 5).should('be.visible')
+    multipleTitleParts.titleParts.forEach((title, index) => {
+      cy.findByTestId(`titleParts-${index}`).should('have.text', `${title}`)
+    })
+
+    cy.percySnapshot()
+  })
+})
---
+++ packages/app/src/debug/DebugFailedTest.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    data-cy="test-row"
+    class="flex flex-row items-center gap-x-2.5 non-italic text-base text-gray-700 font-normal"
+  >
+    <div
+      v-for="(titlePart, index) in props.failedTestResult.titleParts"
+      :key="`${titlePart}-${index}`"
+      class="flex flex-row items-center gap-x-2.5"
+      :data-cy="`titleParts-${index}`"
+    >
+      <IconChevronRightSmall
+        v-if="index !== 0"
+        data-cy="right-chevron"
+        size="8"
+        stroke-color="gray-200"
+        fill-color="gray-200"
+      />
+      <span>
+        {{ titlePart }}
+      </span>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { IconChevronRightSmall } from '@cypress-design/vue-icon'
+import type { TestResults } from './DebugSpec.vue'
+
+const props = defineProps<{
+  failedTestResult: TestResults
+}>()
+
+</script>
