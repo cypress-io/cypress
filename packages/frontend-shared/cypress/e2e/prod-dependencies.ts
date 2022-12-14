5510419bea3bd9f35233681527eabeab9bf20da4
Ryan Manuel <ryanm@cypress.io>
2022-12-14 13:27:41 -0600

    fix: ensure cypress in cypress uses prod dependencies from the snapshot - run ci

---
+++ packages/frontend-shared/cypress/e2e/prod-dependencies.ts
@@ -0,0 +1,13 @@
+export { makeGraphQLServer } from '@packages/graphql/src/makeGraphQLServer'
+
+export { clearCtx, DataContext, globalPubSub, setCtx } from '@packages/data-context'
+
+export * as fs from 'fs-extra'
+
+export { buildSchema, execute, ExecutionResult, GraphQLError, parse } from 'graphql'
+
+export { Response } from 'cross-fetch'
+
+export { getOperationName } from '@urql/core'
+
+export const makeDataContext = require('@packages/server/lib/makeDataContext').makeDataContext
