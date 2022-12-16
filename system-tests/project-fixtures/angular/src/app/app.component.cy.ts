01460d9799315510b8083702fff6c32c172baabe
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-16 23:49:21 +1000

    fix test

--- system-tests/project-fixtures/angular/src/app/app.component.cy.ts
+++ system-tests/project-fixtures/angular/src/app/app.component.cy.ts
@@ -3,5 +3,9 @@ import { AppComponent } from './app.component'
 it('should', () => {
   cy.mount(AppComponent)
   cy.get('h1').contains('Hello World')
-  cy.get('.test-img').should('have.css', 'background-image', 'url("http://localhost:4455/__cypress/src/test.png")')
+  cy.get('.test-img')
+  .invoke('css', 'background-image')
+  .then((img) => {
+    expect(img).to.contain('__cypress/src/test.png')
+  })
 })
