03a95586c77dadf28ffcaff8abb9d478953235e7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-16 13:44:32 +1000

    chore: add tests

--- system-tests/project-fixtures/angular/src/app/app.component.cy.ts
+++ system-tests/project-fixtures/angular/src/app/app.component.cy.ts
@@ -3,4 +3,5 @@ import { AppComponent } from './app.component'
 it('should', () => {
   cy.mount(AppComponent)
   cy.get('h1').contains('Hello World')
+  cy.get('.test-img').should('have.css', 'background-image', 'url("http://localhost:4455/__cypress/src/test.png")')
 })
