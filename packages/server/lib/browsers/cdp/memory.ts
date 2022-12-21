b4be5eba682a8d74e44ea367fcacab898cf2059f
Matt Schile <mschile@cypress.io>
2022-12-20 23:35:33 -0700

    updates

--- packages/server/lib/browsers/cdp/memory.ts
+++ packages/server/lib/browsers/cdp/memory.ts
@@ -1,6 +1,6 @@
 import debugModule from 'debug'
 import si from 'systeminformation'
-import browsers from '../../browsers'
+import { _groupCyProcesses } from '../../util/process_profiler'
 import type { SendDebuggerCommand } from './cdp_automation'
 
 const debugVerbose = debugModule('cypress-verbose:server:browsers:cdp:memory')
@@ -14,16 +14,15 @@ const getJsHeapSizeLimit = async ({ sendDebuggerCommandFn }) => {
 
 const findRendererProcess = async () => {
   const processes = await si.processes()
-  const instance = browsers.getBrowserInstance()
 
-  // electron will return a list of pids, since it's not a hierarchy
-  const pids = instance?.allPids ? instance.allPids : [instance?.pid]
+  // filter down to the renderer processes
+  const groupedProcesses = _groupCyProcesses(processes)
+  const rendererProcesses = groupedProcesses.filter((p) => p.group === 'browser' && (p.command.includes('--type=renderer') || p.params.includes('--type=renderer')))
 
-  // filter down to renderer processes, they could be a child process (chrome, edge, etc) or the main process (electron)
-  const childBrowserProcesses = processes.list.filter((process) => (pids.includes(process.parentPid) || pids.includes(process.pid)) && process.params.includes('--type=renderer'))
+  if (rendererProcesses.length === 0) return null
 
   // assume the renderer process with the most memory is the one we're interested in
-  const maxRendererProcess = childBrowserProcesses.reduce((prev, current) => (prev.memRss > current.memRss) ? prev : current)
+  const maxRendererProcess = rendererProcesses.reduce((prev, current) => (prev.memRss > current.memRss) ? prev : current)
 
   debugVerbose('renderer processes memory: %o', { pid: maxRendererProcess.pid, 'memRss(bytes)': maxRendererProcess.memRss * KILOBYTE })
 
@@ -36,6 +35,12 @@ const checkMemoryAndCollectGarbage = async (sendDebuggerCommandFn: SendDebuggerC
   debugVerbose('jsHeapSizeLimit:', jsHeapSizeLimit)
 
   const rendererProcess = await findRendererProcess()
+
+  if (!rendererProcess) {
+    debugVerbose('no renderer process found, skipping garbage collection')
+
+    return
+  }
 
   // only collect garbage if we're using more than the MEMORY_THRESHOLD_PERCENTAGE of the heap
   const shouldCollectGarbage = ((rendererProcess.memRss * KILOBYTE) / jsHeapSizeLimit) >= MEMORY_THRESHOLD_PERCENTAGE
