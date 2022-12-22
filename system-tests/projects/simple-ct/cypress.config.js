6bcc8da4310030c3bf031728bb7039188e0bd59b
astone123 <adams@cypress.io>
2022-12-22 17:09:04 -0500

    add comment

--- system-tests/projects/simple-ct/cypress.config.js
+++ system-tests/projects/simple-ct/cypress.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   component: {
     experimentalSingleTabRunMode: true,
+    // In order for Webkit to correctly exit in single tab mode, we have to disable video
+    // https://github.com/cypress-io/cypress/issues/23815
     video: false,
     supportFile: false,
     devServer: {
