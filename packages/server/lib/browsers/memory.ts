54b8904fb7fd1c9c6a60bb437e765a2600e05355
Matt Schile <mschile@cypress.io>
2022-12-22 15:10:42 -0700

    75% [run ci]

--- packages/server/lib/browsers/memory.ts
+++ packages/server/lib/browsers/memory.ts
@@ -10,7 +10,7 @@ import type { SendDebuggerCommand } from './cdp_automation'
 const debug = debugModule('cypress:server:browsers:memory')
 const debugVerbose = debugModule('cypress-verbose:server:browsers:memory')
 
-const MEMORY_THRESHOLD_PERCENTAGE = 50
+const MEMORY_THRESHOLD_PERCENTAGE = 75
 const KIBIBYTE = 1024
 const FOUR_GIBIBYTES = 4294967296
 
