aef442608798cea549e5bf33e5f985445dd767e6
Matt Schile <mschile@cypress.io>
2022-12-21 17:31:30 -0700

    updates

--- packages/server/lib/browsers/memory.ts
+++ packages/server/lib/browsers/memory.ts
@@ -1,6 +1,7 @@
 import debugModule from 'debug'
 import si from 'systeminformation'
 import os from 'os'
+import fs from 'fs-extra'
 import { execSync } from 'child_process'
 import { _groupCyProcesses } from '../util/process_profiler'
 
@@ -9,7 +10,7 @@ import type { SendDebuggerCommand } from './cdp_automation'
 const debug = debugModule('cypress:server:browsers:memory')
 const debugVerbose = debugModule('cypress-verbose:server:browsers:memory')
 
-const MEMORY_THRESHOLD_PERCENTAGE = 50
+const MEMORY_THRESHOLD_PERCENTAGE = 75
 const KIBIBYTE = 1024
 const FOUR_GIBIBYTES = 4294967296
 
@@ -20,22 +21,23 @@ const FOUR_GIBIBYTES = 4294967296
  *   1. set total mem limit for the container/host by reading off cgroup memory limits (if available) otherwise use os.totalmem()
 *
 * Before each test:
- *   2. set current mem usage for the container/host by reading off cgroup memory usage (if available) otherwise use os.freemem()
- *   1. set current renderer mem usage (using ps, top, or systemd-cgtop)
- *   2. set max avail render mem to minimum of v8 heap size limit and (total mem limit - current mem usage - current renderer mem usage)
- *   3. calc the % left, max avail render mem - current renderer mem usage
- *   4. if that falls below the threshold percentage (e.g. 50%) do a GC
+ *   1. set current mem usage for the container/host by reading off cgroup memory usage (if available) otherwise use si.mem().available
+ *   2. set current renderer mem usage (using ps, top, or systemd-cgtop)
+ *   3. set max avail render mem to minimum of v8 heap size limit and (total mem limit - current mem usage)
+ *   4. calc % of memory used,  current renderer mem usage / max avail render mem
+ *   5. if that exceeds the defined memory threshold percentage (e.g. 50%) do a GC
  */
 
 const isCgroupMemoryAvailable = (() => {
   try {
-    execSync('cat /sys/fs/cgroup/memory', { encoding: 'utf8' })
+    // TODO: this is only checking for cgroup v2, we should also check and support cgroup v1
+    execSync('cat /sys/fs/cgroup/memory/memory.usage_in_bytes', { encoding: 'utf8' })
 
     debugVerbose('cgroup memory available')
 
     return true
   } catch (err) {
-    debugVerbose('cgroup memory not available, will use system information instead')
+    debugVerbose('cgroup v2 memory not available, will use system information instead')
 
     return false
   }
@@ -60,6 +62,8 @@ const getAvailableMemory = async () => {
 
   if (isCgroupMemoryAvailable) {
     const usage = Number(execSync('cat /sys/fs/cgroup/memory/memory.usage_in_bytes', { encoding: 'utf8' }))
+
+    debugVerbose('memory usage from cgroup', usage)
 
     available = totalMemoryLimit - usage
   } else {
@@ -116,12 +120,14 @@ const checkMemoryAndCollectGarbage = async (sendDebuggerCommandFn: SendDebuggerC
 
   const currentAvailableMemory = await getAvailableMemory()
 
-  const maxAvailableRendererMemory = Math.min(jsHeapSizeLimit, (currentAvailableMemory - rendererProcess.memRss))
+  const maxAvailableRendererMemory = Math.min(jsHeapSizeLimit, currentAvailableMemory)
 
   debugVerbose('maxAvailableRendererMemory:', maxAvailableRendererMemory, 'bytes')
 
   // only collect garbage if less than the MEMORY_THRESHOLD_PERCENTAGE of the heap left
-  const shouldCollectGarbage = ((maxAvailableRendererMemory - (rendererProcess.memRss * KIBIBYTE)) / maxAvailableRendererMemory) * 100 <= MEMORY_THRESHOLD_PERCENTAGE
+  const shouldCollectGarbage = ((rendererProcess.memRss * KIBIBYTE) / maxAvailableRendererMemory) * 100 >= MEMORY_THRESHOLD_PERCENTAGE
+
+  let measurement
 
   if (shouldCollectGarbage) {
     debug('forcing garbage collection')
@@ -129,10 +135,31 @@ const checkMemoryAndCollectGarbage = async (sendDebuggerCommandFn: SendDebuggerC
     await sendDebuggerCommandFn('HeapProfiler.collectGarbage')
     performance.mark('gc-end')
 
-    debugVerbose(performance.measure('garbage collection', 'gc-start', 'gc-end'))
+    measurement = performance.measure('garbage collection', 'gc-start', 'gc-end')
+    debugVerbose(measurement)
   } else {
     debug('skipping garbage collection')
   }
+
+  if (debugVerbose.enabled) {
+    logMemory({ memRss: rendererProcess.memRss * KIBIBYTE, garbageCollected: shouldCollectGarbage, gcDuration: measurement?.duration, currentAvailableMemory, maxAvailableRendererMemory, jsHeapSizeLimit })
+  }
+}
+
+let testCount = 0
+const logMemory = ({ memRss, garbageCollected, gcDuration, currentAvailableMemory, maxAvailableRendererMemory, jsHeapSizeLimit }) => {
+  testCount++
+  const log = {
+    test: testCount,
+    memRss,
+    garbageCollected,
+    gcDuration,
+    currentAvailableMemory,
+    maxAvailableRendererMemory,
+    jsHeapSizeLimit,
+  }
+
+  fs.appendFile('/tmp/memory.json', JSON.stringify(log))
 }
 
 export {
