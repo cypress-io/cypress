7c22e845f37137a31c37cd20fdbc3d1a7d0e2985
astone123 <adams@cypress.io>
2022-12-22 12:24:07 -0500

    revert changes to files in pristine projects [run ci]

--- system-tests/projects/pristine-cjs-project/expected-cypress-js-component-vue.js-3-webpack/cypress.config.js
+++ system-tests/projects/pristine-cjs-project/expected-cypress-js-component-vue.js-3-webpack/cypress.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
-const webpackConfig = require('./webpack.config.js')
+const webpackConfig = require("./webpack.config.js");
 
 module.exports = defineConfig({
   component: {
     devServer: {
-      framework: 'vue',
-      bundler: 'webpack',
+      framework: "vue",
+      bundler: "webpack",
       webpackConfig,
     },
   },
-})
+});
