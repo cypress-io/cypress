1c08b8ea15a82a41b02c784531e9fc7d171e55a7
Matt Schile <mschile@cypress.io>
2023-01-16 09:16:39 -0700

    updating workflow

--- packages/driver/cypress/memory/memory.cy.js
+++
@@ -1,7 +0,0 @@
-describe('memory spec', () => {
-  for (let index = 0; index < 100; index++) {
-    it(`passes test ${index + 1}`, () => {
-      cy.visit('http://localhost:3500/memory')
-    })
-  }
-})
