353fab74e79c81a9c9557664eb040ba2be64f063
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-01-18 20:21:32 +0100

    add tests for angular inject

--- system-tests/project-fixtures/angular/cypress/support/component.ts
+++ system-tests/project-fixtures/angular/cypress/support/component.ts
@@ -1,12 +1,13 @@
-import { mount } from 'cypress/angular'
+import { mount, inject } from 'cypress/angular'
 
 declare global {
   namespace Cypress {
     interface Chainable {
       mount: typeof mount
+      inject: typeof inject
     }
   }
 }
 
-
-Cypress.Commands.add('mount', mount);
+Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('inject', inject)
---
+++ system-tests/project-fixtures/angular/src/app/inject.cy.ts
@@ -0,0 +1,64 @@
+import { Component, Injectable } from '@angular/core'
+
+@Injectable({ providedIn: 'root' })
+export class Cart {
+    #items: string[] = []
+
+    add (product: string) {
+      this.#items.push(product)
+    }
+
+    getItems () {
+      return this.#items
+    }
+}
+
+@Component({
+  template: `
+      <div><h2>Great Product</h2>
+      <button (click)="buy()" data-testid="btn-buy">Buy</button>
+      </div>`,
+})
+export class ProductComponent {
+  constructor (private cart: Cart) {
+  }
+
+  buy () {
+    this.cart.add('Great Product')
+  }
+}
+
+describe('inject function', () => {
+  it('should use inject to verify against a dependency implicitly', () => {
+    cy.mount(ProductComponent)
+    cy.inject(Cart).invoke('getItems').should('have.length', 0)
+    cy.get('[data-testid=btn-buy]').click()
+    cy.inject(Cart).invoke('getItems').should('have.length', 1)
+  })
+
+  it('should use inject to verify against a dependency explicitly', () => {
+    cy.mount(ProductComponent)
+    cy.inject(Cart).invoke('getItems').should('have.length', 0)
+    cy.get('[data-testid=btn-buy]').click()
+    cy.inject(Cart).should((cart) => {
+      expect(cart.getItems()).to.have.length(1)
+    })
+  })
+
+  it('should use inject in combination with a fake', () => {
+    const cartFake = {
+      items: [] as string[],
+      add (product: string) {
+        this.items.push(product)
+      },
+      getItems () {
+        return this.items
+      },
+    }
+
+    cy.mount(ProductComponent, { providers: [{ provide: Cart, useValue: cartFake }] })
+    cy.inject(Cart).invoke('getItems').should('have.length', 0)
+    cy.get('[data-testid=btn-buy]').click()
+    cy.wrap(Cart).invoke('getItems').should('have.length', 1)
+  })
+})
