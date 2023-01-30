a24d3bae5147ee43aa10b801392e6acc78434294
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-01-30 13:10:14 +1000

    update tests

--- packages/app/cypress/e2e/debug.cy.ts
+++ packages/app/cypress/e2e/debug.cy.ts
@@ -1,5 +1,11 @@
 import RelevantRunsDataSource_RunsByCommitShas from '../fixtures/gql-RelevantRunsDataSource_RunsByCommitShas.json'
 import Debug from '../fixtures/gql-Debug.json'
+import CloudViewerAndProject_RequiredData from '../fixtures/gql-CloudViewerAndProject_RequiredData.json'
+import MainApp from '../fixtures/gql-MainAppQuery.json'
+import SideBarNavigationContainer from '../fixtures/gql-SideBarNavigationContainer.json'
+import HeaderBar from '../fixtures/gql-HeaderBar_HeaderBarQuery.json'
+import SpecsPageContainer from '../fixtures/gql-SpecsPageContainer.json'
+import SetTestsForDebug from '../fixtures/gql-Mutation-SetTestsForDebug.json'
 
 Cypress.on('window:before:load', (win) => {
   win.__CYPRESS_GQL_NO_SOCKET__ = 'true'
@@ -27,20 +33,56 @@ describe('App - Debug Page', () => {
     cy.visitApp()
 
     cy.intercept('POST', '/__cypress/graphql/query-Debug', (req) => {
-      req.reply({
-        data: Debug,
-      })
+      req.reply(
+        Debug,
+      )
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/query-CloudViewerAndProject_RequiredData', (req) => {
+      req.reply(
+        CloudViewerAndProject_RequiredData,
+      )
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/query-MainAppQuery', (req) => {
+      req.reply(
+        MainApp,
+      )
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/query-SideBarNavigationContainer', (req) => {
+      req.reply(SideBarNavigationContainer)
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/query-HeaderBar_HeaderBarQuery', (req) => {
+      req.reply(HeaderBar)
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/query-HeaderBar_HeaderBarQuery', (req) => {
+      req.reply(HeaderBar)
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/query-SpecsPageContainer', (req) => {
+      req.reply(SpecsPageContainer)
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/query-RunAllSpecsData', (req) => {
+      req.reply(SpecsPageContainer)
+    })
+
+    cy.intercept('POST', '/__cypress/graphql/mutation-SetTestsForDebug', (req) => {
+      req.reply(SetTestsForDebug)
     })
 
     cy.findByTestId('sidebar-link-debug-page').click()
     cy.findByTestId('debug-container').should('be.visible')
 
-    cy.findByTestId('header-top').contains('wip - cypress testing')
+    cy.findByTestId('header-top').contains('chore: testing cypress')
     cy.findByTestId('debug-header-dashboard-link')
     .contains('View in Cypress Cloud')
-    .should('have.attr', 'href', Debug.currentProject.cloudProject.runByNumber.url)
+    .should('have.attr', 'href', Debug.data.currentProject.cloudProject.runByNumber.url)
 
-    cy.findByTestId('debug-runNumber-FAILED').contains('#130')
+    cy.findByTestId('debug-runNumber-FAILED').contains('#136')
     cy.findByTestId('debug-commitsAhead').contains('You are 1 commit ahead')
 
     cy.findByTestId('metadata').within(() => {
@@ -49,9 +91,9 @@ describe('App - Debug Page', () => {
       cy.get('[title="skipped"]').contains('0')
       cy.get('[title="pending"]').contains('0')
       cy.findByTestId('debug-header-branch').contains('main')
-      cy.findByTestId('debug-header-commitHash').contains('commit1')
+      cy.findByTestId('debug-header-commitHash').contains('6895fdc')
       cy.findByTestId('debug-header-author').contains('Lachlan Miller')
-      cy.findByTestId('debug-header-createdAt').contains('00:33')
+      cy.findByTestId('debug-header-createdAt').contains('00:19')
     })
 
     cy.findByTestId('spec-contents').within(() => {
