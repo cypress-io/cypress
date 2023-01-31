2f189aa549b014813276c4f8da6007713a45edd2
Matt Schile <mschile@cypress.io>
2023-01-31 07:26:56 -0700

    updating memory.cy.js

--- packages/driver/cypress/e2e/memory/memory.cy.js
+++ packages/driver/cypress/e2e/memory/memory.cy.js
@@ -1,20 +1,7 @@
 describe('memory spec', { browser: { family: 'chromium' } }, () => {
-  const text = 'x'.repeat(100000)
-
-  beforeEach(() => {
-    if (!Cypress.config('isActuallyInteractive')) {
-      Cypress.config('isInteractive', false)
-      Cypress.config('numTestsKeptInMemory', 0)
-    }
-  })
-
   for (let index = 0; index < 50; index++) {
     it(`test ${index + 1} passes`, () => {
       cy.visit('http://localhost:3500/memory')
-
-      for (let index = 0; index < 100; index++) {
-        cy.get(`#p${index}`).should('have.text', text)
-      }
     })
   }
 })
