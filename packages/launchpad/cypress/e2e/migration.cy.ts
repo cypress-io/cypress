5d13579123021cb04382b53fc27be1053f697655
Zachary Williams <ZachJW34@gmail.com>
2023-02-03 09:06:52 -0600

    Update packages/launchpad/cypress/e2e/migration.cy.ts
    Co-authored-by: Mark Noonan <mark@cypress.io>

--- packages/launchpad/cypress/e2e/migration.cy.ts
+++ packages/launchpad/cypress/e2e/migration.cy.ts
@@ -80,8 +80,6 @@ function renameSupport (lang: 'js' | 'ts' | 'coffee' = 'js') {
 }
 
 describe('global mode', () => {
-  // TODO: Figure out why it is flaky. Seems to be due to MigrationWizard query being executed multiple times
-  // see: https://github.com/cypress-io/cypress/issues/25377
   it('migrates 2 projects in global mode', () => {
     cy.openGlobalMode()
     cy.addProject('migration-e2e-export-default')
