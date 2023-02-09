70b709c4f3e35417f7920fd028bd42f531218789
Bill Glesias <bglesias@gmail.com>
2023-02-09 14:15:22 -0500

    chore: change name of fixture to something more accurate

--- packages/driver/cypress/e2e/e2e/origin/cookie_misc.cy.ts
+++ packages/driver/cypress/e2e/e2e/origin/cookie_misc.cy.ts
@@ -4,7 +4,7 @@ describe('misc cookie tests', () => {
   // NOTE: For this test to work correctly, we need to have a FQDN, not localhost (www.foobar.com).
   // FIXES: https://github.com/cypress-io/cypress/issues/25174 (cookies are duplicated with prepended dot (.))
   it('does not duplicate cookies with a prepended dot for cookies that are stored inside the server side cookie jar', () => {
-    cy.visit('https://www.foobar.com:3502/fixtures/duplicate-hostOnly-cookie.html')
+    cy.visit('https://www.foobar.com:3502/fixtures/trigger-cross-origin-redirect-to-self.html')
 
     // does a 302 redirect back to www.foobar.com primary-origin page, but sets a sameSite=None cookie
     cy.get('[data-cy="cookie-cross-origin-redirects"]').click()
