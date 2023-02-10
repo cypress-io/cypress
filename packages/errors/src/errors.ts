c843efd86db4c90174b3069217c095087b3776ec
Tim Griesser <tgriesser10@gmail.com>
2023-02-10 12:43:48 -0500

    Update packages/errors/src/errors.ts
    Co-authored-by: Emily Rohrbough <emilyrohrbough@users.noreply.github.com>

--- packages/errors/src/errors.ts
+++ packages/errors/src/errors.ts
@@ -205,7 +205,7 @@ export const AllCypressErrors = {
 
         ${fmt.highlightSecondary(arg1.response)}`
   },
-  CLOUD_UNKNOWN_CREATE_RUN_WARNING: (arg1: {props: any, message: string}) => {
+  CLOUD_UNKNOWN_CREATE_RUN_WARNING: (arg1: {props?: any, message: string}) => {
     if (!Object.keys(arg1.props).length) {
       return errTemplate`\
           Warning from Cypress Cloud: ${fmt.highlight(arg1.message)}
