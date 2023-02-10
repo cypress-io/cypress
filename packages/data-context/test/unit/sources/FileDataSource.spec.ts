7dfdd6a8e972dc5997c1d78986b1fd7b18b2e3bd
Mike Plummer <mikep@cypress.io>
2023-02-10 14:44:02 -0600

    Add test case for failure on retry

--- packages/data-context/test/unit/sources/FileDataSource.spec.ts
+++ packages/data-context/test/unit/sources/FileDataSource.spec.ts
@@ -288,6 +288,19 @@ describe('FileDataSource', () => {
         expect(matchGlobsStub.getCall(0).args[1].suppressErrors).to.be.undefined
         expect(matchGlobsStub.getCall(1).args[1].suppressErrors).to.equal(true)
       })
+
+      it('should return empty array if retry with suppression fails', async () => {
+        matchGlobsStub.rejects(new Error('mocked filesystem error'))
+
+        const files = await fileDataSource.getFilesByGlob(
+          '/',
+          '/cypress/e2e/**.cy.js',
+          { absolute: false, objectMode: true },
+        )
+
+        expect(files).to.eql([])
+        expect(matchGlobsStub).to.have.callCount(2)
+      })
     })
   })
 })
