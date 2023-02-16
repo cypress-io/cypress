b8f643a7cd176e754e0e328f267852ff40333117
astone123 <adams@cypress.io>
2023-02-15 17:29:18 -0700

    misc: use icon from design system

--- packages/launchpad/src/setup/FrameworkOptionsFooter.vue
+++ packages/launchpad/src/setup/FrameworkOptionsFooter.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="flex bg-gray-50 h-65px py-12px px-15px text-gray-800 items-center">
-    <div><IconComponent class="h-40px min-w-0 min-h-0 w-40px" /></div>
-    <div class="ml-10px">
-      <div>{{ t("setupPage.projectSetup.frameworkNotListed") }}</div>
-      <div>
-        <ExternalLink
-          href="https://on.cypress.io/component-integrations"
-          class="text-indigo-500 block hocus-link-default group"
-        >
-          {{ t('setupPage.projectSetup.browseIntegrations') }}<i-cy-arrow-right_x16
-            class="ml-4px transform transition-transform ease-in -translate-y-1px duration-200 inline-block icon-dark-current group-hocus:translate-x-2px"
-          />
-        </ExternalLink>
-      </div>
+  <div class="flex bg-gray-50 h-65px py-12px px-17px text-gray-800 items-center">
+    <div>
+      <Icon
+        name="general-grid-2x2"
+        size="16"
+      />
+    </div>
+    <div class="ml-15px">
+      {{ t("setupPage.projectSetup.frameworkNotListed") }}
+      <ExternalLink
+        href="https://on.cypress.io/component-integrations"
+        class="text-indigo-500 block hocus-link-default group"
+      >
+        {{ t('setupPage.projectSetup.browseIntegrations') }}<i-cy-arrow-right_x16
+          class="ml-4px transform transition-transform ease-in -translate-y-1px duration-200 inline-block icon-dark-current group-hocus:translate-x-2px"
+        />
+      </ExternalLink>
     </div>
   </div>
 </template>
@@ -20,7 +23,7 @@
 <script lang="ts" setup>
 import { useI18n } from '@cy/i18n'
 import ExternalLink from '@packages/frontend-shared/src/gql-components/ExternalLink.vue'
-import IconComponent from '~icons/cy/testing-type-component_x64.svg'
+import Icon from '@cypress-design/vue-icon'
 
 const { t } = useI18n()
 
