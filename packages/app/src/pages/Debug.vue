fc58e7aa1fab5b12f736700eb332eddb350f404a
Stokes Player <stokes.player@gmail.com>
2023-02-26 19:52:43 -0500

    Caching gql results between changes to remove UI flicker

--- packages/app/src/pages/Debug.vue
+++ packages/app/src/pages/Debug.vue
@@ -1,7 +1,7 @@
 <template>
   <DebugContainer
     data-cy="debug-container"
-    :gql="query.data.value"
+    :gql="cachedProject"
     :is-loading="isLoading"
     :commits-ahead="relevantRuns?.commitsAhead || 0"
     :online="online"
@@ -14,8 +14,8 @@ import DebugContainer from '../debug/DebugContainer.vue'
 import { gql, useQuery, useSubscription } from '@urql/vue'
 import { useOnline } from '@vueuse/core'
 
-import { DebugDocument, Debug_SpecsChangeDocument } from '../generated/graphql'
-import { computed } from 'vue'
+import { DebugDocument, DebugSpecsFragment, Debug_SpecsChangeDocument } from '../generated/graphql'
+import { computed, ref, watchEffect } from 'vue'
 import { useRelevantRun } from '../composables/useRelevantRun'
 
 const online = useOnline()
@@ -52,17 +52,35 @@ const shouldPauseQuery = computed(() => {
   return variables.value.runNumber === -1 || !online
 })
 
+const cachedProject = ref<DebugSpecsFragment>()
+
 const query = useQuery({ query: DebugDocument, variables, pause: shouldPauseQuery, requestPolicy: 'network-only' })
 
 const isLoading = computed(() => {
   const relevantRunsHaveNotLoaded = !relevantRuns.value
   const queryIsBeingFetched = query.fetching.value
+
+  const currentRunIsChanging = !!relevantRuns.value?.current
+    && cachedProject.value?.currentProject?.cloudProject?.__typename === 'CloudProject'
+    && !!cachedProject.value.currentProject.cloudProject.runByNumber?.runNumber
+    && relevantRuns.value.current !== cachedProject.value.currentProject.cloudProject.runByNumber.runNumber
+
   const waitingForRunToFetchFromTheCloud = !!relevantRuns.value?.current && relevantRuns.value.current > 0
     && (!query.data.value?.currentProject?.cloudProject
       || query.data.value?.currentProject?.cloudProject?.__typename === 'CloudProject'
       && !query.data.value.currentProject.cloudProject.runByNumber?.status)
 
-  return relevantRunsHaveNotLoaded || queryIsBeingFetched || waitingForRunToFetchFromTheCloud
+  return relevantRunsHaveNotLoaded || (currentRunIsChanging && (queryIsBeingFetched || waitingForRunToFetchFromTheCloud))
+})
+
+watchEffect(() => {
+  if (query.data.value?.currentProject?.cloudProject?.__typename === 'CloudProject') {
+    const cloudProject = query.data.value.currentProject.cloudProject
+
+    if (cloudProject.runByNumber?.id !== undefined) {
+      cachedProject.value = query.data.value
+    }
+  }
 })
 
 useSubscription({ query: Debug_SpecsChangeDocument })
