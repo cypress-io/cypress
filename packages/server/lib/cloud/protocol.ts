e37638a52b03fe37739fdcacea18899b53ae6940
Ryan Manuel <ryanm@cypress.io>
2023-03-09 21:10:26 -0600

    slight refactoring

--- packages/server/lib/cloud/protocol.ts
+++ packages/server/lib/cloud/protocol.ts
@@ -4,58 +4,73 @@ import Debug from 'debug'
 import CDP from 'chrome-remote-interface'
 import type { ProtocolManager } from '@packages/types'
 
+interface AppCaptureProtocolInterface {
+  setupProtocol (url?: string): Promise<void>
+  connectToBrowser (options: any): void
+  beforeSpec (spec: any): void
+  afterSpec (): void
+  beforeTest (attr: any, test: any): void
+}
+
 const debug = Debug('cypress:server:protocol')
 
-const setupProtocol = async () => {
-  let script: string
+const setupProtocol = async (url?: string): Promise<AppCaptureProtocolInterface | undefined> => {
+  let script: string | undefined
 
   // TODO: We will need to remove this option in production
   if (process.env.CYPRESS_LOCAL_PROTOCOL_PATH) {
     script = await fs.readFile(process.env.CYPRESS_LOCAL_PROTOCOL_PATH, 'utf8')
-  } else {
+  } else if (url) {
     // TODO: Download the protocol script from the cloud
-    script = ''
   }
 
-  const vm = new NodeVM({
-    console: 'inherit',
-    sandbox: { Debug, CDP },
-  })
+  if (script) {
+    const vm = new NodeVM({
+      console: 'inherit',
+      sandbox: { Debug, CDP },
+    })
 
-  const { AppCaptureProtocol } = vm.run(script)
+    const { AppCaptureProtocol } = vm.run(script)
 
-  return new AppCaptureProtocol()
+    return new AppCaptureProtocol()
+  }
+
+  return
 }
 
 class ProtocolManagerImpl implements ProtocolManager {
-  private protocol: any
+  private protocol: AppCaptureProtocolInterface | undefined
 
-  async setupProtocol () {
+  protocolEnabled (): boolean {
+    return !!this.protocol
+  }
+
+  async setupProtocol (url?: string) {
     debug('setting up protocol')
 
-    this.protocol = await setupProtocol()
+    this.protocol = await setupProtocol(url)
   }
 
   connectToBrowser (options) {
     debug('connecting to browser for new spec')
-    this.protocol.connectToBrowser(options)
+    this.protocol?.connectToBrowser(options)
   }
 
   beforeSpec (spec) {
     debug('initializing new spec %O', spec.relative)
-    this.protocol.beforeSpec(spec)
+    this.protocol?.beforeSpec(spec)
 
     // Initialize DB here
   }
 
   afterSpec () {
     debug('after spec')
-    this.protocol.afterSpec()
+    this.protocol?.afterSpec()
   }
 
   beforeTest (attr, test) {
     debug('initialize new test %O', test.title)
-    this.protocol.beforeTest(attr, test)
+    this.protocol?.beforeTest(attr, test)
   }
 }
 
--- packages/types/src/protocol.ts
+++ packages/types/src/protocol.ts
@@ -1,6 +1,8 @@
 // TODO: This is basic for now but will evolve as we progress with the protocol work
 
 export interface ProtocolManager {
+  setupProtocol(url?: string): Promise<void>
+  protocolEnabled(): boolean
   connectToBrowser(options: any): void
   beforeSpec(spec: any): void
   afterSpec(): void
