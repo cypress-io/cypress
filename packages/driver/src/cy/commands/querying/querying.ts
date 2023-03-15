901b9ce9f6cff37d361ec074fdb03fc264b6a41d
Emily Rohrbough <emilyrohrbough@yahoo.com>
2023-03-15 09:06:38 -0500

    and remove from querying file

--- packages/driver/src/cy/commands/querying/querying.ts
+++ packages/driver/src/cy/commands/querying/querying.ts
@@ -14,7 +14,6 @@ type GetOptions = Partial<Cypress.Loggable & Cypress.Timeoutable & Cypress.Withi
 }>
 
 type ContainsOptions = Partial<Cypress.Loggable & Cypress.Timeoutable & Cypress.CaseMatchable & Cypress.Shadow>
-type ShadowOptions = Partial<Cypress.Loggable & Cypress.Timeoutable>
 
 function getAlias (selector, log, cy) {
   const alias = selector.slice(1)
@@ -343,50 +342,6 @@ export default (Commands, Cypress, cy, state) => {
             'Applied To': $dom.getElements(subject),
             Yielded: $el.get(0),
             Elements: $el.length,
-          }
-        },
-      })
-
-      return $el
-    }
-  })
-
-  Commands.addQuery('shadow', function contains (userOptions: ShadowOptions = {}) {
-    const log = userOptions.log !== false && Cypress.log({
-      timeout: userOptions.timeout,
-      consoleProps: () => ({}),
-    })
-
-    this.set('timeout', userOptions.timeout)
-    this.set('onFail', (err) => {
-      switch (err.type) {
-        case 'existence': {
-          const { message, docsUrl } = $errUtils.cypressErrByPath('shadow.no_shadow_root')
-
-          err.message = message
-          err.docsUrl = docsUrl
-          break
-        }
-        default:
-          break
-      }
-    })
-
-    return (subject) => {
-      Cypress.ensure.isType(subject, 'element', 'shadow', cy)
-
-      // find all shadow roots of the subject(s), if any exist
-      const $el = subject
-      .map((i, node) => node.shadowRoot)
-      .filter((i, node) => node !== undefined && node !== null)
-
-      log && cy.state('current') === this && log.set({
-        $el,
-        consoleProps: () => {
-          return {
-            'Applied To': $dom.getElements(subject),
-            Yielded: $dom.getElements($el),
-            Elements: $el?.length,
           }
         },
       })
