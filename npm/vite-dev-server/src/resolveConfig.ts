a6a8b8802bd5bdb697de76338f34754ab91e2899
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-03-16 13:12:39 +1000

    add null check

--- npm/vite-dev-server/src/resolveConfig.ts
+++ npm/vite-dev-server/src/resolveConfig.ts
@@ -113,7 +113,8 @@ function makeCypressViteConfig (config: ViteDevServerConfig, vite: Vite): Inline
           vitePathNodeModules,
           cypressBinaryRoot,
           // Allow in monorepo: https://vitejs.dev/config/server-options.html#server-fs-allow
-          vite.searchForWorkspaceRoot(process.cwd()),
+          // Supported from Vite v3 - add null check for v2 users.
+          vite.searchForWorkspaceRoot?.(process.cwd()),
         ],
       },
       host: '127.0.0.1',
