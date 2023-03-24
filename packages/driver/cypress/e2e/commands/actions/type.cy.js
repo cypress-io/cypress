eeb2f97c4a27edb4ac69f3bdd5b2ea60b147107c
Kevin Groat <apps@kgroat.dev>
2023-03-24 06:38:00 -0400

    Update type click test

--- packages/driver/cypress/e2e/commands/actions/type.cy.js
+++ packages/driver/cypress/e2e/commands/actions/type.cy.js
@@ -3094,14 +3094,15 @@ describe('src/cy/commands/actions/type - #type', () => {
     })
 
     // https://github.com/cypress-io/cypress/issues/26198
-    it('text calls on elements in shadow dom do not fire clicks if the element already has focus', () => {
-      cy.visit('fixtures/shadow-dom-type-no-click.html')
+    it('text calls on elements in shadow dom do not fire click if the element already has focus', () => {
+      cy.visit('fixtures/shadow-dom-type.html')
 
-      cy
-      .get('test-element').shadow()
-      .find('input').focus().type('asdf')
+      cy.get('test-element').shadow().find('input').as('input')
 
-      cy.get('#result').should('not.have.text', 'clicked')
+      cy.get('@input').invoke('on', 'click', cy.spy().as('clickSpy'))
+      cy.get('@input').focus().type('asdf')
+
+      cy.get('@clickSpy').should('not.have.been.called')
     })
   })
 })
