0d62a57c73a398d478f1d6af7844ee13bdea59c0
Ryan Manuel <ryanm@cypress.io>
2023-03-27 15:20:13 -0500

    fix tests

--- packages/server/test/support/fixtures/cloud/protocol/test-protocol.js
+++ packages/server/test/support/fixtures/cloud/protocol/test-protocol.js
@@ -1,13 +1,10 @@
-/* global Debug, CDP, CY_PROTOCOL_DIR, betterSqlite3Binding, nodePath, Database */
+/* global Debug, Kysely, SqliteDialect */
 
 const AppCaptureProtocol = class {
   constructor () {
     this.Debug = Debug
-    this.CDP = CDP
-    this.CY_PROTOCOL_DIR = CY_PROTOCOL_DIR
-    this.betterSqlite3Binding = betterSqlite3Binding
-    this.nodePath = nodePath
-    this.Database = Database
+    this.Kysely = Kysely
+    this.SqliteDialect = SqliteDialect
 
     this.connectToBrowser = this.connectToBrowser.bind(this)
     this.addRunnables = this.addRunnables.bind(this)
