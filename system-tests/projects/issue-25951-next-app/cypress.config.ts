e61c11290d644914f66f5996611a9d74080071d6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-04-03 15:20:48 +1000

    make project more minimal

--- system-tests/projects/issue-25951-next-app/cypress.config.ts
+++ system-tests/projects/issue-25951-next-app/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   component: {
+    fixturesFolder: false,
+    supportFile: false,
     devServer: {
       framework: 'next',
       bundler: 'webpack',
--- system-tests/projects/issue-25951-next-app/src/pages/_app.cy.tsx
+++ system-tests/projects/issue-25951-next-app/src/pages/_app.cy.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { mount } from 'cypress/react18'
 import App from './_app'
 
 describe('<App />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
+    mount(<App />)
     cy.contains('Hello world')
   })
 })
