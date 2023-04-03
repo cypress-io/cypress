4e7f3da3cb7ccdf8e56a41a7665755dab9b0dc44
Mike Plummer <mikep@cypress.io>
2023-04-03 10:40:19 -0500

    misc: Update styling of Runs Page
    * Use new status icons to match other areas of the app
    * Improve responsiveness of UI on smaller viewports
    * Update link params

--- packages/app/src/runs/RunCard.vue
+++ packages/app/src/runs/RunCard.vue
@@ -1,15 +1,19 @@
 <template>
   <ExternalLink
     :data-cy="`runCard-${run.id}`"
-    class="border rounded bg-light-50 border-gray-100 w-full
-  block overflow-hidden hocus-default"
-    :href="run.url || '#'"
+    class="border rounded bg-light-50 border-gray-100 w-full block overflow-hidden hocus-default"
+    :href="runUrl"
     :use-default-hocus="false"
   >
     <ListRowHeader
-      :icon="icon"
       :data-cy="`run-card-icon-${run.status}`"
     >
+      <template #icon>
+        <SolidStatusIcon
+          size="24"
+          :status="STATUS_MAP[run.status!]"
+        />
+      </template>
       <template #header>
         <span class="font-semibold mr-8px whitespace-pre-wrap">{{ run.commitInfo?.summary }}</span>
         <span
@@ -20,45 +24,46 @@
         >{{ tag }}</span>
       </template>
       <template #description>
-        <ul class="flex flex-wrap text-sm text-gray-700 gap-8px items-center whitespace-nowrap children:flex children:items-center">
-          <li
-            v-if="run.commitInfo?.authorName"
-            data-cy="run-card-author"
-          >
-            <i-cy-general-user_x16
-              class="mr-1 icon-dark-gray-500 icon-light-gray-100 icon-secondary-light-gray-200"
-              data-cy="run-card-avatar"
-            />
-            <span class="sr-only">Commit Author:</span>{{ run.commitInfo.authorName }}
-          </li>
+        <span class="flex flex-wrap justify-between">
+          <ul class="flex flex-wrap text-sm text-gray-700 gap-8px items-center whitespace-nowrap children:flex children:items-center">
+            <li
+              v-if="run.commitInfo?.authorName"
+              data-cy="run-card-author"
+            >
+              <i-cy-general-user_x16
+                class="mr-1 icon-dark-gray-500 icon-light-gray-100 icon-secondary-light-gray-200"
+                data-cy="run-card-avatar"
+              />
+              <span class="sr-only">Commit Author:</span>{{ run.commitInfo.authorName }}
+            </li>
 
-          <li
-            v-if="run.commitInfo?.branch"
-            data-cy="run-card-branch"
-          >
-            <i-cy-tech-branch-h_x16 class="mr-1 icon-dark-gray-300" />
-            <span class="sr-only">Branch Name:</span>{{ run.commitInfo.branch }}
-          </li>
+            <li
+              v-if="run.commitInfo?.branch"
+              data-cy="run-card-branch"
+            >
+              <i-cy-tech-branch-h_x16 class="mr-1 icon-dark-gray-300" />
+              <span class="sr-only">Branch Name:</span>{{ run.commitInfo.branch }}
+            </li>
 
-          <li
-            v-if="run.createdAt"
-            data-cy="run-card-created-at"
-          >
-            <span class="sr-only">Run Created At:</span>{{ relativeCreatedAt }}
-          </li>
+            <li
+              v-if="run.createdAt"
+              data-cy="run-card-created-at"
+            >
+              <span class="sr-only">Run Created At:</span>{{ relativeCreatedAt }}
+            </li>
 
-          <li
-            v-if="run.totalDuration"
-            data-cy="run-card-duration"
-          >
-            <span class="sr-only">Run Total Duration:</span>{{ totalDuration }}
-          </li>
-        </ul>
-      </template>
-      <template #right>
-        <RunResults
-          :gql="props.gql"
-        />
+            <li
+              v-if="run.totalDuration"
+              data-cy="run-card-duration"
+            >
+              <span class="sr-only">Run Total Duration:</span>{{ totalDuration }}
+            </li>
+          </ul>
+
+          <RunResults
+            :gql="props.gql"
+          />
+        </span>
       </template>
     </ListRowHeader>
   </ExternalLink>
@@ -70,14 +75,11 @@ import ListRowHeader from '@cy/components/ListRowHeader.vue'
 import ExternalLink from '@cy/gql-components/ExternalLink.vue'
 import { gql } from '@urql/core'
 import RunResults from './RunResults.vue'
-import type { RunCardFragment } from '../generated/graphql'
-import PassedIcon from '~icons/cy/status-passed-solid_x24.svg'
-import FailedIcon from '~icons/cy/status-failed-solid_x24.svg'
-import ErroredIcon from '~icons/cy/status-errored-solid_x24.svg'
-import SkippedIcon from '~icons/cy/status-skipped_x24.svg'
-import PendingIcon from '~icons/cy/status-pending_x24.svg'
+import type { CloudRunStatus, RunCardFragment } from '../generated/graphql'
 import { dayjs } from './utils/day.js'
 import { useDurationFormat } from '../composables/useDurationFormat'
+import { SolidStatusIcon, StatusType } from '@cypress-design/vue-statusicon'
+import { getUrlWithParams } from '@packages/frontend-shared/src/utils/getUrlWithParams'
 
 gql`
 fragment RunCard on CloudRun {
@@ -100,24 +102,32 @@ fragment RunCard on CloudRun {
 }
 `
 
+const STATUS_MAP: Record<CloudRunStatus, StatusType> = {
+  PASSED: 'passed',
+  FAILED: 'failed',
+  CANCELLED: 'cancelled',
+  ERRORED: 'errored',
+  RUNNING: 'running',
+  NOTESTS: 'noTests',
+  OVERLIMIT: 'overLimit',
+  TIMEDOUT: 'timedOut',
+} as const
+
 const props = defineProps<{
   gql: RunCardFragment
 }>()
 
-const ICON_MAP = {
-  PASSED: PassedIcon,
-  FAILED: FailedIcon,
-  TIMEDOUT: ErroredIcon,
-  ERRORED: ErroredIcon,
-  OVERLIMIT: ErroredIcon,
-  CANCELLED: SkippedIcon,
-  NOTESTS: SkippedIcon,
-  RUNNING: PendingIcon,
-} as const
-
-const icon = computed(() => ICON_MAP[props.gql.status!])
-
 const run = computed(() => props.gql)
+
+const runUrl = computed(() => {
+  return getUrlWithParams({
+    url: run.value.url || '#',
+    params: {
+      utm_medium: 'Runs Tab',
+      utm_campaign: 'Cloud Run',
+    },
+  })
+})
 
 const relativeCreatedAt = computed(() => dayjs(new Date(run.value.createdAt!)).fromNow())
 
