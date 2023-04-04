ac6f1de08bb64ae5e799200429bcc88421209ea3
Ben M <benm@cypress.io>
2023-04-04 12:45:11 -0400

    chore: updating update-browser-versions action to add the generated PR to the firewatch board for visibility

--- scripts/github-actions/create-pull-request.js
+++ scripts/github-actions/create-pull-request.js
@@ -17,6 +17,84 @@ const createPullRequest = async ({ context, github, baseBranch, branchName, desc
       reviewers,
     })
   }
+  // Get node information about the PR since adding it to the Firewatch Project Board Beta requires use of graphQL
+  const getItemInfoQuery = `
+    query ($org: String!, $repo: String!, $project: Int!, $issue: Int!) {
+      organization(login: $org) {
+        repository(name: $repo) {
+          pullRequest(number: $issue) {
+            closed
+            closedAt
+            id
+            projectItems(first: 10, includeArchived: true) {
+              nodes {
+                id
+                isArchived
+                fieldValueByName(name: "Status") {
+                  ... on ProjectV2ItemFieldSingleSelectValue {
+                    name
+                    field {
+                      ... on ProjectV2SingleSelectField {
+                        project {
+                          ... on ProjectV2 {
+                            id
+                            number
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        projectV2(number: $project) {
+          id
+          field(name: "Status") {
+            ... on ProjectV2SingleSelectField {
+              id
+              options {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }`;
+  
+  const getItemInfoVars = {
+      org: context.repo.owner,
+      repo: context.repo.repo,
+      issue: number,
+      project: 9 // Firewatch board: https://github.com/orgs/cypress-io/projects/9
+  };
+
+  const getItemInfo = await github.graphql(getItemInfoQuery,getItemInfoVars);
+
+  // Need to get the node_id for use in graphQL mutations
+  const prItemId = getItemInfo.organization.repository.pullRequest.id;
+  const projectID = getItemInfo.organization.projectV2.id;
+
+  //Add PR to firewatch board -  https://github.com/orgs/cypress-io/projects/9
+  const addToProjectBoardQuery = `
+    mutation ($project_id: ID!, $item_id: ID!) {
+      addProjectV2ItemById(input: {contentId: $item_id, projectId: $project_id}) {
+        clientMutationId
+        item {
+          id
+        }
+      }
+    }`;
+
+  const addToProjectBoardQueryVars = {
+    project_id: projectID,
+    item_id: prItemId
+  };
+
+  const addToProjectBoard = await github.graphql(addToProjectBoardQuery,addToProjectBoardQueryVars);
+
 }
 
 module.exports = {
