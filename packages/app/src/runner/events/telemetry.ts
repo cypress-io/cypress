bd92d6954efc9353b8a55adfda1b412c4ea48693
Bill Glesias <bglesias@gmail.com>
2023-04-12 10:54:31 -0400

    try removing command tele for build. [run ci]

--- packages/app/src/runner/events/telemetry.ts
+++ packages/app/src/runner/events/telemetry.ts
@@ -23,42 +23,42 @@ export const addTelemetryListeners = (Cypress) => {
     }
   })
 
-  Cypress.on('test:after:run', (attributes, test) => {
-    try {
-      const span = telemetry.getSpan(`test:${test.fullTitle()}`)
+  // Cypress.on('test:after:run', (attributes, test) => {
+  //   try {
+  //     const span = telemetry.getSpan(`test:${test.fullTitle()}`)
 
-      span?.setAttributes({
-        timings: JSON.stringify(attributes.timings),
-      })
+  //     span?.setAttributes({
+  //       timings: JSON.stringify(attributes.timings),
+  //     })
 
-      span?.end()
-    } catch (error) {
-      // TODO: log error when client side debug logging is available
-    }
-  })
+  //     span?.end()
+  //   } catch (error) {
+  //     // TODO: log error when client side debug logging is available
+  //   }
+  // })
 
-  Cypress.on('command:start', (command) => {
-    const runnable = Cypress.state('runnable')
+  // Cypress.on('command:start', (command) => {
+  //   const runnable = Cypress.state('runnable')
 
-    const runnableType = runnable.type === 'hook' ? runnable.hookName : runnable.type
+  //   const runnableType = runnable.type === 'hook' ? runnable.hookName : runnable.type
 
-    const span = telemetry.startSpan({
-      name: `${runnableType}: ${command.attributes.name}(${command.attributes.args.join(',')})`,
-    })
+  //   const span = telemetry.startSpan({
+  //     name: `${runnableType}: ${command.attributes.name}(${command.attributes.args.join(',')})`,
+  //   })
 
-    span?.setAttribute('command-name', command.attributes.name)
-    span?.setAttribute('runnable-type', command.attributes.runnableType)
-  })
+  //   span?.setAttribute('command-name', command.attributes.name)
+  //   span?.setAttribute('runnable-type', command.attributes.runnableType)
+  // })
 
-  Cypress.on('command:end', (command) => {
-    const runnable = Cypress.state('runnable')
+  // Cypress.on('command:end', (command) => {
+  //   const runnable = Cypress.state('runnable')
 
-    const runnableType = runnable.type === 'hook' ? runnable.hookName : runnable.type
+  //   const runnableType = runnable.type === 'hook' ? runnable.hookName : runnable.type
 
-    const span = telemetry.getSpan(`${runnableType}: ${command.attributes.name}(${command.attributes.args.join(',')})`)
+  //   const span = telemetry.getSpan(`${runnableType}: ${command.attributes.name}(${command.attributes.args.join(',')})`)
 
-    span?.setAttribute('state', command.state)
-    span?.setAttribute('numLogs', command.logs?.length || 0)
-    span?.end()
-  })
+  //   span?.setAttribute('state', command.state)
+  //   span?.setAttribute('numLogs', command.logs?.length || 0)
+  //   span?.end()
+  // })
 }
