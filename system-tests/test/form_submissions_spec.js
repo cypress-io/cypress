80a5190e5815c1ec23a4b934352ab92897527532
mjhenkes <mjhenkes@gmail.com>
2023-04-13 07:47:52 -0500

    moar system test fixes

--- system-tests/test/form_submissions_spec.js
+++ system-tests/test/form_submissions_spec.js
@@ -141,6 +141,7 @@ describe('e2e forms', () => {
 
     systemTests.it('passes with https on localhost', {
       config: {
+        videoCompression: false,
         baseUrl: `https://localhost:${HTTPS_PORT}`,
       },
       spec: 'form_submission_multipart.cy.js',
@@ -149,6 +150,7 @@ describe('e2e forms', () => {
 
     systemTests.it('passes with http on localhost', {
       config: {
+        videoCompression: false,
         baseUrl: `http://localhost:${HTTP_PORT}`,
         e2e: {},
       },
