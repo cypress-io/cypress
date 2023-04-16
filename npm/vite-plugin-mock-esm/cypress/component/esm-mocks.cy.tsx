99f9c775e58d5a9af7dc1d72c64f63edeb05ce38
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-04-17 09:11:41 +1000

    add tests

--- npm/vite-plugin-mock-esm/cypress/component/esm-mocks.cy.tsx
+++ npm/vite-plugin-mock-esm/cypress/component/esm-mocks.cy.tsx
@@ -34,15 +34,6 @@ describe('ESM Mock Plugin', () => {
     cy.get('@add').should('have.been.called')
   })
 
-  // TODO: maximum stack trace exceeded when calling M.add
-  it.skip('spies', () => {
-    cy.spy(M, 'add').as('add')
-
-    expect(M.add(2, 5)).to.eq(10)
-
-    cy.get('@add').should('be.calledOnceWith', 2, 5)
-  })
-
   it('stubs react component', () => {
     cy.stub(Foo, 'Foo').callsFake(() => <h1>Stub Component</h1>)
     mount(<Foo.Foo msg='Hello world' />)
@@ -50,8 +41,45 @@ describe('ESM Mock Plugin', () => {
     cy.contains('Hello world').should('not.exist')
   })
 
-  it('stubs lodash from node_modules', () => {
+  it('stubs diff from node_modules', () => {
     cy.stub(Diff, 'diffChars').callsFake(() => 'FAKE')
     expect(Diff.diffChars('ab', 'ac')).to.eq('FAKE')
+  })
+
+  it('stubs lodash method from node_modules using dynamic import', () => {
+    async function run () {
+      const _ = await import('lodash')
+
+      cy.stub(_, 'camelCase').callsFake((str: string) => str.toUpperCase())
+      const result = _.camelCase('foo_bar')
+
+      expect(result).to.eq('FOO_BAR')
+    }
+
+    cy.wrap(run())
+  })
+
+  // TODO: __cypressModule(...).then is not a function
+  it.skip('stubs lodash method from node_modules using `then`', async () => {
+    await import('lodash').then((mod) => {
+      cy.stub(mod, 'camelCase').callsFake((str: string) => str.toUpperCase())
+      const result = mod.camelCase('foo_bar')
+
+      expect(result).to.eq('FOO_BAR')
+    })
+  })
+
+  // import _ from 'lodash'. Just importing lodash breaks the runner.
+  // TODO: Weird error: "Cannot redefine property: prototype". Forces you to restart Cypress.
+  // it.skip('stubs lodash method from node_modules using static import', () => {
+  //   cy.stub(_, 'camelCase').callsFake(() => 'STUB')
+  //   expect(_.camelCase('aaaa')).to.eq('STUB')
+  // })
+
+  // TODO: maximum stack trace exceeded when calling M.add
+  it.skip('spies', () => {
+    cy.spy(M, 'add').as('add')
+    expect(M.add(2, 5)).to.eq(10)
+    cy.get('@add').should('be.calledOnceWith', 2, 5)
   })
 })
