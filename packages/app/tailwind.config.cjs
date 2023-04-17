b05ea3ba00ecd4ebec53de73777904e1723820d0
elevatebart <bart@cypress.io>
2023-04-17 14:38:46 -0500

    fix: tailwind config

--- packages/app/tailwind.config.cjs
+++ packages/app/tailwind.config.cjs
@@ -1,35 +1,8 @@
-const cypressCSS = require('@cypress-design/css')
+const config = require('@packages/frontend-shared/tailwind.config.cjs')
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  presets: [cypressCSS.TailwindConfig()],
-  content: {
-    files: [
-      './src/**/*.{vue,js,ts,jsx,tsx,scss,css}', //
-      '../frontend-shared/src/**/*.{vue,js,ts,jsx,tsx,scss,css}',
-    ],
-    extract: ['vue', 'js', 'tsx'].reduce((acc, ext) => {
-      acc[ext] = cypressCSS.TailwindIconExtractor
+config.content.files = [
+  './src/**/*.{vue,js,ts,jsx,tsx,scss,css}', //
+  '../frontend-shared/src/**/*.{vue,js,ts,jsx,tsx,scss,css}',
+],
 
-      return acc
-    }, {}),
-  },
-  theme: {
-    extend: {
-      zIndex: {
-        '1': '1',
-        '10': '10',
-        '20': '20',
-        '30': '30',
-        '40': '40',
-        '50': '50',
-        '51': '51',
-      },
-    },
-  },
-  plugins: [
-    function ({ addVariant }) {
-      addVariant('children', '& > *')
-    },
-  ],
-}
+module.exports = config
