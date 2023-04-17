386d1431fb10451cbb918b21776631238b75d814
mjhenkes <mjhenkes@gmail.com>
2023-04-17 09:01:08 -0500

    Add Tests

--- packages/telemetry/src/detectors/circleCiDetectorSync.ts
+++ packages/telemetry/src/detectors/circleCiDetectorSync.ts
@@ -1,33 +1,16 @@
-/*
- * Copyright The OpenTelemetry Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import { Resource } from '@opentelemetry/resources'
 import type { DetectorSync, ResourceAttributes, IResource } from '@opentelemetry/resources'
+import { Resource } from '@opentelemetry/resources'
 
 /**
- * EnvDetectorSync can be used to detect the presence of and create a Resource
- * from the OTEL_RESOURCE_ATTRIBUTES environment variable.
+ * CircleCiDetectorSync can be used to detect the presence of and create a Resource
+ * from circle ci env variables.
  */
 class CircleCiDetectorSync implements DetectorSync {
   /**
    * Returns a {@link Resource} populated with attributes from the
-   * OTEL_RESOURCE_ATTRIBUTES environment variable. Note this is an async
-   * function to conform to the Detector interface.
+   * circle ci environment variable.
    *
-   * @param config The resource detection config
+   * @param config The resource detection config -- ignored
    */
   detect (): IResource {
     const attributes: ResourceAttributes = {}
