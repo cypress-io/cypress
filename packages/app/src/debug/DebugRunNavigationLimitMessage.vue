a90360ede500faf1f3168a1fa7c8a1dfffea0017
Adam Stone-Lord <adams@cypress.io>
2023-04-18 13:48:21 -0600

    feat: display a limit warning on the run navigation component when there are 100 total runs (#26523)

---
+++ packages/app/src/debug/DebugRunNavigationLimitMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex text-sm p-4 align-center">
+    <div class="mr-2 pt-[2px]">
+      <IconWarningCircle
+        stroke-color="gray-500"
+        fill-color="gray-50"
+      />
+    </div>
+    <div class="text-gray-700">
+      {{ t('debugPage.foundMoreThan100Runs') }}
+      <ExternalLink
+        v-if="cloudProjectUrl"
+        :href="cloudProjectUrl"
+      >
+        {{ t('debugPage.goToCypressCloud') }}
+      </ExternalLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ExternalLink from '@cy/gql-components/ExternalLink.vue'
+import { useI18n } from '@cy/i18n'
+import { IconWarningCircle } from '@cypress-design/vue-icon'
+
+const { t } = useI18n()
+
+defineProps<{ cloudProjectUrl?: string }>()
+
+</script>
