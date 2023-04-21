c86a97e7cc24c3f0fe48e80dc378e70701985a2e
Stokes Player <stokes@cypress.io>
2023-04-21 13:51:33 -0400

    chore: add missing utm parameters for cloud links to Debug page (#26556)

--- packages/app/src/debug/DebugRunNavigation.cy.tsx
+++ packages/app/src/debug/DebugRunNavigation.cy.tsx
@@ -240,7 +240,7 @@ describe('<DebugRunNavigation />', () => {
     cy.findByTestId('debug-toggle').click()
 
     cy.contains('We found more than 100 runs.').should('be.visible')
-    cy.findByRole('link', { name: 'Go to Cypress Cloud to see all runs' }).should('be.visible').should('have.attr', 'href', 'https://cloud.cypress.io/projects/ypt4pf/')
+    cy.findByRole('link', { name: 'Go to Cypress Cloud to see all runs' }).should('be.visible').should('have.attr', 'href', 'https://cloud.cypress.io/projects/ypt4pf/?utm_medium=Debug+Tab&utm_campaign=Run+Navigation+Limit&utm_source=Binary%3A+Launchpad')
 
     cy.percySnapshot()
   })
