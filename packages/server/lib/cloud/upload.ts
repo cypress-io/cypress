a3a15c86422a201409fdccbe923b20bece050424
Tim Griesser <tgriesser10@gmail.com>
2023-04-24 10:23:40 -0400

    refactor: use createReadStream rather than readFile for asset upload

--- packages/server/lib/cloud/upload.ts
+++ packages/server/lib/cloud/upload.ts
@@ -1,16 +1,12 @@
 const rp = require('@cypress/request-promise')
-const { fs } = require('../util/fs')
+import { createReadStream } from 'fs'
 
 export = {
   send (pathToFile: string, url: string) {
-    return fs
-    .readFileAsync(pathToFile)
-    .then((buf) => {
-      return rp({
-        url,
-        method: 'PUT',
-        body: buf,
-      })
+    return rp({
+      url,
+      method: 'PUT',
+      body: createReadStream(pathToFile),
     })
   },
 }
