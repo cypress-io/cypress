922d0130bad7271ee46faf5ac2c1c4a99c513b85
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-04-25 12:14:49 +1000

    fix padding

--- packages/launchpad/src/setup/LaunchpadHeader.vue
+++ packages/launchpad/src/setup/LaunchpadHeader.vue
@@ -6,9 +6,10 @@
     {{ title }}
   </h1>
   <p
-    class="mx-[42px] mt-[12px] text-center text-body-gray-600 text-[18px]"
-    v-html="description"
-  />
+    class="mx-[42px] text-center text-body-gray-600 text-[18px]"
+  >
+    {{ description }}
+  </p>
 </template>
 
 <script setup lang="ts">
