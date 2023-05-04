791d7a17af333ca4a5485ebfcf2d98bd6a50f316
astone123 <adams@cypress.io>
2023-05-04 10:50:38 -0700

    remove noise

--- packages/server/lib/modes/interactive.ts
+++ packages/server/lib/modes/interactive.ts
@@ -23,13 +23,6 @@ const isDev = () => {
   return Boolean(process.env['CYPRESS_INTERNAL_ENV'] === 'development')
 }
 
-// function showNotification () {
-//   const NOTIFICATION_TITLE = 'Basic Notification'
-//   const NOTIFICATION_BODY = 'Notification from the Main process'
-
-//   new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
-// }
-
 export = {
   isMac () {
     return os.platform() === 'darwin'
