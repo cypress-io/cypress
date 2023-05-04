9a9ecacfeb5fd9506fb1b8b93b1078e1486ff4f1
Emily Rohrbough <emilyrohrbough@yahoo.com>
2023-05-04 08:18:37 -0500

    use https://github.com/cypress-io/cypress/compare/v13?expand=1 to verify changes are correct

--- packages/server/lib/cloud/api.ts
+++ packages/server/lib/cloud/api.ts
@@ -35,6 +35,7 @@ let responseCache = {}
 
 class DecryptionError extends Error {
   isDecryptionError = true
+
   constructor (message: string) {
     super(message)
     this.name = 'DecryptionError'
