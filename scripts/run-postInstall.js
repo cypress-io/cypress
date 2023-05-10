5095802f9967e19d296accb3b4f256ba5de8b119
Ryan Manuel <ryanm@cypress.io>
2023-05-10 12:43:19 -0500

    fix issue with better-sqlite3

--- scripts/run-postInstall.js
+++ scripts/run-postInstall.js
@@ -3,8 +3,8 @@ const { execSync } = require('child_process')
 const executionEnv = process.env.CI ? 'ci' : 'local'
 
 const postInstallCommands = {
-  local: 'patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build && yarn build-v8-snapshot-dev',
-  ci: 'patch-package && yarn clean && gulp postinstall',
+  local: 'patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn workspace @packages/server rebuild-better-sqlite3 && yarn build && yarn build-v8-snapshot-dev',
+  ci: 'patch-package && yarn clean && gulp postinstall && yarn workspace @packages/server rebuild-better-sqlite3',
 }
 
 execSync(postInstallCommands[executionEnv], {
