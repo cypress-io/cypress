b629fa390a7169a39d5d076396739f3994195927
Emily Rohrbough <emilyrohrbough@yahoo.com>
2023-05-11 08:05:24 -0500

    use v11 syntax, not v13

--- npm/webpack-preprocessor/test/e2e/e2e.spec.js
+++ npm/webpack-preprocessor/test/e2e/e2e.spec.js
@@ -5,7 +5,7 @@ const globby = require('globby')
 describe('can test', async () => {
   // runs every test in cypress/tests/e2e as its own test
   // the comment above the test will determine the assertion on the results
-  globby.globbySync(path.join(__dirname, '../../cypress/tests/e2e/**/*'))
+  globby.sync(path.join(__dirname, '../../cypress/tests/e2e/**/*'))
   .map((v) => {
     const filename = path.relative(process.cwd(), v)
 
