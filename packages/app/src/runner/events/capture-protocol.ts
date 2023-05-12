cf7a73e821d8a41c0bcc6a3863db642132f07df9
Ryan Manuel <ryanm@cypress.io>
2023-05-11 19:25:03 -0500

    fix for race condition

--- packages/app/src/runner/events/capture-protocol.ts
+++ packages/app/src/runner/events/capture-protocol.ts
@@ -1,11 +1,21 @@
 export const addCaptureProtocolListeners = (Cypress: Cypress.Cypress) => {
   Cypress.on('log:added', (log) => {
+    // TODO: UNIFY-1318 - Race condition in unified runner - we should not need this null check
+    if (!Cypress.runner) {
+      return
+    }
+
     const displayProps = Cypress.runner.getDisplayPropsForLog(log)
 
     Cypress.backend('protocol:command:log:added', displayProps)
   })
 
   Cypress.on('log:changed', (log) => {
+    // TODO: UNIFY-1318 - Race condition in unified runner - we should not need this null check
+    if (!Cypress.runner) {
+      return
+    }
+
     const displayProps = Cypress.runner.getDisplayPropsForLog(log)
 
     Cypress.backend('protocol:command:log:changed', displayProps)
