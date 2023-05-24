50e352580fc9ecbc5330d2a4d003bd30c58e4d9f
mjhenkes <mjhenkes@gmail.com>
2023-05-24 12:26:16 -0500

    service ports :/

--- scripts/run-docker-local.sh
+++ scripts/run-docker-local.sh
@@ -11,4 +11,4 @@ echo "Starting Docker image with cypress volume attached"
 echo "You should be able to edit files locally"
 echo "but execute the code in the container"
 
-docker compose run ${IMAGE}
+docker compose run --service-ports ${IMAGE}
