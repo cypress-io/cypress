003cdae9ccf5b80a3da52cce1969eaae78f452c1
mjhenkes <mjhenkes@gmail.com>
2023-05-24 12:24:02 -0500

    add docker-dev script

--- scripts/run-docker-local.sh
+++ scripts/run-docker-local.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+IMAGE=${1:-ci}
+
 set e+x
 
 echo "This script should be run from cypress's root"
 
-docker compose build ci
+docker compose build ${IMAGE}
 
 echo "Starting Docker image with cypress volume attached"
 echo "You should be able to edit files locally"
 echo "but execute the code in the container"
 
-docker compose run ci
+docker compose run ${IMAGE}
