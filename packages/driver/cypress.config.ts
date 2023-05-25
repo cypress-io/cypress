4e3a9774cc8cec917b4d6df72537b74bdfefbc47
mjhenkes <mjhenkes@gmail.com>
2023-05-25 12:19:55 -0500

    clean up

--- packages/driver/cypress.config.ts
+++ packages/driver/cypress.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
     configFile: '../../mocha-reporter-config.json',
   },
   videoCompression: false, // turn off video compression for CI
-  video: false,
   e2e: {
     experimentalOriginDependencies: true,
     experimentalModifyObstructiveThirdPartyCode: true,
