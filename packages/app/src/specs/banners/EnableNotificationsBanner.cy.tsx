f7a4556068af273214eabea4b777ca82b004dfe4
astone123 <adams@cypress.io>
2023-05-26 08:49:47 -0700

    remove extra percy snapshot

--- packages/app/src/specs/banners/EnableNotificationsBanner.cy.tsx
+++ packages/app/src/specs/banners/EnableNotificationsBanner.cy.tsx
@@ -1,7 +1,7 @@
 import EnableNotificationsBanner from './EnableNotificationsBanner.vue'
 
 describe('EnableNotificationsBanner', () => {
-  [1200, 1000, 800].forEach((viewportWidth) => {
+  [1200, 800].forEach((viewportWidth) => {
     it(`renders at ${viewportWidth}px width`, { viewportWidth }, () => {
       cy.mount(<EnableNotificationsBanner />)
       cy.percySnapshot()
