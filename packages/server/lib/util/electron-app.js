c17b9ac0b49628b93280c5044110fa7915ca9130
Stokes Player <stokes.player@gmail.com>
2023-06-29 13:28:44 -0400

    fix: do not override electron debug port if previously set

--- packages/server/lib/util/electron-app.js
+++ packages/server/lib/util/electron-app.js
@@ -24,11 +24,20 @@ const getRemoteDebuggingPort = () => {
 
 const setRemoteDebuggingPort = async () => {
   try {
-    const port = await getPort()
     const { app } = require('electron')
+
+    // if port was already set via passing from environment variable ELECTRON_EXTRA_LAUNCH_ARGS,
+    // then just keep the supplied value
+    if (app.commandLine.getSwitchValue('remote-debugging-port')) {
+      return
+    }
+
+    const port = await getPort()
 
     // set up remote debugging port
     app.commandLine.appendSwitch('remote-debugging-port', String(port))
+
+    return app.commandLine.appendSwitch
   } catch (err) {
     // Catch errors for when we're running outside of electron in development
     return
