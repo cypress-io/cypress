c3c685519d74acde908d1620bc4517d4f26de3bd
David Kasper <dave.m.kasper@gmail.com>
2023-07-11 19:54:30 -0500

    add unit tests

--- packages/server/test/unit/browsers/cdp_automation_spec.ts
+++ packages/server/test/unit/browsers/cdp_automation_spec.ts
@@ -1,10 +1,61 @@
 const { expect, sinon } = require('../../spec_helper')
 
+import { ProtocolManagerShape } from '@packages/types'
 import { CdpAutomation } from '../../../lib/browsers/cdp_automation'
 
 context('lib/browsers/cdp_automation', () => {
   context('.CdpAutomation', () => {
     let cdpAutomation: CdpAutomation
+
+    describe('create', function () {
+      it('networkEnabledOptions - protocol enabled', async function () {
+        const enabledObject = {
+          maxPostDataSize: 64 * 1024,
+        }
+        const localCommand = sinon.stub()
+        const localOnFn = sinon.stub()
+        const localSendCloseTargetCommand = sinon.stub()
+        const localAutomation = {
+          onBrowserPreRequest: sinon.stub(),
+          onRequestEvent: sinon.stub(),
+        }
+        const localManager = {
+          protocolEnabled: true,
+        } as ProtocolManagerShape
+
+        const localCommmandStub = localCommand.withArgs('Network.enable', enabledObject).resolves()
+
+        await CdpAutomation.create(localCommand, localOnFn, localSendCloseTargetCommand, localAutomation as any, localManager)
+
+        expect(localCommmandStub).to.have.been.calledWith('Network.enable', enabledObject)
+      })
+
+      it('networkEnabledOptions - protocol disabled', async function () {
+        const disabledObject = {
+          maxTotalBufferSize: 0,
+          maxResourceBufferSize: 0,
+          maxPostDataSize: 0,
+        }
+        const localCommand = sinon.stub()
+        const localOnFn = sinon.stub()
+        const localSendCloseTargetCommand = sinon.stub()
+        const localAutomation = {
+          onBrowserPreRequest: sinon.stub(),
+          onRequestEvent: sinon.stub(),
+        }
+        const localManager = {
+          protocolEnabled: false,
+        } as ProtocolManagerShape
+
+        const localCommmandStub = localCommand.withArgs('Network.enable', disabledObject).resolves()
+
+        await CdpAutomation.create(localCommand, localOnFn, localSendCloseTargetCommand, localAutomation as any, localManager)
+        await CdpAutomation.create(localCommand, localOnFn, localSendCloseTargetCommand, localAutomation as any)
+
+        expect(localCommmandStub).to.have.been.calledTwice
+        expect(localCommmandStub).to.have.been.calledWithExactly('Network.enable', disabledObject)
+      })
+    })
 
     beforeEach(async function () {
       this.sendDebuggerCommand = sinon.stub()
