314179d4610c526d287751885a3397ee83d1c18b
astone123 <adams@cypress.io>
2023-07-12 12:29:53 -0700

    update stdout for script, skip workspace upload [run ci]

--- scripts/binary/trigger-publish-binary-pipeline.ts
+++ scripts/binary/trigger-publish-binary-pipeline.ts
@@ -4,13 +4,16 @@ import os from 'os'
 
 const pipelineInfoFilePath = '~/triggered_pipeline.json'
 
-exec(`curl --fail --request POST \
+const curlCommand = `curl --fail --request POST \
 --url https://circleci.com/api/v2/project/github/cypress-io/publish-binary/pipeline \
 --header 'Circle-Token: ${process.env.CIRCLE_TOKEN}' \
 --header 'content-type: application/json' \
 --output ${pipelineInfoFilePath} \
 --data '{ "parameters": { "temp_dir": "${os.tmpdir()}", "sha": "${process.env.CIRCLE_SHA1}", "job_name": "${process.env.CIRCLE_JOB}", "binary_artifact_url": "https://output.circle-artifacts.com/output/job/${process.env.CIRCLE_WORKFLOW_JOB_ID}/artifacts/${process.env.CIRCLE_NODE_INDEX}/cypress-dist.tgz", "triggered_workflow_id": "${process.env.CIRCLE_WORKFLOW_ID}" }}'`
-, (error, stdout) => {
+
+console.log(curlCommand)
+
+exec(curlCommand, (error, _, stdout) => {
   if (error) {
     throw new Error(`could not trigger publish-binary pipeline ${error}`)
   }
