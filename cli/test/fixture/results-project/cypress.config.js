4a1658dda19811abb28965a80096f0274634c703
Chris Breiding <chrisbreiding@gmail.com>
2023-07-18 11:14:13 -0400

    add debug logging

--- cli/test/fixture/results-project/cypress.config.js
+++ cli/test/fixture/results-project/cypress.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const fs = require('fs-extra')
 const path = require('path')
 
@@ -7,11 +8,37 @@ module.exports = {
   e2e: {
     setupNodeEvents (on) {
       on('after:spec', (spec, results) => {
-        return fs.outputJSON(path.resolve(__dirname, '_results', 'results-after-spec.json'), { spec, results }, { spaces: 2 })
+        console.log('🔵🔵🔵🔵 after:spec 🔵🔵🔵🔵')
+
+        const filePath = path.resolve(__dirname, '_results', 'results-after-spec.json')
+
+        console.log('filePath:', filePath)
+
+        return fs.outputJson(filePath, { spec, results }, { spaces: 2 })
+        .then(() => {
+          console.log('🟢🟢🟢🟢 after:spec success 🟢🟢🟢🟢')
+        })
+        .catch((err) => {
+          console.log('🔴🔴🔴🔴 after:spec fail 🔴🔴🔴🔴')
+          console.log(err.stack)
+        })
       })
 
       on('after:run', (results) => {
-        return fs.outputJSON(path.resolve(__dirname, '_results', 'results-after-run.json'), results, { spaces: 2 })
+        console.log('🔵🔵🔵🔵 after:run 🔵🔵🔵🔵')
+
+        const filePath = path.resolve(__dirname, '_results', 'results-after-run.json')
+
+        console.log('filePath:', filePath)
+
+        return fs.outputJson(filePath, results, { spaces: 2 })
+        .then(() => {
+          console.log('🟢🟢🟢🟢 after:run success 🟢🟢🟢🟢')
+        })
+        .catch((err) => {
+          console.log('🔴🔴🔴🔴 after:run fail 🔴🔴🔴🔴')
+          console.log(err.stack)
+        })
       })
     },
     supportFile: false,
