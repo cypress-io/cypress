23c9c71e2cff4a0d821cf0d78225e4c4671351f1
Alex Schwartz <alexschwartz01@gmail.com>
2023-07-27 11:13:33 -0400

    Update spec.cy.js

--- system-tests/projects/default-browser-config/cypress/e2e/spec.cy.js
+++ system-tests/projects/default-browser-config/cypress/e2e/spec.cy.js
@@ -1,11 +1,11 @@
-it("Test running without defaultBrowser config option", () => {
+it("Test running without browser config option", () => {
     cy.exec('cd default-browser-unset && npx cypress run --quiet').then((e) => {
         expect(e.stdout).to.contain('electron')
         expect(e.stdout).to.not.contain('chrome')
     })
 })
 
-it("Test running with defaultBrowser: 'chrome' config option", () => {
+it("Test running with browser: 'chrome' config option", () => {
     cy.exec('cd default-browser-chrome && npx cypress run --quiet').then((e) => {
         expect(e.stdout).to.contain('chrome')
         expect(e.stdout).to.not.contain('electron')
