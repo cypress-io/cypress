c56b8a038dff7dda2f225f699798cdd159d8bb81
Bill Glesias <bglesias@gmail.com>
2023-08-07 00:06:31 -0400

    chore: mark tests that need to be updated as node 12 and 14 are no longer supported. node 12 doesnt ship with fs/promises, which causes fatal errors with babel-loader 9
    try to test against new docker images [run ci]

--- system-tests/test-binary/node_versions_spec.ts
+++ system-tests/test-binary/node_versions_spec.ts
@@ -26,10 +26,9 @@ function smokeTestDockerImage (dockerImage: string) {
 
 describe('binary node versions', () => {
   [
-    'cypress/base:12',
-    'cypress/base:14',
     'cypress/base:16.16.0',
-    'cypress/base:17.3.0',
+    'cypress/base:18.15.0',
+    'cypress/base:20.5.0',
   ].forEach(smokeTestDockerImage)
 })
 
@@ -37,6 +36,7 @@ describe('type: module', () => {
   [
     'cypress/base:16.17.0',
     'cypress/base:18.6.0',
+    'cypress/base:20.5.0',
   ].forEach((dockerImage) => {
     systemTests.it(`can run in ${dockerImage}`, {
       withBinary: true,
