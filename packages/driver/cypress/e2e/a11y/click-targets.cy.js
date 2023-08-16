b864aaa9b010b66906d45bf6f2d8d524cd5f34d7
David Kasper <dave.m.kasper@gmail.com>
2023-08-16 15:33:39 -0500

    functional with basic button color

--- packages/driver/cypress/e2e/a11y/click-targets.cy.js
+++ packages/driver/cypress/e2e/a11y/click-targets.cy.js
@@ -2,52 +2,52 @@
 
 describe('a11y - click targets', () => {
   beforeEach(() => {
-    cy.visit('/fixtures/a11y/a11y-click-targets.html')
+    cy.visit('/fixtures/a11y/click-targets.html')
   })
 
-  context('button', () => {
-    it('receives native click event', () => {
+  context('button - handles click behavior', () => {
+    it('styled', () => {
       cy.get('#test-a11y-semantic-button').click()
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Click - SEMANTIC Button')
     })
 
-    it('receives native click event', () => {
+    it('styled', () => {
       cy.get('#test-a11y-div-button').click()
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Click - DIV Button')
     })
 
-    it('receives native click event', () => {
+    it('styled', () => {
       cy.get('#test-a11y-role-button').click()
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Click - ROLE Button')
 
       cy.get('#test-a11y-role-button').type('{enter}')
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Keydown - Enter - ROLE Button')
 
       cy.get('#test-a11y-role-button').type(' ')
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Keydown - Space - ROLE Button')
     })
   })
 
   context('link', () => {
-    it('receives native click event', () => {
+    it('styled', () => {
       cy.get('#test-a11y-semantic-link').click()
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Click - SEMANTIC Link')
     })
 
-    it('receives native click event', () => {
+    it('styled', () => {
       cy.get('#test-a11y-div-link').click()
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Click - DIV Link')
     })
 
-    it('receives native click event', () => {
+    it('styled', () => {
       cy.get('#test-a11y-role-link').click()
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Click - ROLE Link')
 
       cy.get('#test-a11y-role-link').type('{enter}')
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Keydown - Enter - ROLE Link')
 
       cy.get('#test-a11y-role-link').type(' ')
-      cy.findByTestId('test-a11y-click-result').should('have.text', 'You are 2 commits ahead')
+      cy.get('#test-a11y-click-result').should('have.text', 'Keydown - Space - ROLE Link')
     })
   })
 })
