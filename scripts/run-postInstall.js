771ded3228c4c452288117ded2fe14a8bdf2f73d
astone123 <adams@cypress.io>
2023-08-16 15:11:15 -0600

    re-build snapshot if re-built

--- scripts/run-postInstall.js
+++ scripts/run-postInstall.js
@@ -4,7 +4,7 @@ const executionEnv = process.env.CI ? 'ci' : 'local'
 
 const postInstallCommands = {
   local: 'patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build && yarn build-v8-snapshot-dev',
-  ci: 'patch-package && yarn clean && gulp postinstall && yarn build',
+  ci: 'patch-package && yarn clean && gulp postinstall && yarn build && V8_SNAPSHOT_DISABLE_MINIFY=1 yarn build-v8-snapshot-prod',
 }
 
 execSync(postInstallCommands[executionEnv], {
