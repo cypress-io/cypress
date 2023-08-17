42133bcbbb92734d880bd3b988b94222e3be753c
Carolina Urrea <curestrepo@gmail.com>
2023-08-17 18:10:03 -0500

    update visability check for left and right

--- packages/driver/src/dom/visibility.ts
+++ packages/driver/src/dom/visibility.ts
@@ -323,11 +323,12 @@ const elIsOutOfBoundsOfAncestorsOverflow = function ($el, $ancestor = getParent(
 
     // target el is out of bounds
     if (
+
       // target el is to the right of the ancestor's visible area
-      (elProps.fromElWindow.left > (ancestorProps.width + ancestorProps.fromElWindow.left)) ||
+      (elProps.fromElWindow.left >= (ancestorProps.width + ancestorProps.fromElWindow.left)) ||
 
       // target el is to the left of the ancestor's visible area
-      ((elProps.fromElWindow.left + elProps.width) < ancestorProps.fromElWindow.left) ||
+      ((elProps.fromElWindow.left + elProps.width) <= ancestorProps.fromElWindow.left) ||
 
       // target el is under the ancestor's visible area
       (elProps.fromElWindow.top > (ancestorProps.height + ancestorProps.fromElWindow.top)) ||
