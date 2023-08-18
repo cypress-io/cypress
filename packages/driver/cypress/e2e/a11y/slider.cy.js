6bfe342340447130b19d1551fe188cb50b340337
David Kasper <dave.m.kasper@gmail.com>
2023-08-18 16:05:45 -0500

    basic interaction

--- packages/driver/cypress/e2e/a11y/slider.cy.js
+++ packages/driver/cypress/e2e/a11y/slider.cy.js
@@ -5,15 +5,27 @@ describe('a11y - slider', () => {
 
   context('slider - handles activation', () => {
     it('semantic', () => {
-      cy.get('#test-a11y-semantic-slider')
+      cy.get('#test-a11y-semantic-slider').as('range')
+      .focus()
+      .invoke('val', 1)
+      .trigger('change')
+      .should('have.value', '1')
     })
 
     it('styled div', () => {
-      cy.get('#test-a11y-styled-slider')
+      cy.get('#test-a11y-styled-slider-container').click(20, 4)
     })
 
     it('role', () => {
-      cy.get('#test-a11y-role-slider')
+      cy.get('#test-a11y-role-slider-container')
+      .click(20, 4)
+      .should('have.attr', 'aria-valuenow')
+      .and('equal', '1')
+
+      cy.get('#test-a11y-role-slider-container')
+      .type('{rightArrow}')
+      .should('have.attr', 'aria-valuenow')
+      .and('equal', '2')
     })
   })
 })
