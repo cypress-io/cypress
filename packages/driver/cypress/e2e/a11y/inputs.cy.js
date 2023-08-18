2e392f9ca82245fc3a27e2ea9ad8ced70edeabdc
David Kasper <dave.m.kasper@gmail.com>
2023-08-18 11:58:51 -0500

    working input tests, next slider

--- packages/driver/cypress/e2e/a11y/inputs.cy.js
+++ packages/driver/cypress/e2e/a11y/inputs.cy.js
@@ -5,33 +5,31 @@ describe('a11y - click targets', () => {
 
   context('checkbox - handles activation', () => {
     it('semantic', () => {
-      cy.get('#test-a11y-semantic-checkbox').click()
+      cy.get('#test-a11y-semantic-checkbox').click().should('have.value', 'on')
     })
 
     it('styled div', () => {
-      cy.get('#test-a11y-styled-checkbox').click()
+      cy.get('#test-a11y-styled-checkbox').click().should('have.class', 'checked')
     })
 
     it('role', () => {
-      cy.get('#test-a11y-role-checkbox').click()
+      cy.get('#test-a11y-role-checkbox').click().should('have.attr', 'aria-checked').and('equal', 'true')
 
-      cy.get('#test-a11y-role-checkbox').type('{enter}')
-
-      cy.get('#test-a11y-role-checkbox').type(' ')
+      cy.get('#test-a11y-role-checkbox').type(' ').should('have.attr', 'aria-checked').and('equal', 'false')
     })
   })
 
   context('text - handles text entry', () => {
     it('semantic', () => {
-      cy.get('#test-a11y-semantic-text').type('test value')
+      cy.get('#test-a11y-semantic-text').type('test value').should('have.value', 'test value')
     })
 
     it('styled div', () => {
-      cy.get('#test-a11y-styled-text').type('test value')
+      cy.get('#test-a11y-styled-text').type('test value').should('have.text', 'test value')
     })
 
     it('role', () => {
-      cy.get('#test-a11y-role-text').type('test value')
+      cy.get('#test-a11y-role-text').type('test value').should('have.text', 'test value')
     })
   })
 })
