1de6d8454972c5d6c7deed96454530236a284191
Adam Stone-Lord <adams@cypress.io>
2023-08-22 14:45:41 -0400

    chore: remove gulp tasks from postinstall (#27616)

--- scripts/run-postInstall.js
+++ scripts/run-postInstall.js
@@ -3,8 +3,8 @@ const { execSync } = require('child_process')
 const executionEnv = process.env.CI ? 'ci' : 'local'
 
 const postInstallCommands = {
-  local: 'patch-package && yarn-deduplicate --strategy=highest && yarn clean && gulp postinstall && yarn build && yarn build-v8-snapshot-dev',
-  ci: 'patch-package && yarn clean && gulp postinstall && yarn build && V8_SNAPSHOT_DISABLE_MINIFY=1 yarn build-v8-snapshot-prod',
+  local: 'patch-package && yarn-deduplicate --strategy=highest && yarn build && yarn build-v8-snapshot-dev',
+  ci: 'patch-package && yarn build && V8_SNAPSHOT_DISABLE_MINIFY=1 yarn build-v8-snapshot-prod',
 }
 
 execSync(postInstallCommands[executionEnv], {
