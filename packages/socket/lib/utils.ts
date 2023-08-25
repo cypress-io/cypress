bbedffa762e031971538e4587b67968d904806df
Ryan Manuel <ryanm@cypress.io>
2023-08-25 13:34:08 -0500

    add error handling

--- packages/socket/lib/utils.ts
+++ packages/socket/lib/utils.ts
@@ -3,37 +3,45 @@ import * as socketIoParser from 'socket.io-parser'
 import * as engineParser from 'engine.io-parser'
 
 export const encode = (data: any, namespace: string): Promise<string> => {
-  return new Promise((resolve) => {
-    const encoder = new socketIoParser.Encoder()
-    const socketIoEncodedData = encoder.encode({
-      type: socketIoParser.PacketType.EVENT,
-      data,
-      nsp: namespace,
-    })
+  return new Promise((resolve, reject) => {
+    try {
+      const encoder = new socketIoParser.Encoder()
+      const socketIoEncodedData = encoder.encode({
+        type: socketIoParser.PacketType.EVENT,
+        data,
+        nsp: namespace,
+      })
 
-    engineParser.encodePayload(socketIoEncodedData.map((item) => {
-      return {
-        type: 'message',
-        data: item,
-      }
-    }), (encoded: any) => {
-      resolve(encoded)
-    })
+      engineParser.encodePayload(socketIoEncodedData.map((item) => {
+        return {
+          type: 'message',
+          data: item,
+        }
+      }), (encoded: any) => {
+        resolve(encoded)
+      })
+    } catch (err) {
+      reject(err)
+    }
   })
 }
 
 export const decode = (data: any): Promise<any> => {
-  return new Promise((resolve) => {
-    const decoded = engineParser.decodePayload(data)
-    const decoder = new socketIoParser.Decoder()
+  return new Promise((resolve, reject) => {
+    try {
+      const decoded = engineParser.decodePayload(data)
+      const decoder = new socketIoParser.Decoder()
 
-    decoder.on('decoded', (packet: any) => {
-      decoder.destroy()
-      resolve(packet.data)
-    })
+      decoder.on('decoded', (packet: any) => {
+        decoder.destroy()
+        resolve(packet.data)
+      })
 
-    decoded.forEach((packet: any) => {
-      decoder.add(packet.data)
-    })
+      decoded.forEach((packet: any) => {
+        decoder.add(packet.data)
+      })
+    } catch (error) {
+      reject(error)
+    }
   })
 }
