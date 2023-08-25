4293b98f7a883957f31d9f24086a736aa0c3917b
Alex Schwartz <alexschwartz01@gmail.com>
2023-08-25 13:10:50 -0400

    Update uninstallDependenciesInScaffoldedProject.ts

--- packages/launchpad/cypress/tasks/uninstallDependenciesInScaffoldedProject.ts
+++ packages/launchpad/cypress/tasks/uninstallDependenciesInScaffoldedProject.ts
@@ -1,8 +1,8 @@
-import fs from 'fs'
+import { promises as fs } from 'fs'
 import path from 'path'
 
 export async function uninstallDependenciesInScaffoldedProject ({ currentProject }) {
-  await fs.promises.rm(path.resolve(currentProject, '../node_modules'), { recursive: true, force: true })
+  await fs.rm(path.resolve(currentProject, '../node_modules'), { recursive: true, force: true })
 
   return null
 }
