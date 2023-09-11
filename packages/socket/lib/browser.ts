27b44ed1a67b17e529b5e8016c8470587cc9f239
Ryan Manuel <ryanm@cypress.io>
2023-09-11 10:32:12 -0500

    add comments

--- packages/socket/lib/browser.ts
+++ packages/socket/lib/browser.ts
@@ -10,17 +10,20 @@ declare global {
   }
 }
 
-window.cypressSockets ||= {}
 let chromium = false
 
 export function client (uri: string, opts?: Partial<ManagerOptions & SocketOptions>): SocketShape {
   if (chromium) {
     const fullNamespace = `${opts?.path}${uri}`
 
+    // When running in Chromium and with a baseUrl set to something that includes basic auth: (e.g. http://user:pass@localhost:1234), the assets
+    // will load twice. Thus, we need to add the cypress sockets to the window object rather than just relying on a local variable.
+    window.cypressSockets ||= {}
     if (!window.cypressSockets[fullNamespace]) {
       window.cypressSockets[fullNamespace] = new CDPBrowserSocket(fullNamespace)
     }
 
+    // Connect the socket regardless of whether or not we have newly created it
     window.cypressSockets[fullNamespace].connect()
 
     return window.cypressSockets[fullNamespace]
@@ -35,10 +38,14 @@ export function createWebsocket ({ path, browserFamily }: { path: string, browse
 
     const fullNamespace = `${path}/default`
 
+    // When running in Chromium and with a baseUrl set to something that includes basic auth: (e.g. http://user:pass@localhost:1234), the assets
+    // will load twice. Thus, we need to add the cypress sockets to the window object rather than just relying on a local variable.
+    window.cypressSockets ||= {}
     if (!window.cypressSockets[fullNamespace]) {
       window.cypressSockets[fullNamespace] = new CDPBrowserSocket(fullNamespace)
     }
 
+    // Connect the socket regardless of whether or not we have newly created it
     window.cypressSockets[fullNamespace].connect()
 
     return window.cypressSockets[fullNamespace]
