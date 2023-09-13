4e6dcf9804f0381a916f1cd09fe9b96a5e354bd8
jordanpowell88 <jordan@jpdesigning.com>
2023-09-13 12:28:32 -0400

    update snapshots

--- scripts/__snapshots__/move-binaries-spec.js
+++ scripts/__snapshots__/move-binaries-spec.js
@@ -1,12 +1,3 @@
-exports['move-binaries parseBuildPath parses into SHA and build 1'] = {
-  'path': 'beta/binary/3.3.0/darwin-x64/circle-develop-47e98fa1d0b18867a74da91a719d0f1ae73fcbc7-101843/',
-  'parsed': {
-    'commit': '47e98fa1d0b18867a74da91a719d0f1ae73fcbc7',
-    'build': 101843,
-    's3path': 'beta/binary/3.3.0/darwin-x64/circle-develop-47e98fa1d0b18867a74da91a719d0f1ae73fcbc7-101843/',
-  },
-}
-
 exports['collected builds and copied desktop'] = {
   'lastBuilds': [
     {
@@ -20,4 +11,13 @@ exports['collected builds and copied desktop'] = {
       's3zipPath': 'desktop/3.3.0/darwin-x64/cypress.zip',
     },
   ],
+}
+
+exports['move-binaries parseBuildPath parses into SHA and build 1'] = {
+  'path': 'beta/binary/3.3.0/darwin-x64/circle-develop-47e98fa1d0b18867a74da91a719d0f1ae73fcbc7-101843/',
+  'parsed': {
+    'commit': '47e98fa1d0b18867a74da91a719d0f1ae73fcbc7',
+    'build': 101843,
+    's3path': 'beta/binary/3.3.0/darwin-x64/circle-develop-47e98fa1d0b18867a74da91a719d0f1ae73fcbc7-101843/',
+  },
 }
