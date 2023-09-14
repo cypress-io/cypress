8ddeb41b8f9f0ffc0c4fb4f7bab6924d7e30a42e
Cacie Prins <cacie@cypress.io>
2023-09-14 15:17:36 -0400

    revert snapshot for tab close system test

--- packages/server/lib/browsers/browser-cri-client.ts
+++ packages/server/lib/browsers/browser-cri-client.ts
@@ -158,7 +158,6 @@ export class BrowserCriClient {
             closing: browserCriClient.closing,
             closed: browserCriClient.closed,
             resettingBrowserTargets: browserCriClient.resettingBrowserTargets,
-            currentlyAttachedTarget: browserCriClient.currentlyAttachedTarget,
           })
 
           // we may have gotten a delayed "Target.targetDestroyed" even for a page that we
@@ -203,10 +202,6 @@ export class BrowserCriClient {
           })
           .timeout(500)
           .then((expectedDestroyedEvent) => {
-            debug({
-              expectedDestroyedEvent,
-            })
-
             if (expectedDestroyedEvent === true) {
               return
             }
