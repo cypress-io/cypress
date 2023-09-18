da48ea5f9709ddb086c7b508a7fade0e0e91ad1b
mjhenkes <mjhenkes@gmail.com>
2023-09-18 08:57:26 -0500

    chore: use docker compose to run cypress.

--- scripts/run-docker-local.sh
+++ scripts/run-docker-local.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+SERVICE=${1:-ci}
+
 set e+x
 
 echo "This script should be run from cypress's root"
 
-name=cypress/browsers:node16.16.0-chrome106-ff99-edge
-echo "Pulling CI container $name"
+docker compose build ${SERVICE}
 
-docker pull $name
-
-echo "Starting Docker image with cypress volume attached"
+echo "Starting Docker compose service, $SERVICE, with cypress volume attached"
 echo "You should be able to edit files locally"
 echo "but execute the code in the container"
 
-docker run -v $PWD:/home/person/cypress \
-  -w /home/person/cypress${WORKING_DIR:-} \
-  -it $name \
-  /bin/bash
+docker compose run --service-ports ${SERVICE}
