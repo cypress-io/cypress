fd1ca2ad60c8bdd1f5aa9d65254b68f5bb40ce99
Matthew Schile <mschile@cypress.io>
2023-09-19 14:39:17 -0600

    updating test to handle electron

--- packages/driver/cypress/e2e/commands/actions/click.cy.js
+++ packages/driver/cypress/e2e/commands/actions/click.cy.js
@@ -46,6 +46,7 @@ const getMidPoint = (el) => {
 
 const isFirefox = Cypress.isBrowser('firefox')
 const isWebKit = Cypress.isBrowser('webkit')
+const isChromium116OrLater = Cypress.isBrowser({ family: 'chromium' }) && Cypress.browserMajorVersion() >= 116
 
 describe('src/cy/commands/actions/click', () => {
   beforeEach(() => {
@@ -4449,10 +4450,11 @@ describe('mouse state', () => {
 
       cy.get('#btn').click()
 
-      cy.getAll('btn', 'pointerover pointerenter mouseover mouseenter').each(shouldBeCalledOnce)
+      cy.getAll('btn', 'pointerover pointerenter').each(shouldBeCalledOnce)
 
       // On disabled inputs, pointer events are still fired in chrome, not in firefox or webkit
       cy.getAll('btn', 'pointerdown pointerup').each(isFirefox || isWebKit ? shouldNotBeCalled : shouldBeCalledOnce)
+      cy.getAll('btn', 'mouseover mouseenter').each(isFirefox || isWebKit || isChromium116OrLater ? shouldBeCalled : shouldNotBeCalled)
       cy.getAll('btn', 'mousedown mouseup click').each(shouldNotBeCalled)
     })
 
