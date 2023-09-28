1503fe22d7fee6e5e4fe3f2dea2905571a292964
Matthew Schile <mschile@cypress.io>
2023-09-28 13:37:59 -0600

    removing 119 check

--- system-tests/projects/e2e/cypress/e2e/headless_old.cy.js
+++ system-tests/projects/e2e/cypress/e2e/headless_old.cy.js
@@ -1,9 +1,5 @@
 describe('e2e headless old spec', function () {
   it('has expected launch args', function () {
-    if (Cypress.isBrowser({ family: 'chromium' }) && Cypress.browserMajorVersion() >= 119) {
-      cy.fail('headless old is supported in Chromium >= 119, please update this system test.')
-    }
-
     cy.task('get:browser:args').should('contain', '--headless=old')
   })
 })
