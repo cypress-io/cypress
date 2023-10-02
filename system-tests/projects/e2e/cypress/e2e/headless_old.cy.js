59fb48bf1e73e1e2384d398dffd1da00cd074184
Matt Schile <mschile@cypress.io>
2023-10-02 14:15:26 -0600

    test: re-enable test for --headless=old (#27932)

--- system-tests/projects/e2e/cypress/e2e/headless_old.cy.js
+++ system-tests/projects/e2e/cypress/e2e/headless_old.cy.js
@@ -1,9 +1,5 @@
 describe('e2e headless old spec', function () {
   it('has expected launch args', function () {
-    if (Cypress.isBrowser({ family: 'chromium' }) && Cypress.browserMajorVersion() >= 119) {
-      cy.fail('headless old is supported in Chromium >= 119, please update this system test.')
-    }
-    // TODO: re-enable this once https://bugs.chromium.org/p/chromium/issues/detail?id=1483163 is resolved
-    // cy.task('get:browser:args').should('contain', '--headless=old')
+    cy.task('get:browser:args').should('contain', '--headless=old')
   })
 })
