6b2232469fc44aee7b307cdaec7d22875316cbf4
Cacie Prins <cacie@cypress.io>
2023-10-02 12:09:57 -0400

    fix screenshot res in electron tab crash snap

--- system-tests/__snapshots__/browser_crash_handling_spec.js
+++ system-tests/__snapshots__/browser_crash_handling_spec.js
@@ -149,7 +149,7 @@ https://on.cypress.io/renderer-process-crashed
 
   (Screenshots)
 
-  -  /XXX/XXX/XXX/cypress/screenshots/chrome_tab_crash.cy.js/navigates to about html     (2560x1440)
+  -  /XXX/XXX/XXX/cypress/screenshots/chrome_tab_crash.cy.js/navigates to about html      (1280x720)
      (failed).png                                                                                   
 
 
