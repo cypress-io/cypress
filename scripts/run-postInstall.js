1e066dee6b742daf8335ff84dd84ecb3da41006b
David Kasper <dave.m.kasper@gmail.com>
2023-10-03 18:58:40 -0500

    remove unneeded yarn in lerna command

--- scripts/run-postInstall.js
+++ scripts/run-postInstall.js
@@ -3,8 +3,8 @@ const { execSync } = require('child_process')
 const executionEnv = process.env.CI ? 'ci' : 'local'
 
 const postInstallCommands = {
-  local: 'patch-package && yarn-deduplicate --strategy=highest && yarn lerna run rebuild-better-sqlite3 --scope @packages/server && yarn build && yarn build-v8-snapshot-dev',
-  ci: 'patch-package && yarn lerna run rebuild-better-sqlite3 --scope @packages/server',
+  local: 'patch-package && yarn-deduplicate --strategy=highest && lerna run rebuild-better-sqlite3 --scope @packages/server && yarn build && yarn build-v8-snapshot-dev',
+  ci: 'patch-package && lerna run rebuild-better-sqlite3 --scope @packages/server',
 }
 
 execSync(postInstallCommands[executionEnv], {
