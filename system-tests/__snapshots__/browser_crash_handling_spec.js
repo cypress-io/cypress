9bf8025386982a4580022ec75ca60027f392f402
Cacie Prins <cacie@cypress.io>
2023-10-05 14:57:14 -0400

    update snapshots

--- system-tests/__snapshots__/browser_crash_handling_spec.js
+++ system-tests/__snapshots__/browser_crash_handling_spec.js
@@ -43,17 +43,11 @@ https://on.cypress.io/renderer-process-crashed
   │ Failing:      1                                                                                │
   │ Pending:      0                                                                                │
   │ Skipped:      0                                                                                │
-  │ Screenshots:  1                                                                                │
+  │ Screenshots:  0                                                                                │
   │ Video:        false                                                                            │
   │ Duration:     X seconds                                                                        │
   │ Spec Ran:     chrome_tab_crash.cy.js                                                           │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-
-
-  (Screenshots)
-
-  -  /XXX/XXX/XXX/cypress/screenshots/chrome_tab_crash.cy.js/navigates to about html      (1280x720)
-     (failed).png                                                                                   
 
 
 ────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -142,17 +136,11 @@ https://on.cypress.io/renderer-process-crashed
   │ Failing:      1                                                                                │
   │ Pending:      0                                                                                │
   │ Skipped:      0                                                                                │
-  │ Screenshots:  1                                                                                │
+  │ Screenshots:  0                                                                                │
   │ Video:        false                                                                            │
   │ Duration:     X seconds                                                                        │
   │ Spec Ran:     chrome_tab_crash.cy.js                                                           │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-
-
-  (Screenshots)
-
-  -  /XXX/XXX/XXX/cypress/screenshots/chrome_tab_crash.cy.js/navigates to about html      (1280x720)
-     (failed).png                                                                                   
 
 
 ────────────────────────────────────────────────────────────────────────────────────────────────────
