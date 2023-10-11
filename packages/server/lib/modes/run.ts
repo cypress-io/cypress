cypress-io/cypress

commit 1a6f87922cb4722792b5ead43de7d32feaf3ca19

fix: report results correctly when the browser crashes mid-test (#27786)
* report correct number of failures if browser crashes in the middle of a test suite

* report failure correctly when browser crashes during test

* refactor crash handling

* correct exit option check, clean up debug

* exit on success if exit option !== false

* use default stats when reporter stats are unavailable

* fix error messaging

* move reporter types to an intermediate .ts file, to be combined with reporter.js when migrated

* debug tab close test in ci

* move debug env from pkg to ci yml

* set debug env in spec

* fix pckg

* adds some logging to cri-client

* remove event emit logging from project-base

* revert snapshot for tab close system test

* fixes console output for no exit on success

* changelog

* changelog wsp

* cleanup

* clean up tests

* refactor to more straightforward control flow

* rm export for unused type

* correct tab close snapshot for ci

* new system test for mid-test config crash

* update snapshots

packages/server/lib/modes/run.ts
@@ -21,12 +21,13 @@ import random from '../util/random'
 import system from '../util/system'
 import chromePolicyCheck from '../util/chrome_policy_check'
 import type { SpecWithRelativeRoot, SpecFile, TestingType, OpenProjectLaunchOpts, FoundBrowser, BrowserVideoController, VideoRecording, ProcessOptions } from '@packages/types'
-import type { Cfg } from '../project-base'
+import type { Cfg, ProjectBase } from '../project-base'
 import type { Browser } from '../browsers/types'
 import * as printResults from '../util/print-run'
 import type { ProtocolManager } from '../cloud/protocol'
 import { telemetry } from '@packages/telemetry'
 import { CypressRunResult, createPublicBrowser, createPublicConfig, createPublicRunResults, createPublicSpec, createPublicSpecResults } from './results'
+import { EarlyExitTerminator } from '../util/graceful_crash_handling'
 
 type SetScreenshotMetadata = (data: TakeScreenshotProps) => void
 type ScreenshotMetadata = ReturnType<typeof screenshotMetadata>
@@ -36,17 +37,12 @@ type BeforeSpecRun = any
 type AfterSpecRun = any
 type Project = NonNullable<ReturnType<typeof openProject['getProject']>>
 
-let exitEarly = (err) => {
-  debug('set early exit error: %s', err.stack)
-
-  earlyExitErr = err
-}
-let earlyExitErr: Error
 let currentSetScreenshotMetadata: SetScreenshotMetadata
 
 const debug = Debug('cypress:server:run')
-
 const DELAY_TO_LET_VIDEO_FINISH_MS = 1000
+
+let earlyExitTerminator = new EarlyExitTerminator()
 
 const relativeSpecPattern = (projectRoot, pattern) => {
   if (typeof pattern === 'string') {
@@ -411,53 +407,30 @@ function launchBrowser (options: { browser: Browser, spec: SpecWithRelativeRoot,
   return openProject.launch(browser, spec, browserOpts)
 }
 
-function listenForProjectEnd (project, exit): Bluebird<any> {
+async function listenForProjectEnd (project: ProjectBase, exit: boolean): Promise<any> {
   if (globalThis.CY_TEST_MOCK?.listenForProjectEnd) return Bluebird.resolve(globalThis.CY_TEST_MOCK.listenForProjectEnd)
 
-  return new Bluebird((resolve, reject) => {
-    if (exit === false) {
-      resolve = () => {
+  // if exit is false, we need to intercept the resolution of tests - whether
+  // an early exit with intermediate results, or a full run.
+  return new Promise((resolve, reject) => {
+    Promise.race([
+      new Promise((res) => {
+        project.once('end', (results) => {
+          debug('project ended with results %O', results)
+          res(results)
+        })
+      }),
+      earlyExitTerminator.waitForEarlyExit(project, exit),
+    ]).then((results) => {
+      if (exit === false) {
+        // eslint-disable-next-line no-console
         console.log('not exiting due to options.exit being false')
+      } else {
+        resolve(results)
       }
-    }
-
-    const onEarlyExit = function (err) {
-      if (err.isFatalApiErr) {
-        return reject(err)
-      }
-
-      console.log('')
-      errors.log(err)
-
-      const obj = {
-        error: errors.stripAnsi(err.message),
-        stats: {
-          failures: 1,
-          tests: 0,
-          passes: 0,
-          pending: 0,
-          suites: 0,
-          skipped: 0,
-          wallClockDuration: 0,
-          wallClockStartedAt: new Date().toJSON(),
-          wallClockEndedAt: new Date().toJSON(),
-        },
-      }
-
-      return resolve(obj)
-    }
-
-    project.once('end', (results) => resolve(results))
-
-    // if we already received a reason to exit early, go ahead and do it
-    if (earlyExitErr) {
-      return onEarlyExit(earlyExitErr)
-    }
-
-    // otherwise override exitEarly so we exit as soon as there is a reason
-    exitEarly = (err) => {
-      onEarlyExit(err)
-    }
+    }).catch((err) => {
+      reject(err)
+    })
   })
 }
 
@@ -729,6 +702,13 @@ async function waitForTestsToFinishRunning (options: { project: Project, screens
   if (videoCaptureFailed || videoCompressionFailed) {
     results.video = null
   }
+
+  // the early exit terminator persists between specs,
+  // so if this spec crashed, the next one will report as
+  // a crash too unless it is reset. Would like to not rely
+  // on closure, but threading through fn props via options is also not
+  // great.
+  earlyExitTerminator = new EarlyExitTerminator()
 
   return results
 }
@@ -1005,7 +985,11 @@ async function ready (options: ReadyOptions) {
   // this needs to be a closure over `exitEarly` and not a reference
   // because `exitEarly` gets overwritten in `listenForProjectEnd`
   // TODO: refactor this so we don't need to extend options
-  const onError = options.onError = (err) => exitEarly(err)
+
+  const onError = options.onError = (err) => {
+    debug('onError')
+    earlyExitTerminator.exitEarly(err)
+  }
 
   // alias and coerce to null
   let specPatternFromCli = options.spec || null
@@ -1140,6 +1124,7 @@ async function ready (options: ReadyOptions) {
 }
 
 export async function run (options, loading: Promise<void>) {
+  debug('run start')
   // Check if running as electron process
   if (require('../util/electron-app').isRunningAsElectronProcess({ debug })) {
     const app = require('electron').app
@@ -1161,6 +1146,8 @@ export async function run (options, loading: Promise<void>) {
   try {
     return ready(options)
   } catch (e) {
-    return exitEarly(e)
+    debug('caught outer error', e)
+
+    return earlyExitTerminator.exitEarly(e)
   }
 }

packages/server/lib/project-base.ts
@@ -376,7 +376,6 @@ export class ProjectBase extends EE {
       },
 
       onMocha: async (event, runnable) => {
-        debug('onMocha', event)
         // bail if we dont have a
         // reporter instance
         if (!reporterInstance) {
@@ -385,7 +384,12 @@ export class ProjectBase extends EE {
 
         reporterInstance.emit(event, runnable)
 
-        if (event === 'end') {
+        if (event === 'test:before:run') {
+          this.emit('test:before:run', {
+            runnable,
+            previousResults: reporterInstance?.results() || {},
+          })
+        } else if (event === 'end') {
           const [stats = {}] = await Promise.all([
             (reporterInstance != null ? reporterInstance.end() : undefined),
             this.server.end(),

packages/server/lib/types/reporter.ts
@@ -1,0 +1,48 @@
+interface ReporterTestAttempt {
+  state: 'skipped' | 'failed' | 'passed'
+  error: any
+  timings: any
+  failedFromHookId: any
+  wallClockStartedAt: Date
+  wallClockDuration: number
+  videoTimestamp: any
+}
+interface ReporterTest {
+  testId: string
+  title: string[]
+  state: 'skipped' | 'passed' | 'failed'
+  body: string
+  displayError: any
+  attempts: ReporterTestAttempt[]
+}
+
+export interface BaseReporterResults {
+  error?: string
+  stats: {
+    failures: number
+    tests: number
+    passes: number
+    pending: number
+    suites: number
+    skipped: number
+    wallClockDuration: number
+    wallClockStartedAt: string
+    wallClockEndedAt: string
+  }
+}
+
+export interface ReporterResults extends BaseReporterResults {
+  reporter: string
+  reporterStats: {
+    suites: number
+    tests: number
+    passes: number
+    pending: number
+    failures: number
+    start: string
+    end: string
+    duration: number
+  }
+  hooks: any[]
+  tests: ReporterTest[]
+}

packages/server/lib/util/graceful_crash_handling.ts
@@ -1,0 +1,115 @@
+import type { ProjectBase } from '../project-base'
+import type { BaseReporterResults, ReporterResults } from '../types/reporter'
+import * as errors from '../errors'
+import Debug from 'debug'
+import pDefer, { DeferredPromise } from 'p-defer'
+
+const debug = Debug('cypress:util:crash_handling')
+
+const patchRunResultsAfterCrash = (error: Error, reporterResults: ReporterResults, mostRecentRunnable: any): ReporterResults => {
+  const endTime: number = reporterResults?.stats?.wallClockEndedAt ? Date.parse(reporterResults?.stats?.wallClockEndedAt) : new Date().getTime()
+  const wallClockDuration = reporterResults?.stats?.wallClockStartedAt ?
+    endTime - Date.parse(reporterResults.stats.wallClockStartedAt) : 0
+  const endTimeStamp = new Date(endTime).toJSON()
+
+  // in crash situations, the most recent report will not have the triggering test
+  // so the results are manually patched, which produces the expected exit=1 and
+  // terminal output indicating the failed test
+  return {
+    ...reporterResults,
+    stats: {
+      ...reporterResults?.stats,
+      wallClockEndedAt: endTimeStamp,
+      wallClockDuration,
+      failures: (reporterResults?.stats?.failures ?? 0) + 1,
+      skipped: (reporterResults?.stats?.skipped ?? 1) - 1,
+    },
+    reporterStats: {
+      ...reporterResults?.reporterStats,
+      tests: (reporterResults?.reporterStats?.tests ?? 0) + 1, // crashed test does not increment this value
+      end: reporterResults?.reporterStats?.end || endTimeStamp,
+      duration: wallClockDuration,
+      failures: (reporterResults?.reporterStats?.failures ?? 0) + 1,
+    },
+    tests: (reporterResults?.tests || []).map((test) => {
+      if (test.testId === mostRecentRunnable.id) {
+        return {
+          ...test,
+          state: 'failed',
+          attempts: [
+            ...test.attempts.slice(0, -1),
+            {
+              ...test.attempts[test.attempts.length - 1],
+              state: 'failed',
+            },
+          ],
+        }
+      }
+
+      return test
+    }),
+    error: errors.stripAnsi(error.message),
+  }
+}
+
+const defaultStats = (error: Error): BaseReporterResults => {
+  return {
+    error: errors.stripAnsi(error.message),
+    stats: {
+      failures: 1,
+      tests: 0,
+      passes: 0,
+      pending: 0,
+      suites: 0,
+      skipped: 0,
+      wallClockDuration: 0,
+      wallClockStartedAt: new Date().toJSON(),
+      wallClockEndedAt: new Date().toJSON(),
+    },
+  }
+}
+
+export class EarlyExitTerminator {
+  private terminator: DeferredPromise<BaseReporterResults>
+
+  private pendingRunnable: any
+  private intermediateStats: ReporterResults | undefined
+
+  constructor () {
+    this.terminator = pDefer<BaseReporterResults>()
+  }
+
+  waitForEarlyExit (project: ProjectBase, exit?: boolean) {
+    debug('waiting for early exit')
+
+    project.on('test:before:run', ({
+      runnable,
+      previousResults,
+    }) => {
+      debug('preparing to run test, previous stats reported as %O', previousResults)
+
+      this.intermediateStats = previousResults
+      this.pendingRunnable = runnable
+    })
+
+    return this.terminator.promise
+  }
+
+  exitEarly (error) {
+    if (error.isFatalApiErr) {
+      this.terminator.reject(error)
+
+      return
+    }
+
+    // eslint-disable-next-line no-console
+    console.log('')
+    errors.log(error)
+
+    const runResults: BaseReporterResults = (this.intermediateStats && this.pendingRunnable) ?
+      patchRunResultsAfterCrash(error, this.intermediateStats, this.pendingRunnable) :
+      defaultStats(error)
+
+    this.terminator.resolve(runResults)
+  }
+}

system-tests/__snapshots__/browser_crash_handling_spec.js
@@ -17,7 +17,8 @@ exports['Browser Crash Handling / when the tab crashes in chrome / fails'] = `
   Running:  chrome_tab_crash.cy.js                                                          (1 of 2)
 
 
-  1) navigates to about /html
+  a test suite with a browser crash
+    ✓ navigates to about:blank
 
 We detected that the Chrome Renderer process just crashed.
 
@@ -37,22 +38,16 @@ https://on.cypress.io/renderer-process-crashed
   (Results)
 
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ Tests:        0                                                                                │
-  │ Passing:      0                                                                                │
+  │ Tests:        2                                                                                │
+  │ Passing:      1                                                                                │
   │ Failing:      1                                                                                │
   │ Pending:      0                                                                                │
   │ Skipped:      0                                                                                │
-  │ Screenshots:  1                                                                                │
+  │ Screenshots:  0                                                                                │
   │ Video:        false                                                                            │
   │ Duration:     X seconds                                                                        │
   │ Spec Ran:     chrome_tab_crash.cy.js                                                           │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-
-
-  (Screenshots)
-
-  -  /XXX/XXX/XXX/cypress/screenshots/chrome_tab_crash.cy.js/navigates to about html      (1280x720)
-     (failed).png                                                                                   
 
 
 ────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -87,11 +82,11 @@ https://on.cypress.io/renderer-process-crashed
 
        Spec                                              Tests  Passing  Failing  Pending  Skipped  
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ ✖  chrome_tab_crash.cy.js                   XX:XX        -        -        1        -        - │
+  │ ✖  chrome_tab_crash.cy.js                   XX:XX        2        1        1        -        - │
   ├────────────────────────────────────────────────────────────────────────────────────────────────┤
   │ ✔  simple.cy.js                             XX:XX        1        1        -        -        - │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-    ✖  1 of 2 failed (50%)                      XX:XX        1        1        1        -        -  
+    ✖  1 of 2 failed (50%)                      XX:XX        3        2        1        -        -  
 
 
 `
@@ -115,7 +110,8 @@ exports['Browser Crash Handling / when the tab crashes in electron / fails'] = `
   Running:  chrome_tab_crash.cy.js                                                          (1 of 2)
 
 
-  1) navigates to about /html
+  a test suite with a browser crash
+    ✓ navigates to about:blank
 
 We detected that the Electron Renderer process just crashed.
 
@@ -135,22 +131,16 @@ https://on.cypress.io/renderer-process-crashed
   (Results)
 
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ Tests:        0                                                                                │
-  │ Passing:      0                                                                                │
+  │ Tests:        2                                                                                │
+  │ Passing:      1                                                                                │
   │ Failing:      1                                                                                │
   │ Pending:      0                                                                                │
   │ Skipped:      0                                                                                │
-  │ Screenshots:  1                                                                                │
+  │ Screenshots:  0                                                                                │
   │ Video:        false                                                                            │
   │ Duration:     X seconds                                                                        │
   │ Spec Ran:     chrome_tab_crash.cy.js                                                           │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-
-
-  (Screenshots)
-
-  -  /XXX/XXX/XXX/cypress/screenshots/chrome_tab_crash.cy.js/navigates to about html      (1280x720)
-     (failed).png                                                                                   
 
 
 ────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -185,11 +175,11 @@ https://on.cypress.io/renderer-process-crashed
 
        Spec                                              Tests  Passing  Failing  Pending  Skipped  
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ ✖  chrome_tab_crash.cy.js                   XX:XX        -        -        1        -        - │
+  │ ✖  chrome_tab_crash.cy.js                   XX:XX        2        1        1        -        - │
   ├────────────────────────────────────────────────────────────────────────────────────────────────┤
   │ ✔  simple.cy.js                             XX:XX        1        1        -        -        - │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-    ✖  1 of 2 failed (50%)                      XX:XX        1        1        1        -        -  
+    ✖  1 of 2 failed (50%)                      XX:XX        3        2        1        -        -  
 
 
 `
@@ -229,7 +219,7 @@ This can happen for many different reasons:
   (Results)
 
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ Tests:        0                                                                                │
+  │ Tests:        1                                                                                │
   │ Passing:      0                                                                                │
   │ Failing:      1                                                                                │
   │ Pending:      0                                                                                │
@@ -273,11 +263,11 @@ This can happen for many different reasons:
 
        Spec                                              Tests  Passing  Failing  Pending  Skipped  
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ ✖  chrome_process_crash.cy.js               XX:XX        -        -        1        -        - │
+  │ ✖  chrome_process_crash.cy.js               XX:XX        1        -        1        -        - │
   ├────────────────────────────────────────────────────────────────────────────────────────────────┤
   │ ✔  simple.cy.js                             XX:XX        1        1        -        -        - │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-    ✖  1 of 2 failed (50%)                      XX:XX        1        1        1        -        -  
+    ✖  1 of 2 failed (50%)                      XX:XX        2        1        1        -        -  
 
 
 `
@@ -317,7 +307,7 @@ This can happen for many different reasons:
   (Results)
 
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ Tests:        0                                                                                │
+  │ Tests:        1                                                                                │
   │ Passing:      0                                                                                │
   │ Failing:      1                                                                                │
   │ Pending:      0                                                                                │
@@ -371,11 +361,11 @@ This can happen for many different reasons:
 
        Spec                                              Tests  Passing  Failing  Pending  Skipped  
   ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │ ✖  chrome_process_crash.cy.js               XX:XX        -        -        1        -        - │
+  │ ✖  chrome_process_crash.cy.js               XX:XX        1        -        1        -        - │
   ├────────────────────────────────────────────────────────────────────────────────────────────────┤
   │ ✔  simple.cy.js                             XX:XX        1        1        -        -        - │
   └────────────────────────────────────────────────────────────────────────────────────────────────┘
-    ✖  1 of 2 failed (50%)                      XX:XX        1        1        1        -        -  
+    ✖  1 of 2 failed (50%)                      XX:XX        2        1        1        -        -  
 
 
 `

system-tests/projects/config-with-crashing-plugin/cypress-with-project-id.config.js
@@ -1,0 +1,15 @@
+module.exports = {
+  'projectId': 'pid123',
+  'e2e': {
+    'supportFile': false,
+    setupNodeEvents (on, config) {
+      on('before:spec', () => {
+        setTimeout(() => {
+          throw new Error('Async error from plugins file')
+        }, 1000)
+      })
+
+      return config
+    },
+  },
+}

system-tests/projects/config-with-crashing-plugin/cypress/e2e/simple_multiple.cy.js
@@ -1,0 +1,13 @@
+describe('suite', function () {
+  it('is true', () => {
+    expect(true).to.be.true
+    cy.wait(150)
+  })
+
+  it('is still true', () => {
+  // the config should crash before this test completes;
+  // this is a long wait in order to improve predictability
+    cy.wait(10000)
+    expect(true).to.be.true
+  })
+})

commit 5efecf9a4a72fbb0199a23917623218b843bd97c

chore: fix problem with single tab run mode introduced by #27786 (#28032)
* chore: fix problem introduced by <pr>

* Update packages/server/lib/modes/run.ts

packages/server/lib/modes/run.ts
@@ -545,10 +545,10 @@ function waitForSocketConnection (project: Project, id: string) {
   })
 }
 
-async function waitForTestsToFinishRunning (options: { project: Project, screenshots: ScreenshotMetadata[], videoCompression: number | boolean, exit: boolean, spec: SpecWithRelativeRoot, estimated: number, quiet: boolean, config: Cfg, shouldKeepTabOpen: boolean, testingType: TestingType, videoRecording?: VideoRecording, protocolManager?: ProtocolManager }) {
+async function waitForTestsToFinishRunning (options: { project: Project, screenshots: ScreenshotMetadata[], videoCompression: number | boolean, exit: boolean, spec: SpecWithRelativeRoot, estimated: number, quiet: boolean, config: Cfg, shouldKeepTabOpen: boolean, isLastSpec: boolean, testingType: TestingType, videoRecording?: VideoRecording, protocolManager?: ProtocolManager }) {
   if (globalThis.CY_TEST_MOCK?.waitForTestsToFinishRunning) return Promise.resolve(globalThis.CY_TEST_MOCK.waitForTestsToFinishRunning)
 
-  const { project, screenshots, videoRecording, videoCompression, exit, spec, estimated, quiet, config, shouldKeepTabOpen, testingType, protocolManager } = options
+  const { project, screenshots, videoRecording, videoCompression, exit, spec, estimated, quiet, config, shouldKeepTabOpen, isLastSpec, testingType, protocolManager } = options
 
   const results = await listenForProjectEnd(project, exit)
 
@@ -644,12 +644,12 @@ async function waitForTestsToFinishRunning (options: { project: Project, screens
   // @ts-expect-error experimentalSingleTabRunMode only exists on the CT-specific config type
   const usingExperimentalSingleTabMode = testingType === 'component' && config.experimentalSingleTabRunMode
 
-  if (usingExperimentalSingleTabMode) {
+  if (usingExperimentalSingleTabMode && !isLastSpec) {
     await project.server.destroyAut()
   }
 
-  // we do not support experimentalSingleTabRunMode for e2e
-  if (!usingExperimentalSingleTabMode) {
+  // we do not support experimentalSingleTabRunMode for e2e. We always want to close the tab on the last spec to ensure that things get cleaned up properly at the end of the run
+  if (!usingExperimentalSingleTabMode || isLastSpec) {
     debug('attempting to close the browser tab')
 
     await openProject.resetBrowserTabsForNextTest(shouldKeepTabOpen)
@@ -920,6 +920,7 @@ async function runSpec (config, spec: SpecWithRelativeRoot, options: { project: 
       videoCompression: options.videoCompression,
       quiet: options.quiet,
       shouldKeepTabOpen: !isLastSpec,
+      isLastSpec,
       protocolManager: options.protocolManager,
     }),
     waitForBrowserToConnect({