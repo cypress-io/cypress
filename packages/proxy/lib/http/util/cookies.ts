5385c6f39cc39c7dca291aea39eb1213a30e2174
Emily Rohrbough <emilyrohrbough@yahoo.com>
2024-01-05 10:13:08 -0600

    run ci

--- packages/proxy/lib/http/util/cookies.ts
+++ packages/proxy/lib/http/util/cookies.ts
@@ -5,7 +5,7 @@ import { cors } from '@packages/network'
 import { urlOriginsMatch, urlSameSiteMatch } from '@packages/network/lib/cors'
 import { SerializableAutomationCookie, Cookie, CookieJar, toughCookieToAutomationCookie } from '@packages/server/lib/util/cookies'
 import type { RequestCredentialLevel } from '../../types'
-import type { ResourceType } from 'cypress/types/net-stubbing'
+import type { ResourceType } from '@packages/net-stubbing'
 
 type SiteContext = 'same-origin' | 'same-site' | 'cross-site'
 
