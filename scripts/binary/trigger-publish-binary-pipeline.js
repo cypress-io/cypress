a7a5a6b422060c3e2e67df5570e1c34ef453929d
Cacie Prins <cacie@cypress.io>
2024-01-12 13:02:05 -0500

    point to consolidated binary publish branch

--- scripts/binary/trigger-publish-binary-pipeline.js
+++ scripts/binary/trigger-publish-binary-pipeline.js
@@ -10,7 +10,7 @@ const { getNextVersionForBinary } = require('../get-next-version')
   const { nextVersion } = await getNextVersionForBinary()
 
   const body = JSON.stringify({
-    branch: 'ryanm/chore/more-27-changes',
+    branch: 'cacie/upgrade-electron-27',
     parameters: {
       temp_dir: os.tmpdir(),
       sha: process.env.CIRCLE_SHA1,
