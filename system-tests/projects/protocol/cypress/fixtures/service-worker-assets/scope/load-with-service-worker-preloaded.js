983f2ade8eba19678a6fe4006296c4bea48e10ac
Ryan Manuel <ryanm@cypress.io>
2024-01-18 18:42:03 -0600

    get prerelease

--- system-tests/projects/protocol/cypress/fixtures/service-worker-assets/scope/load-with-service-worker-preloaded.js
+++ system-tests/projects/protocol/cypress/fixtures/service-worker-assets/scope/load-with-service-worker-preloaded.js
@@ -1,4 +1,6 @@
 navigator.serviceWorker?.ready.then(async () => {
+  // Wait for the service worker to be ready and deterministically ensure these requests are handled by the service worker
+  await new Promise((resolve) => setTimeout(resolve, 500))
   const exampleResponse = await fetch('/cypress/fixtures/service-worker-assets/example.json')
   const example = await exampleResponse.json()
   const cachedResponse = await fetch('/cypress/fixtures/service-worker-assets/scope/cached-service-worker.json')
