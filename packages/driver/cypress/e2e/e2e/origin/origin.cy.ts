9ea0e9c98b1ae3db5a48031d5f5774f904800c10
Jennifer Shehane <jennifer@cypress.io>
2024-02-08 14:40:03 -0500

    chore: Move retry tests that are intended to retry to system tests to reduce flake (#28881)
    * Move currentRetries tests to a system test to reduce flake
    
    * empty commit
    
    * remove typo on test title
    
    * Migrate origin retries test, which purposefully retries, into a system test
    
    * Snapshot updates around the 'errors errors' typo
    
    * Fix port issue
    
    * Update origin test to reuse existing html files in directory

--- packages/driver/cypress/e2e/e2e/origin/origin.cy.ts
+++ packages/driver/cypress/e2e/e2e/origin/origin.cy.ts
@@ -154,69 +154,6 @@ describe('cy.origin', { browser: '!webkit' }, () => {
       cy.log('after cy.origin')
     })
 
-    ;[{
-      title: 'first attempt',
-      retries: 0,
-    }, {
-      title: 'on retry',
-      retries: 1,
-    }].forEach(({ title, retries }) => {
-      it(`passes runnable state to the secondary origin ${title}`, { retries }, () => {
-        const runnable = cy.state('runnable')
-        const expectedRunnable = {
-          clearTimeout: null,
-          isPending: null,
-          resetTimeout: null,
-          timeout: null,
-          id: runnable.id,
-          _currentRetry: retries,
-          _timeout: 4000,
-          type: 'test',
-          title: `passes runnable state to the secondary origin ${title}`,
-          titlePath: [
-            'cy.origin',
-            'withBeforeEach',
-            `passes runnable state to the secondary origin ${title}`,
-          ],
-          parent: {
-            id: runnable.parent.id,
-            type: 'suite',
-            title: 'withBeforeEach',
-            titlePath: [
-              'withBeforeEach',
-            ],
-            parent: {
-              id: runnable.parent.parent.id,
-              type: 'suite',
-              title: '',
-              titlePath: undefined,
-              ctx: {},
-            },
-            ctx: {},
-          },
-          ctx: {},
-        }
-
-        cy.origin('http://www.foobar.com:3500', { args: expectedRunnable }, (expectedRunnable) => {
-          const actualRunnable = cy.state('runnable')
-
-          expect(actualRunnable.titlePath()).to.deep.equal(expectedRunnable.titlePath)
-          expectedRunnable.titlePath = actualRunnable.titlePath
-
-          expect(actualRunnable.title).to.equal(expectedRunnable.title)
-          expect(actualRunnable.id).to.equal(expectedRunnable.id)
-          expect(actualRunnable.ctx).to.deep.equal(expectedRunnable.ctx)
-          expect(actualRunnable._currentRetry).to.equal(expectedRunnable._currentRetry)
-          expect(actualRunnable._timeout).to.equal(expectedRunnable._timeout)
-          expect(actualRunnable.type).to.equal(expectedRunnable.type)
-          expect(actualRunnable.callback).to.exist
-          expect(actualRunnable.timeout).to.exist
-          expect(actualRunnable.parent.title).to.equal(expectedRunnable.parent.title)
-          expect(actualRunnable.parent.type).to.equal(expectedRunnable.parent.type)
-        })
-      })
-    })
-
     it('handles querying nested elements', () => {
       cy.origin('http://www.foobar.com:3500', () => {
         cy
