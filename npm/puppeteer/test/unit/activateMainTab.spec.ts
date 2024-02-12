d6becbaf993fabcf1eeb5fb0da8c388963bd65ea
Cacie Prins <cacie@cypress.io>
2024-02-12 11:21:23 -0500

    tests for activateMainTab

---
+++ npm/puppeteer/test/unit/activateMainTab.spec.ts
@@ -0,0 +1,55 @@
+import { expect, use } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+import { activateMainTab, ACTIVATION_TIMEOUT } from '../../src/plugin/activateMainTab'
+
+use(chaiAsPromised)
+use(sinonChai)
+
+describe('activateMainTab', () => {
+  let clock: sinon.SinonFakeTimers
+  let prevWin: Window
+  let window: Partial<Window>
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers()
+
+    window = {
+      addEventListener: sinon.stub(),
+      removeEventListener: sinon.stub(),
+      // @ts-ignore
+      postMessage: sinon.stub(),
+    }
+
+    prevWin = global.window
+    // @ts-ignore
+    global.window = window
+  })
+
+  afterEach(() => {
+    clock.restore()
+    // @ts-ignore
+    global.window = prevWin
+  })
+
+  it('sends a tab activation request to the plugin, and resolves when the ack event is received', () => {
+    const p = activateMainTab()
+
+    expect(window.postMessage).to.be.calledWith({ message: 'cypress:extension:activate:main:tab' })
+
+    // @ts-ignore
+    window.addEventListener.yields({ message: 'cypress:extension:main:tab:activated' })
+
+    return expect(p).to.eventually.be.true
+  })
+
+  it('sends a tab activation request to the plugin, and rejects if it times out', () => {
+    const p = activateMainTab()
+
+    clock.tick(ACTIVATION_TIMEOUT + 1)
+
+    expect(p).to.be.rejected
+  })
+})
