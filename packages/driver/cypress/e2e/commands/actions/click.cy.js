fb0f330811dd3cf13bb88beb31d5920e28738597
Teo Anastasiadis <31965686+TheoAnastasiadis@users.noreply.github.com>
2024-02-15 22:39:52 +0200

    Update packages/driver/cypress/e2e/commands/actions/click.cy.js
    Co-authored-by: Bill Glesias <bglesias@gmail.com>

--- packages/driver/cypress/e2e/commands/actions/click.cy.js
+++ packages/driver/cypress/e2e/commands/actions/click.cy.js
@@ -561,7 +561,9 @@ describe('src/cy/commands/actions/click', () => {
 
       cy.get('button:first span').click()
 
-      cy.getAll('btn', 'mouseenter mousedown mouseup').each(shouldBeCalled)
+      if (Cypress.browser.name === 'chrome') {
+          cy.getAll('btn', 'mouseenter mousedown mouseup').each(shouldBeCalled)
+      }
       cy.getAll('btn', 'focus click').each(shouldNotBeCalled)
       cy.getAll('span', 'mouseenter mousedown mouseup').each(shouldBeCalled)
       cy.getAll('span', 'focus click').each(shouldNotBeCalled)
