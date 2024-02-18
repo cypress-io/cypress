ac26ec40f8ac9d166dbacf08ce6d7a1521c3a336
ezra <ezra.mizrahi@hey.com>
2024-02-19 10:16:44 +1100

    export knownBrowsers

--- packages/launcher/index.ts
+++ packages/launcher/index.ts
@@ -2,10 +2,13 @@ import { detect, detectByPath } from './lib/detect'
 
 import { launch } from './lib/browsers'
 
+import { knownBrowsers } from './lib/known-browsers'
+
 export {
   detect,
   detectByPath,
   launch,
+  knownBrowsers,
 }
 
 export * from './lib/types'
