f59361fb3c0952a4648078b5c3d5a6576e88932f
Cacie Prins <cacie@cypress.io>
2024-03-05 17:18:25 -0500

    workable templated filestream uploader

--- packages/server/lib/cloud/uploadStream.ts
+++ packages/server/lib/cloud/uploadStream.ts
@@ -1,41 +1,43 @@
-import type { ReadStream } from 'fs'
+import type { Readable } from 'stream'
+import fetch from 'cross-fetch'
+
 //import { Transform } from 'stream'
 
-/*
-class HttpError extends Error {
+export class HttpError extends Error {
   constructor (
-    public readonly statusCode: number,
-    public readonly status: string,
-    public readonly url: string
+    public readonly status: number,
+    public readonly statusText: string,
+    public readonly url: string,
   ) {
-    super(`${statusCode}`)
+    super(`${status}: ${statusText}`)
   }
 }
-*/
 
-export const uploadStream = <T>(fileStream: ReadStream, destinationUrl: string, opts: {
-  startTimeout: number
-  stallThreshold: number
-}): Promise<T> => {
-  return new Promise<T>((resolve, reject) => {
-    /*
-    const abortController = new AbortController()
+export const uploadStream = <T>(fileStream: Readable, destinationUrl: string, opts: {
+  startTimeout?: number
+  stallThreshold?: number
+} = {}): Promise<T | string | undefined> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(destinationUrl, {
+        method: 'PUT',
+        // @ts-expect-error - this is supported
+        body: fileStream,
+      })
 
-    const fail = (err) => {
-      abortController.abort()
-      // clear upload begin, stall timeouts
-      reject(err)
+      if (response.status === 404) {
+        resolve(undefined)
+      } else if (response.status >= 400) {
+        reject(new HttpError(response.status, response.statusText, destinationUrl))
+      } else {
+        if (response.headers.get('Content-Type') === 'application/json') {
+          resolve(await response.json())
+        } else {
+          resolve(await response.text())
+        }
+      }
+    } catch (e) {
+      reject(e)
     }
-
-    fileStream.on('error', fail)
-
-    const progressMonitor = new Transform({
-      transform (chunk, encoding, callback) {
-        // reset stall timeout
-        // clear start timeout
-        callback(null, chunk)
-      },
-    })
-*/
   })
 }
--- packages/server/test/unit/cloud/uploadStream_spec.ts
+++ packages/server/test/unit/cloud/uploadStream_spec.ts
@@ -1,44 +1,106 @@
-//import { uploadStream } from '../../../lib/cloud/uploadStream'
-//import { ReadStream } from 'fs'
-//import { PassThrough } from 'stream'
+import chai, { expect } from 'chai'
+
+import chaiAsPromised from 'chai-as-promised'
+import { uploadStream, HttpError } from '../../../lib/cloud/uploadStream'
+
+import { PassThrough } from 'stream'
+
+chai.use(chaiAsPromised)
+
 import nock from 'nock'
 
 describe('uploadStream', () => {
-  let destinationUrl
-  //let fileStream: ReadStream
+  let destinationUrl: string
+  let destinationPath: string
+  let destinationDomain: string
+  let fileStream: PassThrough
+  let uploadPromise: Promise<any>
   let scope: nock.Scope
 
+  function completeStream () {
+    fileStream.emit('readable')
+    fileStream.emit('data', 'data')
+    fileStream.emit('end')
+    fileStream.emit('finish')
+    fileStream.emit('close')
+  }
+
   beforeEach(() => {
-    //fileStream = new ReadStream()
-    destinationUrl = 'http://somedomain.test'
-
-    scope = nock(destinationUrl)
+    fileStream = new PassThrough()
+    destinationDomain = 'http://somedomain.test:80'
+    destinationPath = '/upload'
+    destinationUrl = `${destinationDomain}${destinationPath}`
+    scope = nock(destinationDomain)
   })
-  /*
-  describe('when file stream emits an error', () => {
-
-  })
-
-  describe('when fetch rejects with a connection error', () => {
-
-  })
-
-  describe('when fetch resolves with an HTTP error', () => {
-
-  })
-
-  describe('when upload does not begin within timeout window', () => {
-
-  })
-
-  describe('when upload stalls', () => {
-
-  })
-*/
 
   describe('when fetch resolves with a 200 OK', () => {
     beforeEach(() => {
-      scope.put('/').reply(200, 'OK')
+      scope.put(destinationPath).reply(200, 'OK')
+    })
+
+    it(`resolves with 'OK'`, async () => {
+      uploadPromise = uploadStream<string>(fileStream, destinationUrl)
+      completeStream()
+      await uploadPromise
+      expect(uploadPromise).to.eventually.equal('OK')
+    })
+  })
+
+  describe('when fetch resolves with a 200 JSON response', () => {
+    let response: {
+      message: string
+    }
+
+    beforeEach(() => {
+      response = { message: 'ok' }
+
+      scope.put(destinationPath).reply(200, JSON.stringify(response), {
+        'Content-type': 'application/json',
+      })
+    })
+
+    it('resolves with the json decoded response', async () => {
+      uploadPromise = uploadStream(fileStream, destinationUrl)
+      completeStream()
+      await uploadPromise
+      expect(uploadPromise).to.eventually.deep.equal(response)
+    })
+  })
+
+  describe('when fetch resolves with a 404 response', () => {
+    beforeEach(() => {
+      scope.put(destinationPath).reply(404)
+    })
+
+    it('resolves with undefined', async () => {
+      uploadPromise = uploadStream(fileStream, destinationUrl)
+      completeStream()
+      await uploadPromise
+      expect(uploadPromise).to.eventually.be.undefined
+    })
+  })
+
+  describe('when fetch resolves with a non-404 4xx/5xx response', () => {
+    const status = 403
+
+    beforeEach(() => {
+      scope.put(destinationPath).reply(status)
+    })
+
+    it('rejects with an appropriate HttpError', async () => {
+      let caught = false
+
+      try {
+        const p = uploadStream(fileStream, destinationUrl)
+
+        completeStream()
+        await p
+      } catch (e) {
+        caught = true
+        expect(e instanceof HttpError).to.be.true
+        expect((e as HttpError).status).to.eq(status)
+      }
+      expect(caught).to.be.true
     })
   })
 })
