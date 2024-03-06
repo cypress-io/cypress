6f8f349dd01b3cbcfa5be5cb1b8dbc2b20411c07
BaboonKing <changfan.xu.up@qq.com>
2024-03-06 23:44:31 +0800

    更新 rollup.config.mjs
    Co-authored-by: Bill Glesias <bglesias@gmail.com>

--- npm/vue2/rollup.config.mjs
+++ npm/vue2/rollup.config.mjs
@@ -29,4 +29,4 @@ const config = {
   },
 }
 
-export default createEntries({ formats: ['es', 'cjs'], input: 'src/index.ts', config, dtsOptions})
+export default createEntries({ formats: ['es', 'cjs'], input: 'src/index.ts', config, dtsOptions })
