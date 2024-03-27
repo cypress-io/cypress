dc130be4fb05bba211623510098d3a7b3e80aa85
Cacie Prins <cacie@cypress.io>
2024-03-27 11:59:24 -0400

    move stream activity monitor params to consts - no magic numbers. docs.

--- packages/server/lib/cloud/api/put_protocol_artifact.ts
+++ packages/server/lib/cloud/api/put_protocol_artifact.ts
@@ -6,6 +6,13 @@ import { StreamActivityMonitor } from '../upload/stream_activity_monitor'
 
 const debug = Debug('cypress:server:cloud:api:protocol-artifact')
 
+// the upload will get canceled if the source stream does not
+// begin flowing within 5 seconds, or if the stream pipeline
+// stalls (does not push data to the `fetch` sink) for more
+// than 5 seconds
+const MAX_START_DWELL_TIME = 5000
+const MAX_ACTIVITY_DWELL_TIME = 5000
+
 export const putProtocolArtifact = async (artifactPath: string, maxFileSize: number, destinationUrl: string) => {
   debug(`Atttempting to upload Test Replay archive from ${artifactPath} to ${destinationUrl})`)
   const { size } = await fsAsync.stat(artifactPath)
@@ -14,7 +21,7 @@ export const putProtocolArtifact = async (artifactPath: string, maxFileSize: num
     throw new Error(`Spec recording too large: artifact is ${size} bytes, limit is ${maxFileSize} bytes`)
   }
 
-  const activityMonitor = new StreamActivityMonitor(5000, 5000)
+  const activityMonitor = new StreamActivityMonitor(MAX_START_DWELL_TIME, MAX_ACTIVITY_DWELL_TIME)
   const fileStream = fs.createReadStream(artifactPath)
 
   await uploadStream(
