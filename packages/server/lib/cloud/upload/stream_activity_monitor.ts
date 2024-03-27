4cc05bd7f3cce36ad85c8a7fd51675edcc19891a
Cacie Prins <cacie@cypress.io>
2024-03-27 11:22:53 -0400

    improve docs for StreamActivityMonitor

--- packages/server/lib/cloud/upload/stream_activity_monitor.ts
+++ packages/server/lib/cloud/upload/stream_activity_monitor.ts
@@ -16,6 +16,34 @@ export class StreamStalledError extends Error {
   }
 }
 
+/**
+ * `StreamActivityMonitor` encapsulates state with regard to monitoring a stream
+ * for flow failure states. Given a maxStartDwellTime and a maxActivityDwellTime, this class
+ * can `monitor` a Node Readable stream and signal if the sink (e.g., a `fetch`) should be
+ * aborted via an AbortController that can be retried via `getController`. It does this
+ * by creating an identity Transform stream and piping the source stream through it. The
+ * transform stream receives each chunk that the source emits, and orchestrates some timeouts
+ * to determine if the stream has failed to start, or if the data flow has stalled.
+ *
+ * Example usage:
+ *
+ * const MAX_START_DWELL_TIME = 5000
+ * const MAX_ACTIVITY_DWELL_TIME = 5000
+ * const stallDetection = new StreamActivityMonitor(MAX_START_DWELL_TIME, MAX_ACTIVITY_DWELL_TIME)
+ * try {
+ *   const source = fs.createReadStream('/some/source/file')
+ *   await fetch('/destination/url', {
+ *     method: 'PUT',
+ *     body: stallDetection.monitor(source)
+ *     signal: stallDetection.getController().signal
+ *   })
+ * } catch (e) {
+ *   if (stallDetection.getController().signal.reason) {
+ *     // the `fetch` was aborted by the signal that `stallDetection` controlled
+ *   }
+ * }
+ *
+ */
 export class StreamActivityMonitor {
   private streamMonitor: Transform | undefined
   private startTimeout: NodeJS.Timeout | undefined
