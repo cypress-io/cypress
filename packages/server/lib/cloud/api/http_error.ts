3a739f3f4f616b5b56b187ff84b53bc84cc21d4e
Cacie Prins <cacieprins@users.noreply.github.com>
2024-03-29 12:22:57 -0400

    perf: improve replay upload resiliency (#29174)
    * tbd
    
    * workable templated filestream uploader
    
    * clean up async assertions
    
    * upload stream uses activity monitor to detect start timeouts and stalls
    
    * makes uploadStream retryable
    
    * filesize detection, enoent errors, and testable retry delays
    
    * extract fs logic to putProtocolArtifact, impl putProtocolArtifact
    
    * aggregate errors for retryable upload streams
    
    * fixes imports from moving api.ts, uses new upload mechanism in protocol.ts
    
    * use spec_helper in StreamActivityMonitor_spec due to global sinon clock changes
    
    * fix putProtocolArtifact specs when run as a part of the unit test suite
    
    * fix return type of ProtocolManager.uploadCaptureArtifact
    
    * convert from whatwg streams back to node streams
    
    * extract HttpError
    
    * ensure system test snapshots
    
    * changelog
    
    * more changelog
    
    * fix unit tests
    
    * fix api ref in integration test
    
    * fix refs to api in snapshotting and after-pack
    
    * small edits
    
    * Update packages/server/lib/cloud/api/HttpError.ts
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    * Update packages/server/lib/cloud/upload/uploadStream.ts
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    * camelcase -> snakeCase filenames
    
    * improve docs for StreamActivityMonitor
    
    * added documentation to: upload_stream, put_protocol_artifact_spec
    
    * move stream activity monitor params to consts - no magic numbers. docs.
    
    * Update packages/server/lib/cloud/api/http_error.ts
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    * Update packages/server/test/unit/cloud/api/put_protocol_artifact_spec.ts
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    * fix check-ts
    
    * fix imports in put_protocol_artifact_spec
    
    * Update packages/server/test/unit/cloud/upload/stream_activity_monitor_spec.ts
    
    Co-authored-by: Ryan Manuel <ryanm@cypress.io>
    
    * api.ts -> index.ts
    
    * fix comment style, remove confusingly inapplicable comment about whatwg streams
    
    ---------
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    Co-authored-by: Ryan Manuel <ryanm@cypress.io>

---
+++ packages/server/lib/cloud/api/http_error.ts
@@ -0,0 +1,33 @@
+const SENSITIVE_KEYS = Object.freeze(['x-amz-credential', 'x-amz-signature', 'Signature', 'AWSAccessKeyId'])
+const scrubUrl = (url: string, sensitiveKeys: readonly string[]): string => {
+  const parsedUrl = new URL(url)
+
+  for (const [key, value] of parsedUrl.searchParams) {
+    if (sensitiveKeys.includes(key)) {
+      parsedUrl.searchParams.set(key, 'X'.repeat(value.length))
+    }
+  }
+
+  return parsedUrl.href
+}
+
+export class HttpError extends Error {
+  constructor (
+    message: string,
+    public readonly originalResponse: Response,
+  ) {
+    super(message)
+  }
+
+  public static async fromResponse (response: Response): Promise<HttpError> {
+    const status = response.status
+    const statusText = await (response.json().catch(() => {
+      return response.statusText
+    }))
+
+    return new HttpError(
+      `${status} ${statusText} (${scrubUrl(response.url, SENSITIVE_KEYS)})`,
+      response,
+    )
+  }
+}
