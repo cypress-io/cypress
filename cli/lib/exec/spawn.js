670d0874da69c6cecb5a99f7089d394bb131db98
Cacie Prins <cacie@cypress.io>
2024-04-05 12:21:51 -0400

    fix: suppress benign vulkan warnings

--- cli/lib/exec/spawn.js
+++ cli/lib/exec/spawn.js
@@ -43,7 +43,34 @@ const isCertVerifyProcBuiltin = /(^\[.*ERROR:cert_verify_proc_builtin\.cc|^-----
 // objc[60540]: Class WebSwapCGLLayer is implemented in both /System/Library/Frameworks/WebKit.framework/Versions/A/Frameworks/WebCore.framework/Versions/A/Frameworks/libANGLE-shared.dylib (0x7ffa5a006318) and /{path/to/app}/node_modules/electron/dist/Electron.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Libraries/libGLESv2.dylib (0x10f8a89c8). One of the two will be used. Which one is undefined.
 const isMacOSElectronWebSwapCGLLayerWarning = /^objc\[\d+\]: Class WebSwapCGLLayer is implemented in both.*Which one is undefined\./
 
-const GARBAGE_WARNINGS = [isXlibOrLibudevRe, isHighSierraWarningRe, isRenderWorkerRe, isDbusWarning, isCertVerifyProcBuiltin, isMacOSElectronWebSwapCGLLayerWarning]
+/**
+ * Electron logs benign warnings about Vulkan when run on hosts that do not have a GPU. This is coming from the primary Electron process,
+ * and not the browser being used for tests.
+ * Samples:
+ * Warning: loader_scanned_icd_add: Driver /usr/lib/x86_64-linux-gnu/libvulkan_intel.so supports Vulkan 1.2, but only supports loader interface version 4. Interface version 5 or newer required to support this version of Vulkan (Policy #LDP_DRIVER_7)
+ * Warning: loader_scanned_icd_add: Driver /usr/lib/x86_64-linux-gnu/libvulkan_lvp.so supports Vulkan 1.1, but only supports loader interface version 4. Interface version 5 or newer required to support this version of Vulkan (Policy #LDP_DRIVER_7)
+ * Warning: loader_scanned_icd_add: Driver /usr/lib/x86_64-linux-gnu/libvulkan_radeon.so supports Vulkan 1.2, but only supports loader interface version 4. Interface version 5 or newer required to support this verison of Vulkan (Policy #LDP_DRIVER_7)
+ * Warning: Layer VK_LAYER_MESA_device_select uses API version 1.2 which is older than the application specified API version of 1.3. May cause issues.
+ */
+
+const isHostVulkanDriverWarning = /^Warning: (?:Layer VK_LAYER_MESA_device_select|loader_scanned_icd_add:(?:.*)(?:libvulkan_intel|libvulkan_lvp|libvulkan_radeon)(?:.*)LDP_DRIVER_7\)$)/
+
+/**
+ * Electron logs benign warnings about Vulkan when run in docker containers whose host does not have a GPU. This is coming from the primary
+ * Electron process, and not the browser being used for tests.
+ * Sample:
+ * Warning: vkCreateInstance: Found no drivers!
+ * Warning: vkCreateInstance failed with VK_ERROR_INCOMPATIBLE_DRIVER
+ *     at CheckVkSuccessImpl (../../third_party/dawn/src/dawn/native/vulkan/VulkanError.cpp:88)
+ *     at CreateVkInstance (../../third_party/dawn/src/dawn/native/vulkan/BackendVk.cpp:458)
+ *     at Initialize (../../third_party/dawn/src/dawn/native/vulkan/BackendVk.cpp:344)
+ *     at Create (../../third_party/dawn/src/dawn/native/vulkan/BackendVk.cpp:266)
+ *     at operator() (../../third_party/dawn/src/dawn/native/vulkan/BackendVk.cpp:521)
+ */
+
+const isContainerVulkanDriverWarning = /^(?:Warning: vkCreateInstance(\: Found no drivers!| failed with VK_ERROR_INCOMPATIBLE_DRIVER)$|\s*at (?:CheckVkSuccessImpl|CreateVkInstance|Initialize|Create|operator)(?:.*)(?:VulkanError|BackendVk))/
+
+const GARBAGE_WARNINGS = [isXlibOrLibudevRe, isHighSierraWarningRe, isRenderWorkerRe, isDbusWarning, isCertVerifyProcBuiltin, isMacOSElectronWebSwapCGLLayerWarning, isHostVulkanDriverWarning, isContainerVulkanDriverWarning]
 
 const isGarbageLineWarning = (str) => {
   return _.some(GARBAGE_WARNINGS, (re) => {
