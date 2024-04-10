8144dda1766e756d6aec2d6b1e37207a3eb1a9ce
AtofStryker <bglesias@gmail.com>
2024-04-10 16:00:39 -0400

    feat: support webpack-dev-server-5 for @cypress/webpack-dev-server [run ci]

--- npm/webpack-dev-server/src/createWebpackDevServer.ts
+++ npm/webpack-dev-server/src/createWebpackDevServer.ts
@@ -1,6 +1,7 @@
 import debugLib from 'debug'
 import type { Configuration as WebpackDevServer3Configuration } from 'webpack-dev-server-3'
 import type { Configuration as WebpackDevServer4Configuration } from 'webpack-dev-server'
+import type { Configuration as WebpackDevServer5Configuration } from 'webpack-dev-server-5'
 
 import type { WebpackDevServerConfig } from './devServer'
 import type { SourceRelativeWebpackResult } from './helpers/sourceRelativeWebpackModules'
@@ -46,6 +47,12 @@ export async function createWebpackDevServer (
   const finalWebpackConfig = await makeWebpackConfig(config)
   const webpackCompiler = webpack(finalWebpackConfig)
 
+  if (webpackDevServerMajorVersion === 5) {
+    debug('using webpack-dev-server v5')
+
+    return webpackDevServer5(config, webpackCompiler, finalWebpackConfig)
+  }
+
   if (webpackDevServerMajorVersion === 4) {
     debug('using webpack-dev-server v4')
 
@@ -59,6 +66,41 @@ export async function createWebpackDevServer (
   }
 
   throw new Error(`Unsupported webpackDevServer version ${webpackDevServerMajorVersion}`)
+}
+
+function webpackDevServer5 (
+  config: CreateFinalWebpackConfig,
+  compiler: object,
+  finalWebpackConfig: Record<string, any>,
+) {
+  const { devServerConfig: { cypressConfig: { devServerPublicPathRoute } } } = config
+  const isOpenMode = !config.devServerConfig.cypressConfig.isTextTerminal
+  const WebpackDevServer = config.sourceWebpackModulesResult.webpackDevServer.module
+  const webpackDevServerConfig: WebpackDevServer5Configuration = {
+    host: '127.0.0.1',
+    port: 'auto',
+    // @ts-ignore
+    ...finalWebpackConfig?.devServer,
+    devMiddleware: {
+      publicPath: devServerPublicPathRoute,
+      stats: finalWebpackConfig.stats ?? 'minimal',
+    },
+    hot: false,
+    // Only enable file watching & reload when executing tests in `open` mode
+    liveReload: isOpenMode,
+  }
+
+  debug(WebpackDevServer)
+  debug(webpackDevServerConfig)
+
+  const server = new WebpackDevServer(webpackDevServerConfig, compiler)
+
+  debug(server)
+
+  return {
+    server,
+    compiler,
+  }
 }
 
 function webpackDevServer4 (
