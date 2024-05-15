9985f75d50b9ccb4d397ca3ade0e389d88190738
AtofStryker <bglesias@gmail.com>
2024-05-15 13:53:31 -0400

    update trigger binary publish and bump cache [run ci]

--- scripts/binary/trigger-publish-binary-pipeline.js
+++ scripts/binary/trigger-publish-binary-pipeline.js
@@ -10,14 +10,13 @@ const { getNextVersionForBinary } = require('../get-next-version')
   const { nextVersion } = await getNextVersionForBinary()
 
   const body = JSON.stringify({
-    branch: 'electron-28',
     parameters: {
       temp_dir: os.tmpdir(),
       sha: process.env.CIRCLE_SHA1,
       job_name: process.env.CIRCLE_JOB,
       triggered_workflow_id: process.env.CIRCLE_WORKFLOW_ID,
       triggered_job_url: process.env.CIRCLE_BUILD_URL,
-      branch: process.env.CIRCLE_BRANCH,
+      branch: 'upgrade-electron-28',
       should_persist_artifacts: Boolean(process.env.SHOULD_PERSIST_ARTIFACTS),
       binary_version: nextVersion,
     },
