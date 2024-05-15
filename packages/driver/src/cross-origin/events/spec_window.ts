7bac35f15410adb36a64ded542f039c3a14fe17c
Cacie Prins <cacie@cypress.io>
2024-05-15 16:36:04 -0400

    rm more unnecessary debug

--- packages/driver/src/cross-origin/events/spec_window.ts
+++ packages/driver/src/cross-origin/events/spec_window.ts
@@ -1,17 +1,11 @@
 import type { $Cy } from '../../cypress/cy'
 import { handleErrorEvent } from './errors'
 
-import Debug from 'debug'
-
-const debug = Debug('cypress:driver:cross-origin:events')
-
 export const handleSpecWindowEvents = (cy: $Cy) => {
-  debug('handle spec window events')
   const handleWindowErrorEvent = handleErrorEvent(cy, 'spec')('error')
   const handleWindowUnhandledRejectionEvent = handleErrorEvent(cy, 'spec')('unhandledrejection')
 
   const handleUnload = () => {
-    debug('handling unload')
     window.removeEventListener('pagehide', handleUnload)
     window.removeEventListener('error', handleWindowErrorEvent)
     window.removeEventListener('unhandledrejection', handleWindowUnhandledRejectionEvent)
