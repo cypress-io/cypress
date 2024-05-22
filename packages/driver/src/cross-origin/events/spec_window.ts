c8ec30ceab5df8aeae7cf730ef00a671b2c0a586
Cacie Prins <cacieprins@users.noreply.github.com>
2024-05-22 14:06:40 -0400

    fix: preserve navigation functionality when `unload` event is deprecated (#29525)
    * chore: Force deprecation of unload event
    
    * empty commit
    
    * use pagehide instead of unload event
    
    * tslint
    
    * bump cache
    
    * some debugging
    
    * rm debug
    
    * comment out forced deprecation of unload - tbd how to enable
    
    * rm more unnecessary debug
    
    * changelog
    
    * rename event callback from onUnload -> onPageHide
    
    * comment on discrepency of event naming for end of page lifecycle events
    
    * use pagehide in chrome, unload elsewhere
    
    * comment on chromium-specific pagehide behavior
    
    * changelog
    
    ---------
    
    Co-authored-by: Jennifer Shehane <jennifer@cypress.io>

--- packages/driver/src/cross-origin/events/spec_window.ts
+++ packages/driver/src/cross-origin/events/spec_window.ts
@@ -2,16 +2,20 @@ import type { $Cy } from '../../cypress/cy'
 import { handleErrorEvent } from './errors'
 
 export const handleSpecWindowEvents = (cy: $Cy) => {
+  // While we must move to pagehide for Chromium, it does not work for our
+  // needs in Firefox. Until that is addressed, only Chromium uses the pagehide
+  // event as a proxy for AUT unloads.
+  const unloadEvent = cy.Cypress.browser.family === 'chromium' ? 'pagehide' : 'unload'
   const handleWindowErrorEvent = handleErrorEvent(cy, 'spec')('error')
   const handleWindowUnhandledRejectionEvent = handleErrorEvent(cy, 'spec')('unhandledrejection')
 
   const handleUnload = () => {
-    window.removeEventListener('unload', handleUnload)
+    window.removeEventListener(unloadEvent, handleUnload)
     window.removeEventListener('error', handleWindowErrorEvent)
     window.removeEventListener('unhandledrejection', handleWindowUnhandledRejectionEvent)
   }
 
-  window.addEventListener('unload', handleUnload)
+  window.addEventListener(unloadEvent, handleUnload)
   window.addEventListener('error', handleWindowErrorEvent)
   window.addEventListener('unhandledrejection', handleWindowUnhandledRejectionEvent)
 }
