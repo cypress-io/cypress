1e24da13556c226d8a4dcbdfc9bcf2df8e939ad3
Michał Urbanek <urbanekmike@gmail.com>
2024-06-11 10:52:12 +0200

    tests for 29605

---
+++ packages/driver/cypress/e2e/issues/29605.cy.js
@@ -0,0 +1,12 @@
+// https://github.com/cypress-io/cypress/issues/29093
+describe('issue #29605', () => {
+  before(() => {
+    cy
+    .viewport('macbook-16')
+    .visit('/fixtures/issue-29605.html')
+  })
+
+  it('can click selection when display: contents width used', () => {
+    cy.get('#sidebar-left > section').click()
+  })
+})
