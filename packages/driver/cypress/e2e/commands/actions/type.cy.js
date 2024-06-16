cfc48feab265f61641f63f127b2a3c86460c8544
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-06-16 09:23:37 -0700

    Add tests of {upArrow} and {downArrow} for all supporting input types

--- packages/driver/cypress/e2e/commands/actions/type.cy.js
+++ packages/driver/cypress/e2e/commands/actions/type.cy.js
@@ -3157,4 +3157,104 @@ describe('src/cy/commands/actions/type - #type', () => {
       cy.get('@clickSpy').should('not.have.been.called')
     })
   })
+
+  describe('{upArrow} and {downArrow}', () => {
+    before(() => {
+      cy.visit('fixtures/dom.html')
+    })
+
+    context('input[type=date]', () => {
+      it('{upArrow} day by 1', () => {
+        cy.get('#date-without-value').then(($input) => $input.val('2000-01-01'))
+        cy.get('#date-without-value').type('{upArrow}')
+        cy.get('#date-without-value').should('have.value', '2000-01-02')
+      })
+
+      it('{downArrow} decreases day by 1', () => {
+        cy.get('#date-without-value').then(($input) => $input.val('2000-01-01'))
+        cy.get('#date-without-value').type('{downArrow}')
+        cy.get('#date-without-value').should('have.value', '1999-12-31')
+      })
+    })
+
+    context('input[type=month]', () => {
+      it('{upArrow} increases month by 1', () => {
+        cy.get('#month-without-value').then(($input) => $input.val('2000-01'))
+        cy.get('#month-without-value').type('{upArrow}')
+        cy.get('#month-without-value').should('have.value', '2000-02')
+      })
+
+      it('{downArrow} decreases month by 1', () => {
+        cy.get('#month-without-value').then(($input) => $input.val('2000-01'))
+        cy.get('#month-without-value').type('{downArrow}')
+        cy.get('#month-without-value').should('have.value', '1999-12')
+      })
+    })
+
+    context('input[type=week]', () => {
+      it('{upArrow} increases week by 1', () => {
+        cy.get('#week-without-value').then(($input) => $input.val('2017-W05'))
+        cy.get('#week-without-value').type('{upArrow}')
+        cy.get('#week-without-value').should('have.value', '2017-W06')
+      })
+
+      it('{downArrow} decreases week by 1', () => {
+        cy.get('#week-without-value').then(($input) => $input.val('2017-W05'))
+        cy.get('#week-without-value').type('{downArrow}')
+        cy.get('#week-without-value').should('have.value', '2017-W04')
+      })
+    })
+
+    context('input[type=time]', () => {
+      it('{upArrow} increases minute by 1', () => {
+        cy.get('#time-without-value').then(($input) => $input.val('01:23'))
+        cy.get('#time-without-value').type('{upArrow}')
+        cy.get('#time-without-value').should('have.value', '01:24')
+      })
+
+      it('{downArrow} decreases minute by 1', () => {
+        cy.get('#time-without-value').then(($input) => $input.val('01:23'))
+        cy.get('#time-without-value').type('{downArrow}')
+        cy.get('#time-without-value').should('have.value', '01:22')
+      })
+    })
+
+    context('input[type=datetime-local]', () => {
+      it('{upArrow} increases time value', () => {
+        cy.get('#datetime-local-without-value').type('{upArrow}')
+      })
+
+      it('{downArrow} decreases time value', () => {
+        cy.get('#datetime-local-without-value').type('{downArrow}')
+      })
+    })
+
+    context('input[type=number]', () => {
+      it('{upArrow} increases number value', () => {
+        cy.get('#number-without-value').then(($input) => $input.val(0))
+        cy.get('#number-without-value').type('{upArrow}')
+        cy.get('#number-without-value').should('have.value', 1)
+      })
+
+      it('{downArrow} decreases number value', () => {
+        cy.get('#number-without-value').then(($input) => $input.val(1))
+        cy.get('#number-without-value').type('{downArrow}')
+        cy.get('#number-without-value').should('have.value', 0)
+      })
+    })
+
+    context('input[type=range]', () => {
+      it('{upArrow} increases value by 1', () => {
+        cy.get('#range-without-value').then(($input) => $input.val(1))
+        cy.get('#range-without-value').type('{upArrow}')
+        cy.get('#range-without-value').should('have.value', 2)
+      })
+
+      it('{downArrow} decreases value by 1', () => {
+        cy.get('#range-without-value').then(($input) => $input.val(1))
+        cy.get('#range-without-value').type('{downArrow}')
+        cy.get('#range-without-value').should('have.value', 0)
+      })
+    })
+  })
 })
