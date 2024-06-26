8b5bb05b9345b482e0b12af12ba2d86281d4f25d
Cacie Prins <cacie@cypress.io>
2024-06-26 16:18:05 -0400

    removes 408 as a non-retriable error in unit test

--- packages/server/test/unit/cloud/network/is_retryable_error_spec.ts
+++ packages/server/test/unit/cloud/network/is_retryable_error_spec.ts
@@ -24,7 +24,7 @@ describe('isRetryableError', () => {
   })
 
   it('returns false with non-retriable http errors', () => {
-    [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 431, 451, 500, 501, 505, 507, 508, 510, 511].forEach((status) => {
+    [400, 401, 402, 403, 404, 405, 406, 407, 409, 410, 411, 412, 413, 414, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 431, 451, 500, 501, 505, 507, 508, 510, 511].forEach((status) => {
       const err = new HttpError('some error', url, status, 'status text', sinon.createStubInstance(Response))
 
       expect(isRetryableError(err)).to.be.false
