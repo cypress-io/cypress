303fbfde16feca4a786a37a14c7a65d29af40090
Matt Schile <mschile@cypress.io>
2024-07-03 12:43:55 -0600

    Update packages/server/test/unit/cloud/network/is_retryable_error_spec.ts

--- packages/server/test/unit/cloud/network/is_retryable_error_spec.ts
+++ packages/server/test/unit/cloud/network/is_retryable_error_spec.ts
@@ -23,7 +23,7 @@ describe('isRetryableError', () => {
     })
   })
 
-  it('returns false with non-retriable http errors', () => {
+  it('returns false with non-retryable http errors', () => {
     [400, 401, 402, 403, 404, 405, 406, 407, 409, 410, 411, 412, 413, 414, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 431, 451, 500, 501, 505, 507, 508, 510, 511].forEach((status) => {
       const err = new HttpError('some error', url, status, 'status text', sinon.createStubInstance(Response))
 
