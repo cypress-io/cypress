44c538a6d6d993668b4ed1a0ae619438a28aa187
DESNOS-PIAZZA <benoit.d@springcard.com>
2024-07-17 11:58:32 +0200

    fix: 29798 : DevServerPublicPathRoute cause intercept not being called

--- system-tests/project-fixtures/vue/src/components/HelloWorld.vue
+++ system-tests/project-fixtures/vue/src/components/HelloWorld.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { ref } from "vue"
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 
 function handleClick () {
-  count.value++
+  count.value++;
   fetch("http://localhost:3000/hello", {
     method: "GET",
-  })
+  });
 }
 </script>
 
