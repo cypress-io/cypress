12c14b254582707c3ce8a396d78caa8770767ce9
Matthew Schile <mschile@cypress.io>
2024-07-25 09:18:06 -0600

    trying to reproduce memory leak

--- packages/driver/cypress/e2e/memory/memory.cy.js
+++ packages/driver/cypress/e2e/memory/memory.cy.js
@@ -1,7 +1,38 @@
+// describe('memory spec', { browser: { family: 'chromium' } }, () => {
+//   for (let index = 0; index < 300; index++) {
+//     it(`test ${index + 1} passes`, () => {
+//       cy.visit('http://localhost:3500/memory')
+//     })
+//   }
+// })
+
+// failed with the following error:
+// Response stream timed out for request: 45541.17727
+// CdpLagTimeoutError: Response stream timed out for request: 45541.17727
+// describe('memory spec', { browser: { family: 'chromium' } }, () => {
+//   for (let index = 0; index < 300; index++) {
+//     it(`test ${index + 1} passes`, () => {
+//       cy.visit('https://www.msn.com/')
+//       cy.wait(1000)
+//     })
+//   }
+// })
+
+// fails with OOM when recording
+// describe('memory spec', { browser: { family: 'chromium' } }, () => {
+//   for (let index = 0; index < 200; index++) {
+//     it(`test ${index + 1} passes`, () => {
+//       cy.visit('https://www.google.com/maps')
+//       cy.wait(1000)
+//     })
+//   }
+// })
+
 describe('memory spec', { browser: { family: 'chromium' } }, () => {
-  for (let index = 0; index < 50; index++) {
+  Cypress._.times(100, (index) => {
     it(`test ${index + 1} passes`, () => {
-      cy.visit('http://localhost:3500/memory')
+      cy.visit('https://www.amazon.com/')
+      cy.wait(1000)
     })
-  }
+  })
 })
--- packages/driver/cypress/support/defaults.js
+++ packages/driver/cypress/support/defaults.js
@@ -1,56 +1,56 @@
-const { $ } = Cypress
+// const { $ } = Cypress
 
-const isActuallyInteractive = Cypress.config('isInteractive')
+// const isActuallyInteractive = Cypress.config('isInteractive')
 
 window.top.__cySkipValidateConfig = true
 
-if (!isActuallyInteractive) {
-  // we want to only enable retries in runMode
-  // and because we set `isInteractive` above
-  // we have to set retries here
-  Cypress.config('retries', 2)
-}
+// if (!isActuallyInteractive) {
+//   // we want to only enable retries in runMode
+//   // and because we set `isInteractive` above
+//   // we have to set retries here
+//   Cypress.config('retries', 2)
+// }
 
-let ranPrivilegedCommandsInBeforeEach = false
+// let ranPrivilegedCommandsInBeforeEach = false
 
 beforeEach(function () {
   // always set that we're interactive so we
   // get consistent passes and failures when running
   // from CI and when running in GUI mode
-  Cypress.config('isInteractive', true)
+  // Cypress.config('isInteractive', true)
 
-  if (!isActuallyInteractive) {
-    // necessary or else snapshots will not be taken
-    // and we can't test them
-    Cypress.config('numTestsKeptInMemory', 1)
-  }
+  // if (!isActuallyInteractive) {
+  //   // necessary or else snapshots will not be taken
+  //   // and we can't test them
+  //   Cypress.config('numTestsKeptInMemory', 1)
+  // }
 
   // remove all event listeners
   // from the window
   // this could fail if this window
   // is a cross origin window
-  try {
-    $(cy.state('window')).off()
-  } catch (error) {} // eslint-disable-line no-empty
+  // try {
+  //   $(cy.state('window')).off()
+  // } catch (error) {} // eslint-disable-line no-empty
 
   // only want to run this as part of the privileged commands spec
-  if (cy.config('spec').baseName === 'privileged_commands.cy.ts') {
-    cy.visit('/fixtures/files-form.html')
+  // if (cy.config('spec').baseName === 'privileged_commands.cy.ts') {
+  //   cy.visit('/fixtures/files-form.html')
 
-    // it only needs to run once per spec run
-    if (ranPrivilegedCommandsInBeforeEach) return
+  //   // it only needs to run once per spec run
+  //   if (ranPrivilegedCommandsInBeforeEach) return
 
-    ranPrivilegedCommandsInBeforeEach = true
+  //   ranPrivilegedCommandsInBeforeEach = true
 
-    cy.exec('echo "hello"')
-    cy.readFile('cypress/fixtures/app.json')
-    cy.writeFile('cypress/_test-output/written.json', 'contents')
-    cy.task('return:arg', 'arg')
-    cy.get('#basic').selectFile('cypress/fixtures/valid.json')
-    if (!Cypress.isBrowser({ family: 'webkit' })) {
-      cy.origin('http://foobar.com:3500', () => {})
-    }
-  }
+  //   cy.exec('echo "hello"')
+  //   cy.readFile('cypress/fixtures/app.json')
+  //   cy.writeFile('cypress/_test-output/written.json', 'contents')
+  //   cy.task('return:arg', 'arg')
+  //   cy.get('#basic').selectFile('cypress/fixtures/valid.json')
+  //   if (!Cypress.isBrowser({ family: 'webkit' })) {
+  //     cy.origin('http://foobar.com:3500', () => {})
+  //   }
+  // }
 })
 
 // this is here to test that cy.origin() dependencies used directly in the
