39df60b5b182b4cfe19c4180df8cb162e8739e97
AtofStryker <bglesias@gmail.com>
2024-08-12 15:02:48 -0400

    add system test projects

---
+++ system-tests/projects/experimental-JIT/webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = ({ mode } = { mode: 'production' }) => {
+  return {
+    mode,
+    resolve: {
+      extensions: ['.js', '.ts', '.jsx', '.tsx'],
+      alias: {
+        'react': path.resolve(__dirname, './node_modules/react'),
+        'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-syntax-jsx'],
+            },
+          },
+        },
+      ],
+    },
+  }
+}
