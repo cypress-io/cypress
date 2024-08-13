c20b242ac63202a8b6fd62334eb8024c0a173321
Azvya Erstevan <azvyae@gmail.com>
2024-08-13 21:37:26 +0700

    misc: allow HiDPI Screen running wayland to use cypress window/browser (#29366)
    * misc: allow HiDPI Screen running wayland to use cypress window/browser
    
    * Added changelog entry
    
    * Fix changelog entry release date to pending
    
    * Changed release changelog entry to actual date
    
    * Added issue number to the changelog entry
    
    * fix tests
    
    * update changelog
    
    ---------
    
    Co-authored-by: Ryan Manuel <ryanm@cypress.io>

--- cli/test/lib/exec/spawn_spec.js
+++ cli/test/lib/exec/spawn_spec.js
@@ -121,6 +121,7 @@ describe('lib/exec/spawn', function () {
       return spawn.start('--foo', { foo: 'bar' })
       .then(() => {
         expect(cp.spawn).to.be.calledWithMatch('/path/to/cypress', [
+          '--ozone-platform=auto',
           '--',
           '--foo',
           '--cwd',
@@ -148,6 +149,7 @@ describe('lib/exec/spawn', function () {
         const args = cp.spawn.firstCall.args.slice(0, 2)
         // it is important for "--no-sandbox" to appear before "--" separator
         const expectedCliArgs = [
+          '--ozone-platform=auto',
           '--no-sandbox',
           '--',
           '--foo',
@@ -173,6 +175,7 @@ describe('lib/exec/spawn', function () {
       .then(() => {
         expect(cp.spawn).to.be.calledWithMatch('node', [
           p,
+          '--ozone-platform=auto',
           '--',
           '--foo',
           '--cwd',
@@ -198,6 +201,7 @@ describe('lib/exec/spawn', function () {
       .then(() => {
         expect(cp.spawn).to.be.calledWithMatch('node', [
           p,
+          '--ozone-platform=auto',
           '--',
           '--foo',
           '--cwd',
