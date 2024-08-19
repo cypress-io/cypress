fedd32771e718ab858333fdda0a176aaf82a6559
Cacie Prins <cacie@cypress.io>
2024-08-19 16:47:14 -0400

    rm unnecessary ts-ignore

--- packages/app/src/runner/get-runner-config-from-window.ts
+++ packages/app/src/runner/get-runner-config-from-window.ts
@@ -1,6 +1,5 @@
 import { decodeBase64Unicode } from '@packages/frontend-shared/src/utils/base64'
 
 export function getRunnerConfigFromWindow () {
-  // @ts-ignore
   return JSON.parse(decodeBase64Unicode(window.__CYPRESS_CONFIG__?.base64Config)) as Cypress.Config
 }
