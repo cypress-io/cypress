e607865cab9cbbac718b4fe7eb49c7a57a2f82e4
Jennifer Shehane <jennifer@cypress.io>
2024-08-26 16:34:02 -0400

    new workflow for a11y results

---
+++ scripts/verify-accessibility-results.js
@@ -0,0 +1,32 @@
+const { getAccessibilityResults } = require('@cypress/extract-cloud-results')
+
+getAccessibilityResults({
+  projectId: 'ypt4pf',
+})
+.then((results) => {
+  const {
+    runNumber,
+    accessibilityReportUrl,
+    summary,
+    rules,
+  } = results
+  const { total } = summary.violationCounts
+
+  console.log(`Received ${summary.isPartialReport ? 'partial' : ''} results for run #${runNumber}.`).
+  console.log(`See full report at ${accessibilityReportUrl}.`)
+
+  // write your logic to conditionally fail based on the results
+  if (total > 0) {
+    const { critical, serious, moderate, minor } = summary.violationCounts
+
+    console.log(`${total} Accessibility violations were detected - ${critical} critical, ${serious} serious, ${moderate} moderate, and ${minor} minor.`)
+    console.log('The following rules were violated:')
+    console.log(rules)
+
+    throw new Error(
+      `${total} Accessibility violations detected and must be fixed.`,
+    )
+  }
+
+  console.log('No Accessibility violations detected!')
+})
