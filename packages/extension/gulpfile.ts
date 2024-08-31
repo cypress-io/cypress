695abf1e4729a3ded505bc161b4a0f6abf8cadfa
GweesinChan <gweesin2000@gmail.com>
2024-08-31 12:40:54 +0800

    chore(deps): update dependency rimraf to v5.0.10

--- packages/extension/gulpfile.ts
+++ packages/extension/gulpfile.ts
@@ -1,5 +1,5 @@
 import gulp from 'gulp'
-import rimraf from 'rimraf'
+import { rimraf } from 'rimraf'
 import { waitUntilIconsBuilt } from '../../scripts/ensure-icons'
 import cp from 'child_process'
 import * as path from 'path'
