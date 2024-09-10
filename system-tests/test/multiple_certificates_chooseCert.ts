66cc2e3a9efe9060b907b2f4a46b357df17d73f3
Abhishek Karmakar <abhi.karma.2007@gmail.com>
2024-09-10 13:20:05 +0530

    Project and test naming fix for consistency

--- system-tests/test/multiple_certificates_chooseCert.ts
+++ system-tests/test/multiple_certificates_chooseCert.ts
@@ -2,7 +2,7 @@ import systemTests from '../lib/system-tests'
 
 describe('e2e', async () => {
   systemTests.it('passes', {
-    project: 'multiple-certificates-selectCert',
+    project: 'multiple-certificates-chooseCert',
     testingType: 'e2e',
     snapshot: true,
     browser: ['electron'],
