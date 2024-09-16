bcba5ed2ebb7a0895a3d495cc3f4f0423b28bec2
AtofStryker <bglesias@gmail.com>
2024-09-16 10:48:55 -0400

    fix: fix checkbox click events for firefox 129 and up [run ci]

--- packages/driver/src/cy/commands/actions/type.ts
+++ packages/driver/src/cy/commands/actions/type.ts
@@ -192,7 +192,7 @@ export default function (Commands, Cypress, cy, state, config) {
       const isFirefoxBefore91 = Cypress.isBrowser('firefox') && Cypress.browserMajorVersion() < 91
       const isFirefoxBefore98 = Cypress.isBrowser('firefox') && Cypress.browserMajorVersion() < 98
       const isFirefox106OrLater = Cypress.isBrowser('firefox') && Cypress.browserMajorVersion() >= 106
-      const isFirefox130OrLater = Cypress.isBrowser('firefox') && Cypress.browserMajorVersion() >= 130
+      const isFirefox129OrLater = Cypress.isBrowser('firefox') && Cypress.browserMajorVersion() >= 129
 
       const simulateSubmitHandler = function () {
         const form = options.$el.parents('form')
@@ -386,10 +386,9 @@ export default function (Commands, Cypress, cy, state, config) {
 
             keydownEvents = []
 
-            // After Firefox 98 and before 130
+            // After Firefox 98 and before 129
             // Firefox doesn't update checkbox automatically even if the click event is sent.
-            // TODO: verify firefox version this starts happening
-            if (Cypress.isBrowser('firefox') && !isFirefox130OrLater) {
+            if (Cypress.isBrowser('firefox') && !isFirefox129OrLater) {
               if (event.target.type === 'checkbox') {
                 event.target.checked = !event.target.checked
               } else if (event.target.type === 'radio') { // when checked is false, here cannot be reached because of the above condition
