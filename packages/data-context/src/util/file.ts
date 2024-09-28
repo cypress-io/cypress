337acb416824142716b9d3d2404bcbeaf01f25e7
GweesinChan <gweesin2000@gmail.com>
2024-09-28 23:03:19 +0800

    refactor: add comment for toOS function and remove unused parameters

--- packages/data-context/src/util/file.ts
+++ packages/data-context/src/util/file.ts
@@ -4,6 +4,15 @@ export function toPosix (file: string, sep: string = path.sep) {
   return file.split(sep).join(path.posix.sep)
 }
 
-export function toOS (file: string, sep: string = path.posix.sep) {
-  return file.split(sep).join(path.sep)
+/**
+ * Converts a POSIX file path to the current operating system's file path format.
+ *
+ * This function takes a file path string that uses POSIX separators ('/') and
+ * replaces them with the current operating system's path separators.
+ *
+ * @param file The file path to convert.
+ * @returns The converted file path with the current OS's path separators.
+ */
+export function toOS (file: string) {
+  return file.split(path.posix.sep).join(path.sep)
 }
