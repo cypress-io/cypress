1d3bea0a0f5a063f8782989eac5fe9ca2fb31ce3
Jennifer Shehane <jennifer@cypress.io>
2024-10-03 09:26:23 -0400

    Remove firefox-memory system test

--- system-tests/test/firefox_spec.ts
+++ system-tests/test/firefox_spec.ts
@@ -1,52 +1,7 @@
-import path from 'path'
-import util from 'util'
-import fs from 'fs-extra'
-
 import systemTests from '../lib/system-tests'
-import Bluebird from 'bluebird'
-import Fixtures from '../lib/fixtures'
-
-const e2ePath = Fixtures.projectPath('e2e')
-const outputPath = path.join(e2ePath, 'output.json')
 
 describe('e2e firefox', function () {
   systemTests.setup()
-
-  // NOTE: This can be used to demonstrate the Firefox out-of-memory issue, but it is skipped
-  // because it takes forever and is redundant since we test `services` against Cypress prereleases.
-  // @see https://github.com/cypress-io/cypress/issues/6187
-  systemTests.it.skip('can run a lot of tests', {
-    outputPath,
-    project: 'firefox-memory',
-    spec: 'spec.js',
-    browser: 'firefox',
-    expectedExitCode: 0,
-    timeout: 1e9,
-    onRun: (exec) => {
-      return exec()
-      .then(() => {
-        return Bluebird.resolve(fs.readJson(outputPath))
-        .get('runs')
-        .get(0)
-        .get('tests')
-        .map((test: any, i) => {
-          return {
-            num: i + 1,
-            ...test.timings,
-          }
-        })
-        .then((tests) => {
-          // eslint-disable-next-line
-          console.log(util.inspect(tests, {
-            depth: Infinity,
-            breakLength: Infinity,
-            maxArrayLength: Infinity,
-          }))
-        })
-      })
-    },
-    // snapshot: true,
-  })
 
   systemTests.it('launches maximized by default', {
     browser: 'firefox',
