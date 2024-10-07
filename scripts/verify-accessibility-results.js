0ed7de8a7c64702046b6e581db2189e18617e255
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2024-10-07 11:33:42 -0500

    chore: aq results api (#30325)
    * empty commit
    
    * new workflow for a11y results
    
    * empty commit
    
    * Update required jobs to be a11y focused
    
    * persist files to workspace
    
    * restore workspace
    
    * yarn
    
    * .
    
    * update
    
    * update
    
    * include context
    
    * .
    
    * translate record key
    
    * .
    
    * add rule check
    
    * re-add all jobs
    
    * cleanup
    
    * add to the contributor pr workflkow
    
    * rename to verify results
    
    * ensure run is completed before fetching a11y results
    
    * flip the logic to only track problematic rules, not passing rules
    
    * speed up install
    
    * ssh
    
    * install locally
    
    * re-add commented out jobs
    
    * Update scripts/verify-accessibility-results.js
    
    * pr feedback
    
    * address pr feedback
    
    * remove rules that were just resolved
    
    ---------
    
    Co-authored-by: Jennifer Shehane <jennifer@cypress.io>

---
+++ scripts/verify-accessibility-results.js
@@ -0,0 +1,76 @@
+const { getAccessibilityResults } = require('@cypress/extract-cloud-results')
+
+/**
+ * The list of rules that currently have 1+ elements that have been flagged with
+ * violations within the Cypress Accessibility report that need to be addressed.
+ *
+ * For example, the `aria-required-children` rule can have multiple
+ * elements that caused this rule to fail.
+ *
+ * Once the violation is fixed in the Cypress App's Accessibility report,
+ * the fixed rule should be removed from this list.
+ *
+ * View the Accessibility report for the Cypress run in the Cloud
+ * for more details on how to address these failures.
+ */
+const rulesWithExistingViolations = [
+  'aria-required-children',
+  'aria-required-parent',
+  'image-alt',
+  'aria-allowed-role',
+  'empty-heading',
+  'heading-order',
+  'page-has-heading-one',
+  'aria-dialog-name',
+  'aria-hidden-focus',
+  'color-contrast',
+  'link-in-text-block',
+  'list',
+  'listitem',
+  'nested-interactive',
+  'scrollable-region-focusable',
+]
+
+// This polls up to 30 times every 30 seconds.
+// This resolves when the Accessibility report is ready for the Cypress run.
+getAccessibilityResults({
+  projectId: 'ypt4pf',
+})
+.then((results) => {
+  const {
+    runNumber,
+    accessibilityReportUrl,
+    summary,
+    rules,
+  } = results
+  const { total } = summary.violationCounts
+
+  console.log(`Received ${summary.isPartialReport ? 'partial' : ''} results for run #${runNumber}.`)
+  console.log(`See full report at ${accessibilityReportUrl}.`)
+
+  // write your logic to conditionally fail based on the results
+  if (total === 0) {
+    console.log('No Accessibility violations detected!')
+
+    return
+  }
+
+  const { critical, serious, moderate, minor } = summary.violationCounts
+
+  console.log(`${total} Accessibility violations were detected - ${critical} critical, ${serious} serious, ${moderate} moderate, and ${minor} minor.`)
+
+  const newRuleViolations = rules.filter((rule) => !rulesWithExistingViolations.includes(rule.name))
+
+  if (newRuleViolations.length > 0) {
+    console.error('The following rules were violated that were previously passing:')
+    console.error(newRuleViolations)
+
+    throw new Error(`${newRuleViolations.length} rule regressions were introduced and must be fixed.`)
+  }
+
+  if (total < rulesWithExistingViolations.length) {
+    console.warn(`It seems you have resolved ${rulesWithExistingViolations.length - total} rule(s). Remove them from the list of problematic rules so regressions are not introduced.`)
+  }
+
+  console.log('No new Accessibility violations detected!')
+})
