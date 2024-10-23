bf7250dffd63075b097d8ccd790da123bd0eaa3c
Ryan Manuel <ryanm@cypress.io>
2024-10-23 15:54:05 -0500

    chore: upgrade electron to 33.0.2

--- scripts/binary/trigger-publish-binary-pipeline.js
+++ scripts/binary/trigger-publish-binary-pipeline.js
@@ -10,7 +10,7 @@ const { getNextVersionForBinary } = require('../get-next-version')
   const { nextVersion } = await getNextVersionForBinary()
 
   const body = JSON.stringify({
-    branch: 'ryanm/chore/electron-upgrade',
+    branch: 'ryanm/chore/electron-33-upgrade',
     parameters: {
       temp_dir: os.tmpdir(),
       sha: process.env.CIRCLE_SHA1,
