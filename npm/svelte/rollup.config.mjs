faf4115f6d335f058941fe914adb319db75a6ac8
Matthew Schile <mschile@cypress.io>
2024-11-07 11:27:58 -0700

    fix build

--- npm/svelte/rollup.config.mjs
+++ npm/svelte/rollup.config.mjs
@@ -1,3 +1,3 @@
 import { createEntries } from '@cypress/mount-utils/create-rollup-entry.mjs'
 
-export default createEntries({ formats: ['es', 'cjs'], input: 'src/index.ts' })
+export default createEntries({ formats: ['es', 'cjs'], input: 'src/index.ts', dtsOptions: { respectExternal: false } })
