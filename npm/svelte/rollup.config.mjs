60dd24993271dac64fbffc95702152a10adfb54b
Matthew Schile <mschile@cypress.io>
2024-11-08 13:19:13 -0700

    pr updates

--- npm/svelte/rollup.config.mjs
+++ npm/svelte/rollup.config.mjs
@@ -1,3 +1,4 @@
 import { createEntries } from '@cypress/mount-utils/create-rollup-entry.mjs'
 
+// updated respectExternal to false due to this issue: https://github.com/Swatinem/rollup-plugin-dts/issues/162#issuecomment-1702374232
 export default createEntries({ formats: ['es', 'cjs'], input: 'src/index.ts', dtsOptions: { respectExternal: false } })
