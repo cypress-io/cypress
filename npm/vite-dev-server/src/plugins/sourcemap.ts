86075410a545f84215dcaa369b0c83ce9b54a9df
Ryan Manuel <ryanm@cypress.io>
2024-11-13 11:11:23 -0600

    PR comments and other tweaks

--- npm/vite-dev-server/src/plugins/sourcemap.ts
+++ npm/vite-dev-server/src/plugins/sourcemap.ts
@@ -19,19 +19,11 @@ export const CypressSourcemap = (
         // have a cache buster query parameter (e.g. `?v=12345`)
         const queryParameterLessId = id.split('?')[0]
 
-        if (/\.js$/i.test(queryParameterLessId) && !/\/\/# sourceMappingURL=data/i.test(code)) {
+        if (/\.(js|jsx|ts|tsx|vue|mjs|cjs)$/i.test(queryParameterLessId) && !/\/\/# sourceMappingURL=data/i.test(code)) {
           /*
-          The Vite dev server and plugins automatically generate sourcemaps for most files, but they are
-          only included in the served files if any transpilation actually occurred (JSX, TS, etc). This
-          means that files that are "pure" JS won't include sourcemaps, so JS spec files that don't require
-          transpilation won't get code frames in the runner.
-
-          A sourcemap for these files is generated (just not served) which is in effect an "identity"
-          sourcemap mapping 1-to-1 to the output file. We can grab this and pass it along as a sourcemap
-          we want Vite to embed into the output, giving Cypress a sourcemap to use for codeFrame lookups.
-          @see https://rollupjs.org/guide/en/#thisgetcombinedsourcemap
-
-          We utilize a 'post' plugin here and manually append the sourcemap content to the code. We *should*
+          The Vite dev server and plugins automatically generate sourcemaps for most files, but there are
+          some files that don't have sourcemaps generated for them or are not inlined. We utilize a 'post' plugin
+          here and manually append the sourcemap content to the code in these cases. We *should*
           be able to pass the sourcemap along using the `map` attribute in the return value, but Babel-based
           plugins don't work with this which causes sourcemaps to break for files that go through common
           plugins like `@vitejs/plugin-react`. By manually appending the sourcemap at this point in time
