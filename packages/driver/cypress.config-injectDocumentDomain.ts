34e0573f92f746a135be5ccfdddae36575784249
Cacie Prins <cacie@cypress.io>
2024-12-20 14:16:18 -0500

    export base config from primary cypress config in driver for use in inject-document-domain test subset

--- packages/driver/cypress.config-injectDocumentDomain.ts
+++ packages/driver/cypress.config-injectDocumentDomain.ts
@@ -1,39 +1,12 @@
 import { defineConfig } from 'cypress'
+import { baseConfig } from './cypress.config'
 
 export default defineConfig({
-  projectId: 'ypt4pf',
-  experimentalStudio: true,
-  experimentalMemoryManagement: true,
-  experimentalWebKitSupport: true,
-  hosts: {
-    'foobar.com': '127.0.0.1',
-    '*.foobar.com': '127.0.0.1',
-    'barbaz.com': '127.0.0.1',
-    '*.barbaz.com': '127.0.0.1',
-    '*.idp.com': '127.0.0.1',
-    'localalias': '127.0.0.1',
-  },
-  reporter: '../../node_modules/cypress-multi-reporters/index.js',
-  reporterOptions: {
-    configFile: '../../mocha-reporter-config.json',
-  },
+  ...baseConfig,
   e2e: {
+    ...baseConfig.e2e,
     specPattern: '{cypress/**/origin/**/*.cy.{js,ts},cypress/**/cookies.cy.js,cypress/**/net_stubbing.cy.js}',
     injectDocumentDomain: true,
-    experimentalOriginDependencies: true,
-    experimentalModifyObstructiveThirdPartyCode: true,
-    setupNodeEvents: (on, config) => {
-      on('task', {
-        log (message) {
-          // eslint-disable-next-line no-console
-          console.log(message)
-
-          return null
-        },
-      })
-
-      return require('./cypress/plugins')(on, config)
-    },
-    baseUrl: 'http://localhost:3500',
   },
+  component: undefined,
 })
