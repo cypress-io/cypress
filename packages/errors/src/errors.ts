58db915ceddba92f6642997acbab322724d8ae27
Cacie Prins <cacie@cypress.io>
2024-12-20 13:46:52 -0500

    add on links to error messages

--- packages/errors/src/errors.ts
+++ packages/errors/src/errors.ts
@@ -1350,28 +1350,34 @@ export const AllCypressErrors = {
     return errTemplate`\
     The ${fmt.highlight(`justInTimeCompile`)} configuration is only supported for Component Testing.`
   },
-  // TODO: link to docs on the new injectDocumentDomain config option
   EXPERIMENTAL_SKIP_DOMAIN_INJECTION: () => {
     return errTemplate`\
       The ${fmt.highlight(`experimentalSkipDomainInjection`)} experiment is over. ${fmt.highlight('document.domain')} injection is now off by default.
+
+      Read the migration guide for Cypress v14.0.0: https://on.cypress.com/migration-guide
     `
   },
   EXPERIMENTAL_USE_DEFAULT_DOCUMENT_DOMAIN_E2E_ONLY: () => {
-    // TODO: link to docs on the new injectDocumentDomain config option
     return errTemplate`\
-        The ${fmt.highlight(`experimentalSkipDomainInjection`)} experiment is over, and this configuration option is no longer honored.
+      The ${fmt.highlight(`experimentalSkipDomainInjection`)} experiment is over, and this configuration option is no longer honored.
+    
+      Read the migration guide for Cypress v14.0.0: https://on.cypress.com/migration-guide
     `
   },
   // TODO: link to docs on injectDocumentDomain
   INJECT_DOCUMENT_DOMAIN_DEPRECATION: () => {
     return errTemplate`\
       The ${fmt.highlight('injectDocumentDomain')} option is deprecated. Interactions with intra-test navigations to differing hostnames must now be wrapped in ${fmt.highlight('cy.origin')} commands, even if the hostname is a subdomain. This configuration option will be removed in Cypress 15.
+    
+      Read the documentation for the injectDocumentDomain configuration option: https://on.cypress.com/inject-document-domain-configuration
     `
   },
   INJECT_DOCUMENT_DOMAIN_E2E_ONLY: () => {
     // TODO: link to docs on injectDocumentDomain
     return errTemplate`\
       The ${fmt.highlight('injectDocumentDomain')} option is only available for E2E testing.
+
+      Read the documentation for the injectDocumentDomain configuration option: https://on.cypress.com/inject-document-domain-configuration
     `
   },
   FIREFOX_GC_INTERVAL_REMOVED: () => {
