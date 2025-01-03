1fd3feee79ab935298e1c645ff0eece57097162a
Cacie Prins <cacie@cypress.io>
2025-01-03 12:23:16 -0500

    snapshot updatesfor experimental skip domain injection err msg

--- system-tests/__snapshots__/experimental_skip_domain_injection_spec.ts.js
+++ system-tests/__snapshots__/experimental_skip_domain_injection_spec.ts.js
@@ -1,5 +1,7 @@
 exports['e2e experimentalSkipDomainInjection=true / fails with an error message about experimentalSkipDomainInjection being removed'] = `
 The experimentalSkipDomainInjection experiment is over. document.domain injection is now off by default.
 
+Read the migration guide for Cypress v14.0.0: https://on.cypress.com/migration-guide
+
 
 `
