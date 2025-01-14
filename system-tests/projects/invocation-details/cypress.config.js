15ed6aaba2ec3d123a5ec20efcdc812719451495
Cacie Prins <cacie@cypress.io>
2025-01-14 09:21:24 -0500

    rm binary system test

--- system-tests/projects/invocation-details/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  retries: null,
-  e2e: {
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    supportFile: 'cypress/support.js',
-  },
-})
