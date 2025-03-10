32c7411058a2a4fc1060755b31f5f478b68faec1
Ryan Manuel <ryanm@cypress.io>
2025-03-10 17:52:47 -0500

    remove new dom link

--- packages/driver/cypress.config.ts
+++ packages/driver/cypress.config.ts
@@ -10,8 +10,6 @@ export const baseConfig: Cypress.ConfigOptions = {
   hosts: {
     'foobar.com': '127.0.0.1',
     '*.foobar.com': '127.0.0.1',
-    'foobar-protocol-enabled.com': '127.0.0.1',
-    '*.foobar-protocol-enabled.com': '127.0.0.1',
     'barbaz.com': '127.0.0.1',
     '*.barbaz.com': '127.0.0.1',
     '*.idp.com': '127.0.0.1',
