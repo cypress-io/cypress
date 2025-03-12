4b9dcb3c00bd7d4d711cd2937996f19732923cae
Ryan Manuel <ryanm@cypress.io>
2025-03-12 13:00:20 -0500

    fix types

--- packages/app/src/studio/studio-app-types.ts
+++ packages/app/src/studio/studio-app-types.ts
@@ -1,13 +1,7 @@
-import type React from 'react'
-
 export interface StudioPanelProps {
   canAccessStudioLLM: boolean
 }
 
-export interface StudioPanelShape {
-  (): React.FC<StudioPanelProps>
-}
-
 export interface StudioAppDefaultShape {
-  StudioPanel: StudioPanelShape
+  StudioPanel: (props: StudioPanelProps) => JSX.Element
 }
