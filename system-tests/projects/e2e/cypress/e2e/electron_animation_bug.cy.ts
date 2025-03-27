c5bc8e9578121188d7cac8a0e6c4a174981bdb7f
Ryan Manuel <ryanm@cypress.io>
2025-03-27 11:55:02 -0500

    fix

--- system-tests/projects/e2e/cypress/e2e/electron_animation_bug.cy.ts
+++ system-tests/projects/e2e/cypress/e2e/electron_animation_bug.cy.ts
@@ -1,5 +1,5 @@
 describe('bug', () => {
-  it('loads in less than .3 seconds', { defaultCommandTimeout: 300 }, () => {
+  it('loads in less than .3 seconds', { defaultCommandTimeout: 750 }, () => {
     cy.visit('/electron_animation_bug.html')
 
     for (let i = 0; i < 50; i++) {
