03f4a0ea7728b01d610ae9b32993058d21a00471
Ryan Manuel <ryanm@cypress.io>
2025-03-27 11:49:02 -0500

    fix

--- system-tests/projects/e2e/cypress/e2e/electron_animation_bug.cy.ts
+++ system-tests/projects/e2e/cypress/e2e/electron_animation_bug.cy.ts
@@ -2,7 +2,7 @@ describe('bug', () => {
   it('loads in less than .3 seconds', { defaultCommandTimeout: 300 }, () => {
     cy.visit('/electron_animation_bug.html')
 
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < 50; i++) {
       cy.get('#app').should('exist')
       cy.get('#remove').click()
       cy.get('#app').should('not.exist')
