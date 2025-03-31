a1f9a278c30c428dbc54593afaf7c7a2545afe9b
Cacie Prins <cacie@cypress.io>
2025-03-31 17:28:18 -0400

    skip press() driver test in ff below v135

--- packages/driver/cypress/e2e/commands/actions/press.cy.ts
+++ packages/driver/cypress/e2e/commands/actions/press.cy.ts
@@ -1,5 +1,9 @@
 describe('src/cy/commands/actions/press', () => {
   it('dispatches the tab keypress to the AUT', () => {
+    if (Cypress.browser.family === 'firefox' && Cypress.browserMajorVersion() < 135) {
+      return
+    }
+
     cy.visit('/fixtures/input_events.html')
 
     cy.press(Cypress.Keyboard.Keys.TAB)
