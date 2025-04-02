fe3a435dd1d8242c1f28fc5630c40909b906c3e6
Cacie Prins <cacie@cypress.io>
2025-04-02 10:38:03 -0400

    skip press driver test in webkit

--- packages/driver/cypress/e2e/commands/actions/press.cy.ts
+++ packages/driver/cypress/e2e/commands/actions/press.cy.ts
@@ -1,6 +1,12 @@
 describe('src/cy/commands/actions/press', () => {
   it('dispatches the tab keypress to the AUT', () => {
+    // Non-BiDi firefox is not supported
     if (Cypress.browser.family === 'firefox' && Cypress.browserMajorVersion() < 135) {
+      return
+    }
+
+    // TODO: Webkit is not supported. https://github.com/cypress-io/cypress/issues/31054
+    if (Cypress.isBrowser('webkit')) {
       return
     }
 
