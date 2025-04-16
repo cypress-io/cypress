025755fa5eae017a1b536bb2a9a130f1ec008de7
Cacie Prins <cacie@cypress.io>
2025-04-16 16:14:45 -0400

    fix ts

--- packages/server/lib/cloud/api/index.ts
+++ packages/server/lib/cloud/api/index.ts
@@ -658,7 +658,7 @@ export default {
       }
 
       const postReqs = async () => {
-        const initialPreflightTimeout = noproxyPreflightTimeout()
+        const initialPreflightTimeout = noProxyPreflightTimeout()
 
         if (initialPreflightTimeout >= 0) {
           try {
