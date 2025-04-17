fd80e064ee8fe66baaf7d64fc1b68091005a4877
Bill Glesias <bglesias@gmail.com>
2025-04-17 12:59:49 -0400

    make sure to filter out stack code correctly for windows

--- packages/data-context/src/sources/ErrorDataSource.ts
+++ packages/data-context/src/sources/ErrorDataSource.ts
@@ -2,8 +2,11 @@ import { ErrorWrapperSource, stackUtils } from '@packages/errors'
 import path from 'path'
 import _ from 'lodash'
 import { codeFrameColumns } from '@babel/code-frame'
-
+import os from 'os'
 import type { DataContext } from '..'
+
+const tsxCodeFrameFilter = '/node_modules/tsx/dist/register'
+const isWindows = os.platform() === 'win32'
 
 export interface CodeFrameShape {
   line: number
@@ -38,8 +41,9 @@ export class ErrorDataSource {
     } else {
       // Skip any stack trace lines which come from node:internal code
       const stackLines = stackUtils.getStackLines(source.cypressError.stack ?? '')
-      // we want to filter out any tsx transformation code in the stack to help identify the error
-      const filteredStackLines = stackLines.filter((stackLine) => !stackLine.includes('node:electron') && !stackLine.includes('node:internal') && !stackLine.includes('source-map-support') && !stackLine.includes('/node_modules/tsx/dist/register'))
+
+      // we want to filter out any tsx transformation code in the stack to help identify the error. Windows stack can have both posix paths and dos paths so we need to filter both (last line is a no-op on posix as its the same thing)
+      const filteredStackLines = stackLines.filter((stackLine) => !stackLine.includes('node:electron') && !stackLine.includes('node:internal') && !stackLine.includes('source-map-support') && !stackLine.includes(tsxCodeFrameFilter) && !(isWindows && stackLine.includes(tsxCodeFrameFilter.replaceAll('/', '\\'))))
       const parsedLine = stackUtils.parseStackLine(filteredStackLines[0] ?? '')
 
       if (parsedLine) {
