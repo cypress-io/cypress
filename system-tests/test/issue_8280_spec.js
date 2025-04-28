95f04f01e51eb62d510488cf129b0f1e21df0cc8
Yashodhan Joshi <yjdoc2@gmail.com>
2025-04-28 10:37:34 +0530

    fix: convert the test to async/await
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- system-tests/test/issue_8280_spec.js
+++ system-tests/test/issue_8280_spec.js
@@ -9,8 +9,9 @@ describe('e2e issue 8280', () => {
 
   // https://github.com/cypress-io/cypress/issues/8280
 
-  it('should retain the videos from previous runs if trashAssetsBeforeRuns=false', function () {
-    return systemTests.exec(this, {
+  it('should retain the videos from previous runs if trashAssetsBeforeRuns=false', async function () {
+    // first run
+    await systemTests.exec(this, {
       project: PROJECT_NAME,
       snapshot: false,
       expectedExitCode: 2,
@@ -18,42 +19,42 @@ describe('e2e issue 8280', () => {
         'CYPRESS_trashAssetsBeforeRuns': 'false',
       },
     })
-    .then(() => {
-      return systemTests.exec(this, {
-        project: PROJECT_NAME,
-        snapshot: false,
-        expectedExitCode: 2,
-        processEnv: {
-          'CYPRESS_trashAssetsBeforeRuns': 'false',
-        },
-      })
-    }).then(() => {
-      return fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec1.cy.js`))
-    }).then((screenshots) => {
-      expect(screenshots.length).to.eq(2)
-      expect(screenshots).to.include('spec1 -- testCase1 (failed).png')
-      expect(screenshots).to.include('spec1 -- testCase1 (failed) (1).png')
-    }).then(() => {
-      return fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec2.cy.js`))
-    }).then((screenshots) => {
-      expect(screenshots.length).to.eq(2)
-      expect(screenshots).to.include('spec2 -- testCase1 (failed).png')
-      expect(screenshots).to.include('spec2 -- testCase1 (failed) (1).png')
+
+    // second run
+    await systemTests.exec(this, {
+      project: PROJECT_NAME,
+      snapshot: false,
+      expectedExitCode: 2,
+      processEnv: {
+        'CYPRESS_trashAssetsBeforeRuns': 'false',
+      },
     })
-    .then(() => {
-      return fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/videos`))
-    }).then((videos) => {
-      expect(videos.length).to.eq(4)
-      expect(videos).to.include('spec1.cy.js.mp4')
-      expect(videos).to.include('spec1.cy.js (1).mp4')
-      expect(videos).to.include('spec2.cy.js.mp4')
-      expect(videos).to.include('spec2.cy.js (1).mp4')
-    })
+
+    const spec1Screenshots = await fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec1.cy.js`))
+
+    expect(spec1Screenshots.length).to.eq(2)
+    expect(spec1Screenshots).to.include('spec1 -- testCase1 (failed).png')
+    expect(spec1Screenshots).to.include('spec1 -- testCase1 (failed) (1).png')
+
+    const spec2Screenshots = await fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec2.cy.js`))
+
+    expect(spec2Screenshots.length).to.eq(2)
+    expect(spec2Screenshots).to.include('spec2 -- testCase1 (failed).png')
+    expect(spec2Screenshots).to.include('spec2 -- testCase1 (failed) (1).png')
+
+    const videos = await fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/videos`))
+
+    expect(videos.length).to.eq(4)
+    expect(videos).to.include('spec1.cy.js.mp4')
+    expect(videos).to.include('spec1.cy.js (1).mp4')
+    expect(videos).to.include('spec2.cy.js.mp4')
+    expect(videos).to.include('spec2.cy.js (1).mp4')
   })
 
   // if trash assets = true, then there will be no retention of screenshots or videos
-  it('should not retain the videos from previous runs if trashAssetsBeforeRuns=true', function () {
-    return systemTests.exec(this, {
+  it('should not retain the videos from previous runs if trashAssetsBeforeRuns=true', async function () {
+    // first run
+    await systemTests.exec(this, {
       project: PROJECT_NAME,
       snapshot: false,
       expectedExitCode: 2,
@@ -61,31 +62,31 @@ describe('e2e issue 8280', () => {
         'CYPRESS_trashAssetsBeforeRuns': 'true',
       },
     })
-    .then(() => {
-      return systemTests.exec(this, {
-        project: PROJECT_NAME,
-        snapshot: false,
-        expectedExitCode: 2,
-        processEnv: {
-          'CYPRESS_trashAssetsBeforeRuns': 'true',
-        },
-      })
-    }).then(() => {
-      return fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec1.cy.js`))
-    }).then((screenshots) => {
-      expect(screenshots.length).to.eq(1)
-      expect(screenshots).to.include('spec1 -- testCase1 (failed).png')
-    }).then(() => {
-      return fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec2.cy.js`))
-    }).then((screenshots) => {
-      expect(screenshots.length).to.eq(1)
-      expect(screenshots).to.include('spec2 -- testCase1 (failed).png')
-    }).then(() => {
-      return fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/videos`))
-    }).then((videos) => {
-      expect(videos.length).to.eq(2)
-      expect(videos).to.include('spec1.cy.js.mp4')
-      expect(videos).to.include('spec2.cy.js.mp4')
+
+    // second run
+    await systemTests.exec(this, {
+      project: PROJECT_NAME,
+      snapshot: false,
+      expectedExitCode: 2,
+      processEnv: {
+        'CYPRESS_trashAssetsBeforeRuns': 'true',
+      },
     })
+
+    const spec1Screenshots = await fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec1.cy.js`))
+
+    expect(spec1Screenshots.length).to.eq(1)
+    expect(spec1Screenshots).to.include('spec1 -- testCase1 (failed).png')
+
+    const spec2Screenshots = await fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/screenshots/spec2.cy.js`))
+
+    expect(spec2Screenshots.length).to.eq(1)
+    expect(spec2Screenshots).to.include('spec2 -- testCase1 (failed).png')
+
+    const videos = await fs.readdir(Fixtures.projectPath(`${PROJECT_NAME}/cypress/videos`))
+
+    expect(videos.length).to.eq(2)
+    expect(videos).to.include('spec1.cy.js.mp4')
+    expect(videos).to.include('spec2.cy.js.mp4')
   })
 })
