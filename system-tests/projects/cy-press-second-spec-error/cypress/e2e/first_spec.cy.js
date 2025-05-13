16a1ce6c98878d2c8dd2ad0f9c8880d3cbcc7602
Tim Griesser <tgriesser10@gmail.com>
2025-05-12 21:25:12 -0400

    Update first_spec.cy.js
    Updated test for chainable

--- system-tests/projects/cy-press-second-spec-error/cypress/e2e/first_spec.cy.js
+++ system-tests/projects/cy-press-second-spec-error/cypress/e2e/first_spec.cy.js
@@ -1,5 +1,7 @@
 describe('this one should pass', () => {
   it('dispatches a key press', () => {
-    cy.press(Cypress.Keyboard.Keys.TAB)
+    cy.press(Cypress.Keyboard.Keys.TAB).then((val) => {
+      expect(val).to.be.null
+    })
   })
 })
