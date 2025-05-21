9d67afcea7fad672c98c32e44453ae89bfc3affb
estrada9166 <estrada9166@gmail.com>
2025-05-21 16:54:02 -0500

    refactor: enhance UI states tests with specific Percy snapshot names

--- packages/app/cypress/e2e/runner/ui-states.cy.ts
+++ packages/app/cypress/e2e/runner/ui-states.cy.ts
@@ -12,47 +12,47 @@ describe('src/cypress/runner ui states', { retries: 0, defaultCommandTimeout: 60
         })
       }).then(() => {
         cy.contains('test hooks').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: test hooks')
         cy.contains('test hooks').click()
 
         cy.contains('commands that dont display in UI').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: commands that dont display in UI')
         cy.contains('commands that dont display in UI').click()
 
         cy.contains('commands that display in UI').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: commands that display in UI')
         cy.contains('commands that display in UI').click()
 
         cy.contains('command options').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: command options')
         cy.contains('command options').click()
 
         cy.contains('Element Visibility').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: Element Visibility')
         cy.contains('Element Visibility').click()
 
         cy.contains('Request Statuses').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: Request Statuses')
         cy.contains('Request Statuses').click()
 
         cy.contains('page events').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: page events')
         cy.contains('page events').click()
 
         cy.contains('events - page events').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: events - page events')
         cy.contains('events - page events').click()
 
         cy.contains('simple error with docs link').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: simple error with docs link')
         cy.contains('simple error with docs link').click()
 
         cy.contains('long error').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: long error')
         cy.contains('long error').click()
 
         cy.contains('Nested Tests').scrollIntoView().click()
-        cy.percySnapshot()
+        cy.percySnapshot('ui-states: Nested Tests')
         cy.contains('Nested Tests').click()
       })
     })
