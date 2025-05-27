b344f56dff400a1ab79631b84c2462d4fe6c82d6
estrada9166 <estrada9166@gmail.com>
2025-05-27 16:55:11 -0500

    Refactor snapshotReporter to dynamically set width based on reporter panel size, improving snapshot accuracy and visibility of UI elements.

--- packages/app/cypress/e2e/runner/support/snapshot-reporter.ts
+++ packages/app/cypress/e2e/runner/support/snapshot-reporter.ts
@@ -1,26 +1,30 @@
 // Takes percy snapshot with navigation/AUT/reporter hidden
 export const snapshotReporter = () => {
-  cy.percySnapshot({
-    width: 450,
-    elementOverrides: {
-      '.cy-tooltip': true,
-      '[data-cy=sidebar]': ($el) => {
-        $el.attr('style', 'display: none !important')
+  cy.get('[data-cy=reporter-panel]')
+  .invoke('width')
+  .then((w) => {
+    cy.percySnapshot({
+      width: w,
+      elementOverrides: {
+        '.cy-tooltip': true,
+        '[data-cy=sidebar]': ($el) => {
+          $el.attr('style', 'display: none !important')
+        },
+        '[data-cy=aut-panel]': ($el) => {
+          $el.attr('style', 'display: none !important')
+        },
+        '[data-cy=reporter-panel]': ($el) => {
+          $el.attr('style', `width: ${w}px !important`)
+        },
+        '[data-cy=reporter-running-icon]': ($el) => {
+          // remove 'fa-spin' class so that the icon is not animated
+          $el.attr('class', '')
+        },
+        '.command-progress': ($el) => {
+          // don't display command progress bar in snapshot
+          $el.attr('style', 'display: none !important')
+        },
       },
-      '[data-cy=aut-panel]': ($el) => {
-        $el.attr('style', 'display: none !important')
-      },
-      '[data-cy=reporter-panel]': ($el) => {
-        $el.attr('style', 'width: 450px !important')
-      },
-      '[data-cy=reporter-running-icon]': ($el) => {
-        // remove 'fa-spin' class so that the icon is not animated
-        $el.attr('class', '')
-      },
-      '.command-progress': ($el) => {
-        // don't display command progress bar in snapshot
-        $el.attr('style', 'display: none !important')
-      },
-    },
+    })
   })
 }
