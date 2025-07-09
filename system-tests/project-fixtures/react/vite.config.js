349945f5c7f51ecc9cb7ceb64ea610c4ca9eb522
Bill Glesias <bglesias@gmail.com>
2025-07-09 13:59:34 -0400

    does this work?

--- system-tests/project-fixtures/react/vite.config.js
+++ system-tests/project-fixtures/react/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   logLevel: 'silent',
   plugins: [react({ jsxRuntime: 'classic' })],
-  optimizeDeps: {
-    // users 99% of the time want this option enabled but we want consistent snapshots without optimized dependency messages printed to the console
-    // that are not suppressed by logLevel='silent'
-    noDiscovery: true,
-  },
 })
