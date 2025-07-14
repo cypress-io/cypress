a74549ca0f7cfdfe2faf30158bffb3640881ae16
Matthew Schile <mschile@cypress.io>
2025-07-14 14:21:31 -0600

    adding comment

--- packages/app/src/store/studio-store.ts
+++ packages/app/src/store/studio-store.ts
@@ -260,6 +260,7 @@ export const useStudioStore = defineStore('studioRecorder', {
     },
 
     interceptTest (test) {
+      // if this test is the one we created, we can just set the test id
       if (test.invocationDetails?.line === this.newTestLineNumber) {
         this.setTestId(test.id)
       }
