05e598e37db4821009483395a767618863c90c15
Mabel Amaya <mabel@cypress.io>
2025-07-15 16:36:35 -0400

    add useScrollIntoView hook

---
+++ packages/reporter/src/lib/useScrollIntoView.ts
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from 'react'
+import { AppState } from './app-state'
+import scroller from './scroller'
+
+interface UseScrollIntoViewOptions {
+  appState: AppState
+  testState?: string
+  isStudioActive?: boolean
+}
+
+export const useScrollIntoView = ({ appState, testState, isStudioActive = false }: UseScrollIntoViewOptions) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [isMounted, setIsMounted] = useState(false)
+
+  const _scrollIntoView = () => {
+    if (appState.autoScrollingEnabled && (appState.isRunning || isStudioActive) && testState !== 'processing') {
+      window.requestAnimationFrame(() => {
+        // since this executes async in a RAF the ref might be null
+        if (containerRef.current) {
+          scroller.scrollIntoView(containerRef.current as HTMLElement)
+        }
+      })
+    }
+  }
+
+  useEffect(() => {
+    _scrollIntoView()
+    if (!isMounted) {
+      setIsMounted(true)
+    }
+  })
+
+  return {
+    containerRef,
+    isMounted,
+    scrollIntoView: _scrollIntoView,
+  }
+}
