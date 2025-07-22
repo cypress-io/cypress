df0a4074b897333b310ef1e8982cfbcf8fe0ed89
Alex Schwartz <alexschwartz01@gmail.com>
2025-07-22 12:31:05 -0400

    Update cypress.d.ts

--- cli/types/cypress.d.ts
+++ cli/types/cypress.d.ts
@@ -7,8 +7,9 @@ type Equals<X, Y> =
   (<T>() => T extends X ? 1 : 2) extends
   (<T>() => T extends Y ? 1 : 2) ? true : false;
 
-type AllTasks_CJS = typeof import('../../../cypress.config')['CypressTasks']
-type AllTasks_ESM = typeof import('../../../cypress.config')['default']['CypressTasks']
+type CypressConfig_Data = typeof import('../../../cypress.config')
+type AllTasks_CJS = CypressConfig_Data['CypressTasks']
+type AllTasks_ESM = CypressConfig_Data['default']['CypressTasks']
 
 type AllTasks = Equals<AllTasks_CJS, any> extends true ? AllTasks_ESM : AllTasks_CJS
 type TaskEventNames = keyof AllTasks & string
