239c97aeddd60574788709614b8f8009eee57a5c
Ryan Manuel <ryanm@cypress.io>
2025-07-29 10:02:53 -0500

    Update packages/server/test/unit/modes/record_spec.js

--- packages/server/test/unit/modes/record_spec.js
+++ packages/server/test/unit/modes/record_spec.js
@@ -141,6 +141,7 @@ describe('lib/modes/record', () => {
       afterEach(() => {
         resetEnv()
       })
+
       it('calls api.createRun with the commit extracted from environment variables', () => {
         const createRun = sinon.stub(api, 'createRun').resolves()
         const runAllSpecs = sinon.stub()
