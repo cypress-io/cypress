0bcdb4c3c5b3504fd99a4798d48fc5d8d2260a0a
Cacie Prins <cacie@cypress.io>
2025-08-04 17:27:48 -0400

    we do still run tests against firefox 134

--- packages/driver/cypress/e2e/commands/cookies.cy.js
+++ packages/driver/cypress/e2e/commands/cookies.cy.js
@@ -1376,10 +1376,12 @@ describe('src/cy/commands/cookies', () => {
 
       // @see https://bugzilla.mozilla.org/show_bug.cgi?id=1550032
       // Firefox bidi returns "unspecified" for sameSite;
-      // webkit returns "no_restriction" for sameSite;
+      // webkit & firefox < 135 return "no_restriction" for sameSite;
       // other browsers do not return sameSite at all
-      const sameSite = Cypress.isBrowser('webkit')
-        ? 'no_restriction' :
+      const sameSite = (
+        (Cypress.isBrowser('firefox') && Number(Cypress.browser.majorVersion) < 135) ||
+        Cypress.isBrowser('webkit')
+      ) ? 'no_restriction' :
         Cypress.isBrowser('firefox') ? 'unspecified' : null
 
       if (sameSite) {
