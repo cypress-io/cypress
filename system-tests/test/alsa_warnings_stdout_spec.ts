734e611f622a68d0e0f2a287407d5c658fd22049
Cacie Prins <cacie@cypress.io>
2025-08-08 15:17:29 -0400

    clean up system test name

--- system-tests/test/alsa_warnings_stdout_spec.ts
+++ system-tests/test/alsa_warnings_stdout_spec.ts
@@ -1,12 +1,12 @@
 import systemTests from '../lib/system-tests'
 
-describe('Electron std{out,err} ALSA warnings', function () {
+describe('Electron sysctrlbyname warning', function () {
   it('does not render warnings in the stdout', function () {
     return systemTests.exec(this, {
       project: 'e2e',
       spec: 'embedded_video_with_audio.cy.js',
       browser: 'electron',
-    }).then(({ stdout, stderr, code }) => {
+    }).then(({ stderr }) => {
       expect(stderr).not.to.include('sysctlbyname for kern.hv_vmm_present failed with status -1')
     })
   })
