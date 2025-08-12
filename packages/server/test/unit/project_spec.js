574f553e2c5b4dbd52c19c960ab9d7d68967081e
Tyler Biethman <tyler@cypress.io>
2025-08-12 17:45:16 -0500

    Fixing unit test

--- packages/server/test/unit/project_spec.js
+++ packages/server/test/unit/project_spec.js
@@ -828,6 +828,8 @@ This option will not have an effect in Some-other-name. Tests that rely on web s
           this.project['_protocolManager'] = protocolManager
         })
 
+        sinon.stub(this.project, 'resetBrowserState').resolves()
+
         let studioInitPromise
 
         this.project.server.startWebsockets.callsFake(async (automation, config, callbacks) => {
