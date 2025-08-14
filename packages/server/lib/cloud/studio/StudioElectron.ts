91cb59c5c6b2c26699d0045d4bad2fec91ec1fa4
Tyler Biethman <tyler@cypress.io>
2025-08-13 22:56:15 -0500

    Clean up debug logs

--- packages/server/lib/cloud/studio/StudioElectron.ts
+++ packages/server/lib/cloud/studio/StudioElectron.ts
@@ -8,20 +8,19 @@ export class StudioElectron {
   private browserWindow: BrowserWindow | undefined
 
   async createBrowserWindow () {
-    this.destroy()
-
     debug('creating new browser window')
-    const createWindowStart = performance.now()
+
+    this.destroy()
 
     this.browserWindow = new BrowserWindow({
       // Hide the title bar for accurate viewport sizes
       titleBarStyle: 'hidden',
       // Hide window by default - we should never show it
-      // in production environments.
+      // in production environments
       show: false,
     })
 
-    debug('created window in', performance.now() - createWindowStart, 'ms')
+    debug('created browser window')
 
     return this.browserWindow
   }
