19d46c38ba8ac9339972c6461f801f11c52a0bf1
Bill Glesias <bglesias@gmail.com>
2025-08-24 18:57:08 -0400

    chore: convert logger to vitest

--- cli/vitest.config.js
+++ cli/vitest.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['test/lib/util.spec.ts'],
+    include: [
+      'test/lib/util.spec.ts',
+      'test/lib/logger.spec.ts',
+    ],
   },
 })
