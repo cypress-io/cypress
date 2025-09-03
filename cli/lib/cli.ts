ee38e76a79d128836a37d8ff7c5d88e349c4b981
Bill Glesias <bglesias@gmail.com>
2025-09-03 19:34:04 -0400

    chore: convert cypress spec to vitest

--- cli/lib/cli.ts
+++ cli/lib/cli.ts
@@ -608,11 +608,3 @@ const cliModule = {
 }
 
 export default cliModule
-
-// @ts-ignore
-if (!module.parent) {
-  logger.error('This CLI module should be required from another Node module')
-  logger.error('and not executed directly')
-
-  process.exit(-1)
-}
--- cli/test/lib/cypress.spec.ts
+++ cli/test/lib/cypress.spec.ts
@@ -1,197 +1,248 @@
-import '../spec_helper'
+import { vi, beforeEach, describe, it, expect } from 'vitest'
 import os from 'os'
 import path from 'path'
-import _ from 'lodash'
-import snapshot from '../support/snapshot'
-import BluebirdPromise from 'bluebird'
-import tmpModule from 'tmp'
-import mockfs from 'mock-fs'
-import fs from '../../lib/fs'
+import tmp from 'tmp'
+import fs from 'fs-extra'
 import open from '../../lib/exec/open'
 import run from '../../lib/exec/run'
 import cypress from '../../lib/cypress'
 
-const tmp = BluebirdPromise.promisifyAll(tmpModule)
+vi.mock('fs-extra', async (importActual) => {
+  const actual = await importActual()
+
+  return {
+    // @ts-expect-error
+    ...actual,
+    default: {
+      // @ts-expect-error
+      ...actual.default,
+      readJson: vi.fn(),
+    },
+  }
+})
+
+vi.mock('tmp', async (importActual) => {
+  const actual = await importActual()
+
+  return {
+    // @ts-expect-error
+    ...actual,
+    default: {
+      // @ts-expect-error
+      ...actual.default,
+      fileSync: vi.fn(),
+    },
+  }
+})
+
+vi.mock('../../lib/exec/open', async (importActual) => {
+  const actual = await importActual()
+
+  return {
+    // @ts-expect-error
+    ...actual,
+    default: {
+      // @ts-expect-error
+      ...actual.default,
+      start: vi.fn(),
+    },
+  }
+})
+
+vi.mock('../../lib/exec/run', async (importActual) => {
+  const actual = await importActual()
+
+  return {
+    // @ts-expect-error
+    ...actual,
+    default: {
+      // @ts-expect-error
+      ...actual.default,
+      start: vi.fn(),
+    },
+  }
+})
 
 describe('cypress', function () {
   beforeEach(function () {
-    mockfs({})
+    vi.unstubAllEnvs()
+    vi.resetModules()
   })
 
-  afterEach(() => {
-    mockfs.restore()
-  })
+  describe('.open', function () {
+    it('calls open#start, passing in options', async function () {
+      await cypress.open({ foo: 'foo' })
 
-  context('.open', function () {
-    beforeEach(function () {
-      sinon.stub(open, 'start').resolves()
+      expect(open.start).toHaveBeenCalledWith({ foo: 'foo' })
     })
 
-    const getStartArgs = () => {
-      expect(open.start).to.be.called
-
-      return _.get(open.start, ['lastCall', 'args', 0])
-    }
-
-    it('calls open#start, passing in options', function () {
-      return cypress.open({ foo: 'foo' })
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args.foo).to.equal('foo')
-      })
-    })
-
-    it('normalizes config object', () => {
+    it('normalizes config object', async () => {
       const config = {
         pageLoadTime: 10000,
         watchForFileChanges: false,
       }
 
-      return cypress.open({ config })
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args).to.deep.eq({ config: JSON.stringify(config) })
-      })
+      await cypress.open({ config })
+
+      expect(open.start).toHaveBeenCalledWith({ config: JSON.stringify(config) })
     })
   })
 
-  context('.run fails to write results file', function () {
-    it('resolves with error object', function () {
+  describe('.run fails to write results file', function () {
+    it('resolves with error object', async function () {
       const outputPath = path.join(os.tmpdir(), 'cypress/monorepo/cypress_spec/output.json')
 
-      // @ts-expect-error - type doesn't exist
-      sinon.stub(tmp, 'fileAsync').resolves(outputPath)
-      sinon.stub(run, 'start').resolves(2)
-      sinon.stub(fs, 'readJsonAsync').withArgs(outputPath).resolves()
+      // @ts-expect-error - mockImplementation
+      tmp.fileSync.mockReturnValue({
+        name: outputPath,
+      })
 
-      return cypress.run().then((result: any) => {
-        expect(result).to.deep.equal({
-          status: 'failed',
-          failures: 2,
-          message: 'Could not find Cypress test run results',
-        })
+      // @ts-expect-error - mockImplementation
+      run.start.mockResolvedValue(2)
+
+      // @ts-expect-error - mockImplementation
+      fs.readJson.mockImplementation(async (args) => {
+        if (args === outputPath) {
+          return Promise.resolve(undefined)
+        }
+
+        return Promise.reject(new Error('readJson not expected to fall through to this point'))
+      })
+
+      const results = await cypress.run()
+
+      expect(results).toEqual({
+        status: 'failed',
+        failures: 2,
+        message: 'Could not find Cypress test run results',
       })
     })
   })
 
-  context('.run', function () {
+  describe('.run', function () {
     let outputPath: string
 
-    beforeEach(function () {
+    beforeEach(async function () {
       outputPath = path.join(os.tmpdir(), 'cypress/monorepo/cypress_spec/output.json')
-      // @ts-expect-error - type doesn't exist
-      sinon.stub(tmp, 'fileAsync').resolves(outputPath)
-      sinon.stub(run, 'start').resolves()
 
-      return fs.outputJsonAsync(outputPath, {
-        code: 0,
-        failingTests: [],
+      // @ts-expect-error
+      tmp.fileSync.mockReturnValue({
+        name: outputPath,
+      })
+
+      // @ts-expect-error
+      run.start.mockResolvedValue(undefined)
+
+      // @ts-expect-error - mockImplementation
+      fs.readJson.mockImplementation(async (args) => {
+        if (args === outputPath) {
+          return Promise.resolve({
+            code: 0,
+            failingTests: [],
+          })
+        }
+
+        return Promise.reject(new Error('readJson not expected to fall through to this point'))
       })
     })
 
-    const normalizeCallArgs = (args: any) => {
-      expect(args.outputPath).to.equal(outputPath)
-      delete args.outputPath
+    it('calls run#start, passing in options', async () => {
+      await cypress.run({ spec: 'foo', autoCancelAfterFailures: 4 })
 
-      return args
-    }
-    const getStartArgs = () => {
-      expect(run.start).to.be.called
-
-      return normalizeCallArgs(_.get(run.start, ['lastCall', 'args', 0]))
-    }
-
-    it('calls run#start, passing in options', () => {
-      return cypress.run({ spec: 'foo', autoCancelAfterFailures: 4 })
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args.spec).to.equal('foo')
-        expect(args.autoCancelAfterFailures).to.equal(4)
-        expect(args.runnerUi).to.be.undefined
+      expect(run.start).toHaveBeenCalledWith({
+        outputPath,
+        spec: 'foo',
+        autoCancelAfterFailures: 4,
       })
     })
 
-    it('calls run#start, passing in autoCancelAfterFailures false', () => {
-      return cypress.run({ autoCancelAfterFailures: false })
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args.autoCancelAfterFailures).to.equal(false)
+    it('calls run#start, passing in autoCancelAfterFailures false', async () => {
+      await cypress.run({ autoCancelAfterFailures: false })
+
+      expect(run.start).toHaveBeenCalledWith({
+        outputPath,
+        autoCancelAfterFailures: false,
       })
     })
 
-    it('calls run#start, passing in autoCancelAfterFailures 0', () => {
-      return cypress.run({ autoCancelAfterFailures: 0 })
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args.autoCancelAfterFailures).to.equal(0)
+    it('calls run#start, passing in autoCancelAfterFailures 0', async () => {
+      await cypress.run({ autoCancelAfterFailures: 0 })
+
+      expect(run.start).toHaveBeenCalledWith({
+        outputPath,
+        autoCancelAfterFailures: 0,
       })
     })
 
-    it('normalizes config object', () => {
+    it('normalizes config object', async () => {
       const config = {
         pageLoadTime: 10000,
         watchForFileChanges: false,
       }
 
-      return cypress.run({ config })
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args).to.deep.eq({ config: JSON.stringify(config) })
+      await cypress.run({ config })
+
+      expect(run.start).toHaveBeenCalledWith({
+        outputPath,
+        config: JSON.stringify(config),
       })
     })
 
-    it('normalizes env option if passed an object', () => {
+    it('normalizes env option if passed an object', async () => {
       const env = { foo: 'bar', another: 'one' }
 
-      return cypress.run({ env })
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args).to.deep.eq({ env: JSON.stringify(env) })
+      await cypress.run({ env })
+
+      expect(run.start).toHaveBeenCalledWith({
+        outputPath,
+        env: JSON.stringify(env),
       })
     })
 
-    it('gets random tmp file and passes it to run#start', function () {
-      return cypress.run().then(() => {
-        expect((run.start as any).lastCall.args[0].outputPath).to.equal(outputPath)
-      })
+    it('gets random tmp file and passes it to run#start', async () => {
+      await cypress.run()
+
+      expect(run.start).toHaveBeenCalledWith(expect.objectContaining({
+        outputPath,
+      }))
     })
 
-    it('resolves with contents of tmp file', () => {
-      return cypress.run().then(snapshot)
+    it('resolves with contents of tmp file', async () => {
+      const results = await cypress.run()
+
+      expect(results).toMatchSnapshot()
     })
 
-    it('rejects if project is an empty string', () => {
-      return expect(cypress.run({ project: '' })).to.be.rejected
+    it('rejects if project is an empty string', async () => {
+      await expect(cypress.run({ project: '' })).rejects.toThrow()
     })
 
-    it('rejects if project is true', () => {
-      return expect(cypress.run({ project: true })).to.be.rejected
+    it('rejects if project is true', async () => {
+      await expect(cypress.run({ project: true })).rejects.toThrow()
     })
 
-    it('rejects if project is false', () => {
-      return expect(cypress.run({ project: false })).to.be.rejected
+    it('rejects if project is false', async () => {
+      await expect(cypress.run({ project: false })).rejects.toThrow()
     })
 
-    it('passes quiet: true', () => {
-      const opts = {
+    it('passes quiet: true', async () => {
+      await cypress.run({
         quiet: true,
-      }
-
-      return cypress.run(opts)
-      .then(getStartArgs)
-      .then((args: any) => {
-        expect(args).to.deep.eq(opts)
       })
+
+      expect(run.start).toHaveBeenCalledWith(expect.objectContaining({
+        quiet: true,
+      }))
     })
   })
 
-  context('cli', function () {
+  describe('cli', function () {
     describe('.parseRunArguments', function () {
       it('parses CLI cypress run arguments', async () => {
         const args = 'cypress run --browser chrome --spec my/test/spec.js'.split(' ')
         const options = await cypress.cli.parseRunArguments(args)
 
-        expect(options).to.deep.equal({
+        expect(options).toEqual({
           browser: 'chrome',
           spec: 'my/test/spec.js',
         })
@@ -201,7 +252,7 @@ describe('cypress', function () {
         const args = 'cypress run -b firefox -p 5005 --headed --quiet'.split(' ')
         const options = await cypress.cli.parseRunArguments(args)
 
-        expect(options).to.deep.equal({
+        expect(options).toEqual({
           browser: 'firefox',
           port: 5005,
           headed: true,
@@ -213,7 +264,7 @@ describe('cypress', function () {
         const args = 'cypress run --record false --dev true'.split(' ')
         const options = await cypress.cli.parseRunArguments(args)
 
-        expect(options).to.deep.equal({
+        expect(options).toEqual({
           record: false,
           dev: true,
         })
@@ -223,7 +274,7 @@ describe('cypress', function () {
         const args = 'cypress run --config-file cypress.config.js'.split(' ')
         const options = await cypress.cli.parseRunArguments(args)
 
-        expect(options).to.deep.equal({
+        expect(options).toEqual({
           configFile: 'cypress.config.js',
         })
       })
@@ -234,7 +285,7 @@ describe('cypress', function () {
 
         // we don't need to convert the config into an object
         // since the logic inside cypress.run handles that
-        expect(options).to.deep.equal({
+        expect(options).toEqual({
           config: 'baseUrl=localhost,video=true',
         })
       })
@@ -245,7 +296,7 @@ describe('cypress', function () {
 
         // we don't need to convert the environment into an object
         // since the logic inside cypress.run handles that
-        expect(options).to.deep.equal({
+        expect(options).toEqual({
           env: 'MY_NUMBER=42,MY_FLAG=true',
         })
       })
