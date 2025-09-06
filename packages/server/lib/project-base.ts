c36cad099c27a1fa46573798913ce0ad43d102e0
Ryan Manuel <ryanm@cypress.io>
2025-09-05 19:07:48 -0500

    fix tests

--- packages/server/lib/project-base.ts
+++ packages/server/lib/project-base.ts
@@ -158,8 +158,7 @@ export class ProjectBase extends EE {
     process.chdir(this.projectRoot)
 
     this._server = new ServerBase(cfg)
-    // @ts-expect-error - this will not error when we actually release the experimentalPromptCommand flag
-    if (cfg.resolved.experimentalPromptCommand) {
+    if (cfg.experimentalPromptCommand) {
       const cyPromptLifecycleManager = new CyPromptLifecycleManager()
 
       cyPromptLifecycleManager.initializeCyPromptManager({
