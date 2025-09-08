f7d1198663acbbcf7dfaf31e4a40f7bc859b73f9
Cacie Prins <cacieprins@users.noreply.github.com>
2025-09-08 15:26:45 -0400

    Apply suggestion from @cacieprins

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     projects: [
-      '{packages,tools,scripts}/*/vi{test,te}.config.{ts,mjs}',
+      '{packages,tooling,scripts}/*/vitest.config.{ts,mjs}',
       'cli/vitest.config.*',
     ],
     exclude: ['**/node_modules/**', '**/dist/**', 'system-tests/**'],
