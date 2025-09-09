281c04bd135a92e15ff6c4fc202ae756a6cd050b
Cacie Prins <cacie@cypress.io>
2025-09-09 12:46:20 -0400

    fix test pattern for vite-dev-server

--- npm/vite-dev-server/vitest.config.ts
+++ npm/vite-dev-server/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.spec.ts'],
+    include: ['test/**/*.spec.ts'],
     globals: true,
     environment: 'node',
   },
