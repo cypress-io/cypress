fc9c1c7c219a18ac5c1691ee980ca744665fac9f
Bill Glesias <bglesias@gmail.com>
2025-09-19 12:21:39 -0400

    convert utils to vitest

--- packages/config/vitest.config.ts
+++ packages/config/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['test/project/index.spec.ts'],
+    include: ['test/project/index.spec.ts', 'test/project/utils.spec.ts'],
     globals: true,
     environment: 'node',
   },
