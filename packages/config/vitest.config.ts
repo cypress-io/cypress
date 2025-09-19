8ba41977a5797cb9720fbd1b72d3a13841f3ca03
Bill Glesias <bglesias@gmail.com>
2025-09-19 13:02:10 -0400

    add plugin tests to vitest config

--- packages/config/vitest.config.ts
+++ packages/config/vitest.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
       'test/index.spec.ts',
       'test/utils.spec.ts',
       'test/ast-utils/addPluginId.spec.ts',
+      'test/ast-utils/addToCypressConfigPlugin-e2e.spec.ts',
+      'test/ast-utils/addToCypressConfigPlugin.spec.ts',
       'test/ast-utils/addToCypressConfig.spec.ts',
     ],
     globals: true,
