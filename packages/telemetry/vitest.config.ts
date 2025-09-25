a6211afc4472a80b8f5824991b06919514831c67
Bill Glesias <bglesias@gmail.com>
2025-09-25 18:27:07 -0400

    convert githubActionsDetectorSync to vitest

--- packages/telemetry/vitest.config.ts
+++ packages/telemetry/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     include: [
       'test/detectors/circleCiDetectorSync.spec.ts',
+      'test/detectors/githubActionsDetectorSync.spec.ts',
     ],
     globals: true,
     environment: 'node',
