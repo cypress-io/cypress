83640df21962eea182fb82290d9e5e781a1cb323
Bill Glesias <bglesias@gmail.com>
2025-09-25 15:10:51 -0400

    convert the visualsnapshoterrors spec to vitest

--- packages/errors/vitest.config.ts
+++ packages/errors/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    env: {
+      CYPRESS_INTERNAL_ENV: 'test',
+    },
     include: ['test/**/*.spec.ts'],
     globals: true,
     environment: 'node',
