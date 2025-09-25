fc76c6a809393823c367aeb3ba09bd48854414c0
Bill Glesias <bglesias@gmail.com>
2025-09-24 21:32:18 -0400

    chore: convert frameworks spec to vitest

--- packages/scaffold-config/vitest.config.ts
+++ packages/scaffold-config/vitest.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['test/unit/supportFile.spec.ts'],
+    include: [
+      'test/unit/supportFile.spec.ts',
+      'test/unit/frameworks.spec.ts',
+    ],
     globals: true,
     environment: 'node',
   },
