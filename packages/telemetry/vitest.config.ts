5460613e125286105488916e4d4caecd39eb5250
Bill Glesias <bglesias@gmail.com>
2025-09-25 20:35:57 -0400

    convert trace link exporter to vitest

--- packages/telemetry/vitest.config.ts
+++ packages/telemetry/vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       'test/detectors/githubActionsDetectorSync.spec.ts',
       'test/processors/on-start-span-processor.spec.ts',
       'test/span-exporters/cloud-span-exporter.spec.ts',
+      'test/span-exporters/console-trace-link-exporter.spec.ts',
     ],
     globals: true,
     environment: 'node',
