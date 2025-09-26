b45038a83d1b9e48d027f77f3852ee59303b8ec5
Bill Glesias <bglesias@gmail.com>
2025-09-25 21:22:45 -0400

    clean up telemetry package after vite conversion

--- packages/telemetry/vitest.config.ts
+++ packages/telemetry/vitest.config.ts
@@ -2,18 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: [
-      'test/detectors/circleCiDetectorSync.spec.ts',
-      'test/detectors/githubActionsDetectorSync.spec.ts',
-      'test/processors/on-start-span-processor.spec.ts',
-      'test/span-exporters/cloud-span-exporter.spec.ts',
-      'test/span-exporters/console-trace-link-exporter.spec.ts',
-      'test/span-exporters/ipc-span-exporter.spec.ts',
-      'test/span-exporters/websocket-span-exporter.spec.ts',
-      'test/browser.spec.ts',
-      'test/index.spec.ts',
-      'test/node.spec.ts',
-    ],
+    include: ['test/**/*.spec.ts'],
     globals: true,
     environment: 'node',
   },
