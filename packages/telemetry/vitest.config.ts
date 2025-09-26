ec3d6d7e8837a522ff5b28385bbd355b51a78f2a
Bill Glesias <bglesias@gmail.com>
2025-09-25 20:28:12 -0400

    chore: convert cloud span exporter to vitest

--- packages/telemetry/vitest.config.ts
+++ packages/telemetry/vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
       'test/detectors/circleCiDetectorSync.spec.ts',
       'test/detectors/githubActionsDetectorSync.spec.ts',
       'test/processors/on-start-span-processor.spec.ts',
+      'test/span-exporters/cloud-span-exporter.spec.ts',
     ],
     globals: true,
     environment: 'node',
