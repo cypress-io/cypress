fb0ce2fc35f855d2d53938736a084068af3efc65
Bill Glesias <bglesias@gmail.com>
2025-09-29 10:01:50 -0400

    chore: convert revelvant runs data source to jest

--- packages/data-context/jest.config.ts
+++ packages/data-context/jest.config.ts
@@ -32,6 +32,7 @@ export default async (): Promise<Config> => {
       '<rootDir>/test/unit/sources/GitDataSource.spec.ts',
       '<rootDir>/test/unit/sources/GraphQLDataSource.spec.ts',
       '<rootDir>/test/unit/sources/ProjectDataSource.spec.ts',
+      '<rootDir>/test/unit/sources/RelevantRunsDataSource.spec.ts',
     ],
     testEnvironment: 'node',
     transform: {
