99b38daab8f0e35d34b3446916ef194c8efff246
Bill Glesias <bglesias@gmail.com>
2025-09-29 14:22:13 -0400

    chore: convert weighted choice spec to jest

--- packages/data-context/jest.config.ts
+++ packages/data-context/jest.config.ts
@@ -41,6 +41,7 @@ export default async (): Promise<Config> => {
       '<rootDir>/test/unit/util/DocumentNodeBuilder.spec.ts',
       '<rootDir>/test/unit/util/hasTypescript.spec.ts',
       '<rootDir>/test/unit/util/testCounts.spec.ts',
+      '<rootDir>/test/unit/util/weightedChoice.spec.ts',
     ],
     testEnvironment: 'node',
     transform: {
