d3e6fd7b1dc0428ef7f3eda39f485f83373ad12f
Bill Glesias <bglesias@gmail.com>
2025-09-28 23:22:13 -0400

    chore: convert git data source to jest

--- packages/data-context/jest.config.ts
+++ packages/data-context/jest.config.ts
@@ -29,6 +29,7 @@ export default async (): Promise<Config> => {
       '<rootDir>/test/unit/sources/CloudDataSource.spec.ts',
       '<rootDir>/test/unit/sources/GitDataSource_unit.spec.ts',
       '<rootDir>/test/unit/sources/FileDataSource.spec.ts',
+      '<rootDir>/test/unit/sources/GitDataSource.spec.ts',
     ],
     testEnvironment: 'node',
     transform: {
