8824abec56588ce02a3e1d16761a8c68fe77e440
Cacie Prins <cacie@cypress.io>
2025-10-01 13:57:44 -0400

    lift disabled posix system test to different desc block

--- system-tests/test/posix_exit_codes_spec.ts
+++ system-tests/test/posix_exit_codes_spec.ts
@@ -4,18 +4,12 @@ describe('posix exit codes', () => {
   systemTests.setup()
 
   describe('when posix exit codes are enabled', () => {
+    const posixExitCodes = true
+
     systemTests.it('returns 1 when there are failing tests', {
       spec: 'simple_failing.cy.js',
-      posixExitCodes: true,
+      posixExitCodes,
       expectedExitCode: 1,
-      browser: ['electron'],
-      project: 'e2e',
-    })
-
-    systemTests.it('returns 2 when there are 2 failing tests and posix is disabled', {
-      spec: 'simple_failing.cy.js',
-      posixExitCodes: false,
-      expectedExitCode: 2,
       browser: ['electron'],
       project: 'e2e',
     })
@@ -24,6 +18,18 @@ describe('posix exit codes', () => {
       spec: 'simple_passing.cy.js',
       posixExitCodes: true,
       expectedExitCode: 0,
+      browser: ['electron'],
+      project: 'e2e',
+    })
+  })
+
+  describe('when posix exit codes are disabled', () => {
+    const posixExitCodes = false
+
+    systemTests.it('returns 2 when there are 2 failing tests', {
+      spec: 'simple_failing.cy.js',
+      posixExitCodes,
+      expectedExitCode: 2,
       browser: ['electron'],
       project: 'e2e',
     })
