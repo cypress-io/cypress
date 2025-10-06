3cf811e60c1a623a499b973a1140df7d5e4d10f7
Bill Glesias <bglesias@gmail.com>
2025-10-06 11:28:02 -0400

    correctly configure ES Module for packages/icons

--- packages/icons/test/icons.spec.ts
+++ packages/icons/test/icons.spec.ts
@@ -1,18 +1,22 @@
 import { describe, it, expect } from 'vitest'
-import * as icons from '../index'
+import * as iconsESM from '../index.mjs'
+import * as iconsCjs from '../index.js'
 
 const cwd = process.cwd()
 
 describe('Cypress Icons', function () {
   it('returns path to favicon', function () {
-    expect(icons.getPathToFavicon('favicon-red.ico')).toEqual(`${cwd }/dist/favicon/favicon-red.ico`)
+    expect(iconsESM.getPathToFavicon('favicon-red.ico')).toEqual(`${cwd }/dist/favicon/favicon-red.ico`)
+    expect(iconsCjs.getPathToFavicon('favicon-red.ico')).toEqual(`${cwd }/dist/favicon/favicon-red.ico`)
   })
 
   it('returns path to icon', function () {
-    expect(icons.getPathToIcon('cypress.icns')).toEqual(`${cwd }/dist/icons/cypress.icns`)
+    expect(iconsESM.getPathToIcon('cypress.icns')).toEqual(`${cwd }/dist/icons/cypress.icns`)
+    expect(iconsCjs.getPathToIcon('cypress.icns')).toEqual(`${cwd }/dist/icons/cypress.icns`)
   })
 
   it('returns path to logo', function () {
-    expect(icons.getPathToLogo('cypress-bw.png')).toEqual(`${cwd }/dist/logo/cypress-bw.png`)
+    expect(iconsESM.getPathToLogo('cypress-bw.png')).toEqual(`${cwd }/dist/logo/cypress-bw.png`)
+    expect(iconsCjs.getPathToLogo('cypress-bw.png')).toEqual(`${cwd }/dist/logo/cypress-bw.png`)
   })
 })
