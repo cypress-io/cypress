894f8564f4443b54eabf74780ce87b92d81c5558
estrada9166 <estrada9166@gmail.com>
2025-10-07 11:55:43 -0500

    Update with code review, add test

--- packages/reporter/src/commands/command.tsx
+++ packages/reporter/src/commands/command.tsx
@@ -491,7 +491,7 @@ const Command: React.FC<CommandProps> = observer(({ model, aliasesWithDuplicates
     }
   }
 
-  if (model.name === 'request' && !appState.showFetchRequests) {
+  if (model.name === 'request' && model.event && !appState.showFetchRequests) {
     return null
   }
 
