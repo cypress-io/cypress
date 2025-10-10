97bde14ea7c90815bc4c8dd59b3ef1f00323b28f
Jennifer Shehane <shehane.jennifer@gmail.com>
2025-10-10 10:21:27 -0400

    chore: remove todomvc testing + other outdated docs / references found

--- npm/react/cypress/component/advanced/custom-command/spec.cy.jsx
+++ npm/react/cypress/component/advanced/custom-command/spec.cy.jsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import { mount } from '@cypress/react'
 
-// https://github.com/bahmutov/@cypress/react/issues/184
 Cypress.Commands.add('myMount', () => {
   return mount(<div>My mount</div>)
 })
