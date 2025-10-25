fd71292aa4d9f2ce93dffb2ee958330de38bcc33
Ryan Manuel <ryanm@cypress.io>
2025-10-25 11:23:21 -0500

    Add wait before interacting with error file path
    Add wait time before clicking the error file path.

--- npm/vite-dev-server/cypress/e2e/vite-dev-server.cy.ts
+++ npm/vite-dev-server/cypress/e2e/vite-dev-server.cy.ts
@@ -168,6 +168,7 @@ describe('sourcemaps', () => {
 
       // Wait for the error to be fully rendered and interactive
       cy.get('.runnable-err-file-path').eq(1).should('be.visible').and('not.be.disabled')
+      cy.wait(2000)
       cy.get('.runnable-err-file-path').eq(1).click()
       cy.get('@emit').should('have.been.calledWithMatch', 'open:file', {
         absoluteFile: Cypress.sinon.match(new RegExp(`cy-projects/vite7.0.0-react/src/${specName}$`)),
