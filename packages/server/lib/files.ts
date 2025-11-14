be4bd837e7d0255f36ec7648f0528cce489b5c77
Bill Glesias <bglesias@gmail.com>
2025-11-14 14:33:00 -0500

    empty commit

--- packages/server/lib/files.ts
+++ packages/server/lib/files.ts
@@ -1,9 +1,8 @@
 import path from 'path'
 import { fs } from './util/fs'
 
-export async function readFile (projectRoot: string, options: { file: string, encoding?: string } = { file: '', encoding: 'utf8' }) {
+export async function readFile (projectRoot: string, options: { file: string, encoding?: BufferEncoding } = { file: '', encoding: 'utf8' }) {
   const filePath = path.resolve(projectRoot, options.file)
-  const readFn = (path.extname(filePath) === '.json' && options.encoding !== null) ? fs.readJsonAsync : fs.readFileAsync
 
   // https://github.com/cypress-io/cypress/issues/1558
   // If no encoding is specified, then Cypress has historically defaulted
@@ -11,8 +10,15 @@ export async function readFile (projectRoot: string, options: { file: string, en
   // NodeJs, which defaults to binary. We allow users to pass in `null`
   // to restore the default node behavior.
   try {
-    // @ts-ignore as we are mixing types with fs
-    const contents = await readFn(filePath, options.encoding === undefined ? 'utf8' : options.encoding)
+    let contents
+
+    if (path.extname(filePath) === '.json' && options.encoding !== null) {
+      contents = await fs.readJsonAsync(filePath, options.encoding === undefined ? 'utf8' : options.encoding)
+    } else {
+      contents = await fs.readFileAsync(filePath, {
+        encoding: options.encoding === undefined ? 'utf8' : options.encoding,
+      })
+    }
 
     return {
       contents,
@@ -25,7 +31,7 @@ export async function readFile (projectRoot: string, options: { file: string, en
   }
 }
 
-export async function readFiles (projectRoot: string, options: { files: { path: string, encoding?: string }[] } = { files: [] }) {
+export async function readFiles (projectRoot: string, options: { files: { path: string, encoding?: BufferEncoding }[] } = { files: [] }) {
   const files = await Promise.all(options.files.map(async (file) => {
     const { contents, filePath } = await readFile(projectRoot, {
       file: file.path,
@@ -42,7 +48,7 @@ export async function readFiles (projectRoot: string, options: { files: { path: 
   return files
 }
 
-export async function writeFile (projectRoot: string, options: { fileName: string, contents: string, encoding?: string, flag?: string } = { fileName: '', contents: '', encoding: 'utf8', flag: 'w' }) {
+export async function writeFile (projectRoot: string, options: { fileName: string, contents: string, encoding?: BufferEncoding, flag?: string } = { fileName: '', contents: '', encoding: 'utf8', flag: 'w' }) {
   const filePath = path.resolve(projectRoot, options.fileName)
   const writeOptions = {
     encoding: options.encoding === undefined ? 'utf8' : options.encoding,
--- packages/server/test/unit/plugins/child/run_child_fixture.js
+++ packages/server/test/unit/plugins/child/run_child_fixture.js
@@ -1,7 +1,6 @@
 const childProcess = require('child_process')
 const path = require('path')
 
-// load with tsx?
 const REQUIRE_ASYNC_CHILD_PATH = require.resolve('@packages/server/lib/plugins/child/require_async_child')
 
 let proc
@@ -10,6 +9,7 @@ process.on('message', (msg) => {
   if (msg.msg === 'spawn') {
     proc = childProcess.fork(REQUIRE_ASYNC_CHILD_PATH, ['--projectRoot', msg.data.projectRoot, '--file', path.join(msg.data.projectRoot, 'cypress.config.js')], {
       env: {
+        // since some files are being converted to TypeScript AND we are using tsx to load the config process, we can reliably use it to load the async child here
         NODE_OPTIONS: '--import tsx',
       },
     })
