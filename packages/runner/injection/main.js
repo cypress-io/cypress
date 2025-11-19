be6a22f413e99400f71df5b35e357b2d3688566d
Alex Schwartz <alexschwartz01@gmail.com>
2025-11-19 13:05:48 -0500

    Update main.js

--- packages/runner/injection/main.js
+++ packages/runner/injection/main.js
@@ -38,7 +38,7 @@ const originalXmlHttpRequestOpen = window.XMLHttpRequest.prototype.open
 window.XMLHttpRequest.prototype.open = function (...args) {
   const result = originalXmlHttpRequestOpen.apply(this, args)
 
-  if (args[2] === false) {
+  if (args.length > 2 && !args[2]) {
     this.setRequestHeader('x-cypress-is-sync-request', 'true')
   }
 
