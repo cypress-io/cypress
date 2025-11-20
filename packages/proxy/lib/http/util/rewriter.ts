536ef85bb36323b701117c4ded8e43000722c266
Alex Schwartz <alexschwartz01@gmail.com>
2025-11-20 17:38:59 -0500

    Fix mocked

--- packages/proxy/lib/http/util/rewriter.ts
+++ packages/proxy/lib/http/util/rewriter.ts
@@ -98,8 +98,8 @@ const isScriptTopLevel = (scriptMatch, htmlMatch) => {
   return scriptIndex < htmlIndex
 }
 
-export async function html (html: string, opts: SecurityOpts & InjectionOpts) {
-  const htmlToInject = await Promise.resolve(getHtmlToInject(opts))
+export async function html (html: string, opts: SecurityOpts & InjectionOpts, mockedHtmlToInject: string = "") {
+  const htmlToInject = mockedHtmlToInject || await Promise.resolve(getHtmlToInject(opts))
 
   // strip clickjacking and framebusting
   // from the HTML if we've been told to
