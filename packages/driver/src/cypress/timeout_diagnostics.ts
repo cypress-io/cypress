413b58ebd1ea0a18fe45afc6e1008e4bac3c2e87
lucas.gontijo <lucgonp@hotmail.com>
2025-12-02 14:43:13 -0300

    chore: update timeout diagnostics documentation for clarity

--- packages/driver/src/cypress/timeout_diagnostics.ts
+++ packages/driver/src/cypress/timeout_diagnostics.ts
@@ -224,8 +224,8 @@ export class TimeoutDiagnostics {
   private static escapeSelector (selector: string): string {
     return selector
     .replace(/\\/g, '\\\\')
-    .replace(/"/g, '\\"')
-    .replace(/'/g, '\\\'')
+    .replace(/"/g, '\\\\\\"')
+    .replace(/'/g, '\\'')
   }
 
   /**
