afeeac790243fea02b99b311e5a99d9128c27949
Alex Schwartz <alexschwartz01@gmail.com>
2025-12-03 07:11:44 -0500

    revert one of them back to space

--- packages/proxy/lib/http/util/rewriter.ts
+++ packages/proxy/lib/http/util/rewriter.ts
@@ -66,7 +66,7 @@ function getHtmlToInject (opts: InjectionOpts & SecurityOpts) {
 const insertAfter = (originalString, match, stringToInsert) => {
   const index = (match.index || 0) + match[0].length
 
-  return `${originalString.slice(0, index)}\n${stringToInsert}${originalString.slice(index)}`
+  return `${originalString.slice(0, index)} ${stringToInsert}${originalString.slice(index)}`
 }
 
 export async function html (html: string, opts: SecurityOpts & InjectionOpts) {
