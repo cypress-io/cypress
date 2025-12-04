5469fef744f16ff47b847afe07c84591f76346bb
Alex Schwartz <alexschwartz01@gmail.com>
2025-12-04 09:51:59 -0500

    simplify

--- packages/proxy/lib/http/util/rewriter.ts
+++ packages/proxy/lib/http/util/rewriter.ts
@@ -22,7 +22,7 @@ export type InjectionOpts = {
   shouldInjectDocumentDomain: boolean
 }
 
-const doctypeRe = /^[\s]*?<\!doctype.*?>/i
+const doctypeRe = /^\s*<\!doctype.*?>/i
 
 function getRewriter (useAstSourceRewriting: boolean) {
   return useAstSourceRewriting ? astRewriter : regexRewriter
