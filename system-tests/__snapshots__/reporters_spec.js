7bb52cc95e4af5855b49dc04c1bef2a4a4da1387
Jennifer Shehane <shehane.jennifer@gmail.com>
2025-12-18 11:03:38 -0500

    fix snapshot mismatches

--- system-tests/__snapshots__/reporters_spec.js
+++ system-tests/__snapshots__/reporters_spec.js
@@ -11,14 +11,6 @@ Learn more at https://on.cypress.io/reporters
 Error: Cannot find module '/foo/bar/.projects/e2e/node_modules/module-does-not-exist'
 Require stack:
 - lib/reporter.js
-- lib/project-base.ts
-- lib/open_project.ts
-- lib/makeDataContext.ts
-- lib/modes/index.ts
-- lib/cypress.ts
-- start-cypress.js
-- index.js
-- 
       [stack trace lines]
 
 
@@ -153,8 +145,6 @@ Learn more at https://on.cypress.io/reporters
 
 Error: this reporter threw an error
       [stack trace lines]
-
-
 `
 
 exports['e2e reporters supports teamcity reporter and reporter options 1'] = `
