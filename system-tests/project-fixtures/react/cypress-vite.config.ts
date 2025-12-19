4617d4b39e8247743c887ae421af87249762a7f6
Jennifer Shehane <shehane.jennifer@gmail.com>
2025-12-19 10:36:13 -0500

    put tests back in chrome + turn off screenshots in failures

--- system-tests/project-fixtures/react/cypress-vite.config.ts
+++ system-tests/project-fixtures/react/cypress-vite.config.ts
@@ -32,4 +32,5 @@ export default defineConfig({
   // These tests should run quickly / fail quickly,
   // since we intentionally causing error states for testing
   defaultCommandTimeout: 1000,
+  screenshotOnRunFailure: false,
 })
