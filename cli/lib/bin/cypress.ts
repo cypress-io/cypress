157f7802f08ff61a7e3f6a85a3abd43e20f08018
Cacie Prins <cacie@cypress.io>
2025-12-30 15:38:16 -0500

    fix clean

--- cli/lib/bin/cypress.ts
+++ cli/lib/bin/cypress.ts
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
 // declared here in order to avoid consumers who are looking for the binary to be available relative to the dist directory
-import CLI from '../cli'
-
-CLI.init()
+import '../../bin/cypress'
