513ef758e2af64dcc808cdfa077ba91580a17e64
Cacie Prins <cacie@cypress.io>
2025-12-30 15:05:34 -0500

    and

--- cli/scripts/prep-build-dir.ts
+++ cli/scripts/prep-build-dir.ts
@@ -4,6 +4,8 @@ import shell from 'shelljs'
 shell.set('-v') // verbose
 shell.set('-e') // any error is fatal
 
+shell.mkdir('-p', 'build')
+
 shell.cp('NPM_README.md', 'build/README.md')
 shell.cp('.release.json', 'build/.release.json')
 
