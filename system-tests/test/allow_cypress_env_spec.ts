445fa24174d6212ba696fc74591565f389979434
Cacie Prins <cacie@cypress.io>
2026-01-26 15:03:23 -0500

    chore: clarify allowCypressEnv error message

--- system-tests/test/allow_cypress_env_spec.ts
+++ system-tests/test/allow_cypress_env_spec.ts
@@ -5,7 +5,7 @@ describe('allowCypressEnv', () => {
 
   systemTests.it('throws an error when trying to use Cypress.env() with allowCypressEnv=false', {
     project: 'allow-cypress-env',
-    configFile: 'cypress-with-allow-cypress-env.config.ts',
+    configFile: 'cypress-without-allow-cypress-env.config.ts',
     snapshot: true,
     expectedExitCode: 1,
     browser: 'electron',
@@ -13,7 +13,7 @@ describe('allowCypressEnv', () => {
 
   systemTests.it('correctly prints a warning when trying to use Cypress.env() with allowCypressEnv=true', {
     project: 'allow-cypress-env',
-    configFile: 'cypress-without-allow-cypress-env.config.ts',
+    configFile: 'cypress-with-allow-cypress-env.config.ts',
     snapshot: true,
     expectedExitCode: 0,
     browser: 'electron',
@@ -21,10 +21,10 @@ describe('allowCypressEnv', () => {
       const res = await exec()
 
       // verify the error actually exists within the stdout in addition to a snapshot
-      expect(res.stdout).to.contain('The use of Cypress.env() is deprecated and will be removed in a future major version of Cypress')
-      expect(res.stdout).to.contain('Cypress recommends migrating to the cy.env() command and disabling allowCypressEnv within your Cypress configuration.')
-      expect(res.stdout).to.contain('The use of Cypress.env() will warn and throw an error when allowCypressEnv is explicitly set to false.')
-      expect(res.stdout).to.contain('Read our Migration Guide for the allowCypressEnv configuration option, why Cypress.env() is deprecated, and how to migrate to cy.env(): https://on.cypress.io/cypress-env-migration.')
+      expect(res.stdout).to.contain('The allowCypressEnv configuration option is enabled.')
+      expect(res.stdout).to.contain('This allows any browser code to read values from Cypress.env().')
+      expect(res.stdout).to.contain('This is insecure and will be removed in a future major version.')
+      expect(res.stdout).to.contain('Learn more: https://on.cypress.io/cypress-env-migration')
     },
   })
 })
