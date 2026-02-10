bdca3a05c306a92db648acd0ce1fd5a1c91b93a2
Matthew Schile <mschile@cypress.io>
2026-02-09 23:08:56 -0700

    prevent caching

--- packages/driver/cypress/plugins/server.js
+++ packages/driver/cypress/plugins/server.js
@@ -417,6 +417,11 @@ const createApp = (port) => {
 
     res.setHeader('Content-Type', contentType)
 
+    // Prevent caching
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate')
+    res.setHeader('Pragma', 'no-cache')
+    res.setHeader('Expires', '0')
+
     let buffer
     let contentEncoding
 
