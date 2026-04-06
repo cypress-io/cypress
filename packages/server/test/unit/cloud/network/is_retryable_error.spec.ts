58c4be557277f6a38d1198ee4a51fbbf337f7976
Cacie Prins <cacie@cypress.io>
2026-04-06 12:16:34 -0400

    is_retryable_error

--- packages/server/test/unit/cloud/network/is_retryable_error.spec.ts
+++ packages/server/test/unit/cloud/network/is_retryable_error.spec.ts
@@ -1,33 +1,30 @@
+import { Response } from 'cross-fetch'
+import { describe, expect, it } from 'vitest'
+
 import { isRetryableError } from '../../../../lib/cloud/network/is_retryable_error'
-import { SystemError } from '../../../../lib/cloud/network/system_error'
 import { HttpError } from '../../../../lib/cloud/network/http_error'
-
-import sinon from 'sinon'
-import chai, { expect } from 'chai'
-import sinonChai from 'sinon-chai'
-
-chai.use(sinonChai)
+import { SystemError } from '../../../../lib/cloud/network/system_error'
 
 describe('isRetryableError', () => {
   const url = 'http://some/url'
 
   it('returns true with a NetworkError', () => {
-    expect(isRetryableError(new SystemError(new Error(), url, 'ECONNRESET', 100))).to.be.true
+    expect(isRetryableError(new SystemError(new Error(), url, 'ECONNRESET', 100))).toBe(true)
   })
 
   it('returns true with retryable http errors', () => {
     [408, 429, 502, 503, 504].forEach((status) => {
-      const err = new HttpError('some error', url, status, 'status text', 'response_body', sinon.createStubInstance(Response))
+      const err = new HttpError('some error', url, status, 'status text', 'response_body', new Response())
 
-      expect(isRetryableError(err)).to.be.true
+      expect(isRetryableError(err)).toBe(true)
     })
   })
 
   it('returns false with non-retryable http errors', () => {
     [400, 401, 402, 403, 404, 405, 406, 407, 409, 410, 411, 412, 413, 414, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 431, 451, 500, 501, 505, 507, 508, 510, 511].forEach((status) => {
-      const err = new HttpError('some error', url, status, 'status text', 'response_body', sinon.createStubInstance(Response))
+      const err = new HttpError('some error', url, status, 'status text', 'response_body', new Response())
 
-      expect(isRetryableError(err)).to.be.false
+      expect(isRetryableError(err)).toBe(false)
     })
   })
 
@@ -37,13 +34,13 @@ describe('isRetryableError', () => {
     const err3 = new SystemError(new Error(), url, 'SELF_SIGNED_CERT_IN_CHAIN', 100)
     const err4 = new SystemError(new Error(), url, 'UNABLE_TO_GET_ISSUER_CERT_LOCALLY', 100)
 
-    expect(isRetryableError(err1)).to.be.false
-    expect(isRetryableError(err2)).to.be.false
-    expect(isRetryableError(err3)).to.be.false
-    expect(isRetryableError(err4)).to.be.false
+    expect(isRetryableError(err1)).toBe(false)
+    expect(isRetryableError(err2)).toBe(false)
+    expect(isRetryableError(err3)).toBe(false)
+    expect(isRetryableError(err4)).toBe(false)
   })
 
   it('returns false for other errors', () => {
-    expect(isRetryableError(new Error())).to.be.false
+    expect(isRetryableError(new Error())).toBe(false)
   })
 })
