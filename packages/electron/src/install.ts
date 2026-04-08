cec9e9c26aa8550adb9836ef376ef2d03aa7977a
Cacie Prins <cacie@cypress.io>
2026-04-08 11:12:34 -0400

    access .default for pkgr; rm unused resolutions

--- packages/electron/src/install.ts
+++ packages/electron/src/install.ts
@@ -155,7 +155,7 @@ async function pkgElectronApp (
 
   const e = 'electron'
   const p = 'packager'
-  const pkgr = require(`@${e}/${p}`)
+  const pkgr = require(`@${e}/${p}`).default
   const icons = require('@packages/icons')
 
   const iconPath = icons.getPathToIcon('cypress')
