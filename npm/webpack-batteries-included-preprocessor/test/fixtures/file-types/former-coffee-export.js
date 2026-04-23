7d5a6aaee6b64fb83b1b6dbc73cb48940082ce88
Jennifer Shehane <shehane.jennifer@gmail.com>
2026-04-23 09:16:25 -0400

    remove coffeescript import

--- npm/webpack-batteries-included-preprocessor/test/fixtures/file-types/former-coffee-export.js
+++
@@ -1,1 +0,0 @@
-export const fromCoffee = 'from coffee'
--- npm/webpack-batteries-included-preprocessor/test/fixtures/various_imports_spec.js
+++ npm/webpack-batteries-included-preprocessor/test/fixtures/various_imports_spec.js
@@ -6,13 +6,11 @@ import graphql from 'graphql'
 
 import { fromJs } from './file-types/js-file'
 import { fromJsx } from './file-types/jsx-file'
-import { fromCoffee } from './file-types/former-coffee-export'
 import { fromMjs } from './file-types/mjs-file'
 
 import json from './json_file'
 
 expect(fromJs).equal('from js')
 expect(fromJsx).to.be.an('object')
-expect(fromCoffee).equal('from coffee')
 expect(fromMjs).equal('from mjs')
 expect(json).to.eql({ json: 'contents' })
