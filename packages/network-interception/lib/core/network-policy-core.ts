b3b79fdb6aae684ea19971c59c6ec76cd53b6fab
Cacie Prins <cacie@cypress.io>
2026-05-26 13:23:30 -0400

    fix type-only imports

--- packages/network-interception/lib/core/network-policy-core.ts
+++ packages/network-interception/lib/core/network-policy-core.ts
@@ -9,9 +9,12 @@ import type {
 } from '../ports/driven-ports'
 import type { BackendRoute } from '../types/backend-route'
 import type { CyHttpMessages } from '../types/external-types'
-import { mergeIncomingRequestChanges, type MergeIncomingRequestChangesOptions } from './merge-handler-result'
-import { planSubscriptions, type PlanSubscriptionsOptions, type PlannedRouteSubscriptions } from './plan-subscriptions'
-import { matchRoutes, matchesRoutePreflight, type RouteMatchableRequest } from './route-matching'
+import { mergeIncomingRequestChanges } from './merge-handler-result'
+import type { MergeIncomingRequestChangesOptions } from './merge-handler-result'
+import { planSubscriptions } from './plan-subscriptions'
+import type { PlanSubscriptionsOptions, PlannedRouteSubscriptions } from './plan-subscriptions'
+import { matchRoutes, matchesRoutePreflight } from './route-matching'
+import type { RouteMatchableRequest } from './route-matching'
 
 export type NetworkPolicyCoreOptions = {
   requestInterception?: ForRequestInterception
