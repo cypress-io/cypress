4e07dd8643400c7b6ebb906a6332887796ef30f2
Mabel Amaya <mabel@cypress.io>
2026-06-01 15:08:07 -0400

    revert accidental project-base.ts change from remoteOrigin commit

--- packages/server/lib/project-base.ts
+++ packages/server/lib/project-base.ts
@@ -509,20 +509,16 @@ export class ProjectBase extends EE {
               return { canAccessStudioAI, cloudStudioSessionId }
             }
 
-            const protocolManager = studio.protocolManager
-
-            protocolManager.setupProtocol()
-            protocolManager.beforeSpec({
+            this.protocolManager = studio.protocolManager
+            this.protocolManager.setupProtocol()
+            this.protocolManager.beforeSpec({
               ...this.spec,
               instanceId: randomUUID(),
             })
 
             telemetryManager.mark(INITIALIZATION_MARK_NAMES.CONNECT_PROTOCOL_TO_BROWSER_START)
-            await browsers.connectProtocolToBrowser({ browser: this.browser, foundBrowsers: this.options.browsers, protocolManager })
+            await browsers.connectProtocolToBrowser({ browser: this.browser, foundBrowsers: this.options.browsers, protocolManager: studio.protocolManager })
             telemetryManager.mark(INITIALIZATION_MARK_NAMES.CONNECT_PROTOCOL_TO_BROWSER_END)
-
-            // Attach to the proxy only after connectToBrowser has initialized network capture.
-            this.protocolManager = protocolManager
 
             telemetryManager.mark(INITIALIZATION_MARK_NAMES.CONNECT_STUDIO_TO_BROWSER_START)
             await browsers.connectStudioToBrowser({ browser: this.browser, foundBrowsers: this.options.browsers, studioManager: studio })
