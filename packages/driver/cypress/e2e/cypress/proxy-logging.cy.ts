30df656bbefd9a63a3bf7d37cca91c6ffdf7a890
sarang-001 <techietriumphbusiness@gmail.com>
2026-07-11 21:32:22 +0530

    fix: treat 3xx responses as successful in Command Log
    The getIndicator() function in proxy-logging.ts only treated 200-299
    status codes as 'successful', causing 304 and other 3xx responses to
    show as red dots in the Command Log. This is inconsistent with the
    server-side isOk() function which treats both 2xx and 3xx as OK.
    
    Update getIndicator() to treat status codes 200-399 as successful,
    aligning with the server-side behavior.
    
    Closes #34066

--- packages/driver/cypress/e2e/cypress/proxy-logging.cy.ts
+++ packages/driver/cypress/e2e/cypress/proxy-logging.cy.ts
@@ -248,6 +248,56 @@ describe('Proxy Logging', () => {
         })
       })
 
+      it('shows successful indicator for 304 Not Modified responses', (done) => {
+        cy.intercept('/some-url', { statusCode: 304 }).as('alias')
+        .then(() => {
+          // tslint:disable:no-floating-promises
+          fetch('/some-url')
+        })
+
+        cy.on('log:changed', (log) => {
+          if (log.displayName !== 'fetch') return
+
+          try {
+            expect(log.renderProps).to.deep.include({
+              indicator: 'successful',
+              message: 'GET 304 /some-url',
+            })
+
+            done()
+          } catch (error) {
+            // don't throw, eventually the log update will come in
+            // eslint-disable-next-line no-console
+            console.error('assertion error', error)
+          }
+        })
+      })
+
+      it('shows successful indicator for 3xx redirect responses', (done) => {
+        cy.intercept('/some-url', { statusCode: 301, headers: { Location: '/other-url' } }).as('alias')
+        .then(() => {
+          // tslint:disable:no-floating-promises
+          fetch('/some-url', { redirect: 'manual' })
+        })
+
+        cy.on('log:changed', (log) => {
+          if (log.displayName !== 'fetch') return
+
+          try {
+            expect(log.renderProps).to.deep.include({
+              indicator: 'successful',
+              message: 'GET 301 /some-url',
+            })
+
+            done()
+          } catch (error) {
+            // don't throw, eventually the log update will come in
+            // eslint-disable-next-line no-console
+            console.error('assertion error', error)
+          }
+        })
+      })
+
       it('works with forceNetworkError', () => {
         const logs = new Map()
 
