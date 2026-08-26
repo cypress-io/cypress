da6f9c8f79cae552e025d2f8d0745689b8488230
Bill Glesias <bglesias@gmail.com>
2026-08-26 14:38:46 -0400

    chore: test npm releasing of packages

--- .releaserc.js
+++ .releaserc.js
@@ -26,6 +26,6 @@ module.exports = {
   ],
   extends: 'semantic-release-monorepo',
   branches: [
-    { name: 'develop', channel: 'latest' },
+    { name: 'chore/test_16_release_npm_packages', channel: 'latest' },
   ],
 }
